90ff2a5e6d71cb695e1552f37bc4c79534a053e7
TomOne <user.email@fake.com>
2013-06-23 22:36:20 +0200

    added other automatic packages

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 chocolateyautomaticpackages
 ===========================
+Annotations:
+* codelite package has static version in Ketarin
+* compact-timer too
+* encfs4win too
+* ffmpeg too
+* freeciv download link must be handled with regex
---
+++ _output/encfs4win/1.7.4/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'encfs4win'
+$url = 'http://members.ferrara.linux.it/freddy77/encfs.zip'
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage $packageName $url $unzipLocation
