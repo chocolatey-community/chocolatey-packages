144e153f1fda0a98ce3da3ed7d452420a64e069e
TomOne <user.email@fake.com>
2013-07-04 23:08:09 +0200

    updating, adding mp3tag package

---
+++ _output/tv-browser/3.3.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'tv-browser'
+$fileType = 'exe'
+$silentArgs = '/S'
+$url = 'http://kent.dl.sourceforge.net/project/tvbrowser/TV-Browser Releases (Java 6 and higher)/3.3.1/tvbrowser_3.3.1_win32.exe'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
