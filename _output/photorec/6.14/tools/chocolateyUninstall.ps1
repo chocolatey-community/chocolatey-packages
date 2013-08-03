579addb52717702d19f678184b6a340a591ba5f3
TomOne <user.email@fake.com>
2013-08-04 00:48:37 +0200

    migrated testdisk and photorec to testdisk-photorec, better icon for maxima

--- _output/photorec/6.14/tools/chocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$desktop = [Environment]::GetFolderPath("Desktop")
-if (Test-Path "$desktop\PhotoRec.lnk") {Remove-Item "$desktop\PhotoRec.lnk"}
-
-$startMenu = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::StartMenu))
-if (Test-Path "$startMenu\Programs\PhotoRec.lnk") {Remove-Item "$startMenu\Programs\PhotoRec.lnk"}
