bfae46a37f7879ae5f44219821530c727fbd631f
TomOne <user.email@fake.com>
2013-09-13 18:13:41 +0200

    added flashplayerplugin, flashplayeractivex and javaruntime

---
+++ flashplayerplugin/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage '{{PackageName}}' 'msi' '/quiet /norestart REMOVE_PREVIOUS=YES' '{{DownloadUrl}}'
---
+++ javaruntime/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+﻿$name   = '{{PackageName}}'
+# Find download URLs at http://www.java.com/en/download/manual.jsp
+$url    = '{{DownloadUrl}}'
+$url64  = '{{DownloadUrlx64}}'
+$type   = 'exe'
+$silent = '/s'
+$java   = Join-Path $env:ProgramFiles 'Java\jre7'
+$bin    = Join-Path $java 'bin'
+	
+try {	
+  Install-ChocolateyPackage $name $type $silent $url
+	
+  $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
+  if($is64bit) { 
+    Install-ChocolateyPackage $name $type $silent $url64
+  }
+	
+  Install-ChocolateyPath $bin 'Machine'
+  Start-ChocolateyProcessAsAdmin @"
+[Environment]::SetEnvironmentVariable('JAVA_HOME', '$java', 'Machine')
+"@
+	
+  Write-ChocolateySuccess $name
+}
+catch {
+	Write-ChocolateyFailure $name $($_.Exception.Message)
+	return
+}
