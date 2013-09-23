c6b4b7ebfb13bd46b6ddc1d1f750df04fb955384
TomOne <user.email@fake.com>
2013-09-23 02:16:42 +0200

    updating

---
+++ _output/sparkleshare/1.0.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'sparkleshare'
+$fileType = 'msi'
+$silentArgs = '/qn'
+$url = 'https://github.com/downloads/hbons/SparkleShare/sparkleshare-windows-1.0.0.msi'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
