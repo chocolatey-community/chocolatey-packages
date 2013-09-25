33bd0578f9bd78b2a7ec0b4cdb7991fd6dc14369
TomOne <user.email@fake.com>
2013-09-25 23:33:00 +0200

    updating some packages, added speccy

---
+++ _output/maxima/5.31.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'maxima'
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT'
+$url = 'http://heanet.dl.sourceforge.net/project/maxima/Maxima-Windows/5.31.1-Windows/maxima-5.31.1.exe'
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
