fcb3df981a9b3476f0d3e58e6677cbfe856c1524
TomOne <user.email@fake.com>
2013-09-27 22:27:00 +0200

    updating

--- youtube-dl/tools/chocolateyInstall.ps1
+++ youtube-dl/tools/chocolateyInstall.ps1
@@ -6,21 +6,21 @@ try {
   $exeDir = "$installDir\youtube-dl.exe"
 
     # Copy 32-bit ffmpeg.exe and ffprobe.exe to tools folder of youtube-dl package
-  if (Test-Path "$env:ChocolateyInstall\lib\ffmpeg.1.2\tools\ffmpeg-1.2-win32-static\bin") {
-    Copy-Item "$env:ChocolateyInstall\lib\ffmpeg.1.2\tools\ffmpeg-1.2-win32-static\bin\ffmpeg.exe" "$installDir"
-    Copy-Item "$env:ChocolateyInstall\lib\ffmpeg.1.2\tools\ffmpeg-1.2-win32-static\bin\ffprobe.exe" "$installDir"
+  if (Test-Path "$env:ChocolateyInstall\lib\ffmpeg.2.0.1\tools\ffmpeg-2.0.1-win32-static\bin") {
+    Copy-Item "$env:ChocolateyInstall\lib\ffmpeg.2.0.1\tools\ffmpeg-2.0.1-win32-static\bin\ffmpeg.exe" "$installDir"
+    Copy-Item "$env:ChocolateyInstall\lib\ffmpeg.2.0.1\tools\ffmpeg-2.0.1-win32-static\bin\ffprobe.exe" "$installDir"
   }
 
   # Copy 64-bit ffmpeg.exe and ffprobe.exe to tools folder of youtube-dl package
-  if (Test-Path "$env:ChocolateyInstall\lib\ffmpeg.1.2\tools\ffmpeg-1.2-win64-static\bin") {
-    Copy-Item "$env:ChocolateyInstall\lib\ffmpeg.1.2\tools\ffmpeg-1.2-win64-static\bin\ffmpeg.exe" "$installDir"
-    Copy-Item "$env:ChocolateyInstall\lib\ffmpeg.1.2\tools\ffmpeg-1.2-win64-static\bin\ffprobe.exe" "$installDir"
+  if (Test-Path "$env:ChocolateyInstall\lib\ffmpeg.2.0.1\tools\ffmpeg-2.0.1-win64-static\bin") {
+    Copy-Item "$env:ChocolateyInstall\lib\ffmpeg.2.0.1\tools\ffmpeg-2.0.1-win64-static\bin\ffmpeg.exe" "$installDir"
+    Copy-Item "$env:ChocolateyInstall\lib\ffmpeg.2.0.1\tools\ffmpeg-2.0.1-win64-static\bin\ffprobe.exe" "$installDir"
   }
 
   Get-ChocolateyWebFile $packageName $exeDir $url
 
-  Write-ChocolateySuccess "$packageName"
+  Write-ChocolateySuccess $packageName
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
   throw
 }
