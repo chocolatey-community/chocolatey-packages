c81524959ce5dc11712fd97aa90c61c7e7afdb03
TomOne <user.email@fake.com>
2013-10-11 19:44:01 +0200

    updated some packages, removed songbird package, because the development stopped and all related services are offline

---
+++ _output/assaultcube/1.2.0.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'assaultcube'
+$url = 'http://switch.dl.sourceforge.net/project/actiongame/AssaultCube Version 1.2.0.0/AssaultCube_v1.2.0.0.exe'
+$zipPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\assaultcube.zip"
+$assaultcubePath = 
+if (Test-Path "$env:ProgramFiles\AssaultCube_v1.2.0.0") {
+    $unzipLocation = "$env:ProgramFiles\AssaultCube_v1.2.0.0"
+} else {
+    $unzipLocation = "${env:ProgramFiles(x86)}\AssaultCube_v1.2.0.0"
+}
+
+Install-ChocolateyPackage $packageName 'exe' '/S' $url
+Install-ChocolateyZipPackage $packageName $zipPath $unzipLocation
---
+++ _output/autohotkey_l.install/1.1.13.01/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$name   = 'autohotkey_l.install'
+$url = 'http://l.autohotkey.net/v/AutoHotkey111301_Install.exe'
+$silent = '/S'
+
+$is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64  
+if($is64bit) { $silent += ' /x64' }
+ 
+Install-ChocolateyPackage $name 'exe' $silent $url
