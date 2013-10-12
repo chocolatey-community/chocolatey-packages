147550ba159ef26546efd56b92bce5415cd69d88
TomOne <user.email@fake.com>
2013-10-12 15:26:33 +0200

    updated some packages with their icon locations

---
+++ _output/alldup/3.4.24.20131012/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'alldup'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT'
+$url = 'http://www.allsync.de/download/alldup.exe'
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url
