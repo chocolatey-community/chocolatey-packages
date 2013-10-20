f00064c1a33b5c3822c30630c0082aef619b4000
TomOne <user.email@fake.com>
2013-10-20 18:37:20 +0200

    updated autohotkey_l.portable, chromium and mkvtoolnix, firefox and thunderbird now check if the latest version is already installed and sikp dowlnoad/installing in that case

--- Firefox/tools/chocolateyInstall.ps1
+++ Firefox/tools/chocolateyInstall.ps1
@@ -9,36 +9,45 @@ $urlFile = 'https://www.mozilla.org/en-US/firefox/all/'
 $filePath = "$env:TEMP\chocolatey\$packageName"
 $fileFullPath = "$filePath\downloadLinks.html"
 
+$uninstallEntry = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Mozilla Firefox ${version}*"
+$uninstallEntryx86 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Mozilla Firefox ${version}*"
+
 try {
 
-    if (-not (Test-Path $filePath)) {
-        New-Item -ItemType directory -Path $filePath
-    }
+    if ((Test-Path $uninstallEntry) -or (Test-Path $uninstallEntryx86)) {
+        Write-Host "Firefox $version is already installed."
+    } else {
 
-    Get-ChocolateyWebFile $packageName $fileFullPath $urlFile
+        if (-not (Test-Path $filePath)) {
+            New-Item -ItemType directory -Path $filePath
+        }
 
-    # The $out variable contains a value when Firefox supports the user language.
-    # Otherwise it will download the en-US installer.
+        Get-ChocolateyWebFile $packageName $fileFullPath $urlFile
 
-    $out = Select-String -Path $fileFullPath -Pattern "os=win&amp;lang=$language`""
-    if ($out -eq $null) {
+        # The $out variable contains a value when Firefox supports the user language.
+        # Otherwise it will download the en-US installer.
+
         $out = Select-String -Path $fileFullPath -Pattern "os=win&amp;lang=$language`""
+        if ($out -eq $null) {
+            $out = Select-String -Path $fileFullPath -Pattern "os=win&amp;lang=$language`""
+        }
+
+        if ($out -eq $null) {
+            $language = (Get-Culture).TwoLetterISOLanguageName
+            $out = Select-String -Path $fileFullPath -Pattern "os=win&amp;lang=$language`""
+        }
+
+        if ($out -eq $null) {
+            $language = 'en-US' # Fallback language if others fail
+        }
+
+        $url = "http://download.mozilla.org/?product=firefox-$version&os=win&lang=$language"
+
+        Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+
     }
-
-    if ($out -eq $null) {
-        $language = "$language" -replace '-[a-z]{2}', '' # Remove country code and hyphen
-        $out = Select-String -Path $fileFullPath -Pattern "os=win&amp;lang=$language`""
-    }
-
-    if ($out -eq $null) {
-        $language = 'en-US' # Fallback language if others fail
-    }
-
-    $url = "http://download.mozilla.org/?product=firefox-$version&os=win&lang=$language"
-
-    Install-ChocolateyPackage $packageName $fileType $silentArgs $url
 
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
   throw 
 }
