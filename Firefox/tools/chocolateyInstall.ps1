5b010aa953e4bf7476c3750b844e2ea3099565a2
TomOne <user.email@fake.com>
2013-12-17 01:35:09 +0100

    package updates, improved firefox package

--- Firefox/tools/chocolateyInstall.ps1
+++ Firefox/tools/chocolateyInstall.ps1
@@ -1,50 +1,90 @@
-﻿$packageName = '{{PackageName}}'
+﻿# ---------------- Function definitions ------------------
+
+function GetLocale($installArguments) {
+
+    $availableLocales = Get-Content "$env:TEMP\chocolatey\Firefox\availableLocales.html"
+
+    # --- Get locale from installArgs if specified
+
+    $argumentMap = ConvertFrom-StringData $installArguments
+    $localeFromInstallArgs = $argumentMap.Item('l')
+
+    # ---
+
+    # --- Get already installed locale if available
+
+    $regUninstallDir = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
+    $regUninstallDirWow64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
+
+    $uninstallPaths = $(Get-ChildItem $regUninstallDir).Name + $(Get-ChildItem $regUninstallDirWow64).Name
+    $uninstallPath = $uninstallPaths -match "Mozilla Firefox [\d\.]+ \([^\s]+ [a-zA-Z\-]+\)" | Select -First 1
+
+    $alreadyInstalledLocale = $uninstallPath -replace ".+\s([a-zA-Z\-]+)\)", '$1'
+
+    # ---
+
+    # --- Other locales
+
+    $systemLocaleAndCountry = (Get-Culture).Name
+    $systemLocaleTwoLetter = (Get-Culture).TwoLetterISOLanguageName
+    $fallbackLocale = 'en-US'
+
+    # ---
+
+    $locales = $localeFromInstallArgs, $alreadyInstalledLocale, $systemLocaleAndCountry, $systemLocaleTwoLetter, $fallbackLocale
+
+    foreach ($locale in $locales) {
+        $localeMatch = $availableLocales -match "os=win&amp;lang=$locale`"" | Select -First 1
+        if ($localeMatch -and $locale -ne $null) {
+            break
+        }
+    }
+
+    return $locale
+}
+
+function AlreadyInstalled($version) {
+    $uninstallEntry = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Mozilla Firefox ${version}*"
+    $uninstallEntryWow64 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Mozilla Firefox ${version}*"
+
+    if ((Test-Path $uninstallEntry) -or (Test-Path $uninstallEntryWow64)) {
+        return $true
+    } else {
+        return $false
+    }
+}
+
+# ----------------------------------
+
+
+
+
+$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $version = '{{PackageVersion}}'
-$silentArgs = '-ms'
-$language = (Get-Culture).Name # Get language and country code separated by hyphen
-#$language = 'xx-XX' # Language override for testing purposes
 
 $urlFile = 'https://www.mozilla.org/en-US/firefox/all/'
 $filePath = "$env:TEMP\chocolatey\$packageName"
-$fileFullPath = "$filePath\downloadLinks.html"
-
-$uninstallEntry = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Mozilla Firefox ${version}*"
-$uninstallEntryx86 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Mozilla Firefox ${version}*"
 
 try {
+        $alreadyInstalled = AlreadyInstalled($version)
 
-    if ((Test-Path $uninstallEntry) -or (Test-Path $uninstallEntryx86)) {
+    if ($alreadyInstalled) {
         Write-Host "Firefox $version is already installed."
     } else {
+
 
         if (-not (Test-Path $filePath)) {
             New-Item -ItemType directory -Path $filePath
         }
 
-        Get-ChocolateyWebFile $packageName $fileFullPath $urlFile
+        Get-ChocolateyWebFile 'locales list for Firefox' "$env:TEMP\chocolatey\$packageName\availableLocales.html" $urlFile
 
-        # The $out variable contains a value when Firefox supports the user language.
-        # Otherwise it will download the en-US installer.
+        $locale = GetLocale($installArguments)
 
-        $out = Select-String -Path $fileFullPath -Pattern "os=win&amp;lang=$language`""
-        if ($out -eq $null) {
-            $out = Select-String -Path $fileFullPath -Pattern "os=win&amp;lang=$language`""
-        }
+        $url = "http://download.mozilla.org/?product=firefox-$version&os=win&lang=$locale"
 
-        if ($out -eq $null) {
-            $language = (Get-Culture).TwoLetterISOLanguageName
-            $out = Select-String -Path $fileFullPath -Pattern "os=win&amp;lang=$language`""
-        }
-
-        if ($out -eq $null) {
-            $language = 'en-US' # Fallback language if others fail
-        }
-
-        $url = "http://download.mozilla.org/?product=firefox-$version&os=win&lang=$language"
-
-        Install-ChocolateyPackage $packageName $fileType $silentArgs $url
-
+        Install-ChocolateyPackage $packageName $fileType '-ms' $url
     }
 
 } catch {
