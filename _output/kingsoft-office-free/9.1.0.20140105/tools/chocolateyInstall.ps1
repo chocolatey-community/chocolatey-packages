373b8a0a84cb842a8fb190c1f6dd3d58701888f8
TomOne <user.email@fake.com>
2014-01-05 02:40:21 +0100

    fixed and bumped kingsoft-office-free package

--- _output/kingsoft-office-free/9.1.0.20140105/tools/chocolateyInstall.ps1
+++ _output/kingsoft-office-free/9.1.0.20140105/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'kingsoft-office-free'
 $fileType = 'exe'
 $silentArgs = '/S'
-$url = '{{DownloadUrl}}'
+$url = 'http://www.ksosoft.com/download/office_free_2013.exe'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
