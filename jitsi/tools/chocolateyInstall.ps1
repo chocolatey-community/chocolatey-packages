6d551833e643fdca32102ac137a6f1740a03f22e
TomOne <user.email@fake.com>
2014-01-07 19:02:26 +0100

    improved jitsi package

--- jitsi/tools/chocolateyInstall.ps1
+++ jitsi/tools/chocolateyInstall.ps1
@@ -1,14 +1,8 @@
-﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
-
-$packageName = 'jitsi' # arbitrary name for the package, used in messages
-$installerType = 'MSI' #only one of these two: exe or msi
+﻿$packageName = '{{PackageName}}'
+$installerType = 'msi'
 $url = '{{DownloadUrl}}'
 $url64 = '{{DownloadUrlx64}}'
-$silentArgs = '/quiet' 
+$silentArgs = '/passive'
 $validExitCodes = @(0) 
 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
-
-
-
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 -validExitCodes $validExitCodes
