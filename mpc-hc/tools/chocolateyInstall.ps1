f604a42c9e45f4ca09a78d3d6ff9070e2ecb758f
TomOne <user.email@fake.com>
2014-01-18 22:26:41 +0100

    easier way to download from SourceForge

--- mpc-hc/tools/chocolateyInstall.ps1
+++ mpc-hc/tools/chocolateyInstall.ps1
@@ -1,8 +1,7 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT'
-$urlArray = {{DownloadUrlx64}} # { {DownloadUrlx64} } here is not the actual 64-bit URL, but it’s an array that contains both 32- and 64-bit URLs.
-$url = $urlArray[0] 
-$url64bit = $urlArray[1]
+$url = '{{DownloadUrl}}'
+$url64bit = '{{DownloadUrlx64}}'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
