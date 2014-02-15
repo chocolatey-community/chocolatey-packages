4a4a9e4b42b9c30abf1022c98e84823561a15e26
TomOne <user.email@fake.com>
2014-02-15 21:45:11 +0100

    bumped some packages

--- avidemux/tools/chocolateyInstall.ps1
+++ avidemux/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/S'
-$url = 'http://sourceforge.net/projects/avidemux/files/avidemux/{{PackageVersion}}/avidemux_{{PackageVersion}}_win32.exe/download'
-$url64bit = 'http://sourceforge.net/projects/avidemux/files/avidemux/{{PackageVersion}}/avidemux_{{PackageVersion}}_win64.exe/download'
+$url = '{{DownloadUrl}}'
+$url64bit = '{{DownloadUrlx64}}'
 $validExitCodes = @(0,1223)
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit -validExitCodes $validExitCodes
