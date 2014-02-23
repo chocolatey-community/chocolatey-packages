986d08f6e20b45cdbee019132e2eaeaa793f23e6
TomOne <user.email@fake.com>
2014-02-23 16:08:06 +0100

    fixed nomacs package sf urls and winXP support

--- nomacs/tools/chocolateyInstall.ps1
+++ nomacs/tools/chocolateyInstall.ps1
@@ -1,18 +1,26 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/S'
-$url = 'http://sourceforge.net/projects/nomacs/files/nomacs-{{PackageVersion}}/nomacs-setup.{{PackageVersion}}-x86.exe/download'
-$url64bit = 'http://sourceforge.net/projects/nomacs/files/nomacs-{{PackageVersion}}/nomacs-setup-{{PackageVersion}}-x64.exe/download'
+$urlsArray = {{DownloadUrlx64}}
+$urlWinXp = 'http://sourceforge.net/projects/nomacs/files/nomacs-1.6.2/nomacs-setup-1.6.3-WinXP-x86.exe/download'
+$url = $urlsArray[0]
+$url64bit = $urlsArray[1]
 
-# If Windows 2000/XP, download matching version
-$WinVersion = [System.Environment]::OSVersion.Version.Major
-if ($WinVersion -eq 5) {
-Install-ChocolateyPackage $packageName $fileType $silentArgs 'http://sourceforge.net/projects/nomacs/files/nomacs-{{PackageVersion}}/nomacs-setup-{{PackageVersion}}-WinXP-x86.exe/download'
-}
+try {
+
+    # If Windows 2000/XP, download matching version
+    $WinVersion = [System.Environment]::OSVersion.Version.Major
+    if ($WinVersion -eq 5) {
+        Write-Host 'You have Windows 2000/XP. There is no newer version than Nomacs 1.6.3 available for these operating systems.'
+        $url = $urlWinXp
+        $url64bit =$urlWinXp
+    }
 
 
-# If Windows Vista/7/8 or later, download matching version
-$WinVersion = [System.Environment]::OSVersion.Version.Major
-if ($WinVersion -ge 6) {
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
+    # Else download the version for Windows Vista/7/8 or later
+    Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
+
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
 }
