53331e11bafc33dfca411ba3a61566a8cde25b7c
TomOne <user.email@fake.com>
2014-04-12 17:29:36 +0200

    updating

--- automatic/_output/classic-shell/4.0.6/tools/chocolateyInstall.ps1
+++ automatic/_output/classic-shell/4.0.6/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'classic-shell'
 $installerType = 'exe'
 $installArguments = '/passive'
-$url = '{{DownloadUrl}}'
+$url = 'http://www.mediafire.com/download/bvfx3vfkjohd8f6/ClassicShellSetup_4_0_6.exe'
 
 Install-ChocolateyPackage $packageName $installerType $installArguments $url
