6f5dd848656a07db58d81b6102911ee0fe7311cf
TomOne <user.email@fake.com>
2014-04-12 17:50:54 +0200

    fixed keepassx-langfiles not working when UAC enabled

--- manual/keepass-langfiles/tools/chocolateyInstall.ps1
+++ manual/keepass-langfiles/tools/chocolateyInstall.ps1
@@ -1,24 +1,24 @@
 ﻿try {
 
-  $name = "keepass-langfiles"
-  $scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $fileFullPath = "$scriptPath\keepass_2.x_langfiles.zip"
-  
-  $keepasspath = "$env:ProgramFiles\KeePass Password Safe 2"
-  $keepasspathx86 = "${env:ProgramFiles(x86)}\KeePass Password Safe 2"
-  if (Test-Path "$keepasspath") {$destination = "$keepasspath"}
-  if (Test-Path "$keepasspathx86") {$destination = "$keepasspathx86"}
-  
-  $extractPath = "$scriptPath\keepass-langfiles"
-  if (-not (Test-Path $extractPath)) {
-    mkdir $extractPath
-  }
+    $name = "keepass-langfiles"
+    $scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $fileFullPath = "$scriptPath\keepass_2.x_langfiles.zip"
 
-  Get-ChocolateyUnzip $fileFullPath $extractPath
-  Start-ChocolateyProcessAsAdmin "Copy-Item -Force '$extractPath\*.lngx' '$destination'"
+    $keepasspath = "$env:ProgramFiles\KeePass Password Safe 2"
+    $keepasspathx86 = "${env:ProgramFiles(x86)}\KeePass Password Safe 2"
+    if (Test-Path "$keepasspath") {$destination = "$keepasspath"}
+    if (Test-Path "$keepasspathx86") {$destination = "$keepasspathx86"}
 
-    Write-ChocolateySuccess $name
+    $extractPath = "$scriptPath\keepass-langfiles"
+    if (-not (Test-Path $extractPath)) {
+        mkdir $extractPath
+    }
+
+    Get-ChocolateyUnzip $fileFullPath $extractPath
+    Start-ChocolateyProcessAsAdmin "Copy-Item -Force '$extractPath\*.lngx' '$destination'"
+
+        Write-ChocolateySuccess $name
 } catch {
-  Write-ChocolateyFailure $name $($_.Exception.Message)
-  throw
+    Write-ChocolateyFailure $name $($_.Exception.Message)
+    throw
 }
