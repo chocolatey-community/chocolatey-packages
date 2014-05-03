8f709a2019ae2edc2fddbeef3ff676acb4b9a7e0
TomOne <user.email@fake.com>
2014-05-04 00:55:56 +0200

    uninstall script for brackets

--- automatic/Brackets/tools/chocolateyUninstall.ps1
+++ automatic/Brackets/tools/chocolateyUninstall.ps1
@@ -1,16 +1,17 @@
-﻿<#
-$package = '{{PackageName}}'
+﻿$packageName = '{{PackageName}}'
 
 try {
 
-  # http://stackoverflow.com/questions/450027/uninstalling-an-msi-file-from-the-command-line-without-using-msiexec
-  $msiArgs = "/X{CA6586CA-1C03-488B-B791-2A4533C1B1C6} /qb-! REBOOT=ReallySuppress"
-  Start-ChocolateyProcessAsAdmin "$msiArgs" 'msiexec'
+    $app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq 'Brackets'}
+    if ($app) {
+        $msiArgs = $('/x' + $app.IdentifyingNumber + ' /q REBOOT=ReallySuppress')
+        Start-ChocolateyProcessAsAdmin $msiArgs 'msiexec'
+    }
 
-  Write-ChocolateySuccess $package
+    Write-ChocolateySuccess $packageName
+
+    
 } catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
 }
-
-#>
