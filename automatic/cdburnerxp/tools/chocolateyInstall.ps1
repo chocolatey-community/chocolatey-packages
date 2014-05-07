1c32ee6891ce20936bbbc0b2e2b9723f519e3c16
Gary Ewan Park <gep13@gep13.co.uk>
2014-05-07 19:00:45 +0000

    (CDBurnerXP) Changed exe to msi
    Since the exe for CDBurnerXP now bundles OpenCandy, and the download link for the non OpenCandy version creating a GUID for each new user, I have switched to using the MSI for the installation, which doesn't have OpenCandy bundled in.

--- automatic/cdburnerxp/tools/chocolateyInstall.ps1
+++ automatic/cdburnerxp/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
-$installerType = 'EXE'
+$installerType = 'MSI'
 $32BitUrl  = '{{DownloadUrl}}'
 $64BitUrl  = '{{DownloadUrlx64}}'
-$silentArgs = '/verysilent'
+$silentArgs = '/quiet'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
--- automatic/cdburnerxp/tools/chocolateyUninstall.ps1
+++ automatic/cdburnerxp/tools/chocolateyUninstall.ps1
@@ -1,15 +1,13 @@
-$packageName = '{{PackageName}}'
-$installerType = 'EXE'
-$silentArgs = '/SILENT'
-$validExitCodes = @(0,1)
+$packageName = "{{PackageName}}"
+$packageWildCard = $packageName + "*";
 
 try {
-	# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{7E265513-8CDA-4631-B696-F40D983F3B07}_is1
-	$file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{7E265513-8CDA-4631-B696-F40D983F3B07}_is1").UninstallString
-	Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
-  
-	Write-ChocolateySuccess $package
-} catch {
-	Write-ChocolateyFailure $package "$($_.Exception.Message)" 
+	$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like $packageWildCard  -and ($_.Version -eq "{{PackageVersion}}") }
+	$result = $app.Uninstall();
+
+	Write-ChocolateySuccess $packageName
+}
+catch {
+	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
 	throw
 }
