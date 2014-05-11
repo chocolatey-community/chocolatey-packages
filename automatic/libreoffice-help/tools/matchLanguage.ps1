3a7c8f07ca9c13580cfb816bbe93312eb2cfa220
TomOne <user.email@fake.com>
2014-05-11 16:31:32 +0200

    Windows 7 fallback for libreoffice-help i18n detection
    The script now only uses the Windows ≥ 8 specific
    cmdlets if the are available. On Windows ≤ 7, it
    uses the Get-Culture cmdlet, which is far from
    optimal, but currently the workaround I know.
    Get-Culture only returns the locale format
    settings, not the actual Windows UI language.
    For some users, this could pick the wrong
    language.

--- automatic/libreoffice-help/tools/matchLanguage.ps1
+++ automatic/libreoffice-help/tools/matchLanguage.ps1
@@ -3,12 +3,13 @@
     $detectedLangs = @{
         installOverride = $installOverride
         ofExistentInstall = $ofExistentInstall
-        winUiOverride = ''
-        listFirst = ''
+        winUiOverride = $null
+        listFirst = $null
+        fromCulture = $null
         fallback = $fallback
     }
 
-    $detectedLangsOrder = @('installOverride', 'ofExistentInstall', 'winUiOverride', 'listFirst', 'fallback')
+    $detectedLangsOrder = @('installOverride', 'ofExistentInstall', 'winUiOverride', 'listFirst', 'fromCulture', 'fallback')
 
     # Get get language override for user account if present. Overriding the language is possible from the control panel
     if (Get-Command Get-WinUILanguageOverride -ErrorAction SilentlyContinue) {
@@ -17,7 +18,13 @@
 
     # Get the first language from the user account language list. If the language override is not set, this is the
     # actual language of the current user account
-    $detectedLangs.listFirst = (Get-WinUserLanguageList)[0].LanguageTag
+    if (Get-Command Get-WinUserLanguageList -ErrorAction SilentlyContinue) {
+        $detectedLangs.listFirst = (Get-WinUserLanguageList)[0].LanguageTag
+    }
+
+    # The language from Get-Culture is not the actual UI language, only the locale format. I use this because on Windows 7
+    # there seems no other possibility to get the system or user UI language.
+    $detectedLangs.fromCulture = (Get-Culture).Name
 
     # Loop over detected languages and for each detected language, loop over the available language
     # and return the first matched language
