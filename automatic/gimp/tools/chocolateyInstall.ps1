1034b1a48368f2303b31bd5932873631ca09d5c5
TomOne <user.email@fake.com>
2014-05-14 21:45:54 +0200

    gimp: add uninstaller script, installed version recognition

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -1,1 +1,23 @@
-﻿Install-ChocolateyPackage '{{PackageName}}' 'exe' 'SP- /SILENT /NORESTART' '{{DownloadUrl}}' -validExitCodes @(0)
+﻿$packageName = '{{PackageName}}'
+$version = '{{PackageVersion}}'
+$url = '{{DownloadUrl}}'
+$installerType = 'exe'
+$installArgs = 'SP- /SILENT /NORESTART'
+$gimpRegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
+
+
+try {
+
+    if (Test-Path $gimpRegistryPath) {
+        $installedVersion = (Get-ItemProperty -Path $gimpRegistryPath -Name 'DisplayVersion').DisplayVersion
+    }
+
+    if ($installedVersion -eq $version) {
+        Write-Output "GIMP $installedVersion is already installed. Skipping download and installation."
+    } else {
+        Install-ChocolateyPackage $packageName $installerType $installArgs $url -validExitCodes @(0)
+    }
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw 
+}
