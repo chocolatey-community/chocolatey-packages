41bb9622626a016c7c24e417dfdce3adcc6cf7e8
TomOne <user.email@fake.com>
2014-06-06 21:02:33 +0200

    Refactor scripts of PDFCreator package

--- automatic/PDFCreator/tools/chocolateyinstall.ps1
+++ automatic/PDFCreator/tools/chocolateyinstall.ps1
@@ -1,15 +1,43 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'exe'
-$silentArgs = '/VERYSILENT /NORESTART /COMPONENTS="program,ghostscript,comsamples,languages,languages\bosnian,languages\catalan,languages\catalan_valencia,languages\chinese_simplified,languages\chinese_traditional,languages\corsican,languages\croatian,languages\czech,languages\danish,languages\dutch,languages\english,languages\estonian,languages\finnish,languages\french,languages\galician,languages\german,languages\greek,languages\hebrew,languages\hungarian,languages\italian,languages\irish,languages\ligurian,languages\latvian,languages\lithuanian,languages\norwegian_bokmal,languages\polish,languages\portuguese_br,languages\romanian,languages\russian,languages\serbian_cyrillic,languages\slovak,languages\slovenian,languages\spanish,languages\swedish,languages\turkish,languages\valencian_avl"'
+$baseInstallArgs = '/VERYSILENT /NORESTART'
+$fullInstallArgs = $('' +
+    $baseInstallArgs + ' ' +
+    '/COMPONENTS="program,ghostscript,comsamples,' +
+    'languages,languages\bosnian,languages\catalan,languages\catalan_valencia,' +
+    'languages\chinese_simplified,languages\chinese_traditional,' +
+    'languages\corsican,languages\croatian,languages\czech,languages\danish,' +
+    'languages\dutch,languages\english,languages\estonian,languages\finnish,' +
+    'languages\french,languages\galician,languages\german,languages\greek,' +
+    'languages\hebrew,languages\hungarian,languages\italian,languages\irish,' +
+    'languages\ligurian,languages\latvian,languages\lithuanian,' +
+    'languages\norwegian_bokmal,languages\polish,languages\portuguese_br,' +
+    'languages\romanian,languages\russian,languages\serbian_cyrillic,' +
+    'languages\slovak,languages\slovenian,languages\spanish,' +
+    'languages\swedish,languages\turkish,languages\valencian_avl"'
+)
+
 $url = '{{DownloadUrl}}'
 
-# Uninstall PDFCreator if older version is installed
-if (Test-Path "$env:ProgramFiles\PDFCreator") {
-    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "$env:ProgramFiles\PDFCreator\unins000.exe"
-}
+try {
 
-if (Test-Path "${env:ProgramFiles(x86)}\PDFCreator") {
-    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "${env:ProgramFiles(x86)}\PDFCreator\unins000.exe"
-}
+    $uninstallerPathLastPart = 'PDFCreator\unins000.exe'
+    $uninstallerPath = Join-Path $env:ProgramFiles $uninstallerPathLastPart
+    $uninstallerPathx86 = Join-Path ${env:ProgramFiles(x86)} $uninstallerPathLastPart
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url
+    if (Test-Path $uninstallerPathx86) {
+        $uninstallerPath = $uninstallerPathx86
+    }
+
+    # Uninstall PDFCreator if older version is installed, otherwise the installation
+    # of the new version will fail.
+    if (Test-Path $uninstallerPath) {
+        Uninstall-ChocolateyPackage $packageName $installerType $baseInstallArgs $uninstallerPath
+    }
+
+    Install-ChocolateyPackage $packageName $installerType $fullInstallArgs $url
+
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
--- automatic/PDFCreator/tools/chocolateyuninstall.ps1
+++ automatic/PDFCreator/tools/chocolateyuninstall.ps1
@@ -1,12 +1,22 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'exe'
+$baseInstallArgs = '/VERYSILENT /NORESTART'
 
+try {
 
-# Uninstall PDFCreator if older version is installed
-if (Test-Path "$env:ProgramFiles\PDFCreator") {
-    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "$env:ProgramFiles\PDFCreator\unins000.exe"
-}
+    $uninstallerPathLastPart = 'PDFCreator\unins000.exe'
+    $uninstallerPath = Join-Path $env:ProgramFiles $uninstallerPathLastPart
+    $uninstallerPathx86 = Join-Path ${env:ProgramFiles(x86)} $uninstallerPathLastPart
 
-if (Test-Path "${env:ProgramFiles(x86)}\PDFCreator") {
-    Uninstall-ChocolateyPackage $packageName $installerType "/VERYSILENT /NORESTART" "${env:ProgramFiles(x86)}\PDFCreator\unins000.exe"
+    if (Test-Path $uninstallerPathx86) {
+        $uninstallerPath = $uninstallerPathx86
+    }
+
+    if (Test-Path $uninstallerPath) {
+        Uninstall-ChocolateyPackage $packageName $installerType $baseInstallArgs $uninstallerPath
+    }
+
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
 }
