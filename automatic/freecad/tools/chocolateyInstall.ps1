512c7d54fda17f596b93e3ca452850e5693ab574
TomOne <user.email@fake.com>
2014-07-06 03:42:29 +0200

    Adapt freecad automatic job to work with updated installers, add 64-bit version

--- automatic/freecad/tools/chocolateyInstall.ps1
+++ automatic/freecad/tools/chocolateyInstall.ps1
@@ -1,7 +1,10 @@
 ﻿$packageName = '{{PackageName}}'
-$fileType = 'msi'
-$silentArgs = '/quiet'
-# {\{DownloadUrlx64}\} gets “misused” here as 32-bit download link due to limitations of Ketarin/chocopkgup
-$url = '{{DownloadUrlx64}}'
+$fileType = 'exe'
+$silentArgs = '/S'
+# {\{DownloadUrlx64}\} gets “misused” here as array of the download
+# links due to limitations of Ketarin/chocopkgup
+$urlArray = {{DownloadUrlx64}}
+$url = $urlArray[0]
+$url64 = $urlArray[1]
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
