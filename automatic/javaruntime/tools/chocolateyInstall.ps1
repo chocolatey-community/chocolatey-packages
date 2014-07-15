a6242b37e833cdc99fe41192cfe1b3c993019df2
TomOne <user.email@fake.com>
2014-07-15 22:39:34 +0200

    Deduplicate *.ps1 files for the JRE package, update jobs.db

--- automatic/javaruntime/tools/chocolateyInstall.ps1
+++ automatic/javaruntime/tools/chocolateyInstall.ps1
@@ -19,27 +19,42 @@
     $jreForPathVariable = Join-Path $javaHome 'bin'
 
     $thisJreInstalledHash = thisJreInstalled($version)
-
-    # Checks if JRE 32/64-bit in the same version is already installed,
-    # otherwise it downloads and installs it.
-    # This is to avoid unnecessary downloads and 1603 errors.
-    if ($thisJreInstalledHash.x86_32) {
-        Write-Output "Java Runtime Environment $version (32-bit) is already installed. Skipping download and installation"
-    } else {
-        Install-ChocolateyPackage $packageName $installerType $installArgs $url
-    }
-
-    # Only check for the 64-bit version if the system is 64-bit
-
     $osBitness = Get-ProcessorBits
-    if ($osBitness -eq 64) {
-        if ($thisJreInstalledHash.x86_64) {
-            Write-Output "Java Runtime Environment $version (64-bit) is already installed. Skipping download and installation"
+
+    # This is the code for both javaruntime and javaruntime-platformspecific packages.
+    # If the package is javaruntime-platformspecific, only install the jre version
+    # based on the OS bitness, otherwise install both 32- and 64-bit versions
+    if ($packageName -match 'platformspecific') {
+
+        if ($thisJreInstalledHash.x86_32 -or $thisJreInstalledHash.x86_64) {
+            Write-Output "Java Runtime Environment $version is already installed. Skipping download and installation to avoid 1603 errors."
         } else {
-            # Here $url64 is used twice to obtain the correct message from Chocolatey
-            # that it installed the 64-bit version, otherwise it would display 32-bit,
-            # regardless of the actual bitness of the software.
-            Install-ChocolateyPackage $packageName $installerType $installArgs $url64 $url64
+            Install-ChocolateyPackage $packageName $installerType $installArgs $url $url64
+        }
+
+    } else {
+        # Otherwise it is the javaruntime package which installs both 32- and 64-bit jre versions on 64-bit systems
+
+        # Checks if JRE 32/64-bit in the same version is already installed,
+        # otherwise it downloads and installs it.
+        # This is to avoid unnecessary downloads and 1603 errors.
+        if ($thisJreInstalledHash.x86_32) {
+            Write-Output "Java Runtime Environment $version (32-bit) is already installed. Skipping download and installation"
+        } else {
+            Install-ChocolateyPackage $packageName $installerType $installArgs $url
+        }
+
+        # Only check for the 64-bit version if the system is 64-bit
+
+        if ($osBitness -eq 64) {
+            if ($thisJreInstalledHash.x86_64) {
+                Write-Output "Java Runtime Environment $version (64-bit) is already installed. Skipping download and installation"
+            } else {
+                # Here $url64 is used twice to obtain the correct message from Chocolatey
+                # that it installed the 64-bit version, otherwise it would display 32-bit,
+                # regardless of the actual bitness of the software.
+                Install-ChocolateyPackage $packageName $installerType $installArgs $url64 $url64
+            }
         }
     }
 
