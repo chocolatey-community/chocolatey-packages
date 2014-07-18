6be09411e22b187572e064b6fbd8c2deb43d1623
TomOne <user.email@fake.com>
2014-07-18 22:46:02 +0200

    Split ffdshow package into ffdshow and ffdshow-x86_32

--- automatic/ffdshow-x86_32/tools/chocolateyInstall.ps1
+++ automatic/ffdshow-x86_32/tools/chocolateyInstall.ps1
@@ -4,4 +4,4 @@ $silentArgs = '/VERYSILENT'
 # \{\{DownloadUrlx64\}\} gets “misused” here as has to store both 32- and 64-bit URLs
 $urlHash = {{DownloadUrlx64}}
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $urlHash.url32 $urlHash.url64
+Install-ChocolateyPackage $packageName $fileType $silentArgs $urlHash.url32
