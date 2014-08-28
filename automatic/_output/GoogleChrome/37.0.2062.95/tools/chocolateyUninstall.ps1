32fc2369022fcd41048129165312f17a8fe12eac
TomOne <TomOne@users.noreply.github.com>
2014-08-28 21:02:13 +0200

    Add GoogleChrome built package with improved description.

---
+++ automatic/_output/GoogleChrome/37.0.2062.95/tools/chocolateyUninstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'GoogleChrome'
+
+try {
+
+  $app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq 'Google Chrome'}
+  if ($app) {
+    $msiArgs = $('/x' + $app.IdentifyingNumber + ' /q REBOOT=ReallySuppress')
+    Start-ChocolateyProcessAsAdmin $msiArgs 'msiexec'
+  }
+
+  Write-ChocolateySuccess $packageName
+
+
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
