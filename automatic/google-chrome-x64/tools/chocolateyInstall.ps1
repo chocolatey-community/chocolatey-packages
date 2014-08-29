9a99441fd5e412b1f34dbcc9707a858d290b1e7f
TomOne <TomOne@users.noreply.github.com>
2014-08-30 01:09:05 +0200

    Revert "Workaround for Ketarin's google-chrome* jobs"
    This reverts commit 43f618722209504a153252aa1f24e2796bcc9dbe.

--- automatic/google-chrome-x64/tools/chocolateyInstall.ps1
+++ automatic/google-chrome-x64/tools/chocolateyInstall.ps1
@@ -1,12 +1,7 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = ''
-$url = $('' +
-  'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96' +
-  '%7D%26iid%3D%7B00000000-0000-0000-0000-000000000000%7D%26lang%3Den%26browser' +
-  '%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin' +
-  '%3Dprefers%26ap%3Dx64-stable/update2/installers/ChromeSetup.exe'
-)
+$url = '{{DownloadUrl}}'
 $version = '{{PackageVersion}}'
 
 function Find-CID {
