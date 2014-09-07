6fbea548b427239d2e8a5b25ef3229cbe2d3884b
TomOne <TomOne@users.noreply.github.com>
2014-09-07 15:59:42 +0200

    Bump gimp to 2.8.14.1 (officially released fixed Windows installer for GIMP)

---
+++ automatic/_output/gimp/2.8.14.1/tools/chocolateyUninstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = 'gimp'
+$installerType = 'exe'
+$installArgs = 'SP- /SILENT /NORESTART'
+$gimpRegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
+
+
+try {
+
+  if (Test-Path $gimpRegistryPath) {
+    $uninstallString = (Get-ItemProperty -Path $gimpRegistryPath -Name 'UninstallString').UninstallString
+  }
+
+  if ($uninstallString) {
+    Uninstall-ChocolateyPackage $packageName $installerType $installArgs $uninstallString
+  }
+
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
