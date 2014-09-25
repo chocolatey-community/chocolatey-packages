a2409d9136523333e24291c69f94927b680340db
TomOne <TomOne@users.noreply.github.com>
2014-09-25 21:48:33 +0200

    Add 1223 as valid exit code for qbittorrent

--- automatic/_output/qbittorrent/3.1.10.20140925/tools/chocolateyInstall.ps1
+++ automatic/_output/qbittorrent/3.1.10.20140925/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = '{{PackageName}}'
+﻿$packageName = 'qbittorrent'
 $installerType = 'exe'
 # {\{DownloadUrlx64}\} gets “misused” here as 32-bit download link due to limitations of Ketarin/chocopkgup
-$url = '{{DownloadUrlx64}}'
+$url = 'http://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-3.1.10/qbittorrent_3.1.10_setup.exe/download'
 $silentArgs = '/S'
 $validExitCodes = @(0, 1223)
 
---
+++ automatic/_output/qbittorrent/3.1.10.20140925/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+
+  $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
+
+  Import-Module (Join-Path $scriptPath 'getUninstallString.ps1')
+
+
+  $uninstallString = getUninstallString 'qbittorrent' 'UninstallString'
+  if ($uninstallString) {
+    Uninstall-ChocolateyPackage $packageName 'exe' '/S' $uninstallString
+  }
+
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
