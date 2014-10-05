624ca9ff28af858871293659bd195025e9760d39
TomOne <TomOne@users.noreply.github.com>
2014-10-05 11:29:37 +0000

    Check in updated content of output folder and update jobs.db

---
+++ automatic/_output/blender/2.72/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'blender'
+$installerType = 'exe'
+$url = 'http://download.blender.org/release/Blender2.72/blender-2.72-windows32.exe'
+$url64 = 'http://download.blender.org/release/Blender2.72/blender-2.72-windows64.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 -validExitCodes $validExitCodes
