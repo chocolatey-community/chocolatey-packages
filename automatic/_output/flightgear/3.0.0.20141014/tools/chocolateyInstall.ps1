1b32928d71412e63f2276152afcbd69e0ab5df5d
TomOne <TomOne@users.noreply.github.com>
2014-10-14 23:15:27 +0200

    flightgear: switch to working download mirror

---
+++ automatic/_output/flightgear/3.0.0.20141014/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'flightgear'
+$fileType = "exe"
+$silentArgs = "/VERYSILENT"
+$url = 'http://ftp.icm.edu.pl/packages/flightgear/Windows/Setup FlightGear 3.0.0.exe'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
