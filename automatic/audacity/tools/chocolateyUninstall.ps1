61c1aa99ca92ad67e607ce0abf2598e5634cb993
TomOne <TomOne@users.noreply.github.com>
2014-10-18 19:07:43 +0200

    Add audacity package

---
+++ automatic/audacity/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿try {
+  $processor = Get-WmiObject Win32_Processor
+  $is64bit = $processor.AddressWidth -eq 64
+  if ($is64bit) {
+    $unpath = "${Env:ProgramFiles(x86)}\Audacity\unins000.exe"
+  } else {
+    $unpath = "${Env:ProgramFiles}\Audacity\unins000.exe"
+  }
+  Uninstall-ChocolateyPackage '{{PackageName}}' 'EXE' '/S' "$unpath" -validExitCodes @(0)
+
+  # the following is all part of error handling
+  Write-ChocolateySuccess '{{PackageName}}'
+} catch {
+  Write-ChocolateyFailure '{{PackageName}}' "$($_.Exception.Message)"
+  throw
+}
