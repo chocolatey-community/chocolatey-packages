fcf92a4e8bf39755975151d075bfe32611ac01db
Gary Ewan Park <gep13@gep13.co.uk>
2014-10-29 21:04:16 +0000

    Update after moderation comment
    As per suggestion from @ferventcoder during package moderation:
    
    > You should not point the 32bit link as the 64bit link, it confuses folks.
    
    Removed the 64bit URL, in favour of just 32Bit one, as this is all there is.

--- automatic/fiddler4/tools/chocolateyInstall.ps1
+++ automatic/fiddler4/tools/chocolateyInstall.ps1
@@ -1,8 +1,7 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'EXE'
 $32BitUrl  = '{{DownloadUrl}}'
-$64BitUrl  = $32BitUrl
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
