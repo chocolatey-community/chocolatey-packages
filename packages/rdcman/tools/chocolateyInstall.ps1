f6c842a64c1317af43de2b451a78668af99332a6
Gary Ewan Park <gary.park@dai.co.uk>
2014-11-06 20:48:23 +0000

    (rdcman) Added Package

---
+++ packages/rdcman/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿﻿$packageName = 'rdcman'
+$installerType = 'MSI'
+$32BitUrl = 'http://download.microsoft.com/download/5/2/8/5282718D-87FE-4A4F-9226-789ACF368DB1/RDCMan.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl -validExitCodes $validExitCodes
