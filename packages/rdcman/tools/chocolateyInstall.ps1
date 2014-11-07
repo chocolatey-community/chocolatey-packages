fc1d7e654302b47c2fc777e955a94c9aeaa834c5
Gary Ewan Park <gep13@gep13.co.uk>
2014-11-07 07:51:54 +0000

    (rdcman) Trying to correct issue with encoding

--- packages/rdcman/tools/chocolateyInstall.ps1
+++ packages/rdcman/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿﻿$packageName = 'rdcman'
+﻿$packageName = 'rdcman'
 $installerType = 'MSI'
 $32BitUrl = 'http://download.microsoft.com/download/5/2/8/5282718D-87FE-4A4F-9226-789ACF368DB1/RDCMan.msi'
 $silentArgs = '/quiet'
