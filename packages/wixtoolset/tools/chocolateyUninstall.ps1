3dd93ef6562a90523b671b6d70952fd3dc249957
Gary Ewan Park <gep13@gep13.co.uk>
2014-11-07 21:48:43 +0000

    (wixtoolset) Corrected uninstall to include 32bit Key

--- packages/wixtoolset/tools/chocolateyUninstall.ps1
+++ packages/wixtoolset/tools/chocolateyUninstall.ps1
@@ -1,6 +1,14 @@
 $packageName = 'wixtoolset'
-$uninstallRegKey = 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{3d1f6355-a0b0-49a1-b977-43cdbe771169}'
 $silentArgs = '/uninstall /quiet'
+
+$osBitness = Get-ProcessorBits
+
+# Remove not needed folder with binaries
+if ($osBitness -eq 64) {
+  $uninstallRegKey = 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{3d1f6355-a0b0-49a1-b977-43cdbe771169}'
+} else {
+  $uninstallRegKey = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{3d1f6355-a0b0-49a1-b977-43cdbe771169}'
+}
 
 $uninstallPath = (Get-ItemProperty $uninstallRegKey).BundleCachePath
 
