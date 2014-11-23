8199bdb29ebd932103287dd56f5cea5332616f5c
Gary Ewan Park <gep13@gep13.co.uk>
2014-11-23 20:12:48 +0000

    (#28) Updated package with latest version
    - Changed version number in nuspec file
    - Changed download URL

--- packages/rdcman/tools/chocolateyInstall.ps1
+++ packages/rdcman/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'rdcman'
 $installerType = 'MSI'
-$32BitUrl = 'http://download.microsoft.com/download/5/2/8/5282718D-87FE-4A4F-9226-789ACF368DB1/RDCMan.msi'
+$32BitUrl = 'http://download.microsoft.com/download/A/F/0/AF0071F3-B198-4A35-AA90-C68D103BDCCF/rdcman.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0,3010)
 
