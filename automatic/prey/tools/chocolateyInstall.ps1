633d9a61b776b2cb97e95e4ff98153dc0f62db1e
TomOne <TomOne@users.noreply.github.com>
2014-12-05 22:26:33 +0100

    prey package: fix ketarin job and package scripts for new version

--- automatic/prey/tools/chocolateyInstall.ps1
+++ automatic/prey/tools/chocolateyInstall.ps1
@@ -1,6 +1,26 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/S'
+$version = '{{PackageVersion}}'
 $url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+try {
+
+  $app = Get-WmiObject -Class Win32_Product | Where-Object {
+    $_.Name -eq 'Prey Anti-Theft' -and $_.Version -eq $version}
+
+  if ($app) {
+    Write-Host $("Prey $version is already installed. " +
+      "No need to re-install the same version. " +
+      "Skipping download."
+    )
+  } else {
+    # Proceed with installation
+    Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
+  }
+
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
