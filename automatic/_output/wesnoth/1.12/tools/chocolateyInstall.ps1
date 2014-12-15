22aa143b76af703b16e10d139a597ed89f1d3a2a
TomOne <TomOne@users.noreply.github.com>
2014-12-15 23:12:18 +0000

    Update _output folder

---
+++ automatic/_output/wesnoth/1.12/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'wesnoth'
+$fileType = 'exe'
+$silentArgs = '/S'
+# {\{DownloadUrlx64}\} gets “misused” here as 32-bit download link due to limitations of Ketarin/chocopkgup
+$url = 'http://sourceforge.net/projects/wesnoth/files//wesnoth-1.12/wesnoth-1.12/wesnoth-1.12-win32.exe/download'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url
