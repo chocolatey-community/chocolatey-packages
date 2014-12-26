6f114445a259702d6e345b5225c292beee76f81c
Gary Ewan Park <gep13@gep13.co.uk>
2014-12-26 10:43:58 +0000

    (poweriso) Correcting 64bit Download Url
    - Not sure why I didn't pick up on this earlier, but during moderation,
    it was pointed out that there should be a 64bit download URL for
    PowerIso
    - This has been added in

--- automatic/poweriso/tools/chocolateyInstall.ps1
+++ automatic/poweriso/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = '{{PackageName}}'
 $installerType = 'EXE'
 $32BitUrl  = '{{DownloadUrl}}'
-$64BitUrl  = $32BitUrl
+$64BitUrl  = '{{DownloadUrlx64}}'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
