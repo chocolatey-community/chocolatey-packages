f18eb03aafa64efa4aae4bb065958987470a5c7a
TomOne <TomOne@users.noreply.github.com>
2014-12-27 21:25:07 +0100

    Fix download of classic-shell package

--- automatic/_output/classic-shell/4.1.0.20141227/tools/chocolateyInstall.ps1
+++ automatic/_output/classic-shell/4.1.0.20141227/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageId = '{{PackageName}}'
+﻿$packageId = 'classic-shell'
 $fileType = 'exe'
 $fileArgs = '/passive'
-$url = '{{DownloadUrl}}'
+$url = 'http://www.mediafire.com/download/62xr4o45m4b2i85/ClassicShellSetup_4_1_0.exe'
 
 # Installer is for both 32- and 64-bit versions
 $url64 = $url
