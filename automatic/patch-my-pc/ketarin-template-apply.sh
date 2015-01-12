526e991e581867ed1c6ca1a4e43a3b1fb11f8621
Sander Steenhuis <redsandro@gmail.com>
2015-01-13 00:30:13 +0100

    Created ketarin cleanup xsl and added patch-my-pc for testing; needed update anyway

---
+++ automatic/patch-my-pc/ketarin-template-apply.sh
@@ -0,0 +1,1 @@
+java -jar /usr/share/maven-repo/net/sf/saxon/Saxon-HE/9.x/Saxon-HE-9.x.jar -s:patch-my-pc.ketarin-original.xml -xsl:ketarin-template.xsl
---
+++ automatic/patch-my-pc/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+$name = 'Patch my PC'
+$id = 'patch-my-pc'
+$url = 'http://patchmypc.net/PatchMyPC.exe'
+$pwd = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
+
+# Combatibility - This function has not been merged
+if (!(Get-Command Install-ChocolateyPinnedItem -errorAction SilentlyContinue)) {
+	Import-Module "$($pwd)\Install-ChocolateyPinnedItem.ps1"
+}
+
+try {
+
+	$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $chocTempDir = Join-Path $env:TEMP "chocolatey"
+    $tempDir = Join-Path $chocTempDir "$id"
+
+	# Calculate $binRoot, which should always be set in $env:ChocolateyBinRoot as a full path (not relative)
+	$binRoot = Get-BinRoot;
+
+    Write-Host "Downloading to: $nugetExePath";
+    $tempFile = Join-Path $nugetExePath "PatchMyPC.exe"
+
+    Get-ChocolateyWebFile $id "$tempFile" "$url"
+
+	# Copy shortcut to start menu
+
+	Install-ChocolateyPinnedItem $tempFile
+
+    Write-ChocolateySuccess $name
+} catch {
+	Write-ChocolateyFailure $name $($_.Exception.Message)
+	throw
+}
