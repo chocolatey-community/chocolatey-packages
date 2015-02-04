cbdf209d738ab59fa9c8b83733bf4e5424e4519e
dtgm <dtgm1594@gmail.com>
2015-02-03 19:18:07 -0600

    fixed; install and uninstall were not silent

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -3,9 +3,8 @@ $version = '{{PackageVersion}}'
 $url = '{{DownloadUrl}}'
 $url64bit = $url
 $installerType = 'exe'
-$installArgs = 'SP- /SILENT /NORESTART'
-$gimpRegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
-
+$installArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$gimpRegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\gimp*'
 
 try {
 
