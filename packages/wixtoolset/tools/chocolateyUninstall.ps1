7b3ad0cb06154b935ff908b6591557c23b8ddb4d
Gary Ewan Park <gep13@gep13.co.uk>
2015-02-05 20:15:51 +0000

    (wixtoolset) Updating to latest release

--- packages/wixtoolset/tools/chocolateyUninstall.ps1
+++ packages/wixtoolset/tools/chocolateyUninstall.ps1
@@ -1,15 +1,4 @@
 $packageName = 'wixtoolset'
-$silentArgs = '/uninstall /quiet'
 
-$osBitness = Get-ProcessorBits
-
-# Remove not needed folder with binaries
-if ($osBitness -eq 64) {
-  $uninstallRegKey = 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\{3d1f6355-a0b0-49a1-b977-43cdbe771169}'
-} else {
-  $uninstallRegKey = 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{3d1f6355-a0b0-49a1-b977-43cdbe771169}'
-}
-
-$uninstallPath = (Get-ItemProperty $uninstallRegKey).BundleCachePath
-
-Start-ChocolateyProcessAsAdmin -exeToRun $uninstallPath -statements $silentArgs
+$msiArgs = $('/x{7B9D7DD6-3490-417D-843C-7982B1DB1859} /q REBOOT=ReallySuppress')
+Start-ChocolateyProcessAsAdmin $msiArgs 'msiexec'
