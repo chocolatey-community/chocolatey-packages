f47c573f12e0505f678bcdbe75624d128704f393
Sander Steenhuis <redsandro@gmail.com>
2015-02-21 20:42:05 +0100

    Added UltraDefrag

---
+++ automatic/ultradefrag/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿try {
+  $version = '{{PackageVersion}}'
+  $downUrl = "http://sourceforge.net/projects/audacity/files/audacity/${version}/audacity-win-${version}.exe/download"
+  # installer, will assert administrative rights
+  Install-ChocolateyPackage '{{PackageName}}' 'EXE' '/VERYSILENT' "$downUrl" -validExitCodes @(0)
+
+  # the following is all part of error handling
+  Write-ChocolateySuccess '{{PackageName}}'
+} catch {
+  Write-ChocolateyFailure '{{PackageName}}' "$($_.Exception.Message)"
+  throw
+}
