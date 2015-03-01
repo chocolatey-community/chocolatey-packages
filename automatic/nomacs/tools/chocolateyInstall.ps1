b45a35289fb737aa2dac8be7b88924ebf839639b
TomOne <TomOne@users.noreply.github.com>
2015-03-01 19:35:21 +0100

    Fix nomacs package

--- automatic/nomacs/tools/chocolateyInstall.ps1
+++ automatic/nomacs/tools/chocolateyInstall.ps1
@@ -3,18 +3,21 @@ $fileType = 'exe'
 $silentArgs = '/S'
 # {\{DownloadUrlx64}\} gets “misused” here as 32- and 64-bit link array due to limitations of Ketarin/chocopkgup
 $urlsArray = {{DownloadUrlx64}}
-$urlWinXp = 'http://sourceforge.net/projects/nomacs/files/nomacs-1.6.2/nomacs-setup-1.6.3-WinXP-x86.exe/download'
+$urlOldOs = 'http://sourceforge.net/projects/nomacs/files/nomacs-2.4.0/nomacs-setup-2.4.0-WinXP-x86.exe/download'
 $url = $urlsArray[0]
 $url64bit = $urlsArray[1]
 
 try {
 
   # If Windows 2000/XP, download matching version
-  $WinVersion = [System.Environment]::OSVersion.Version.Major
-  if ($WinVersion -eq 5) {
-    Write-Host 'You have Windows 2000/XP. There is no newer version than Nomacs 1.6.3 available for these operating systems.'
-    $url = $urlWinXp
-    $url64bit =$urlWinXp
+  $winVersion = [System.Environment]::OSVersion.Version
+  if ($winVersion -lt [Version]'6.1') {
+    Write-Host $(
+        'You have Windows 2000, XP or Vista. ' +
+        'There is no newer version than Nomacs 2.4.0 available for these operating systems.'
+    )
+    $url = $urlOldOs
+    $url64bit =$urlOldOs
   }
 
 
