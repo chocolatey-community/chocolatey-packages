f0ebdf98e4d53b42123f8f79fd524f58ca909042
TomOne <TomOne@users.noreply.github.com>
2015-03-02 22:58:02 +0100

    Fix Win 2000/XP/Vista download link for nomacs

--- automatic/nomacs/tools/chocolateyInstall.ps1
+++ automatic/nomacs/tools/chocolateyInstall.ps1
@@ -1,23 +1,19 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/S'
-# {\{DownloadUrlx64}\} gets “misused” here as 32- and 64-bit link array due to limitations of Ketarin/chocopkgup
+# {\{DownloadUrlx64}\} gets “misused” here as 32-bit, 64-bit and Win ≤ Vista link array due to limitations of Ketarin/chocopkgup
 $urlsArray = {{DownloadUrlx64}}
-$urlOldOs = 'http://sourceforge.net/projects/nomacs/files/nomacs-2.4.0/nomacs-setup-2.4.0-WinXP-x86.exe/download'
 $url = $urlsArray[0]
 $url64bit = $urlsArray[1]
+$urlVistaAndBefore = $urlsArray[2]
 
 try {
 
   # If Windows 2000/XP, download matching version
   $winVersion = [System.Environment]::OSVersion.Version
   if ($winVersion -lt [Version]'6.1') {
-    Write-Host $(
-        'You have Windows 2000, XP or Vista. ' +
-        'There is no newer version than Nomacs 2.4.0 available for these operating systems.'
-    )
-    $url = $urlOldOs
-    $url64bit =$urlOldOs
+    $url = $urlVistaAndBefore
+    $url64bit = $urlVistaAndBefore
   }
 
 
