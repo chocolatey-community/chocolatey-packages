e070fac5d621867b93c7e05cc7473212ff645344
TomOne <TomOne@users.noreply.github.com>
2015-05-02 23:32:20 +0200

    tor-browser: create ignore files to prevent adding unnecessary .exeâ€™s to PATH

--- automatic/tor-browser/tools/chocolateyInstall.ps1
+++ automatic/tor-browser/tools/chocolateyInstall.ps1
@@ -10,13 +10,13 @@ $destinationFolder = Join-Path $binRoot 'tor-browser'
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 if (!(Test-Path $destinationFolder)) {
-    $destinationFolder =  Join-Path $toolsDir 'tor-browser'
+  $destinationFolder =  Join-Path $toolsDir 'tor-browser'
 } else {
-    Write-Host $(
-        'Deprecated installation folder detected (binRoot). ' +
-        'This package will continue to install tor-browser there ' +
-        'unless you manuall remove if from "' + $destinationFolder + '".'
-    )
+  Write-Host $(
+    'Deprecated installation folder detected (binRoot). ' +
+    'This package will continue to install tor-browser there ' +
+    'unless you manuall remove if from "' + $destinationFolder + '".'
+  )
 }
 
 $pathDownloadedInstaller = Join-Path $env:TEMP 'tor-browserInstall.exe'
@@ -25,9 +25,9 @@ $desktopPath = $([Environment]::GetFolderPath('Desktop'))
 $oldDestinationFolder = Join-Path $desktopPath 'Tor Browser'
 
 if ((Test-Path $oldDestinationFolder) -and
-    ($oldDestinationFolder -ne $destinationFolder)) {
+  ($oldDestinationFolder -ne $destinationFolder)) {
 
-    $destinationFolder = $oldDestinationFolder
+  $destinationFolder = $oldDestinationFolder
 
 Write-Output @"
 Warning: Deprecated installation folder detected: Desktop/Tor Browser.
@@ -43,21 +43,21 @@ $language = (Get-Culture).Name -replace '-[a-z]{2}', '' # get language code
 #$language = 'xx' # Language override for testing
 
 $table = @{
-    'en' = 'en-US';
-    'ar' = 'ar';
-    'de' = 'de';
-    'es' = 'es-ES';
-    'fa' = 'fa';
-    'fr' = 'fr';
-    'it' = 'it';
-    'ko' = 'ko';
-    'nl' = 'nl';
-    'pl' = 'pl';
-    'pt' = 'pt-PT';
-    'ru' = 'ru';
-    'tr' = 'tr';
-    'vi' = 'vi';
-    'zh' = 'zh-CN';
+  'en' = 'en-US';
+  'ar' = 'ar';
+  'de' = 'de';
+  'es' = 'es-ES';
+  'fa' = 'fa';
+  'fr' = 'fr';
+  'it' = 'it';
+  'ko' = 'ko';
+  'nl' = 'nl';
+  'pl' = 'pl';
+  'pt' = 'pt-PT';
+  'ru' = 'ru';
+  'tr' = 'tr';
+  'vi' = 'vi';
+  'zh' = 'zh-CN';
 }
 
 $langcode = $table[$language]
@@ -72,3 +72,11 @@ Get-ChocolateyWebFile $packageName $pathDownloadedInstaller $url
 Start-Process -Wait $pathDownloadedInstaller -ArgumentList '/S', "/D=$destinationFolder"
 
 Remove-Item $pathDownloadedInstaller
+
+
+# Create .ignore files for exe’s
+Get-ChildItem -Path $destinationFolder -Recurse | Where {
+  $_.Extension -eq '.exe'} | % {
+  New-Item $($_.FullName + '.ignore') -Force -ItemType file
+# Suppress output of New-Item
+} | Out-Null
