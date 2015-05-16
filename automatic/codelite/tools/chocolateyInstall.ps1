59a50391a2b86a75f6adc9b0c419deb03459c515
TomOne <TomOne@users.noreply.github.com>
2015-05-16 22:07:26 +0200

    codelite: fix package to update to latest

--- automatic/codelite/tools/chocolateyInstall.ps1
+++ automatic/codelite/tools/chocolateyInstall.ps1
@@ -1,6 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT'
-$url = '{{DownloadUrlx64}}' # This variable is actually not the 64-bit installer, it is only a workaround to get the automatic package working
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$url = 'http://sourceforge.net/projects/codelite/files/Releases/codelite-{{PackageVersion}}/codelite-x86-{{PackageVersion}}.exe/download'
+$url64 = 'http://sourceforge.net/projects/codelite/files/Releases/codelite-{{PackageVersion}}/codelite-amd64-{{PackageVersion}}.exe/download'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
