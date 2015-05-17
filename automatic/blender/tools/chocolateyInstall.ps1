e7e848d6315a7f9555d6fd0e79705029cba1c19a
TomOne <TomOne@users.noreply.github.com>
2015-05-17 12:30:58 +0200

    Fix blender package

--- automatic/blender/tools/chocolateyInstall.ps1
+++ automatic/blender/tools/chocolateyInstall.ps1
@@ -5,4 +5,5 @@ $url64 = '{{DownloadUrlx64}}'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 -validExitCodes $validExitCodes
+Install-ChocolateyPackage $packageName $installerType $silentArgs `
+  $url $url64 -validExitCodes $validExitCodes
