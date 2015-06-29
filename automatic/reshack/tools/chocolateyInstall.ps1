da1a06bf2e600442203ca66680b1d1539002b813
Sander Steenhuis <redsandro@gmail.com>
2015-06-29 21:20:28 +0200

    Copied from local, converted to autopackage

---
+++ automatic/reshack/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
+# I need to add an 'open with resource hacker' context menu item for .exe only
+# "C:\Program Files (x86)\Resource Hacker\ResHacker.exe"
+# if 32 bit
+# %ProgramFiles%\Resource Hacker\ResHacker.exe
+# if 64 bit
+# %ProgramFiles(x86)%\Resource Hacker\ResHacker.exe
