c9e44306ea7d963e5a9eb6df8fe39e7c4f2a2cf9
TomOne <TomOne@users.noreply.github.com>
2015-07-12 01:38:51 +0200

    Avidemux moved to FossHub; adapting package

--- automatic/avidemux/tools/chocolateyInstall.ps1
+++ automatic/avidemux/tools/chocolateyInstall.ps1
@@ -1,12 +1,20 @@
-﻿$packageName = '{{PackageName}}'
+﻿$PSScriptRoot = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+Import-Module (Join-Path $PSScriptRoot 'Get-UrlFromFosshub.ps1')
+
+$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/S'
-# {\{DownloadUrlx64}\} gets “misused” here as hashtable for the download links
-# due to limitations of Ketarin/chocopkgup
-$urlsHashTable = {{DownloadUrlx64}}
-$url = $urlsHashTable.url
-$url64bit = $urlsHashTable.url64
-$validExitCodes = @(0,1223)
+
+# {\{DownloadUrlx64}\} gets “misused” here as genLink
+# part for FossHub (only visible in the template, not in
+# the final file generated for the package.)
+$genLinkUrl = '{{DownloadUrlx64}}32.exe'
+$genLinkUrl64bit = '{{DownloadUrlx64}}64.exe'
+$validExitCodes = @(0, 1223)
+
+$url = Get-UrlFromFosshub $genLinkUrl
+$url64bit = Get-UrlFromFosshub $genLinkUrl64bit
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs `
   $url $url64bit -validExitCodes $validExitCodes
