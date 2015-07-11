a83c63b2acd15bc341e0cff96595e0ee6c819137
TomOne <TomOne@users.noreply.github.com>
2015-07-12 00:22:19 +0200

    Mumble has moved to GitHub, adapting package

--- automatic/mumble/tools/chocolateyInstall.ps1
+++ automatic/mumble/tools/chocolateyInstall.ps1
@@ -1,9 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'msi'
-# {\{DownloadUrlx64}\} gets “misused” here as 32-bit download link due to limitations of Ketarin/chocopkgup
-$url = '{{DownloadUrlx64}}'
+$url = '{{DownloadUrl}}'
 $silentArgs = '/passive /norestart'
 $validExitCodes = @(0)
 
-
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+Install-ChocolateyPackage $packageName $installerType `
+  $silentArgs $url -validExitCodes $validExitCodes
