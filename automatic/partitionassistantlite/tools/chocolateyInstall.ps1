f6f98182519f22d27322e0f11de8342a0cbc5ed0
Sander Steenhuis <redsandro@gmail.com>
2015-07-18 14:34:08 +0200

    Original autopackages for AOMEI partition assistants

---
+++ automatic/partitionassistantlite/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+$name	= 'AOMEI Partition Assistant Lite Server'
+$id		= '{{PackageName}}'
+$url	= 'http://www.disk-partition.com/download-lite.html'
+$regex	= '(?ms).*href="(http://download.cnet.com/.+?)".*'
+$pwd	= "$(split-path -parent $MyInvocation.MyCommand.Definition)"
+
+# Combatibility - This function has not been merged
+if (!(Get-Command Get-UrlFromCnet -errorAction SilentlyContinue)) {
+	Import-Module "$($pwd)\Get-UrlFromCnet2.ps1"
+}
+
+
+
+# Let's get the link from CNET first.
+$url = Get-UrlFromCnet "$url" "$regex"
+# Warning: Somehow $url is an array now.
+$url = $url[1]
+
+# Download manually
+$downTarget	= "$pwd\PAssist_Lite_Setup.exe"
+Write-Host "Trying to download to $downTarget..."
+Get-ChocolateyWebFile "$id" "$downTarget" $url
+
+Write-Host
+Write-Host "The installation is unattended." -ForegroundColor "White"
+Write-Host "However, AOMEI PA Lite Server will open after installing." -ForegroundColor "White"
+Write-Host "Sorry!" -ForegroundColor "White"
+Write-Host
+
+# Installer
+Write-Host "Trying to install..."
+Install-ChocolateyPackage "$id" 'EXE' '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' "$downTarget"
