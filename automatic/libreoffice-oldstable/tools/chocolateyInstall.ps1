06a11fa7c79e7a98081abff2fac92d03dd48e88a
TomOne <TomOne@users.noreply.github.com>
2015-08-11 13:31:14 +0200

    libreoffice: add 64-bit version

--- automatic/libreoffice-oldstable/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-oldstable/tools/chocolateyInstall.ps1
@@ -3,43 +3,39 @@
 
 $packageName = '{{PackageName}}'
 $version = '{{PackageVersion}}'
+$url32 = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
 $validExitCodes = @(0,3010)
 
-$versionsHtmlFile = "$env:TEMP\libreoffice-versions.html"
-$versionsHtmlUrl = 'http://download.documentfoundation.org/libreoffice/stable/'
-
-Get-ChocolateyWebFile 'libreoffice-versions-html' $versionsHtmlFile $versionsHtmlUrl
-
-$matchArray = (Get-Content $versionsHtmlFile) -match 'href="([\d\.]+)\/"'
-
-Remove-Item $versionsHtmlFile
-
-$downloadableVersions = @()
-
-for ($i = 0; $i -lt $matchArray.Length; $i += 1) {
-  $matchArray[$i] -match '[\d\.]+'
-  $downloadableVersions += $Matches[0]
-}
-
-if (-not($downloadableVersions -match $version)) {
-
-  Write-Output 'The version of LibreOffice specified in the package is no longer available to download. This package will download the latest available version instead.'
-
-  if ([System.Version]$downloadableVersions[0] -gt [System.Version]$downloadableVersions[1]) {
-    $version = $downloadableVersions[0]
-  } else {
-    $version = $downloadableVersions[1]
-  }
-}
-
-$downUrl = "http://download.documentfoundation.org/libreoffice/stable/${version}/win/x86/LibreOffice_${version}_Win_x86.msi"
-
-
 # Check if LibreOffice in the same version is already installed
-$alreadyInstalled = Get-WmiObject -Class Win32_Product | Where-Object {($_.Name -match '^LibreOffice [\d\.]+$') -and ($_.Version -match "^$version")}
+$alreadyInstalled = Get-WmiObject -Class Win32_Product | Where-Object {
+  ($_.Name -match '^LibreOffice [\d\.]+$') -and ($_.Version -match "^$version")
+}
 
 if ($alreadyInstalled) {
-  Write-Host "LibreOffice $version is already installed on the computer. Skipping download."
+
+  Write-Host $(
+    "LibreOffice $version is already installed on the computer. " +
+    "Skipping download."
+  )
+
+  if ((Get-ProcessorBits 64) -and $url64) {
+
+    Write-Host $(
+      "Do you already have LibreOffice $version 32-bit installed " +
+      "and want to switch to 64-bit? " +
+      "In that case you have to manually uninstall the 32-bit version " +
+      "and reinstall this package."
+    )
+  }
+
 } else {
-  Install-ChocolateyPackage $packageName 'msi' '/passive /norestart' $downUrl -validExitCodes $validExitCodes
+
+  if ($url64) {
+    Install-ChocolateyPackage $packageName 'msi' '/passive /norestart' `
+      $url32 $url64 -validExitCodes $validExitCodes
+  } else {
+    Install-ChocolateyPackage $packageName 'msi' '/passive /norestart' `
+      $url32 -validExitCodes $validExitCodes
+  }
 }
