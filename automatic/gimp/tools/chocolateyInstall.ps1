e02e7af70e8e4f98d3c8173f1b1ab39f44aaedd6
TomOne <TomOne@users.noreply.github.com>
2015-10-06 22:50:06 +0200

    Fix gimp download link, refactor install/uninstall scripts

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -4,20 +4,22 @@ $url = '{{DownloadUrl}}'
 $url64bit = $url
 $installerType = 'exe'
 $installArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$gimpRegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
+$gimpRegistryPath = $(
+  'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
+)
 
-try {
+if (Test-Path $gimpRegistryPath) {
+  $installedVersion = (
+    Get-ItemProperty -Path $gimpRegistryPath -Name 'DisplayVersion'
+  ).DisplayVersion
+}
 
-  if (Test-Path $gimpRegistryPath) {
-    $installedVersion = (Get-ItemProperty -Path $gimpRegistryPath -Name 'DisplayVersion').DisplayVersion
-  }
-
-  if ($installedVersion -eq $version) {
-    Write-Output "GIMP $installedVersion is already installed. Skipping download and installation."
-  } else {
-    Install-ChocolateyPackage $packageName $installerType $installArgs $url $url64bit -validExitCodes @(0)
-  }
-} catch {
-  Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw
+if ($installedVersion -eq $version) {
+  Write-Host $(
+    "GIMP $installedVersion is already installed. " +
+    "Skipping download and installation."
+  )
+} else {
+  Install-ChocolateyPackage $packageName $installerType `
+    $installArgs $url $url64bit -validExitCodes @(0)
 }
--- automatic/gimp/tools/chocolateyUninstall.ps1
+++ automatic/gimp/tools/chocolateyUninstall.ps1
@@ -1,19 +1,17 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'exe'
 $installArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$gimpRegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
+$gimpRegistryPath = $(
+  'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
+)
 
-try {
+if (Test-Path $gimpRegistryPath) {
+  $uninstallString = (
+    Get-ItemProperty -Path $gimpRegistryPath -Name 'UninstallString'
+  ).UninstallString
+}
 
-  if (Test-Path $gimpRegistryPath) {
-    $uninstallString = (Get-ItemProperty -Path $gimpRegistryPath -Name 'UninstallString').UninstallString
-  }
-
-  if ($uninstallString) {
-    Uninstall-ChocolateyPackage $packageName $installerType $installArgs $uninstallString
-  }
-
-} catch {
-  Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw
+if ($uninstallString) {
+  Uninstall-ChocolateyPackage $packageName $installerType `
+    $installArgs $uninstallString
 }
