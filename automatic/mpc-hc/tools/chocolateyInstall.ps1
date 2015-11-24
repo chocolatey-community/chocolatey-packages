a49d8645d699daf49cfd0716d96abaea5450cd14
TomOne <TomOne@users.noreply.github.com>
2015-11-24 22:10:17 +0100

    Fix mpc-hc package
    * Update Ketarin job to new download location
    * Project URL to https

--- automatic/mpc-hc/tools/chocolateyInstall.ps1
+++ automatic/mpc-hc/tools/chocolateyInstall.ps1
@@ -1,9 +1,7 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/VERYSILENT'
-# {\{DownloadUrlx64}\} gets “misused” here as 32- and 64-bit link array due to limitations of Ketarin/chocopkgup
-$urlsArray = {{DownloadUrlx64}}
-$url = $urlsArray[0]
-$url64bit = $urlsArray[1]
+$url = '{{DownloadUrl}}'
+$url64bit = '{{DownloadUrlx64}}'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
