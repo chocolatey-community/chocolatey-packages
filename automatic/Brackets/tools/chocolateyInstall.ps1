3a4d87870ba7183e95c532f3f8c6c0b69b0a6ba3
TomOne <TomOne@users.noreply.github.com>
2015-11-24 22:31:43 +0100

    Fix Brackets package
    * Add /norestart parameter
    * Remove deprecated try/catch blocks

--- automatic/Brackets/tools/chocolateyInstall.ps1
+++ automatic/Brackets/tools/chocolateyInstall.ps1
@@ -1,28 +1,24 @@
 ﻿$packageName = '{{PackageName}}'
 $version = '{{PackageVersion}}'
 
-try {
+$bracketsRegistryVersion = $version -replace '^(\d+\.\d+)\..+', '$1'
 
-  $bracketsRegistryVersion = $version -replace '^(\d+\.\d+)\..+', '$1'
+$params = @{
+  PackageName = $packageName;
+  FileType = 'msi';
+  SilentArgs = '/q /norestart';
+  Url = '{{DownloadUrl}}';
+}
 
-  $params = @{
-    PackageName = $packageName;
-    FileType = 'msi';
-    SilentArgs = '/q';
-    Url = '{{DownloadUrl}}';
-  }
+# We know that using Win32_Product is bad.
+# Is there a better alternative to check if it’s
+# already installed?
+$alreadyInstalled = Get-WmiObject -Class Win32_Product |
+  Where-Object {($_.Name -eq 'Brackets') -and
+  ($_.Version -match $bracketsRegistryVersion)}
 
-  $alreadyInstalled = Get-WmiObject -Class Win32_Product |
-    Where-Object {($_.Name -eq 'Brackets') -and
-    ($_.Version -match $bracketsRegistryVersion)}
-
-  if ($alreadyInstalled) {
-    Write-Host "Brackets $version is already installed. Skipping installation."
-  } else {
-    Install-ChocolateyPackage @params
-  }
-
-} catch {
-  Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw
+if ($alreadyInstalled) {
+  Write-Host "Brackets $version is already installed. Skipping installation."
+} else {
+  Install-ChocolateyPackage @params
 }
--- automatic/Brackets/tools/chocolateyUninstall.ps1
+++ automatic/Brackets/tools/chocolateyUninstall.ps1
@@ -1,17 +1,10 @@
 ﻿$packageName = '{{PackageName}}'
 
-try {
-
-  $app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq 'Brackets'}
-  if ($app) {
-    $msiArgs = $('/x' + $app.IdentifyingNumber + ' /q REBOOT=ReallySuppress')
-    Start-ChocolateyProcessAsAdmin $msiArgs 'msiexec'
-  }
-
-  Write-ChocolateySuccess $packageName
-
-
-} catch {
-  Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw
+# We know that using Win32_Product is bad.
+# Is there a better alternative to check if it’s
+# already installed?
+$app = Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq 'Brackets'}
+if ($app) {
+  $msiArgs = $('/x' + $app.IdentifyingNumber + ' /q REBOOT=ReallySuppress')
+  Start-ChocolateyProcessAsAdmin $msiArgs 'msiexec'
 }
