3b855078684d5b5a7ea136a4c3097637490ff37e
TomOne <TomOne@users.noreply.github.com>
2015-11-27 19:43:39 +0100

    Update and fix keepass-langfiles package

--- packages/keepass-langfiles/tools/chocolateyInstall.ps1
+++ packages/keepass-langfiles/tools/chocolateyInstall.ps1
@@ -1,24 +1,29 @@
-﻿try {
+﻿$packageID = 'keepass-langfiles'
+$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$fileFullPath = Join-Path $PSScriptRoot 'keepass_2.x_langfiles.zip'
 
-  $name = "keepass-langfiles"
-  $scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $fileFullPath = "$scriptPath\keepass_2.x_langfiles.zip"
+function Get-KeePassInstallLocation {
+  @(
+    'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\KeePassPasswordSafe2_is1',
+    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\KeePassPasswordSafe2_is1'
+  ) | foreach {
 
-  $keepasspath = "$env:ProgramFiles\KeePass Password Safe 2"
-  $keepasspathx86 = "${env:ProgramFiles(x86)}\KeePass Password Safe 2"
-  if (Test-Path "$keepasspath") {$destination = "$keepasspath"}
-  if (Test-Path "$keepasspathx86") {$destination = "$keepasspathx86"}
-
-  $extractPath = "$scriptPath\keepass-langfiles"
-  if (-not (Test-Path $extractPath)) {
-    mkdir $extractPath
+    if (Test-Path $_) {
+      return (Get-ItemProperty $_).InstallLocation
+    }
   }
-
-  Get-ChocolateyUnzip $fileFullPath $extractPath
-  Start-ChocolateyProcessAsAdmin "Copy-Item -Force '$extractPath\*.lngx' '$destination'"
-
-    Write-ChocolateySuccess $name
-} catch {
-  Write-ChocolateyFailure $name $($_.Exception.Message)
-  throw
 }
+
+$destination = Get-KeePassInstallLocation
+
+if (!$destination) {
+  throw 'KeePass is not installed. Please install it before installing this package.'
+}
+
+$extractPath = Join-Path $PSScriptRoot $packageID
+if (-not (Test-Path $extractPath)) {
+  mkdir $extractPath
+}
+
+Get-ChocolateyUnzip $fileFullPath $extractPath
+Start-ChocolateyProcessAsAdmin "Copy-Item -Force '$extractPath\*.lngx' '$destination'"
