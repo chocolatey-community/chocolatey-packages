7d5cb271df677408edbd95fcb73d870ee0a1f8cd
TomOne <TomOne@users.noreply.github.com>
2015-11-28 11:41:52 +0100

    Fix and update djv package
    * Add 32 and 64 bit versions
    * Fix Ketarin job
    * Use correct projectUrl and licenseUrl

--- automatic/djv/tools/chocolateyInstall.ps1
+++ automatic/djv/tools/chocolateyInstall.ps1
@@ -1,7 +1,18 @@
-$packageName = '{{PackageName}}'
+﻿$packageName = '{{PackageName}}'
 $installerType = 'EXE'
-$url = '{{DownloadUrl}}'
+$url64 = 'http://sourceforge.net/projects/djv/files/djv-stable/{{PackageVersion}}/djv-{{PackageVersion}}-Windows-64.exe/download'
+
+# v1.0.5 is the latest version for 32 bit OSes
+$url = 'http://sourceforge.net/projects/djv/files/djv-stable/1.0.5/djv-1.0.5-Windows-32.exe/download'
+
 $silentArgs = '/S' # NSIS package
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+if (Get-ProcessorBits 32) {
+  Write-Host $("You’re using a 32 bit OS. " +
+    "DJV v1.0.5 is the latest version available for 32 bit OSes."
+  )
+}
+
+Install-ChocolateyPackage "$packageName" "$installerType" `
+  "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
