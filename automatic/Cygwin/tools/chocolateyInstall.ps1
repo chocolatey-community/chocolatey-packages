4c64f5d949ee11c38a1e004443b71cd3785b2ad2
TomOne <TomOne@users.noreply.github.com>
2015-12-12 22:42:11 +0100

    Improve Cygwin package
    * $PSScriptRoot as default installation folder
    * All cygwin.com links to https
    * Add description about installation folder

--- automatic/Cygwin/tools/chocolateyInstall.ps1
+++ automatic/Cygwin/tools/chocolateyInstall.ps1
@@ -4,20 +4,49 @@ $url = '{{DownloadUrl}}'
 $url64 = '{{DownloadUrlx64}}'
 $validExitCodes = @(0)
 
-$binRoot = Get-BinRoot
-$cygRoot = join-path $binRoot 'cygwin'
-$cygWinSetupFile = "$cygRoot\cygwinsetup.exe"
-$cygPackages = join-path $cygRoot packages
+# Set the default install path to the package folder
+$PSScriptRoot = Split-Path -parent $MyInvocation.MyCommand.Definition
+$cygRoot = Join-Path $PSScriptRoot 'cygwin'
+$cygWinSetupFile = Join-Path $cygRoot 'cygwinsetup.exe'
+$cygPackages = Join-Path $cygRoot packages
 
 if (!(Test-Path($cygRoot))) {
-  [System.IO.Directory]::CreateDirectory($cygRoot) | Out-Null  
+  [System.IO.Directory]::CreateDirectory($cygRoot) | Out-Null
 }
 
-Get-ChocolateyWebFile $packageName $cygWinSetupFile $url $url64 
+Get-ChocolateyWebFile $packageName $cygWinSetupFile $url $url64
 
 # https://cygwin.com/faq/faq.html#faq.setup.cli
-$silentArgs = "--quiet-mode --root $cygRoot --local-package-dir $cygPackages -s http://mirrors.kernel.org/sourceware/cygwin/ --packages default"
+$silentArgs = $(
+  "--quiet-mode " +
+  "-s http://mirrors.kernel.org/sourceware/cygwin/ " +
+  "--packages default"
+)
 
-Install-ChocolateyInstallPackage $packageName $installerType $silentArgs $cygWinSetupFile -validExitCodes $validExitCodes
+# These install arguments are only appended to the silent args
+# if Cygwin is installed for the first time, otherwise these
+# values are already set.
+$firstInstallArgs = $(
+  "--root $cygRoot " +
+  "--local-package-dir $cygPackages"
+)
 
-Install-ChocolateyPath $cygRoot
+# Check if Cygwin is already installed
+$alreadyInstalled = Test-Path 'HKLM:\SOFTWARE\Cygwin\setup'
+
+if (!$alreadyInstalled) {
+  $silentArgs = $silentArgs + ' ' + $firstInstallArgs
+
+  Install-ChocolateyPath $cygRoot 'Machine'
+  $env:Path = "$($env:Path);$cygRoot"
+}
+
+Install-ChocolateyInstallPackage $packageName $installerType $silentArgs `
+  $cygWinSetupFile -validExitCodes $validExitCodes
+
+# Create .ignore files for exe files
+Get-ChildItem -Path $cygRoot -Recurse | Where {
+  $_.Extension -eq '.exe'} | % {
+  New-Item $($_.FullName + '.ignore') -Force -ItemType file
+# Suppress output of New-Item
+} | Out-Null
