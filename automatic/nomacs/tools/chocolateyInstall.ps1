ca39eab9da7dbae5e1a88d0c55193484a7c7685b
TomOne <TomOne@users.noreply.github.com>
2015-12-21 23:52:38 +0100

    Update nomacs package to new architecture, add new icon

--- automatic/nomacs/tools/chocolateyInstall.ps1
+++ automatic/nomacs/tools/chocolateyInstall.ps1
@@ -1,26 +1,10 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
-$silentArgs = '/S'
-# {\{DownloadUrlx64}\} gets “misused” here as 32-bit, 64-bit and Win ≤ Vista link array due to limitations of Ketarin/chocopkgup
-$urlsArray = {{DownloadUrlx64}}
-$url = $urlsArray[0]
-$url64bit = $urlsArray[1]
-$urlVistaAndBefore = $urlsArray[2]
+$url = '{{DownloadUrl}}'
+$url64bit = $url
 
-try {
+$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$pathToInstallScript = Join-Path $PSScriptRoot 'install-script.js'
+$silentArgs = "--script `"$pathToInstallScript`""
 
-  # If Windows 2000/XP, download matching version
-  $winVersion = [System.Environment]::OSVersion.Version
-  if ($winVersion -lt [Version]'6.1') {
-    $url = $urlVistaAndBefore
-    $url64bit = $urlVistaAndBefore
-  }
-
-
-  # Else download the version for Windows Vista/7/8 or later
-  Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
-
-} catch {
-  Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw
-}
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
