7f019a8c8daf7e9b51d48e87477764fd35c1a764
TomOne <TomOne@users.noreply.github.com>
2016-01-08 22:55:24 +0100

    Fix libreoffice-oldstable package
    * Remove nonexistent 64-bit download URL
    * Update links to https
    * Ketarin job: more reliable regex for determining the version
    * Improve wording in package description
    * Stop using Win32_Product for determining installed versions

--- automatic/libreoffice-oldstable/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-oldstable/tools/chocolateyInstall.ps1
@@ -1,41 +1,27 @@
-﻿# Important: The contents of this file – the `libreoffice` and
-# `libreoffice-oldstable` install scripts – **must** be identical.
-
-$packageName = '{{PackageName}}'
+﻿$packageName = '{{PackageName}}'
 $version = '{{PackageVersion}}'
-$url32 = '{{DownloadUrl}}'
-$url64 = '{{DownloadUrlx64}}'
-$validExitCodes = @(0,3010)
+$url = '{{DownloadUrl}}'
+$validExitCodes = @(0, 3010)
 
-# Check if LibreOffice in the same version is already installed
-$alreadyInstalled = Get-WmiObject -Class Win32_Product | Where-Object {
-  ($_.Name -match '^LibreOffice [\d\.]+$') -and ($_.Version -match "^$version")
+function Check-SameVersionInstalled() {
+  $registryPaths = @(
+    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+    'HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+  )
+
+  return Get-ItemProperty $registryPaths -ErrorAction SilentlyContinue | Where-Object {
+    $_.DisplayName -match '^LibreOffice [\d\.]+$' -and
+    $_.DisplayVersion -like "${version}*"
+  }
 }
 
-if ($alreadyInstalled) {
+if (Check-SameVersionInstalled) {
 
   Write-Host $(
     "LibreOffice $version is already installed on the computer. " +
     "Skipping download."
   )
-
-  if ((Get-ProcessorBits 64) -and $url64) {
-
-    Write-Host $(
-      "Do you already have LibreOffice $version 32-bit installed " +
-      "and want to switch to 64-bit? " +
-      "In that case you have to manually uninstall the 32-bit version " +
-      "and reinstall this package."
-    )
-  }
-
 } else {
-
-  if ($url64) {
-    Install-ChocolateyPackage $packageName 'msi' '/passive /norestart' `
-      $url32 $url64 -validExitCodes $validExitCodes
-  } else {
-    Install-ChocolateyPackage $packageName 'msi' '/passive /norestart' `
-      $url32 -validExitCodes $validExitCodes
-  }
+  Install-ChocolateyPackage $packageName 'msi' '/qn /norestart' `
+    $url -validExitCodes $validExitCodes
 }
