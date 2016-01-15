c1e986f4dd0e92ea0e31567625b3284050c23bbd
Mat <mberchtold@gmail.com>
2016-01-15 05:01:08 +0100

    removed Write-ChocolateySuccess
    Write-ChocolateySuccess is deprecated as well

--- automatic/smartftp/tools/chocolateyUninstall.ps1
+++ automatic/smartftp/tools/chocolateyUninstall.ps1
@@ -4,4 +4,3 @@ $productName = "SmartFTP Client";
 $app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq $productName -and ($_.Version -eq "{{PackageVersion}}") }
 $result = $app.Uninstall();
 
-Write-ChocolateySuccess $packageName
