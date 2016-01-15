f95353ecef9ef8251db769fb11089bf3e4050f92
Mat <mberchtold@gmail.com>
2016-01-15 04:56:40 +0100

    removed Write-ChocolateyFailure

--- automatic/smartftp/tools/chocolateyUninstall.ps1
+++ automatic/smartftp/tools/chocolateyUninstall.ps1
@@ -1,13 +1,7 @@
 $packageName = "{{PackageName}}"
 $productName = "SmartFTP Client";
 
-try {
-	$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq $productName -and ($_.Version -eq "{{PackageVersion}}") }
-	$result = $app.Uninstall();
+$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq $productName -and ($_.Version -eq "{{PackageVersion}}") }
+$result = $app.Uninstall();
 
-	Write-ChocolateySuccess $packageName
-}
-catch {
-	Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-	throw
-}
+Write-ChocolateySuccess $packageName
