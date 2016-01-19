6ae8aba1e9120319ced4425f195dc1c78580cfd3
Link <link.satonaka@outlook.com>
2016-01-19 14:43:53 -0500

    added

--- automatic/SRWareIron/tools/chocolateyInstall.ps1
+++ automatic/SRWareIron/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
 $installerType = 'exe'
-$url = '{{DownloadUrl}}'
+$url = 'http://www.srware.net/downloads/srware_iron.exe'
+$url64 = 'http://www.srware.net/downloads/srware_iron64.exe'
 $silentArgs = '/silent'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url  -validExitCodes $validExitCodes
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes
