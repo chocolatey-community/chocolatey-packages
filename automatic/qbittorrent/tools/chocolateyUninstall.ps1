48bc3532f866012357bf863305488e0842a9356b
Link <link.satonaka@outlook.com>
2016-01-19 12:39:18 -0500

    capitalize B

--- automatic/qbittorrent/tools/chocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
-
-Import-Module (Join-Path $scriptPath 'getUninstallString.ps1')
-
-$uninstallString = getUninstallString 'qbittorrent' 'UninstallString'
-if ($uninstallString) {
-  Uninstall-ChocolateyPackage $packageName 'exe' '/S' $uninstallString
-}
