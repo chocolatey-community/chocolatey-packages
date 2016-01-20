baeab78eac7253f046632c31e6978a94570cb018
Wong Leong Hui <wong.leonghui@gmail.com>
2016-01-20 18:30:36 +0800

    Initial commit

---
+++ automatic/flashplayerppapi/tools/ChocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'flashpluginppapi'
+$version = '20.0.0.286'
+$installerType = 'exe'
+$url = 'https://fpdownload.adobe.com/pub/flashplayer/pdc/20.0.0.286/install_flash_player_ppapi.exe'
+$silentArgs = '-install'
+$validExitCodes = @(0)
+
+#installer automatically overrides existing PPAPI installation
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
---
+++ automatic/flashplayerppapi/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+﻿$registryPaths = @(
+  'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
+  'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+)
+
+$uninstallString = (Get-ChildItem -Path $registryPaths |
+    Get-ItemProperty |
+    Where-Object {$_.DisplayName -match 'Mp3tag' } |
+    Select-Object -Property DisplayName, UninstallString).uninstallString -replace "maintain","uninstall"
+
+if ($uninstString) {
+    Uninstall-ChocolateyPackage 'flashplayerppapi' 'exe' '' $uninstString
+}
