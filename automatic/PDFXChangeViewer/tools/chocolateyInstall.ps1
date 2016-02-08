092e768a300a3ea159a2d362f0759e0807d41f06
Gary Ewan Park <gep13@gep13.co.uk>
2016-02-08 07:10:40 +0000

    (GH-126) Added NORESTART to install arguments
    Relates to #126

--- automatic/PDFXChangeViewer/tools/chocolateyInstall.ps1
+++ automatic/PDFXChangeViewer/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $installerType = 'exe'
 $url = '{{DownloadUrl}}'
 $url64 = $url
-$silentArgs = '/VERYSILENT /NOINSTASK'
+$silentArgs = '/VERYSILENT /NOINSTASK /NORESTART'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 -validExitCodes $validExitCodes
