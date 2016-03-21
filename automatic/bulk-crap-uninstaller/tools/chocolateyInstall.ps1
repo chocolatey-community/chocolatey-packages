11b6ffab0e41ca0e687b1202046aa0248682e8be
Sander Steenhuis <redsandro@gmail.com>
2016-03-21 16:35:22 +0100

    Added open source bulk crap uninstaller

---
+++ automatic/bulk-crap-uninstaller/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = '{{PackageName}}'
+$version = '{{PackageVersion}}'
+$installerType = 'EXE'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = "http://sourceforge.net/projects/bulk-crap-uninstaller/files/BCUninstaller_${version}_setup.exe/download"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
