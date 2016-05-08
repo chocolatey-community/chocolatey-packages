c47e005e7d4ae355eb35cb1d2f5609af47f3d508
TomOne <TomOne@users.noreply.github.com>
2016-05-08 15:21:11 +0200

    Fix maxima download links

--- automatic/maxima/tools/chocolateyInstall.ps1
+++ automatic/maxima/tools/chocolateyInstall.ps1
@@ -4,12 +4,13 @@ Import-Module (Join-Path $PSScriptRoot 'functions.ps1')
 $packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/S'
-$url = 'http://sourceforge.net/projects/maxima/files/Maxima-Windows/{{PackageVersion}}-Windows/maxima-clisp-{{PackageVersion}}.exe/download'
+$url = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/{{PackageVersion}}-Windows/maxima-clisp-sbcl-{{PackageVersion}}.exe/download'
+$url64 = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/{{PackageVersion}}-Windows/maxima-sbcl-{{PackageVersion}}-win64.exe/download'
 $version = '{{PackageVersion}}'
 $installedVersion = (Get-InstallProperties).DisplayVersion
 
 if ($version -eq $installedVersion) {
   Write-Output "Maxima v$version is already installed. No need to install again."
 } else {
-  Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+  Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
 }
