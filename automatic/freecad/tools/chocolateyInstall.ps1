220dfd6552a78894cfd01b63ae301a8a7f0773fa
TomOne <TomOne@users.noreply.github.com>
2016-05-12 21:06:32 +0200

    freecad: update download URLs

--- automatic/freecad/tools/chocolateyInstall.ps1
+++ automatic/freecad/tools/chocolateyInstall.ps1
@@ -1,10 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/S'
-# {\{DownloadUrlx64}\} gets “misused” here as array of the download
-# links due to limitations of Ketarin/chocopkgup
-$urlArray = {{DownloadUrlx64}}
-$url = $urlArray[0]
-$url64 = $urlArray[1]
+
+$url = '{{DownloadUrl}}'
+$url64 = '{{DownloadUrlx64}}'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
