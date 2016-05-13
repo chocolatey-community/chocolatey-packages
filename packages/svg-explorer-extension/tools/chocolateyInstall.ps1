498b80dbeaff5f01e7f19fa5dc492f6b63812e1c
TomOne <TomOne@users.noreply.github.com>
2016-05-13 21:01:10 +0200

    Convert svg-explorer-extension to manual packages

---
+++ packages/svg-explorer-extension/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'svg-explorer-extension'
+$fileType = 'exe'
+$silentArgs = '/VERYSILENT'
+$url = 'https://svgextension.codeplex.com/downloads/get/803085'
+$url64 = 'https://svgextension.codeplex.com/downloads/get/803086'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
