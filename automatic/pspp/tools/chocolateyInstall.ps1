f02c04eb99514fa5e541b13a0853f9e1bf04f01b
Sander Steenhuis <redsandro@gmail.com>
2016-06-07 12:18:39 +0200

    :wrench: Fix for PSPP

--- automatic/pspp/tools/chocolateyInstall.ps1
+++ automatic/pspp/tools/chocolateyInstall.ps1
@@ -1,10 +1,4 @@
-$packageName = '{{PackageName}}'
-$installerType = 'EXE'
+#$url      = 'http://sourceforge.net/projects/pspp4windows/files/{{DateStamp}}/pspp-{{VersionSec}}%2B{{DateStampSec}}-snapshot-32bits-setup.exe/download'
+#$url64      = 'http://sourceforge.net/projects/pspp4windows/files/{{DateStamp}}/pspp-{{VersionSec}}%2B{{DateStampSec}}-snapshot-64bits-setup.exe/download'
 
-$url      = 'http://sourceforge.net/projects/pspp4windows/files/{{DateStamp}}/pspp-{{VersionSec}}%2B{{DateStampSec}}-snapshot-32bits-setup.exe/download'
-$url64      = 'http://sourceforge.net/projects/pspp4windows/files/{{DateStamp}}/pspp-{{VersionSec}}%2B{{DateStampSec}}-snapshot-64bits-setup.exe/download'
-
-$silentArgs   = '/S'
-$validExitCodes = @(0)
- 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "{{PackageName}}" "EXE" "/S" "{{DownloadUrl}}" "{{DownloadUrlx64}}" -validExitCodes @(0)
