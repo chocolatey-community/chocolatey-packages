a4f16e564199224ea2d9aff4ef7edd1e920568e1
leonghui <leonghui@users.noreply.github.com>
2016-06-23 17:07:40 +0800

    Create ChocolateyUninstall.ps1

---
+++ automatic/tuniac/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+$packageName = '{{PackageName}}'
+$programName = 'Tuniac 1.0'
+$fileType = 'EXE'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+# unable to suppress MsgBox prompt, will contact author to use SuppressibleMsgBox instead
+
+$key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
+
+$uninstaller = Get-ChildItem $key | %{ Get-ItemProperty $_.PSPath } | ?{ $_.DisplayName -match $programName }
+
+$uninstallString = $uninstaller.uninstallString
+
+if ($uninstallString) {
+    Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallString
+}
