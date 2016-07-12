ac85b96cf78bfccac82e31637830417ec7552853
030 <chocolatey030@gmail.com>
2016-07-12 23:24:14 +0200

    [GH-256] move eclipse from 030 to this repo

---
+++ packages/eclipse/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = '{{PackageName}}'
+
+if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+. "$PSScriptRoot\Uninstall-ZipPackage.ps1"
+. "$PSScriptRoot\Uninstall-DesktopLinkAndPinnedTaskBarItem.ps1"
+
+Uninstall-ZipPackage "$packageName"
+
+Uninstall-DesktopLinkAndPinnedTaskBarItem "$packageName"
+Uninstall-DesktopLinkAndPinnedTaskBarItem "$packageName"
