865903553aa39161d9e5ab47cdc387d469c97446
RB <RedBaron2@users.noreply.github.com>
2016-07-14 21:48:02 -0700

    Create chocolateyUninstall.ps1
    This is a Uninstall file for the Chromium Package. This will find if a registry key exists, and then find the uninstall string from the registry. This may not be silent

---
+++ automatic/chromium/tools/chocolateyUninstall.ps1
@@ -0,0 +1,25 @@
+$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+$validExitCodes = @(0)
+
+	
+	$chromium_string = "\SOFTWARE" + $bitness +"\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
+	$hive = "hkcu"
+	$Chromium = $hive + ":" + $chromium_string
+
+
+	if (Test-Path $Chromium) {
+	  $hk_level = "hkcu"
+	} else {
+	  $hk_level = "hkcu"
+	}
+
+	$bitness = Get-ProcessorBits
+	@{$true = "\WOW6432Node"; $false = ""}[$bitness -eq "64"]
+
+	$file = (Get-ItemProperty -Path ( $hk_level + ":" +$chromium_string ) ).UninstallString
+  
+
+return $file
+
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
