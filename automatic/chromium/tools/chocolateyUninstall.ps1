5d66159395a2a67acad2062a3a9a124ddd54e152
RB <RedBaron2@users.noreply.github.com>
2016-07-21 22:50:07 -0700

    Update chocolateyUninstall.ps1
    This should allow those that installed to the hkcu of the original install to uninstall through chocolatey. Even if it was done on a 64bit system.

--- automatic/chromium/tools/chocolateyUninstall.ps1
+++ automatic/chromium/tools/chocolateyUninstall.ps1
@@ -3,21 +3,26 @@ $installerType = 'EXE'
 $validExitCodes = @(0)
 
 	
-	$chromium_string = "\SOFTWARE" + $bitness +"\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
+	$chromium_string = "\SOFTWARE" + $bitty +"\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 	$hive = "hkcu"
 	$Chromium = $hive + ":" + $chromium_string
 
-
-	if (Test-Path $Chromium) {
+	if ( (Test-Path $Chromium) -eq $true ) {
 	  $hk_level = "hkcu"
 	} else {
 	  $hk_level = "hklm"
 	}
 
 	$bitness = Get-ProcessorBits
-	@{$true = "\WOW6432Node"; $false = ""}[$bitness -eq "64"]
+	$bitty = @{$true = "\WOW6432Node"; $false = ""}[$bitness -eq "64"]
 
-	$file = (Get-ItemProperty -Path ( $hk_level + ":" +$chromium_string ) ).UninstallString
+  if ( (Test-Path ( $hk_level + ":" +$chromium_string )) -eq $true ) {
+    $chromium_key = ( $hk_level + ":" + $chromium_string )
+  } else  {
+    $chromium_key = ( "hkcu:" + "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium" )
+  }
+
+	$file = (Get-ItemProperty -Path ( $chromium_key ) ).UninstallString
   
 
 return $file
