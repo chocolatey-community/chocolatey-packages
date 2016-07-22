eeebeaf9ffcf5d8f40a8c2033386590b7c2ce2aa
RB <RedBaron2@users.noreply.github.com>
2016-07-21 23:16:53 -0700

    Update chocolateyInstall.ps1
    Slimmed the file down, adjusted for the functions, everything should work better.

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -4,34 +4,20 @@ $version = '{{PackageVersion}}'
 $url = '{{DownloadUrl}}'
 $url64bit = '{{DownloadUrlx64}}'
 
-
-function GetLevel() {
-
 	$chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 	$hive = "hkcu"
 	$Chromium = $hive + ":\" + $chromium_string
   
   if (Test-Path $Chromium) {
-    $level = ''
+    $silentArgs = ''
   } else {
-    $level = '--system-level --do-not-launch-chrome'
+    $silentArgs = '--system-level --do-not-launch-chrome'
   }
   
-  return $level
-}
-
-function Get-32bitOnlyInstalled {
-  $systemIs64bit = Get-ProcessorBits 64
+  $systemIs64bit = Get-ProcessorBits
   
-  if (-Not $systemIs64bit) {
-    return $false
-}
-
-}
-  $silentArgs = GetLevel
-  
-  if ((Get-32bitOnlyInstalled) -or (Get-ProcessorBits 32)) {
+  if ($systemIs64bit -eq "32") {
     Install-ChocolateyPackage $packageName $fileType $silentArgs $url
   } else {
     Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
-}
+  }
