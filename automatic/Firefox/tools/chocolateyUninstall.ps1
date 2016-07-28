62d1519b37216afc3b86fea6c10e22f3aff91728
RB <RedBaron2@users.noreply.github.com>
2016-07-27 18:34:02 -0700

    Create chocolateyUninstall.ps1
    This is a proposed change for a uninstall for Firefox by sparkvilla's PR #173

---
+++ automatic/Firefox/tools/chocolateyUninstall.ps1
@@ -0,0 +1,24 @@
+$packageName = '{{PackageName}}'
+$fileType = 'exe'
+$validExitCodes = @(0)
+$silentArgs = '-ms'
+
+  $regUninstallDir = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
+  $regUninstallDirWow64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
+  
+  if ( Get-ProcessorBits 64 ) {
+      if (Test-Path $regUninstallDirWow64) {
+        $uninstallPaths += $(Get-ChildItem $regUninstallDirWow64).Name
+      }
+  }
+  
+  $uninstallPaths = $(Get-ChildItem $regUninstallDir).Name
+
+  $uninstallPath = $uninstallPaths -match
+    "Mozilla Firefox [\d\.]+ \([^\s]+ [a-zA-Z\-]+\)" | Select -First 1
+  
+  $firefox_key = ( $uninstallPath.replace('HKEY_LOCAL_MACHINE\','HKLM:\') )
+   	
+  $file = (Get-ItemProperty -Path ( $firefox_key ) ).UninstallString 
+  
+  Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
