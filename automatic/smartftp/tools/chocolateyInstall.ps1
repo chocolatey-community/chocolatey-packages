993cc2569bbb1b4bf019fbf5f6d5165ca38750f5
Mat <mberchtold@gmail.com>
2016-09-07 21:33:10 +0200

    SmartFTP: updated install script
    use registry to find installed package

--- automatic/smartftp/tools/chocolateyInstall.ps1
+++ automatic/smartftp/tools/chocolateyInstall.ps1
@@ -1,18 +1,30 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'MSI'
-$32BitUrl  = '{{DownloadUrl}}'
-$64BitUrl  = '{{DownloadUrlx64}}'
-$silentArgs = '/quiet'
-$validExitCodes = @(0)
+function Check-SoftwareInstalled ($displayName, $displayVersion) {
+  $registryPaths = @(
+    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+    'HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+  )
 
-$alreadyInstalled = Get-WmiObject -Class Win32_Product | Where-Object {
-  $_.Name -eq "SmartFTP Client" -and
-  $_.Version -eq $version
+  return Get-ItemProperty $registryPaths -ErrorAction SilentlyContinue | Where-Object {
+    $_.DisplayName -eq $displayName -and $_.DisplayVersion -eq $displayVersion
+  }
 }
 
+$version = '{{PackageVersion}}'
+
+$packageArgs = @{
+  packageName = '{{PackageName}}'
+  fileType = 'msi'
+  url  = '{{DownloadUrl}}'
+  url64bit  = '{{DownloadUrlx64}}'
+  silentArgs = '/quiet'
+  validExitCodes = @(0)
+  softwareName  = 'SmartFTP Client'
+}
+
+$alreadyInstalled = Check-SoftwareInstalled 'SmartFTP Client' $version
+
 if ($alreadyInstalled) {
-  Write-Output $('SmartFTP ' +
-    $version + ' is already installed.')
+  Write-Output $('SmartFTP Client ' + $version + ' is already installed.')
 } else {
-  Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl $64BitUrl -validExitCodes $validExitCodes
+  Install-ChocolateyPackage @packageArgs
 }
