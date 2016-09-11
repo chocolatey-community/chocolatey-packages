49c7a48648e41de5cdcf1710a078c8de6da40727
Ruiyang Li <Elderry@outlook.com>
2016-09-11 22:25:06 +0800

    Added checksum for autohotkey.portable

--- automatic/autohotkey.portable/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,17 @@
-﻿$packageName = 'autohotkey.portable'
-$url = '{{DownloadUrl}}'
+$packageName   = 'autohotkey.portable'
+$checksumType  = 'MD5'
+$url           = '{{DownloadUrl}}'
+$checksum      = '521c8acba859da18167d1dcca5decad2'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url64 = '{{DownloadUrlx64}}'
+$url64         = '{{DownloadUrlx64}}'
+$checksum64    = 'a9435c1353c83f82afacd94454143ffa'
 
-Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64
+Install-ChocolateyZipPackage `
+    -PackageName $packageName `
+    -Url $url `
+    -UnzipLocation $unzipLocation `
+    -Url64bit $url64 `
+    -Checksum $checksum `
+    -ChecksumType $checksumType `
+    -Checksum64 $checksum64 `
+    -ChecksumType64 $checksumType
