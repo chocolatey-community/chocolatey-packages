b075a6e3595769c306c40b452c57c4b9414b3f41
Miodrag Milic <miodrag.milic@gmail.com>
2016-09-28 00:19:27 +0200

    moved install to root of the system drive and provided parameter for the install dir

--- automatic/python3/tools/chocolateyInstall.ps1
+++ automatic/python3/tools/chocolateyInstall.ps1
@@ -5,24 +5,43 @@ $url32       = '{{DownloadUrl}}'
 $url64       = '{{DownloadUrlx64}}'
 $checksum32  = ''
 $checksum64  = ''
-$fileType    = 'exe'
-$partialInstallArgs = '/quiet InstallAllUsers=1 PrependPath=1'
 
-$installPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+if ($Env:ChocolateyPackageParameters -match '/InstallDir:\s*(.+)') {
+    $installDir = $Matches[1]
+    if ($installDir.StartsWith("'") -or $installDir.StartsWith('"')){  $installDir = $installDir -replace '^.|.$' }
+    $parent = Split-Path $installDir
+    mkdir -force $parent -ea 0 | out-null
+}
 
-$installArgs = $($partialInstallArgs + ' TargetDir="' + $installPath + '"')
+$installDir   = '{0}\Python{1}' -f $Env:SystemDrive, ($Env:ChocolateyPackageVersion -replace '\.').Substring(0,2)
+$installArgs  = '/quiet InstallAllUsers=1 PrependPath=1 TargetDir="{0}"' -f $installDir
+Write-host $installArgs
+
+$params = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  silentArgs     = $installArgs
+  url            = $url32
+  url64Bit       = $url64
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+}
 
 # If the package is only intended for the 32-bit version, only pass
 # the 32-bit version to the install package function.
 if ($packageName -match 32) {
-  Install-ChocolateyPackage $packageName $fileType $installArgs $url32 -Checksum $checksum32 -ChecksumType sha256
-} else {
-  Install-ChocolateyPackage $packageName $fileType $installArgs $url32 $url64 -Checksum $checksum32 -ChecksumType sha256 -Checksum64 $checksum64 -ChecksumType64 sha256
+    $params.Remove('url64Bit')
+    $params.Remove('checksum64')
 }
+
+Write-Host "Installing to '$installDir'"
+Install-ChocolateyPackage @params
 
 # Generate .ignore files for unwanted .exe files
 $exesLeftToPathInclude = @('python.exe', 'pythonw.exe', 'pip.exe', 'easy_install.exe');
-Get-ChildItem -Path $installPath -Recurse | Where {
+Get-ChildItem -Path $installDir -Recurse | Where {
 
   $_.Extension -eq '.exe'} | % {
   # Exclude .exe files that should en up in PATH
