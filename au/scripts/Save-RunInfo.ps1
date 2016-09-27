50e2ed524c15880abbf9e1dbc7311b5416f92f75
Gary Ewan Park <gep13@gep13.co.uk>
2016-09-27 20:07:35 +0100

    Not required anymore

--- au/scripts/Save-RunInfo.ps1
+++
@@ -1,9 +0,0 @@
-function Save-RunInfo {
-    "Saving run info"
-    try { $p = $Info.Options.Mail.Password } catch {}
-    if ($p) { $Info.Options.Mail.Password='' }
-
-    $Info | Export-CliXML update_info.xml
-
-    if ($p) { $Info.Options.Mail.Password = $p }
-}
