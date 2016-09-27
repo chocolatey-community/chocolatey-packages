a811eb03d861e0f4deac2af885e366ac1896818e
Miodrag Milic <miodrag.milic@gmail.com>
2016-09-27 23:38:10 +0200

    Added AU updater and just whats needed for it to work + Checksum

--- automatic/python3/tools/chocolateyInstall.ps1
+++ automatic/python3/tools/chocolateyInstall.ps1
@@ -1,10 +1,11 @@
 ﻿# This file should be identical for all python3* packages
 
 $packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$url64 = '{{DownloadUrlx64}}'
-$version = '{{PackageVersion}}'
-$fileType = 'exe'
+$url32       = '{{DownloadUrl}}'
+$url64       = '{{DownloadUrlx64}}'
+$checksum32  = ''
+$checksum64  = ''
+$fileType    = 'exe'
 $partialInstallArgs = '/quiet InstallAllUsers=1 PrependPath=1'
 
 $installPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
@@ -14,14 +15,14 @@ $installArgs = $($partialInstallArgs + ' TargetDir="' + $installPath + '"')
 # If the package is only intended for the 32-bit version, only pass
 # the 32-bit version to the install package function.
 if ($packageName -match 32) {
-  Install-ChocolateyPackage $packageName $fileType $installArgs $url
+  Install-ChocolateyPackage $packageName $fileType $installArgs $url32 -Checksum $checksum32 -ChecksumType sha256
 } else {
-  Install-ChocolateyPackage $packageName $fileType $installArgs $url $url64
+  Install-ChocolateyPackage $packageName $fileType $installArgs $url32 $url64 -Checksum $checksum32 -ChecksumType sha256 -Checksum64 $checksum64 -ChecksumType64 sha256
 }
 
 # Generate .ignore files for unwanted .exe files
 $exesLeftToPathInclude = @('python.exe', 'pythonw.exe', 'pip.exe', 'easy_install.exe');
-Get-ChildItem -Path $destinationFolder -Recurse | Where {
+Get-ChildItem -Path $installPath -Recurse | Where {
 
   $_.Extension -eq '.exe'} | % {
   # Exclude .exe files that should en up in PATH
---
+++ automatic/python3/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+
+$releases = 'https://www.python.org/downloads'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $re      = '.exe'
+    $url     = $download_page.links | ? href -match $re | select -First 1
+    if ($url.InnerText -notmatch 'Download Python (3[\d\.]+)' ) { throw "Python version 3.x not found on $releases" }
+
+    $url32   = $url.href
+    $url64   = $url32 -replace '.exe', '-amd64.exe'
+    $version = $url32 -split '-|.exe' | select -Last 1 -Skip 1
+
+    return @{ URL32 = $url32; URL64 = $url64; Version = $version }
+}
+
+update
