4394cfd12f1e6f481f6818da4e710b7c1a9f4071
Gary Ewan Park <gep13@gep13.co.uk>
2016-10-04 21:19:32 +0100

    (calibre) Updating to 2.69.0

--- automatic/calibre/tools/chocolateyInstall.ps1
+++ automatic/calibre/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'calibre'
   fileType       = 'MSI'
-  url            = 'https://download.calibre-ebook.com/2.68.0/calibre-2.68.0.msi'
-  url64Bit       = 'https://download.calibre-ebook.com/2.68.0/calibre-64bit-2.68.0.msi'
-  checksum       = '08717BBC3B37A664EDE623BC3B8C99AFD831792BFAEE5E61C935BE94159F7304'
-  checksum64     = '69991DB1AC753E4A5586432D6970012E3278B9B0FE38E1670C4F323CAC25E317'
+  url            = 'https://download.calibre-ebook.com/2.69.0/calibre-2.69.0.msi'
+  url64Bit       = 'https://download.calibre-ebook.com/2.69.0/calibre-64bit-2.69.0.msi'
+  checksum       = 'a35c36c065be4ae4796c01676722a45af19c6e2db864fbcc2b6c0f36c9887bf6'
+  checksum64     = '46d02d6866f8d2e2630a131589cf9cd5258a7d1e5b1554aa1aaad2fae1cb6083'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/quiet'
