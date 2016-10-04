40ffe44f462b1be9bfc5c2fb1358a54638a11d5c
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-04 23:53:38 +0200

    ah.install AU updater

--- automatic/autohotkey.install/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.install/tools/chocolateyInstall.ps1
@@ -1,9 +1,31 @@
-﻿$name   = 'autohotkey.install'
-$url = '{{DownloadUrl}}'
-$url64 = $url
-$silent = '/S'
+﻿$ErrorActionPreference = 'Stop'
 
-$is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
-if($is64bit) { $silent += ' /x64' }
+$packageName = 'autohotkey.install'
+$url32       = 'https://autohotkey.com/download/1.1/AutoHotkey112401_Install.exe'
+$url64       = $url32
+$checksum32  = '78ea671c3305465933a5d00f39da7e86c116a8a5a1849835196a27799ac29f8f'
+$checksum64  = $checksum32
 
-Install-ChocolateyPackage $name 'exe' $silent $url $url64
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url32
+  url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = $silent
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
+}
+Install-ChocolateyPackage @packageArgs
+
+$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
+if ($installLocation)  {
+    Write-Host "$packageName installed to '$installLocation'"
+}
+else {
+    Write-Warning "Can't find $PackageName install location"
+}
+
