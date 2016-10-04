0a096e65a4fd9c273501b367ec64d9cce635c429
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-04 23:59:56 +0200

    ahk.portable AU

---
+++ automatic/autohotkey.portable/update.ps1
@@ -0,0 +1,14 @@
+. $PSScriptRoot\..\autohotkey\update.ps1
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL_p32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL_p64)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+update -NoCheckUrl
