a0cc93bebe923515c19ee234318d63f5aee92b2e
Gary Ewan Park <gep13@gep13.co.uk>
2016-10-05 22:26:43 +0100

    (audacity) Update Audacity with checksums

--- automatic/audacity/tools/chocolateyInstall.ps1
+++ automatic/audacity/tools/chocolateyInstall.ps1
@@ -1,8 +1,15 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$fileArgs = '/VERYSILENT'
-$version = '{{PackageVersion}}'
+﻿$ErrorActionPreference = 'Stop'
 
-$url = Get-UrlFromFosshub "http://www.fosshub.com/genLink/Audacity/audacity-win-${version}.exe"
+$url = Get-UrlFromFosshub "https://www.fosshub.com/Audacity.html/audacity-win-2.1.2.exe"
 
-Install-ChocolateyPackage $packageName $fileType $fileArgs $url
+$packageArgs = @{
+  packageName    = 'audacity'
+  fileType       = 'exe'
+  url            = $url
+  checksum       = '22E0F0ADA3E8D24690DD741CA9FEB868DFFC024D45D2CD3168F8C54C47EEC3C9'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
