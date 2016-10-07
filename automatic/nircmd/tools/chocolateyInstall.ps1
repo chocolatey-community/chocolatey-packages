c05e8a4f0298fe1c279f9fe53626f2cda51d81ff
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-07 15:09:22 +0200

    nircmd AU and new install

--- automatic/nircmd/tools/chocolateyInstall.ps1
+++ automatic/nircmd/tools/chocolateyInstall.ps1
@@ -1,6 +1,20 @@
-﻿$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$url64 = '{{DownloadUrlx64}}'
-$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyZipPackage $packageName $url $unzipLocation $url64
+$packageName = 'nirmcmd'
+$url32       = 'http://www.nirsoft.net/utils/nircmd.zip'
+$url64       = 'http://www.nirsoft.net/utils/nircmd-x64.zip'
+$checksum32  = 'f56a6166f8956c507cc2bfec71339f3c467c0421fe6d16fbe4c52a36d79ce0ea'
+$checksum64  = '37445b39e2fc437688164866c495bd28890ded5d0dbf71615bf92776e57c5189'
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url32
+  url64Bit       = $url64
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/nircmd/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'http://www.nirsoft.net/utils/nircmd.html'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page.RawContent -match 'NirCmd v(.+)'
+
+    $version = $Matches[1]
+    $url32   = 'http://www.nirsoft.net/utils/nircmd.zip'
+    $url64   = 'http://www.nirsoft.net/utils/nircmd-x64.zip'
+
+    @{ URL32 = $url32; URL64 = $url64; Version = $version }
+}
+
+update
