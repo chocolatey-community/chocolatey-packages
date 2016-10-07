1c9aef9a9f58970f5b6c7c78b938032293a5189d
Gary Ewan Park <gep13@gep13.co.uk>
2016-10-07 12:33:24 +0100

    (avidemux) Change to using AU

--- automatic/avidemux/tools/chocolateyInstall.ps1
+++ automatic/avidemux/tools/chocolateyInstall.ps1
@@ -1,18 +1,22 @@
-﻿$PSScriptRoot = Split-Path -parent $MyInvocation.MyCommand.Definition
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/S'
+$packageName = 'avidemux'
+$url32 = Get-UrlFromFosshub 'to_be_determined'
+$url64 = Get-UrlFromFosshub 'to_be_determined'
+$checksum32  = 'to_be_determined'
+$checksum64  = 'to_be_determined'
 
-# {\{DownloadUrlx64}\} gets “misused” here as genLink
-# part for FossHub (only visible in the template, not in
-# the final file generated for the package.)
-$genLinkUrl = '{{DownloadUrlx64}}32.exe'
-$genLinkUrl64bit = '{{DownloadUrlx64}}64.exe'
-$validExitCodes = @(0, 1223)
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url32
+  url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/S'
+  validExitCodes         = @(0, 1223)
+}
 
-$url = Get-UrlFromFosshub $genLinkUrl
-$url64bit = Get-UrlFromFosshub $genLinkUrl64bit
-
-Install-ChocolateyPackage $packageName $fileType $silentArgs `
-  $url $url64bit -validExitCodes $validExitCodes
+Install-ChocolateyPackage @packageArgs
