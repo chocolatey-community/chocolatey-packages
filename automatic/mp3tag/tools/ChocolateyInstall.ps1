bdea4da39a80776cc537f197a9dc8c746b4fd34d
Gary Ewan Park <gep13@gep13.co.uk>
2016-10-08 20:38:41 +0100

    (mp3tag) Updated to use AU

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -1,14 +1,16 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$version = '{{PackageVersion}}'
-$silentArgs = '/S'
-$url = '{{DownloadUrl}}'
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'mp3tag'
+$url32       = 'http://download.mp3tag.de/mp3tagv279setup.exe'
+$checksum32  = '0674fe816add02fa911d5eb9e911344a0298eab9c63f151c830d9c4c62c43297'
+$silentArgs  = '/S'
+
 $PSScriptRoot = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 # The installer doesn’t like being renamed. The installation wouldn’t be
 # silent if it is renamed. Therefore the original filename is parsed
 # from the URL
-$installerPath = Join-Path $PSScriptRoot $url.Split('/')[-1]
+$installerPath = Join-Path $PSScriptRoot $url32.Split('/')[-1]
 $iniFile = Join-Path $PSScriptRoot 'Mp3tagSetup.ini'
 
 # Automatic language selection
@@ -26,7 +28,16 @@ language=$LCID
 # Create the ini file for the installer
 New-Item $iniFile -type file -force -value $iniContent
 
-Get-ChocolateyWebFile $packageName $installerPath $url
+$packageArgs = @{
+  packageName            = $packageName
+  fileFullPath           = $installerPath
+  fileType               = 'EXE'
+  url                    = $url32
+  checksum               = $checksum32
+  checksumType           = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
 
 # Download the file to $PSScriptRoot.
 # Using Chocolatey’s Installer-ChocolateyPackage function isn’t a good idea here
--- automatic/mp3tag/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$uninstString = (Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
-    'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall' |
-    Get-ItemProperty |
-    Where-Object {$_.DisplayName -match 'Mp3tag' } |
-    Select-Object -Property DisplayName, UninstallString).uninstallString
-
-if ($uninstString) {
-    Uninstall-ChocolateyPackage '{{PackageName}}' 'exe' '/S' $uninstString
-}
