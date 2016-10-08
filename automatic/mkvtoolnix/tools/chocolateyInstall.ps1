07c732ef9b17304b9c24d5bb70fc48b3c16f9499
Chocolatey AU <chocolatey@realdimensions.net>
2016-10-08 10:41:15 +0000

    AU:  updated: mkvtoolnix [skip ci]

--- automatic/mkvtoolnix/tools/chocolateyInstall.ps1
+++ automatic/mkvtoolnix/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'mkvtoolnix'
-$url32 = Get-UrlFromFosshub 'to_be_determined'
-$url64 = Get-UrlFromFosshub 'to_be_determined'
-$checksum32  = 'to_be_determined'
-$checksum64  = 'to_be_determined'
+$url32 = Get-UrlFromFosshub 'https://www.fosshub.com/MKVToolNix.html/mkvtoolnix-32bit-9.4.2-setup.exe'
+$url64 = Get-UrlFromFosshub 'https://www.fosshub.com/MKVToolNix.html/mkvtoolnix-64bit-9.4.2-setup.exe'
+$checksum32  = '66ce6fd8cf9d5aed6a8084aa659e3cc009b5ff2fb9c982a95b8e7f653dc86846'
+$checksum64  = '40cb363f7c6160df1da4851a6ed409afcc9945871b234393173c6800b3ee9ac1'
 
 $packageArgs = @{
   packageName    = $packageName
