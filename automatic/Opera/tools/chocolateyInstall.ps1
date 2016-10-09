1407b65bfa679d7531d74e6f12ad4776a4083ec7
Gary Ewan Park <gep13@gep13.co.uk>
2016-10-09 21:36:55 +0100

    (opera) Updating to use AU

--- automatic/Opera/tools/chocolateyInstall.ps1
+++ automatic/Opera/tools/chocolateyInstall.ps1
@@ -1,8 +1,19 @@
-﻿$packageName = '{{PackageName}}'
-$version = '{{PackageVersion}}'
-$fileType = 'exe'
-$installArgs = '/install /silent /launchopera 0 /setdefaultbrowser 0'
-$url = '{{DownloadUrl}}'
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'Opera'
+$url32       = 'http://get.geo.opera.com/pub/opera/desktop/40.0.2308.81/win/Opera_40.0.2308.81_Setup.exe'
+$checksum32  = '4496e3ff7d2143fd2781deb0b96e933bebd08ffceae9528adde9bc6a467f3990'
+$version     = '40.0.2308.81'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0'
+  validExitCodes = @(0)
+}
 
 $regPathModifierArray = @('Wow6432Node\', '')
 $alreadyInstalled = $null
@@ -17,5 +28,5 @@ foreach ($regPathModifier in $regPathModifierArray) {
 if ($alreadyInstalled) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
-  Install-ChocolateyPackage $packageName $fileType $installArgs $url
+  Install-ChocolateyPackage @packageArgs
 }
