d6d37a03e025fdfa95b9604d518b22fcb04da4e3
AdmiringWorm <kim.nordmo@gmail.com>
2016-10-10 17:29:58 +0200

    (absolute-installer) Updated to use AU

--- automatic/absolute-uninstaller/tools/chocolateyInstall.ps1
+++ automatic/absolute-uninstaller/tools/chocolateyInstall.ps1
@@ -1,6 +1,19 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'EXE'
-$url = '{{DownloadUrl}}'
-$silentArgs = "/S" # NSIS package
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+﻿$packageName = 'absolute-uninstaller'
+$url = 'http://download.glarysoft.com/ausetup.exe'
+$checksum = '1a9146ac0eaa94b6389625e743cc17406073f4217b3a46df3799dc834fe8119a'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName = $packageName
+  fileType = 'exe'
+  softwareName = 'Absolute Uninstaller'
+
+  checksum = $checksum
+  checksumType = $checksumType
+  url = $url
+
+  silentArgs = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
