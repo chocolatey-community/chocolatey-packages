d40a94d56a0e0a2aac7ba94ca6de3cf9980f19f3
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-11 23:06:02 +0200

    Troubleshooting AU - added UBP to iwr.

--- automatic/ant-renamer/update.ps1
+++ automatic/ant-renamer/update.ps1
@@ -13,7 +13,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
     $re    = 'install\.exe'
     $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
