b1f3ac0112ecfacef43956dd2b0b7feff6c5c187
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-11 23:13:22 +0200

    Troubleshooting AU - switched checksum mode for youtube-dl

--- automatic/youtube-dl/update.ps1
+++ automatic/youtube-dl/update.ps1
@@ -18,19 +18,19 @@ function global:au_GetLatest {
 
   $re    = '\.exe$'
   $url   = $domain + ($download_page.Links | ? href -match $re | select -First 1 -expand href)
-  $filename = $url.Substring($url.LastIndexOf('/') + 1);
-  $version  = $url -split '\/' | select -skip 1 -last 1;
+  $filename = $url.Substring($url.LastIndexOf('/') + 1)
+  $version  = $url -split '\/' | select -skip 1 -last 1
 
   $checksumAsset = $domain + ($download_page.Links | ? href -match "SHA2\-512SUMS$" | select -first 1 -expand href)
-  $checksum_page = Invoke-WebRequest -UseBasicParsing -Uri $checksumAsset;
-  $checksum = [regex]::Match($checksum_page, "([a-f\d]+)\s*$([regex]::Escape($filename))").Groups[1].Value;
+  $checksum_page = Invoke-WebRequest -Uri $checksumAsset
+  $checksum = [regex]::Match($checksum_page, "([a-f\d]+)\s*$([regex]::Escape($filename))").Groups[1].Value
 
   return @{
     Version = $version
     URL32 = $url
-    Checksum32 = $checksum
+    #Checksum32 = $checksum  <-- doesn't work on AV
     ChecksumType32 = "sha512"
-  };
+  }
 }
 
 update -ChecksumFor 32
