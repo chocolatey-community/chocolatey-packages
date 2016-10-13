36f9579c6545361cf54ed517571886fe23976f5e
RB <RedBaron2@users.noreply.github.com>
2016-10-13 12:46:33 -0700

    Update chocolateyInstall.ps1

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -12,16 +12,16 @@
   }
   
     $packageArgs = @{
-  packageName   = '{{PackageName}}'
+  packageName   = 'Chromium'
   fileType      = 'exe'
-  url           = '{{DownloadUrl}}'
-  url64bit      = '{{DownloadUrlx64}}'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/425094/mini_installer.exe'
+  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/425083/mini_installer.exe'
   silentArgs    = $silentArgs
   validExitCodes= @(0)
   softwareName  = 'Chromium'
-  checksum      = '{{Checksum}}'
+  checksum      = '97da1f6f835b4b6ca7156c723e897083'
   checksumType  = 'md5'
-  checksum64    = '{{Checksumx64}}'
+  checksum64    = '0ffc77b8638e7f5c0d5bb8c6cf02aeff'
   checksumType64= 'md5'
 }
 
