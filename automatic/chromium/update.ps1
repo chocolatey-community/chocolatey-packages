6bc6d3931d30801d6441913db531f36e5e8bd120
RB <RedBaron2@users.noreply.github.com>
2016-10-13 20:39:18 -0700

    Update update.ps1
    minor format changes for readability

--- automatic/chromium/update.ps1
+++ automatic/chromium/update.ps1
@@ -1,8 +1,8 @@
 
 import-module au
 
-$releases_x32 = 'https://chromium.woolyss.com/api/?os=windows&bit=32&out=string' # URL to for GetLatest 32bit
-$releases_x64 = 'https://chromium.woolyss.com/api/?os=windows&bit=64&out=string' # URL to for GetLatest 64bit
+ $releases_x32 = 'https://chromium.woolyss.com/api/?os=windows&bit=32&out=string' # URL to for GetLatest 32bit
+ $releases_x64 = 'https://chromium.woolyss.com/api/?os=windows&bit=64&out=string' # URL to for GetLatest 64bit
 
 function global:au_SearchReplace {
    @{
@@ -19,20 +19,20 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $hashtype = 'md5'
-  $download_page32 = Invoke-WebRequest -Uri $releases_x32
-  $download_page64 = Invoke-WebRequest -Uri $releases_x64
-	$val32 = $download_page32 -split ";"
-	$val64 = $download_page64 -split ";" 
-	$chromium32 = $val32 | out-string | ConvertFrom-StringData
-	$chromium64 = $val64 | out-string | ConvertFrom-StringData
-  $checksum32 = $chromium32.checksum_md5
-  $checksum64 = $chromium64.checksum_md5
-	$version = $chromium64.version
-  $url32   = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/<revision>/mini_installer.exe'
-  $url64   = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/<revision>/mini_installer.exe'
-  $url32   = $url32 -replace '<revision>', $chromium32.revision
-  $url64   = $url64 -replace '<revision>', $chromium64.revision
+ $hashtype = 'md5'
+ $download_page32 = Invoke-WebRequest -Uri $releases_x32
+ $download_page64 = Invoke-WebRequest -Uri $releases_x64
+ $val32 = $download_page32 -split ";"
+ $val64 = $download_page64 -split ";" 
+ $chromium32 = $val32 | out-string | ConvertFrom-StringData
+ $chromium64 = $val64 | out-string | ConvertFrom-StringData
+ $checksum32 = $chromium32.checksum_md5
+ $checksum64 = $chromium64.checksum_md5
+ $version = $chromium64.version
+ $url32   = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/<revision>/mini_installer.exe'
+ $url64   = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/<revision>/mini_installer.exe'
+ $url32   = $url32 -replace '<revision>', $chromium32.revision
+ $url64   = $url64 -replace '<revision>', $chromium64.revision
 
     return @{
     URL32 = $url32; URL64 = $url64; Version = $version;
