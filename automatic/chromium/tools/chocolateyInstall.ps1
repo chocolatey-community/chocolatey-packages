9e3481fb9a42827ed51d0a9f2ed993fc074a5317
Gary Ewan Park <gep13@gep13.co.uk>
2016-10-14 07:40:13 +0100

    (chromium) Fixed indentation

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -1,28 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
 
-  $version = '56.0.2890.0'
+$version = '56.0.2890.0'
 
-	$chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
-	$hive = "hkcu"
-	$Chromium = $hive + ":" + $chromium_string
-  
-  if (Test-Path $Chromium) {
-    $silentArgs = ''
-  } else {
-    $silentArgs = '--system-level --do-not-launch-chrome'
-  }
-  
-  $packageArgs = @{
+$chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
+$hive = "hkcu"
+$Chromium = $hive + ":" + $chromium_string
+
+if (Test-Path $Chromium) {
+  $silentArgs = ''
+} else {
+  $silentArgs = '--system-level --do-not-launch-chrome'
+}
+
+$packageArgs = @{
   packageName   = 'chromium'
   fileType      = 'exe'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/425094/mini_installer.exe'
-  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/425083/mini_installer.exe'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/425259/mini_installer.exe'
+  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/425256/mini_installer.exe'
   silentArgs    = $silentArgs
   validExitCodes= @(0)
   softwareName  = 'Chromium'
-  checksum      = '97da1f6f835b4b6ca7156c723e897083'
+  checksum      = '0e9dfb1ac26e79092c50d373153265e5'
   checksumType  = 'md5'
-  checksum64    = '0ffc77b8638e7f5c0d5bb8c6cf02aeff'
+  checksum64    = 'eb7aa4f1eb6fbece11a0cb072cadd84f'
   checksumType64= 'md5'
-  }
+}
 
-    Install-ChocolateyPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
