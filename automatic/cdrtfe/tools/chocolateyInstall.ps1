647204d1b42cde3f95bddcf54d061ebe55a60f9e
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-20 11:19:25 +0200

    (cdrtfe) AU migration; nuspec update; install update; uninstall remove;

--- automatic/cdrtfe/tools/chocolateyInstall.ps1
+++ automatic/cdrtfe/tools/chocolateyInstall.ps1
@@ -1,7 +1,22 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$installArgs = "/VERYSILENT"
-# {\{DownloadUrlx64}\} gets “misused” here as 32-bit download link due to limitations of Ketarin/chocopkgup
-$url = '{{DownloadUrlx64}}'
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage $packageName $fileType $installArgs $url
+$packageName = 'cdrtfe'
+$url32       = 'http://sourceforge.net/projects/cdrtfe/files/cdrtfe/cdrtfe%201.5.6/cdrtfe-1.5.6.exe'
+$url64       = $url32
+$checksum32  = '5bb668821147bbc2acc7beacf242b25d84f9e3bfb1e7ac44025c204a1ae6cec4'
+$checksum64  = $checksum32
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url32
+  url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/VERYSILENT'
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
+}
+Install-ChocolateyPackage @packageArgs
