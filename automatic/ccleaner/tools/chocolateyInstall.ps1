e90f6e7b11ba438bf133b2d759b1a91954c893f4
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-20 16:27:05 +0200

    (ccleaner) AU; improved everything;

--- automatic/ccleaner/tools/chocolateyInstall.ps1
+++ automatic/ccleaner/tools/chocolateyInstall.ps1
@@ -1,27 +1,40 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'EXE'
-$LCID = (Get-Culture).LCID
-$url = '{{DownloadUrl}}'
+$ErrorActionPreference = 'Stop'
 
-# The installer contains the 64-bit version too.
-# Therefore $url64 is identical to $url
-$url64 = $url
-$silentArgs = "/S /L=$LCID"
+$packageName = 'ccleaner'
+$url32       = 'http://download.piriform.com/ccsetup523.exe'
+$url64       = $url32
+$checksum32  = 'dcd3e05242212af0a07915c13c609b9fa465caab9aee5873810f76f684f90eca'
+$checksum64  = $checksum32
 
-# Please test every new version of CCleaner for possible adware/spyware/crapware
-# which installs silently together with Piriform software products.
-# Only push the new package to the gallery if you are 100% sure that this
-# package prevents the install of the bundled adware.
-$regAdd = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\regAdd.ps1"
+if ($Env:ChocolateyPackageParameters -match '/UseSystemLocale') {
+    Write-Host "Using system locale"
+    $locale = "/L=" + (Get-Culture).LCID 
+}
 
-$validExitCodes = @(0)
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'EXE'
+  url                    = $url32
+  url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = "/S $locale"
+  validExitCodes         = @(0)
+}
+Install-ChocolateyPackage @packageArgs
 
-
-# This adds a registry key which prevents Google Chrome
-# from getting installed together with Piriform software products.
-Start-ChocolateyProcessAsAdmin "& `'$regAdd`'"
-
-Install-ChocolateyPackage $packageName $installerType $silentArgs `
-  $url $url64 -validExitCodes $validExitCodes
-
-
+# This adds a registry keys which prevent Google Chrome from getting installed together with Piriform software products.
+$regDirChrome    = 'HKLM:\SOFTWARE\Google\No Chrome Offer Until'
+$regDirToolbar   = 'HKLM:\SOFTWARE\Google\No Toolbar Offer Until'
+if (Get-ProcessorBits 64) {
+    $regDirChrome  = $regDirChrome -replace 'SOFTWARE', 'SOFTWARE\Wow6432Node'
+    $regDirToolbar = $regDirChrome -replace 'SOFTWARE', 'SOFTWARE\Wow6432Node'
+}
+& {
+    New-Item $regDirChrome -ItemType directory -Force
+    New-ItemProperty -Name "Piriform Ltd" -Path $regDirChrome -PropertyType DWORD -Value 20991231 -Force
+    New-Item $regDirToolbar -ItemType directory -Force
+    New-ItemProperty -Name "Piriform Ltd" -Path $regDirToolbar -PropertyType DWORD -Value 20991231 -Force
+} | Out-Null
