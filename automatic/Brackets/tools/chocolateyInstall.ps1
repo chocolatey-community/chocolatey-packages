5fd9addd0052e1c98cd6d1e9aa86572a43f717e6
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-20 10:46:15 +0200

    (brackets) migrated to AU; nuspec updated; installer updated; uninstaller removed;

--- automatic/Brackets/tools/chocolateyInstall.ps1
+++ automatic/Brackets/tools/chocolateyInstall.ps1
@@ -1,24 +1,22 @@
-﻿$packageName = '{{PackageName}}'
-$version = '{{PackageVersion}}'
+$ErrorActionPreference = 'Stop'
 
-$bracketsRegistryVersion = $version -replace '^(\d+\.\d+)\..+', '$1'
+$packageName = 'brackets'
+$url32       = 'https://github.com/adobe/brackets/releases/download/release-1.7/Brackets.Release.1.7.msi'
+$url64       = $url32
+$checksum32  = 'f5c8d66eba8cdb67c607aa818ec93a7a0a578764782fc7b414ce91a1764b4aff'
+$checksum64  = $checsum32
 
-$params = @{
-  PackageName = $packageName;
-  FileType = 'msi';
-  SilentArgs = '/q /norestart';
-  Url = '{{DownloadUrl}}';
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'MSI'
+  url                    = $url32
+  url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/q /norestart'
+  validExitCodes         = @(0)
+  registryUninstallerKey = $packageName
 }
-
-# We know that using Win32_Product is bad.
-# Is there a better alternative to check if it’s
-# already installed?
-$alreadyInstalled = Get-WmiObject -Class Win32_Product |
-  Where-Object {($_.Name -eq 'Brackets') -and
-  ($_.Version -match $bracketsRegistryVersion)}
-
-if ($alreadyInstalled) {
-  Write-Output "Brackets $version is already installed. Skipping installation."
-} else {
-  Install-ChocolateyPackage @params
-}
+Install-ChocolateyPackage @packageArgs
