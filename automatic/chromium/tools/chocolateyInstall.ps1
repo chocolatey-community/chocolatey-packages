84e2f6cf72cbe994d6c4cafb4c202b51c7876fae
Chocolatey <chocolatey@realdimensions.net>
2016-10-22 06:09:45 +0000

    AU:  updated - chromium [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -15,14 +15,14 @@ if (Test-Path $Chromium) {
 $packageArgs = @{
   packageName   = 'chromium'
   fileType      = 'exe'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/426726/mini_installer.exe'
-  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/426724/mini_installer.exe'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/426964/mini_installer.exe'
+  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/426967/mini_installer.exe'
   silentArgs    = $silentArgs
   validExitCodes= @(0)
   softwareName  = 'Chromium'
-  checksum      = '4b851eab0bf8c7344f3e603de641e658'
+  checksum      = '2a4ca25259cedce1ffc4b4fdba8dcc28'
   checksumType  = 'md5'
-  checksum64    = '48514a5169b83c2c2f830885dce6374a'
+  checksum64    = '70c2c7f51b83bc1253a9a899553a18b7'
   checksumType64= 'md5'
 }
 
