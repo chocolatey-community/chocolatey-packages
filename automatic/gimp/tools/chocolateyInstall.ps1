074c7495f96c35387a3d3bde969950edeb7a884b
AdmiringWorm <kim.nordmo@gmail.com>
2016-10-25 16:37:50 +0200

    Added comment regarding installer containing 32bit and 64bit

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -9,6 +9,9 @@ $packageArgs = @{
   checksum64     = '41e98ef75c2992acc9b481147bf3db35652a4c7e1da70bb06a2d2ec9a0853a2d'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
+
+  # The installer contains both 32-bit and 64-bit versions of GIMP, and will automatically use the appropriate one.
+  # Text quoted from <https://www.gimp.org/downloads/>, look below download buttons.
   url            = 'https://download.gimp.org/mirror/pub/gimp/v2.8/windows/gimp-2.8.18-setup.exe'
   url64bit       = 'https://download.gimp.org/mirror/pub/gimp/v2.8/windows/gimp-2.8.18-setup.exe'
 
