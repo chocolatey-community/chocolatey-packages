e8726c888efc5bc4ba24a5acb7795ce200ec058a
AdmiringWorm <kim.nordmo@gmail.com>
2016-10-25 03:35:14 +0200

    (gimp) Migrated to AU
    * Added update.ps1 script
    * Updated install script
    * Updated icon url
    * updated package source url

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -1,9 +1,21 @@
-﻿$packageName = '{{PackageName}}'
-$version = '{{PackageVersion}}'
-$url = '{{DownloadUrl}}'
-$url64bit = $url
-$installerType = 'exe'
-$installArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'gimp'
+  fileType       = 'exe'
+  softwareName   = 'GIMP'
+
+  checksum       = '41e98ef75c2992acc9b481147bf3db35652a4c7e1da70bb06a2d2ec9a0853a2d'
+  checksum64     = '41e98ef75c2992acc9b481147bf3db35652a4c7e1da70bb06a2d2ec9a0853a2d'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  url            = 'https://download.gimp.org/mirror/pub/gimp/v2.8/windows/gimp-2.8.18-setup.exe'
+  url64bit       = 'https://download.gimp.org/mirror/pub/gimp/v2.8/windows/gimp-2.8.18-setup.exe'
+
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+$version = '2.8.18'
 $gimpRegistryPath = $(
   'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
 )
@@ -20,6 +32,5 @@ if ($installedVersion -eq $version) {
     "Skipping download and installation."
   )
 } else {
-  Install-ChocolateyPackage $packageName $installerType `
-    $installArgs $url $url64bit -validExitCodes @(0)
+  Install-ChocolateyPackage @packageArgs
 }
---
+++ automatic/gimp/update.ps1
@@ -0,0 +1,31 @@
+﻿import-module au
+
+$releases = 'https://www.gimp.org/downloads/'
+
+function global:au_SearchReplace {
+   @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url(64bit)?\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum(64)?\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType(64)?\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(^[$]version\s*=\s*)('.*')" = "`$1'$($Latest.RemoteVersion)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases
+
+  $re    = '\.exe$'
+  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+  if ($url.StartsWith("//")) {
+    $url = "https:" + $url;
+  }
+
+  $version  = $url -split '[-]|.exe' | select -Last 1 -Skip 2
+
+  return @{ URL32 = $url; Version = $version ; RemoteVersion = $version }
+}
+
+update -ChecksumFor 32
