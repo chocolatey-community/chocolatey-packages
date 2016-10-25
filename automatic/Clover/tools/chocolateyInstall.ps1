dbce8358fcf15c060d3812f7defc3956f0bfe53b
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-25 16:25:07 +0200

    (clover) AU; install refactored; ketarin removed

--- automatic/Clover/tools/chocolateyInstall.ps1
+++ automatic/Clover/tools/chocolateyInstall.ps1
@@ -1,7 +1,15 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'exe'
-$url = '{{DownloadUrl}}'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName            = 'clover'
+  fileType               = 'EXE'
+  url                    = 'http://ejie.me/uploads/setup_clover@3.2.0.exe'
+  checksum               = '78a60d2321d8da2837ee5c3f50893ad4da2686dbcfb5ca6a1cc4f046f2dadcd3'
+  checksumType           = 'sha256'
+  silentArgs             = '/S'
+  validExitCodes         = @(0)
+  softwareName           = ''
+}
+Install-ChocolateyPackage @packageArgs
