18dd6697a208db234c524e88e9906ad3599e0537
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-25 21:33:44 +0200

    (python2) AU; refactoring; removed ketarin #250

--- automatic/python2/tools/chocolateyInstall.ps1
+++ automatic/python2/tools/chocolateyInstall.ps1
@@ -1,82 +1,35 @@
-﻿# This file should be identical for all python* packages
+# This file should be identical for all python* packages
+# https://docs.python.org/3/using/windows.html#installing-without-ui
 
-$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$url64 = '{{DownloadUrlx64}}'
-$version = '{{PackageVersion}}'
-$fileType = 'msi'
-$partialInstallArgs = '/qn /norestart ALLUSERS=1 ADDLOCAL=ALL TARGETDIR='
+$installDir  = '{0}\Python{1}' -f $Env:SystemDrive, ($Env:ChocolateyPackageVersion -replace '\.').Substring(0,2)
+if ($Env:ChocolateyPackageParameters -match '/InstallDir:\s*(.+)') {
+    $installDir = $Matches[1]
+    if ($installDir.StartsWith("'") -or $installDir.StartsWith('"')){  $installDir = $installDir -replace '^.|.$' }
+}
+$installArgs  = '/qn /norestart ALLUSERS=1 ADDLOCAL=ALL TargetDir="{0}"' -f $installDir
 
+$params = @{
+  packageName    = 'python2'
+  fileType       = 'msi'
+  silentArgs     = $installArgs
+  url            = 'https://www.python.org/ftp/python/2.7.12/python-2.7.12.msi'
+  url64Bit       = 'https://www.python.org/ftp/python/2.7.12/python-2.7.12.msi'
+  checksum       = '3ac291535bcf15fd5a15bcd29b066570e8d2a0cab4f3b92a2372f41aa09a4f48'
+  checksum64     = '3ac291535bcf15fd5a15bcd29b066570e8d2a0cab4f3b92a2372f41aa09a4f48'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+}
 
+# If the package is only intended for the 32-bit version, only pass
+# the 32-bit version to the install package function.
+if ($packageName -match 32) {  'url64Bit', 'checksum64' | % $params.Remove($_) }
 
-  $binRoot = Get-BinRoot
-  $installPath = Join-Path $binRoot $packageName
+Install-ChocolateyPackage @params
+Write-Host "Installed to '$installDir'"
 
-  # The name of the installed Python version from Win32_Product
-  $win32ProductName = $('^Python' + ' ' + [regex]::escape($version) + '$')
+if (($Env:PYTHONHOME -ne $null) -and ($Env:PYTHONHOME -ne $InstallDir)) {
+   Write-Warning "Environment variable PYTHONHOME points to different version: $Env:PYTHONHOME"
+}
 
-  # The Get-ProcessorBits is a misnomer. It returns the OS bitness, not the CPU bitness
-  $osBitness = Get-ProcessorBits
-
-  # If the OS is 64-bit and the package is not Python 32-bit specific,
-  # it detects only an installed 64-bit version. This is incompatible with
-  # the usage of the '-x86' parameter of Chocolatey, but I know no other
-  # method to do this. Therefore I also created 32-bit specific Python packages
-  # ({{PackageName}}-x86_32 in this case).
-  if (($osBitness -eq 64) -and ($packageName -notmatch 32)) {
-    $win32ProductName = $('^Python' + ' ' + [regex]::escape($version) + ' ' + '\(64-bit\)$')
-  }
-
-
-  # Check if the same version and bitness of Python is already installed
-  $sameVersionAlreadyInstalled = Get-WmiObject -Class Win32_Product |
-    Where-Object {$_.Name -match $win32ProductName}
-
-  # Construct the old installation path, which is mostly C:\PythonXX,
-  # where XX stands for the major and minor version digits.
-  $versionMajorMinor = $version -replace '^(\d+)\.(\d+).*', '$1$2'
-  $oldInstallPath = Join-Path $env:SystemDrive "Python$versionMajorMinor"
-
-  if (Test-Path $oldInstallPath) {
-    $installPath = $oldInstallPath
-    Write-Output @"
-Warning: Old installation path “$oldInstallPath” detected.
-This package will continue to install $packageName there unless you uninstall
-$packageName from there and remove the “$oldInstallPath” folder. If you decide
-to do that, reinstall this package with the -force parameter and it will
-install to the Chocolatey bin root.
-
-"@
-  }
-
-  $installArgs = $($partialInstallArgs + '"' + $installPath + '"')
-
-  # Check if the same version of Python is already installed.
-  # This prevents 1603 errors during the installation and an
-  # unnecessary download of the Python installer.
-  if ($sameVersionAlreadyInstalled) {
-    Write-Output @"
-$packageName v$version is already installed. Skipping unnecessary download
-and installation. If you have installed $packageName in “$oldInstallPath”
-and you want to use the Chocolatey bin root as installation path instead,
-uninstall $packageName from the control panel, remove the “$oldInstallPath”
-folder and reinstall this package with the -force parameter.
-
-"@
-  } else {
-    # Otherwise install Python and and add the installation folder to the PATH.
-
-    # If the package is only intended for the 32-bit version, only pass
-    # the 32-bit version to the install package function.
-    if ($packageName -match 32) {
-      Install-ChocolateyPackage $packageName $fileType $installArgs $url
-    } else {
-      Install-ChocolateyPackage $packageName $fileType $installArgs $url $url64
-    }
-
-
-    Install-ChocolateyPath $installPath 'Machine'
-    $env:Path = "$($env:Path);$installPath;$installPath\Scripts"
-  }
-
-
+Write-Host "Adding $installDir to PATH if needed"
+Install-ChocolateyPath $installDir 'Machine'
