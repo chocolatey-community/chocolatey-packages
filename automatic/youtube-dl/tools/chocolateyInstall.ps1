04011ccb3158e31696077cf18f5a15cb15c0d609
Chocolatey <chocolatey@realdimensions.net>
2016-10-26 14:31:44 +0000

    AU: 2 updated - ffmpeg youtube-dl [skip ci]

--- automatic/youtube-dl/tools/chocolateyInstall.ps1
+++ automatic/youtube-dl/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'youtube-dl'
-$url = 'https://github.com/rg3/youtube-dl/releases/download/2016.10.21.1/youtube-dl.exe'
-$checksum = '65839646a6d018a3e408c5fd0b73937cc2825b7fe200fcd82de5fce0147a4fc8f632d319628bf01dc792e5fc720bce0950c7b00dae78f67a4cd31fb993e6e8f9'
+$url = 'https://github.com/rg3/youtube-dl/releases/download/2016.10.26/youtube-dl.exe'
+$checksum = 'f0d8bbbdfd723fe414679a250831f7ae6e30c0ae354e2889edfc965fa3a150534bb3af75eae1be719cf7f6dd8722bf9337464e4f48737ccc14b373dac47fb267'
 $checksumType = 'sha512'
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
