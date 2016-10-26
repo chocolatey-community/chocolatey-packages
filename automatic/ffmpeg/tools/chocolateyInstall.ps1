8bb3788e1e20d959d3de18c31359b176c699deac
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-26 16:26:14 +0200

    (ffmpeg) AU; new installer; keterin removed;

--- automatic/ffmpeg/tools/chocolateyInstall.ps1
+++ automatic/ffmpeg/tools/chocolateyInstall.ps1
@@ -1,34 +1,26 @@
-﻿$packageName = '{{PackageName}}'
-$url = '{{DownloadUrl}}'
-$url64 = '{{DownloadUrlx64}}'
+$ErrorActionPreference = 'Stop'
 
-$destinationFolder = Split-Path $script:MyInvocation.MyCommand.Path
+$packageName = 'ffmpeg'
 
-$binRoot = Get-BinRoot
-$oldDestinationFolders = @(
-  (Join-Path $env:SystemDrive 'tools\ffmpeg'),
-  (Join-Path $binRoot 'ffmpeg')
-)
-
-foreach ($oldDestFolder in $oldDestinationFolders) {
-  if (Test-Path $oldDestFolder) {
-
-    Write-Output @"
-Warning: Deprecated installation folder detected: ${oldDestFolder}.
-Please manually remove that folder. This package now installs the ffmpeg binaries to ${scriptDir}\ffmpeg,
-which is a better choice.
-"@
-
-  }
+$packageArgs = @{
+  packageName    = $packageName
+  url            = 'https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-3.1.5-win32-static.zip'
+  url64Bit       = 'https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-3.1.5-win64-static.zip'
+  checksum       = '8f19d37cddcfde6e2a0c5791ac5228252f97be1a52268aaa37d2c5813b08186d'
+  checksum64     = '5c6a1263780509db438296e6e6555dc731212564088094ff3a16f0e69f95a633'
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
 }
+$is32bit = (Get-ProcessorBits 32) -or ($Env:chocolateyForceX86 -eq 'true')
 
-if ($PSVersionTable.PSVersion.Major -eq 2)
-{
-    Remove-Item $(Join-Path "$destinationFolder" "$(Get-ChildItem -Path $destinationFolder)") -Recurse
-}
-else
-{
-  Remove-Item $(Join-Path "$destinationFolder" "$(Get-ChildItem -Directory $destinationFolder)") -Recurse
-}
+$url          = if ($is32bit) { $packageArgs.url } else { $packageArgs.url64Bit }
+$fileName     = Split-Path -Leaf $url
+$download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion"
 
-Install-ChocolateyZipPackage $packageName $url $destinationFolder $url64
+$file_path = "$download_dir\$fileName"
+$options =  @{ Headers=@{ Referer = $url -replace '/[^/]+$'} }
+Get-WebFile -Url $url -FileName $file_path -Options $options
+
+if ($is32bit) { $packageArgs.url = $file_path } else { $packageArgs.url64Bit = $file_path }
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/ffmpeg/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://ffmpeg.zeranoe.com/builds'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri "$releases/win32/static" -UseBasicParsing
+    $version       = $download_page.links | ? href -match '\.(zip|7z)$' | % { $_.href -split '-' | select -Index 1} | ? { $__=$null; [version]::TryParse($_, [ref] $__) } | sort -desc | select -First 1
+    $url32         = "$releases/win32/static/" + ($download_page.links | ? href -like "*$version*" | % href)
+
+    $download_page = Invoke-WebRequest -Uri "$releases/win64/static" -UseBasicParsing
+    $url64         = "$releases/win64/static/" + ($download_page.links | ? href -like "*$version*" | % href)
+
+    @{ URL32 = $url32; URL64=$url64; Version = $version }
+}
+
+update -NoCheckUrl
