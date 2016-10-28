2b803a1392c0485c91c62cdcfc87d10a37eb4f48
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-28 14:27:56 +0200

    (wincdemu) AU; cert updated; install refactoring; uninstall

--- automatic/wincdemu/tools/chocolateyInstall.ps1
+++ automatic/wincdemu/tools/chocolateyInstall.ps1
@@ -1,9 +1,20 @@
-﻿$tools="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\sysprogs.cer'"
+$ErrorActionPreference = 'Stop'
 
-$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/UNATTENDED'
-$url = '{{DownloadUrl}}'
+$tools = Split-Path -parent $MyInvocation.MyCommand.Definition
+Write-Host 'Adding program certificate: sysprogs.cer'
+Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\sysprogs.cer'" | Out-Null
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$packageArgs = @{
+  packageName            = 'wincdemu'
+  fileType               = 'EXE'
+  url                    = 'http://sysprogs.com/files/WinCDEmu/WinCDEmu-4.1.exe'
+  url64bit               = 'http://sysprogs.com/files/WinCDEmu/WinCDEmu-4.1.exe'
+  checksum               = '7716e2e5165402bc3337147ee555bc1b4641fe5fdfdc72329e08753697fe1b90'
+  checksum64             = '7716e2e5165402bc3337147ee555bc1b4641fe5fdfdc72329e08753697fe1b90'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/UNATTENDED'
+  validExitCodes         = @(0)
+  softwareName           = 'wincdemu.*'
+}
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/wincdemu/tools/chocolateyUninstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'wincdemu'
+
+$packageArgs = @{
+    packageName            = $packageName
+    silentArgs             = "/UNATTENDED"
+    fileType               = 'EXE'
+    validExitCodes         = @(0)
+    file                   = ''
+}
+
+[array] $key = Get-UninstallRegistryKey $packageName
+if ($key.Count -eq 1) {
+    $key | % {
+        $packageArgs.file = $_.UninstallString
+        Uninstall-ChocolateyPackage @packageArgs
+        sleep 2
+        ps wcduninst -ea 0 | kill   #Kill the message 'WincdEMU was uninstalled | OK'
+    }
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$key.Count matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | % {Write-Warning "- $_.DisplayName"}
+}
+
