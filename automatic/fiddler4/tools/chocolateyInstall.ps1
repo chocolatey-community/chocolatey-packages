ce435f784128a5be0b4442cf243d5eeca348549b
Miodrag Milic <miodrag.milic@gmail.com>
2016-10-29 16:44:36 +0200

    (fiddler4) AU; removed uninstaller & ketarin;

--- automatic/fiddler4/tools/chocolateyInstall.ps1
+++ automatic/fiddler4/tools/chocolateyInstall.ps1
@@ -1,7 +1,13 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'EXE'
-$32BitUrl  = '{{DownloadUrl}}'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName            = 'fiddler4'
+  fileType               = 'EXE'
+  url                    = 'https://www.telerik.com/docs/default-source/fiddler/fiddlersetup.exe'
+  checksum               = 'd81509447329b7384926a55a16e1c807aebdf787c3d6b6c43bb59c6fde501555'
+  checksumType           = 'sha256'
+  silentArgs             = '/S'
+  validExitCodes         = @(0)
+  softwareName           = 'fiddler*'
+}
+Install-ChocolateyPackage @packageArgs
