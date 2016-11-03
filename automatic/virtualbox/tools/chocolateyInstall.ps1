c9f08adeb0cc2dcda323211894358e69d3af323c
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-03 21:27:14 +0100

    (virtualbox) AU + modernization + nuspec improvements + icon

---
+++ automatic/virtualbox/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$tools\oracle.cer'"
+
+$packageArgs = @{
+  packageName            = 'virtualbox'
+  fileType               = 'EXE'
+  url                    = 'http://download.virtualbox.org/virtualbox/5.1.8/VirtualBox-5.1.8-111374-Win.exe'
+  url64bit               = 'http://download.virtualbox.org/virtualbox/5.1.8/VirtualBox-5.1.8-111374-Win.exe'
+  checksum               = '12860b5bd923878d2ac21c6d2f2c431e65ceb204bdab51bf2872155ccb040af1'
+  checksum64             = '12860b5bd923878d2ac21c6d2f2c431e65ceb204bdab51bf2872155ccb040af1'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '-s -l -msiparams REBOOT=ReallySuppress'
+  validExitCodes         = @(0)
+  softwareName           = 'Oracle VM VirtualBox *'
+}
+Install-ChocolateyPackage @packageArgs
+
+$installLocation = Get-AppInstallLocation $packageArgs.packageName
+Install-ChocolateyPath $installLocation
+if ($installLocation)  {
+    Write-Host "$packageName installed to '$installLocation'"
+    Register-Application "$installLocation\$packageName.exe"
+    Write-Host "$packageName registered as $packageName"
+}
+else { Write-Warning "Can't find $PackageName install location" }
---
+++ automatic/virtualbox/update.ps1
@@ -0,0 +1,25 @@
+import-module au
+
+$releases = 'https://www.virtualbox.org/wiki/Downloads'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url     = $download_page.links | ? href -match '\.exe' | % href
+    $version = $url -split '/' | select -Last 1 -Skip 1
+
+    @{ URL32 = $url; Version = $version }
+}
+
+update -ChecksumFor 32
