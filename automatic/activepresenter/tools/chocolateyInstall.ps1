1f8e9eae96f9f498c520385ccdbf05e2d8696ee2
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-03 15:33:51 +0100

    (activepresenter) AU + installer

---
+++ automatic/activepresenter/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName            = 'activepresenter'
+  fileType               = 'EXE'
+  url                    = 'https://cdn.atomisystems.com/apdownloads/ActivePresenter_v6.0.3_setup.exe'
+  checksum               = '4ffb6e600a255043e33fa59873ceb3ab263a71d481d3515ab5fbdc63a38e15db'
+  checksumType           = 'sha256'
+  silentArgs             = '/VERYSILENT'
+  validExitCodes         = @(0)
+  softwareName           = 'Active Presenter*'
+}
+Install-ChocolateyPackage @packageArgs
