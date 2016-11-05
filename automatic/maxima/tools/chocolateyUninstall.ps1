c4da0e801407dba1aaf155f13f42f2b152bac3a0
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-05 14:00:48 +0100

    (maxima) AU + modernization + installer fixes;

--- automatic/maxima/tools/chocolateyUninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$PSScriptRoot = Split-Path -parent $MyInvocation.MyCommand.Definition
-Import-Module (Join-Path $PSScriptRoot 'functions.ps1')
-
-$uninstallerPath = (Get-InstallProperties).UninstallString
-
-if ($uninstallerPath -and (Test-Path $uninstallerPath)) {
-  Uninstall-ChocolateyPackage '{{PackageName}}' 'exe' '/S' $uninstallerPath
-}
