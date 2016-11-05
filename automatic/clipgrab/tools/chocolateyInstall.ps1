81db692c820d18000fc8db9bc0066d177adca617
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-05 10:22:23 +0100

    (clipgrab) Migrated to AU and modernized

--- automatic/clipgrab/tools/chocolateyInstall.ps1
+++ automatic/clipgrab/tools/chocolateyInstall.ps1
@@ -1,6 +1,15 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/VERYSILENT'
-$url = '{{DownloadUrl}}'
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName            = 'clipgrab'
+  fileType               = 'exe'
+  url                    = 'https://download.clipgrab.org/clipgrab-3.6.1.exe'
+  checksum               = 'bafeffa79e300853c8629b0d2248144c148e8ce1fc09a04e7198538dbfbc3953'
+  checksumType           = 'sha256'
+  silentArgs             = '/VERYSILENT'
+  validExitCodes         = @(0)
+  softwareName           = 'clipgrab*'
+}
+Install-ChocolateyPackage @packageArgs
