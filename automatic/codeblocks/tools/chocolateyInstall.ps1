a23506be5e974c7b002a3d8f10848d59b56017f6
AdmiringWorm <kim.nordmo@gmail.com>
2016-11-06 14:20:24 +0100

    (codeblocks) Migrated to AU

--- automatic/codeblocks/tools/chocolateyInstall.ps1
+++ automatic/codeblocks/tools/chocolateyInstall.ps1
@@ -1,7 +1,18 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/S'
-# {\{DownloadUrlx64}\} gets “misused” here as 32-bit download link due to limitations of Ketarin/chocopkgup
-$url = '{{DownloadUrlx64}}'
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$fosshubUrl = 'https://www.fosshub.com/Code-Blocks.html/codeblocks-16.01mingw-setup.exe'
+
+$packageArgs = @{
+  packageName    = 'codeblocks'
+  fileType       = 'exe'
+  softwareName   = 'CodeBlocks'
+
+  checksum       = '4d1b66c02ea6be91912b3f9cd67a1c3a7933b7e2dd062db00344faf39e3cae1c'
+  checksumType   = 'sha256'
+  url            = Get-UrlFromFosshub $fosshubUrl
+
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/codeblocks/update.ps1
@@ -0,0 +1,40 @@
+import-module au
+import-module "./../../extensions/chocolatey-fosshub.extension/extensions/Get-UrlFromFosshub.psm1"
+
+$releases = 'http://www.codeblocks.org/downloads/26'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]fosshubUrl\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+    }
+    ".\codeblocks.nuspec" = @{
+      "(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+  $re    = 'fosshub.*mingw-setup\.exe$'
+  $url   = $download_page.links | ? href -match $re | select -first 1 -expand href
+
+  if (!$url.StartsWith("https")) {
+    $url = $url -replace "^http","https"
+  }
+
+  $version  = $url -split '[-]|mingw' | select -Last 1 -Skip 2
+
+  $changelog = $download_page.links | ? title -match "^changelog$" | select -first 1 -expand href
+
+  return @{
+    URL32 = $url;
+    Version = $version
+    ReleaseNotes = $changelog
+  }
+}
+
+update -NoCheckUrl -ChecksumFor 32
