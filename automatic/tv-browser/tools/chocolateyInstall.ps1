f2d4bbcf3d7a48b3dd56c71dd942ec05755a18c1
AdmiringWorm <kim.nordmo@gmail.com>
2016-11-06 14:46:06 +0100

    (tv-browser) Migrated to AU

--- automatic/tv-browser/tools/chocolateyInstall.ps1
+++ automatic/tv-browser/tools/chocolateyInstall.ps1
@@ -1,7 +1,16 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/S'
-# {\{DownloadUrlx64}\} gets “misused” here as 32-bit download link due to limitations of Ketarin/chocopkgup
-$url = '{{DownloadUrlx64}}'
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$packageArgs = @{
+  packageName    = 'tv-browser'
+  fileType       = 'exe'
+  softwareName   = 'TV-Browser*'
+
+  checksum       = '3362f3940f92c51114330a9c496609d96ef19a25bf74181e39a4263a4442047c'
+  checksumType   = 'sha256'
+  url            = 'https://sourceforge.net/projects/tvbrowser/files/TV-Browser%20Releases%20%28Java%206%20and%20higher%29/3.4.4/tvbrowser_3.4.4_win32.exe/download'
+
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/tv-browser/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'http://www.tvbrowser.org/index.php?id=tv-browser'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+  $re    = 'tvbrowser.*\.exe'
+  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+  if ($url.StartsWith("http://sourceforge")) {
+    $url = $url -replace '^http','https'
+  }
+
+  $version  = $url -split '[_]' | select -Last 1 -Skip 1
+
+  return @{ URL32 = $url; Version = $version }
+}
+
+update -ChecksumFor 32 -NoCheckChocoVersion
