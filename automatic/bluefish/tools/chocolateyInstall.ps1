bf68092afb4bb363a5d7da4b8059db51d2179436
AdmiringWorm <kim.nordmo@gmail.com>
2016-11-07 06:53:58 +0100

    (bluefish) Migrated to AU + modernization
    * Removed ketarin file
    * Added AU update script
    * Updated install script
    * Updated uninstall script
    * Updated nuspec
      * Added mailingListUrl
      * Added bugTrackerUrl
      * Added docsUrl
      * Updated iconUrl
      * Added copyright
      * Updated packageSourceUrl
      * Added chocolatey-uninstall.extension dependency

--- automatic/bluefish/tools/chocolateyInstall.ps1
+++ automatic/bluefish/tools/chocolateyInstall.ps1
@@ -1,6 +1,16 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/S'
-$url = '{{DownloadUrl}}'
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$packageArgs = @{
+  packageName   = 'bluefish'
+  fileType      = 'exe'
+  softwareName  = 'Bluefish*'
+
+  checksum      = '4f2ab210c68f2e3c382708aae68ad0d881463bb7c2bf42ca2fd250a34e099ffa'
+  checksumType  = 'sha256'
+  url           = 'https://www.bennewitz.com/bluefish/stable/binaries/win32/Bluefish-2.2.9-setup.exe'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
