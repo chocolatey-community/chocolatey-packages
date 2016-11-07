caeb703a6329d9a6183095261b049c21862f7823
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-07 20:08:17 +0100

    (mumble) Migration to AU + modernization [AU mumble]

--- automatic/mumble/tools/chocolateyInstall.ps1
+++ automatic/mumble/tools/chocolateyInstall.ps1
@@ -1,8 +1,22 @@
-﻿$packageName = '{{PackageName}}'
-$installerType = 'msi'
-$url = '{{DownloadUrl}}'
-$silentArgs = '/passive /norestart'
-$validExitCodes = @(0)
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage $packageName $installerType `
-  $silentArgs $url -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName            = 'mumble'
+  fileType               = 'msi'
+  url                    = 'https://github.com/mumble-voip/mumble/releases/download/1.2.17/mumble-1.2.17.msi'
+  checksum               = 'f6a47baf4418ee78658c60e93a605dbdd804739a27cf027233adda6132a24d45'
+  checksumType           = 'sha256'
+  silentArgs             = '/passive /norestart'
+  validExitCodes         = @(0)
+  softwareName           = 'mumble*'
+}
+Install-ChocolateyPackage @packageArgs
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageName
+if ($installLocation)  {
+    Write-Host "$packageName installed to '$installLocation'"
+    Register-Application "$installLocation\$packageName.exe"
+    Write-Host "$packageName registered as $packageName"
+}
+else { Write-Warning "Can't find $PackageName install location" }
