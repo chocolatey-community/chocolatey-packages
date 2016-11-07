d75cc48d5445fccf4f5f4d2dc4c40cc9801b5df7
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-07 19:22:17 +0100

    (mpc-hc) Migration to AU + modernization [AU mpc-hc]

--- automatic/mpc-hc/tools/chocolateyInstall.ps1
+++ automatic/mpc-hc/tools/chocolateyInstall.ps1
@@ -1,7 +1,16 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/VERYSILENT'
-$url = '{{DownloadUrl}}'
-$url64bit = '{{DownloadUrlx64}}'
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
+$packageArgs = @{
+  packageName            = 'mpc-hc'
+  fileType               = 'exe'
+  url                    = 'https://binaries.mpc-hc.org/MPC%20HomeCinema%20-%20Win32/MPC-HC_v1.7.10_x86/MPC-HC.1.7.10.x86.exe'
+  url64bit               = 'https://binaries.mpc-hc.org/MPC%20HomeCinema%20-%20x64/MPC-HC_v1.7.10_x64/MPC-HC.1.7.10.x64.exe'
+  checksum               = 'a00009fb70b3f8ab7675bad4c323d354bdb297bcbca44b690f7e18526c394dd6'
+  checksum64             = 'ac93fcf3bb3f0c571f74fb6917d057a7c030143aac68cd53365355a188bc2edd'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/VERYSILENT'
+  validExitCodes         = @(0)
+  softwareName           = 'mpc-hc*'
+}
+Install-ChocolateyPackage @packageArgs
