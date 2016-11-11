371d65ee52b9dc3c919f4980b788fba0b9de8df4
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-11 12:10:21 +0100

    (ext2fsd) AU + modernization [AU ext2fsd]

--- automatic/ext2fsd/tools/chocolateyInstall.ps1
+++ automatic/ext2fsd/tools/chocolateyInstall.ps1
@@ -1,8 +1,16 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/VERYSILENT /NORESTART'
+$ErrorActionPreference = 'Stop'
 
-# {\{DownloadUrlx64}\} gets “misused” here as 32-bit download link due to limitations of Ketarin/chocopkgup
-$url = '{{DownloadUrlx64}}'
-
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$packageArgs = @{
+  packageName            = 'ext2fsd'
+  fileType               = 'exe'
+  url                    = 'https://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.68/Ext2Fsd-0.68.exe/download'
+  url64bit               = 'https://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.68/Ext2Fsd-0.68.exe/download'
+  checksum               = '1546264153c2cf73d3119d3ce0db2d2302aac45a14596a00ead6e39cc72cc05e'
+  checksum64             = '1546264153c2cf73d3119d3ce0db2d2302aac45a14596a00ead6e39cc72cc05e'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/VERYSILENT /NORESTART'
+  validExitCodes         = @(0)
+  softwareName           = 'ext2fsd*'
+}
+Install-ChocolateyPackage @packageArgs
--- automatic/ext2fsd/tools/chocolateyUninstall.ps1
+++ automatic/ext2fsd/tools/chocolateyUninstall.ps1
@@ -1,12 +1,30 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = "exe"
-$silentArgs = "/VERYSILENT /NORESTART"
-$unpath = "${Env:ProgramFiles}\Ext2Fsd\unins000.exe"
-$unpathx86 = "${Env:ProgramFiles(x86)}\Ext2Fsd\unins000.exe"
-$validExitCodes = @(0,2)
+﻿$ErrorActionPreference = 'Stop'
 
-if (Test-Path $unpath) {$file = "$unpath"}
-if (Test-Path $unpathx86) {$file = "$unpathx86"}
+$packageName = 'ext2fsd'
+$softwareNamePattern = 'ext2fsd*'
 
-Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file  -validExitCodes $validExitCodes
-Write-Output The system must be rebooted for the changes to be completed.
+$packageArgs = @{
+    packageName            = $packageName
+    silentArgs             = "/VERYSILENT /NORESTART"
+    fileType               = 'EXE'
+    validExitCodes         = @(0)
+    file                   = ''
+}
+
+[array] $key = Get-UninstallRegistryKey $softwareNamePattern
+if ($key.Count -eq 1) {
+    $key | % {
+        $packageArgs.file = "$($_.UninstallString.Replace(' /x86=0', ''))"   #"C:\Program Files\OpenSSH\uninstall.exe" /x86=0
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$key.Count matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | % {Write-Warning "- $_.DisplayName"}
+}
+
