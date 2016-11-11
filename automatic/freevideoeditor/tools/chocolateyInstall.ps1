b3306cee98a19dc3f95984f076283ac6854ffc34
Chocolatey <chocolatey@realdimensions.net>
2016-11-11 01:00:46 +0000

    AU: 1 updated - freevideoeditor [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/ad186170d0ad1f2159e78fd8e563d83a0897e7cc

--- automatic/freevideoeditor/tools/chocolateyInstall.ps1
+++ automatic/freevideoeditor/tools/chocolateyInstall.ps1
@@ -8,6 +8,6 @@ $packageArgs = @{
   checksumType           = 'sha256'
   silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
   validExitCodes         = @(0)
-  softwareName           = 'VSDC Free Video Editor*'
+  softwareName           = 'freevideoeditor*'
 }
 Install-ChocolateyPackage @packageArgs
