faae0bca8fd7e5b5516468f355718c2c0893108c
Mike Cole <cole.mike@gmail.com>
2016-11-10 22:45:03 -0600

    Adjusted PDFCreator install script to not remove settings on upgrade.

--- automatic/PDFCreator/tools/chocolateyInstall.ps1
+++ automatic/PDFCreator/tools/chocolateyInstall.ps1
@@ -1,8 +1,9 @@
-﻿$packageName = 'pdfcreator'
-$installerType = 'exe'
-$baseInstallArgs = '/VERYSILENT /NORESTART'
-$fullInstallArgs = $('' +
-  $baseInstallArgs + ' ' +
+﻿$checksum = 'C6A05C5A0A3035881EE7E1C7C14F9A15850A2C955A69E2F353DA0DCABF4438FF'
+$url = 'http://orange.download.pdfforge.org/pdfcreator/2.4.0/PDFCreator-2_4_0-Setup.exe?file=AMIfv97pmlXptiTqMp-BHVoAI8s-Cm7VHxjpiPsVcuGxVnMs4_45c2BsnRUFjdrYS688IX5WWKCgalXMZwLUYaaB3JL2yQFOc
+C8Oqa1SzC3jXtG7uTkAbe9kIbxmkUDONXfgrLhlZPCeLOXyZxkZyI5uxoVDvvEZrNQpQ-jaolP7S4aOBoKBeHI&download'
+ 
+$installArgs = $('' +
+  '/VERYSILENT /NORESTART ' +
   '/COMPONENTS="program,ghostscript,comsamples,' +
   'languages,languages\bosnian,languages\catalan,languages\catalan_valencia,' +
   'languages\chinese_simplified,languages\chinese_traditional,' +
@@ -17,25 +18,16 @@ $fullInstallArgs = $('' +
   'languages\swedish,languages\turkish,languages\valencian_avl"'
 )
 
-$url = 'http://orange.download.pdfforge.org/pdfcreator/2.4.0/PDFCreator-2_4_0-Setup.exe?file=AMIfv97pmlXptiTqMp-BHVoAI8s-Cm7VHxjpiPsVcuGxVnMs4_45c2BsnRUFjdrYS688IX5WWKCgalXMZwLUYaaB3JL2yQFOc
-C8Oqa1SzC3jXtG7uTkAbe9kIbxmkUDONXfgrLhlZPCeLOXyZxkZyI5uxoVDvvEZrNQpQ-jaolP7S4aOBoKBeHI&download'
-
-$uninstallerPathLastPart = 'PDFCreator\unins000.exe'
-$uninstallerPath = Join-Path $env:ProgramFiles $uninstallerPathLastPart
-
-if (${env:ProgramFiles(x86)}) {
-  $uninstallerPathx86 = Join-Path ${env:ProgramFiles(x86)} $uninstallerPathLastPart
-  if (Test-Path $uninstallerPathx86) {
-    $uninstallerPath = $uninstallerPathx86
-  }
+$packageArgs = @{
+  packageName   = 'pdfcreator'
+  unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType      = 'exe'
+  url           = $url
+  silentArgs    = $installArgs
+  validExitCodes= @(0)
+  softwareName  = 'pdfcreator*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
 }
 
-# Uninstall PDFCreator if older version is installed, otherwise the installation
-# of the new version will fail.
-if (Test-Path $uninstallerPath) {
-  Uninstall-ChocolateyPackage $packageName $installerType $baseInstallArgs $uninstallerPath
-}
-
-$checksum = 'C6A05C5A0A3035881EE7E1C7C14F9A15850A2C955A69E2F353DA0DCABF4438FF'
-
-Install-ChocolateyPackage $packageName $installerType $fullInstallArgs $url -checksum $checksum -checksumType 'SHA256'
+Install-ChocolateyPackage @packageArgs
