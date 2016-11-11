5249a420e6210acc8a8d91855651647c065e0583
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-11 01:36:00 +0100

    (freeevideoeditor) Changed updater to UseBasicParsing due to Vector Smash #382

--- automatic/freevideoeditor/update.ps1
+++ automatic/freevideoeditor/update.ps1
@@ -15,9 +15,10 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $version = $download_page.Content -split '\n' | sls 'Current version:' -Context 0,5 | out-string
     @{
-        Version = ($download_page.AllElements | ? tagname -eq 'strong').innerText | ? { [version]::TryParse($_, [ref]($__))}
+        Version = $version -split '<|>' | ? { [version]::TryParse($_, [ref]($__)) } | select -First 1
         URL32   = 'http://downloads.videosoftdev.com/video_tools/video_editor.exe'
     }
 }
