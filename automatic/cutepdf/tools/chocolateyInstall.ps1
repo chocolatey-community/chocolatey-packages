cb199a11518eb26f97f3c0f9c491e4dee61ef303
Mike Cole <cole.mike@gmail.com>
2016-11-11 20:57:39 -0600

    Updated cutepdf au.

--- automatic/cutepdf/tools/chocolateyInstall.ps1
+++ automatic/cutepdf/tools/chocolateyInstall.ps1
@@ -1,10 +1,11 @@
 $checksum = '2D9E07F25358C9D2317BC639AFCDEDDB893D1FCFD43BB66FF372DBA11E169EE1'
+$url = 'http://www.cutepdf.com/download/CuteWriter.exe'
 
 $packageArgs = @{
   packageName   = 'cutepdf'
   unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType      = 'exe'
-  url           = 'http://www.cutepdf.com/download/CuteWriter.exe'
+  url           = $url
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup Package
   validExitCodes= @(0)
   softwareName  = 'cutepdf*'
