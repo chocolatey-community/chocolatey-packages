254132b8d37c276a9502f70a122541da3b9cb420
Gary Ewan Park <gep13@gep13.co.uk>
2016-11-12 22:06:40 +0000

    (feeddemon) Updated with checksums

--- manual/feeddemon/tools/chocolateyInstall.ps1
+++ manual/feeddemon/tools/chocolateyInstall.ps1
@@ -1,8 +1,15 @@
-$packageName = 'feeddemon'
-$installerType = 'EXE'
-$32BitUrl  = 'http://bradsoft.com/download/FeedDemonInstall45.exe'
-$64BitUrl  = $32BitUrl
-$silentArgs = '/verysilent'
-$validExitCodes = @(0)
+$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName    ='feeddemon'
+  fileType       = 'exe'
+
+  checksum       = 'A60A4E8DFD0F194E235F232FB525F84A6F6984E910E3C44B2DE772F3035601DD'
+  checksumType   = 'sha256'
+  url            = 'http://bradsoft.com/download/FeedDemonInstall45.exe'
+
+  silentArgs     = '/verysilent'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
