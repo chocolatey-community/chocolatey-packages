d0529f4db2eb80affc7743eb55f3966895cf2a5a
AdmiringWorm <kim.nordmo@gmail.com>
2016-11-12 20:12:36 +0100

    (aptana-studio) Migrated to AU
    * Modernized install script + fixed silent args
    * Modernized uninstall script + Fixed logic
    * Added AU update script
    * Updated metadata
      * Added project source url
      * Updated description
      * Added missing javaruntime dependency
      * Added missing git dependency
      * Updated release notes url
      * Added bug tracker url
      * Updated icon url
      * Added new tags
    
    fixes #242
    fixes #179

---
+++ automatic/aptana-studio/update.ps1
@@ -0,0 +1,35 @@
+﻿import-module au
+
+$domain   = 'https://github.com'
+$releases = "$domain/aptana/studio3/releases/latest"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+  $re    = '\.exe$'
+  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+  $version  = $url -split '/' | select -Last 1 -Skip 1
+  $version = $version.TrimStart('v');
+
+  if ($url.StartsWith('/')) {
+    $url = $domain + $url
+  }
+
+  @{
+    URL32 = $url
+    Version = $version
+  }
+}
+
+update -ChecksumFor 32
