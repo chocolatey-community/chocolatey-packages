145052c8838152417e195241f333d98a6d2bdf2f
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-13 23:02:41 +0100

    (winpcap) AU + guidelines + fixes

--- automatic/WinPcap/tools/chocolateyInstall.ps1
+++ automatic/WinPcap/tools/chocolateyInstall.ps1
@@ -1,20 +1,15 @@
-﻿try {
-  $packageName = '{{PackageName}}'
-  $url = '{{DownloadUrl}}'
-  $filePath = "$env:TEMP\chocolatey\winpcap"
-  $fileFullPath = "$filePath\winpcapInstall.exe"
+$ErrorActionPreference = 'Stop'
 
-  if (!(Test-Path $filePath)) {
-    New-Item -ItemType directory $filePath -Force
-  }
-
-  Get-ChocolateyWebFile $packageName $fileFullPath $url
-
-  $ahkScript = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\winpcapInstall.ahk"
-
-  Start-ChocolateyProcessAsAdmin $ahkScript 'AutoHotkey'
-
-}   catch {
-  Write-Output $packageName $($_.Exception.Message)
-  throw
+$packageArgs = @{
+  packageName    = 'WinPcap'
+  fileFullPath   = "$(Get-PackageCacheLocation)\WinPcapInstall.exe"
+  url            = 'https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe'
+  checksum       = 'fc4623b113a1f603c0d9ad5f83130bd6de1c62b973be9892305132389c8588de'
+  checksumType   = 'sha256'
 }
+Get-ChocolateyWebFile @packageArgs
+
+Write-Output "Running Autohotkey installer"
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$ahkScript = "$toolsPath\winpcapInstall.ahk"
+AutoHotkey $ahkScript $packageArgs.fileFullPath
