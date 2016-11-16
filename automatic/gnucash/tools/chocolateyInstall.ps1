e493d1d9cd59d8e57e15a178b786e31cf3aa1418
Chocolatey <chocolatey@realdimensions.net>
2016-11-16 22:16:31 +0000

    AU: 3 updated - gnucash smartftp WinPcap [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/c3b0d7ef47e6471b94c103bb289385eb80e82c52

--- automatic/gnucash/tools/chocolateyInstall.ps1
+++ automatic/gnucash/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = 'gnucash'
   fileType      = 'exe'
-  url           = ''
+  url           = 'https://github.com/Gnucash/gnucash/releases/download/2.6.14a/gnucash-2.6.14-setup.exe'
 
   softwareName  = 'GnuCash*'
 
-  checksum      = ''
+  checksum      = 'c0b71a6839df4fa17bf57d9a3c6fb104c8c456f80f1871cb3c842d6623ee868a'
   checksumType  = 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /SP-"
