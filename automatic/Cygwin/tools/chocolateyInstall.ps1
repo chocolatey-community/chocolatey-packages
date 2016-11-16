2f66dea8bcc30d4ffd9f2ee5959a2e56505bca14
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-16 08:30:54 +0100

    (cygwin) Added package parameters; Copied setup to install dir (fix #73, fix #85, fix #150, fix #101, fix #176, close 199, fix #200)

--- automatic/Cygwin/tools/chocolateyInstall.ps1
+++ automatic/Cygwin/tools/chocolateyInstall.ps1
@@ -1,29 +1,35 @@
-# Set the default install path to Bin-Root
-$binRoot = Get-BinRoot
-$cygRoot = Join-Path $binRoot 'cygwin'
-$alreadyInstalled = Test-Path 'HKLM:\SOFTWARE\Cygwin\setup'
-
 # https://cygwin.com/faq/faq.html#faq.setup.cli
-$silentArgs = @(
-  "--quiet-mode"
-  "--site http://mirrors.kernel.org/sourceware/cygwin/"
-  "--no-desktop"
-  "--packages default"
-)
 
-# Arguments only for the first installation, otherwise these values are already set
-if (!$alreadyInstalled) {
-  $silentArgs += @(
-    "--root $cygRoot"
-    "--local-package-dir $cygRoot"
-    )
+$ErrorActionPreference = 'Stop'
+
+$pp = Get-PackageParameters
+
+$cygwin_root = (Get-ItemProperty 'HKLM:\SOFTWARE\Cygwin\setup' -ea 0).rootdir
+if (!$cygwin_root) {
+    $cygwin_root = if ($pp.InstallDir) { $pp.InstallDir } else { (Get-BinRoot) + '\cygwin' }
+} else { Write-Host 'Existing installation detected, ignoring InstallDir argument' }
+
+if (!$pp.Proxy) {
+    $pp.Proxy = $Env:ChocolateyProxyLocation
+    if (!$pp.Proxy) {
+        $wc = New-Object System.Net.WebClient; $url = 'https://cygwin.com'
+        $pp.Proxy = if (!$wc.Proxy.IsBypassed($url)) { $wc.Proxy.GetProxy($url).Authority }
+    }
 }
-else {
-    # Configure --local-package-dir for subsequent or pre-existing installs
-    # (otherwise they get dropped in $ENV:ChocolateyInstall)
-    $cygRoot = (Get-ItemProperty 'HKLM:\SOFTWARE\Cygwin\setup').rootdir
-    $silentArgs += "--local-package-dir $cygRoot"
-}
+
+$silentArgs = @(
+    '--quiet-mode'
+    '--site http://mirrors.kernel.org/sourceware/cygwin/'
+    '--packages default'
+    "--root $cygwin_root"
+    "--local-package-dir $cygwin_root"
+
+    if (!$pp.DesktopIcon) { '--no-desktop' } else {  Write-Host 'Desktop icon will be created' }
+    if ($pp.NoStartMenu)  { '--no-startmenu';        Write-Host 'No start menu items will be created' }
+    if ($pp.Proxy)        { "--proxy $($pp.Proxy)";  Write-Host "Using proxy: $($pp.Proxy)" }
+    if ($pp.Pubkey)       { "--pubkey $($pp.Pubkey)";Write-Host "URL of extra public key file is provided" }
+    if ($pp.Site)         { "--site $($pp.Site)";    Write-Host "Download site: $($pp.Site)" }
+)
 
 $packageArgs = @{
   packageName            = 'Cygwin'
@@ -36,7 +42,12 @@ $packageArgs = @{
   checksumType64         = 'sha256'
   silentArgs             = $silentArgs
   validExitCodes         = @(0)
-  softwareName           = 'Cygwin*'
 }
 Install-ChocolateyPackage @packageArgs
-Install-BinFile -Name "Cygwin" -Path "$cygRoot\Cygwin.bat"
+Install-BinFile -Name "Cygwin" -Path "$cygwin_root\Cygwin.bat"
+
+Write-Host "Copying cygwin package manager (setup) to $cygwin_root"
+$setup_path = if (Get-ProcessorBits 64) { $packageArgs.url64bit } else { $packageArgs.url }
+$setup_path = "{0}\{1}" -f (Get-PackageCacheLocation), ($setup_path -split '/' | select -Last 1)
+if (!$setup_path)  { Write-Warning "Can't find setup path"; return }
+cp $setup_path $cygwin_root\cygwinsetup.exe
