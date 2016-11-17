d5297580d77e3cc4746a28237f7f8d6f94f96c7a
AdmiringWorm <kim.nordmo@gmail.com>
2016-11-17 18:55:02 +0100

    (juju) Bumped version

--- automatic/juju/tools/chocolateyinstall.ps1
+++ automatic/juju/tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType      = 'exe'
   softwareName  = 'Juju'
 
-  checksum      = '22224346ce7e81cb406502c7307925a4fc6a0d8977305b134cd4090efd88a2a2'
+  checksum      = 'a3756cacb6ed3f4176c9ddd81e01f6381deedc5f26cd74a31e76055e4327b497'
   checksumType  = 'sha256'
-  url           = 'https://launchpad.net/juju/2.0/2.0.2/+download/juju-setup-2.0.2-signed.exe'
+  url           = 'https://launchpad.net/juju/2.1/2.1-beta1/+download/juju-setup-2.1-beta1.exe'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
