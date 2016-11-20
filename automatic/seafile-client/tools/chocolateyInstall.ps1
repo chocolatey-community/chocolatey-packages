504243ec6a0b8f7eab0ae241d73910947ac6355a
AdmiringWorm <kim.nordmo@gmail.com>
2016-11-20 15:41:07 +0100

    (seafile-client) Updated install script

--- automatic/seafile-client/tools/chocolateyInstall.ps1
+++ automatic/seafile-client/tools/chocolateyInstall.ps1
@@ -1,15 +1,24 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'msi'
-$silentArgs = '/passive'
-$url = '{{DownloadUrl}}'
-$version = '{{PackageVersion}}'
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName = 'seafile-client'
+  fileType    = 'msi'
+  softwareName = 'Seafile '
+
+  checksum     = '64f0e7192acbbbad4e6ac34d5b531ff27697172b9109f2c00cf61ebae923a642'
+  checksumType = 'sha256'
+  url          = 'https://bintray.com/artifact/download/seafile-org/seafile/seafile-6.0.0-en.msi'
+
+  silentArgs   = '/passive'
+  validExitCodes = @(0)
+}
 
 $app = Get-WmiObject -Class Win32_Product | Where-Object {
-  $_.Name -match "^Seafile ${version}$"
+  $_.Name -match "^$($packageArgs.softwareName)$"
 }
 
 if (!$app) {
-  Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+  Install-ChocolateyPackage @packageArgs
 } else {
   Write-Output $(
     "$packageName $version is already installed. " +
