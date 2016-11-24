f0b935021052bdac0712cc8b9c5de8ea121cc486
Chocolatey <chocolatey@realdimensions.net>
2016-11-24 10:29:49 +0000

    AU: 1 updated - flightgear [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/9972b06ecc70b1c169f8a0330179355500ee2e82

--- automatic/flightgear/tools/chocolateyInstall.ps1
+++ automatic/flightgear/tools/chocolateyInstall.ps1
@@ -1,17 +1,17 @@
 $ErrorActionPreference = 'Stop'
 
-$version = '2016.3.1'
+$version = '2016.4.1'
 $softwareName = "FlightGear v$version"
 
 $packageArgs = @{
   packageName = 'flightgear'
   fileType    = 'exe'
-  url         = 'http://ftp.igh.cnrs.fr/pub/flightgear/ftp/Windows/FlightGear-2016.3.1.exe'
+  url         = 'http://ftp.igh.cnrs.fr/pub/flightgear/ftp/Windows/FlightGear-2016.4.1.exe'
 
   softwareName = $softwareName
 
-  checksum     = '3cbc1ab7f0cbbb9b3c98901b22b7ca7cca882d3904ba8b31fbb94fdf21b4e8b3'
-  checksumType = 'sha256'
+  checksum     = '8B1851999233047E3DA451AFB2739C5024E914E92011B3F5F37E42CDB5ED1F64'
+  checksumType = ''
 
   silentArgs   = '/VERYSILENT'
   validExitCodes = @(0)
