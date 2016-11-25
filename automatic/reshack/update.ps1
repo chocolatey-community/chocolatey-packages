52329964fa2082ac9782454eb4d64b96ed0cc2e5
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-25 22:58:37 +0100

    (reshack) fixed updater for new version

--- automatic/reshack/update.ps1
+++ automatic/reshack/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
     $download_page.AllElements | ? tagName -eq 'strong' | ? InnerText -match '^Version' | % InnerText | set version
     $version = $version -split ' ' | select -Last 1
-    $url =  $download_page.Links | ? href -match 'exe' | ? InnerText -match $version | % href
+    $url =  $download_page.Links | ? href -match 'exe' | % href | Select -First 1
     $url = "$releases/$url"
 
     @{ URL32 = $url; Version = $version }
