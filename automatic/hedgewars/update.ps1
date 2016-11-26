f8150fa4aa3d3e46cebdda021af8c162c59e1659
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-26 20:24:54 +0100

    (hedgewars) removed x64 url [skip ci]

--- automatic/hedgewars/update.ps1
+++ automatic/hedgewars/update.ps1
@@ -6,9 +6,7 @@ function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
             "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
             "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
@@ -25,7 +23,6 @@ function global:au_GetLatest {
     @{
         Version = $version
         URL32   = "http://download.gna.org/hedgewars/Hedgewars-${version}.exe"
-        URL64   = "http://download.gna.org/hedgewars/Hedgewars-${version}.exe"
     }
 }
 
