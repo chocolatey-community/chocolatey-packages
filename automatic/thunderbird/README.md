7ca3db5fd177fb9b7be4eeb6102b28d5dff483bf
AdmiringWorm <kim.nordmo@gmail.com>
2016-11-28 15:24:29 +0100

    (thunderbird) Migrated to AU

---
+++ automatic/thunderbird/README.md
@@ -0,0 +1,2 @@
+If updating any of the scripts in the tools directory or to `update.ps1`,
+remember to make the same changes in the firefox package.
--- automatic/thunderbird/tools/chocolateyInstall.ps1
+++ automatic/thunderbird/tools/chocolateyInstall.ps1
@@ -1,113 +1,38 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$version = '{{PackageVersion}}'
+$ErrorActionPreference = 'Stop'
+# This is the general install script for Mozilla products (Firefox and Thunderbird).
+# This file must be identical for all Choco packages for Mozilla products in this repository.
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\helpers.ps1
 
-$softwareNameLowerCase = $packageName.ToLower()
+$packageName = 'thunderbird'
+$softwareName = 'Mozilla Thunderbird'
 
-$softwareNameTitleCase = $packageName.Substring(0, 1).ToUpper() +
-  $packageName.Substring(1).ToLower()
+$allLocalesListURL = 'https://www.mozilla.org/en-US/thunderbird/all/'
 
-$allLocalesListURL = Switch ($softwareNameLowerCase) {
-  'firefox' {
-    'https://www.mozilla.org/en-US/firefox/all/'
-    break
-  }
-
-  'thunderbird' {
-    'https://www.mozilla.org/en-US/thunderbird/all/'
-    break
-  }
-}
-
-
-
-# ---------------- Function definitions ------------------
-
-function GetUninstallPath () {
-  $regUninstallDir = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
-  $regUninstallDirWow64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
-
-  $uninstallPaths = $(Get-ChildItem $regUninstallDir).Name
-
-  if (Test-Path $regUninstallDirWow64) {
-    $uninstallPaths += $(Get-ChildItem $regUninstallDirWow64).Name
-  }
-
-  $uninstallPath = $uninstallPaths -match "Mozilla $softwareNameTitleCase [\d\.]+ \([^\s]+ [a-zA-Z\-]+\)" | Select -First 1
-  return $uninstallPath
-}
-
-function GetLocale() {
-
-  $availableLocales = Get-WebContent $allLocalesListURL
-
-  # --- Get locale from installArgs if specified
-
-  $packageParameters = $env:chocolateyPackageParameters
-
-  $packageParameters = if ($packageParameters -ne $null) { $packageParameters } else { "" }
-  $argumentMap = ConvertFrom-StringData $packageParameters
-  $localeFromPackageParameters = $argumentMap.Item('l')
-
-  # ---
-
-  # --- Get already installed locale if available
-
-  $uninstallPath = GetUninstallPath($null)
-
-  $alreadyInstalledLocale = $uninstallPath -replace ".+\s([a-zA-Z\-]+)\)", '$1'
-
-
-  # ---
-
-  # --- Other locales
-
-  $systemLocaleAndCountry = (Get-Culture).Name
-  $systemLocaleTwoLetter = (Get-Culture).TwoLetterISOLanguageName
-  $fallbackLocale = 'en-US'
-
-  # ---
-
-  $locales = $localeFromPackageParameters, $alreadyInstalledLocale, `
-    $systemLocaleAndCountry, $systemLocaleTwoLetter, $fallbackLocale
-
-  foreach ($locale in $locales) {
-    $localeMatch = $availableLocales -match "os=win&amp;lang=$locale`"" | Select -First 1
-    if ($localeMatch -and $locale -ne $null) {
-      break
-    }
-  }
-
-  return $locale
-}
-
-
-function AlreadyInstalled($version) {
-  $uninstallEntry = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Mozilla $softwareNameTitleCase ${version}*"
-  $uninstallEntryWow64 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Mozilla $softwareNameTitleCase ${version}*"
-
-  if ((Test-Path $uninstallEntry) -or (Test-Path $uninstallEntryWow64)) {
-    return $true
-  } else {
-    return $false
-  }
-}
-
-# ----------------------------------
-
-$alreadyInstalled = AlreadyInstalled($version)
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '45.5.0')
 
 if ($alreadyInstalled) {
   Write-Output $(
-    "$softwareNameTitleCase $version is already installed. "
+    "Thunderbird is already installed. " +
     'No need to download an re-install again.'
   )
 } else {
 
-  $locale = GetLocale
+  $locale = GetLocale -localeUrl $allLocalesListURL -product $softwareName
+  $checksums = GetChecksums -language $locale -checksumFile "$toolsPath\LanguageChecksums"
 
-  $url = "https://download.mozilla.org/?product=${softwareNameLowerCase}-${version}&os=win&lang=${locale}"
-  $silentArgs = '-ms'
+  $packageArgs = @{
+    packageName = $packageName
+    fileType = 'exe'
+    softwareName = "$softwareName*"
 
-  Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+    Checksum = $checksums.Win32
+    ChecksumType = 'sha512'
+    Url = "https://download.mozilla.org/?product=thunderbird-45.5.0-SSL&amp;os=win&amp;lang=${locale}"
+
+    silentArgs = '-ms'
+    validExitCodes = @(0)
+  }
+
+  Install-ChocolateyPackage @packageArgs
 }
