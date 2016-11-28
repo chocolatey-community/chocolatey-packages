fd01fd63e37322b0c6a20e9f212bd72e1c28318c
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-28 11:36:35 +0100

    (virtualbox) Added all documented unattended options; Registration changed to vbox; Graceful shutdown #423

---
+++ automatic/virtualbox/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,20 @@
+$packageName = 'virtualbox'
+
+$installLocation = Get-AppInstallLocation $packageName
+if (!($installLocation -and (Test-Path $installLocation)) { return }
+
+Set-Alias vboxmanage $installLocation\VBoxManage.exe
+if (!(Test-Path vboxmanage)) {
+    Write-Warning "Existing installation of $pakcageName found but unable to find VBoxManage.exe"
+    return
+}
+
+$runningvms = vboxmanage list runningvms
+if ($LastExitCode -ne 0) { Write-Warning "Error running vboxmanage - can't get running vms" }
+foreach ($vm in $runningvms) {
+     $vmid = $vm -split ' ' | select -Last 1
+     $vmname = $vm.Replace($vmdid, '').Trim()
+     vboxmanage controlvm $vmid acpipowerbutton
+     if ($LastExitCode -ne 0) { Write-Warning "Error running vboxmanage - can't power down running vm: $vmname" }
+     Write-Host "Machine $vmname shut down prior to installation"
+}
--- automatic/virtualbox/tools/chocolateyInstall.ps1
+++ automatic/virtualbox/tools/chocolateyInstall.ps1
@@ -1,8 +1,19 @@
+#https://www.virtualbox.org/manual/ch02.html#idm819
+#ALLUSERS=2
+
 $cert = ls cert: -Recurse | ? { $_.Thumbprint -eq 'a88fd9bdaa06bc0f3c491ba51e231be35f8d1ad5' }
 if (!$cert) {
     $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
     Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$toolsPath\oracle.cer'"
 }
+
+$pp = Get-PackageParameters
+$silentArgs = @('-s -l -msiparams REBOOT=ReallySuppress')
+$silentArgs += if (!$pp.CurrentUser) { 'ALLUSERS=1' } else { 'ALLUSERS=2' Write-Host 'Param: Installing for current user' }
+$silentArgs += if ($pp.NoDesktopShortcut) { 'VBOX_INSTALLDESKTOPSHORTCUT=0';     Write-Host 'Param: No desktop shortcut' }
+$silentArgs += if ($pp.NoQuickLaunch)     { 'VBOX_INSTALLQUICKLAUNCHSHORTCUT=0'; Write-Host 'Param: No quick launch shortcut' }
+$silentArgs += if ($pp.NoRegister)        { 'VBOX_REGISTERFILEEXTENSIONS=0';     Write-Host 'Param: No registration for virtualbox file extensions' }
+
 
 $packageArgs = @{
   packageName            = 'virtualbox'
@@ -13,7 +24,7 @@ $packageArgs = @{
   checksum64             = '24193297a1d6ed14450c9569955768421ec433e5f93a02a5aa9a8c9214a080ed'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
-  silentArgs             = '-s -l -msiparams REBOOT=ReallySuppress'
+  silentArgs             = $silentArgs
   validExitCodes         = @(0)
   softwareName           = 'Oracle VM VirtualBox *'
 }
@@ -24,7 +35,7 @@ $installLocation = Get-AppInstallLocation $packageName
 Install-ChocolateyPath $installLocation
 if ($installLocation)  {
     Write-Host "$packageName installed to '$installLocation'"
-    Register-Application "$installLocation\$packageName.exe"
-    Write-Host "$packageName registered as $packageName"
+    Register-Application "$installLocation\$packageName.exe" vbox
+    Write-Host "$packageName registered as vbox"
 }
 else { Write-Warning "Can't find $packageName install location" }
