8d3e8e0d63450ff8f63193ae14d767d85154e3dc
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-30 10:12:16 +0100

    (virtualbox) non elevated code in BeforeModify

---
+++ automatic/virtualbox/tools/Start-ProcessNonElevated.ps1
@@ -0,0 +1,41 @@
+function Start-ProcessNonElevated( [string] $Cmd, [switch]$UsePowerShell ) {
+    $svc = gsv Schedule -ea 0
+    if ($svc -and $svc.Status -ne 'Running') { throw 'Start-ProcessNonElevated requires running Task Scheduler service' }
+
+    $res = @{}
+
+    $tmp_base  = [System.IO.Path]::GetTempFileName()
+    $tmp_base  = $tmp_base -replace '\.tmp$'
+    $tmp_name  = Split-Path $tmp_base -Leaf
+    $task_name = "Start-ProcessNonElevated-$tmp_name"
+    Write-Verbose "Temporary files: $tmp_base"
+
+    if ($UsePowershell) {
+        @(
+            '$r = "{0}"' -f $tmp_base
+            ". {{`n{0}`n}} >`"`$r.out.log`" 2>`"`$r.err.log`"" -f $Cmd
+        ) -join "`n" | Out-String | Out-File "$tmp_base.ps1"
+        $cmd = "powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -NoLogo -NonInteractive -File '$tmp_base.ps1'"
+    }
+
+    Write-Verbose "Creating scheduled task for command:`n$cmd"
+    schtasks.exe /Create /RU $Env:USERNAME /TN $task_name /SC ONCE /ST 00:00 /F /TR $cmd *> "$tmp_base.schtasks.log"
+    schtasks.exe /run /tn $task_name *>> "$tmp_base.schtasks.log"
+
+    Write-Verbose 'Waiting for scheduled task to finish'
+    do {
+        $status = schtasks /query /tn $task_name /FO csv | ConvertFrom-Csv | select -expand Status
+        sleep 1
+    }
+    until ($status -eq 'Ready')
+    schtasks.exe /delete /F /tn $task_name *>> "$tmp_base.schtasks.log"
+
+    if ($UsePowershell) {
+        $res = @{
+            out = cat "$tmp_base.out.log" -ea 0
+            err = cat "$tmp_base.err.log" -ea 0
+        }
+    }
+
+    return $res
+}
--- automatic/virtualbox/tools/chocolateyBeforeModify.ps1
+++ automatic/virtualbox/tools/chocolateyBeforeModify.ps1
@@ -1,35 +1,56 @@
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\Start-ProcessNonElevated.ps1
+
 $packageName = 'virtualbox'
+
+#For acpipowerbutton to work on Windows server see: http://ethertubes.com/unattended-acpi-shutdown-of-windows-server/
+#sp HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system shutdownwithoutlogon 1
+$shutdown_type = 'poweroff'  # 'acpipowerbutton', 'poweroff', 'savestate'
+
 $pvbox = ps virtualbox -ea 0
 if (!$pvbox) {
     Write-Host "$packageName is not running"
     return
 }
 
+Write-Host "$packageName is running"
+
 $installLocation = Get-AppInstallLocation $packageName
 if (!$installLocation) { $installLocation = gcm VBoxManage.exe -ea 0 | select -expand Path | Split-Path }
 
+
 if (!($installLocation -and (Test-Path $installLocation))) {
-    Write-Warning "Can not find existing installation of $packageName"
-} else {
-
-    Set-Alias vboxmanage $installLocation\VBoxManage.exe
-    if (!(Test-Path $installLocation\VBoxManage.exe)) {
-        Write-Warning "Existing installation of $packageName found but unable to find VBoxManage.exe"
-    } else {
-
-        $runningvms = vboxmanage list runningvms
-        if ($LastExitCode -ne 0) { Write-Warning "Error running vboxmanage - can't get running vms" }
-        else {
-            foreach ($vm in $runningvms) {
-                $vmid = $vm -split ' ' | select -Last 1
-                $vmname = $vm.Replace($vmdid, '').Trim()
-                vboxmanage controlvm $vmid acpipowerbutton
-                if ($LastExitCode -ne 0) { Write-Warning "Error running vboxmanage - can't power down running vm: $vmname" }
-                else { Write-Host "Machine $vmname powered down prior to installation" }
-            }
-        }
-    }
+    Write-Warning "Can not find existing installation location of $packageName"
+    return
 }
 
-Write-Host 'Virtualbox is running, killing it prior to update'
-$pvbox | kill
+if (!(Test-Path $installLocation\VBoxManage.exe)) {
+    Write-Warning "Existing installation of $packageName found but unable to find VBoxManage.exe"
+    return
+}
+
+$commands = "Set-Alias vboxmanage '$installLocation\VBoxManage.exe'`n"
+$commands += @'
+[string[]] $runningvms = vboxmanage list runningvms
+if ($LastExitCode -ne 0) { Write-Error "Error running vboxmanage - can't get running vms" }
+if ($runningvms -and ($runningvms.Length -eq 0)) { Write-Host "No running machines" }
+
+Write-Host 'Number of machines running:' $runningvms.Length
+foreach ($vm in $runningvms) {
+    $vmid = $vm -split ' ' | select -Last 1
+    $vmname = $vm.Replace($vmid, '').Trim()
+
+    Write-Host "Shutting down the machine $vmname"
+    vboxmanage controlvm $vmid SHUTDOWN_TYPE
+    if ($LastExitCode -ne 0) { Write-Error "Error running vboxmanage - can't power down running vm: $vmname" }
+    else { Write-Host "Machine $vmname powered down prior to installation using 'SHUTDOWN_TYPE' method" }
+}
+'@ -replace 'SHUTDOWN_TYPE', $shutdown_type
+
+$res = Start-ProcessNonElevated -Cmd $commands -UsePowershell
+$res.out
+if ($res.err) { Write-Warning $res.err }
+
+## Doesn't seem to be required
+#Write-Host "Killing $packageName process"
+#$pvbox | kill
