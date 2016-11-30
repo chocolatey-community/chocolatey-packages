a267d55d6eb46ad4d4a3b7699aa3eee0c6ecf9fb
Miodrag Milic <miodrag.milic@gmail.com>
2016-11-30 11:18:04 +0100

    (virtualbox) changed power down method to savestate

--- automatic/virtualbox/tools/chocolateyBeforeModify.ps1
+++ automatic/virtualbox/tools/chocolateyBeforeModify.ps1
@@ -5,7 +5,7 @@ $packageName = 'virtualbox'
 
 #For acpipowerbutton to work on Windows server see: http://ethertubes.com/unattended-acpi-shutdown-of-windows-server/
 #sp HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system shutdownwithoutlogon 1
-$shutdown_type = 'poweroff'  # 'acpipowerbutton', 'poweroff', 'savestate'
+$shutdown_type = 'savestate'  # 'acpipowerbutton', 'poweroff', 'savestate'
 
 $pvbox = ps virtualbox -ea 0
 if (!$pvbox) {
@@ -13,7 +13,7 @@ if (!$pvbox) {
     return
 }
 
-Write-Host "$packageName is running"
+Write-Host "$packageName is running, trying to gracefully shutdown any running machines"
 
 $installLocation = Get-AppInstallLocation $packageName
 if (!$installLocation) { $installLocation = gcm VBoxManage.exe -ea 0 | select -expand Path | Split-Path }
@@ -31,25 +31,25 @@ if (!(Test-Path $installLocation\VBoxManage.exe)) {
 
 $commands = "Set-Alias vboxmanage '$installLocation\VBoxManage.exe'`n"
 $commands += @'
-[string[]] $runningvms = vboxmanage list runningvms
+[string[]] $runningvms = vboxmanage list runningvms 2>&1
 if ($LastExitCode -ne 0) { Write-Error "Error running vboxmanage - can't get running vms" }
-if ($runningvms -and ($runningvms.Length -eq 0)) { Write-Host "No running machines" }
+if ($runningvms -and ($runningvms.Length -eq 0)) { "No running machines" }
 
-Write-Host 'Number of machines running:' $runningvms.Length
+'Number of machines running: ' + $runningvms.Length
 foreach ($vm in $runningvms) {
     $vmid = $vm -split ' ' | select -Last 1
     $vmname = $vm.Replace($vmid, '').Trim()
 
     Write-Host "Shutting down the machine $vmname"
-    vboxmanage controlvm $vmid SHUTDOWN_TYPE
+    vboxmanage controlvm $vmid SHUTDOWN_TYPE 2>$null
     if ($LastExitCode -ne 0) { Write-Error "Error running vboxmanage - can't power down running vm: $vmname" }
-    else { Write-Host "Machine $vmname powered down prior to installation using 'SHUTDOWN_TYPE' method" }
+    else { "Machine $vmname powered down prior to installation using 'SHUTDOWN_TYPE' method" }
 }
 '@ -replace 'SHUTDOWN_TYPE', $shutdown_type
 
 $res = Start-ProcessNonElevated -Cmd $commands -UsePowershell
-$res.out
-if ($res.err) { Write-Warning $res.err }
+$res.out -join "`n" | Write-Host
+if ($res.err) { "Errors during graceful shutdown`n" + ($res.err -join "`n") | Write-Warning }
 
 ## Doesn't seem to be required
 #Write-Host "Killing $packageName process"
