c9f017d0379886da95c2c33f47ec9106d2c6210e
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-01 11:54:06 +0100

    (mkvtoolnix) not using fosshub anymore.

--- automatic/mkvtoolnix/tools/chocolateyInstall.ps1
+++ automatic/mkvtoolnix/tools/chocolateyInstall.ps1
@@ -1,22 +1,17 @@
-﻿$ErrorActionPreference = 'Stop'
-
-$packageName = 'mkvtoolnix'
-$url32 = Get-UrlFromFosshub 'https://www.fosshub.com/MKVToolNix.html/mkvtoolnix-32bit-9.5.0-setup.exe'
-$url64 = Get-UrlFromFosshub 'https://www.fosshub.com/MKVToolNix.html/mkvtoolnix-64bit-9.5.0-setup.exe'
-$checksum32  = '3b6f0f8b69bb9a4f3cfd560ccf740869ebf159229213dd52c321f2abc2c1cbfc'
-$checksum64  = '001b06cfda2074afd5d3f563a0569a7cab58933f3590121f151d60c9282e7f0a'
+$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = 'mkvtoolnix'
   fileType       = 'exe'
-  url            = $url32
-  url64bit       = $url64
-  checksum       = $checksum32
-  checksum64     = $checksum64
+  url            = 'https://mkvtoolnix.download/windows/releases/9.6.0/mkvtoolnix-32bit-9.6.0-setup.exe'
+  url64bit       = 'https://mkvtoolnix.download/windows/releases/9.6.0/mkvtoolnix-64bit-9.6.0-setup.exe'
+  checksum       = '520e9b435826a7bd941a05e0542ee21632cb5e0ae1884e164c5bab5946363f9a'
+  checksum64     = 'c4f99f1fd70d0284dce7c6ac30e9f31ae19f561a80cd2257c1cf442337477c43'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
+  softwareName   = 'mkvtoolnix*'
 }
 
 Install-ChocolateyPackage @packageArgs
@@ -27,4 +22,4 @@ if (!$installLocation)  { Write-Warning "Can't find $PackageName install locatio
 Write-Host "$packageName installed to '$installLocation'"
 
 Write-Host 'Adding to PATH if needed'
-Install-ChocolateyPath "$installLocation"       #TODO: Uninstall-ChocolateyPath #310
+Install-ChocolateyPath "$installLocation"       #TODO: Uninstall-ChocolateyPath #310nstall-ChocolateyPackage @packageArgs
