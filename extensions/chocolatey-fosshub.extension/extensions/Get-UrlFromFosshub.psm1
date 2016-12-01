c3738372ffaaf58095aa7470bd814fcdd89b7390
William Elwood <w.elwood08@gmail.com>
2016-12-01 08:33:02 +0000

    Fixed fosshub parsing to find the download url in any attribute
    fixes #414

--- extensions/chocolatey-fosshub.extension/extensions/Get-UrlFromFosshub.psm1
+++ extensions/chocolatey-fosshub.extension/extensions/Get-UrlFromFosshub.psm1
@@ -30,9 +30,9 @@ Function Get-UrlFromFosshub($linkUrl) {
 
   # Construct the Regular Expression pattern.  An example of what is being searched for is this
   # <a href="/Audacity.html/audacity-win-2.1.2.exe" rel="nofollow" class="dwl-link xlink" file="audacity-win-2.1.2.exe" xdts="https://download.fosshub.com/Protected/expiretime=1480500963;badurl=aHR0cDovL3d3dy5mb3NzaHViLmNvbS9BdWRhY2l0eS5odG1s/5cd3862bc6dd39508c537b1ad21a0bdc2e7d46dd4c64d182cfb6a819d0ef20a1/Audacity/audacity-win-2.1.2.exe">
-  $regexPattern = "<a href=`"$hrefText`".*(?:data|xdts)=`"(.*)`""
+  $regexPattern = "<a href=`"$hrefText`"[^>]+=`"(https:\/\/download\.fosshub\.com\/[^`"]+)`""
 
-  $opts = @{ Headers = @{ Referer = 'https://www.fosshub.com/${fosshubAppName}' } }
+  $opts = @{ Headers = @{ Referer = "https://www.fosshub.com/${fosshubAppName}" } }
   $htmlPage = Get-WebContent -url $linkUrl -options $opts
 
   $dataLink = $htmlPage -match $regexPattern
