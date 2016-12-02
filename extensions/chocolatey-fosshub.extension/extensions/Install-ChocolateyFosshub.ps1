eb378d25734ba9bf4fe7efa4d0fda24a92b79669
AdmiringWorm <kim.nordmo@gmail.com>
2016-12-02 13:11:53 +0100

    Implemented Install-ChocolateyFosshub

---
+++ extensions/chocolatey-fosshub.extension/extensions/Install-ChocolateyFosshub.ps1
@@ -0,0 +1,36 @@
+Function Install-ChocolateyFosshub() {
+  param(
+    [parameter(Mandatory=$true, Position=0)][string] $packageName,
+    [parameter(Mandatory=$false, Position=1)]
+    [alias("installerType","installType")][string] $fileType = 'exe',
+    [parameter(Mandatory=$false, Position=2)][string[]] $silentArgs = '',
+    [parameter(Mandatory=$false, Position=3)][string] $url = '',
+    [parameter(Mandatory=$false, Position=4)]
+    [alias("url64")][string] $url64bit = '',
+    [parameter(Mandatory=$false)] $validExitCodes = @(0),
+    [parameter(Mandatory=$false)][string] $checksum = '',
+    [parameter(Mandatory=$false)][string] $checksumType = '',
+    [parameter(Mandatory=$false)][string] $checksum64 = '',
+    [parameter(Mandatory=$false)][string] $checksumType64 = '',
+    [parameter(Mandatory=$false)][hashtable] $options = @{Headers=@{}},
+    [parameter(Mandatory=$false)][switch] $forceDownload,
+    [alias("useOnlyPackageSilentArgs")][switch] $useOnlyPackageSilentArguments = $false,
+    [parameter(ValueFromRemainingArguments = $true)][Object[]] $ignoredArguments
+  )
+
+  $filePath = Get-FosshubWebFile -PackageName $packageName `
+                                 -Url $url `
+                                 -Url64bit $url64bit `
+                                 -Checksum $checksum `
+                                 -ChecksumType $checksumType `
+                                 -Checksum64 $checksum64 `
+                                 -ChecksumType64 $checksumType64 `
+                                 -Options $options
+
+  Install-ChocolateyInstallPackage -PackageName $packageName `
+                                   -FileType $fileType `
+                                   -SilentArgs $silentArgs `
+                                   -File $filePath `
+                                   -ValidExitCodes $validExitCodes `
+                                   -UseOnlyPackageSilentArguments $useOnlyPackageSilentArguments
+}
