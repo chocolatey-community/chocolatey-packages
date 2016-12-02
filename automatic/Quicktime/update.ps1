a1f6d371bf02d710e0999c2bf871ff838f784a49
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-02 08:01:54 +0100

    (quicktime) 'access denied' ignored

--- automatic/Quicktime/update.ps1
+++ automatic/Quicktime/update.ps1
@@ -4,30 +4,33 @@ import-module "../../extensions/extensions.psm1"
 $releases = 'https://support.apple.com/kb/DL837?locale=en_US'
 
 function global:au_SearchReplace {
-  @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*-Url\s+)('.*')"            = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*-Checksum\s+)('.*')"       = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*-ChecksumType\s+)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+        "(?i)(^\s*-Url\s+)('.*')"            = "`$1'$($Latest.URL32)'"
+        "(?i)(^\s*-Checksum\s+)('.*')"       = "`$1'$($Latest.Checksum32)'"
+        "(?i)(^\s*-ChecksumType\s+)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+        }
     }
-  }
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
-  $re    = 'https:\/\/secure-appldnld\.apple\.com\/QuickTime\/[a-zA-Z\d\-\.]+\/QuickTimeInstaller\.exe'
-  $download_page.content -match $re | Out-Null
-  $url = $Matches[0]
+    $re = 'https:\/\/secure-appldnld\.apple\.com\/QuickTime\/[a-zA-Z\d\-\.]+\/QuickTimeInstaller\.exe'
+    $download_page.content -match $re | Out-Null
+    $url = $Matches[0]
 
-  $re    = 'strTitle"\s*:\s*"[^\d\.]+([\d\.]+)[^"]+'
-  $download_page.content -match $re | Out-Null
-  $version  = $Matches[1]
-
-  @{
-    URL32 = $url
-    Version = $version
-  }
+    $re = 'strTitle"\s*:\s*"[^\d\.]+([\d\.]+)[^"]+'
+    $download_page.content -match $re | Out-Null
+    $version = $Matches[1]
+    @{
+        URL32   = $url
+        Version = $version
+    }
 }
 
-update -ChecksumFor 32
+try {
+    update -ChecksumFor 32
+} catch {
+    if ($_ -match 'Access Denied') { Write-Host 'Access Denied error'; 'ignored' }  else { throw $_ }
+}
