15b130cc09d40bab80835d444d30ad3e38594049
Chocolatey <chocolatey@realdimensions.net>
2016-12-02 17:16:22 +0000

    AU: 3 updated - Cygwin palemoon ultradefrag [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/9a6db1305b826acf9c9c3a11ba0d1d82ccc6fb63

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'http://relmirror.palemoon.org/release/palemoon-27.0.1.win32.installer.exe'
-  url64         = 'http://relmirror.palemoon.org/release/palemoon-27.0.1.win64.installer.exe'
+  url           = 'http://relmirror.palemoon.org/release/palemoon-27.0.2.win32.installer.exe'
+  url64         = 'http://relmirror.palemoon.org/release/palemoon-27.0.2.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '0ff71bbe02031f255b9389f42297e2cd9fb3ed656c40724b184808c56c3af47d'
+  checksum      = '31839566e045e14846dbb836785906a413749450721ca8e595368c5381d3b3fc'
   checksumType  = 'sha256'
-  checksum64    = '8f9bc672cf2f84c096315bb5d7f91f7d4afcb166c591a15e39dac8fb9045755b'
+  checksum64    = '33530db10f40842e2855f69786f3efb28444058777b0188527bdde18649a0d3c'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
