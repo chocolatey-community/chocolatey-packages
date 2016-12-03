1641070cbf31ad92a4a47ccea4f4de154a3ef4d8
AdmiringWorm <kim.nordmo@gmail.com>
2016-12-03 11:11:37 +0100

    Updated qbittorrent to get checksum

--- automatic/qbittorrent/tools/chocolateyinstall.ps1
+++ automatic/qbittorrent/tools/chocolateyinstall.ps1
@@ -1,6 +1,3 @@
-Import-Module "D:\ChocoInstall\helpers\chocolateyInstaller.psm1"
-Import-Module "$PSScriptRoot\..\..\..\extensions\extensions.psm1"
-
 $packageName = 'qBittorrent'
 $fosshubUrl = 'https://www.fosshub.com/qBittorrent.html/qbittorrent_3.3.7_setup.exe'
 $checksum = '49ae9a0adfc3272bec38822c528f732d9495b79a2a7ca934f8c6635237b15d07'
--- automatic/qbittorrent/update.ps1
+++ automatic/qbittorrent/update.ps1
@@ -1,5 +1,6 @@
 import-module au
 import-module "./../../extensions/extensions.psm1"
+. "./../../extensions/chocolatey-fosshub.extension/extensions/Get-FosshubWebFile.ps1"
 
 $releases = 'https://www.fosshub.com/qBittorrent.html'
 
@@ -17,12 +18,27 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     $re    = '\.exe$'
-    $urlPart   = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $urlElement   = $download_page.links | ? href -match $re | select -First 1
+    $urlPart = $urlElement | select -expand href
     $url = "https://www.fosshub.com$urlPart"
+
+    $dlUrl = $urlElement.PSObject.Properties | ? {
+      $_.Value -match "^https:\/\/download\.fosshub\.com"
+    } | select -first 1 -expand Value
 
     $version  = $url -split '[_]' | select -Last 1 -Skip 1
 
-    return @{ URL32 = $url; Version = $version }
+    return @{ URL32 = $url; Version = $version; DownloadURL = $dlUrl }
 }
 
-update -ChecksumFor 32 -NoCheckUrl
+function global:au_BeforeUpdate {
+  # Since We need a referer header we can't use Get-RemoteChecksum
+  $stream = Invoke-WebRequest $Latest.DownloadURL -Headers @{ 'Referer' = $Latest.URL32 } | select -expand RawContentStream
+
+  $Latest.Checksum32 = Get-FileHash -Algorithm SHA256 -InputStream $stream | % Hash
+  $Latest.ChecksumType32 = 'sha256'
+
+  $stream.Dispose()
+}
+
+update -ChecksumFor none -NoCheckUrl
