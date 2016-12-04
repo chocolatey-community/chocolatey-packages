01e5f333b909bef173d181c2f30176295d6c7357
Pascal Berger <pascal.berger@gmail.com>
2016-12-04 21:08:16 +0100

    (visualstudiocode) Add Visual Studio Code package

---
+++ automatic/visualstudiocode/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,38 @@
+$ErrorActionPreference = 'Stop';
+
+$pp = Get-PackageParameters
+$mergeTasks = "!runCode"
+$mergeTasks += if ($pp.NoDesktopIcon) { ',!desktopicon' } else { ',desktopicon' }
+$mergeTasks += if ($pp.NoQuicklaunchIcon) { ',!quicklaunchicon' } else { ',quicklaunchicon' } 
+$mergeTasks += if ($pp.NoContextMenuFiles) { ',!addcontextmenufiles' } else { ',addcontextmenufiles' }
+$mergeTasks += if ($pp.NoContextMenuFolders) { ',!addcontextmenufolders' } else { ',addcontextmenufolders' }
+$mergeTasks += if ($pp.DontAddToPath) { ',!addtopath' } else { ',addtopath' }
+Write-Host "Merge Tasks: "
+Write-Host "$mergeTasks"
+
+$packageName = 'visualstudiocode'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32 = 'https://az764295.vo.msecnd.net/stable/7ba55c5860b152d999dda59393ca3ebeb1b5c85f/VSCodeSetup-1.7.2.exe'
+$url64 = 'https://az764295.vo.msecnd.net/stable/7ba55c5860b152d999dda59393ca3ebeb1b5c85f/VSCodeSetup-1.7.2.exe'
+$checksum32 = 'ce706a90880dfc94f1c6b3911ffd66be0ec4ec4c0fcb70eec78b20357bdcee23'
+$checksum64 = 'ce706a90880dfc94f1c6b3911ffd66be0ec4ec4c0fcb70eec78b20357bdcee23'
+
+$packageArgs = @{
+  packageName = $packageName
+  unzipLocation = $toolsDir
+  fileType = 'EXE'
+  url = $url32
+  url64bit = $url64
+
+  softwareName = 'Microsoft Visual Studio Code'
+
+  checksum = $checksum32
+  checksumType = 'sha256'
+  checksum64 = $checksum64
+  checksumType64 = 'sha256'
+
+  silentArgs = "/verysilent /suppressmsgboxes /mergetasks=$mergeTasks /log=""$env:temp\vscode.log"""
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/visualstudiocode/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://vscode-update.azurewebsites.net/api/update/win32/stable/VERSION'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $json = Invoke-WebRequest -UseBasicParsing -Uri $releases | ConvertFrom-Json
+
+    @{
+        Version   = $json.productVersion
+        URL32     = $json.Url
+        URL64     = $json.Url
+    }
+}
+
+update
