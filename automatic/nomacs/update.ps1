a40cf179f7ff1f5eb3509a626af25f9b1029154a
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-05 14:23:16 +0100

    (nomacs) Guidelines (fixes #395)

---
+++ automatic/nomacs/update.ps1
@@ -0,0 +1,34 @@
+import-module au
+
+$releases = 'http://download.nomacs.org/versions'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url     = $download_page.links | ? href -like '*/nomacs-*' | % href | sort -desc | select -First 1
+    $version = $url -split '-|\.zip' | select -Last 1 -Skip 1
+    @{
+        Version      = $version
+        URL32        = 'http://download.nomacs.org/nomacs-setup.exe'
+        ReleaseNotes = "https://github.com/nomacs/nomacs/releases/tag/${version}"
+    }
+}
+
+update -ChecksumFor 32
