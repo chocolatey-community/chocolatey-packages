c8ad3ec2b347e6016cb20e3b724425215ddefb8b
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-06 12:51:05 +0100

    (autohotkey.install) embedded package

--- automatic/autohotkey.install/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.install/tools/chocolateyInstall.ps1
@@ -1,29 +1,25 @@
 $ErrorActionPreference = 'Stop'
 
-$silent = '/S'
-if(Get-ProcessorBits 64) { $silent += ' /x64' }
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+
+$silentArgs = @('/S')
+$silentArgs += if ((Get-ProcessorBits 64) -and ($env:chocolateyForceX86 -ne 'true')) { '/x64' }
 
 $packageArgs = @{
   packageName    = 'autohotkey.install'
   fileType       = 'exe'
-  url            = 'https://github.com/Lexikos/AutoHotkey_L/releases/download/v1.1.24.03/AutoHotkey_1.1.24.03_setup.exe'
-  url64bit       = 'https://github.com/Lexikos/AutoHotkey_L/releases/download/v1.1.24.03/AutoHotkey_1.1.24.03_setup.exe'
-  checksum       = '588ac27ea8a16e0512a28cd4f7560ae12d0b55500ef5abca92a3e95208319029'
-  checksum64     = '588ac27ea8a16e0512a28cd4f7560ae12d0b55500ef5abca92a3e95208319029'
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  silentArgs     = $silent
-  validExitCodes = @(0)
-  softwareName   = 'autohotkey'
+  file           = gi "$toolsDir\*.exe"
+  silentArgs     = $silentArgs
+  validExitCodes = @(0, 1223)
 }
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+rm $toolsDir\*.exe
 
 $packageName = $packageArgs.packageName
-$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+$installLocation = Get-AppInstallLocation $packageName
 if ($installLocation)  {
     Write-Host "$packageName installed to '$installLocation'"
+    Register-Application "$installLocation\$packageName.exe"
+    Write-Host "$packageName registered as $packageName"
 }
-else {
-    Write-Warning "Can't find $packageName install location"
-}
-
+else { Write-Warning "Can't find $PackageName install location" }
--- automatic/autohotkey.install/update.ps1
+++ automatic/autohotkey.install/update.ps1
@@ -5,25 +5,42 @@ $releases = 'https://autohotkey.com/download/1.1'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
             "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
+        }
+
+        ".\tools\verification.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL)"
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum)"
+          "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.Checksum)"
         }
     }
 }
 
+function global:au_BeforeUpdate {
+    rm "$PSScriptRoot\tools\*.exe"
+
+    $client = New-Object System.Net.WebClient
+        $filePath = "$PSScriptRoot\tools\$($Latest.FileName)"
+        $client.DownloadFile($Latest.URL, $filePath)
+    $client.Dispose()
+
+    $Latest.ChecksumType = 'sha256'
+    $Latest.Checksum = Get-FileHash -Algorithm $Latest.ChecksumType -Path $filePath | % Hash
+}
+
 function global:au_GetLatest {
     $version = Invoke-WebRequest -Uri "$releases\version.txt" -UseBasicParsing | % Content
-    $url     = "$releases/AutoHotkey_${version}"
+    $url     = "https://github.com/Lexikos/AutoHotkey_L/releases/download/v${version}/AutoHotkey_${version}_setup.exe"
     @{
-        Version = $version
-        URL32   = "https://github.com/Lexikos/AutoHotkey_L/releases/download/v${version}/AutoHotkey_${version}_setup.exe"
+        Version  = $version
+        URL      = $url
+        FileName = $url -split '/' | select -Last 1
     }
 }
 
 if ($MyInvocation.InvocationName -ne '.') {
-    update -ChecksumFor 32
+    update -ChecksumFor none
 }
