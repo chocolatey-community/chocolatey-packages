08359d0d7c09a12bd60b5c4d60c6b282db022fea
Chocolatey <chocolatey@realdimensions.net>
2016-12-06 12:11:37 +0000

    AU: 2 updated - bluegriffon Clover [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/6374fe6d9ee32a6b231a3f71b5e788c1fb3b65ff

--- automatic/bluegriffon/tools/chocolateyInstall.ps1
+++ automatic/bluegriffon/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'bluegriffon'
   fileType       = 'exe'
   softwareName   = 'BlueGriffon*'
 
-  checksum       = ''
-  checksumType   = ''
-  url            = ''
+  checksum       = 'a63ddfcf19d1d68c75311964bb0b8ab52e1d41d306b40f8e588851f6d1d62b30'
+  checksumType   = 'sha256'
+  url            = 'http://bluegriffon.org/freshmeat/2.1.2/bluegriffon-2.1.2.win32-installer.exe'
 
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
