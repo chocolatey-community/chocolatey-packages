0138e76c4587c4254a3fd7a180facf34db676b55
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-08 23:31:28 +0100

    (avidemux) embeded package

--- automatic/avidemux/tools/chocolateyInstall.ps1
+++ automatic/avidemux/tools/chocolateyInstall.ps1
@@ -1,22 +1,25 @@
 $ErrorActionPreference = 'Stop'
 
-$packageName = 'avidemux'
-$url32 = Get-UrlFromFosshub 'https://www.fosshub.com/Avidemux.html/avidemux_2.6.15_win32.exe'
-$url64 = Get-UrlFromFosshub 'https://www.fosshub.com/Avidemux.html/avidemux_2.6.15_win64.exe'
-$checksum32  = 'b4bbf5bf995dbaca3ea9ca8b87a0c69de5ccedf9de95cc3d7c942b5305fa0fe7'
-$checksum64  = '8ff161d5714f148f9a316e2bca1e01080bfb12222ba5991d8edab859b4072814'
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+$installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') { gi "$toolsDir\*win64.exe" } else { gi "$toolsDir\*win32.exe" }
+
+$silentArgs = @('/S')
 
 $packageArgs = @{
-  packageName            = $packageName
-  fileType               = 'EXE'
-  url                    = $url32
-  url64bit               = $url64
-  checksum               = $checksum32
-  checksum64             = $checksum64
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  silentArgs             = '/S'
-  validExitCodes         = @(0, 1223)
+  packageName    = 'avidemux'
+  fileType       = 'exe'
+  file           = $installerFile
+  silentArgs     = $silentArgs
+  validExitCodes = @(0, 1223)
 }
+Install-ChocolateyInstallPackage @packageArgs
+rm $toolsDir\*.exe
 
-Install-ChocolateyPackage @packageArgs
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageName
+if ($installLocation)  {
+    Write-Host "$packageName installed to '$installLocation'"
+    Register-Application "$installLocation\$packageName.exe"
+    Write-Host "$packageName registered as $packageName"
+}
+else { Write-Warning "Can't find $PackageName install location" }
