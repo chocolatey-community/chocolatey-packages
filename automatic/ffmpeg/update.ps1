5e36683183ab973a01b30be8ad100bf922799885
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-09 07:09:12 +0100

    (ffmpeg) synced au_GetLatest with latest vendor changes

--- automatic/ffmpeg/update.ps1
+++ automatic/ffmpeg/update.ps1
@@ -14,11 +14,11 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri "$releases/win32/static" -UseBasicParsing
+    $download_page = Invoke-WebRequest -Uri "$releases/win32/static/" -UseBasicParsing -Header @{ Referer = $releases }
     $version       = $download_page.links | ? href -match '\.(zip|7z)$' | % { $_.href -split '-' | select -Index 1} | ? { $__=$null; [version]::TryParse($_, [ref] $__) } | sort -desc | select -First 1
     $url32         = "$releases/win32/static/" + ($download_page.links | ? href -like "*$version*" | % href)
 
-    $download_page = Invoke-WebRequest -Uri "$releases/win64/static" -UseBasicParsing
+    $download_page = Invoke-WebRequest -Uri "$releases/win64/static/" -UseBasicParsing -Header @{ Referer = $releases }
     $url64         = "$releases/win64/static/" + ($download_page.links | ? href -like "*$version*" | % href)
 
     @{ URL32 = $url32; URL64=$url64; Version = $version }
