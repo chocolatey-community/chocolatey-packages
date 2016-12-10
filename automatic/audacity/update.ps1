36859b5b2b3d6803a40f079a5e4c0b2c8bbaf02a
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-10 13:28:35 +0100

    (audacity) vector smash fixes [AU audacity]

--- automatic/audacity/update.ps1
+++ automatic/audacity/update.ps1
@@ -9,9 +9,8 @@ function global:au_SearchReplace {
         }
 
         ".\tools\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.BaseURL)"
           "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-          "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
         }
 
     }
@@ -65,14 +64,18 @@ function global:au_BeforeUpdate {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    $url     = $download_page.links | ? href -match 'download_audacity/download' | % href | select -first 1
-    $version = $download_page.AllElements | ? class -eq 'title-text' | % title
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url = $download_page.Content -split "`n" | sls 'href=".+download_audacity/download/[^"]+'
+    $url = $url -split '"' | select -Index 1
+    $version = $download_page.Content -split "`n" | sls 'class="title-text"'
+    $version = $version -split '"' | select -Last 1 -Skip 1
 
-    $download_page = Invoke-WebRequest $url
-    $url = $download_page.links | ? id -eq 'download-link' | % href
+    $download_page = Invoke-WebRequest $url -UseBasicParsing
+    $url32 = $download_page.Content -split "`n" | sls 'id="download-link"'
+    $url32 = $url32 -split '"' | select -Index 1
     @{
-        URL32    = 'http://filehippo.com' + $url
+        URL32    = 'http://filehippo.com' + $url32
+        BaseUrl  = $url
         Version  = $version -split ' ' | select -Last 1
         FileType = 'exe'
     }
