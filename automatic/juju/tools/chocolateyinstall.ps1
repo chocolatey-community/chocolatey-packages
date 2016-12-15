0529ae296d40b8cd9687c8d350f226e3e45cf3a6
AdmiringWorm <kim.nordmo@gmail.com>
2016-12-15 09:04:41 +0100

    (juju) Changed to Embedded package

--- automatic/juju/tools/chocolateyinstall.ps1
+++ automatic/juju/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 $ErrorActionPreference = 'Stop';
 
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
 $packageArgs = @{
   packageName   = 'juju'
   fileType      = 'exe'
   softwareName  = 'Juju'
-
-  checksum      = 'affc683c79550c2d60f53e73dbdec5dd9317827f979cace0b7aa296aa8a24f8b'
-  checksumType  = 'sha256'
-  url           = 'https://launchpad.net/juju/2.1/2.1-beta2/+download/juju-setup-2.1-beta2.exe'
-
+  file          = "$toolsDir\juju-setup-2.1-beta2.exe"
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force $packageArgs.file
