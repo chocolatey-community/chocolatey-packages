021621868f4dd9fedcc1592ac235665562de243c
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-15 21:53:41 +0100

    (activepresenter) embedded package

--- automatic/activepresenter/tools/chocolateyInstall.ps1
+++ automatic/activepresenter/tools/chocolateyInstall.ps1
@@ -1,13 +1,23 @@
 $ErrorActionPreference = 'Stop'
 
+$fileType      = 'exe'
+$toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
+$embedded_path = gi "$toolsDir\*.$fileType"
+
 $packageArgs = @{
-  packageName            = 'activepresenter'
-  fileType               = 'EXE'
-  url                    = 'https://cdn.atomisystems.com/apdownloads/ActivePresenter_v6.0.3_setup.exe'
-  checksum               = '4ffb6e600a255043e33fa59873ceb3ab263a71d481d3515ab5fbdc63a38e15db'
-  checksumType           = 'sha256'
-  silentArgs             = '/VERYSILENT'
-  validExitCodes         = @(0)
-  softwareName           = 'ActivePresenter'
+  packageName    = 'activepresenter'
+  fileType       = $fileType
+  file           = $embedded_path
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0)
 }
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+rm $embedded_path -ea 0
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
+
+Register-Application "$installLocation\$packageName.exe"
+Write-Host "$packageName registered as $packageName"
