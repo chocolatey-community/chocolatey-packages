b9586a5f90dfd4dbb2263a12c22c0abd5aacc2f1
Chocolatey <chocolatey@realdimensions.net>
2016-12-15 10:17:42 +0000

    AU: 1 updated - sourcetree [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/62933c5f7229cab0573b750dd1592376b5c76c92

--- automatic/sourcetree/tools/chocolateyInstall.ps1
+++ automatic/sourcetree/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName = 'sourcetree'
   fileType = 'EXE'
-  url = 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.9.9.20.exe'
-  url64bit = 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.9.9.20.exe'
+  url = 'https://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.9.10.0.exe'
+  url64bit = 'https://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.9.10.0.exe'
 
   softwareName = 'SourceTree'
 
-  checksum = '62f795e548bac9ab41e8d545945636fd77f5e621af38997cf4554cd4420b6a4f'
+  checksum = '0d4bb9f9f6835f0dcdd46cd0dae38f1958bde09ebf41b7555e7115ffcadff837'
   checksumType = 'sha256'
-  checksum64 = '62f795e548bac9ab41e8d545945636fd77f5e621af38997cf4554cd4420b6a4f'
+  checksum64 = '0d4bb9f9f6835f0dcdd46cd0dae38f1958bde09ebf41b7555e7115ffcadff837'
   checksumType64= 'sha256'
 
   silentArgs = "/passive"
