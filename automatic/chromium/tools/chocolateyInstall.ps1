c9a3a65863eb03c1e9d951c50bd5cf4ffd4e7086
Chocolatey <chocolatey@realdimensions.net>
2016-12-15 07:17:59 +0000

    AU: 2 updated - chromium sourcetree [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/74c7a486fcb57438491c11d90312e9d7dfa86d16

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
-$version = '57.0.2952.0'
+$version = '57.0.2953.0'
 
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $hive = "hkcu"
@@ -15,14 +15,14 @@ if (Test-Path $Chromium) {
 $packageArgs = @{
   packageName   = 'chromium'
   fileType      = 'exe'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/438470/mini_installer.exe'
-  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/438471/mini_installer.exe'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/438765/mini_installer.exe'
+  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/438764/mini_installer.exe'
   silentArgs    = $silentArgs
   validExitCodes= @(0)
   softwareName  = 'Chromium'
-  checksum      = '0b58fc8bb24911a9a39e776188234b18'
+  checksum      = '51c4951222a163e0bbe78e3322acf1db'
   checksumType  = 'md5'
-  checksum64    = '6a95d00f53c49fbbffc31ea02aa5a8bc'
+  checksum64    = '8275f7acbb917d0db46d76dbb46888d3'
   checksumType64= 'md5'
 }
 
--- automatic/sourcetree/tools/chocolateyInstall.ps1
+++ automatic/sourcetree/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName = 'sourcetree'
   fileType = 'EXE'
-  url = ''
-  url64bit = ''
+  url = 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.9.9.20.exe'
+  url64bit = 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.9.9.20.exe'
 
   softwareName = 'SourceTree'
 
-  checksum = ''
+  checksum = '62f795e548bac9ab41e8d545945636fd77f5e621af38997cf4554cd4420b6a4f'
   checksumType = 'sha256'
-  checksum64 = ''
+  checksum64 = '62f795e548bac9ab41e8d545945636fd77f5e621af38997cf4554cd4420b6a4f'
   checksumType64= 'sha256'
 
   silentArgs = "/passive"
