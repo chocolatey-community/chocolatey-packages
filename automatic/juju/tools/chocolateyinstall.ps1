2cade8bd46c4321d47fcb405aa90bccaa145940d
Chocolatey <chocolatey@realdimensions.net>
2016-12-16 00:11:38 +0000

    AU: 1 updated - juju [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/dcee3b2946ec881af37afb0bfb96dbba6d6cad7d

--- automatic/juju/tools/chocolateyinstall.ps1
+++ automatic/juju/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName   = 'juju'
   fileType      = 'exe'
   softwareName  = 'Juju'
-  file          = "$toolsDir\juju-setup-2.1-beta2.exe"
+  file          = "$toolsDir\juju-setup-2.1-beta3.exe"
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
