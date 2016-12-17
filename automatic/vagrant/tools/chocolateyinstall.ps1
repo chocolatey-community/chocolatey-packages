276358e6a8fd72127cfa9a1fcf4fa918cd0cef1c
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-17 18:49:26 +0100

    (vagrant) original package from fervent coder

---
+++ automatic/vagrant/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'vagrant'
+$url = '{{DownloadUrl}}'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  url           = $url
+  silentArgs    = "/qn /norestart"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
