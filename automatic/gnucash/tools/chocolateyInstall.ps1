868bed8261887b8aa7de96f8f4661feda617a619
Chocolatey <chocolatey@realdimensions.net>
2016-12-18 22:32:35 +0000

    AU: 1 updated - gnucash [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/9229d7dcb82754cae7204c3638c700341609beac

--- automatic/gnucash/tools/chocolateyInstall.ps1
+++ automatic/gnucash/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName   = 'gnucash'
   fileType      = 'exe'
-  url           = 'https://github.com/Gnucash/gnucash/releases/download/2.6.14a/gnucash-2.6.14-setup.exe'
+  url           = 'https://github.com/Gnucash/gnucash/releases/download/2.6.15/gnucash-2.6.15.setup.exe'
 
   softwareName  = 'GnuCash*'
 
-  checksum      = 'c0b71a6839df4fa17bf57d9a3c6fb104c8c456f80f1871cb3c842d6623ee868a'
+  checksum      = '822d4c99e402cf9526c5ec1f2f246ded704eefd96185e7e18712ab06f12b00e9'
   checksumType  = 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /SP-"
