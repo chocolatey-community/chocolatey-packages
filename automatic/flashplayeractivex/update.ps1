aaef87cccef7c6512b1597064825e1246c27eb97
AdmiringWorm <kim.nordmo@gmail.com>
2016-12-18 19:58:07 +0100

    (flashplayeractivex) Normalized TabsToSpaces

--- automatic/flashplayeractivex/update.ps1
+++ automatic/flashplayeractivex/update.ps1
@@ -1,7 +1,7 @@
 
 import-module au
 
-$releases = "http://get.adobe.com/en/flashplayer/" # URL to for GetLatest
+$releases = "https://get.adobe.com/en/flashplayer/" # URL to for GetLatest
 
 function global:au_BeforeUpdate {
   # We need this, otherwise the checksum won't get created
@@ -11,33 +11,32 @@ function global:au_BeforeUpdate {
 }
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]version\s*=\s*)('.*')"= "`$1'$($Latest.Version)'"
-            "(^[$]majorVersion\s*=\s*)('.*')"= "`$1'$($Latest.majorVersion)'"
-            "(^[$]packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]version\s*=\s*)('.*')"= "`$1'$($Latest.Version)'"
+      "(^[$]majorVersion\s*=\s*)('.*')"= "`$1'$($Latest.majorVersion)'"
+      "(^[$]packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
+      "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
+  }
 }
 
 function global:au_GetLatest {
 
-	$HTML = Invoke-WebRequest -Uri $releases
-	$try = ($HTML.ParsedHtml.getElementsByTagName('p') | Where{ $_.className -eq 'NoBottomMargin' } ).innerText
-	$try = $try  -split "\r?\n"
-	$try = $try[0] -replace ' ', ' = '
-	$try =  ConvertFrom-StringData -StringData $try
-	$CurrentVersion = ( $try.Version )
-	$majorVersion = ([version] $CurrentVersion).Major
+  $HTML = Invoke-WebRequest -Uri $releases
+  $try = ($HTML.ParsedHtml.getElementsByTagName('p') | Where{ $_.className -eq 'NoBottomMargin' } ).innerText
+  $try = $try  -split "\r?\n"
+  $try = $try[0] -replace ' ', ' = '
+  $try =  ConvertFrom-StringData -StringData $try
+  $CurrentVersion = ( $try.Version )
+  $majorVersion = ([version] $CurrentVersion).Major
 
-    $url32 = 'https://download.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_<majorVersion>_active_x.msi'
+  $url32 = 'https://download.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_<majorVersion>_active_x.msi'
+  $url32 = $url32 -replace '<majorVersion>', $majorVersion
 
-    $url32 = $url32 -replace '<majorVersion>', $majorVersion
-
-	return @{ URL32 = $url32; Version = $CurrentVersion; majorVersion = $majorVersion; }
+  return @{ URL32 = $url32; Version = $CurrentVersion; majorVersion = $majorVersion; }
 }
 
 update -ChecksumFor 32
