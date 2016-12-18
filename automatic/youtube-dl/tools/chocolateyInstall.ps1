d5e4e495e755e54dfa2e8d062ad6b880c32246f1
Chocolatey <chocolatey@realdimensions.net>
2016-12-18 13:45:22 +0000

    AU: 1 updated - youtube-dl [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/88219b58dce458062e5f5ad15601cfa55132fc61

--- automatic/youtube-dl/tools/chocolateyInstall.ps1
+++ automatic/youtube-dl/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'youtube-dl'
-$url = 'https://github.com/rg3/youtube-dl/releases/download/2016.12.15/youtube-dl.exe'
-$checksum = '534948676578c7fedf16c1df7aa729712b43b7b10ed792b3bbc6d73d733d3f3c2e43eb93f4b1711bf4b7227a2396c1d860591f23f8203a4f27c374d1e26a1300'
+$url = 'https://github.com/rg3/youtube-dl/releases/download/2016.12.18/youtube-dl.exe'
+$checksum = 'ebca98e6bd29239fba2897b020ed302196b2ba73649696dd229e3319e11adf31189674b0c23ba6171e31811592582d483a6d3e44eeac0ebe1e7fb07205870f37'
 $checksumType = 'sha512'
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
