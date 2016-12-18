5eda1e20c8f284ee1d2ca6f5bdc03619a4841538
AdmiringWorm <kim.nordmo@gmail.com>
2016-12-18 23:24:25 +0100

    (gnucash) Fixed gnucash version not found

--- automatic/gnucash/update.ps1
+++ automatic/gnucash/update.ps1
@@ -18,7 +18,12 @@ function global:au_GetLatest {
   $re    = '^/.+\.exe$'
   $url   = $download_page.links | ? href -match $re | select -first 1 -expand href
 
-  $version  = $url -split '[-]' | select -Last 1 -Skip 1
+  $version  = $url -split '[-]|\.setup' | select -Last 1 -Skip 1
+
+  if (!($version -match "^[\d\.]+$")) {
+    # They change the filename again, lets try the tag name
+    $version = ($url -split '\/' | select -last 1 -skip 1).TrimEnd('/')
+  }
 
   @{
     URL32 = 'https://github.com' + $url
