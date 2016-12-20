4dc8e0e8864911ec6f70d5c773394623a4a94e39
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-20 14:51:41 +0100

    (audacity) using Get-RemoteFiles; added license and legal folder

--- automatic/audacity/update.ps1
+++ automatic/audacity/update.ps1
@@ -8,7 +8,7 @@ function global:au_SearchReplace {
             "(^\s*packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
         }
 
-        ".\tools\VERIFICATION.txt" = @{
+        ".\legal\VERIFICATION.txt" = @{
           "(?i)(\s+x32:).*"            = "`${1} $($Latest.BaseURL)"
           "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
         }
@@ -16,52 +16,7 @@ function global:au_SearchReplace {
     }
 }
 
-function global:au_BeforeUpdate {
-    function Convert-ToEmbeded( [switch] $Purge, [string]$FileNameBase, [int]$FileNameSkip=0 ) {
-
-        if (!$FileNameBase) {
-            $url = $Latest.URL32; if (!$url) { $url = $Latest.Url64 }
-            $FileNameBase = $url -split '/' | select -Last 1 -Skip $FileNameSkip
-            if ($FileNameBase -match '\.[a-zA-Z]+$') {
-                $ext = $Matches[0]
-                $FileNameBase = $FileNameBase.Replace($ext, '')
-            }
-        }
-
-        $FileType = $Latest.FileType
-        if (!$FileType) { $FileType = $ext }
-        if (!$FileType) { throw 'Unknown file type' }
-
-        if ($Purge) {
-            Write-Host 'Purging' $FileType
-            rm -Force "$PSScriptRoot\tools\*$FileType"
-        }
-
-        try {
-            $client = New-Object System.Net.WebClient
-
-            if ($Latest.Url32) {
-                $file_name = "{0}_x32.{1}" -f $FileNameBase, $FileType
-                $file_path = "$PSScriptRoot\tools\$file_name"
-
-                Write-Host "Downloading to $file_name -" $Latest.Url32
-                $client.DownloadFile($Latest.URL32, $file_path)
-                $Latest.Checksum32 = Get-FileHash $file_path | % Hash
-            }
-
-            if ($Latest.Url64) {
-                $file_name = "{0}_x64.{1}" -f $FileNameBase, $FileType
-                $file_path = "$PSScriptRoot\tools\$file_name"
-
-                Write-Host "Downloading to $file_name -" $Latest.Url64
-                $client.DownloadFile($Latest.URL32, $file_path)
-                $Latest.Checksum64 = Get-FileHash $file_path | % Hash
-            }
-        } finally { $client.Dispose() }
-    }
-
-    Convert-ToEmbeded -Purge -FileNameBase $Latest.PackageName
-}
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase $Latest.PackageName }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
