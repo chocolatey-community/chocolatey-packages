b3a0cc053e74b3d3aaaa650c5237543a66e49f33
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-20 14:54:23 +0100

    (everythin) using Get-RemoteFiles; legal folder

--- automatic/everything/update.ps1
+++ automatic/everything/update.ps1
@@ -9,7 +9,7 @@ function global:au_SearchReplace {
             "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
         }
 
-        ".\tools\VERIFICATION.txt" = @{
+        ".\legal\VERIFICATION.txt" = @{
           "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
           "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
           "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
@@ -19,33 +19,7 @@ function global:au_SearchReplace {
     }
 }
 function global:au_BeforeUpdate {
-    function Convert-ToEmbeded( [switch] $Purge ) {
-        if ($Purge) {
-            $url = $Latest.URL32; if (!$url) { $url = $Latest.Url64 }
-            $file_name = $url -split '/' | select -Last 1
-            if ($file_name -match '\.[A-Z]+$') { rm -Force "$PSScriptRoot\tools\*.$($Matches[0])"}
-        }
-
-        try {
-            $client = New-Object System.Net.WebClient
-
-            if ($Latest.Url32) {
-                $file_path = "$PSScriptRoot\tools\" + ($Latest.URL32 -split '/' | select -Last 1)
-                $client.DownloadFile($Latest.URL32, $file_path)
-                $Latest.Checksum32 = Get-FileHash $file_path | % Hash
-                $Latest.ChecksumType32 = 'sha256'
-            }
-
-            if ($Latest.Url64) {
-                $file_path = "$PSScriptRoot\tools\" + ($Latest.URL64 -split '/' | select -Last 1)
-                $client.DownloadFile($Latest.URL64, $file_path)
-                $Latest.Checksum64 = Get-FileHash $file_path | % Hash
-                $Latest.ChecksumType64 = 'sha256'
-            }
-        } finally { $client.Dispose() }
-    }
-
-    Convert-ToEmbeded -Purge
+    Get-RemoteFiles -Purge
     iwr 'https://www.voidtools.com/es.exe' -OutFile $PSScriptRoot\tools\es.exe
 }
 
