dbedb56d5ff709cd37f4abecc736d4a9e6400da8
Miodrag Milic <miodrag.milic@gmail.com>
2016-12-21 10:09:10 +0100

    (7zip*) original ferventcoder data

---
+++ automatic/7zip.commandline/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$versionMinusDots = "{{PackageVersion}}".Replace(".","")
+
+$packageName = '7zip.commandline'
+$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$extrasDir = Join-Path "$toolsDir" "7z-extra"
+$url = "http://www.7-zip.org/a/7z$($versionMinusDots).exe"
+$url64 = "http://www.7-zip.org/a/7z$($versionMinusDots)-x64.exe"
+$extrasUrl = "http://www.7-zip.org/a/7z$($versionMinusDots)-extra.7z"
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
+Install-ChocolateyZipPackage $packageName $extrasUrl $extrasDir
+
+Remove-Item -Path "$toolsDir\Uninstall.exe"
+
+if (Get-ProcessorBits 32) {
+  # generate ignore for x64\7za.exe
+  New-Item "$extrasDir\x64\7za.exe.ignore" -Type file -Force | Out-Null
+} else {
+  #generate ignore for 7za.exe and let x64 version pick up and shim
+  New-Item "$extrasDir\7za.exe.ignore" -Type file -Force | Out-Null
+}
---
+++ automatic/7zip.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿Function Get-ExplorerProcessCount
+{
+  $process = Get-Process explorer -ErrorAction SilentlyContinue
+  $processCount = ($process | Measure-Object).Count
+  return $processCount
+}
+
+$initialProcessCount = Get-ExplorerProcessCount
+Write-Warning "This installer is known to close the explorer process. This means `nyou may lose current work. `nIf it doesn't automatically restart explorer, type 'explorer' on the `ncommand shell to restart it."
+
+$versionMinusDots = "{{PackageVersion}}".Replace(".","")
+$packageId = '7zip.install'
+$url = "http://www.7-zip.org/a/7z$($versionMinusDots).exe"
+$url64 = "http://www.7-zip.org/a/7z$($versionMinusDots)-x64.exe"
+
+Install-ChocolateyPackage $packageId 'exe' '/S' $url $url64
+
+$finalProcessCount = Get-ExplorerProcessCount
+if($initialProcessCount -lt $finalProcessCount)
+{
+  Start-Process explorer.exe
+}
