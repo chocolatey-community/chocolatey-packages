40da80591281de666f24852bb71cb12449cf2997
Pascal Berger <pascal.berger@gmail.com>
2016-12-22 14:10:08 +0100

    (autoit*) Original ferventcoder data

---
+++ automatic/autoit.commandline/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = '{{PackageName}}'
+$url = '{{DownloadUrl}}'
+$unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+try {
+    Install-ChocolateyZipPackage $packageName $url $unzipLocation
+} catch {
+    Write-ChocolateyFailure $packageName $($_.Exception.Message)
+    throw
+}
---
+++ automatic/autoit/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyPackage 'autoit' 'exe' '/S' '{{DownloadUrl}}'
