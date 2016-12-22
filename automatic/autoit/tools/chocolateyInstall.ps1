5018e84f62f0033e31296f7842acee11491de4a3
Pascal Berger <pascal.berger@gmail.com>
2016-12-22 15:35:08 +0100

    (autoit) Add AU script, change to embedded package and adopt to guidelines

--- automatic/autoit/tools/chocolateyInstall.ps1
+++ automatic/autoit/tools/chocolateyInstall.ps1
@@ -1,1 +1,18 @@
-﻿Install-ChocolateyPackage 'autoit' 'exe' '/S' '{{DownloadUrl}}'
+$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$filePath = "$toolsDir\autoit-v3-setup.exe"
+
+$packageArgs = @{
+  PackageName    = 'autoit'
+  FileType       = 'exe'
+  SoftwareName   = 'AutoIt*'
+  File           = $filePath
+  SilentArgs     = '/S'
+  ValidExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+# Lets remove the installer as there is no more need for it
+Remove-Item -Force $filePath
