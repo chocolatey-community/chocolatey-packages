25a40985eb12778b130d754058887895f8b1bc40
Pascal Berger <pascal.berger@gmail.com>
2016-12-23 14:22:43 +0100

    (tortoisegit) Original dtgm data

---
+++ automatic/tortoisegit/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart REBOOT=ReallySuppress'
+$url = '{{DownloadUrl}}'
+$checksum = '{{Checksum}}'
+$checksumType = 'sha256'
+$url64 = '{{DownloadUrlx64}}'
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
