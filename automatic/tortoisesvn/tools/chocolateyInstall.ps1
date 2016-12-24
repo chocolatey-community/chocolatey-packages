81c6df0de9bb542ecc516c813101d10525c74e51
Pascal Berger <pascal.berger@gmail.com>
2016-12-24 09:18:44 +0100

    (tortoisesvn) Original dtgm data

---
+++ automatic/tortoisesvn/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart ADDLOCAL=ALL'
+$urlArray = {{DownloadUrlx64}}
+$url = $urlArray[0]
+$checksum = '{{Checksum}}'
+$checksumType = 'sha256'
+$url64 = $urlArray[1]
+$checksum64 = '{{Checksumx64}}'
+$checksumType64 = 'sha256'
+$validExitCodes = @(0,3010)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -Url64bit "$url64" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType" `
+                          -Checksum64 "$checksum64" `
+                          -ChecksumType64 "$checksumType64"
---
+++ automatic/tortoisesvn/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = '{{PackageName}}'
+$packageSearch = "$packageName*"
+$installerType = 'msi'
+$silentArgs = '/quiet /qn /norestart'
+$validExitCodes = @(0)
+
+Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                          'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                 -ErrorAction:SilentlyContinue `
+| Where-Object   { $_.DisplayName -like $packageSearch } `
+| ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                               -FileType "$installerType" `
+                                               -SilentArgs "$($_.PSChildName) $silentArgs" `
+                                               -ValidExitCodes $validExitCodes }
