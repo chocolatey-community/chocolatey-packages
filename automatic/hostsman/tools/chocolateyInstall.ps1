75c0ec96436da86cadf78eef277011d77e250457
Chocolatey <chocolatey@realdimensions.net>
2017-01-03 18:11:10 +0000

    AU: 1 updated - hostsman [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/418f51c3f803d6a8536b211404e44c23f93a7fdf

--- automatic/hostsman/tools/chocolateyInstall.ps1
+++ automatic/hostsman/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'hostsman'
-  url            = 'http://hostsman2.it-mate.co.uk/HostsMan_4.6.103.zip'
-  checksum       = 'A9E5CCDF9734297517772DAD0BE19918EEF8A5837FD78DF8788766B78ECEA8DC'
+  url            = 'http://hostsman2.it-mate.co.uk/HostsMan_4.7.105.zip'
+  checksum       = 'b98f1155cda04e8a96cf29f6cc68497bdb28add7c8317cae79f6ec34c3bdc7fa'
   checksumType   = 'sha256'
   unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
 }
