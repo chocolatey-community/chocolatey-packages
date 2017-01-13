c4b6af3dc2fc2b9a431f3c85218535fc59a314ee
Chocolatey <chocolatey@realdimensions.net>
2017-01-13 00:10:38 +0000

    AU: 2 updated - bulk-crap-uninstaller git-lfs [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/6be7dffda0222ee63109d910e71be3b0b5dbd8a6

--- automatic/bulk-crap-uninstaller/tools/chocolateyInstall.ps1
+++ automatic/bulk-crap-uninstaller/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName            = 'bulk-crap-uninstaller'
   fileType               = 'exe'
-  url                    = 'https://github.com/Klocman/Bulk-Crap-Uninstaller/releases/download/v3.5/BCUninstaller_3.5_setup.exe'
-  url64bit               = 'https://github.com/Klocman/Bulk-Crap-Uninstaller/releases/download/v3.5/BCUninstaller_3.5_setup.exe'
-  checksum               = '215695407efcd5f2096f7a4e98d02ddfdc89fa1e2ebdd63468393620bd014270'
-  checksum64             = '215695407efcd5f2096f7a4e98d02ddfdc89fa1e2ebdd63468393620bd014270'
+  url                    = 'https://github.com/Klocman/Bulk-Crap-Uninstaller/releases/download/v3.6/BCUninstaller_3.6_setup.exe'
+  url64bit               = 'https://github.com/Klocman/Bulk-Crap-Uninstaller/releases/download/v3.6/BCUninstaller_3.6_setup.exe'
+  checksum               = '002e1ce966c4f6401f8387d208ff4d660fbd85cd66c013c8d56d9b80541e6ee1'
+  checksum64             = '002e1ce966c4f6401f8387d208ff4d660fbd85cd66c013c8d56d9b80541e6ee1'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
