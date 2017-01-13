0eab2fba065d5e22dddafbb6f67e67c9286edaf7
AdmiringWorm <kim.nordmo@gmail.com>
2017-01-13 03:16:22 +0100

    (git-lfs.install) Moved au_GetLatest to git-lfs\update.ps1 [AU git-lfs:1.5.5]

--- automatic/git-lfs.install/update.ps1
+++ automatic/git-lfs.install/update.ps1
@@ -1,18 +1,16 @@
-import-module au
-
-$releases = "https://github.com/github/git-lfs/releases"
+. $PSScriptRoot\..\git-lfs\update.ps1
 
 function global:au_BeforeUpdate {
   Remove-Item "$PSScriptRoot\tools\*.exe"
 
   $client = New-Object System.Net.WebClient
-  try 
+  try
   {
     $filePath = "$PSScriptRoot\tools\$($Latest.FileName)"
 
     $client.DownloadFile($Latest.URL32, "$filePath")
   }
-  finally 
+  finally
   {
     $client.Dispose()
   }
@@ -33,27 +31,8 @@ function global:au_SearchReplace {
             "(?i)(1\..+)\<.*\>" = "`${1}<$($Latest.URL32)>"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType)"
             "(?i)(checksum:\s+).*" = "`${1}$($Latest.Checksum)"
-        }        
+        }
      }
 }
 
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    #git-lfs-windows-1.4.4.exe
-    $re  = "git-lfs-windows-.+.exe"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
-
-    $url32 = "https://github.com" + $url
-
-    $filenameVersionPart = $url -split '-' | select -Last 1
-    $version = [IO.Path]::GetFileNameWithoutExtension($filenameVersionPart)
-
-    $filename = $url -split '/' | select -Last 1
-
-    return @{ URL32 = $url32; Version = $version; FileName = $filename }
-}
-
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
-}
+update -ChecksumFor none
--- automatic/git-lfs/update.ps1
+++ automatic/git-lfs/update.ps1
@@ -1,4 +1,6 @@
-. $PSScriptRoot\..\git-lfs.install\update.ps1
+import-module au
+
+$releases = "https://github.com/github/git-lfs/releases"
 
 function global:au_SearchReplace {
    @{
@@ -9,4 +11,23 @@ function global:au_SearchReplace {
     }
 }
 
-update -ChecksumFor none
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    #git-lfs-windows-1.4.4.exe
+    $re  = "git-lfs-windows-.+.exe"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $url32 = "https://github.com" + $url
+
+    $filenameVersionPart = $url -split '-' | select -Last 1
+    $version = [IO.Path]::GetFileNameWithoutExtension($filenameVersionPart)
+
+    $filename = $url -split '/' | select -Last 1
+
+    return @{ URL32 = $url32; Version = $version; FileName = $filename }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}
