06f842eefe467a37f7cbee7b83bddec831e223f7
AdmiringWorm <kim.nordmo@gmail.com>
2017-01-13 16:05:44 +0100

    (waterfox) Migrated and updated

---
+++ automatic/waterfox/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+if ((Get-ProcessorBits -Compare 32) -or $env:chocolateyForceX86 -eq $true) {
+  throw "Waterfox do not support 32bit (x86) installation."
+}
+
+$packageArgs = @{
+  packageName   = 'waterfox'
+  fileType      = 'exe'
+  softwareName  = 'Waterfox*'
+  file          = "$toolsDir\Waterfox 50.1.0 Setup_x64.exe"
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item $packageArgs.file -Force -ErrorAction SilentlyContinue
