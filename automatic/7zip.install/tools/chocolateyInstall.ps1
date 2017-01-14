fe4c5a42898f340d6d73302fb15f40d69981d096
AdmiringWorm <kim.nordmo@gmail.com>
2017-01-14 15:31:23 +0100

    (7zip.install) Updated install script and embedded installer
    fixes https://github.com/ferventcoder/chocolatey-packages/issues/198

--- automatic/7zip.install/tools/chocolateyInstall.ps1
+++ automatic/7zip.install/tools/chocolateyInstall.ps1
@@ -7,16 +7,34 @@
 
 $initialProcessCount = Get-ExplorerProcessCount
 Write-Warning "This installer is known to close the explorer process. This means `nyou may lose current work. `nIf it doesn't automatically restart explorer, type 'explorer' on the `ncommand shell to restart it."
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$versionMinusDots = "{{PackageVersion}}".Replace(".","")
-$packageId = '7zip.install'
-$url = "http://www.7-zip.org/a/7z$($versionMinusDots).exe"
-$url64 = "http://www.7-zip.org/a/7z$($versionMinusDots)-x64.exe"
+$filePath32 = "$toolsDir\7zip_x32.exe"
+$filePath64 = "$toolsDir\7zip_x64.exe"
 
-Install-ChocolateyPackage $packageId 'exe' '/S' $url $url64
+$filePath = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne $true) {
+  Write-Host "Installing 64 bit version" ; $filePath64
+} else { Write-Host "Installing 32 bit version" ; $filePath32 }
+
+$packageArgs = @{
+  packageName    = '7zip.install'
+  fileType       = 'exe'
+  softwareName   = '7-zip 16.04*'
+  file           = "$filePath"
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+# To prevent shimming of installers
+"" | Out-File "$filePath32.ignore"
+"" | Out-File "$filePath64.ignore"
+
+Install-ChocolateyInstallPackage @packageArgs
 
 $finalProcessCount = Get-ExplorerProcessCount
 if($initialProcessCount -lt $finalProcessCount)
 {
   Start-Process explorer.exe
 }
+
+Remove-Item "$filePath32*","$filePath64*" -Force -ErrorAction SilentlyContinue
