34a8befda99bf5d6a8b19d11d2c6ab0d009db128
Pascal Berger <pascal.berger@gmail.com>
2017-01-16 13:36:35 +0100

    (nodejs*) Add AU script

--- automatic/nodejs.install/tools/chocolateyInstall.ps1
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -1,13 +1,24 @@
-﻿$packageName = 'nodejs.install'
-$installerType = 'msi'
-$url = 'https://nodejs.org/dist/v{{PackageVersion}}/node-v{{PackageVersion}}-x86.msi'
-$url64 = 'https://nodejs.org/dist/v{{PackageVersion}}/node-v{{PackageVersion}}-x64.msi'
-$silentArgs = '/quiet'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$filePath32 = "$toolsPath\node-v7.4.0-x86.msi"
+$filePath64 = "$toolsPath\node-v7.4.0-x64.msi"
+$installFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+                      Write-Host "Installing 64 bit version"; $filePath64 }
+               else { Write-Host "Installing 32 bit version"; $filePath32 }
+
+$packageArgs = @{
+    PackageName    = 'nodejs.install'
+    FileType       = 'msi'
+    SoftwareName   = 'Node.js'
+    File           = $installFile
+    SilentArgs     = "/quiet"
+    ValidExitCodes = @(0)
+}
+Install-ChocolateyInstallPackage @packageArgs
 
 $nodePath = "$env:SystemDrive\Program Files\nodejs"
 if (![System.IO.Directory]::Exists($nodePath)) {$nodePath = "$env:SystemDrive\Program Files (x86)\nodejs";}
 
 $env:Path = "$($env:Path);$nodePath"
+
+Remove-Item -Force $filePath32, $filePath64 -ea 0
