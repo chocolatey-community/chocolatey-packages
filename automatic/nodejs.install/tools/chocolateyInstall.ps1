ba5adf020e09cd0e51107206ef3ec390035a6e90
Pascal Berger <pascal.berger@gmail.com>
2017-01-16 12:50:40 +0100

    (nodejs*) Original ferventcode data

---
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'nodejs.install'
+$installerType = 'msi'
+$url = 'https://nodejs.org/dist/v{{PackageVersion}}/node-v{{PackageVersion}}-x86.msi'
+$url64 = 'https://nodejs.org/dist/v{{PackageVersion}}/node-v{{PackageVersion}}-x64.msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
+$nodePath = "$env:SystemDrive\Program Files\nodejs"
+if (![System.IO.Directory]::Exists($nodePath)) {$nodePath = "$env:SystemDrive\Program Files (x86)\nodejs";}
+
+$env:Path = "$($env:Path);$nodePath"
---
+++ automatic/nodejs/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+﻿#Install-VirtualPackage 'nodejs.commandline' 'nodejs.install'
