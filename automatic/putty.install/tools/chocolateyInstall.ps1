d2b04699d5eeea60cf1d810a1e837320b534f388
Pascal Berger <pascal.berger@gmail.com>
2017-01-16 15:32:28 +0100

    (putty.install) Add AU script & change to embedded package

--- automatic/putty.install/tools/chocolateyInstall.ps1
+++ automatic/putty.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,17 @@
-﻿$packageName = 'putty.install'
-$fileType = 'exe'
-$silentArgs = '/VERYSILENT'
-$url = '{{DownloadUrl}}'
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$filePath = "$toolsPath\putty-0.67-installer.msi"
+
+$packageArgs = @{
+    PackageName    = "putty.install"
+    FileType       = "msi"
+    SoftwareName   = "PuTTY"
+    File           = $filePath
+    SilentArgs     = "/quiet"
+    ValidExitCodes = @(0)
+}
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force $filePath -ea 0
