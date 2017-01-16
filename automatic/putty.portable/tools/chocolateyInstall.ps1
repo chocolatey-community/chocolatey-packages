3757b1995e3f69b47e38ffd40d764b865eee05c2
Pascal Berger <pascal.berger@gmail.com>
2017-01-16 15:46:38 +0100

    (putty.portable) Add AU script & change to embedded package

--- automatic/putty.portable/tools/chocolateyInstall.ps1
+++ automatic/putty.portable/tools/chocolateyInstall.ps1
@@ -1,1 +1,13 @@
-﻿Install-ChocolateyZipPackage 'putty' '{{DownloadUrl}}'  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$filePath = "$toolsPath\putty.zip"
+
+$packageArgs = @{
+    PackageName  = "putty.portable"
+    FileFullPath = $filePath
+    Destination  = $toolsPath
+}
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item -force $filePath -ea 0
