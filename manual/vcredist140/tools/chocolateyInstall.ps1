e91c76ab339ec070626b52190ed38fa6ac9ba087
Jakub Bereżański <kuba@berezanscy.pl>
2017-01-23 00:00:28 +0100

    (vcredist140) signal SP1 requirement on Win7

--- manual/vcredist140/tools/chocolateyInstall.ps1
+++ manual/vcredist140/tools/chocolateyInstall.ps1
@@ -4,6 +4,14 @@ $installerType = 'exe'
 $silentArgs = '/quiet /norestart'
 $validExitCodes = @(0, 3010)
 
+$os = Get-WmiObject -Class Win32_OperatingSystem
+$version = [Version]$os.Version
+if ($version -ge [Version]'6.1' -and $version -lt [Version]'6.2' -and $os.ServicePackMajorVersion -lt 1)
+{
+    # On Windows 7, Service Pack 1 is required.
+    throw 'This package requires Service Pack 1 to be installed first. The "KB976932" package may be used to install it.'
+}
+
 Install-ChocolateyPackage -PackageName $packageName `
                           -FileType $installerType `
                           -SilentArgs $silentArgs `
