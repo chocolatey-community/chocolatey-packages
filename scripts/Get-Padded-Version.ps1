06dbbb405f47dafc3b6c5aab120a4d647c8003bd
AdmiringWorm <kim.nordmo@gmail.com>
2017-01-23 21:23:46 +0100

    (Get-Padded-Version) Added basic documentation

--- scripts/Get-Padded-Version.ps1
+++ scripts/Get-Padded-Version.ps1
@@ -1,9 +1,47 @@
+<#
+.SYNOPSIS
+  Pad the revision number if the passed version is using a 4-part version scheme.
+
+.DESCRIPTION
+  Adds padded zeroes to the revision part of the version if the passed
+  Version parameter is using a 4-part scheme and the Parameter OnlyBelowVersion
+  is either $null or greater than the Version parameter.
+
+.PARAMETER Version
+  The version number to add the revision padding to (Pre-Releases is not supported).
+
+.PARAMETER OnlyBelowVersion
+  Only add padded zeroes to a Version when it is below this parameter, or always
+  add padded zeroes if this parameter is $null.
+
+.PARAMETER RevisionLength
+  The total length of the revision number part
+
+.OUTPUTS
+  The full padded version, or the specified Version if padding was not needed.
+
+.EXAMPLE
+  Get-Padded-Version -Version '24.0.0.195'
+  will output '24.0.0.195000' if the nuspec version is less than the version
+
+.EXAMPLE
+  Get-Padded-Version -Version '24.0.0.195'
+  will output '24.0.0.195001' if the nuspec version is equal to '24.0.0.195'
+  or equal to '24.0.0.195000'
+
+.EXAMPLE
+  Get-Padded-Version -Version '24.0.1.2' -OnlyBelowVersion '24.0.1'
+  Will output the same version that was given without padded zeroes.
+#>
+
 function Get-Padded-Version() {
   param(
     [Parameter(Mandatory = $true, Position = 0)]
     [string]$Version,
     [Parameter(Position = 1)]
-    [version]$OnlyBelowVersion = $null
+    [version]$OnlyBelowVersion = $null,
+    [Parameter(Position = 2)]
+    [int]$RevisionLength = 6
   )
 
   if ($Version -match "^([\d]+\.){1,2}[\d]+$") {
@@ -22,7 +60,7 @@ function Get-Padded-Version() {
 
   $versionInfo = [version]$Version
   $revision = $versionInfo.Revision
-  $paddedRev = [int]($revision.ToString().PadRight(6, '0'));
+  $paddedRev = [int]($revision.ToString().PadRight($RevisionLength, '0'));
 
   if ($global:au_force -eq $true) {
     Write-Debug "Loading nuspec file to see if we need to pad the version"
@@ -30,7 +68,7 @@ function Get-Padded-Version() {
     $content | ? { $_ -match "\<version\>([\d\.]+)\<\/version\>" } | Out-Null
     if ($Matches) {
       $newVersion = [version]$Matches[1]
-      $paddedNewRev = [int]$newVersion.Revision.ToString().PadRight(6, '0')
+      $paddedNewRev = [int]$newVersion.Revision.ToString().PadRight($RevisionLength, '0')
       while ($paddedRev -le $paddedNewRev) {
         $paddedRev++ | Out-Null
       }
