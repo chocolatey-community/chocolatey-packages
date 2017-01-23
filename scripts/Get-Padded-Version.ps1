775fb4ea4f5858c4aced5d59c019290cc74f45e4
AdmiringWorm <kim.nordmo@gmail.com>
2017-01-23 17:22:22 +0100

    (build) Added script to pad revision numbers

---
+++ scripts/Get-Padded-Version.ps1
@@ -0,0 +1,41 @@
+function Get-Padded-Version() {
+  param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$Version,
+    [Parameter(Position = 1)]
+    [version]$MaximumVersion = $null
+  )
+
+  if ($Version -match "^([\d]+\.){1,2}[\d]+$") {
+    Write-Debug "There is no need to pad the version information"
+    return $Version
+  } elseif ($Version -notmatch "^[\d\.]+$") {
+    Write-Warning "Padding of pre release versions is not supported";
+    return $Version
+  }
+
+  $Matches = $null
+
+  if ($MaximumVersion -ne $null -and ([version]$Version) -ge $MaximumVersion) {
+    return $Version;
+  }
+
+  $versionInfo = [version]$Version
+  $revision = $versionInfo.Revision
+  $paddedRev = [int]($revision.ToString().PadRight(6, '0'));
+
+  if ($global:au_force -eq $true) {
+    Write-Debug "Loading nuspec file to see if we need to pad the version"
+    $content = gc -Encoding UTF8 (Resolve-Path "*.nuspec");
+    $content | ? { $_ -match "\<version\>([\d\.]+)\<\/version\>" } | Out-Null
+    if ($Matches) {
+      $newVersion = [version]$Matches[1]
+      $paddedNewRev = [int]$newVersion.Revision.ToString().PadRight(6, '0')
+      while ($paddedRev -le $paddedNewRev) {
+        $paddedRev++ | Out-Null
+      }
+    }
+  }
+
+  return $versionInfo.ToString() -replace '^([\d]+\.[\d]+\.[\d]+\.)\d+$',"`${1}$paddedRev"
+}
