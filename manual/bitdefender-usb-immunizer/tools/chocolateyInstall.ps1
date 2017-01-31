508bc37696af407407c197df19d4d22a97424a48
AdmiringWorm <kim.nordmo@gmail.com>
2017-01-31 20:14:12 +0100

    (bitdefender-usb-immunizer) Updated install script

--- manual/bitdefender-usb-immunizer/tools/chocolateyInstall.ps1
+++ manual/bitdefender-usb-immunizer/tools/chocolateyInstall.ps1
@@ -1,6 +1,20 @@
-﻿$packageName = 'bitdefender-usb-immunizer'
-$url = '{{DownloadUrl}}'
-$fileFullPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\BDUSBImmunizerLauncher.exe"
+﻿$ErrorActionPreference = 'Stop';
 
-Get-ChocolateyWebFile $packageName $fileFullPath $url
-Install-ChocolateyDesktopLink $fileFullPath
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = 'bitdefender-usb-immunizer'
+  fileFullPath   = "$toolsPath\BDUSBImmunizerLauncher.exe"
+  url            = 'https://labs.bitdefender.com/wp-content/plugins/download-monitor/download.php?id=BDUSBImmunizerLauncher.exe'
+  checksum       = '47AC96402869B71BF2CBFFC5A72F6251289C59350F7227B5DDAE7039993E8361'
+  checksumType   = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$fileName = [System.IO.Path]::GetFileNameWithoutExtension($packageArgs.fileFullPath)
+
+Install-ChocolateyShortcut `
+  -ShortcutFilePath "$desktop\$fileName.lnk" `
+  -TargetPath $packageArgs.fileFullPath
