31143129b88c4eacd26f6c0031ef1af20155d473
AdmiringWorm <kim.nordmo@gmail.com>
2017-02-01 02:07:01 +0100

    (ietester) Updated install script

--- automatic/ietester/tools/chocolateyInstall.ps1
+++ automatic/ietester/tools/chocolateyInstall.ps1
@@ -1,6 +1,14 @@
-﻿$packageName = '{{PackageName}}'
-$fileType = 'exe'
-$silentArgs = '/S'
-$url = '{{DownloadUrl}}'
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$packageArgs = @{
+  packageName    = 'ietester'
+  fileType       = 'exe'
+  url            = 'http://www.my-debugbar.com/ietester/install-ietester-v0.5.4.exe'
+  softwareName   = 'IETester*'
+  checksum       = 'bf3b91b5e5a0c1e799636a0d2d9cf0991a7b6a00a2fed2eaebe2678b9d0655e4'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
