0301ed92ad353d6a1de3a7af83ea0ab58a421d7a
AdmiringWorm <kim.nordmo@gmail.com>
2017-02-02 17:37:01 +0100

    (firefoxesr) Migrated to Core repo

---
+++ automatic/FirefoxESR/tools/chocolateyUninstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'FirefoxESR'
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName 'Mozilla Firefox*ESR*'
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs = @{
+      packageName = $packageName
+      fileType    = 'exe'
+      silentArgs  = '-ms'
+      validExitCodes= @(0)
+      file          = "$($_.UninstallString.Trim('"'))"
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+
+    Write-Warning "Auto Uninstaller may detect Mozilla Maintenance Service."
+    Write-Warning "This should not be uninstalled if any other Mozilla product is installed."
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
