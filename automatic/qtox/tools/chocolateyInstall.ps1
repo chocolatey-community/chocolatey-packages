5207219dbf5fa59eeeea655745b58a1b22a172d6
Pascal Berger <pascal.berger@gmail.com>
2017-02-02 23:45:06 +0100

    (qtox) Update AU script and change to an embedded package

--- automatic/qtox/tools/chocolateyInstall.ps1
+++ automatic/qtox/tools/chocolateyInstall.ps1
@@ -1,19 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$filePath32 = "$toolsPath\qtox_x86.exe"
+$filePath64 = "$toolsPath\qtox_x64.exe"
+
+$filePath = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne $true) {
+  Write-Host "Installing 64-bit version" ; $filePath64
+} else { Write-Host "Installing 32-bit version" ; $filePath32 }
+
 $packageArgs = @{
   packageName    = 'qtox'
   fileType       = 'exe'
   softwareName   = 'qTox'
-
-  checksum       = ''
-  checksum64     = ''
-  checksumType   = ''
-  checksumType64 = ''
-  url            = ''
-  url64          = ''
-
+  file           = $filePath
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -ea 0 $filePath32,$filePath64,"$filePath32.ignore","$filePath64.ignore"
--- automatic/qtox/update.ps1
+++ automatic/qtox/update.ps1
@@ -1,34 +1,72 @@
 ﻿import-module au
 
-$domain   = 'https://github.com'
-$releases = "$domain/qTox/qTox/releases/latest"
+$releases = "https://github.com/qTox/qTox/blob/master/README.md"
+$changelog = "https://github.com/qTox/qTox/blob/master/CHANGELOG.md"
+$softwareName = 'qTox'
+
+function global:au_BeforeUpdate {
+  $Latest.ChecksumType32 = 'sha256'
+  $toolsDir = "$PSScriptRoot\tools"
+  Remove-Item -Force "$toolsDir\*.exe" -ea 0
+
+  $Latest.FileName32 = "$($Latest.PackageName)_x86.exe"
+  Get-WebFile $Latest.URL32 "$toolsDir\$($Latest.FileName32)"
+
+  $Latest.FileName64 = "$($Latest.PackageName)_x64.exe"
+  Get-WebFile $Latest.URL64 "$toolsDir\$($Latest.FileName64)"
+
+  $Latest.Checksum32 = Get-FileHash "$toolsDir\$($Latest.FileName32)" -Algorithm $Latest.ChecksumType32 | ForEach-Object Hash
+  $Latest.Checksum64 = Get-FileHash "$toolsDir\$($Latest.FileName64)" -Algorithm $Latest.ChecksumType32 | ForEach-Object Hash
+}
 
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-      "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    ".\legal\VERIFICATION.txt"        = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum(64)?\:).*"       = "`${1} $($Latest.Checksum64)"
+    }
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)(^\s*packageName\s*=\s*)'.*'"  = "`${1}'$($Latest.PackageName)'"
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+      "(?i)(^[$]filePath32\s*=\s*`"[$]toolsPath\\)[^`"]*`""= "`${1}$($Latest.FileName32)`""
+      "(?i)(^[$]filePath64\s*=\s*`"[$]toolsPath\\)[^`"]*`""= "`${1}$($Latest.FileName64)`""
+    }
+    ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)(^[$]packageName\s*=\s*)'.*'"  = "`${1}'$($Latest.PackageName)'"
+      "(?i)(\-SoftwareName\s+)'.*'"       = "`${1}'$softwareName'"
     }
   }
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re    = '\.exe$'
-  $urls   = $download_page.links | ? href -match $re | select -First 2 -expand href
+  # https://build.tox.chat/view/qtox/job/qTox_pkg_windows_x86_stable_release/lastSuccessfulBuild/artifact/setup-qtox.exe
+  $re32 = '.+_x86_.+\.exe$'
+  $url32 = $download_page.links | Where-Object href -match $re32 | Select-Object -First 1 -expand href
 
-  $version  = ($urls[0] -split '/' | select -Last 1 -Skip 1).TrimStart('v')
+  # https://build.tox.chat/view/qtox/job/qTox_pkg_windows_x86-64_stable_release/lastSuccessfulBuild/artifact/setup-qtox.exe
+  $re64 = '.+_x86-64_.+\.exe$'
+  $url64 = $download_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href
 
+  $download_page = Invoke-WebRequest -Uri $changelog
+  $reVersion = '<h2><a.+/a>v(.+) \(.+\)</h2>'
+  $download_page.Content -match $reVersion
+
+  if (!$Matches) {
+    throw "qtox version not found on $releases"
+  }
+
+  $version = $Matches[1];
   @{
-    URL32 = $domain + ($urls -notmatch "64" | select -first 1)
-    URL64 = $domain + ($urls -match "64" | select -first 1)
+    URL32 = $url32
+    URL64 = $url64
     Version = $version
   }
 }
 
-update
+update -ChecksumFor none
