52813cfdb1a2c1488408dfcfd9cdf777ad55a705
Pascal Berger <pascal.berger@gmail.com>
2017-02-02 22:44:30 +0100

    (putty*) Update AU script for changes on website and make more robust

--- automatic/putty.portable/update.ps1
+++ automatic/putty.portable/update.ps1
@@ -1,6 +1,4 @@
-import-module au
-
-$releases = "http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html"
+. $PSScriptRoot\..\putty\update.ps1
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if the script is not sourced
   function global:au_BeforeUpdate {
@@ -9,8 +7,8 @@ if ($MyInvocation.InvocationName -ne '.') { # run the update only if the script 
     $client = New-Object System.Net.WebClient
     try
     {
-      $filePath32 = "$PSScriptRoot\tools\$($Latest.FileName32)"
-      $client.DownloadFile($Latest.URL32, "$filePath32")
+      $filePath32 = "$PSScriptRoot\tools\$($Latest.FileName32Portable)"
+      $client.DownloadFile($Latest.URL32Portable, "$filePath32")
     }
     finally
     {
@@ -26,38 +24,15 @@ function global:au_SearchReplace {
   @{
     ".\legal\VERIFICATION.txt" = @{
       "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(1\..+)\<.*\>"        = "`${1}<$($Latest.URL32)>"
+      "(?i)(1\..+)\<.*\>"        = "`${1}<$($Latest.URL32Portable)>"
       "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType)"
       "(?i)(checksum:).*"        = "`${1} $($Latest.Checksum)"
     }
     'tools\chocolateyInstall.ps1' = @{
       "(PackageName\s*=\s*)`"([^*]+)`"" = "`$1`"$($Latest.PackageName)`""
-      "(^[$]filePath\s*=\s*`"[$]toolsPath\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+      "(^[$]filePath\s*=\s*`"[$]toolsPath\\)(.*)`"" = "`$1$($Latest.FileName32Portable)`""
     }
   }
 }
 
-function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases
-
-  $versionString = $download_page.ParsedHtml.body.getElementsByClassName('buildtype') | Select-Object -first 1 -expand innerHtml
-  $reVersion = ".*\((.*)\)"
-  $versionString -match $reVersion | Out-Null
-  $version = $matches[1] -replace "beta ", ""
-
-  #https://the.earth.li/~sgtatham/putty/latest/x86/putty.zip
-  $reInstaller  = "putty.zip"
-  $url32 = $download_page.links | Where-Object href -match $reInstaller | Select-Object -First 1 -expand href
-
-  $filename32 = [IO.Path]::GetFileName($url32)
-
-  return @{
-    URL32 = $url32
-    FileName32 = $filename32
-    Version = $version
-  }
-}
-
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-  update -ChecksumFor none
-}
+update -ChecksumFor none
--- automatic/putty/update.ps1
+++ automatic/putty/update.ps1
@@ -1,4 +1,6 @@
-﻿. $PSScriptRoot\..\putty.portable\update.ps1
+﻿import-module au
+
+$releases = "http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html"
 
 function global:au_SearchReplace {
   @{
@@ -8,4 +10,30 @@ function global:au_SearchReplace {
   }
 }
 
-update -ChecksumFor none
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  #https://the.earth.li/~sgtatham/putty/latest/x86/putty-0.67-installer.msi
+  $reInstaller  = "putty-(.+)-installer.msi"
+  $url32Installer = $download_page.links | Where-Object href -match $reInstaller | Select-Object -First 1 -expand href
+  $version = $matches[1] 
+  $filename32Installer = [IO.Path]::GetFileName($url32Installer)
+
+  #https://the.earth.li/~sgtatham/putty/latest/x86/putty.zip
+  $rePortable  = "putty.zip"
+  $url32Portable = $download_page.links | Where-Object href -match $rePortable | Select-Object -First 1 -expand href
+  $filename32Portable = [IO.Path]::GetFileName($url32Portable)
+
+
+  return @{
+    URL32Installer = $url32Installer
+    FileName32Installer = $filename32Installer
+    URL32Portable = $url32Portable
+    FileName32Portable = $filename32Portable
+    Version = $version
+  }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+  update -ChecksumFor none
+}
