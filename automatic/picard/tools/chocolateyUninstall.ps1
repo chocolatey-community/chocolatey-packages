157ed53e2ce671165848b5a99d8061dcae910e6b
AdmiringWorm <kim.nordmo@gmail.com>
2017-02-03 15:45:31 +0100

    Fixed missing Write-Warning [AU djv picard pyhoca-gui]
    fixes #562

--- automatic/picard/tools/chocolateyUninstall.ps1
+++ automatic/picard/tools/chocolateyUninstall.ps1
@@ -22,7 +22,7 @@ if ($key.Count -eq 1) {
   Write-Warning "$packageName has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
-  To prevent accidental data loss, no programs will be uninstall."
-  Please alert the package maintainer the following keys were matched:"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstall."
+  Write-Warning "Please alert the package maintainer the following keys were matched:"
   $key | % { Write-Warning "- $($_.DisplayName)" }
 }
