ad0bc8d0999896bf771e8aea873325ef4644d0d6
Chocolatey <chocolatey@realdimensions.net>
2017-02-03 21:30:48 +0000

    AU: 1 updated - wesnoth [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/83ad567c05f880e5eaeeb564fa1d3d90a6867d78

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.12/wesnoth-1.12/wesnoth-1.12-win32.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.12/wesnoth-1.12.6/wesnoth-1.12.6-win32.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = 'e9b804eb6b1075b3104d13e679ff5e145ee82afd2352154641245263c04a39dc'
+  checksum       = '340a045deafcc236d2e00cb47598b4a5e8d656463270a3433ed637108916db71'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
