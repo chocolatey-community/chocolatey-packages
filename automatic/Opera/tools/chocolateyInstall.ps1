9daf1891717e45be88b6b50c219c60449adce895
Chocolatey <chocolatey@realdimensions.net>
2017-02-06 18:11:12 +0000

    AU: 2 updated - Opera poweriso [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/f913e4461afbfb76339c0aae1f3f1d9023616257

--- automatic/Opera/tools/chocolateyInstall.ps1
+++ automatic/Opera/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'Opera'
-$url32       = 'http://get.geo.opera.com/pub/opera/desktop/42.0.2393.94/win/Opera_42.0.2393.94_Setup.exe'
-$checksum32  = 'ef6afaeea15640b7bdbd064d00d2e2608346f0c10361c0f4a053f66673a85359'
-$version     = '42.0.2393.94'
+$url32       = 'http://get.geo.opera.com/pub/opera/desktop/43.0.2442.806/win/Opera_43.0.2442.806_Setup.exe'
+$checksum32  = 'd9ff04df950464bb3357dad2337972e699fa2243775105c7f5a6c21a85326e52'
+$version     = '43.0.2442.806'
 
 $packageArgs = @{
   packageName    = $packageName
