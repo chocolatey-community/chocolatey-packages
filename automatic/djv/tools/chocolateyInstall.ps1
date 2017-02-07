8d42d41461681d6a1643baf23e01a20621699132
AdmiringWorm <kim.nordmo@gmail.com>
2017-02-07 22:28:02 +0100

    (djv) Updated package to latest edition

--- automatic/djv/tools/chocolateyInstall.ps1
+++ automatic/djv/tools/chocolateyInstall.ps1
@@ -2,17 +2,13 @@
 
 $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$filePath32 = "$toolsPath\djv-1.0.5-Windows-32.exe"
-$filePath64 = "$toolsPath\djv-1.0.5-Windows-64.exe"
-
-$filePath = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne $true) {
-  Write-Host "Installing 64-bit version" ; $filePath64
-} else { Write-Host "Installing 32-bit version" ; $filePath32 }
+if ((Get-ProcessorBits 32) -or $env:chocolateyForceX86 -eq $true)
+  { Write-Error "32-bit is no longer supported. Please install version 1.0.5.20170203" }
 
 $packageArgs = @{
   packageName    = 'djv'
   fileType       = 'exe'
-  file           = $filePath
+  file           = "$toolsPath\djv-1.1.0-Windows-64.exe"
   softwareName   = 'djv-*'
   silentArgs     = '/S'
   validExitCodes = @(0)
@@ -20,4 +16,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-Remove-Item -Force -ea 0 $filePath32,$filePath64,"$filePath32.ignore","$filePath64.ignore"
+Remove-Item -Force -ea 0 $packageArgs.file,"$($packageArgs.file).ignore"
--- automatic/djv/update.ps1
+++ automatic/djv/update.ps1
@@ -2,43 +2,35 @@
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
 # Temporary to allow pushing version 1.0.5
-$releases     = 'https://sourceforge.net/projects/djv/files/djv-stable/1.0.5/'
-#$releases     = 'http://djv.sourceforge.net/index.html'
+$releases     = 'http://djv.sourceforge.net/index.html'
 $softwareName = 'djv-*'
 
 function global:au_BeforeUpdate {
-  $Latest.ChecksumType32 = 'sha256'
+  $Latest.ChecksumType64 = 'sha256'
   $toolsDir = "$PSScriptRoot\tools"
   Remove-Item -Force "$toolsDir\*.exe" -ea 0
 
-  $fileName32 = Get-WebFileName $Latest.URL32 "$($Latest.PackageName)_x86.exe"
-  $fileName64 = Get-WebFileName $Latest.URL64 "$($Latest.PackageName)_x64.exe"
+  $fileName64 = Get-WebFileName $Latest.URL64 "$($Latest.PackageName).exe"
 
-  Get-WebFile $Latest.URL32 "$toolsDir\$fileName32"
   Get-WebFile $Latest.URL64 "$toolsDir\$fileName64"
 
-  $Latest.FileName32 = $fileName32
   $Latest.FileName64 = $fileName64
 
-  $Latest.Checksum32 = Get-FileHash "$toolsDir\$fileName32" -Algorithm $Latest.ChecksumType32 | % Hash
-  $Latest.Checksum64 = Get-FileHash "$toolsDir\$fileName64" -Algorithm $Latest.ChecksumType32 | % Hash
+  $Latest.Checksum64 = Get-FileHash "$toolsDir\$fileName64" -Algorithm $Latest.ChecksumType64 | % Hash
 }
 
 function global:au_SearchReplace {
   @{
     ".\legal\VERIFICATION.txt"        = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(\s*1\..*)\<.*\>"              = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
       "(?i)(^\s*checksum(64)?\:).*"       = "`${1} $($Latest.Checksum64)"
     }
     ".\tools\chocolateyInstall.ps1"   = @{
       "(?i)(^\s*packageName\s*=\s*)'.*'"  = "`${1}'$($Latest.PackageName)'"
       "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
-      "(?i)(^[$]filePath32\s*=\s*`"[$]toolsPath\\)[^`"]*`""= "`${1}$($Latest.FileName32)`""
-      "(?i)(^[$]filePath64\s*=\s*`"[$]toolsPath\\)[^`"]*`""= "`${1}$($Latest.FileName64)`""
+      "(?i)(`"[$]toolsPath\\)[^`"]*`""    = "`${1}$($Latest.FileName64)`""
     }
     ".\tools\chocolateyUninstall.ps1" = @{
       "(?i)(^[$]packageName\s*=\s*)'.*'"  = "`${1}'$($Latest.PackageName)'"
@@ -49,9 +41,6 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re = '32\.exe\/download$'
-  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
-
   $re = '64\.exe\/download$'
   $url64 = $download_page.Links | ? href -match $re | select -first 1 -expand href
 
@@ -60,7 +49,6 @@ function global:au_GetLatest {
 
 
   @{
-    URL32   = $url32
     URL64   = $url64
     Version = $version
   }
