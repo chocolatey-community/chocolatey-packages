bb3563e7da3bc1bc7ba2d225035f29a93ece30c0
Chocolatey <chocolatey@realdimensions.net>
2017-02-09 17:16:41 +0000

    AU: 2 updated - fiddler4 palemoon [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/74b4247a30a9f04151963d937c179f123c9efdd3

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'http://relmirror.palemoon.org/release/palemoon-27.0.3.win32.installer.exe'
-  url64         = 'http://relmirror.palemoon.org/release/palemoon-27.0.3.win64.installer.exe'
+  url           = 'http://relmirror.palemoon.org/release/palemoon-27.1.0.win32.installer.exe'
+  url64         = 'http://relmirror.palemoon.org/release/palemoon-27.1.0.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '14fab6abe9eb9cf4d55e54f230a85ddfeedfa51cb66a602a29228a557afdac6d'
+  checksum      = '24115d11a6d66fb9af1eaf1ee025ef6020e2c1eb148f17109cda588c1ffb82d3'
   checksumType  = 'sha256'
-  checksum64    = 'fa869f9adb7d513cd2933e745b8d6f2173a97440e7c1036a0a8ce84e6414858a'
+  checksum64    = 'c9f55687949e7d6c8b633ba8b246939afefda7f4e10104306c63623aea6992ea'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
