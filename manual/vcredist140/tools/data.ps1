37b426692a57e8f668f59a58d8e95e9b7ca6ee05
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-09 20:01:41 +0100

    (vcredist140) update to VS 2015 Update 1

--- manual/vcredist140/tools/data.ps1
+++ manual/vcredist140/tools/data.ps1
@@ -1,12 +1,12 @@
 ﻿$installData32 = @{
-    Url = 'https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x86.exe'
-    Checksum = 'FDD1E1F0DCAE2D0AA0720895EFF33B927D13076E64464BB7C7E5843B7667CD14'
+    Url = 'https://download.microsoft.com/download/C/E/5/CE514EAE-78A8-4381-86E8-29108D78DBD4/VC_redist.x86.exe'
+    Checksum = '4FF07492947C3E52607AA8DE0C241898AA35C439C442DE1CEA5D17DE5B7C7F01'
     ChecksumType = 'sha256'
 }
 
 $installData64 = @{
-    Url64 = 'https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe'
-    Checksum64 = '5EEA714E1F22F1875C1CB7B1738B0C0B1F02AEC5ECB95F0FDB1C5171C6CD93A3'
+    Url64 = 'https://download.microsoft.com/download/C/E/5/CE514EAE-78A8-4381-86E8-29108D78DBD4/VC_redist.x64.exe'
+    Checksum64 = '4504CD3BBCCDD7EFB9EC68261D57A04C2C8D704812C56856F4786AC90F7F00A8'
     ChecksumType64 = 'sha256'
 }
 
@@ -15,7 +15,7 @@ $uninstallData = @{
 }
 
 $otherData = @{
-    ThreePartVersion = [version]'14.0.23026'
+    ThreePartVersion = [version]'14.0.23506'
     FamilyRegistryKey = '14.0'
     PackageName = 'vcredist140'
 }
