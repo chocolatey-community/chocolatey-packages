5015eabd64c9ab684ab6fb79b9df876b926d1c62
Jakub Bereżański <kuba@berezanscy.pl>
2017-02-10 16:39:19 +0100

    (vcredist140) update to VS 2017 RC 1

--- manual/vcredist140/tools/data.ps1
+++ manual/vcredist140/tools/data.ps1
@@ -1,21 +1,21 @@
 ﻿$installData32 = @{
-    Url = 'https://download.microsoft.com/download/4/5/6/456cf79a-4046-4232-8e6f-7cf3d8075d9a/vc_redist.x86.exe'
-    Checksum = 'A8A60D9C23AE708FEA0D7D757E4757EDAFEB80BCC0596A54D8C5DF426701352F'
+    Url = 'https://download.microsoft.com/download/e/6/6/e66c5871-7afe-4640-a454-786eabe8aae8/vc_redist.x86.exe'
+    Checksum = 'FDCCB907365A76E94B9DDC36538A35709688528F03283047F660AA2CB9BCBB1A'
     ChecksumType = 'sha256'
 }
 
 $installData64 = @{
-    Url64 = 'https://download.microsoft.com/download/8/5/e/85edb843-93af-4daa-ad1e-c33dfa95b2ea/vc_redist.x64.exe'
-    Checksum64 = 'E8E1896B0E0F608E183B90D4CBC086AF638067C57C0368BB20E49E209DCA678F'
+    Url64 = 'https://download.microsoft.com/download/7/2/5/72572684-052f-4aa9-9170-9d40813a87be/vc_redist.x64.exe'
+    Checksum64 = 'DBD2A04C202214181E2D7E0618F4B3B88DDFE690C97338A45CA3038DAA4ADBA2'
     ChecksumType64 = 'sha256'
 }
 
 $uninstallData = @{
-    SoftwareName = 'Microsoft Visual C++ 2015 Redistributable*'
+    SoftwareName = 'Microsoft Visual C++ vNext Redistributable*'
 }
 
 $otherData = @{
-    ThreePartVersion = [version]'14.0.24516'
+    ThreePartVersion = [version]'14.10.24629'
     FamilyRegistryKey = '14.0'
     PackageName = 'vcredist140'
 }
