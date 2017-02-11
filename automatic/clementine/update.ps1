587ca110f86ecb1911297aa890853749500a870e
AdmiringWorm <kim.nordmo@gmail.com>
2017-02-11 08:57:21 +0100

    (clementine) Cleaned up install and update script

--- automatic/clementine/update.ps1
+++ automatic/clementine/update.ps1
@@ -3,21 +3,21 @@ import-module au
 $releases = 'https://www.clementine-player.org/downloads'
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)^(\s*url\s*=\s*)'.*'"          = "`${1}'$($Latest.URL32)'"
+      "(?i)^(\s*checksum\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum32)'"
     }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re      = '\.exe$'
-    $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $version = Split-Path (Split-Path $url) -Leaf
-    @{ URL32 = $url; Version = $version }
+  $re      = '\.exe$'
+  $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
+  $version = Split-Path (Split-Path $url) -Leaf
+  @{ URL32 = $url; Version = $version }
 }
 
 update -ChecksumFor 32
