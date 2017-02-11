73208bc77b269e5a58019c6a3d70aee8fb7c3c63
AdmiringWorm <kim.nordmo@gmail.com>
2017-02-11 09:45:35 +0100

    (clemetine) Changed to embedded package

--- automatic/clementine/tools/chocolateyInstall.ps1
+++ automatic/clementine/tools/chocolateyInstall.ps1
@@ -1,26 +1,27 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$filePath = "$toolsPath\ClementineSetup-1.3.1.exe"
 $packageName = 'clementine'
 
 $packageArgs = @{
   packageName    = $packageName
-  fileType       = 'EXE'
-  url            = 'https://github.com/clementine-player/Clementine/releases/download/1.3.1/ClementineSetup-1.3.1.exe'
-  checksum       = '8ebf4808de874c0fe6a71a5953a3d302cb6348e6ca45dcc268fb4e5c641eddf0'
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
+  fileType       = 'exe'
+  file           = $filePath
+  softwareName   = $packageName
   silentArgs     = '/S'
   validExitCodes = @(0)
-  softwareName   = $packageName
 }
-Install-ChocolateyPackage @packageArgs
 
-$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
-if ($installLocation)  {
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item -Force -ea 0 $filePath,"$filePath.ignore"
+
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+if ($installLocation) {
   Write-Host "$packageName installed to '$installLocation'"
   Register-Application "$installLocation\$packageName.exe"
   Write-Host "$packageName registered as $packageName"
-}
-else {
+} else {
   Write-Warning "Can't find $PackageName install location"
 }
--- automatic/clementine/update.ps1
+++ automatic/clementine/update.ps1
@@ -1,23 +1,39 @@
-import-module au
+﻿Import-Module AU
 
 $releases = 'https://www.clementine-player.org/downloads'
 
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge
+  $toolsDir = "$PSScriptRoot\tools"
+  $fileName = [System.IO.Path]::GetFileName($Latest.FileName32)
+  $newFileName = $fileName -replace '_x32',''
+  Move-Item "$toolsDir\$($Latest.FileName32)" "$toolsDir\$newFileName"
+  $Latest.FileName32 = $newFileName
+}
+
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1"   = @{
-      "(?i)^(\s*url\s*=\s*)'.*'"          = "`${1}'$($Latest.URL32)'"
-      "(?i)^(\s*checksum\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum32)'"
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+    }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^[$]filePath\s*=\s*`"[$]toolsPath\\)[^`"]*`"" = "`${1}$($Latest.FileName32)`""
     }
   }
 }
-
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
   $re      = '\.exe$'
   $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
   $version = Split-Path (Split-Path $url) -Leaf
-  @{ URL32 = $url; Version = $version }
+  @{
+    URL32 = $url
+    Version = $version
+  }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
