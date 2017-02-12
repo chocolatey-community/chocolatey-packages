8c40a6b411d2f519b3fe71110a3c8db60c1367bf
Kim Nordmo <AdmiringWorm@users.noreply.github.com>
2017-02-12 10:31:56 +0100

    (clementine) Changed to embedded package (#578) [AU clementine]
    * (clementine) Added 'UseBasicParsing'
    
    * (clementine) Updated metadata
    
    * (clementine) Cleaned up install and update script
    
    * (clemetine) Changed to embedded package
    
    * (clementine) Added uninstall script

---
+++ automatic/clementine/tools/chocolateyUninstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'clementine'
+
+$uninstalled = $false
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageName
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs = @{
+      packageName = $packageName
+      fileType    = 'exe'
+      silentArgs  = '/S'
+      file        = "$($_.UninstallString)"
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+
+    $regKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\clementine.exe'
+    if (Test-Path $regKey) {
+      Remove-Item $regKey -Force -ea 0
+    }
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstall."
+  Write-Warning "Please alert the package maintainer the following keys were matched:"
+  $key | % { Write-Warning "- $($_.DisplayName)" }
+}
