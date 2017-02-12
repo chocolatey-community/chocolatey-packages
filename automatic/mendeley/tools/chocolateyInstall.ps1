ab988d6acf24f847ceb29f9515f052e5aec26b43
Pascal Berger <pascal.berger@gmail.com>
2017-02-12 22:16:56 +0100

    (mendeley) Make fixes to be conform to guidelines

--- automatic/mendeley/tools/chocolateyInstall.ps1
+++ automatic/mendeley/tools/chocolateyInstall.ps1
@@ -1,22 +1,25 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName            = 'mendeley'
-  fileType               = 'EXE'
-  url                    = ''
-  checksum               = ''
-  checksumType           = 'sha256'
-  silentArgs             = '/S'
-  validExitCodes         = @(0)
-  softwareName           = 'Mendeley Desktop'
+  packageName    = 'mendeley'
+  fileType       = 'EXE'
+  url            = ''
+  checksum       = ''
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'Mendeley Desktop'
 }
 Install-ChocolateyPackage @packageArgs
 
 $packageName = $packageArgs.packageName
 $installLocation = Get-AppInstallLocation $packageName
-if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+if (!$installLocation) { 
+  Write-Warning "Can't find $packageName install location"
+  return 
+}
 Write-Host "$packageName installed to '$installLocation'"
 
 Register-Application "$installLocation\$packageName.exe"
