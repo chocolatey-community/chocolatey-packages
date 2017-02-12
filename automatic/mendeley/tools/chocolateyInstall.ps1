4c7b68edb2f1db5d189b220fa21221fb7a9e2704
Sander Steenhuis <redsandro@gmail.com>
2017-02-12 20:49:56 +0100

    Added mendeley

---
+++ automatic/mendeley/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName            = 'mendeley'
+  fileType               = 'EXE'
+  url                    = ''
+  checksum               = ''
+  checksumType           = 'sha256'
+  silentArgs             = '/S'
+  validExitCodes         = @(0)
+  softwareName           = 'Mendeley Desktop'
+}
+Install-ChocolateyPackage @packageArgs
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
+
+Register-Application "$installLocation\$packageName.exe"
+Write-Host "$packageName registered as $packageName"
---
+++ automatic/mendeley/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://www.mendeley.com/release-notes/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+	$url = $download_page.Content -match 'Release Notes for Mendeley Desktop v([1-9\.]+)'
+	$version = $matches[1]
+	$url = "http://desktop-download.mendeley.com/download/Mendeley-Desktop-${version}-win32.exe"
+
+    @{
+        Version      = $version
+        URL32        = $url
+    }
+}
+
+update -ChecksumFor 32
