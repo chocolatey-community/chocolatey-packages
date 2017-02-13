da77344aeb0abce4b72031ec30c3f56504a7984e
Pascal Berger <pascal.berger@gmail.com>
2017-02-13 11:05:30 +0100

    (sourcetree-disableautoupdate) Add package to disable auto-update of sourcetree

---
+++ manual/sourcetree-disableautoupdate/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Disable auto update
+New-Item 'HKCU:\Software\Caphyon\Advanced Updater\{138B87CA-4905-43FE-BB63-184FFB0CE1EC}\Settings' -Force | New-ItemProperty -Name AutoUpdatePolicy -Value 0 -Force
