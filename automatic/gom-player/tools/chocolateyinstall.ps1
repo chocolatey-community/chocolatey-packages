63d7c7aa61dae82e1271150d983e5b33a12685f1
AdmiringWorm <kim.nordmo@gmail.com>
2017-02-13 21:34:16 +0100

    (gom-player) Added new package

---
+++ automatic/gom-player/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = 'https://cdn.gomlab.com/gretech/player/new/GOMPLAYERGLOBALSETUP.EXE'
+  softwareName   = 'GOM Player'
+  checksum       = '5f36c373ad830fdc8bd813d2265f9f20377a4fae0de57dcba537b90d18698145'
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
