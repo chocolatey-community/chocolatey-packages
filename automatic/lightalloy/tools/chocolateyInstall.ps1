a42e18ce78d949a90be5253c58adf52bee14f10a
Sander Steenhuis <redsandro@gmail.com>
2017-02-14 15:50:32 +0100

    Added lightalloy

---
+++ automatic/lightalloy/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'lightalloy'
+$url32 = ''
+$checksum32  = ''
+
+$pwd			= "$(split-path -parent $MyInvocation.MyCommand.Definition)"
+$au3			= Join-Path $pwd 'lightalloy.au3'
+
+# Need some AutoIt3 wizardry because installer is not silent
+$chocTempDir	= Join-Path $env:TEMP "chocolatey"
+$tempDir		= Join-Path $chocTempDir "$packageName"
+if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+
+$tempFile		= Join-Path $tempDir "$packageName.installer.exe"
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileFullPath   = $tempFile
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+
+Write-Output "Running AutoIt3 using `'$au3`'"
+Start-ChocolateyProcessAsAdmin "/c AutoIt3.exe `"$au3`" `"$tempFile`"" 'cmd.exe'
---
+++ automatic/lightalloy/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'http://www.light-alloy.ru/download/'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+      "(?i)(^\s*url32\s*=\s*)('.*')" = "`$1'$($Latest.Url32)'"
+      "(?i)(^\s*checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases
+  $url = $download_page.Links | ? href -match "LA_Setup_v[0-9\.]+\.exe$" | % href | select -First 1
+  $version = $url -split 'v' | select -First 1 -Skip 1
+  $version = $version -split '.exe' | select -First 1
+
+  @{
+    Version = $version
+    Url32   = $url32
+  }
+}
+
+update -NoCheckUrl -ChecksumFor 32
