b61878e399e23cd916c11c2a0f7531c341b8ed91
Sander Steenhuis <redsandro@gmail.com>
2017-02-16 00:05:53 +0100

    AU partitionmasterfree

--- automatic/partitionmasterfree/tools/chocolateyInstall.ps1
+++ automatic/partitionmasterfree/tools/chocolateyInstall.ps1
@@ -1,11 +1,15 @@
-$packageName	= '{{PackageName}}'
-$installerType	= 'exe' 
-$url			= 'http://download.easeus.com/free/epm.exe'
-$silentArgs		= '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+﻿$packageArgs = @{
+  packageName    = 'partitionmasterfree'
+  fileType       = 'EXE'
+  url            = 'http://download.easeus.com/free/epm.exe'
+  checksum       = 'ce144032be3ce73fa44e954913664f9ba1bd80de6ac488bcabe702e48d0a77f3'
+  checksumType   = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+  softwareName   = 'EaseUS Partition Master (Free)'
+}
 
-Write-Host
-Write-Host "This package is not fully unattended." -ForegroundColor "White"
-Write-Host "A screen will pop up where you have to accept the license agreement manually." -ForegroundColor "White"
-Write-Host
+Write-Host "This package is not completely silent." -ForegroundColor "White"
+Write-Host "The application will launch after installation." -ForegroundColor "White"
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/partitionmasterfree/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+
+$releases = 'http://www.partition-tool.com/easeus-partition-manager/history.htm'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$version = $download_page.Content -match 'EaseUS Partition Master ([1-9\.]+) Free Edition'
+	$version = $matches[1]
+	$url = "http://download.easeus.com/free/epm.exe"
+
+  @{
+    Version = $version
+    URL32   = $url
+  }
+}
+
+update -ChecksumFor 32
