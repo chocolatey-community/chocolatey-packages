8d4c81c96637e69800aace1778ff8c69d3d68296
Chocolatey <chocolatey@realdimensions.net>
2017-02-23 00:13:20 +0000

    AU: 3 updated - juju tixati tixati.portable [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/e3d8766b5187cb1bd6dd8e699fb196ed687f6a42

--- automatic/juju/tools/chocolateyinstall.ps1
+++ automatic/juju/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName   = 'juju'
   fileType      = 'exe'
   softwareName  = 'Juju'
-  file          = "$toolsDir\juju-setup-2.1-rc2.exe"
+  file          = "$toolsDir\juju-setup-2.1.0.exe"
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
--- automatic/tixati/tools/chocolateyInstall.ps1
+++ automatic/tixati/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'tixati'
-$fileName = 'tixati-2.51-1.install.exe'
+$fileName = 'tixati-2.53-1.install.exe'
 $download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion"
 
 $packageArgs = @{
   packageName    = $packageName
   fileFullPath   = "$download_dir\$fileName"
-  url            = 'https://download1.tixati.com/download/tixati-2.51-1.win32-install.exe'
-  url64bit       = 'https://download1.tixati.com/download/tixati-2.51-1.win64-install.exe'
-  checksum       = '68fb3dce6af2d986da584fc6ecb173041c45705f8451946345814f8824fcb462'
-  checksum64     = '940b8ecc31100ad50b99d71babb65a2dff4e509543b1d85b4d55cfc237842dc4'
+  url            = 'https://download1.tixati.com/download/tixati-2.53-1.win32-install.exe'
+  url64bit       = 'https://download1.tixati.com/download/tixati-2.53-1.win64-install.exe'
+  checksum       = '7c210e86d586a5e5773c0af39778ba87883db6dbe8ce1d5bcd4da341dfa1cc0e'
+  checksum64     = 'b9a59a1d6bbde6fee773e0429a55afbdd792b42d59447ca5adea28966d12971c'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
 }
