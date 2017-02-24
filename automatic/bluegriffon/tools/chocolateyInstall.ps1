7adab6808ced58ff6a7e16e541311534f3150d97
Chocolatey <chocolatey@realdimensions.net>
2017-02-24 08:07:19 +0000

    AU: 1 updated - bluegriffon [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/92e3a40f2a65fe14ce0d680f715836b0355e6944

--- automatic/bluegriffon/tools/chocolateyInstall.ps1
+++ automatic/bluegriffon/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'BlueGriffon*'
 
-  checksum       = '3119bb380d335baccc8fba5d4d023e566600c564b5e76dde1774a84ab032c6d1'
+  checksum       = 'af87a13bfa255f996a857f1936713279d27345b0ac2f8503ea312a82d39c10b7'
   checksumType   = 'sha256'
-  url            = 'http://bluegriffon.org/freshmeat/2.2/bluegriffon-2.2.win32-installer.exe'
+  url            = 'http://bluegriffon.org/freshmeat/2.3.1/bluegriffon-2.3.1.win32-installer.exe'
 
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
