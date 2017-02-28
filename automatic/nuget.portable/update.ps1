9bc1b60c6aa89c399c8f6651eb74c4bb4ab4525b
Pascal Berger <pascal.berger@gmail.com>
2017-02-28 21:51:26 +0100

    (nuget.portable) Add package for nuget.portable

---
+++ automatic/nuget.portable/update.ps1
@@ -0,0 +1,50 @@
+import-module au
+
+$releases = "http://dist.nuget.org/index.json"
+
+function global:au_BeforeUpdate {
+  Remove-Item "$PSScriptRoot\tools\*.exe"
+
+  $client = New-Object System.Net.WebClient
+  try
+  {
+    $filePath32 = "$PSScriptRoot\tools\$($Latest.FileName32)"
+    $client.DownloadFile($Latest.URL32, "$filePath32")
+  }
+  finally
+  {
+    $client.Dispose()
+  }
+
+  $Latest.ChecksumType = "sha256"
+  $Latest.Checksum32 = Get-FileHash -Algorithm $Latest.ChecksumType -Path $filePath32 | ForEach-Object Hash
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(^[$]filePath32\s*=\s*`"[$]toolsPath\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+    }
+    ".\legal\verification.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(1\..+)\<.*\>"        = "`${1}<$($Latest.URL32)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType)"
+      "(?i)(checksum:).*"        = "`${1} $($Latest.Checksum)"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $json = Invoke-WebRequest -UseBasicParsing -Uri $releases | ConvertFrom-Json
+
+  $commandlineArtifacts = $json.artifacts | Where-Object {$_.name -eq "win-x86-commandline"} 
+  $versionDescription = $commandlineArtifacts.versions |  Where-Object {$_.displayName -eq "nuget.exe - latest"}
+
+  return @{
+    URL32 = $versionDescription.url
+    FileName32 = [IO.Path]::GetFileName($versionDescription.url)
+    Version = $versionDescription.version
+  }
+}
+
+update -ChecksumFor 32
