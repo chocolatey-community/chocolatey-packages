d38e100253fbd94878580fa19d793b8e8fbab025
Chocolatey <chocolatey@realdimensions.net>
2017-03-06 18:11:01 +0000

    AU: 1 updated - mpc-hc [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/b6c1b4ac191fd8e0d413ed67597ebdfa4ad674d4

--- automatic/mpc-hc/tools/chocolateyInstall.ps1
+++ automatic/mpc-hc/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName            = 'mpc-hc'
   fileType               = 'exe'
-  url                    = 'https://binaries.mpc-hc.org/MPC%20HomeCinema%20-%20Win32/MPC-HC_v1.7.10_x86/MPC-HC.1.7.10.x86.exe'
-  url64bit               = 'https://binaries.mpc-hc.org/MPC%20HomeCinema%20-%20x64/MPC-HC_v1.7.10_x64/MPC-HC.1.7.10.x64.exe'
-  checksum               = 'a00009fb70b3f8ab7675bad4c323d354bdb297bcbca44b690f7e18526c394dd6'
-  checksum64             = 'ac93fcf3bb3f0c571f74fb6917d057a7c030143aac68cd53365355a188bc2edd'
+  url                    = 'https://binaries.mpc-hc.org/MPC%20HomeCinema%20-%20Win32/MPC-HC_v1.7.11_x86/MPC-HC.1.7.11.x86.exe'
+  url64bit               = 'https://binaries.mpc-hc.org/MPC%20HomeCinema%20-%20x64/MPC-HC_v1.7.11_x64/MPC-HC.1.7.11.x64.exe'
+  checksum               = '9724a7921c48e56a365673d6a0b56660d180e145a1941d4c5110522b207dfa7d'
+  checksum64             = '7f52077596b7967e9f0da30887f31963061db67da006d1d39d3b8f40f5f77c7c'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/VERYSILENT'
