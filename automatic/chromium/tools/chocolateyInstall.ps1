68a771bd06d84788bfcbc8735b06b4e546048959
Chocolatey <chocolatey@realdimensions.net>
2017-03-12 12:11:54 +0000

    AU: 1 updated - chromium [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/24342da2222204fd022600d80dacc43c2fdd6bb6

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '59.0.3039.0'
+$version = '59.0.3040.0'
 
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $hive = "hkcu"
@@ -15,14 +15,14 @@ if (Test-Path $Chromium) {
 $packageArgs = @{
   packageName   = 'chromium'
   fileType      = 'exe'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/456278/mini_installer.exe'
-  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/456277/mini_installer.exe'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/456300/mini_installer.exe'
+  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/456300/mini_installer.exe'
   silentArgs    = $silentArgs
   validExitCodes= @(0)
   softwareName  = 'Chromium'
-  checksum      = '1645019fe6c9ffba448c9e1f8ca49150'
+  checksum      = '12edc12147018530a4246e9da36ed60a'
   checksumType  = 'md5'
-  checksum64    = '8a339f7d70ac302c0b0c4993d268ae20'
+  checksum64    = '64fcff01dfb4df19a7df476f3d647c06'
   checksumType64= 'md5'
 }
 
