302a1ba6bf175cc37fd23b13a131a034385fca88
Pascal Berger <pascal.berger@gmail.com>
2017-03-13 19:50:30 +0100

    Use Get-RemoteFiles

--- automatic/nuget.portable/update.ps1
+++ automatic/nuget.portable/update.ps1
@@ -3,28 +3,13 @@
 $releases = "http://dist.nuget.org/index.json"
 
 function global:au_BeforeUpdate {
-  Remove-Item "$PSScriptRoot\tools\*.exe"
-
-  $client = New-Object System.Net.WebClient
-  try
-  {
-    $filePath32 = "$PSScriptRoot\tools\$($Latest.FileName32)"
-    $client.DownloadFile($Latest.URL32, "$filePath32")
-  }
-  finally
-  {
-    $client.Dispose()
-  }
-
-  $Latest.ChecksumType = "sha256"
-  $Latest.Checksum32 = Get-FileHash -Algorithm $Latest.ChecksumType -Path $filePath32 | ForEach-Object Hash
+  Get-RemoteFiles -Purge -NoSuffix
 }
 
 function global:au_SearchReplace {
   @{
     ".\legal\verification.txt" = @{
       "(?i)(1\..+)\<.*\>"        = "`${1}<$($Latest.URL32)>"
-      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType)"
       "(?i)(checksum:).*"        = "`${1} $($Latest.Checksum32)"
     }
   }
@@ -34,7 +19,7 @@ function global:au_GetLatest {
   $json = Invoke-WebRequest -UseBasicParsing -Uri $releases | ConvertFrom-Json
 
   $commandlineArtifacts = $json.artifacts | Where-Object {$_.name -eq "win-x86-commandline"} 
-  $versionDescription = $commandlineArtifacts.versions |  Where-Object {$_.displayName -eq "nuget.exe - latest"}
+  $versionDescription = $commandlineArtifacts.versions |  Where-Object {$_.displayName -eq "nuget.exe - recommended latest"}
 
   return @{
     URL32 = $versionDescription.url
