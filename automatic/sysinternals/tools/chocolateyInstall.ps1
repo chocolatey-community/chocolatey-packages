bf8c195556a4f7eacf13319ee34437cd94aa7f4c
AdmiringWorm <kim.nordmo@gmail.com>
2017-03-17 18:01:42 +0100

    (sysinternals) Added support for nano server

--- automatic/sysinternals/tools/chocolateyInstall.ps1
+++ automatic/sysinternals/tools/chocolateyInstall.ps1
@@ -9,16 +9,17 @@ Write-Host "Sysinternals Suite is going to be installed in '$installDir'"
 $packageArgs = @{
   packageName    = 'sysinternals'
   url            = 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
-  url64Bit       = 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
-  checksum       = '6ed82a3e5d61c8cff1650dda4e3dec81f9fd25e7e04bc4db8228c5c49a9a016d'
-  checksum64     = '6ed82a3e5d61c8cff1650dda4e3dec81f9fd25e7e04bc4db8228c5c49a9a016d'
+  checksum       = '97afd4053c69ad26163195250ac53670d39c9f59944c436303d2039a042432e8'
   checksumType   = 'sha256'
-  checksumType64 = 'sha256'
   unzipLocation  = $installDir
 }
 Install-ChocolateyZipPackage @packageArgs
 Accept-Eula
 if ($installDir -ne $toolsPath) { Install-ChocolateyPath $installDir }
+if (Is-NanoServer) {
+  $packageArgs.url = 'https://download.sysinternals.com/files/SysinternalsSuite-Nano.zip'
+  $packageArgs.checksum = '75ec02a227b1bc168ca3d749c7f104bcf6d828780e0889a50a74a77c40127527'
+ }
 
 $old_path = 'c:\sysinternals'
 if ((Test-Path $old_path) -and ($installDir -ne $old_path)) {
--- automatic/sysinternals/update.ps1
+++ automatic/sysinternals/update.ps1
@@ -6,32 +6,34 @@ function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*[$]packageArgs\.url\s*=\s*)('.*')" = "`$1'$($Latest.URLNano)'"
+            "(?i)(^\s*[$]packageArgs\.checksum\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumNano)'"
         }
     }
 }
 
 function global:au_BeforeUpdate {
     Write-Host 'Setting checksum'
-    $Latest.Checksum64 = $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
-    $Latest.ChecksumType32 = $Latest.ChecksumType64 = 'sha256'
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+    $Latest.ChecksumType32 = 'sha256'
+    $Latest.ChecksumNano = Get-RemoteChecksum $Latest.URLNano
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re    = '(?<!Nano)\.zip$'
-    $url   = $download_page.links | ? href -match $re | select -First 1 -Expand href
+    $re      = '(?<!Nano)\.zip$'
+    $url     = $download_page.links | ? href -match $re | select -First 1 -Expand href
+    $urlNano = $download_page.links | ? href -match 'Nano\.zip$' | select -First 1 -Expand href
 
     $updated = $download_page.Content -split "`n" | sls '(?<=Updated: )[^/<>]+' | % Matches | % Value
     $updated = [datetime]::ParseExact($updated, 'MMMM dd, yyyy', [cultureinfo]::InstalledUICulture)
     @{
         Version  = $updated.ToString("yyyy.MM.dd")
         URL32    = $url
-        URL64    = $url
+        URLNano  = $urlNano
     }
 }
 
