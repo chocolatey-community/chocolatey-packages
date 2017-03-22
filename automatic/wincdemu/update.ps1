111fb1ebaf53637127d2405d486c348da7a5146e
AdmiringWorm <kim.nordmo@gmail.com>
2017-03-22 11:52:41 +0100

    (wincdemu) Added -UseBasicParsing

--- automatic/wincdemu/update.ps1
+++ automatic/wincdemu/update.ps1
@@ -14,7 +14,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $re      = '\.exe'
     $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
