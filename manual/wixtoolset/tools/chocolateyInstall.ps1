cd1c76cf98076bb9fd34d3eae13fc28af907694f
Tommy Parnell <tparnell@vistaprint.com>
2017-03-23 08:35:05 -0400

    add checksum to wix package

--- manual/wixtoolset/tools/chocolateyInstall.ps1
+++ manual/wixtoolset/tools/chocolateyInstall.ps1
@@ -3,5 +3,7 @@ $installerType = 'EXE'
 $32BitUrl = 'http://wixtoolset.org/downloads/v3.10.3.3007/wix310.exe'
 $silentArgs = '/q'
 $validExitCodes = @(0)
+$checksum      = '3C125E3551C035F69ED24ACD8FB4EF7B74C1311ECACF1F8FC1EB7E0DD47D9C75'
+$checksumType  = 'sha256'
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl -validExitCodes $validExitCodes
+Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType
