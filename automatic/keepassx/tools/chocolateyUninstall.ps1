8b009225c9c68f2169eae53bcf99a53891024350
AdmiringWorm <kim.nordmo@gmail.com>
2017-03-29 01:47:03 +0200

    (keepassx) Migrated to AU + Modernized

---
+++ automatic/keepassx/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$programsDir = [System.Environment]::GetFolderPath('Programs')
+$lnk = "$programsDir\KeePassX.lnk"
+if (Test-Path $lnk) {
+  Remove-Item -Force $lnk
+}
