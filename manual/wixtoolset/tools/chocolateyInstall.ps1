2a35d15ef3fb28b5a9635027befa6d620bfb94c4
Ritch Melton <skyguy94@gmail.com>
2017-03-28 14:16:07 -0500

    Respond to code review. Fix up uninstaller script.

--- manual/wixtoolset/tools/chocolateyInstall.ps1
+++ manual/wixtoolset/tools/chocolateyInstall.ps1
@@ -1,13 +1,11 @@
 ﻿$packageParameters = @{
   PackageName = 'wixtoolset'
+  FileType = 'exe'
   SilentArgs = '/q'
   Url = 'http://wixtoolset.org/downloads/v3.11.0.1507/wix311.exe'
-  Url64bit = ''
-  ValidExitCodes = @('0')
+  ValidExitCodes = @(0)
   Checksum = '407e00a2c8787c347e6963091d066b0e2530866a161a59a88c56817cf88e9367'
   ChecksumType = 'Sha256'
-  Checksum64 = ''
-  ChecksumType64 = ''
 }
 
 Install-ChocolateyPackage @packageParameters
