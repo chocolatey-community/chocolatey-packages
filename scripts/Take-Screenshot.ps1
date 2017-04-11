a167c188d44b058e469f554697c0027161ef4c10
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-11 09:51:57 +0200

    (Screenshot) Added script to take screenshots

---
+++ scripts/Take-Screenshot.ps1
@@ -0,0 +1,27 @@
+﻿# Based on FeodorFitsners screenshot function
+# https://github.com/FeodorFitsner/selenium-tests/blob/master/take-screenshot.ps1
+
+function Take-Screenshot {
+  param(
+    [string]$file,
+    [ValidateSet('bmp','jpeg','png')]
+    [string]$imagetype = 'png'
+  )
+  [void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+  [void][Reflection.Assembly]::LoadWithPartialName('System.Drawing')
+  function screenshot([Drawing.Rectangle]$bounds, $path, [Drawing.Imaging.ImageFormat]$imageFormat) {
+    $bitmap = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+    $graphics = [Drawing.Graphics]::FromImage($bitmap)
+
+    $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+    $bitmap.Save($path, $imageFormat)
+
+    $graphics.Dispose()
+    $bitmap.Dispose()
+  }
+
+  $screen = [System.Windows.Forms.Screen]::PrimaryScreen
+  $bounds = $screen.Bounds
+
+  screenshot $bounds $file $imagetype
+}
