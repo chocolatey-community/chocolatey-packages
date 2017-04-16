6d06e6d196ca3d78ddeef9f2eb9cdb45e9161192
The Running Dev <boyank@gmail.com>
2017-04-16 13:50:59 -0400

    Re-ddded search and replace function

--- automatic/displayfusion/update.ps1
+++ automatic/displayfusion/update.ps1
@@ -14,4 +14,14 @@ function global:au_GetLatest {
     return @{ Url32 = $downloadUrl; Version = $version }
 }
 
+function global:au_SearchReplace {
+    return @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.Url32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
 Update -ChecksumFor 32
