2fe1377938f91655b2b3f9a14165ffc245d8ba3f
The Running Dev <boyank@gmail.com>
2017-04-16 13:39:46 -0400

    Addressed requested PR review changes

--- automatic/itunesfusion/tools/chocolateyInstall.ps1
+++ automatic/itunesfusion/tools/chocolateyInstall.ps1
@@ -1,11 +1,8 @@
 ﻿$ErrorActionPreference  = 'Stop'
 
-$downloadDir        = Get-PackageCacheLocation
-$installer          = 'iTunesFusionSetup-2.6.exe'
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'iTunesFusion'
-    file            = Join-Path $downloadDir $installer
     url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/102/iTunesFusionSetup-2.6.exe'
     checksum        = '2CE11CE6F3C5ED6A6B24D47C9EA346488BA5FA46AE525B31F9ADF7246E06FA1F'
     fileType        = 'exe'
