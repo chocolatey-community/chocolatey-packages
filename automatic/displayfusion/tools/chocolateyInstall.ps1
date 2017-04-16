22f8ce03b1f322e3c1a77c25167d595591e9047c
The Running Dev <boyank@gmail.com>
2017-04-16 13:44:23 -0400

    Addressed requested PR review changes

--- automatic/displayfusion/tools/chocolateyInstall.ps1
+++ automatic/displayfusion/tools/chocolateyInstall.ps1
@@ -1,18 +1,15 @@
 ﻿$ErrorActionPreference  = 'Stop'
 
-$downloadDir        = Get-PackageCacheLocation
-$installer          = 'DisplayFusionSetup-8.1.2.exe'
 $programFiles       = Join-Path ${env:ProgramFiles(x86)} 'DisplayFusion'
 $localeTwoLetter    = (Get-Culture).TwoLetterISOLanguageName
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'DisplayFusion'
-    file            = Join-Path $downloadDir $installer
     url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/101/DisplayFusionSetup-8.1.2.exe'
     checksum        = '547DF90EC756A4B4FD73675403547C0E08EB1345180107D52E4CDE2FD08C3DC0'
     fileType        = 'exe'
     checksumType    = 'sha256'
-    silentArgs      = "/VERYSILENT /LANG=$localeTwoLetter /DIR=`"$$programFiles`" /STARTUPALL=1 /CONTEXTMENU=0 /LAUNCHAFTER=0 /MERGETASKS=`"!desktopicon,!desktopicon\common,!desktopicon\user`""
+    silentArgs      = "/VERYSILENT /LANG=$localeTwoLetter /DIR=`"$$programFiles`" /STARTUPALL=1 /CONTEXTMENU=0 /LAUNCHAFTER=0"
     validExitCodes  = @(0, 1641, 3010)
 }
 
