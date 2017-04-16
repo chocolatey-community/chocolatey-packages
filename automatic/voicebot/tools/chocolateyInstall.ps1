cf005f0d33fc9e14f6142876dd05df9c9d25beac
The Running Dev <boyank@gmail.com>
2017-04-16 13:29:09 -0400

    Addressed requested PR review changes, again...I will get it right eventually

--- automatic/voicebot/tools/chocolateyInstall.ps1
+++ automatic/voicebot/tools/chocolateyInstall.ps1
@@ -1,11 +1,8 @@
 ﻿$ErrorActionPreference  = 'Stop'
 
-$downloadDir        = Get-PackageCacheLocation
-$installer          = 'VoiceBotSetup-3.0.exe'
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'VoiceBot'
-    file            = Join-Path $downloadDir $installer
     url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/123/VoiceBotSetup-3.0.exe'
     checksum        = '8BC560E85A844F6FD0412E6BD43028F30CB6ACD14AFE4092F02C70072E69BEA9'
     fileType        = 'exe'
