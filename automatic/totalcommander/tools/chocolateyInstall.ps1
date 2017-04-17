17063b3401d6542024470438765155cecc508068
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-17 20:25:06 +0100

    (totalcommander) added parameter /AddCommanderPath
    close #677

--- automatic/totalcommander/tools/chocolateyInstall.ps1
+++ automatic/totalcommander/tools/chocolateyInstall.ps1
@@ -3,9 +3,7 @@
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$is64 = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-               Write-Host "Installing 64 bit version"; $true  }
-        else { Write-Host "Installing 32 bit version"; $false }
+$is64 = (Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true'
 
 $tcExeName = if ($is64) { 'totalcmd64.exe' } else { 'totalcmd.exe' }
 $pp = Get-PackageParameters
@@ -18,6 +16,7 @@ $packageArgs = @{
     PackageName    = 'totalcommander'
     FileType       = 'exe'
     File           = "$tcmdWork\INSTALL.exe"
+    File64         = "$tcmdWork\INSTALL.exe"
     SilentArgs     = ''
     ValidExitCodes = @(0)
     SoftwareName   = 'Total Commander*'
@@ -33,6 +32,10 @@ Write-Host "$packageName installed to '$installLocation'"
 if ($pp.ShellExtension) { Set-TCShellExtension }
 if ($pp.DefaultFM -eq $true) { Set-TCAsDefaultFM }
 if ($pp.DefaultFM -like 'explorer*') { Set-ExplorerAsDefaultFM }
+if ($pp.AddCommanderPath) { 
+    Write-Host 'Adding system environment COMMANDER_PATH'
+    Set-EnvironmentVariable 'COMMANDER_PATH' $installLocation Machine
+}
 
 Register-Application "$installLocation\$tcExeName" tc
 Write-Host "$packageName registered as tc"
