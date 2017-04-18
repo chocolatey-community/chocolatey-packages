761e866388ee9cd088ebadb7b074afb8ba9effe2
The Running Dev <boyank@gmail.com>
2017-04-17 20:48:14 -0400

    Updated uninstall script per request. Added better description

--- automatic/datagrip/tools/ChocolateyInstall.ps1
+++ automatic/datagrip/tools/ChocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $arguments              = @{
     packageName         =  $env:ChocolateyPackageName
-    softwareName        = 'Datagrip'
+    softwareName        = 'JetBrains DataGrip*'
     url                 = 'https://download.jetbrains.com/datagrip/datagrip-2017.1.1.exe'
     checksum            = '6AA8F4B0803B67B9A9C4616FE15DA8EE51F9488569D98162A41E52E1A5DDB92B'
     fileType            = 'exe'
