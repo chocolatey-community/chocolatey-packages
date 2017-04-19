43386f6c49b9d42e07e06e8d231f111ebab08439
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-19 22:46:14 +0200

    (juju) Changed to use 'Get-RemoteFiles' function

--- automatic/juju/update.ps1
+++ automatic/juju/update.ps1
@@ -1,27 +1,15 @@
 ﻿import-module au
-import-module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 . "$PSScriptRoot\..\..\scripts\Set-DescriptionFromReadme.ps1"
 
 $releases = 'https://launchpad.net/juju/+download'
 
-function global:au_BeforeUpdate {
-  Remove-Item "$PSScriptRoot\tools\*.exe"
-
-  $Latest.FileName = Get-WebFileName $Latest.URL32 'juju.exe'
-  $filePath = "$PSScriptRoot\tools\$($Latest.FileName)"
-
-  Get-WebFile $Latest.URL32 $filePath
-
-  $Latest.ChecksumType32 = 'sha256'
-  $Latest.Checksum32 = Get-FileHash -Algorithm $Latest.ChecksumType32 -Path $filePath | % Hash
-}
-
-function global:au_AfterUpdate { Set-DescriptionFromReadme -SkipFirst 1 }
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 1 }
 
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(`"[$]toolsDir\\).*`"" = "`${1}$($Latest.FileName)`""
+      "(?i)(`"[$]toolsDir\\).*`"" = "`${1}$($Latest.FileName32)`""
     }
     ".\legal\VERIFICATION.txt" = @{
       "(?i)(1\..+)\<.*\>"      = "`${1}<$($Latest.URL32)>"
