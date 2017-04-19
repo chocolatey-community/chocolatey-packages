c12b911a857cff968e3654047b477bab3dc847a1
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-19 22:17:04 +0200

    (gobby) Converted to embedded package

--- automatic/gobby/tools/chocolateyInstall.ps1
+++ automatic/gobby/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = 'gobby'
-  fileType      = 'exe'
-  softwareName  = "Gobby*"
-
-  checksum      = '97c2eb92df2ba5322c04a13df62531583ea03609cd5eac3051490c5dc8a700e8'
-  checksumType  = 'sha256'
-  url           = 'http://releases.0x539.de/gobby/gobby-0.5.0.exe'
-
-  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  file           = "$toolsPath\"
+  softwareName   = 'Gobby*'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -ea 0 "$toolsPath\*.exe","$toolsPath\*.ignore"
