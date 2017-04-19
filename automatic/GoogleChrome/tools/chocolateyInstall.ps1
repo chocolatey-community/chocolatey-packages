5a3ee262825cdb1387beca0583aabb47deb69efb
Chocolatey <chocolatey@realdimensions.net>
2017-04-19 18:12:17 +0000

    AU: 3 updated - FirefoxESR freevideoeditor GoogleChrome [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/d249556ac5f4ce0f0a6dace0c40c83de5b172fa2

--- automatic/GoogleChrome/tools/chocolateyInstall.ps1
+++ automatic/GoogleChrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '57.0.2987.133'
+$version = '58.0.3029.81'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '26d4e45f7b9eeb229f939560a794f415550db13d3d6e8c717bcd2d22395f7e94'
-  checksum64             = '104aeaf81d04e448fb9d8817c9f993a6f59d47658f5356f10660d3cf6416586f'
+  checksum               = '7922a2c27c364cf7acfdc014b4cde7d8c4506348f64b63e408256d238bf932f1'
+  checksum64             = 'c1f9b0e61eb0646fafcf1c86c0a175bc2b3fb8ed87863534927e61c171f83186'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/quiet'
