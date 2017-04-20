f2b44b1085a2c6b787e1138e942a83be1c18d366
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-20 21:10:07 +0200

    (universal-usb-installer) Migrated to AU
    ref #707

---
+++ automatic/universal-usb-installer/update.ps1
@@ -0,0 +1,36 @@
+﻿Import-Module AU
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+
+$releases = 'https://www.pendrivelinux.com/universal-usb-installer-easy-as-1-2-3/'
+$padUnderVersion = '1.9.8'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameBase $Latest.PackageName }
+
+function global:au_AfterUpdate { Set-DescriptionFromReadme -SkipFirst 1 }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*1\..+)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
+    }
+  }
+}
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = '\.exe$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
+  if ($url32.StartsWith('//')) { $url32 = 'https:' + $url32 }
+
+  $verRe = '[-]|\.exe$'
+  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
+  @{
+    URL32 = $url32
+    Version = Get-PaddedVersion $version32 -OnlyBelowVersion $padUnderVersion -RevisionLength 3
+  }
+}
+
+update -ChecksumFor none
