1605c232d5f654f2d0d95a5194aea7a17ab8701b
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-20 02:47:08 +0200

    (ontopreplica) Embedded package
    Related to issue #698

--- automatic/ontopreplica/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = '{{PackageName}}'
-$silentArgs = '/S'
-$url = '{{DownloadUrl}}'
-$fileFullPath = Join-Path $env:TEMP 'ontopreplicaInstall.exe'
-
-Get-ChocolateyWebFile $packageName $fileFullPath $url
-Start-Process $fileFullPath -ArgumentList $silentArgs -Wait
-Remove-Item $fileFullPath
---
+++ manual/ontopreplica/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  file           = "$toolsPath\OnTopReplica-3_5_1-Setup.exe"
+  softwareName   = 'OnTopReplica'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -ea 0 "$toolsPath\*.exe","$toolsPath\*.ignore"
