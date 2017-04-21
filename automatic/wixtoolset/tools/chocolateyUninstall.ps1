1e2f34b2cb962cee413a4ddc87088a372ed4d261
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-21 08:26:33 +0200

    (wixtoolset) Fixed acording to pr review

--- automatic/wixtoolset/tools/chocolateyUninstall.ps1
+++ automatic/wixtoolset/tools/chocolateyUninstall.ps1
@@ -22,7 +22,7 @@ if ($key.Count -eq 1) {
   Write-Warning "$packageName has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstall."
-  Write-Warning "Please alert the package maintainer the following keys were matched:"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled.."
+  Write-Warning "Please alert the package maintainer that the following keys were matched:"
   $key | % { Write-Warning "- $($_.DisplayName)" }
 }
