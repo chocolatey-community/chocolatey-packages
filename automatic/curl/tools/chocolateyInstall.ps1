a072e23e25bca049828cdff10007b9bb7d6ee786
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-24 18:37:46 +0200

    (curl) migrated from ferventcoder
    closes #97

---
+++ automatic/curl/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
+$embedded_path = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing 64 bit version"; rm "$toolsDir\curl_x32" -Recurse
+} else { Write-Host "Installing 32 bit version"; rm "$toolsDir\curl_x64" -Recurse}
---
+++ automatic/curl/update.ps1
@@ -0,0 +1,46 @@
+import-module au
+. $PSScriptRoot\..\..\scripts\Set-DescriptionFromReadme.ps1
+
+$releases = 'https://curl.haxx.se/download.html'
+
+function global:au_SearchReplace {
+   @{
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { 
+    Get-RemoteFiles -Purge -NoSuffix
+
+    set-alias 7z $Env:chocolateyInstall\tools\7z.exe
+    rm tools\curl_* -Recurse 
+    7z e tools\*win32* curl.exe libcurl.dll -r -otools\curl_x32
+    7z e tools\*win64* curl.exe libcurl.dll -r -otools\curl_x64
+    rm tools\*.7z
+}
+function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 1 }
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re    = '/vszakats/.+\.7z'
+    $url   = $download_page.links | ? href -match $re | % href
+    $version  = ($url[0] -split '/'  | select -Last 1) -split '-' | select -Index 1
+
+    @{
+        Version      = $version
+        URL32        = $url -match 'win32' | select -first 1
+        URL64        = $url -notmatch 'win32' | select -first 1
+    }
+}
+
+update -ChecksumFor none
