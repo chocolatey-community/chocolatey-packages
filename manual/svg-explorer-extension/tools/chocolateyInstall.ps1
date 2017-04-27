8ff3156b970ac39f43a584af4c129c8d1b37b65c
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-27 19:11:02 +0200

    (svg-explorer-extension) Embedded package

--- manual/svg-explorer-extension/tools/chocolateyInstall.ps1
+++ manual/svg-explorer-extension/tools/chocolateyInstall.ps1
@@ -1,7 +1,17 @@
-﻿$packageName = 'svg-explorer-extension'
-$fileType = 'exe'
-$silentArgs = '/VERYSILENT'
-$url = 'https://svgextension.codeplex.com/downloads/get/803085'
-$url64 = 'https://svgextension.codeplex.com/downloads/get/803086'
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = 'svg-explorer-extension'
+  fileType       = 'exe'
+  file           = "$toolsPath\dssee_setup_i386_v011_signed.exe"
+  file64         = "$toolsPath\dssee_setup_x64_v011_signed.exe"
+  softwareName   = 'SVG Explorer Extension*'
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -ea 0 "$toolsPath\*.exe","$toolsPath\*.ignore"
