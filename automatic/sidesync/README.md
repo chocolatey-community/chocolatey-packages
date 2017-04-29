1f464778c65955230a82cb22113ec80917487e1b
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-29 13:08:43 +0200

    (sidesync) migrated to AU

---
+++ automatic/sidesync/README.md
@@ -0,0 +1,10 @@
+# <img src="" width="48" height="48"/> [sidesync](https://chocolatey.org/packages/sidesync)
+
+Using SideSync, you can conveniently share the screen and data between your PC and mobile device. Receive alarms of your phone through PC and use various features of your phone on the computer. Enjoy the smarter and more convenient life with SideSync.
+
+Install SideSync onto a PC and mobile device, and then connect two devices through USB data cable or Wi-Fi. Simply connecting the PC and mobile, you can conveniently use various features of SideSync. For a PC or MAC, click the download button above to install. For a mobile device, you can download the app from Google Play™ or Samsung GALAXY Apps which is preloaded on your latest Galaxy device.
+
+## Features
+
+- Make Phone Calls and Send Text Messages from PC
+- Share Files between your PC and Mobile Device
--- automatic/sidesync/tools/chocolateyInstall.ps1
+++ automatic/sidesync/tools/chocolateyInstall.ps1
@@ -1,7 +1,20 @@
-$packageName = '{{PackageName}}'
-$installerType = 'EXE'
-$url = '{{DownloadUrl}}'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName            = 'sidesync'
+  fileType               = 'exe'
+  url                    = 'https://www.samsung.com//us/sidesync/SideSync_4.7.0.84.exe'
+  checksum               = '21f5a52ff2b94aa57ab34042c6de1c79cebfef20901942bce327b0ae8fbce621'
+  checksumType           = 'sha256'
+  silentArgs             = '/S'
+  validExitCodes         = @(0)
+  softwareName           = 'Samsung SideSync'
+}
+Install-ChocolateyPackage @packageArgs
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
