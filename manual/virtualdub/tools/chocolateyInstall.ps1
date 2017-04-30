e34f506e519bf2fa3ff0e78acee51b6fa12c8f46
AdmiringWorm <kim.nordmo@gmail.com>
2017-04-30 18:47:30 +0200

    (virtualdub) Reformatted++

--- manual/virtualdub/tools/chocolateyInstall.ps1
+++ manual/virtualdub/tools/chocolateyInstall.ps1
@@ -1,24 +1,29 @@
-﻿$packageName = 'virtualdub'
+﻿$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fullPath = "$toolsDir\VirtualDub-1.10.4.zip"
-$fullPath64 = "$toolsDir\VirtualDub-1.10.4-AMD64.zip"
-
-Get-ChocolateyUnzip -FileFullPath $fullPath -Destination $toolsdir `
-	-PackageName $packageName -FileFullPath64 $fullPath64
-
-# Unique names for each bitness
-$binName = "Veedub64.exe"
-If ( Get-OSArchitectureWidth -compare '32' ) { $binName = "VirtualDub.exe" }
 
 # Place shortcuts in appropriate location
 $ProgsFolder = [environment]::getfolderpath('Programs')
 If ( Test-ProcessAdminRights ) {
-  $ProgsFolder = Join-Path `
-	([environment]::getfolderpath('CommonApplicationData')) `
-	"Microsoft\Windows\Start Menu\Programs"
+  $ProgsFolder = Join-Path ([environment]::getfolderpath('CommonApplicationData')) "Microsoft\Windows\Start Menu\Programs"
 }
-Install-ChocolateyShortcut -shortcutFilePath "$ProgsFolder\VirtualDub.lnk" `
-	-targetPath "$toolsDir\$binName"
 
-Remove-Item -Force -ea 0 "$toolsDir\VirtualDub-1.10.4.zip"
-Remove-Item -Force -ea 0 "$toolsDir\VirtualDub-1.10.4-AMD64.zip"
+# Unique names for each bitness
+$binName = "Veedub64.exe"
+If ((Get-OSArchitectureWidth -compare '32') -or $env:ChocolateyForceX86 -eq $true) { $binName = "VirtualDub.exe" }
+
+$packageArgs = @{
+  packageName      = 'virtualdub'
+  file             = "$toolsDir\VirtualDub-1.10.4.zip"
+  file64           = "$toolsDir\VirtualDub-1.10.4-AMD64.zip"
+  destination      = "$toolsDir"
+  shortcutFilePath = "$ProgsFolder\VirtualDub.lnk"
+  targetPath       = "$toolsDir\$binName"
+}
+
+Get-ChocolateyUnzip @packageArgs
+Install-ChocolateyShortcut @packageArgs
+Install-BinFile -Path "$toolsDir\$binName" -Name $packageArgs.packageName -UseStart
+
+Remove-Item -Force -ea 0 "$toolsDir\*.zip"
+
+Get-ChildItem $toolsDir -Recurse -Filter "*.exe" | % { Set-Content -Path  "$($_.FullName).ignore" -Value '' }
--- manual/virtualdub/tools/chocolateyUninstall.ps1
+++ manual/virtualdub/tools/chocolateyUninstall.ps1
@@ -1,6 +1,5 @@
-﻿$commProgs = Join-Path `
-	([environment]::getfolderpath('CommonApplicationData')) `
-	"Microsoft\Windows\Start Menu\Programs"
+﻿$ErrorActionPreference = 'Stop'
+$commProgs = Join-Path ([environment]::getfolderpath('CommonApplicationData')) "Microsoft\Windows\Start Menu\Programs"
 $userProgs = [environment]::getfolderpath('Programs')
-Remove-Item -Path "$commProgs\VirtualDub.lnk" -ErrorAction SilentlyContinue
-Remove-Item -Path "$userProgs\VirtualDub.lnk" -ErrorAction SilentlyContinue
+Remove-Item -Path "$commProgs\VirtualDub.lnk","$userProgs\VirtualDub.lnk" -ea 0
+Uninstall-BinFile -Name 'virtualdub'
