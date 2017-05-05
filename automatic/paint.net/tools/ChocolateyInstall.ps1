ea358ecbf890a4a4aeaf060aea72a48b19bb0660
The Running Dev <boyank@gmail.com>
2017-05-05 11:00:50 -0400

    Moving paint.net to the core team packages, including an AU update script

---
+++ automatic/paint.net/tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference  = 'Stop'
+$arguments              = @{
+    packageName         =  $env:ChocolateyPackageName
+    softwareName        = 'Paint.NET*'
+    url                 = 'https://www.dotpdn.com/files/paint.net.4.0.16.install.zip'
+    checksum            = 'f79b615a6d9ec93f0a00547814c96ad7e74397214a94e0eb53be021144dccd90'
+    fileType            = 'zip'
+    destination         = Join-Path $env:Temp 'Paint.NET'
+    checksumType        = 'sha256'
+    silentArgs          = '/auto'
+    validExitCodes      = @(0, 1641, 3010)
+}
+
+Install-ChocolateyZipPackage @arguments
+
+$arguments.file = Get-ChildItem $arguments.destination -Recurse -Include *.exe | Select-Object -First 1
+$arguments.fileType ='exe'
+
+Install-ChocolateyInstallPackage @arguments
+
+Remove-Item $arguments.destination -Recurse -Force
---
+++ automatic/paint.net/update.ps1
@@ -0,0 +1,25 @@
+﻿Import-Module AU
+
+function global:au_GetLatest {
+    $releaseUrl = 'https://www.getpaint.net/index.html'
+    $versionRegEx = 'paint\.net\s+([0-9\.]+)'
+    $urlString = 'https://www.dotpdn.com/files/paint.net.$version.install.zip'
+
+    $releasePage = Invoke-WebRequest -Uri $releaseUrl -UseBasicParsing
+    $version = ([regex]::match($releasePage.Content, $versionRegEx).Groups[1].Value)
+    $url = $ExecutionContext.InvokeCommand.ExpandString($urlString)
+
+    return @{ Url32 = $url; Version = $version; }
+}
+
+function global:au_SearchReplace {
+    return @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.Url32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+Update -ChecksumFor 32
