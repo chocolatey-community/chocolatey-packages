cbc412cbd2bd3338d717f04a730f619ad27cbd9a
Chocolatey <chocolatey@realdimensions.net>
2017-05-12 00:12:41 +0000

    AU: 2 updated - gimp juju [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/9d85495f726fa8e67f3bc59b2bb5b89dc6a0bcdc

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -5,20 +5,20 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'GIMP'
 
-  checksum       = '4ad9ab85c969093810d4932b9574d9d82e0527263bcf37a042de1503c36111da'
-  checksum64     = '4ad9ab85c969093810d4932b9574d9d82e0527263bcf37a042de1503c36111da'
+  checksum       = 'a2e52129a28feec1ee3f22f5aaf9bdecbb02d51af6da408ace0a2ac2e0365c8b'
+  checksum64     = 'a2e52129a28feec1ee3f22f5aaf9bdecbb02d51af6da408ace0a2ac2e0365c8b'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
 
   # The installer contains both 32-bit and 64-bit versions of GIMP, and will automatically use the appropriate one.
   # Text quoted from <https://www.gimp.org/downloads/>, look below download buttons.
-  url            = 'https://download.gimp.org/mirror/pub/gimp/v2.8/windows/gimp-2.8.20-setup.exe'
-  url64bit       = 'https://download.gimp.org/mirror/pub/gimp/v2.8/windows/gimp-2.8.20-setup.exe'
+  url            = 'https://download.gimp.org/mirror/pub/gimp/v2.8/windows/gimp-2.8.22-setup.exe'
+  url64bit       = 'https://download.gimp.org/mirror/pub/gimp/v2.8/windows/gimp-2.8.22-setup.exe'
 
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
 }
-$version = '2.8.20'
+$version = '2.8.22'
 $gimpRegistryPath = $(
   'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\GIMP-2_is1'
 )
