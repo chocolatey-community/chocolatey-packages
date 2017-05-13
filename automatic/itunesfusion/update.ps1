3e6f8b4ed495057569e32a3c7338ed250032dced
AdmiringWorm <kim.nordmo@gmail.com>
2017-05-13 14:06:40 +0200

    (itunesfusion) Extracted description to its own readme.md file

--- automatic/itunesfusion/update.ps1
+++ automatic/itunesfusion/update.ps1
@@ -1,4 +1,5 @@
 ﻿Import-Module AU
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 function global:au_GetLatest {
     $downloadEndPointUrl = 'https://www.binaryfortress.com/Data/Download/?package=itunesfusion&log=102'
@@ -12,6 +13,10 @@ function global:au_GetLatest {
     }
 
     return @{ Url32 = $downloadUrl; Version = $version }
+}
+
+function global:au_AfterUpdate {
+  Set-DescriptionFromReadme -SkipFirst 1
 }
 
 function global:au_SearchReplace {
