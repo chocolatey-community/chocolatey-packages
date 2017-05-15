a8c854861136caf9aafd30e5cbf6445bc6d87591
Chocolatey <chocolatey@realdimensions.net>
2017-05-15 00:12:05 +0000

    AU: 3 updated - notepadplusplus notepadplusplus.commandline notepadplusplus.install [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/65aefc80fcc48d7cd40f0a703170933b75b8e593

--- automatic/notepadplusplus.install/tools/chocolateyInstall.ps1
+++ automatic/notepadplusplus.install/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'notepadplusplus'
-$url32       = 'https://notepad-plus-plus.org/repository/7.x/7.3.3/npp.7.3.3.Installer.exe'
-$url64       = 'https://notepad-plus-plus.org/repository/7.x/7.3.3/npp.7.3.3.Installer.x64.exe'
-$checksum32  = 'c59a2b8ba98d0bb849e55aa8ad1cfc9af54ed9acc1a087a43033e23bebedc0e8'
-$checksum64  = '656e7a31ee07d7febcb49822dd166cb72afe23d4dccf546f2c6b45bcd959e5a1'
+$url32       = 'https://notepad-plus-plus.org/repository/7.x/7.4/npp.7.4.Installer.exe'
+$url64       = 'https://notepad-plus-plus.org/repository/7.x/7.4/npp.7.4.Installer.x64.exe'
+$checksum32  = 'c14f0c03636044bf4c85d007d9b6fd4aa88278549979d4905c6091088db94d2d'
+$checksum64  = 'a95dcb3726b48adfd4a3364176c0291151e6c79630cab268a9ef3511860418d8'
 
 $packageArgs = @{
   packageName            = $packageName
