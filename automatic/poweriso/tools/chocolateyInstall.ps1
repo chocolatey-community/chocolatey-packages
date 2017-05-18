b8280caf147426da8d3240616aa0c0a0497e4f21
Chocolatey <chocolatey@realdimensions.net>
2017-05-18 10:57:45 +0000

    AU: 1 updated - poweriso [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/aa0bf72a2ef1a22a6ecb40dfbdb3ccbf35458ace

--- automatic/poweriso/tools/chocolateyInstall.ps1
+++ automatic/poweriso/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$download_content = Get-WebContent 'http://www.poweriso.com/download.htm'
+$download_content = Get-WebContent 'https://www.poweriso.com/download.htm'
 
 $reMatches = [regex]::Matches($download_content, '\<a class="download_link" href="([^"]+)"')
 
@@ -9,8 +9,8 @@ $packageArgs = @{
   fileType               = 'exe'
   url                    = $reMatches[0].Groups[1]
   url64bit               = $reMatches[1].Groups[1]
-  checksum               = '925fb61d45e7239e8848d0bb70847eef0ff3987d72efa98d61d13cb304bfc8d0'
-  checksum64             = '40d83449781f8a6c498eed86f6ce2409b3d58a63fb5ccad3dc76157600ee0a7f'
+  checksum               = '037acfb14c28a3dacdf8ffd978e2efdfc9aaedaff72b9f25bea7b4442b4dc392'
+  checksum64             = 'b979b79a8c40e697ee410a4844b1207ac71cba97d8991e0b4cd07bb2ae7bece8'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/S'
