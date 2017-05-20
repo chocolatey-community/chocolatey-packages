5d09d7ec28f9b05613699a8acc153d1de0c63029
Miodrag Milic <miodrag.milic@gmail.com>
2017-05-20 18:06:35 +0200

    (vcredist2008) uninstaller fixes

--- manual/vcredist2008/tools/chocolateyUninstall.ps1
+++ manual/vcredist2008/tools/chocolateyUninstall.ps1
@@ -11,9 +11,8 @@ if ($key.Count -eq 1) {
             silentArgs             = "/quiet /qn /norestart"
             fileType               = 'msi'
             validExitCodes         = @(0,3010) # http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-            file                   = ''
+            file                   = "$($_.UninstallString.Replace(' /x86=0', ''))"   #"C:\Program Files\OpenSSH\uninstall.exe" /x86=0
         }
-        $packageArgs.file = "$($_.UninstallString.Replace(' /x86=0', ''))"   #"C:\Program Files\OpenSSH\uninstall.exe" /x86=0
         Uninstall-ChocolateyPackage @packageArgs
     }
 }
@@ -21,8 +20,8 @@ elseif ($key.Count -eq 0) {
     Write-Warning "$packageName has already been uninstalled by other means."
 }
 elseif ($key.Count -gt 1) {
-    Write-Warning "$key.Count matches found!"
+    Write-Warning "${$key.Count} matches found!"
     Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
     Write-Warning "Please alert package maintainer the following keys were matched:"
-    $key | % {Write-Warning "- $_.DisplayName"}
+    $key | % {Write-Warning "- ${$_.DisplayName}"}
 }
