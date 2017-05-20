7a984c3355c9aa298e2ff190ef320a889beac2cf
AdmiringWorm <kim.nordmo@gmail.com>
2017-05-20 14:15:54 +0200

    (flashplayer*) Ignore when secure channel couldn't be created

--- automatic/flashplayerppapi/update.ps1
+++ automatic/flashplayerppapi/update.ps1
@@ -37,4 +37,11 @@ function global:au_GetLatest {
   }
 }
 
-update -ChecksumFor 32
+try {
+  update -ChecksumFor 32
+} catch {
+  if ($_ -match 'Could not create SSL\/TLS secure channel.') {
+    Write-Host "Unable to create secure channel, ignoring..."
+    return 'ignore'
+  } else { throw $_ }
+}
