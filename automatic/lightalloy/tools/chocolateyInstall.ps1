8e47cce175e7fa68295001abec38781511031a21
Miodrag Milic <miodrag.milic@gmail.com>
2017-05-24 21:18:03 +0200

    (lightalloy) fixed spurious shim

--- automatic/lightalloy/tools/chocolateyInstall.ps1
+++ automatic/lightalloy/tools/chocolateyInstall.ps1
@@ -1,8 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsPath     = Split-Path $MyInvocation.MyCommand.Definition
-$au3		       = Join-Path $toolsPath 'lightalloy.au3'
+$au3		   = Join-Path $toolsPath 'lightalloy.au3'
 $embedded_path = gi "$toolsDir\*.exe"
 
 Write-Output "Running AutoIt3 using `'$au3`'"
 Start-ChocolateyProcessAsAdmin "`"$au3`" `"$tempFile`"" 'AutoIt3.exe'
+
+ls $toolsDir\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { touch "$_.ignore" }}
