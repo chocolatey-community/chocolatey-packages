d1f1cd6c6fd65ef72c85ce52854ccdd06b0bde00
AdmiringWorm <kim.nordmo@gmail.com>
2017-05-26 21:28:16 +0200

    (ant-renamed) Updated install script

--- automatic/ant-renamer/tools/chocolateyInstall.ps1
+++ automatic/ant-renamer/tools/chocolateyInstall.ps1
@@ -1,16 +1,13 @@
-﻿$packageName  = 'alldup'
-$url          = 'http://update.antp.be/renamer/antrenamer2_install.exe'
-$checksum     = '866327c8c5e36e1e525da37ccdb40924e16ce5a5772ff67414b62b9d508cf2e5'
-$checksumType = 'sha256'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs  = @{
-  packageName    = $packageName
+  packageName    = $env:chocolateyPackageName
   fileType       = 'exe'
   softwareName   = 'Ant Renamer'
 
-  checksum       = $checksum
-  checksumType   = $checksumType
-  url            = $url
+  checksum       = ''
+  checksumType   = ''
+  url            = ''
 
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0)
