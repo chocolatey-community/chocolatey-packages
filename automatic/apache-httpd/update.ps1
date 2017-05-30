f5b7a4de88f7b0827afb37998e3cf8c2fb859d13
Walt Sorensen <photodude@users.noreply.github.com>
2017-05-30 16:22:41 -0600

    try suggested change

--- automatic/apache-httpd/update.ps1
+++ automatic/apache-httpd/update.ps1
@@ -1,47 +1,33 @@
-import-module au
+Import-Module au
+Import-module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$releases = 'http://www.apachehaus.com/cgi-bin/download.plx'
+function global:au_AfterUpdate { Set-DescriptionFromReadme -SkipFirst 1 }
 
-function global:au_BeforeUpdate {
-  $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
-  $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
+function global:au_GetLatest {
+    $downloadPageUrl = 'http://www.apachehaus.com/cgi-bin/download.plx'
+    $versionRegEx = 'httpd\-([0-9\.]+)\-x\d+\-(.*)\.zip'
+    $url32 = 'http://www.apachehaus.com/downloads/httpd-$version-x86-$vcNumber.zip'
+    $url64 = 'http://www.apachehaus.com/downloads/httpd-$version-x64-$vcNumber.zip'
 
-  $lines = @(
-    @('vc9'; $Latest.URL32; $Latest.URL64; $Latest.Checksum32; $Latest.Checksum64) -join '|'
-    @('vc11'; $Latest.URL32; $Latest.URL64; $Latest.Checksum32; $Latest.Checksum64) -join '|'
-    @('vc14'; $Latest.URL32; $Latest.URL64; $Latest.Checksum32; $Latest.Checksum64) -join '|'
-  )
+    $downloadPage = Invoke-WebRequest $downloadPageUrl -UseBasicParsing
+    $matches = [regex]::match($downloadPage.Content, $versionRegEx)
+    $version = [version]$matches.Groups[1].Value
+    $vcNumber = $matches.Groups[2].Value
 
-  [System.IO.File]::WriteAllLines("$PSScriptRoot\tools\downloadInfo.csv", $lines);
+    $url32 = $ExecutionContext.InvokeCommand.ExpandString($url32)
+    $url64 = $ExecutionContext.InvokeCommand.ExpandString($url64)
+
+    return @{ Url32 = $url32; Url64 = $url64; Version = $version }
 }
 
 function global:au_SearchReplace {
-   @{
+    return @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*packageName\s*=\s*)('.*')"                        = "`$1'$($Latest.PackageName)'"
-        }
-
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
     }
 }
 
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-
-    $re      = 'httpd-\d.+-vc\d.+\.zip$'
-    $url     = $download_page.links | ? href -match $re  | % href | select -First 2
-    $version = $url[0] -split '-' | select -Index 1
-    $majorVersion = $version -split '\.' | select -first 1
-    $Result = @{
-        Version      = $version
-        URL32     = "http://www.apachehaus.com/downloads/" + ($url -match 'x86' | select -First 1)
-        URL64     = "http://www.apachehaus.com/downloads/" + ($url -match 'x64' | select -First 1)
-        ReleaseNotes = "http://www.apache.org/dist/httpd/CHANGES_${majorVersion}.#${version}"
-    }
-
-    $Result
-}
-
-update -ChecksumFor none
+update -ChecksumFor 32
