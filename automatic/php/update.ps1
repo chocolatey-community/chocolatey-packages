4c054080e72857c01f19ad67eabe76ef2f76f11b
Walt Sorensen <photodude@users.noreply.github.com>
2017-05-30 16:19:11 -0600

    try suggested change

--- automatic/php/update.ps1
+++ automatic/php/update.ps1
@@ -1,59 +1,33 @@
-import-module au
+Import-Module au
+Import-module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$releases = 'http://windows.php.net/download'
+function global:au_AfterUpdate { Set-DescriptionFromReadme -SkipFirst 1 }
 
-function global:au_BeforeUpdate {
-  $Latest.ChecksumTS32 = Get-RemoteChecksum $Latest.URLTS32
-  $Latest.ChecksumTS64 = Get-RemoteChecksum $Latest.URLTS64
-  $Latest.ChecksumNTS32 = Get-RemoteChecksum $Latest.URLNTS32
-  $Latest.ChecksumNTS64 = Get-RemoteChecksum $Latest.URLNTS64
+function global:au_GetLatest {
+    $downloadPageUrl = 'http://www.apachehaus.com/cgi-bin/download.plx'
+    $versionRegEx = 'httpd\-([0-9\.]+)\-x\d+\-(.*)\.zip'
+    $url32 = 'http://www.apachehaus.com/downloads/httpd-$version-x86-$vcNumber.zip'
+    $url64 = 'http://www.apachehaus.com/downloads/httpd-$version-x64-$vcNumber.zip'
 
-  $lines = @(
-    @('threadsafe'; $Latest.URLTS32; $Latest.URLTS64; $Latest.ChecksumTS32; $Latest.ChecksumTS64) -join '|'
-    @('not-threadsafe'; $Latest.URLNTS32; $Latest.URLNTS64; $Latest.ChecksumNTS32; $Latest.ChecksumNTS64) -join '|'
-  )
+    $downloadPage = Invoke-WebRequest $downloadPageUrl -UseBasicParsing
+    $matches = [regex]::match($downloadPage.Content, $versionRegEx)
+    $version = [version]$matches.Groups[1].Value
+    $vcNumber = $matches.Groups[2].Value
 
-  [System.IO.File]::WriteAllLines("$PSScriptRoot\tools\downloadInfo.csv", $lines);
+    $url32 = $ExecutionContext.InvokeCommand.ExpandString($url32)
+    $url64 = $ExecutionContext.InvokeCommand.ExpandString($url64)
+
+    return @{ Url32 = $url32; Url64 = $url64; Version = $version }
 }
 
 function global:au_SearchReplace {
-   @{
+    return @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*packageName\s*=\s*)('.*')"                        = "`$1'$($Latest.PackageName)'"
-        }
-
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
     }
 }
 
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-
-    $re      = 'php-\d.+-nts.+\.zip$'
-    $url     = $download_page.links | ? href -match $re  | % href | select -First 2
-    $urlTS   = $url | % { $_ -replace '\-nts','' }
-    $version = $url[0] -split '-' | select -Index 1
-    $majorVersion = $version -split '\.' | select -first 1
-    $Result = @{
-        Version      = $version
-        URLNTS32     = "http://windows.php.net/" + ($url -match 'x86' | select -First 1)
-        URLNTS64     = "http://windows.php.net/" + ($url -match 'x64' | select -First 1)
-        URLTS32      = "http://windows.php.net/" + ($urlTS -match 'x86' | select -First 1)
-        URLTS64      = "http://windows.php.net/" + ($urlTS -match 'x64' | select -First 1)
-        ReleaseNotes = "https://secure.php.net/ChangeLog-${majorVersion}.php#${version}"
-    }
-
-    if ($Result.URLNTS32 -eq $Result.TS32) {
-      throw "The threadsafe and non-threadsafe 32bit url is equal... This is not expected"
-    }
-
-    if ($Result.URLNTS64 -eq $Result.TS64) {
-      throw "The threadsafe and non-threadsafe 64bit url is equal... This is not expected"
-    }
-
-    $Result
-}
-
-update -ChecksumFor none
+update -ChecksumFor 32
