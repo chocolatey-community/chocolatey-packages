1dca2fa02a2500c840b49561ccdb9b4b6b9175ea
Walt Sorensen <photodude@users.noreply.github.com>
2017-05-30 19:01:05 -0600

    vcNumber is defined

--- automatic/apache-httpd/update.ps1
+++ automatic/apache-httpd/update.ps1
@@ -5,7 +5,6 @@ function global:au_AfterUpdate { Set-DescriptionFromReadme -SkipFirst 1 }
 
 function global:au_GetLatest {
     $downloadPageUrl = 'http://www.apachehaus.com/cgi-bin/download.plx'
-    $vcNumber = "14"
     $versionRegEx = 'httpd\-([0-9\.]+)\-x\d+\-(.*)\.zip'
     $url32 = 'http://www.apachehaus.com/downloads/httpd-$version-x86-$vcNumber.zip'
     $url64 = 'http://www.apachehaus.com/downloads/httpd-$version-x64-$vcNumber.zip'
@@ -18,7 +17,7 @@ function global:au_GetLatest {
     $url32 = $ExecutionContext.InvokeCommand.ExpandString($url32)
     $url64 = $ExecutionContext.InvokeCommand.ExpandString($url64)
 
-    return @{ Url32 = $url32; Url64 = $url64; Version = $version }
+    return @{ Url32 = $url32; Url64 = $url64; Version = $version; vcNumber = $vcNumber }
 }
 
 function global:au_SearchReplace {
@@ -34,4 +33,4 @@ function global:au_SearchReplace {
     }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor 64
