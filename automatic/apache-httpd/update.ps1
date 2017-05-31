614ff80932f91d9e00c909b623daf3e93265fa7d
Walt Sorensen <photodude@users.noreply.github.com>
2017-05-31 10:24:48 -0600

    Implement regex changes suggested by AdmiringWorm

--- automatic/apache-httpd/update.ps1
+++ automatic/apache-httpd/update.ps1
@@ -23,14 +23,14 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     return @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"       = "`$1'$($Latest.Url64)'"
+            "(?i)(^\s*url\s*=\s*)(`".*`")"          = "`$1'$($Latest.Url32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+            "(?i)(^\s*url64bit\s*=\s*)(`".*`")"     = "`$1'$($Latest.Url64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
 }
 
-update -ChecksumFor 64
+update
