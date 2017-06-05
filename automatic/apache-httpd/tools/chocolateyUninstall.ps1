374cda07ba126ef09c59f598cd688c0ecc635a1b
The Running Dev <boyank@gmail.com>
2017-06-05 15:45:20 -0400

    BeforeModify script was missing reading the config options file

--- automatic/apache-httpd/tools/chocolateyUninstall.ps1
+++ automatic/apache-httpd/tools/chocolateyUninstall.ps1
@@ -1,3 +1,6 @@
+$configFile = Join-Path $env:chocolateyPackageFolder 'config.json'
+$config = Get-Content $configFile -Raw | Out-String | ConvertFrom-Json
+
 & $($config.httpdPath) -k uninstall -n "$($config.serviceName)"
 
 Remove-Item $config.destination -Recurse -Force
