2ec504d565ef396bcde25116c1cdf9787b456476
AdmiringWorm <kim.nordmo@gmail.com>
2017-06-08 02:15:11 +0200

    (calibre) Fixed call to update changelog version

--- automatic/calibre/update.ps1
+++ automatic/calibre/update.ps1
@@ -7,7 +7,7 @@ function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_AfterUpdate {
   Set-DescriptionFromReadme -SkipFirst 1
-  Update-ChangelogDescription -version $Latest.Version
+  Update-ChangelogVersion -version $Latest.Version
 }
 
 function global:au_SearchReplace {
