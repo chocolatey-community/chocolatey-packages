94947c92cdd15fb4c73fa0468304d461720957ce
RB <RedBaron2@users.noreply.github.com>
2017-06-23 21:02:07 -0700

    Update update.ps1
    Update to dropbox  to always have the correct stable version, and to allow for the dropbox-beta package in the future

--- automatic/dropbox/update.ps1
+++ automatic/dropbox/update.ps1
@@ -14,26 +14,56 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $downloadEndpointUrl = 'https://www.dropbox.com/download?full=1&plat=win'
-    $versionRegEx = '.*Dropbox%20([0-9\.]+).*'
-    $downloadUrl = Get-RedirectedUrl $downloadEndpointUrl
-    $version = $downloadUrl -replace $versionRegEx, '$1'
+    $releases = 'https://www.dropboxforum.com/t5/Desktop-client-builds/bd-p/101003016'
+    $HTML = Invoke-WebRequest -UseBasicParsing -Uri $releases
+    $stable_builds = @()
+    $beta_builds = @()
+    $HTML.Links | foreach {
+  	  if ($_.href -match "stable" ) { $stable_builds += $_.href }
+  	  if ($_.href -match "beta" ) { $beta_builds += $_.href }
+    }
+    $re_dash = '-'; $re_dashndigits = "\-\D+"; $re_t5 = 't5'; $re_abc = '[a-z]\w+'; $re_num_M = '\d+\#[M]\d+';
+    $re_dot = '.'; $re_non = ''; $re_stable = 'Stable-'; $re_beta = 'Beta-'; $re_build = 'Build-';
+	   $beta_version =  ( drpbx-builds -hrefs $beta_builds -beta_build $true )
+	   $stable_version = ( drpbx-builds -hrefs $stable_builds -beta_version $beta_version )
+    $downloadUrl = "https://dl-web.dropbox.com/u/17/Dropbox%20${stable_version}.exe"
+    $beta_downloadUrl = "https://dl-web.dropbox.com/u/17/Dropbox%20${beta_version}.exe"
 
-    return @{ URL32 = $downloadUrl; Version = $version }
+    return @{
+    URL32 = $downloadUrl;
+    Version = $stable_version;
+    #URL32_b = $beta_downloadUrl; # Not yet used at this time
+    #Version_b = $beta_version; # Not yet used at this time
+    }
 }
 
-Function Get-RedirectedUrl {
-    Param (
-        [Parameter(Mandatory = $true)][String]$url
-    )
-
-    $request = [System.Net.WebRequest]::Create($url)
-    $request.AllowAutoRedirect = $false
-    $response = $request.GetResponse()
-
-    If ($response.StatusCode -eq "Found") {
-        $response.GetResponseHeader("Location")
+function drpbx-builds {
+	param(
+		[string]$hrefs,
+		[string]$beta_version,
+		[bool]$beta_build
+	)
+    $links = $hrefs
+    $links = $links -split ( '\/' ); $build = @()
+	   $build_version = @{$true=($re_beta + $re_build);$false=($re_stable + $re_build)}[( ($beta_build) )]
+    foreach( $_ in $links ) {
+     $_ = $_ -replace ( ($build_version), $re_non ) -replace ( $re_dashndigits , $re_non ) -replace ($re_dash , $re_dot ) -replace ( $re_t5 , $re_non ) -replace ($re_abc  , $re_non ) -replace ( $re_num_M , $re_non )
+      if (( $beta_build )) {
+        if ( $_ -ge '27.3.21' ) {
+        if ( $_ -match '(\d+\.)?(\d+\.)?(\*|\d+)') {
+        $build = $_
+        break;
+        }
+      }
+		} else {
+      $_ = $_  -replace ('m', $re_non )
+			if (( $_ -ge '27.3.21' ) -and ( $_ -le $beta_version )) {
+			$build = $_
+			break;
+			}
+		}
     }
+	return $build
 }
 
 #update -ChecksumFor 32
