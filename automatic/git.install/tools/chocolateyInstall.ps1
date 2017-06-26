efd9eaf10767de34cc8f24e87bdc18897ae0a1b1
Miodrag Milic <miodrag.milic@gmail.com>
2017-06-26 08:56:09 +0200

    (git*) improvements and READMEs

--- automatic/git.install/tools/chocolateyInstall.ps1
+++ automatic/git.install/tools/chocolateyInstall.ps1
@@ -1,12 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
+
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
-
-$filePath32 = "$toolsPath\Git-2.13.2-32-bit.exe"
-$filePath64 = "$toolsPath\Git-2.13.2-64-bit.exe"
-$installFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-                      Write-Host "Installing 64 bit version"; $filePath64 }
-               else { Write-Host "Installing 32 bit version"; $filePath32 }
 
 $pp = Get-PackageParameters
 Set-InstallerRegistrySettings $pp
@@ -17,9 +12,9 @@ $packageArgs = @{
     PackageName    = 'git.install'
     FileType       = 'exe'
     SoftwareName   = 'Git version *'
-    File           = $installFile
+    File           = gi $toolsPath\*-32-bit.exe
+    File64         = gi $toolsPath\*-64-bit.exe
     SilentArgs     = "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART", "/NOCANCEL", "/SP-", "/LOG", (Get-InstallComponents $pp)
-    ValidExitCodes = @(0)
 }
 Install-ChocolateyInstallPackage @packageArgs
 
@@ -28,4 +23,4 @@ $installLocation = Get-AppInstallLocation $packageArgs.SoftwareName
 if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
 Write-Host "$packageName installed to '$installLocation'"
 
-$filePath32, $filepath64 | % { rm $_ -ea 0; if (Test-Path $_) { touch "$_.ignore" }}
+ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { touch "$_.ignore" } }
