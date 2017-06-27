d1d3b28f19773d54b605ac5ca25e6bfe92d35530
Chocolatey <chocolatey@realdimensions.net>
2017-06-27 18:11:05 +0000

    AU: 3 updated - git-lfs git-lfs.install smartftp [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/f8ea1aa8915dcd67c7801c9824db7729cf53fa15

--- automatic/git-lfs.install/tools/chocolateyInstall.ps1
+++ automatic/git-lfs.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\git-lfs-windows-2.1.1.exe"
+$filePath = "$toolsDir\git-lfs-windows-2.2.0.exe"
 
 $packageArgs = @{
   PackageName    = 'git-lfs'
