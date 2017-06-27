a2268aa047b5d1667240bff95e8b6c1deeb4a44c
Chocolatey <chocolatey@realdimensions.net>
2017-06-27 10:14:33 +0000

    AU: 2 updated - git.portable tv-browser [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/1f2b5151a2d9fff950b6df3def38924adb1e7515

--- automatic/tv-browser/tools/chocolateyInstall.ps1
+++ automatic/tv-browser/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'TV-Browser*'
 
-  checksum       = ''
-  checksumType   = ''
+  checksum       = '950dc6e712ccfebe3ff742ff2d1a4cf3f1d93c746432c055a0123bfa294197f8'
+  checksumType   = 'sha256'
   url            = 'https://sourceforge.net/projects/tvbrowser/files/TV-Browser%20Releases%20%28Java%208%20and%20higher%29/4/tvbrowser_4_win32.exe/download'
 
   silentArgs     = '/S'
