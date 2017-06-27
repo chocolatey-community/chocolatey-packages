0f23469643236314aa2bae6904029200bfba3f3a
Chris Evett <cevett@cardlytics.com>
2017-06-27 15:06:47 -0400

    set path variable in openssl.light install

--- automatic/openssl.light/tools/chocolateyInstall.ps1
+++ automatic/openssl.light/tools/chocolateyInstall.ps1
@@ -4,10 +4,15 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $pp = Get-PackageParameters
 $silentArgs = '/silent','/sp-','/suppressmsgboxes'
-if ($pp.InstallDir) { $silentArgs += @("/DIR=`"$($pp.InstallDir)`"") }
-else {
-  $silentArgs += @("/DIR=`"$env:ProgramFiles\OpenSSL`"")
+
+if ($pp.InstallDir) {
+  $dir = $pp.InstallDir 
 }
+else {
+  $dir = "$($env:ProgramFiles)\OpenSSL"
+}
+
+$silentArgs += @("/DIR=`"$dir`"")
 
 $packageArgs = @{
   packageName    = 'OpenSSL.Light'
@@ -20,5 +25,7 @@ $packageArgs = @{
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+$binPath = "$($dir)\bin" 
+Install-ChocolateyPath -PathToInstall $binPath 
 
 Remove-Item -Force -ea 0 "$toolsPath\*.exe","$toolsPath\*.ignore"
