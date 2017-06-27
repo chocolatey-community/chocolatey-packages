d8eeb6b0c8ba848fbc590abbf8a737163e8eb29c
Chocolatey <chocolatey@realdimensions.net>
2017-06-27 12:12:02 +0000

    AU: 1 updated - Opera [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/34926d30827f66bbc3a1c7a1b48d3170a40fe8ad

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -4,14 +4,14 @@ $toolsPath = (Split-Path -Parent $MyInvocation.MyCommand.Definition)
 
 if (Is32BitInstalled) { $env:ChocolateyForceX86 = $true }
 
-$version     = '46.0.2597.26'
+$version     = '46.0.2597.32'
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/46.0.2597.26/win/Opera_46.0.2597.26_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/46.0.2597.26/win/Opera_46.0.2597.26_Setup_x64.exe'
-  checksum       = 'f536dadef999707ee6f0ab5460b7464e6b1c28c7013d429baf34ed5d6f1e570f'
-  checksum64     = '769cc21e48b437b0526a17ab214b63274d904858ca0c82e32342a7e562f8c5f6'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/46.0.2597.32/win/Opera_46.0.2597.32_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/46.0.2597.32/win/Opera_46.0.2597.32_Setup_x64.exe'
+  checksum       = '74977b0da0219d646ef8c34d9cbb15e96a1ed76c2a44bb3d25b70f4e9920b44f'
+  checksum64     = 'f87f091f5eda0cd68390edd18b413e4625ae4bf983fa2b4ba423f9689de571a2'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0'
