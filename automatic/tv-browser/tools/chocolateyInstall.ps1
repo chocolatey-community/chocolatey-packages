d065856cc7743b482df2dec70bf9fbc8d045a9fc
Miodrag Milic <miodrag.milic@gmail.com>
2017-06-27 12:01:04 +0200

    (tv-browser) fixed version parsing

--- automatic/tv-browser/tools/chocolateyInstall.ps1
+++ automatic/tv-browser/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'tv-browser'
   fileType       = 'exe'
   softwareName   = 'TV-Browser*'
 
-  checksum       = '3362f3940f92c51114330a9c496609d96ef19a25bf74181e39a4263a4442047c'
-  checksumType   = 'sha256'
-  url            = 'https://sourceforge.net/projects/tvbrowser/files/TV-Browser%20Releases%20%28Java%206%20and%20higher%29/3.4.4/tvbrowser_3.4.4_win32.exe/download'
+  checksum       = ''
+  checksumType   = ''
+  url            = 'https://sourceforge.net/projects/tvbrowser/files/TV-Browser%20Releases%20%28Java%208%20and%20higher%29/4/tvbrowser_4_win32.exe/download'
 
   silentArgs     = '/S'
   validExitCodes = @(0)
