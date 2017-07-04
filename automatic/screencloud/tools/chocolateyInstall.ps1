1c7defed5af3d4f87905acc3d19cfa1903e78bc4
Adrian L Lange <mail@p3lim.net>
2017-07-04 13:08:54 +0200

    Add ScreenCloud package

---
+++ automatic/screencloud/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+    packageName    = 'screencloud'
+    fileType       = 'msi'
+    softwareName   = 'ScreenCloud'
+
+    checksum       = '41b4a0c38dc677820add2a7b2a695e9f9e8e60a14e3e869abe5bfae27e54a2e5'
+    checksumType   = 'sha256'
+    url            = 'https://screencloud.net/files/windows/ScreenCloud-1.3.0-x86.msi'
+
+    silentArgs     = '/qn /norestart'
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/screencloud/update.ps1
@@ -0,0 +1,31 @@
+Import-Module au
+
+$source = "https://screencloud.net/pages/download"
+$pattern = "ScreenCloud-(.+)-x86.msi"
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_SearchReplace {
+    @{
+        ".\legal\VERIFICATION.txt" = @{
+            "(?i)(\s+x32:).*"      = "`${1} $($Latest.URL32)"
+            "(?i)(\checksum32:).*" = "`${1} $($Latest.Checksum32)"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $res = Invoke-WebRequest -Uri $source -UseBasicParsing
+
+    $url = $res.Links | ? href -match $pattern | % href
+    $version = $matches[1]
+
+    @{
+        Version = $version
+        URL32 = "https:$url"
+    }
+}
+
+update -ChecksumFor 32
