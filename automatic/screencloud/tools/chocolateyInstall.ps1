f66581da6c2b53d869fcc046090ff1c94c18f4c3
Adrian L Lange <mail@p3lim.net>
2017-07-05 23:48:59 +0200

    (screencloud) Use the correct properties for embedded packages

--- automatic/screencloud/tools/chocolateyInstall.ps1
+++ automatic/screencloud/tools/chocolateyInstall.ps1
@@ -1,13 +1,14 @@
 $ErrorActionPreference = 'Stop'
 
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileName = 'ScreenCloud-0.0-x86.msi'
+
 $packageArgs = @{
     packageName    = 'screencloud'
-    fileType       = 'msi'
     softwareName   = 'ScreenCloud'
 
-    checksum       = '41b4a0c38dc677820add2a7b2a695e9f9e8e60a14e3e869abe5bfae27e54a2e5'
-    checksumType   = 'sha256'
-    url            = 'https://screencloud.net/files/windows/ScreenCloud-1.3.0-x86.msi'
+    fileType       = 'msi'
+    file           = "$toolsDir\$fileName"
 
     silentArgs     = '/qn /norestart'
     validExitCodes = @(0)
