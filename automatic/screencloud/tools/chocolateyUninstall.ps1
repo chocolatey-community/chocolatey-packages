214f5200172655615fafbc5109681932a8369f7a
Adrian L Lange <mail@p3lim.net>
2017-07-05 23:49:19 +0200

    (screencloud) Avoid outputting host messages to the user

--- automatic/screencloud/tools/chocolateyUninstall.ps1
+++ automatic/screencloud/tools/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 [array]$key = Get-UninstallRegistryKey -SoftwareName 'ScreenCloud'
 
-if($key.Count -eq 1){
+if($($key.Count) -eq 1){
     $key | % {
         $file = "$($_.UninstallString)"
 
@@ -16,11 +16,11 @@ if($key.Count -eq 1){
 
         Uninstall-ChocolateyPackage @packageArgs
     }
-} elseif ($key.Count -eq 0){
+} elseif ($($key.Count) -eq 0){
     Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1){
+} elseif ($($key.Count) -gt 1){
     Write-Warning "$key.Count matches found!"
     Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
     Write-Warning "Please alert package maintainer the following keys were matched:"
-    $key | % {Write-Warning "- $_.DisplayName"}
+    $key | % {Write-Warning "- $($_.DisplayName)"}
 }
