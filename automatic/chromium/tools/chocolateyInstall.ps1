8f048b4fd3e6e1e39f080afa07c9d85bf58b4c2d
Chocolatey <chocolatey@realdimensions.net>
2017-07-08 00:13:08 +0000

    AU: 2 updated - chromium vagrant [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/4209fa6a0d62de8650f02ae22d262c25b411783e

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '61.0.3151.0'
+$version = '61.0.3152.0'
 
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $hive = "hkcu"
@@ -15,14 +15,14 @@ if (Test-Path $Chromium) {
 $packageArgs = @{
   packageName   = 'chromium'
   fileType      = 'exe'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/484466/mini_installer.exe'
-  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/484464/mini_installer.exe'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/485046/mini_installer.exe'
+  url64bit      = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/485046/mini_installer.exe'
   silentArgs    = $silentArgs
   validExitCodes= @(0)
   softwareName  = 'Chromium'
-  checksum      = 'e006c8e6c6696073876ec32f93842b86'
+  checksum      = '74eb816dec55806c0eab16541d247002'
   checksumType  = 'md5'
-  checksum64    = '2a8eaa7233ac336791651f199c624f26'
+  checksum64    = '2caafef81757aff3f8222d558e57d14f'
   checksumType64= 'md5'
 }
 
--- automatic/vagrant/tools/chocolateyinstall.ps1
+++ automatic/vagrant/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName    = 'vagrant'
   fileType       = 'msi'
-  url            = 'https://releases.hashicorp.com/vagrant/1.9.6/vagrant_1.9.6_i686.msi'
-  url64bit       = 'https://releases.hashicorp.com/vagrant/1.9.6/vagrant_1.9.6_x86_64.msi'
-  checksum       = '63e1842d13b84cf1c677d4d95006aa341481d3cbde6cea73bf448f3af9e0a77d'
-  checksum64     = '3b68fc8977d8322d018b856fae59be5eb791ad447c9258a70ff0b7db9593c871'
+  url            = 'https://releases.hashicorp.com/vagrant/1.9.7/vagrant_1.9.7_i686.msi'
+  url64bit       = 'https://releases.hashicorp.com/vagrant/1.9.7/vagrant_1.9.7_x86_64.msi'
+  checksum       = '00b77ecfba84a335c0b8f183596b0858d673a56f44fad160e4f73e10cc672663'
+  checksum64     = 'b600ffadfd9b390795f95a1440160ebdf52bd1fef90c6948718592f145bea365'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart"
