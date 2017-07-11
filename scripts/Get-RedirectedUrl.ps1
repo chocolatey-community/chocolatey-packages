c7939bec7ad60c92ac80eb5a939f8c1ffd9420be
AdmiringWorm <kim.nordmo@gmail.com>
2017-07-11 19:27:26 +0200

    (Get-RedirectUrl) Fixed wrong url returned when not redirected

--- scripts/Get-RedirectedUrl.ps1
+++ scripts/Get-RedirectedUrl.ps1
@@ -8,12 +8,12 @@ function Get-RedirectedUrl() {
   $req = [System.Net.WebRequest]::CreateDefault($url)
   $resp = $req.GetResponse()
 
-  if ($resp -and $resp.ResponseUri -ne $url) {
+  if ($resp -and $resp.ResponseUri.OriginalString -ne $url) {
     Write-Host "Found redirected url '$($resp.ResponseUri)"
-    $result = $resp.ResponseUri
+    $result = $resp.ResponseUri.OriginalString
   } else {
     Write-Warning "No redirected url was found, returning given url."
-    $result = $resp.ResponseUri
+    $result = $url
   }
 
   $resp.Dispose()
