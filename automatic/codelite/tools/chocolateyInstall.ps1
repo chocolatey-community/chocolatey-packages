a911e21aa3d49a9f1c94cc63d9aa5d1ced1d32be
AdmiringWorm <kim.nordmo@gmail.com>
2017-07-11 19:26:45 +0200

    (codelite) Embedded package

--- automatic/codelite/tools/chocolateyInstall.ps1
+++ automatic/codelite/tools/chocolateyInstall.ps1
@@ -1,36 +1,23 @@
-﻿$packageName    = 'codelite'
-$url32          = 'https://github.com/eranif/codelite/releases/download/10.0/codelite-x86-10.0.0.7z'
-$url64          = 'https://github.com/eranif/codelite/releases/download/10.0/codelite-amd64-10.0.0.7z'
-$checksum32     = '8f940b234becc3c6cd6f1754d07ed78e1c8136c2701bd9d076dc021dd0a03026'
-$checksumType32 = 'sha256'
-$checksum64     = '1600ab540848b3422e5151405cb259e98c00a037e4a6ac298dac060b396217f0'
-$checksumType64 = 'sha256'
+﻿$ErrorActionPreference = 'Stop';
 
-$extractDir     = "$env:TEMP/$packageName/$env:chocolateyPackageVersion"
-$fileType       = 'exe'
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$zipArgs        = @{
-  packageName    = $packageName
-  url            = $url32
-  url64          = $url64
-  unzipLocation  = $extractDir
-  checksum       = $checksum32
-  checksumType   = $checksumType32
-  checksum64     = $checksum64
-  checksumType64 = $checksumType64
-}
-
-Install-ChocolateyZipPackage @zipArgs
-
-$executable = Get-ChildItem -Path $extractDir -Filter "*.$fileType" | select -First 1
-
-$packageArgs= @{
-  packageName = $packageName
-  fileType = $fileType
-  silentArgs = '/VERYSILENT /SP- /SUPPRESSMSGBOXES'
-  file = "$extractDir/$executable"
+$packageArgs = @{
+  packageName    = 'codelite'
+  fileType       = 'exe'
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
+  unzipLocation  = Get-PackageCacheLocation
+  softwareName   = 'CodeLite'
+  silentArgs     = '/VERYSILENT /SP- /SUPPRESSMSGBOXES'
   validExitCodes = @(0)
-  softwareName = 'CodeLite'
 }
+
+Get-ChocolateyUnzip @packageArgs
+
+$packageArgs.Remove('file64')
+$packageArgs.file = Get-ChildItem -Path $packageArgs.unzipLocation -Filter "*.exe" | select -first 1
 
 Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -ea 0 "$toolsPath\*.7z"
--- automatic/codelite/update.ps1
+++ automatic/codelite/update.ps1
@@ -1,43 +1,55 @@
-﻿import-module au
+﻿Import-Module AU
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$domain   = "https://github.com"
+$domain   = 'https://github.com'
 $releases = "$domain/eranif/codelite/releases/latest"
+
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix
+  Get-ChildItem "$PSScriptRoot\tools" -Filter "*.7z.7z" | % {
+    Move-Item $_.FullName ($_.FullName.Substring(0, $_.FullName.Length - 3)) -Force
+  }
+  $Latest.FileName32 = $Latest.FileName32 -replace '7z.7z$','7z'
+  $Latest.FileName64 = $Latest.FileName64 -replace '7z.7z$','7z'
+}
+
+function global:au_AfterUpdate { Set-DescriptionFromReadme -SkipFirst 1 }
 
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-      "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-      "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-      "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-      "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-      "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
     }
-    ".\codelite.nuspec" = @{
-      "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotesUrl)</releaseNotes>"
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
   }
 }
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-function global:au_GetLatest () {
-  $download_page = Invoke-WebRequest -Uri $releases
+  $re = 'x86.*\.7z$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { $domain + $_ }
 
-  $re = "codelite.*\.7z$"
-  $urls = $download_page.links | ? href -match $re | select -first 2 -expand href
-  $url32 = $domain + ($urls -match "x86" | select -first 1)
-  $url64 = $domain + ($urls -match "amd64" | select -first 1)
+  $re = 'amd64.*\.7z$'
+  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href | % { $domain + $_ }
 
-  $releaseRe = "ReleaseNotesCodeLite[0-9]+$"
-  $releaseNotes = $download_page.links | ? href -match $releaseRe | select -first 1 -expand href;
+  $verRe = '\/'
+  $version = $url32 -split "$verRe" | select -last 1 -skip 1
 
-  $version = $url32 -split '[\-]|\.7z' | select -last 1 -skip 1;
-
-  return @{
+  @{
     URL32 = $url32
     URL64 = $url64
-    ReleaseNotesUrl = $releaseNotes
     Version = $version
+    ReleaseNotesUrl = Get-RedirectedUrl $releases
+    FileType = '7z'
   }
 }
 
-update
+update -ChecksumFor none
