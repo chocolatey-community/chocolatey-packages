6d013771bd38498513dc376ccec107de198d9d97
RB <RedBaron2@users.noreply.github.com>
2017-07-13 16:30:51 -0700

    Create update.ps1
    update.ps1 for dropbox-beta or dropbox --pre

---
+++ automatic/dropbox-beta/update.ps1
@@ -0,0 +1,40 @@
+
+import-module au
+ . "$PSScriptRoot\..\dropbox\update_helper.ps1"
+
+function global:au_BeforeUpdate {
+  $Latest.ChecksumType32 = 'sha256'
+  $Latest.Checksum32 = Get-RemoteChecksum -Algorithm $Latest.ChecksumType32 -Url $Latest.URL32
+  # Copy the original file from the dropbox folder
+  if (!(Test-Path "$PSScriptRoot\tools" -PathType Container)) { New-Item -ItemType Directory "$PSScriptRoot\tools" }
+  Copy-Item "$PSScriptRoot\..\dropbox\tools" "$PSScriptRoot" -Force -Recurse
+  Copy-Item "$PSScriptRoot\..\dropbox\Readme.md" "$PSScriptRoot" -Force -Recurse
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+ $build = '-beta';
+
+ $oldversion = ($Latest.nuspecversion -replace($build,''));
+
+ $beta = ( drpbx-compare $oldversion -build ($build -replace('-','')) )
+
+ $url = "https://dl-web.dropbox.com/u/17/Dropbox%20${beta}.exe"
+
+ $Latest.PackageName = 'dropbox'
+ $Latest
+
+ return @{ URL32 = $url; Version = ($beta + $build); }
+
+}
+
+update -ChecksumFor none
