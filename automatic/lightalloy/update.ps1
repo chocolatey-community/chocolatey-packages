19b21c47e742543e3cb302379d819a1beb892d63
AdmiringWorm <kim.nordmo@gmail.com>
2017-07-14 02:49:12 +0200

    (lightalloy) Updated script to store cookies

--- automatic/lightalloy/update.ps1
+++ automatic/lightalloy/update.ps1
@@ -12,18 +12,29 @@ function global:au_SearchReplace {
   }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_BeforeUpdate {
+  $fileName = $Latest.Url32 -split '\/' | select -last 1
+  $toolsDir = "$PSScriptRoot\tools"
+  rm "$toolsDir\*.exe"
+
+  $Latest.FileName32 = $fileName
+  Invoke-WebRequest -Uri $Latest.URL32 -WebSession $Latest.WebSession -UseBasicParsing -OutFile "$toolsDir\$fileName"
+  $Latest.Checksum32 = Get-FileHash "$toolsDir\$fileName" -Algorithm SHA256 | % Hash
+}
 function global:au_AfterUpdate  { Set-DescriptionFromReadme -SkipFirst 1 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page = Invoke-WebRequest -Uri $releases -SessionVariable websession -UseBasicParsing
+  $cookies = $websession.Cookies.GetCookies($releases)
+  $download_page = Invoke-WebRequest -Uri "${releases}?attempt=1" -UseBasicParsing -WebSession $websession
   $url = $download_page.Links | ? href -match "LA_Setup_v[0-9\.]+\.exe$" | % href | select -First 1
   $version = $url -split '_v|\.exe' | select -Last 1 -Skip 1
 
   @{
     Version = $version
     Url32   = $url
+    WebSession = $websession
   }
 }
 
-update -ChecksumFor none
+update -ChecksumFor none -NoCheckUrl
