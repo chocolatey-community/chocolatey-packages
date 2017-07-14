5569c492ae8da858cb96b35e2d7723fa31867ab3
Chocolatey <chocolatey@realdimensions.net>
2017-07-14 12:14:20 +0000

    AU: 3 updated - git git.portable zotero-standalone [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/63f9f3cf09552fb069b9bc1673e7c42f47029a71

--- automatic/zotero-standalone/tools/chocolateyInstall.ps1
+++ automatic/zotero-standalone/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero Standalone *'
-  file           = "$toolsPath\Zotero-5.0.3_setup.exe"
+  file           = "$toolsPath\Zotero-5.0.4_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
