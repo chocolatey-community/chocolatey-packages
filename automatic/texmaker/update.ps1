fc7e378fe2fb55d56be7493fba9613a920ca6c34
AdmiringWorm <kim.nordmo@gmail.com>
2017-07-18 22:38:16 +0200

    (texmaker) Updated to the new icon

--- automatic/texmaker/update.ps1
+++ automatic/texmaker/update.ps1
@@ -21,9 +21,6 @@ function global:au_SearchReplace {
       "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
       "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
     }
-    ".\tools\chocolateyUninstall.ps1" = @{
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
-    }
   }
 }
 function global:au_GetLatest {
