a31e2680c9c704b8d680d440a1918f459ed3c81e
Pascal Berger <pascal.berger@gmail.com>
2017-07-23 11:29:17 +0200

    (GH-790) Support Visual Studio Code 64-Bit version

--- automatic/visualstudiocode/update.ps1
+++ automatic/visualstudiocode/update.ps1
@@ -1,7 +1,8 @@
 import-module au
 import-module "$PSScriptRoot/../../extensions/chocolatey-core.extension/extensions/chocolatey-core.psm1"
 
-$releases = 'https://vscode-update.azurewebsites.net/api/update/win32/stable/VERSION'
+$releases32 = 'https://vscode-update.azurewebsites.net/api/update/win32/stable/VERSION'
+$releases64 = 'https://vscode-update.azurewebsites.net/api/update/win32-x64/stable/VERSION'
 
 function global:au_SearchReplace {
     @{
@@ -16,12 +17,17 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $json = Invoke-WebRequest -UseBasicParsing -Uri $releases | ConvertFrom-Json
+    $json32 = Invoke-WebRequest -UseBasicParsing -Uri $releases32 | ConvertFrom-Json
+    $json64 = Invoke-WebRequest -UseBasicParsing -Uri $releases64 | ConvertFrom-Json
+
+    if ($json32.productVersion -ne $json64.productVersion) {
+        throw "Different versions for 32-Bit and 64-Bit detected."
+    }
 
     @{
-        Version   = $json.productVersion
-        URL32     = $json.Url
-        URL64     = $json.Url
+        Version   = $json32.productVersion
+        URL32     = $json32.Url
+        URL64     = $json64.Url
     }
 }
 
