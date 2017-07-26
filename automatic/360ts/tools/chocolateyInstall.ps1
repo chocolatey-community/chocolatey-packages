9b43065562cb8932df76b2b9c884bd0cf8a0c13f
Chocolatey <chocolatey@realdimensions.net>
2017-07-26 18:12:07 +0000

    AU: 1 updated - 360ts [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/acf983f716b703b391e141633f965a50a03bbfd7

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.2.0.1059.exe'
-  checksum               = '4070c22fafb6656595cac4ba274e4cbf592da85d5d053b7e77ee493c4e3bd934'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.2.0.1090.exe'
+  checksum               = '828ed2f001f652ca0977f604b714c9fcfa6630164a57fae0fa13e511c26c1f0f'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
