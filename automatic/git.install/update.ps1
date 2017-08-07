0803d116616c312332c667d64ca1bf427598ddc6
AdmiringWorm <kim.nordmo@gmail.com>
2017-08-07 14:20:03 +0200

    (git*) Updated to parse releases from github

--- automatic/git.install/update.ps1
+++ automatic/git.install/update.ps1
@@ -1,7 +1,8 @@
 import-module au
 Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$releases = "https://git-scm.com/download/win"
+$domain = 'https://github.com'
+$releases = "$domain/git-for-windows/git/releases/latest"
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
@@ -24,11 +25,11 @@ function global:au_GetLatest {
 
     #https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/Git-2.11.0-32-bit.exe
     $re32  = "Git-.+-32-bit.exe"
-    $url32 = $download_page.links | Where-Object href -match $re32 | Select-Object -First 1 -expand href
+    $url32 = $download_page.links | Where-Object href -match $re32 | Select-Object -First 1 -expand href | % { $domain + $_ }
 
     #https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/Git-2.11.0-64-bit.exe
     $re64  = "Git-.+-64-bit.exe"
-    $url64 = $download_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href
+    $url64 = $download_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href | % { $domain + $_ }
 
     $version32 = $url32 -split '-' | Select-Object -Skip 2 -Last 1
     $version64 = $url64 -split '-' | Select-Object -Skip 2 -Last 1
@@ -37,7 +38,7 @@ function global:au_GetLatest {
     @{
         Version = $version32
         URL32   = $url32
-        URL64   = $url64        
+        URL64   = $url64
     }
 }
 
--- automatic/git.portable/update.ps1
+++ automatic/git.portable/update.ps1
@@ -1,7 +1,8 @@
 import-module au
 Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$releases = "https://git-scm.com/download/win"
+$domain = 'https://github.com'
+$releases = "$domain/git-for-windows/git/releases/latest"
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
@@ -24,11 +25,11 @@ function global:au_GetLatest {
 
     #https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/PortableGit-2.11.0-32-bit.7z.exe
     $re32  = "PortableGit-.+-32-bit.7z.exe"
-    $url32 = $download_page.links | Where-Object href -match $re32 | Select-Object -First 1 -expand href
+    $url32 = $download_page.links | Where-Object href -match $re32 | Select-Object -First 1 -expand href | % { $domain + $_ }
 
     #https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/PortableGit-2.11.0-64-bit.7z.exe
     $re64  = "PortableGit-.+-64-bit.7z.exe"
-    $url64 = $download_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href
+    $url64 = $download_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href | % { $domain + $_ }
 
     $version32 = $url32 -split '-' | Select-Object -Skip 2 -Last 1
     $version64 = $url64 -split '-' | Select-Object -Skip 2 -Last 1
@@ -37,7 +38,7 @@ function global:au_GetLatest {
     @{
         Version = $version32
         URL32   = $url32
-        URL64   = $url64        
+        URL64   = $url64
     }
 }
 
