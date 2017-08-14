7641c701ce2c066a830d7116749cc8288f977218
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-08-14 09:52:23 +0200

    (classic-shell) Changed to not use fosshub url

--- automatic/classic-shell/update.ps1
+++ automatic/classic-shell/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     $re      = '.exe'
-    $url     = $download_page.links | ? href -match $re | select -skip 1 | select -First 1
+    $url     = $download_page.links | ? { $_.href -notmatch "fosshub" -and $_.href -match $re } | select -skip 1 | select -First 1
     if ($url.Title -notmatch 'Download latest version ([\d\.]+)' ) { throw "classic-shell version not found on $releases" }
 
     $url32   = $url.href
