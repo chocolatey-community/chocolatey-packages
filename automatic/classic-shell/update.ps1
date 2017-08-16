b466751dd3ca1683850af6f756bd5c1d180a02ab
Pascal Berger <pascal.berger@gmail.com>
2017-08-16 20:45:26 +0200

    (classic-shell) Fix update

--- automatic/classic-shell/update.ps1
+++ automatic/classic-shell/update.ps1
@@ -15,7 +15,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     $re      = '\.exe$'
-    $url32     = $download_page.links | ? { $_.href -notmatch "fosshub" -and $_.href -match $re } | select -skip 1 | select -First 1 -Expand href
+    $url32     = $download_page.links | ? { $_.href -notmatch "fosshub" -and $_.href -match $re } | select -First 1 -Expand href
 
     $version     = $url32 -split 'Setup_|\.exe' | select -last 1 -skip 1 | % { $_ -replace '_','.' }
 
