d6998d01444ce43ff643964f31c2a085ef09d6a1
Chocolatey <chocolatey@realdimensions.net>
2017-08-16 00:13:18 +0000

    AU: 6 updated - nodejs nodejs.install notepadplusplus notepadplusplus.commandline notepadplusplus.install smartftp [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/9407af44c7901a8c4bae898f013596467ca7ab05

--- automatic/notepadplusplus.commandline/tools/chocolateyInstall.ps1
+++ automatic/notepadplusplus.commandline/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'notepadplusplus.commandline'
-$url32       = 'https://notepad-plus-plus.org/repository/7.x/7.4.2/npp.7.4.2.bin.7z'
-$url64       = 'https://notepad-plus-plus.org/repository/7.x/7.4.2/npp.7.4.2.bin.x64.7z'
-$checksum32  = '53c3780fb4079dca1ea90cc0eab2b533cca279d34d31a96c79d7acb6747ed533'
-$checksum64  = '4a4b869930e5fa3b143aa9c185aaf50ece0387344e5929bb7181c7d1aceffc7a'
+$url32       = 'https://notepad-plus-plus.org/repository/7.x/7.5/npp.7.5.bin.7z'
+$url64       = 'https://notepad-plus-plus.org/repository/7.x/7.5/npp.7.5.bin.x64.7z'
+$checksum32  = 'fa8dd184dd807b4d35dac7d3f9c60380a191410283949e336189f9797033cae6'
+$checksum64  = '6328b0f27b47edac7d1a3a2d46a2a69ed0dc4c7338252b99bfc2ca2502c81189'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
--- automatic/notepadplusplus.install/tools/chocolateyInstall.ps1
+++ automatic/notepadplusplus.install/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'notepadplusplus'
-$url32       = 'https://notepad-plus-plus.org/repository/7.x/7.4.2/npp.7.4.2.Installer.exe'
-$url64       = 'https://notepad-plus-plus.org/repository/7.x/7.4.2/npp.7.4.2.Installer.x64.exe'
-$checksum32  = 'abe92fc51f996eed9fa4953fefe5f904c009d0fbfb2b8a50aaec79ae8a9689be'
-$checksum64  = 'bb89c0811f8566c21eda6e5880added61204eba0f13de9631f91e7e4c768cccd'
+$url32       = 'https://notepad-plus-plus.org/repository/7.x/7.5/npp.7.5.Installer.exe'
+$url64       = 'https://notepad-plus-plus.org/repository/7.x/7.5/npp.7.5.Installer.x64.exe'
+$checksum32  = 'ee05e4e2887dfb2b81239183a240edc116fbd6b8f96233c26cc90d30c631a3d0'
+$checksum64  = '8f406da5631a769bf0323889e07d504f4737b2b41a7f48cb0a67f50d77c59b60'
 
 $packageArgs = @{
   packageName    = $packageName
