c9e5151ba7845600a2c9002baabbc1925ece29bd
AdmiringWorm <kim.nordmo@gmail.com>
2017-08-27 10:18:57 +0200

    (mp3tag) Fixed update script

--- automatic/mp3tag/update.ps1
+++ automatic/mp3tag/update.ps1
@@ -14,16 +14,23 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
-    $result = $download_page.Content -match "<h2>Mp3tag v([\d\.]+)</h2>"
+    $result = $download_page.Content -match "<h2>Mp3tag v([\d\.]+)([a-z])?</h2>"
 
     if (!$Matches) {
       throw "mp3tag version not found on $releases"
     }
 
     $version = $Matches[1]
+    $versionSuffix = $Matches[2]
     $url32   = 'http://download.mp3tag.de/mp3tagv<version>setup.exe'
     $flatVersion = $version -replace '\.', ''
-    $url32   = $url32 -replace '<version>', $flatVersion
+    $url32   = $url32 -replace '<version>', "$flatVersion$versionSuffix"
+
+    if ($versionSuffix) {
+      [char]$letter = $versionSuffix
+      [int]$num = $letter - ([char]'a' - 1)
+      $version += ".$num"
+    }
 
     return @{ URL32 = $url32; Version = $version }
 }
