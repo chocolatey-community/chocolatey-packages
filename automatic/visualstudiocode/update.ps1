3a290662a9cc6f34b18b18b88f5a07a27a74a3bc
Miodrag Milic <miodrag.milic@gmail.com>
2017-08-31 20:18:17 +0200

    (visualstudiocode) fixed au_SerchReplace

--- automatic/visualstudiocode/update.ps1
+++ automatic/visualstudiocode/update.ps1
@@ -9,11 +9,11 @@ function global:au_AfterUpdate {Set-DescriptionFromReadme -SkipFirst 1 }
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"    = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum32)'"
         }
      }
 }
