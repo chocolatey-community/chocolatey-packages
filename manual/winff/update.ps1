3a85f9eeaf05dd5c7891e774403612311f34922f
Miodrag Milic <miodrag.milic@gmail.com>
2017-09-11 10:01:14 +0200

    (winff) embedding package

---
+++ manual/winff/update.ps1
@@ -0,0 +1,5 @@
+﻿rm "$PSScriptRoot\tools\*.exe"
+iwr "https://drive.google.com/uc?export=download&id=0B8HoAIi30ZDkZnZxMko1US1NeTg" -UseBasicParsing -OutFile "$PSScriptRoot\tools\WinFF-1.5.5-Setup.exe"
+
+. "$PSScriptRoot\..\..\scripts\Set-DescriptionFromReadme.ps1"
+Set-DescriptionFromReadme -SkipFirst 1
