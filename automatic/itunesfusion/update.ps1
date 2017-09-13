b23bc5b764b222a27d8f6445c0fbc14a6680d2f8
AdmiringWorm <kim.nordmo@gmail.com>
2017-09-13 20:20:54 +0200

    (itunesfusion) Changed to use custom au extension instead of Get-WebURL

--- automatic/itunesfusion/update.ps1
+++ automatic/itunesfusion/update.ps1
@@ -5,7 +5,7 @@ function global:au_GetLatest {
     $downloadEndPointUrl = 'https://www.binaryfortress.com/Data/Download/?package=itunesfusion&log=102'
     $versionRegEx = 'iTunesFusionSetup-([0-9\.\-]+)\.exe'
 
-    $downloadUrl = ((Get-WebURL -Url $downloadEndPointUrl).ResponseUri).AbsoluteUri
+    $downloadUrl = Get-RedirectedUrl $downloadEndPointUrl
     $versionInfo = $downloadUrl -match $versionRegEx
 
     if ($matches) {
