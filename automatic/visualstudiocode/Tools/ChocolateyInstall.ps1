0e5898658a385f3af5e17ed22e533d7cba18eee5
Chocolatey <chocolatey@realdimensions.net>
2017-09-15 00:13:54 +0000

    AU: 4 updated - git-lfs git-lfs.install smartftp visualstudiocode [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/91fc436a25016f25b8d37b8aec5c3168265867ee

--- automatic/visualstudiocode/Tools/ChocolateyInstall.ps1
+++ automatic/visualstudiocode/Tools/ChocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
@@ -16,14 +16,14 @@ ps code -ea 0 | % { $_.CloseMainWindow() | Out-Null }
 $packageArgs = @{
   packageName    = 'visualstudiocode'
   fileType       = 'EXE'
-  url            = 'https://az764295.vo.msecnd.net/stable/41abd21afdf7424c89319ee7cb0445cc6f376959/VSCodeSetup-ia32-1.15.1.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/41abd21afdf7424c89319ee7cb0445cc6f376959/VSCodeSetup-x64-1.15.1.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/27492b6bf3acb0775d82d2f87b25a93490673c6d/VSCodeSetup-ia32-1.16.1.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/27492b6bf3acb0775d82d2f87b25a93490673c6d/VSCodeSetup-x64-1.16.1.exe'
  
   softwareName   = 'Microsoft Visual Studio Code'
  
-  checksum       = ''
+  checksum       = '49896ed32d9c9e27dfa0f376f6866c1cbc4f2a30ae6b622325eaffdfd9be21b2'
   checksumType   = 'sha256'
-  checksum64     = ''
+  checksum64     = '62b6f28d0bd02c0193389fb1b716fed1a2e8d5aea7d38ed39c70e2c2d0b20797'
   checksumType64 = 'sha256'
  
   silentArgs     = "/verysilent /suppressmsgboxes /mergetasks=$mergeTasks /log=""$env:temp\vscode.log"""
