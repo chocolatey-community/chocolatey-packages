3730692293bf3462609d023253384c225744f051
Chocolatey <chocolatey@realdimensions.net>
2017-09-15 18:14:36 +0000

    AU: 2 updated - 360ts youtube-dl [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/f522609d70a1c9817b871b70da1bbfc9e7e9ce7c

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.2.0.1164.exe'
-  checksum               = 'addcebfe74be69aa20de22d4c1231381a9fe82e481f399bf07fb9ddc2061ee50'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.2.0.1256.exe'
+  checksum               = '7b2db7db79720e8b8ad41c7dfc224db3ffc59598a62eba8aadfb555b354dc0ca'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
