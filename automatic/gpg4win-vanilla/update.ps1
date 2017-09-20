8ab476e14a752dab06e09561c2bf1a60526518a9
Miodrag Milic <miodrag.milic@gmail.com>
2017-09-20 15:11:57 +0200

    (gpg4win*) ignore when downloads are not available

--- automatic/gpg4win-vanilla/update.ps1
+++ automatic/gpg4win-vanilla/update.ps1
@@ -25,6 +25,7 @@ function global:au_GetLatest {
     $packageName = Split-Path -Leaf $PSScriptRoot
     $re  = "$packageName-[0-9.]+.exe$"
     $url = $download_page.links | ? href -match $re | select -First 1 -Expand href
+    if (!$url) { Write-Host "There is no download available"; return 'ignore' }
     @{
         Version = $url -split '-|.exe' | select -Last 1 -Skip 1
         URL32   = $url
