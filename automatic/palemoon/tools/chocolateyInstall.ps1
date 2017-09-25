eb7c3d720310e112bdbf2a9cca608d54d5523246
Chocolatey <chocolatey@realdimensions.net>
2017-09-25 06:45:59 +0000

    AU: 1 updated - palemoon [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/a5e6e5cb81aa7eb1fcb2d3f27cf92fcb5e2b3a29

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -8,9 +8,9 @@ $packageArgs = @{
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '90b72bf64ca7ce51437f91b48136d65369e9e480266296d0932be6fd1c6cc595'
+  checksum      = '8f9cce4cf63ae15d2b0800620d310ffd633dfe91cc739bfc2925ece00dc3bff1'
   checksumType  = 'sha256'
-  checksum64    = '5139d492782a5d17176faeb18432c7aa5a49f08c8c6593ce4c73a8d4afe1f165'
+  checksum64    = 'f3b95c4eba6757fc888bddab78dde40ac4ddb0d82bba16a6a4e3d3e1f916c39a'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
