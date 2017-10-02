8f8d1a786f13ebfa41e9f2d1f5fee01ecab7ac9c
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-02 18:07:04 +0200

    (typescript) Migrated package from ferventcoder repo

---
+++ automatic/typescript/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+Update-SessionEnvironment
+
+$npmPath = Get-Command npm | % { $_.Path }
+"Installing $env:chocolateyPackageName using nodejs..."
+Start-ChocolateyProcess $npmPath -statements install,"-g","typescript@0.0"
