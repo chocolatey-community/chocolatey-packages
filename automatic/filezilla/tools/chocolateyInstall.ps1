ae2ae7a9a254c02d743b5478ca323d5c38c68b27
Miodrag Milic <miodrag.milic@gmail.com>
2017-10-02 20:17:36 +0200

    (filezilla) modernized, embedded

--- automatic/filezilla/tools/chocolateyInstall.ps1
+++ automatic/filezilla/tools/chocolateyInstall.ps1
@@ -1,25 +1,15 @@
-﻿$packageName = '{{packageName}}'
-$version = '{{PackageVersion}}'
-$fileType = 'exe'
-$silentArgs = '/S'
-$url = "https://sourceforge.net/projects/filezilla/files/FileZilla_Client/${version}/FileZilla_${version}_win32-setup.exe/download"
-$url64bit = "https://sourceforge.net/projects/filezilla/files/FileZilla_Client/${version}/FileZilla_${version}_win64-setup.exe/download"
+﻿$ErrorActionPreference = 'Stop'
 
-# Even though the installer exits with error code 1223, FileZilla gets installed properly
-$validExitCodes = @(0, 1223)
+$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
 
-$is64bit = Get-ProcessorBits 64
-
-$regPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FileZilla Client'
-$regPathWow6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\FileZilla Client'
-
-# If the 32-bit version was installed on a 64-bit system, uninstall it first
-if ((Test-Path $regPathWow6432) -and $is64bit) {
-
-  Write-Host 'Uninstalling the 32-bit version before installing the 64-bit version …'
-  $uninstallString = (Get-ItemProperty -Path $regPathWow6432).UninstallString
-  Uninstall-ChocolateyPackage 'filezilla 32-bit' 'exe' '/S' $uninstallString
+$packageArgs = @{
+  packageName    = 'filezilla'
+  fileType       = $fileType
+  file           = gi $toolsPath\*_x32.exe
+  file64         = gi $toolsPath\*_x64.exe
+  silentArgs     = '/S'
+  validExitCodes = @(0, 1223)
+  softwareName   = 'FileZilla Client*'
 }
-
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit `
-  -validExitCodes $validExitCodes
+Install-ChocolateyInstallPackage @packageArgs
+ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { touch "$_.ignore" }}
