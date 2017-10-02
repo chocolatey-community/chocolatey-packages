05868ff567d7b0248be55a5fa7c3985fd573b528
Chocolatey <chocolatey@realdimensions.net>
2017-10-02 18:13:26 +0000

    AU: 1 updated - typescript [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/2a2b8acf1e525739c9b90d4cccfe1bf01f2ab7ca

--- automatic/typescript/tools/chocolateyInstall.ps1
+++ automatic/typescript/tools/chocolateyInstall.ps1
@@ -4,4 +4,4 @@ Update-SessionEnvironment
 
 $npmPath = Get-Command npm | % { $_.Path }
 "Installing $env:chocolateyPackageName using nodejs..."
-Start-ChocolateyProcessAsAdmin $npmPath -statements install,"-g","typescript@0.0"
+Start-ChocolateyProcessAsAdmin $npmPath -statements install,"-g","typescript@2.5.3"
