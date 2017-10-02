3edeb9d5a902846e538b23c5be2f4cb52a4c991a
Miodrag Milic <miodrag.milic@gmail.com>
2017-10-02 20:57:16 +0200

    (kitty) migrated from majkinetor

---
+++ automatic/kitty/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$pp = Get-PackageParameters
+if ($pp.Portable) {
+    Write-Host 'Setting portable mode'
+    
+    $iniPath = Join-Path $toolsPath 'kitty.ini'
+    if (Test-Path $iniPath) { $ini = gc $iniPath -Encoding ascii -ea 0 | Out-String }    
+    # kitty ini file doesn't seem to have any other option now
+    if ($ini -notmatch 'savemode=dir') { "[KiTTY]`nsavemode=dir" | Out-File -Encoding ascii $iniPath }
+}
