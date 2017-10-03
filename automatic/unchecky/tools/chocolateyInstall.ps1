4b04fdcebaacd7824a638119f0855130d094067a
Chocolatey <chocolatey@realdimensions.net>
2017-10-03 12:13:37 +0000

    AU: 1 updated - unchecky [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/2ac16a9cd0007131df520fed8404e621558b6ff1

--- automatic/unchecky/tools/chocolateyInstall.ps1
+++ automatic/unchecky/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'unchecky'
 
-  checksum       = '307a5448402fe4adf3eb48a8e54eedceccb5b23520e0dffc297a2a45e33bbd4d'
+  checksum       = '93b0ddf0225ff5f4a867f038965e7f9e5539e48548b4d17a6ebe03be65279795'
   checksumType   = 'sha256'
   url            = 'https://unchecky.com/files/unchecky_setup.exe'
 
