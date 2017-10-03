3db4ce8acc549d6a9a58fe719b0068a1225446f1
Chocolatey <chocolatey@realdimensions.net>
2017-10-03 00:13:32 +0000

    AU: 3 updated - freevideoeditor paint.net zotero-standalone [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/9de5a9856327d625194ca7310ab7150c95f59255

--- automatic/zotero-standalone/tools/chocolateyInstall.ps1
+++ automatic/zotero-standalone/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero Standalone *'
-  file           = "$toolsPath\Zotero-5.0.21_setup.exe"
+  file           = "$toolsPath\Zotero-5.0.22_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
