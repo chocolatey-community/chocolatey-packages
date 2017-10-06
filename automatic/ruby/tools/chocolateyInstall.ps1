5a895229460a68a1ec697e1280ff26342360d4e1
Miodrag Milic <miodrag.milic@gmail.com>
2017-10-06 18:02:44 +0200

    (ruby) fix NoPath
    closes #868

--- automatic/ruby/tools/chocolateyInstall.ps1
+++ automatic/ruby/tools/chocolateyInstall.ps1
@@ -6,6 +6,9 @@ $rubyDir =  'ruby' + ($Env:ChocolateyPackageVersion -replace '\.').Substring(0,2
 $pp = Get-PackageParameters
 $installDir = if ($pp.InstallDir) { $pp.InstallDir } else { Get-ToolsLocation }
 $installDir = Join-Path $installDir $rubyDir
+$tasks = 'assocfiles', 'noridkinstall'
+if ( !$pp.NoPath ) { $taks += 'modpath'  }
+
 Write-Host "Ruby is going to be installed in '$installDir'"
 
 $packageArgs = @{
@@ -13,11 +16,9 @@ $packageArgs = @{
   fileType       = 'exe'
   file           = gi "$toolsDir\*_x32.exe"
   file64         = gi "$toolsDir\*_x64.exe"
-  silentArgs     = '/verysilent /dir="{0}" /tasks="assocfiles, modpath, noridkinstall"' -f $installDir
+  silentArgs     = '/verysilent /dir="{0}" /tasks="{1}"' -f $installDir, ($tasks -join ',')
   validExitCodes = @(0)
   softwareName   = 'ruby *'
 }
 Install-ChocolateyInstallPackage @packageArgs
-rm $toolsDir\*.exe -ea 0
-
-if (!$pp.NoPath) { Install-ChocolateyPath (Join-Path $installDir 'bin') Machine }
+ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" }}
