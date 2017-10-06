6c71120c1d54dcba366d25111180fc24fa24b96a
Chocolatey <chocolatey@realdimensions.net>
2017-10-06 16:16:31 +0000

    AU: 2 updated - clipboardfusion dropbox [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/2feeb01d4c689c2ea3fce26c56a5c9493d954288

--- automatic/clipboardfusion/tools/chocolateyInstall.ps1
+++ automatic/clipboardfusion/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference  = 'Stop'
 $downloadDir            = Get-PackageCacheLocation
-$installer              = 'ClipboardFusionSetup-5.1.exe'
+$installer              = 'ClipboardFusionSetup-5.1.1.exe'
 $arguments              = @{
     packageName         = $env:ChocolateyPackageName
     softwareName        = 'ClipboardFusion'
     file                = Join-Path $downloadDir $installer
-    url                 = 'https://binaryfortressdownloads.com/Download/BFSFiles/104/ClipboardFusionSetup-5.1.exe'
-    checksum            = '0D2BA5EBE45CA2EC358FB58F7D7AB9D6C2FE888263798682E3AAB59DD75E1B2F'
+    url                 = 'https://binaryfortressdownloads.com/Download/BFSFiles/104/ClipboardFusionSetup-5.1.1.exe'
+    checksum            = '762FECEDCA303C525330DC4D074EDEE0981237A68FF471EB5380A9AB011BDD1D'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = '/VERYSILENT /LAUNCHAFTER=0'
