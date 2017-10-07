f0c57906990ced8885374e623837ec2d6106407c
Chocolatey <chocolatey@realdimensions.net>
2017-10-07 16:35:42 +0000

    AU: 1 updated - lightworks [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/a91acd3880dee1debcd80273d0b4aa77d4455ff1

--- automatic/lightworks/tools/chocolateyInstall.ps1
+++ automatic/lightworks/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'lightworks'
   fileType               = 'EXE'
-  url                    = 'http://downloads.lwks.com/v14/lightworks_v14.0.0_full_32bit_setup.exe'
-  url64bit               = 'http://downloads.lwks.com/v14/lightworks_v14.0.0_full_64bit_setup.exe'
+  url                    = 'https://s3.amazonaws.com/lightworks/lightworks_v14.0.0_full_32bit_setup.exe'
+  url64bit               = 'https://s3.amazonaws.com/lightworks/lightworks_v14.0.0_full_64bit_setup.exe'
   checksum               = '4b7a1dd5033bf8501eda63e877d3e498dbbb9c4cd86031690b386da6370c658a'
   checksum64             = '86c7f33d569e2fa1f3151c9856706008a40806501bb13a335aaea751c45ecae6'
   checksumType           = 'sha256'
