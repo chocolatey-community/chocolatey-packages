56ff5a34616125bc7b0262461128bff83e5e66b5
aelius <link.satonaka@outlook.com>
2017-10-14 01:53:29 -0400

    Rework updater with two major changes:
    1. Use builds from https://github.com/henrypp/chromium which offers
    codecs, chrome-sync, and webrtc. With this, chromium is as capable as chrome
    
    2. Specifically follow the "release" versions, mirroring google chrome's
    current release version. As chrome is built from chromium, and they use
    the same versioning schemes, it follows that the stable version of
    chrome is the stable version of chromium. chromium has no official
    builds other than a nightly build with reduced feature set.
    
    https://github.com/chocolatey/chocolatey-coreteampackages/issues/736

--- automatic/chromium/update.ps1
+++ automatic/chromium/update.ps1
@@ -1,14 +1,10 @@
-
 import-module au
 
- $releases_x32 = 'https://chromium.woolyss.com/api/v2/?os=windows&bit=32&out=string' # URL to for GetLatest 32bit
- $releases_x64 = 'https://chromium.woolyss.com/api/v2/?os=windows&bit=64&out=string' # URL to for GetLatest 64bit
+$releases = 'https://github.com/henrypp/chromium/releases/latest'
 
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]version\s*=\s*)('.*')"= "`$1'$($Latest.Version)'"
-            "(^\s*packageName\s*=\s*)('.*')"= "`$1'$($Latest.PackageName)'"
             "(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
             "(^\s*url64Bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
             "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
@@ -19,37 +15,22 @@ function global:au_SearchReplace {
     }
 }
 
-function global:au_GetLatest {
-    $hashtype = 'md5'
-
-    $download_page32 = Invoke-WebRequest -Uri $releases_x32
-    $download_page64 = Invoke-WebRequest -Uri $releases_x64
-
-    $val32 = $download_page32 -split ";"
-    $val64 = $download_page64 -split ";"
-
-    $chromium32 = $val32 | out-string | ConvertFrom-StringData
-    $chromium64 = $val64 | out-string | ConvertFrom-StringData
-    $checksum32 = @{$true = $chromium32.checksum_md5; $false = $chromium32.checksum }[ $chromium32.checksum -ne ""]
-    $checksum64 = @{$true = $chromium64.checksum_md5; $false = $chromium64.checksum }[ $chromium64.checksum -ne ""]
-    $hashtype32 = @{$true = $hashtype; $false = $chromium32.hashtype }[ $chromium32.hashtype -ne ""]
-    $hashtype64 = @{$true = $hashtype; $false = $chromium64.hashtype }[ $chromium64.hashtype -ne ""]
-
-    $version = $chromium64.version
-
-    $url32 = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/<revision>/mini_installer.exe'
-    $url64 = 'https://storage.googleapis.com/chromium-browser-snapshots/Win_x64/<revision>/mini_installer.exe'
-    $url32 = $url32 -replace '<revision>', $chromium32.revision
-    $url64 = $url64 -replace '<revision>', $chromium64.revision
-
-    @{
-        URL32 = $url32; URL64 = $url64; Version = $version;
-        #Diabled until au/issues/36
-        #Checksum32     = $checksum32
-        #Checksum64     = $checksum64
-        ChecksumType32 = $hashtype32
-        ChecksumType64 = $hashtype64
-    }
+function global:au_BeforeUpdate {
+	$Latest.ChecksumType32 = 'sha256'
+	$Latest.ChecksumType64 = 'sha256'
+	$Latest.Checksum32 = Get-RemoteChecksum -Url $Latest.URL32 -Algorithm $Latest.ChecksumType32
+	$Latest.Checksum64 = Get-RemoteChecksum -Url $Latest.URL64 -Algorithm $Latest.ChecksumType64
 }
 
-update
+function global:au_GetLatest {
+	$download_page = (iwr $releases -UseBasicParsing).Links.href | Select-String '/tag/v' | Select-Object -First 1
+	$Matches = $null
+	$download_page -match '\d+\.\d+\.\d+'
+	$version = $Matches[0]
+	$url32 = "https://github.com" + ($download_page -replace "tag","download" -replace "win64","win32") + "/chromium-sync.exe"
+	$url64 = "https://github.com" + ($download_page -replace "tag","download" -replace "win32","win64") + "/chromium-sync.exe"
+
+	return @{ Version = $version; URL32 = $url32; URL64 = $url64 }
+}
+
+Update-Package -ChecksumFor none
