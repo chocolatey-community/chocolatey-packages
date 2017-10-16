5294b7e222764db0147f6400813ddb12afda0339
Chocolatey <chocolatey@realdimensions.net>
2017-10-16 06:12:41 +0000

    AU: 2 updated - flashplayerplugin flashplayerppapi [skip ci] https://gist.github.com/bd2eaa76f2a9ab739ca0544c502dca6e/95b618f08e0e2a6ee5a9eae69242ac2d862f694f

--- automatic/flashplayerplugin/tools/chocolateyInstall.ps1
+++ automatic/flashplayerplugin/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'flashplayerplugin'
   fileType               = 'msi'
-  url                    = 'https://download.macromedia.com/get/flashplayer/pdc/27.0.0.159/install_flash_player_27_plugin.msi'
-  checksum               = '496657e1fb7947f6e216124a9b4880e0fd77eaa27b4a427ead4e19900dbb701d'
+  url                    = 'https://download.macromedia.com/get/flashplayer/pdc/27.0.0.170/install_flash_player_27_plugin.msi'
+  checksum               = 'b5593c299af962ff484a85cc857c9f559e302ae8789abadd962bf0a6a85a08c0'
   checksumType           = 'sha256'
   silentArgs             = '/quiet /norestart REMOVE_PREVIOUS=YES'
   validExitCodes         = @(0)
--- automatic/flashplayerppapi/tools/ChocolateyInstall.ps1
+++ automatic/flashplayerppapi/tools/ChocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'msi'
-  url           = 'https://download.macromedia.com/pub/flashplayer/pdc/27.0.0.159/install_flash_player_27_ppapi.msi'
+  url           = 'https://download.macromedia.com/pub/flashplayer/pdc/27.0.0.170/install_flash_player_27_ppapi.msi'
   silentArgs    = '/quiet /norestart REMOVE_PREVIOUS=YES'
   softwareName  = 'Flash Player PPAPI'
-  checksum      = '56d06d57be40a8775374a40a013953cc0de4b0af26af5b536a0f9b53946d2868'
+  checksum      = 'fe4d8f66c230bfe654e4385bcaa7e1961292c726bc605ee4fe4a5d56f99a0ccb'
   checksumType  = 'sha256'
 }
 
