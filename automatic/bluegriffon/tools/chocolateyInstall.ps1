a4f2417fc1c7a50305e89257cd148204e0a9bb64
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-20 22:40:58 +0200

    (bluegriffon) Embedded package

--- automatic/bluegriffon/tools/chocolateyInstall.ps1
+++ automatic/bluegriffon/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 $packageArgs = @{
   packageName    = 'bluegriffon'
   fileType       = 'exe'
-  softwareName   = 'BlueGriffon*'
-
-  checksum       = 'af87a13bfa255f996a857f1936713279d27345b0ac2f8503ea312a82d39c10b7'
-  checksumType   = 'sha256'
-  url            = 'http://bluegriffon.org/freshmeat/2.3.1/bluegriffon-2.3.1.win32-installer.exe'
-
+  file           = "$toolsPath\"
+  softwareName   = 'bluegriffon*'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -ea 0 "$toolsPath\*.exe","$toolsPath\*.ignore"
--- automatic/bluegriffon/update.ps1
+++ automatic/bluegriffon/update.ps1
@@ -1,13 +1,24 @@
-﻿import-module au
+﻿Import-Module AU
 
 $releases = 'http://bluegriffon.org/'
+$softwareName = 'BlueGriffon*'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
   @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*1\..+)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
+    }
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+    }
+    ".\$($Latest.PackageName).nuspec" = @{
+      "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
     }
   }
 }
@@ -15,12 +26,15 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
-  $re    = '\.exe'
-  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+  $re    = '\.exe$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
 
-  $version  = $url -split '[-]|.win32|.exe' | select -Last 1 -Skip 3
-
-  @{ URL32 = $url; Version = $version }
+  $version32 = $url32 -split '[-]|.win32|.exe$' | select -Last 1 -Skip 3
+  @{
+    URL32 = $url32
+    Version = $version32
+    ReleaseNotes = "https://github.com/therealglazou/bluegriffon/releases/tag/$version32"
+  }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
