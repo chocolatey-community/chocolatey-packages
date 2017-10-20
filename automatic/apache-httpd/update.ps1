94b47ca27ff9731bbe7de7fa1a0697be9f247081
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-20 19:41:42 +0200

    (apache-httpd) Changed update script to grab only vc14 archives [AU apache-httpd]

--- automatic/apache-httpd/update.ps1
+++ automatic/apache-httpd/update.ps1
@@ -1,37 +1,48 @@
 Import-Module au
 
+$releases = 'https://www.apachehaus.com/cgi-bin/download.plx'
+
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix }
 
 function global:au_GetLatest {
-    $downloadPageUrl = 'http://www.apachehaus.com/cgi-bin/download.plx'
-    $versionRegEx = 'httpd\-([0-9\.]+)\-x\d+\-(.*)\.zip'
-    $url32 = 'https://www.apachehaus.com/downloads/httpd-$version-x86-$vcNumber.zip'
-    $url64 = 'https://www.apachehaus.com/downloads/httpd-$version-x64-$vcNumber.zip'
+    $versionRegEx = 'httpd\-([\d\.]+).*\-x86\-(vc14)\.zip'
 
-    $downloadPage = Invoke-WebRequest $downloadPageUrl -UseBasicParsing
+    $downloadPage = Invoke-WebRequest $releases -UseBasicParsing
     $matches = [regex]::match($downloadPage.Content, $versionRegEx)
-    $version = [version]$matches.Groups[1].Value
-    $vcNumber = $matches.Groups[2].Value
+    $version32 = [version]$matches.Groups[1].Value
+    $url32 = "https://www.apachehaus.com/downloads/$($matches.Groups[0].Value)"
 
-    $url32 = $ExecutionContext.InvokeCommand.ExpandString($url32)
-    $url64 = $ExecutionContext.InvokeCommand.ExpandString($url64)
+    $versionRegEx = $versionRegEx -replace 'x86','x64'
+    $matches = [regex]::match($downloadPage.Content, $versionRegEx)
+    $version64 = [version]$matches.Groups[1].Value
+    $url64 = "https://www.apachehaus.com/downloads/$($matches.Groups[0].Value)"
+
+    if ($version32 -ne $version64) {
+      throw "32bit and 64bit version do not match. Please check the update script."
+    }
 
     return @{
         Url32      = $url32
-        FileName32 = [System.IO.Path]::GetFileName($url32)
         Url64      = $url64
-        FileName64 = [System.IO.Path]::GetFileName($url64)
-        Version    = $version
+        Version    = $version32
     }
 }
 
 function global:au_SearchReplace {
-    return @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^[$]fileName32\s*=\s*)('.*')" = "`$1'$($Latest.FileName32)'"
-            "(?i)(^[$]fileName64\s*=\s*)('.*')" = "`$1'$($Latest.FileName64)'"
-        }
+  return @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
     }
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(32-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL32)>"
+      "(?i)(64-Bit.+)\<.*\>"     = "`${1}<$($Latest.URL64)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
+  }
 }
 
 update -ChecksumFor None
