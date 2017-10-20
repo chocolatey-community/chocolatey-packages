63b19d0b60d02070b8454c8593441d7986d5fdb0
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-20 23:26:22 +0200

    (gimp) Enabled InnoSetup logging

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType       = 'exe'
   file           = "$toolsPath\gimp-2.8.22-setup.exe"
   softwareName   = 'GIMP'
-  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).InnoSetup.log`""
   validExitCodes = @(0)
 }
 
