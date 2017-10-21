d84c039a0ceb33f95f76428a1993ac9b2a1d651d
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-21 12:11:33 +0200

    (freecad) Updated package to get the latest version
    When a new version is published when only 32bit or 64bit version have changed
    the update script does not detect this. With this update those publications
    are also detected

--- automatic/freecad/update.ps1
+++ automatic/freecad/update.ps1
@@ -1,6 +1,7 @@
 import-module au
 
-$releases = "http://www.freecadweb.org/wiki/index.php?title=Download"
+$releases = 'https://www.freecadweb.org/wiki/Download'
+$softwareName = 'FreeCAD*'
 
 function global:au_SearchReplace {
   @{
@@ -11,28 +12,40 @@ function global:au_SearchReplace {
       "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
       "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
       "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
     }
     ".\freecad.nuspec" = @{
       "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
     }
+    ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+    }
   }
 }
-
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
-  $re    = '\.exe$'
-  $urls   = $download_page.links | ? href -match $re | select -First 2 -expand href
+  $re = 'x86.*\.exe$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
 
-  $version  = ($urls[0] -split '/' | select -Last 1 -Skip 1)
+  $re = 'x64.*\.exe$'
+  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href
 
-  $releaseNotesUrl = "http://www.freecadweb.org/wiki/index.php?title=Release_notes_" + $version
+  $verRe = '\/'
+  [version]$version32 = $url32 -split "$verRe" | select -last 1 -skip 1
+  [version]$version64 = $url64 -split "$verRe" | select -last 1 -skip 1
+  if ($version32 -ne $version64 -and ($version32.Major -ne $version64.Major -or $version32.Minor -ne $version64 -ne $version64.Minor)) {
+    throw "32bit version do not match the 64bit version"
+  }
+  elseif ($version64 -gt $version32) {
+    $version32 = $version64
+  }
 
   @{
-    URL32 = ($urls -notmatch "64" | select -first 1)
-    URL64 = ($urls -match "64" | select -first 1)
-    Version = $version
-    ReleaseNotes = $releaseNotesUrl
+    URL32 = $url32
+    URL64 = $url64
+    Version = $version32
+    ReleaseNotes = "https://www.freecadweb.org/wiki/Release_notes_$($version32.Major)$($version32.Minor)"
   }
 }
 
