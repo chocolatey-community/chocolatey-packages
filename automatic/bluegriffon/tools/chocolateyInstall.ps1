2a979be7cf149e5b3c8e88eaca9ff04f6edfcb26
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-21 10:37:40 +0200

    (bluegriffon) Fixed silentArgs using wrong name parameter

--- automatic/bluegriffon/tools/chocolateyInstall.ps1
+++ automatic/bluegriffon/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType       = 'exe'
   file           = "$toolsPath\"
   softwareName   = 'bluegriffon*'
-  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).InnoSetup.log`""
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoSetup.log`""
   validExitCodes = @(0)
 }
 
