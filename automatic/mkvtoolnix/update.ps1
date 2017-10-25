dc7bdbce830873e8ed582deb6299269e2fe28f87
Miodrag Milic <miodrag.milic@gmail.com>
2017-10-25 22:15:38 +0200

    (mkvtoolnix) with nosuffix

--- automatic/mkvtoolnix/update.ps1
+++ automatic/mkvtoolnix/update.ps1
@@ -14,7 +14,7 @@ function global:au_SearchReplace {
     }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
