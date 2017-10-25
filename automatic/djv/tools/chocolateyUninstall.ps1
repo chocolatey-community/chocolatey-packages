cae41401cd5cb99cd8d04762aaaa0faeb62e6276
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-26 00:52:17 +0200

    (djv) Updated package

--- automatic/djv/tools/chocolateyUninstall.ps1
+++ automatic/djv/tools/chocolateyUninstall.ps1
@@ -1,20 +1,20 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'djv'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'djv*'
+  fileType      = 'exe'
+  silentArgs    = '/S'
+  validExitCodes= @(@(0))
+}
 
 $uninstalled = $false
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName 'djv-*'
+[array]$key = Get-UninstallRegistryKey @packageArgs
 
 if ($key.Count -eq 1) {
   $key | % {
-
-    $packageArgs = @{
-      packageName = $packageName
-      fileType    = 'exe'
-      silentArgs  = '/S'
-      file        = "$($_.UninstallString)"
-    }
+    $packageArgs['file'] = "$($_.UninstallString)"
 
     Uninstall-ChocolateyPackage @packageArgs
   }
@@ -22,7 +22,7 @@ if ($key.Count -eq 1) {
   Write-Warning "$packageName has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstall."
-  Write-Warning "Please alert the package maintainer the following keys were matched:"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert the package maintainer that the following keys were matched:"
   $key | % { Write-Warning "- $($_.DisplayName)" }
 }
