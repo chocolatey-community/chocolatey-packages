d881ab0c7e8cec6264a0dc3f53599343ea8a226e
AdmiringWorm <kim.nordmo@gmail.com>
2017-10-25 19:31:45 +0200

    (chromium) Fixed formatting of posh scripts

--- automatic/chromium/tools/chocolateyUninstall.ps1
+++ automatic/chromium/tools/chocolateyUninstall.ps1
@@ -3,14 +3,14 @@ $file = $registry.UninstallString
 $Arg_chk = ($file -match "--system-level")
 $chromiumArgs = @{$true = "--uninstall --system-level"; $false = "--uninstall"}[ $Arg_chk ]
 $silentArgs = @{$true = '--uninstall --system-level --force-uninstall'; $false = '--uninstall --force-uninstall'}[ $Arg_chk ]
-$myfile = $file -replace( $chromiumArgs )
+$myfile = $file -replace ( $chromiumArgs )
 # All arguments for the Uninstallation of this package
 $packageArgs = @{
-    PackageName = 'Chromium'
-    FileType = 'exe'
-    SilentArgs = $silentArgs
-    validExitCodes =  @(0,19,21)
-    File = $myfile
+  PackageName    = 'Chromium'
+  FileType       = 'exe'
+  SilentArgs     = $silentArgs
+  validExitCodes = @(0, 19, 21)
+  File           = $myfile
 }
 # Now to Uninstall the Package
 Uninstall-ChocolateyPackage @packageArgs
--- automatic/chromium/update.ps1
+++ automatic/chromium/update.ps1
@@ -1,48 +1,47 @@
 import-module au
 
 function global:au_SearchReplace {
-   @{
-        ".\legal\VERIFICATION.txt" = @{
-            "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
-            "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-            "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-			"(?i)(^\s*checksum32\:).*"          = "`${1} $($Latest.Checksum32)"
-            "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
-			}
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum32\:).*"          = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
+  }
 }
 
 
 function au_BeforeUpdate() {
-    #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
-    Get-RemoteFiles -Purge
+  #Download $Latest.URL32 / $Latest.URL64 in tools directory and remove any older installers.
+  Get-RemoteFiles -Purge
 }
 
 
 function global:au_GetLatest {
+  $allVersions = Invoke-WebRequest -Uri https://api.github.com/repos/henrypp/chromium/releases -UseBasicParsing | ConvertFrom-Json
+  $allStableVersions = $allVersions | Where-Object {$_.body -match "stable"}
+  $latestStableVersionNumber = ($allStableVersions[0].tag_name.split('-') | Select-Object -First 1) -replace 'v', ''
+  $anyArchLatestStablesVersions = $allVersions  | Where-Object {$_.tag_name -match $latestStableVersionNumber}
 
-    $allVersions                    = Invoke-WebRequest -Uri https://api.github.com/repos/henrypp/chromium/releases -UseBasicParsing | ConvertFrom-Json
-    $allStableVersions              = $allVersions | Where-Object {$_.body -match "stable"}
-    $latestStableVersionNumber      = ($allStableVersions[0].tag_name.split('-') | Select-Object -First 1) -replace 'v',''
-    $anyArchLatestStablesVersions   = $allVersions  | Where-Object {$_.tag_name -match $latestStableVersionNumber}
-    
-    $32LatestVersion        = $anyArchLatestStablesVersions | Where-Object {$_.tag_name -match $latestStableVersionNumber -and $_.tag_name -match "win32"}
-    $32LatestSyncInstallUrl = ($32LatestVersion.assets | Where-Object name -match "-sync.exe").browser_download_url
-    
-    $64LatestVersion        = $anyArchLatestStablesVersions | Where-Object {$_.tag_name -match $latestStableVersionNumber -and $_.tag_name -match "win64"}
-    $64LatestSyncInstallUrl = ($64LatestVersion.assets | Where-Object name -match "-sync.exe").browser_download_url
+  $32LatestVersion = $anyArchLatestStablesVersions | Where-Object {$_.tag_name -match $latestStableVersionNumber -and $_.tag_name -match "win32"}
+  $32LatestSyncInstallUrl = ($32LatestVersion.assets | Where-Object name -match "-sync.exe").browser_download_url
 
-    $ChecksumType = 'sha256'
+  $64LatestVersion = $anyArchLatestStablesVersions | Where-Object {$_.tag_name -match $latestStableVersionNumber -and $_.tag_name -match "win64"}
+  $64LatestSyncInstallUrl = ($64LatestVersion.assets | Where-Object name -match "-sync.exe").browser_download_url
 
-    $Latest = @{
-        Version             = $latestStableVersionNumber
-        URL32               = $32LatestSyncInstallUrl
-        ChecksumType32      = $ChecksumType
-        URL64               = $64LatestSyncInstallUrl
-        ChecksumType64      = $ChecksumType
-    };
+  $ChecksumType = 'sha256'
 
-    return $Latest
+  $Latest = @{
+    Version        = $latestStableVersionNumber
+    URL32          = $32LatestSyncInstallUrl
+    ChecksumType32 = $ChecksumType
+    URL64          = $64LatestSyncInstallUrl
+    ChecksumType64 = $ChecksumType
+  };
+
+  return $Latest
 }
 
 Update-Package -ChecksumFor none
