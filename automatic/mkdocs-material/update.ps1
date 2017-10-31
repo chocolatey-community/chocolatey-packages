41f09c8d8b0e5c97029528b4de0aa2d5086a1b17
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-10-31 21:08:28 +0100

    (mkdocs) Changed to also parse dependency version

--- automatic/mkdocs-material/update.ps1
+++ automatic/mkdocs-material/update.ps1
@@ -7,6 +7,9 @@ function global:au_SearchReplace {
         'tools\ChocolateyInstall.ps1' = @{
             "(^[$]version\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"
         }
+        ".\mkdocs-material.nuspec" = @{
+          "(\<dependency .+?`"mkdocs`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.MkDocsDep)`""
+        }
      }
 }
 
@@ -17,7 +20,11 @@ function global:au_GetLatest {
     $url = $download_page.links | ? href -match $re | select -first 1 -expand href
     $version = $url -split '\/' | select -last 1
 
-    return @{ Version = $version }
+    $dependency = $download_page.links | ? href -match 'mkdocs$' | % OuterHtml
+    $dependencyVersion = $dependency -split '(&gt;|>)=|\)' | select -Last 1 -skip 1
+
+    return @{ Version = $version; MkDocsDep = $dependencyVersion }
 }
+
 
 update -ChecksumFor none
