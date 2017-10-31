d1375b9d510151d42df953d80dda4da8f5909879
Gary Ewan Park <gep13@gep13.co.uk>
2017-10-31 20:58:24 +0000

    (maint) Updated link to status report

--- update_all.ps1
+++ update_all.ps1
@@ -82,7 +82,7 @@ $Options = [ordered]@{
                 Port       = $Env:mail_port
                 EnableSsl  = $Env:mail_enablessl -eq 'true'
                 Attachment = "$PSScriptRoot\update_info.xml"
-                UserMessage = 'Update status: http://gep13.me/choco-au'
+                UserMessage = 'Update status: https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f'
                 SendAlways  = $false                        #Send notifications every time
              }
            } else {}
