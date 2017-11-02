7e7a80fdb094a34e019d35cbe777d1a969d8bad2
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-02 16:37:28 +0100

    (python2) Changed to use streams

--- automatic/python2/update.ps1
+++ automatic/python2/update.ps1
@@ -1,28 +1,52 @@
 import-module au
 
-$releases = 'http://www.python.org/downloads/'
+$releases = 'https://www.python.org/downloads'
 
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+            "(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
+            "(^\s*url64bit\s*=\s*)('.*')"    = "`$1'$($Latest.URL64)'"
+            "(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
+            "(^\s*checksum64\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    $url           = $download_page.links | ? href -match 'python-(2.+)\.msi$' | % href
-    $version       = $Matches[1]
+function GetStreams() {
+  param($releaseUrls)
+  $streams = @{ }
 
-    @{
-        Version = $version
-        URL32   = $url
-        URL64   = $url.Replace('.msi', '.amd64.msi')
+  $releaseUrls | % {
+    $version = $_.InnerText -split ' ' | select -last 1
+    $versionTwoPart = $version -replace '([\d]+\.[\d]+).*',"`$1"
+
+    if ($streams.ContainsKey($versionTwoPart)) { return }
+
+    $download_page = Invoke-WebRequest -Uri "https://www.python.org$($_.href)" -UseBasicParsing
+
+    $url32 = $download_page.links | ? href -match "python\-[\d\.]+\.msi$" | select -first 1 -expand href
+    $url64 = $download_page.links | ? href -match "python\-[\d\.]+\.amd64\.msi$" | select -first 1 -expand href
+    if (!($url32) -and !($url64)) {
+      Write-Host "Installers are missing for version '$version', skipping..."
+      return;
     }
+
+    $streams.Add($versionTwoPart, @{ URL32 = $url32 ; URL64 = $url64 ; Version = $version.ToString() })
+  }
+
+  return $streams
 }
 
-update
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $releaseUrls     = $download_page.links | ? { $_.href -match $re -and $_.InnerText -match "^Python 2\.[\d\.]+$" }
+
+    return @{ Streams = GetStreams $releaseUrls }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced by the virtual package python
+    update
+}
