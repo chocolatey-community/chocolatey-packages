eddb904bde01232a75a52a3612129aef87bf496c
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-02 19:54:17 +0100

    (nodejs*) Converted to streams
    fixes #812

--- automatic/nodejs.install/update.ps1
+++ automatic/nodejs.install/update.ps1
@@ -1,29 +1,7 @@
 ﻿import-module au
 
-$releases = "https://nodejs.org/en/download/current/"
-
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if the script is not sourced
-  function global:au_BeforeUpdate {
-    Remove-Item "$PSScriptRoot\tools\*.msi"
-
-    $client = New-Object System.Net.WebClient
-    try
-    {
-      $filePath32 = "$PSScriptRoot\tools\$($Latest.FileName32)"
-      $client.DownloadFile($Latest.URL32, "$filePath32")
-
-      $filePath64 = "$PSScriptRoot\tools\$($Latest.FileName64)"
-      $client.DownloadFile($Latest.URL64, "$filePath64")
-    }
-    finally
-    {
-      $client.Dispose()
-    }
-
-    $Latest.ChecksumType = "sha256"
-    $Latest.Checksum32 = Get-FileHash -Algorithm $Latest.ChecksumType -Path $filePath32 | ForEach-Object Hash
-    $Latest.Checksum64 = Get-FileHash -Algorithm $Latest.ChecksumType -Path $filePath64 | ForEach-Object Hash
-  }
+  function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 }
 
 function global:au_SearchReplace {
@@ -35,7 +13,7 @@ function global:au_SearchReplace {
     ".\legal\verification.txt" = @{
       "(?i)(32-Bit.+)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(64-Bit.+)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType)"
+      "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
       "(?i)(checksum32:\s+).*" = "`${1}$($Latest.Checksum32)"
       "(?i)(checksum64:\s+).*" = "`${1}$($Latest.Checksum64)"
     }
@@ -43,32 +21,40 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $urlsToGrabMsis = @(
+    "https://nodejs.org/en/download"
+    "https://nodejs.org/en/download/current"
+  )
 
-  #https://nodejs.org/dist/v7.4.0/node-v7.4.0-x86.msi
-  $re32  = "node-v(.+)-x86.msi"
-  $url32 = $download_page.links | Where-Object href -match $re32 | Select-Object -First 1 -expand href
-  $version32 = $matches[1]
+  $lts_page = Invoke-WebRequest -Uri "https://github.com/nodejs/Release/blob/master/README.md" -UseBasicParsing
 
-  #https://nodejs.org/dist/v7.4.0/node-v7.4.0-x64.msi
-  $re64  = "node-v(.+)-x64.msi"
-  $url64 = $download_page.links | Where-Object href -match $re64 | Select-Object -First 1 -expand href
-  $version64 = $matches[1]
+  $urlsToGrabMsis += $lts_page.links | ? href -match "\/latest\-v.*\/$" | select -expand href
 
-  $filename32 = [IO.Path]::GetFileName($url32)
-  $filename64 = [IO.Path]::GetFileName($url64)
+  $streams = @{ }
 
-  if ($version32 -ne $version64) {
-    throw "Different versions for 32-Bit and 64-Bit detected."
+  $urlsToGrabMsis | % {
+    $uri = $_
+    $download_page = Invoke-WebRequest -Uri $uri -UseBasicParsing
+
+    $msis = $download_page.links | ? href -match '\.msi$' | select -expand href | % {
+      if (!$_.StartsWith('http')) { return $uri + $_ } else { $_ }
+    }
+
+    $url32 = $msis | ? { $_ -match 'x86' } | select -first 1
+    $version = $url32 -split '\-v?' | select -last 1 -skip 1
+    $versionTwoPart = $version -replace '(^\d+\.\d+).*',"`$1"
+    if ($streams.ContainsKey($versionTwoPart)) { return ; }
+
+    $url64 = $msis | ? { $msis -match "-x64" } | select -first 1
+    if (!$url64) {
+      Write-Error "No 64bit url was found for version '$version', skipping..."
+      return
+    }
+
+    $streams.Add($versionTwoPart, @{ Version = $version ; URL32 = $url32; URL64 = $url64 } )
   }
 
-  return @{
-    URL32 = $url32
-    URL64 = $url64
-    FileName32 = $filename32
-    FileName64 = $filename64
-    Version = $version32
-  }
+  return @{ Streams = $streams }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
--- automatic/nodejs/README.md
+++ automatic/nodejs/README.md
@@ -3,10 +3,9 @@
 
 Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient. Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world.
 
+This package runs the official Node JS installer, resulting in Node.exe and NPM being installed under Program Files, and added to the SYSTEM PATH environment variable.
+
 ## Notes
-This package used to install `nodejs.commandline`. It is recommended that you uninstall the `nodejs.commandline` and `npm` packages in favor of this or the `nodejs.install` (which this `nodejs` package now points to).
+While this package now provides both **Current** and **LTS** releases of Node.js, it is still recommended to use the [nodejs-lts][] package if only targeting the latest LTS release is required.
 
-This package provides the **Current** releases of Node.js.
-
-If you want the **LTS** (Long Term Support) release of Node.js, install the [nodejs-lts](https://chocolatey.org/packages/nodejs-lts) package instead.
-
+[nodejs-lts]: https://chocolatey.org/packages/nodejs-lts "Node.js LTS Package"
