7e28f9c9c5e45bd5d5e38ab9f502834d8df408d7
Miodrag Milic <miodrag.milic@gmail.com>
2017-11-03 20:25:02 +0100

    (msys2) AU updater; legal; nuspec

---
+++ automatic/msys2/update.ps1
@@ -0,0 +1,42 @@
+import-module au
+
+$releases = 'http://repo.msys2.org/distrib'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+        }
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {
+    $re = '\.exe$'
+
+    $download_page = Invoke-WebRequest -Uri "$releases/i686" -UseBasicParsing
+    $url32 = $download_page.Links | ? href -match $re | select -Last 1 | % { "$releases/i686/" + $_.href }
+    $version32 = $url32 -split '/|-|\.exe' | select -Last 1 -Skip 1
+
+    $download_page = Invoke-WebRequest -Uri "$releases/x86_64" -UseBasicParsing
+    $url64 = $download_page.Links | ? href -match $re | select -Last 1 | % { "$releases/x86_64/" + $_.href }
+    $version64 = $url32 -split '/|-|\.exe' | select -Last 1 -Skip 1
+
+    if ($version32 -ne $version64) { throw "x32 and x64 versions are not the same" }
+
+    @{
+        Version      = "$version32.0.0"
+        URL32        = $url32
+        URL64        = $url64
+    }
+}
+
+update -ChecksumFor none
