c7d8a9e1e8413067d3d13d0b1cc00b21fadc1a42
Miodrag Milic <miodrag.milic@gmail.com>
2017-11-04 10:07:46 +0100

    AU updater now uses xz instead of exe

--- automatic/msys2/update.ps1
+++ automatic/msys2/update.ps1
@@ -20,15 +20,15 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 function global:au_GetLatest {
-    $re = '\.exe$'
+    $re = '\.xz$'
 
     $download_page = Invoke-WebRequest -Uri "$releases/i686" -UseBasicParsing
     $url32 = $download_page.Links | ? href -match $re | select -Last 1 | % { "$releases/i686/" + $_.href }
-    $version32 = $url32 -split '/|-|\.exe' | select -Last 1 -Skip 1
+    $version32 = $url32 -split '/|-|\.' | select -Last 1 -Skip 2
 
     $download_page = Invoke-WebRequest -Uri "$releases/x86_64" -UseBasicParsing
     $url64 = $download_page.Links | ? href -match $re | select -Last 1 | % { "$releases/x86_64/" + $_.href }
-    $version64 = $url32 -split '/|-|\.exe' | select -Last 1 -Skip 1
+    $version64 = $url64 -split '/|-|\.' | select -Last 1 -Skip 2
 
     if ($version32 -ne $version64) { throw "x32 and x64 versions are not the same" }
 
