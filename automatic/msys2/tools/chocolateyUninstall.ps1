f2a9eefe4a7f35b1bcb3bf6b27d1ebdaf1d61f97
Miodrag Milic <miodrag.milic@gmail.com>
2017-11-05 16:06:56 +0100

    (msys2) fix path uninstall

--- automatic/msys2/tools/chocolateyUninstall.ps1
+++ automatic/msys2/tools/chocolateyUninstall.ps1
@@ -3,7 +3,14 @@
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $pp = Import-Clixml $toolsDir\pp.xml
-if (!$pp.NoPath)  { Uninstall-ChocolateyPath $pp.InstallDir } 
+if (!$pp.NoPath)  { 
+    $user_path = [System.Environment]::GetEnvironmentVariable('PATH',  'User')
+    $newPath = $user_path.Replace(";" + $pp.InstallDir, '')
+    if ($newPath -ne $user_path) { 
+        Write-Host "Removing from user PATH"
+        [System.Environment]::SetEnvironmentVariable('PATH', $newPath, 'User')
+    }
+} 
 
-Write-Host "Please remove installdir manually when you don't need it anymore."
+Write-Host "Please remove install dir manually when you don't need it anymore."
 Write-Host "Install dir: " $pp.InstallDir
