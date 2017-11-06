421a815a7a42730fb50cccb91052ba5da3653e9a
Miodrag Milic <miodrag.milic@gmail.com>
2017-11-06 18:22:44 +0100

    [AU nircmd] embedded
    #866

--- automatic/nircmd/tools/chocolateyInstall.ps1
+++ automatic/nircmd/tools/chocolateyInstall.ps1
@@ -1,20 +1,15 @@
 $ErrorActionPreference = 'Stop'
 
-$packageName = 'nirmcmd'
-$url32       = 'http://www.nirsoft.net/utils/nircmd.zip'
-$url64       = 'http://www.nirsoft.net/utils/nircmd-x64.zip'
-$checksum32  = 'f56a6166f8956c507cc2bfec71339f3c467c0421fe6d16fbe4c52a36d79ce0ea'
-$checksum64  = '37445b39e2fc437688164866c495bd28890ded5d0dbf71615bf92776e57c5189'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  url64Bit       = $url64
-  checksum       = $checksum32
-  checksum64     = $checksum64
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  unzipLocation  = $toolsPath
+  PackageName    = 'nircmd'
+  FileFullPath   = gi $toolsPath\*_x32.zip
+  FileFullPath64 = gi $toolsPath\*_x64.zip
+  Destination    = $toolsPath
 }
-Install-ChocolateyZipPackage @packageArgs
+
+ls $toolsPath\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
+Get-ChocolateyUnzip @packageArgs
+rm $toolsPath\*.zip -ea 0
+
--- automatic/nircmd/update.ps1
+++ automatic/nircmd/update.ps1
@@ -4,24 +4,26 @@ $releases = 'http://www.nirsoft.net/utils/nircmd.html'
 
 function global:au_SearchReplace {
    @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
+      ".\legal\VERIFICATION.txt" = @{
+        "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+        "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+        "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+      }
     }
 }
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
     $download_page.RawContent -match 'NirCmd v(.+)' | Out-Null
 
-    $version = $Matches[1]
-    $url32   = 'http://www.nirsoft.net/utils/nircmd.zip'
-    $url64   = 'http://www.nirsoft.net/utils/nircmd-x64.zip'
-
-    @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    @{
+      Version = $Matches[1]
+      URL32 = 'http://www.nirsoft.net/utils/nircmd.zip'
+      URL64 = 'http://www.nirsoft.net/utils/nircmd-x64.zip'
+    }
 }
 
-update
+update -ChecksumFor none
