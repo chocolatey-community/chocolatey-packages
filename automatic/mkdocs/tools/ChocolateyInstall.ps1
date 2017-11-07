7f03915a835787cf916c7d4d3f40149975992d5c
Miodrag Milic <miodrag.milic@gmail.com>
2017-11-07 08:38:43 +0100

    (mkdocs*) added proxy handling

--- automatic/mkdocs/tools/ChocolateyInstall.ps1
+++ automatic/mkdocs/tools/ChocolateyInstall.ps1
@@ -2,4 +2,9 @@
 
 $version = '0.17.1'
 
+$proxy = Get-EffectiveProxy
+if ($proxy) {
+  Write-Host "Setting CLI proxy: $proxy"
+  $env:http_proxy = $env:https_proxy = $proxy
+}
 python -m pip install mkdocs==$version
