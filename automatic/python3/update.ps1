3c76a8b8579bd3e7bdb32703c9db411f3b0896dd
Miodrag Milic <miodrag.milic@gmail.com>
2017-11-08 19:22:55 +0100

    (python3) finished streams implementation
    Downloads from /windows page. Uses both msi and exe installers.
    Package installations not tested.
    There might be difference in silent args.

--- automatic/python3/update.ps1
+++ automatic/python3/update.ps1
@@ -1,52 +1,62 @@
 import-module au
 
-$releases = 'https://www.python.org/downloads'
+$releases = 'https://www.python.org/downloads/windows/'
 
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
-            "(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
-            "(^\s*url64bit\s*=\s*)('.*')"    = "`$1'$($Latest.URL64)'"
-            "(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
-            "(^\s*checksum64\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')"    = "`$1'$($Latest.FileType)'"
         }
+
+      ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+      }
     }
 }
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
 function GetStreams() {
   param($releaseUrls)
   $streams = @{ }
 
   $releaseUrls | % {
-    $version = $_.InnerText -split ' ' | select -last 1
+    $version = $_.InnerText.Trim() -split ' ' | select -Index 1
+    if ($version -match '[a-z]') { Write-Host "Skipping prerelease: '$version'"; return }
     $versionTwoPart = $version -replace '([\d]+\.[\d]+).*',"`$1"
 
-    if ($streams.ContainsKey($versionTwoPart)) { return }
+    if ($streams.$versionTwoPart) { return }
 
     $download_page = Invoke-WebRequest -Uri "https://www.python.org$($_.href)" -UseBasicParsing
 
-    $url32 = $download_page.links | ? href -match "python\-[\d\.]+\.exe$" | select -first 1 -expand href
-    $url64 = $download_page.links | ? href -match "python\-[\d\.]+\-amd64\.exe$" | select -first 1 -expand href
-    if (!($url32) -and !($url64)) {
-      Write-Host "Installers are missing for version '$version', skipping..."
-      return;
-    }
+    $url32 = $download_page.links | ? href -match "python-.+.(exe|msi)$" | select -first 1 -expand href
+    $url64 = $download_page.links | ? href -match "python-.+amd64\.(exe|msi)$" | select -first 1 -expand href
+    if (!$url32 -or !$url64) { 
+        Write-Host "Skipping due to missing installer: '$version'"; return }
 
-    $streams.Add($versionTwoPart, @{ URL32 = $url32 ; URL64 = $url64 ; Version = $version.ToString() })
+    if (!$url32.StartsWith('http')) { 
+        $url32 = 'https://www.python.org' + $url32
+        $url64 = 'https://www.python.org' + $url64
+    }
+    $streams.$versionTwoPart = @{ URL32 = $url32 ; URL64 = $url64 ; Version = $version }
   }
 
-  return $streams
+  Write-Host $streams.Count 'streams collected'
+  $streams
 }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
-    $releaseUrls     = $download_page.links | ? { $_.href -match $re -and $_.InnerText -match "^Python 3\.[\d\.]+$" }
+    $releaseUrls = $download_page.links | ? { $_.href -match 'release' -and $_.InnerText -match "^Python 3\..+$" }
 
-    return @{ Streams = GetStreams $releaseUrls }
+    @{ Streams = GetStreams $releaseUrls }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced by the virtual package python
-    update
+    update -ChecksumFor none
 }
