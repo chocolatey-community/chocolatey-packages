cb00d4611c741da98df8252ca1acae220bbac747
Chocolatey <chocolatey@realdimensions.net>
2017-11-09 17:56:41 +0000

    AU: 4 updated - libreoffice nomacs.portable python python3 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a12f2f0f408ee56eca0ea69ce6e270bec4e30451

--- automatic/nomacs.portable/tools/chocolateyInstall.ps1
+++ automatic/nomacs.portable/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ if ((Get-ProcessorBits 32) -or ($env:chocolateyForceX86 -eq 'true')) {
    throw "This package doesn't support 32bit architecture"
 }
 
-$fileName  = 'nomacs-3.6.0.zip'
+$fileName  = 'nomacs-3.8.0.zip'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $zip_path = "$toolsPath\$fileName"
 ls $toolsPath\* | ? { $_.PSIsContainer } | rm -Force -Recurse
