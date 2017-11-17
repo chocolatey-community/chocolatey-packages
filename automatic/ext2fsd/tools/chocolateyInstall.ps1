42d7c00cc79f4b1ab985d792847ec69247a6e63d
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-17 19:53:53 +0100

    (ext2fsd) Removed duplicate 64bit related properties

--- automatic/ext2fsd/tools/chocolateyInstall.ps1
+++ automatic/ext2fsd/tools/chocolateyInstall.ps1
@@ -1,14 +1,11 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName            = 'ext2fsd'
   fileType               = 'exe'
-  url                    = 'https://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.68/Ext2Fsd-0.68.exe/download'
-  url64bit               = 'https://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.68/Ext2Fsd-0.68.exe/download'
-  checksum               = '1546264153c2cf73d3119d3ce0db2d2302aac45a14596a00ead6e39cc72cc05e'
-  checksum64             = '1546264153c2cf73d3119d3ce0db2d2302aac45a14596a00ead6e39cc72cc05e'
+  url                    = 'https://sourceforge.net/projects/ext2fsd/files/Ext2fsd/0.69/Ext2Fsd-0.69.exe/download'
+  checksum               = ''
   checksumType           = 'sha256'
-  checksumType64         = 'sha256'
   silentArgs             = '/VERYSILENT /NORESTART'
   validExitCodes         = @(0)
   softwareName           = 'ext2fsd*'
