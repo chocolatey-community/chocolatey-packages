6037a925786b2142a94e73359f2a5b2347cb4508
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-17 19:55:31 +0100

    (ext2fsd) Changed to use streams (only latest 5 versions)

--- automatic/ext2fsd/update.ps1
+++ automatic/ext2fsd/update.ps1
@@ -1,3 +1,6 @@
+[CmdletBinding()]
+param($IncludeStream, [switch] $Force)
+
 import-module au
 
 $releases = 'https://sourceforge.net/projects/ext2fsd/files/Ext2fsd'
@@ -6,9 +9,7 @@ function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
             "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
         }
@@ -16,21 +17,29 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    $version = $download_page.Links | ? title -match '\.exe:' | % InnerText
-    $version = $version -split '-| ' | ? {[version]::TryParse($_,[ref]($__))}
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    # We only take the latest 5 releases, to keep web requests to a maintainable level
+    $versions = $download_page.links | ? href -match "[\d]+\.[\d\.]+\/$" | select -expand href | % { $_ -split '\/' | select -last 1 -skip 1} | select -first 5
 
-    $releases = "$releases/$version"
-    $download_page = Invoke-WebRequest -Uri $releases
-    $url = $download_page.Links | ? href -match '\.exe/download' | % href | select -First 1
-    @{
-        Version      = $version
-        URL32        = $url
+    $streams = @{}
+
+    $versions | % {
+      $version = Get-Version $_
+      $verReleases = "$releases/$_"
+      $download_page = Invoke-WebRequest -Uri $verReleases -UseBasicParsing
+
+      $url = $download_page.links | ? href -match "$_\.exe/download$" | select -first 1 -expand href
+
+      if ($url) {
+        $streams.Add($version.ToString(2), @{ Version = $version.ToString(); URL32 = $url })
+      }
     }
+
+    return @{ Streams = $streams }
 }
 
 try {
-    update -ChecksumFor 32
+    update -ChecksumFor 32 -IncludeStream $IncludeStream -Force:$Force
 } catch {
     $ignore = "Unable to connect to the remote server"
     if ($_ -match $ignore) { Write-Host $ignore; 'ignore' } else { throw $_ }
