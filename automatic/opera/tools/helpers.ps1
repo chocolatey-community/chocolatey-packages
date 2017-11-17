7d79e3fa839b89c83c119cb73986d230eccc3f28
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-17 21:55:34 +0100

    (opera) Fixed uneeded/invalid code from helper script

--- automatic/opera/tools/helpers.ps1
+++ automatic/opera/tools/helpers.ps1
@@ -4,8 +4,5 @@ function IsVersionAlreadyInstalled {
   if ($env:ChocolateyForce ) { return $false }
 
   [array]$keys = Get-UninstallRegistryKey -SoftwareName "Opera*" -wa 0 | ? { $_.DisplayVersion -and $_.DisplayVersion -eq $version }
-  if (!$keys) { return $false }
-
-  if (Is32BitInstalled -or ($env:ChocolateyForceX86 -eq $true)) { return $keys | ? { $_.PSPath -match 'Wow6432Node' } }
-  else { return $keys | ? { $_.PSPath -notmatch 'Wow6432Node' }}
+  return $keys.Count -gt 0
 }
