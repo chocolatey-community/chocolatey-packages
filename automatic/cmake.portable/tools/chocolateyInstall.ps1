11acb63e13031cc906cad4e7ef1f05182be88d79
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-18 17:44:48 +0100

    (cmake.portable) Modernized + EMbedded

--- automatic/cmake.portable/tools/chocolateyInstall.ps1
+++ automatic/cmake.portable/tools/chocolateyInstall.ps1
@@ -1,11 +1,13 @@
-$packageName = 'cmake.portable'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'http://cmake.org//files/v3.9/cmake-3.9.4-win32-x86.zip'
-$checksum = '8214df1ff51f9a6a1f0e27f9bd18f402b1749c5b645fbf6e401bcb00047171cd'
-$checksumType = 'sha256'
+﻿$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
-                             -Url "$url" `
-                             -UnzipLocation "$toolsDir" `
-                             -Checksum "$checksum" `
-                             -ChecksumType "$checksumType"
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsPath\cmake-3.9.6-win32-x86.zip"
+  file64      = "$toolsPath\cmake-3.9.6-win64-x64.zip"
+  destination = $toolsPath
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
