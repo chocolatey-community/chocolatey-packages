f3759ed66d688c4e8a57edc7624534d072749281
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-18 14:08:42 +0100

    (notepadplusplus.commandline) Embedded package

--- automatic/notepadplusplus.commandline/tools/chocolateyInstall.ps1
+++ automatic/notepadplusplus.commandline/tools/chocolateyInstall.ps1
@@ -1,20 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'notepadplusplus.commandline'
-$url32       = 'https://notepad-plus-plus.org/repository/7.x/7.5.1/npp.7.5.1.bin.7z'
-$url64       = 'https://notepad-plus-plus.org/repository/7.x/7.5.1/npp.7.5.1.bin.x64.7z'
-$checksum32  = '899972b2e8f2aff5a8355d445241a8b5a4d7a83f582c8dcd7705cb498f58880e'
-$checksum64  = '6ed4756967d679001c8466f2440f9c6dbd75e6474823b5a6cae084ba3431a583'
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
-  url            = $url32
-  url64Bit       = $url64
-  checksum       = $checksum32
-  checksum64     = $checksum64
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
-  unzipLocation  = $toolsPath
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsPath\npp.7.5.1.bin.7z"
+  file64      = "$toolsPath\npp.7.5.1.bin.x64.7z"
+  destination = $toolsPath
 }
-Install-ChocolateyZipPackage @packageArgs
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.zip -ea 0
--- automatic/notepadplusplus.commandline/update.ps1
+++ automatic/notepadplusplus.commandline/update.ps1
@@ -1,14 +1,38 @@
 . $PSScriptRoot\..\notepadplusplus\update.ps1
+function global:au_BeforeUpdate {
+  $Latest.URL32 = $Latest.URL32_p
+  $Latest.URL64 = $Latest.URL64_p
+  Get-RemoteFiles -Purge -NoSuffix
 
-function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32_p)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64_p)'"
-            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
-    }
+  if ($Latest.FileName32.EndsWith('.7z.7z')) {
+    $newFileName = [System.IO.Path]::GetFileNameWithoutExtension($Latest.FileName32)
+    Move-Item "$PSScriptRoot\tools\$($Latest.FileName32)" "$PSScriptRoot\tools\$newFileName"
+    $Latest.FileName32 = $newFileName
+  }
+  if ($Latest.FileName64.EndsWith('.7z.7z')) {
+    $newFileName = [System.IO.Path]::GetFileNameWithoutExtension($Latest.FileName64)
+    Move-Item "$PSScriptRoot\tools\$($Latest.FileName64)" "$PSScriptRoot\tools\$newFileName"
+    $Latest.FileName64 = $newFileName
+  }
 }
 
-update
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
+    }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+  }
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+  update -ChecksumFor none
+}
