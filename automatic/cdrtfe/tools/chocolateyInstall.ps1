fc4be4d374d5acd4f010a5e001eb81eb87f6a7b3
Chocolatey <chocolatey@realdimensions.net>
2017-11-18 12:59:32 +0000

    AU: 2 updated - cdrtfe mkvtoolnix [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9de57c601e8bbcae3a479af45de0b21c686454a6

--- automatic/cdrtfe/tools/chocolateyInstall.ps1
+++ automatic/cdrtfe/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\cdrtfe-1.5.7.exe"
+  file           = "$toolsPath\cdrtfe-1.4.1.exe"
   softwareName   = 'cdrtfe*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
