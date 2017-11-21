8d00e260cef94a9f8a8d365bda8acd2f07debe52
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-21 03:10:20 +0100

    (maxthon.commandline) Modernized package

--- automatic/maxthon.commandline/tools/chocolateyInstall.ps1
+++ automatic/maxthon.commandline/tools/chocolateyInstall.ps1
@@ -1,17 +1,13 @@
-﻿try {
-  $installDIr = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
+﻿$ErrorActionPreference = 'Stop'
 
-  $tempDir = "$env:TEMP\chocolatey\maxthon.commandline"
-  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-  $file = Join-Path $tempDir "maxthon.commandline.7z"
-  Get-ChocolateyWebFile 'maxthon.commandline' "$file" 'http://dl.maxthon.com/mx4/maxthon_portable_4.9.4.1000.7z'
-
-  Start-Process "7za" -ArgumentList "x -o`"$installDir`" -y `"$file`"" -Wait
-
-  Write-ChocolateySuccess 'maxthon.commandline'
-} catch {
-  Write-ChocolateyFailure 'maxthon.commandline' "$($_.Exception.Message)"
-  throw
+$packageArgs = @{
+  packageName  = $env:ChocolateyPackageName
+  url          = ''
+  checksum     = ''
+  checksumType = ''
+  destination  = $toolsDir
 }
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/maxthon.commandline/update.ps1
@@ -0,0 +1,31 @@
+﻿Import-Module AU
+Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+
+$releases = 'http://www.maxthon.com/'
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)^(\s*url\s*=\s*)'.*'"          = "`${1}'$($Latest.URL32)'"
+      "(?i)^(\s*checksum\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum32)'"
+      "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = '\/mx[\d]+\/portable\/dl'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { Get-RedirectedUrl ('http://www.maxthon.com' + $_) }
+
+  $verRe = '_|\.7z$'
+  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
+  @{
+    URL32        = $url32 -replace 'http:', 'https:'
+    Version      = $version32
+    ReleaseNotes = "http://www.maxthon.com/mx$($version32 -replace '^(\d+).*$','$1')/changelog/"
+  }
+}
+
+update -ChecksumFor 32
