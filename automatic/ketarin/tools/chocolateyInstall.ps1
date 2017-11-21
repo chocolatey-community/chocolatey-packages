8ac370ca299bb43958d3d4533b98e43d56afaead
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-21 03:58:31 +0100

    (ketarin) Initial Migration

---
+++ automatic/ketarin/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'ketarin' 'https://ketarin.org/download' $toolsDir
+$guiFile = Join-Path $toolsDir Ketarin.exe.gui
+echo ''> $guiFile
