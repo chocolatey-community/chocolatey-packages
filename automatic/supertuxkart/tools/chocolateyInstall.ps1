428961b11ad631b160d4133e33d7bcfe918e597c
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-21 02:38:50 +0100

    (supertuxkart) Added support for 64bit

--- automatic/supertuxkart/tools/chocolateyInstall.ps1
+++ automatic/supertuxkart/tools/chocolateyInstall.ps1
@@ -1,13 +1,16 @@
 $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName            = 'supertuxkart'
-  fileType               = 'exe'
-  url                    = 'https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/0.9.2/supertuxkart-0.9.2.exe/download'
-  checksum               = 'd55e9af26d1adafc3432527925c54aa0e613b6111170caf9715ca65ed4c196a1'
-  checksumType           = 'sha256'
-  silentArgs             = '/S'
-  validExitCodes         = @(0)
-  softwareName           = 'supertuxkart*'
+  packageName    = 'supertuxkart'
+  fileType       = 'exe'
+  url            = 'https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/0.9.2/supertuxkart-0.9.2.exe/download'
+  url64          = ''
+  checksum       = 'd55e9af26d1adafc3432527925c54aa0e613b6111170caf9715ca65ed4c196a1'
+  checksum64     = ''
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = 'supertuxkart*'
 }
 Install-ChocolateyPackage @packageArgs
--- automatic/supertuxkart/update.ps1
+++ automatic/supertuxkart/update.ps1
@@ -3,30 +3,33 @@ import-module au
 $releases = 'https://sourceforge.net/projects/supertuxkart/files/SuperTuxKart/'
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*url64\s*=\s*)('.*')"       = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+      "(?i)(^\s*fileType\s*=\s*)('.*')"    = "`$1'$($Latest.FileType)'"
     }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.links | ? href -match '[\d]+\.[\d\.]+\/$' | % href | select -first 1
-    if (!$url.StartsWith("http")) { $url = 'https://sourceforge.net' + $url }
-    $version = $url -split '/' | select -Last 1 -Skip 1
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $url = $download_page.links | ? href -match '[\d]+\.[\d\.]+\/$' | % href | select -first 1
+  if (!$url.StartsWith("http")) { $url = 'https://sourceforge.net' + $url }
+  $version = $url -split '/' | select -Last 1 -Skip 1
 
-    $download_page =  Invoke-WebRequest $url -UseBasicParsing
+  $download_page = Invoke-WebRequest $url -UseBasicParsing
 
-    $version = $url -split '/' | select -Last 1 -Skip 1
-    @{
-        URL32    = $download_page.links | ? href -match '\.exe/download$' | % href
-        Version  = $version
-        FileType = 'exe'
-    }
+  $version = $url -split '/' | select -Last 1 -Skip 1
+  @{
+    URL32    = $download_page.links | ? href -match 'win32\.exe\/download$' | select -first 1 -expand href
+    URL64    = $download_page.links | ? href -match 'win64\.exe\/download$' | select -first 1 -expand href
+    Version  = $version
+    FileType = 'exe'
+  }
 }
 
-update -ChecksumFor 32
+update
