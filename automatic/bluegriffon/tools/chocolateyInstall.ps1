9aa5f9451852918a6b13852883c373cc42f40b12
Chocolatey <chocolatey@realdimensions.net>
2017-11-22 12:16:31 +0000

    AU: 3 updated - bluegriffon mendeley zotero-standalone [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ea9f5f76fc73742ea883c2ce1fe2261a8f2d0bf3

--- automatic/bluegriffon/tools/chocolateyInstall.ps1
+++ automatic/bluegriffon/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = 'bluegriffon'
   fileType       = 'exe'
-  file           = "$toolsPath\bluegriffon-3.0.win-i686.exe"
+  file           = "$toolsPath\bluegriffon-3.0.1.win-i686.exe"
   softwareName   = 'BlueGriffon*'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoSetup.log`""
   validExitCodes = @(0)
