0c81599a51b9f9c9418ba8dd46caf6d3081ec8ac
xdimquax <dimqua@riseup.net>
2017-11-23 00:08:11 +0300

    Add BleachBit

---
+++ automatic/bleachbit.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+packageName   = $env:ChocolateyPackageName
+file          = "$toolsDir\BleachBit-1.17-setup.exe"
+fileType      = 'exe'
+silentArgs    = "/S"
+}
+
+Install-ChocolateyInstallPackage @packageArgs
