30fa446ac93a0e46e958b134f24a3f604dd1b5af
dimqua <dimqua@users.noreply.github.com>
2017-11-23 14:58:29 +0300

    Update chocolateyInstall.ps1

--- automatic/bleachbit.install/tools/chocolateyInstall.ps1
+++ automatic/bleachbit.install/tools/chocolateyInstall.ps1
@@ -1,10 +1,13 @@
-﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-packageName   = $env:ChocolateyPackageName
-file          = "$toolsDir\BleachBit-1.17-setup.exe"
-fileType      = 'exe'
-silentArgs    = "/S"
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = "BleachBit"
+  file          = "$toolsDir\BleachBit-1.17-setup.exe"
+  fileType      = "exe"
+  silentArgs    = "/S"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem -Path $toolsDir\*.exe | % { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
