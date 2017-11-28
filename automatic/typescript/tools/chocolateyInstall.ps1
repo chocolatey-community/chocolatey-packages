9e7489c261167f37ffb8fe26d12de8d68a42ce97
Chocolatey <chocolatey@realdimensions.net>
2017-11-28 00:29:33 +0000

    AU: 5 updated - notepadplusplus notepadplusplus.commandline notepadplusplus.install smartftp typescript [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2efedf71c1cf9cd7d1a03160e1c4e1d33624a9b3

--- automatic/typescript/tools/chocolateyInstall.ps1
+++ automatic/typescript/tools/chocolateyInstall.ps1
@@ -4,4 +4,4 @@ Update-SessionEnvironment
 
 $npmPath = Get-Command npm | % { $_.Path }
 "Installing $env:chocolateyPackageName using nodejs..."
-Start-ChocolateyProcessAsAdmin $npmPath -statements install,"-g","typescript@2.6.1"
+Start-ChocolateyProcessAsAdmin $npmPath -statements install,"-g","typescript@2.6.2"
