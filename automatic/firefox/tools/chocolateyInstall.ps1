6c10b6b25f16c98106fe230ac2532278da5a9f3a
Chocolatey <chocolatey@realdimensions.net>
2017-11-30 12:12:41 +0000

    AU: 2 updated - Firefox git.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8c2249b03e2bf1bd6381df0f6c113ecc9ddd82ff

--- automatic/firefox/tools/chocolateyInstall.ps1
+++ automatic/firefox/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'Firefox'
 $softwareName = 'Mozilla Firefox'
 
-$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '57.0')
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '57.0.1')
 
 if (Get-32bitOnlyInstalled -product $softwareName) {
   Write-Output $(
@@ -34,7 +34,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0/win32/${locale}/Firefox%20Setup%2057.0.exe"
+    Url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0.1/win32/${locale}/Firefox%20Setup%2057.0.1.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
@@ -43,7 +43,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
   if (!(Get-32bitOnlyInstalled($softwareName)) -and (Get-ProcessorBits 64)) {
     $packageArgs.Checksum64 = $checksums.Win64
     $packageArgs.ChecksumType64 = 'sha512'
-    $packageArgs.Url64 = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0/win64/${locale}/Firefox%20Setup%2057.0.exe"
+    $packageArgs.Url64 = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0.1/win64/${locale}/Firefox%20Setup%2057.0.1.exe"
   }
 
   Install-ChocolateyPackage @packageArgs
