2e1b019d2952718dd296b36ca9c620f8c4fb5b73
Chocolatey <chocolatey@realdimensions.net>
2017-12-07 06:13:07 +0000

    AU: 1 updated - partitionmasterfree [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/93130617b45309e9deb852ed9b7c5b3315962698

--- automatic/partitionmasterfree/tools/chocolateyInstall.ps1
+++ automatic/partitionmasterfree/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
   packageName    = 'partitionmasterfree'
   fileType       = 'EXE'
   url            = 'http://download.easeus.com/free/epm.exe'
-  checksum       = '112144f9139a5d5ca3936b1c8a385bd2152632d09a787b70dc6b281e79819a94'
+  checksum       = '1634ebb2c21061796ac3c29d1913738402e1323ce2abcf22580653e3bd764cf2'
   checksumType   = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
