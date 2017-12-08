8f5559a90e905fd2127b27e3d7774e7bd9f73961
Chocolatey <chocolatey@realdimensions.net>
2017-12-08 18:16:08 +0000

    AU: 4 updated - 360ts juju nodejs nodejs.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/73d6ee58da7c95157d389251137b940fcb78fff2

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1040.exe'
-  checksum               = 'dbe3b99be8aaf2a53388c31cca2298481310c38111de060f437c0be5705ac307'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1070.exe'
+  checksum               = '5a505416bd469af8e3e0050cdfe17d52b1ac29a20296a8857efc26607919423c'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
