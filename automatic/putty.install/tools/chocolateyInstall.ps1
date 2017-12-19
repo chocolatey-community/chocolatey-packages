c84316ec519bfb02ea53a4da17243b787504e814
Miodrag Milic <miodrag.milic@gmail.com>
2017-12-19 13:06:53 +0100

    [AU putty putty.install putty.portable] added msi logging; docs improved
    #849

--- automatic/putty.install/tools/chocolateyInstall.ps1
+++ automatic/putty.install/tools/chocolateyInstall.ps1
@@ -8,9 +8,8 @@ $packageArgs = @{
     SoftwareName   = "PuTTY"
     File           = "$toolsPath\putty-0.70-installer.msi"
     File64         = "$toolsPath\putty-64bit-0.70-installer.msi"
-    SilentArgs     = "/quiet"
+    SilentArgs     = '/qn /norestart /l*v "{0}\install.log"' -f "$Env:TEMP\chocolatey\$Env:ChocolateyPackageName\$Env:ChocolateyPackageVersion"
     ValidExitCodes = @(0)
 }
 Install-ChocolateyInstallPackage @packageArgs
-
 Remove-Item -Force "$toolsPath\*.msi","$toolsPath\*.ignore" -ea 0
