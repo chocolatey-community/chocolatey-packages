2a79399a107bb5fcf14c77a89cb86c22437f29a2
RB <RedBaron2@users.noreply.github.com>
2017-12-20 11:27:26 -0800

    Update chocolateyInstall.ps1
    added Get-CompareVersion

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -10,13 +10,28 @@ if (Test-Path $Chromium) {
   $silentArgs = '--system-level --do-not-launch-chrome'
 }
 
+function Get-CompareVersion {
+  param(
+    [string]$version,
+    [string]$notation,
+    [string]$package
+  )
+    $packver = @{$true = $version; $false = ($version -replace($notation,""))}[ ( $version -notmatch $notation ) ]
+    [array]$key = Get-UninstallRegistryKey -SoftwareName "$package*"
+    if ($packver -eq ( $key.Version )) {
+      Write-Host "$package $version is already installed."
+      return
+    }
+  }
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$version = "63.0.3239.108"
+Get-CompareVersion -version $version -notation "-snapshots" -package "chromium"
 
 $packageArgs = @{
   packageName   = 'chromium'
-  file          = "$toolsdir\chromium-sync_x32.exe"
-  file64        = "$toolsdir\chromium-sync_x64.exe"
+  file          = "$toolsdir\chromium_x32.exe"
+  file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
   silentArgs    = $silentArgs
   validExitCodes= @(0)
@@ -24,3 +39,4 @@ $packageArgs = @{
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+rm $toolsDir\*.exe -ea 0 -force
