906e8762e93b84d0a551401f6bc81b24bb35d297
RB <RedBaron2@users.noreply.github.com>
2017-12-20 08:22:32 -0800

    Update update.ps1
    updated update

--- automatic/windows10-media-creation-tool/update.ps1
+++ automatic/windows10-media-creation-tool/update.ps1
@@ -1,4 +1,3 @@
-
 import-module au
 
 function global:au_SearchReplace {
@@ -15,32 +14,21 @@ function global:au_BeforeUpdate {
 	Remove-Item ".\tools\*.exe" -Force # Removal of downloaded files
 }
 
-function Get-FileVersion {
-param(
-    [string]$url,
-    [string]$file
-)
-    $packageName = $($Latest.PackageName)
-    if (!(Test-Path "${env:temp}\chocolatey\$packageName" -PathType Container)) {
-    New-Item -ItemType Directory "${env:temp}\chocolatey\$packageName" }
-    Invoke-WebRequest -Uri $url -OutFile "${env:temp}\chocolatey\$packageName\$file"
-    $filer = Get-Item "${env:temp}\chocolatey\$packageName\*.exe" | select -First 1
-	$version = $filer.VersionInfo.FileVersion -replace '((\d+.\d+.\d+.\d+))*','$1'
-    if (( $version -match " " )) { $like=$true;$version = $version -split(" ") }
-    $version = @{$true=$version;$false=$version[0]}[ $version -isnot [system.array] ]
-    return $version
-}
-
 function global:au_GetLatest {
 
-$url = 'http://go.microsoft.com/fwlink/?LinkId=691209'
-$fileName = "MediaCreationTool.exe"
-
+	$fileName = "MediaCreationTool.exe"
+	$url = 'http://go.microsoft.com/fwlink/?LinkId=691209'
+	Invoke-WebRequest -Uri $url -OutFile ".\tools\$fileName"
+	$regex = "((\d+.\d+.\d+.\d+))"
+    $filer = Get-Item ".\tools\*.exe"
+	$version = $filer.VersionInfo.FileVersion -replace '$regex*','$1'
+    $version = $version -match $regex;
+	$version = $Matches[0]
 
 	@{
 		fileType	= 'exe'
 		URL32		= $url
-		Version		= (Get-FileVersion -url $url -file $fileName)
+		Version		= $version
     }
 }
-update
+update -ChecksumFor 32
