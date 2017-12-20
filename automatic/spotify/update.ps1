c6d03a69ebe49c964a60614116216133413321ec
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-12-20 19:54:36 +0100

    (spotify) Improved etag detection

--- automatic/spotify/update.ps1
+++ automatic/spotify/update.ps1
@@ -13,46 +13,51 @@ function global:au_SearchReplace {
   }
 }
 
+function global:au_AfterUpdate {
+  "$($Latest.ETAG)|$($Latest.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
+}
+
 function GetResultInformation([string]$url32) {
   $dest = "$env:TEMP\spotify.exe"
   Get-WebFile $url32 $dest | Out-Null
-  $version = Get-Item $dest | % { $_.VersionInfo.ProductVersion -replace '^([\d]+(\.[\d]+){1,3}).*','$1' }
+  $version = Get-Item $dest | % { $_.VersionInfo.ProductVersion -replace '^([\d]+(\.[\d]+){1,3}).*', '$1' }
 
   $result = @{
-    URL32 = $url32
-    Version = $version
-    Checksum32 = Get-FileHash $dest -Algorithm SHA512 | % Hash
+    URL32          = $url32
+    Version        = $version
+    Checksum32     = Get-FileHash $dest -Algorithm SHA512 | % Hash
     ChecksumType32 = 'sha512'
   }
   Remove-Item -Force $dest
   return $result
 }
 
-function global:au_GetLatest {
+function GetETagIfChanged() {
+  param([string]$uri)
+  if (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
+    $existingETag = $etag = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -first 1
+  }
+  else { $existingETag = $null }
 
-  if (($global:au_Force -ne $true) -and (Test-Path "$PSScriptRoot\info")) {
-    $items = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' }
-    $headers = Get-WebHeaders $downloadUrl
-    if ($items) {
-      $etag = $items[0]
-      $version = $items[1]
-      if ($headers.ETag -ne $etag) {
-        $result = GetResultInformation $downloadUrl
-        "$($headers.ETag)|$($result.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
-      }
-      else {
-        $result = @{ URL32 = $downloadUrl ; Version = $version }
-      }
-    }
-    else {
-      $result = GetResultInformation $downloadUrl
-      "$($headers.ETag)|$($result.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
-    }
+  $etag = Invoke-WebRequest -Method Head -Uri $uri -UseBasicParsing
+  $etag = $etag | % { $_.Headers.ETag }
+  if ($etag -eq $existingETag) { return $null }
+
+  return $etag
+}
+
+function global:au_GetLatest {
+  $etag = GetETagIfChanged -uri $downloadUrl
+
+  if ($etag) {
+    $result = GetResultInformation $downloadUrl
+    $result["ETAG"] = $etag
   }
   else {
-    $headers = Get-WebHeaders $downloadUrl
-    $result = GetResultInformation $downloadUrl
-    "$($headers.ETag)|$($result.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
+    $result = @{
+      URL32   = $downloadUrl
+      Version = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -Last 1
+    }
   }
 
   return $result
