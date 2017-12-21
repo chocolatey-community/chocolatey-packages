3f48ed033a59b8890ad8ae8b0b13730e5ebe0660
RB <RedBaron2@users.noreply.github.com>
2017-12-21 12:26:40 -0800

    Update chocolateyInstall.ps1
    to use Register-Application

--- automatic/windows10-media-creation-tool/tools/chocolateyInstall.ps1
+++ automatic/windows10-media-creation-tool/tools/chocolateyInstall.ps1
@@ -12,12 +12,4 @@ $packageArgs = @{
 }
 Get-ChocolateyWebFile @packageArgs
 
-$AppPathKey = "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\$($exeName)"
-If (!(Test-Path $AppPathKey)) {New-Item "$AppPathKey" | Out-Null}
-Set-ItemProperty -Path $AppPathKey -Name "(Default)" -Value "$($packageArgs.FileFullPath)" -Force
-Set-ItemProperty -Path $AppPathKey -Name "Path" -Value "$toolsDir" -Force
-
-Write-Output "******************************************************************************************************************"
-Write-Output "*  INSTRUCTIONS: In a prompt type `"$exeName`" to start the Windows 10 Media Creation Tool.           *"
-Write-Output "*  You may also type `"$exeName`" in the search prompt of your start menu                             *"
-Write-Output "******************************************************************************************************************"
+Register-Application "$toolsDir\$exeName"
