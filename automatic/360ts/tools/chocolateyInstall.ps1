3249b060d61ed22af029323ca4553c60111b9863
Chocolatey <chocolatey@realdimensions.net>
2017-12-21 06:14:42 +0000

    AU: 2 updated - 360ts gom-player [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/58134510dfbedb907285bc1c3db536c98f016de1

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1097.exe'
-  checksum               = 'c3d34c0de8af55cccb5e455eb7afabd64f4410b72a417d90561a6cbff5c388b8'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1098.exe'
+  checksum               = '24c7564fe499d634939bb16bb3bc6aee57cbf624e6b0b48bbe98a75e88a9b2a0'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
