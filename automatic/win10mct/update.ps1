32e547320bf0a59407917d2ee22469c777373583
RB <RedBaron2@users.noreply.github.com>
2017-12-22 21:16:16 -0800

    Add files via upload
    moved extra functions to helper

--- automatic/win10mct/update.ps1
+++ automatic/win10mct/update.ps1
@@ -1,5 +1,6 @@
 ﻿import-module au
 Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+ . "$PSScriptRoot\..\win10mct\update_helper.ps1"
 
 function global:au_SearchReplace {
   @{
@@ -13,40 +14,6 @@ function global:au_SearchReplace {
 
 function global:au_AfterUpdate {
   "$($Latest.ETAG)|$($Latest.Version)" | Out-File "$PSScriptRoot\info" -Encoding utf8
-}
-
-function GetETagIfChanged() {
-  param([string]$url)
-
-  if (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
-    $existingETag = Get-Content "$PSScriptRoot\info" -Encoding UTF8 | select -first 1 | % { $_ -split '\|' } | select -First 1
-  }
-  else {
-    $existingETag = $null
-  }
-
-  $etag = Invoke-WebRequest -Method Head -Uri $url -UseBasicParsing
-  $etag = $etag | % { $_.Headers.ETag }
-  if ($etag -eq $existingETag) { return $null }
-
-  return $etag
-}
-
-function GetResultInformation() {
-  param([string]$url32)
-
-  $dest = "$env:TEMP\w10mct.exe"
-  Invoke-WebRequest -UseBasicParsing -Uri $url32 -OutFile $dest
-  $version = Get-Item $dest | % { $_.VersionInfo.ProductVersion -replace '^(\d+(\.[\d]+){1,3}).*', '$1' }
-
-  $result = @{
-    URL32          = $url32
-    Version        = $version
-    Checksum32     = Get-FileHash $dest -Algorithm SHA512 | % Hash
-    ChecksumType32 = 'sha512'
-  }
-  Remove-Item -Force $dest
-  return $result
 }
 
 function global:au_GetLatest {
@@ -65,4 +32,5 @@ function global:au_GetLatest {
 
   return $result
 }
-update -ChecksumFor none
+
+    update -ChecksumFor none
