58cd0cdda778620ba03b3f2607883af3c33134fa
RB <RedBaron2@users.noreply.github.com>
2017-12-22 21:27:58 -0800

    Update update.ps1
    meta troubles
    previous package name changed id tag also

--- automatic/windows10-media-creation-tool/update.ps1
+++ automatic/windows10-media-creation-tool/update.ps1
@@ -7,14 +7,14 @@ function global:au_BeforeUpdate {
 function global:au_SearchReplace {
    @{
         ".\windows10-media-creation-tool.nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName)`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
+            "(\<dependency .+?`"$($Latest.Name)`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
 function global:au_GetLatest {
     @{
-	  PackageName = "win10mct"
+      Name = "win10mct"
       Version = Get-Content "$PSScriptRoot\..\win10mct\info" -Encoding UTF8 | select -First 1 | % { $_ -split '\|' } | select -Last 1
     }
 
