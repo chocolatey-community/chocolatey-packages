439a06e5d87d33bab39020c86099d3ca80c9b67b
Chocolatey <chocolatey@realdimensions.net>
2018-01-09 18:13:55 +0000

    AU: 5 updated - 360ts paint.net python python3 smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a6d7e6185b66c0136abe26615272fce1b8603761

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1174.exe'
-  checksum               = '112430d5b4c06f3e91b43344c7a891e0feba9ca2fe83771b4a1aa5b097182e20'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1175.exe'
+  checksum               = '98cf0dfc3f693cf3e11b870478829541e3348d6d058f75c4dcf23742dec7db41'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
