8cb6c499e7189fabdfe02e814e01d42b57c35c2a
AdmiringWorm <kim.nordmo@gmail.com>
2018-01-11 17:25:51 +0100

    (sidesync) Fixed update script

--- automatic/sidesync/update.ps1
+++ automatic/sidesync/update.ps1
@@ -1,31 +1,28 @@
 import-module au
+import-module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = 'http://www.samsung.com/global/download/Sidesyncwin'
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+      "(?i)(^\s*fileType\s*=\s*)('.*')"    = "`$1'$($Latest.FileType)'"
     }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    $url = ($download_page.AllElements |  ? tagName -eq 'meta' | % content) -split 'URL=' | select -Last 1
-    $url = $url -replace '&amp;', '&'
-    $download_page = iwr $url -MaximumRedirection 0 -ea 0 -UseBasicParsing
-    $url = $download_page.links.href
-    $version  = $url -split '_|.exe' | select -Last 1 -skip 1
+  $url = Get-RedirectedUrl $releases
+  $version = $url -split '_|.exe' | select -Last 1 -skip 1
 
-    @{
-        Version      = $version
-        URL32        = $url
-        URL64        = $url
-    }
+  @{
+    Version = $version
+    URL32   = $url
+    URL64   = $url
+  }
 }
 
 update
