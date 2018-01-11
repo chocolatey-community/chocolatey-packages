646ebc3394afa51857538f3df6ece3eb4d499748
Chocolatey <chocolatey@realdimensions.net>
2018-01-11 23:35:33 +0000

    AU: 1 updated - win10mct [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9e2d64c2dbe82a3b4c403e13333d7955982ae47c

--- automatic/win10mct/tools/chocolateyInstall.ps1
+++ automatic/win10mct/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   PackageName  = $env:chocolateyinstall
   FileFullPath = "$toolsDir\$exeName"
   Url          = 'https://download.microsoft.com/download/A/B/E/ABEE70FE-7DE8-472A-8893-5F69947DE0B1/MediaCreationTool.exe'
-  Checksum     = ''
-  ChecksumType = ''
+  Checksum     = 'B83CB0E7938BBECFCD12FAC51CFFCAB859599D9CD99376B9427AD316E01D76754C3E947DF50FE0978097A30B5BE562EA2F3C47140526608276E3609BA252EB56'
+  ChecksumType = 'sha512'
 }
 Get-ChocolateyWebFile @packageArgs
 
