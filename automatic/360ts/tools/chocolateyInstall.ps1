ef0db879f48964a6ae719c878b587a043dde73fb
Chocolatey <chocolatey@realdimensions.net>
2018-01-12 18:13:39 +0000

    AU: 2 updated - 360ts gpg4win [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e1f9f3ef40fb6414309b933b6da06cffcd586751

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1175.exe'
-  checksum               = '98cf0dfc3f693cf3e11b870478829541e3348d6d058f75c4dcf23742dec7db41'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1187.exe'
+  checksum               = '7553c4e4e5df219cf1554af2119566c8becf983d98d40afb185147bcca14552e'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/gpg4win/tools/chocolateyInstall.ps1
+++ automatic/gpg4win/tools/chocolateyInstall.ps1
@@ -7,10 +7,10 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'gpg4win'
   fileType       = $fileType
-  file           = gi $toolsPath\*.exe
+  file           = Get-Item $toolsPath\*.exe
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Gpg4Win *'
 }
 Install-ChocolateyInstallPackage @packageArgs
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { touch "$_.ignore" }}
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { touch "$_.ignore" }}
