05333d37a7c8821ac95debdf6ab99b910302a325
Chocolatey <chocolatey@realdimensions.net>
2018-01-12 12:13:40 +0000

    AU: 1 updated - krita [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/23a72484c70e64bc66f25564343c1a65620d2afc

--- automatic/krita/tools/chocolateyInstall.ps1
+++ automatic/krita/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-file          = "$toolsDir\krita-3.3.2-x86-setup.exe"
-file64        = "$toolsDir\krita-3.3.2-x64-setup.exe"
+file          = "$toolsDir\krita-3.3.3-x86-setup.exe"
+file64        = "$toolsDir\krita-3.3.3-x64-setup.exe"
 fileType      = 'exe'
 packageName   = 'krita'
 softwareName  = 'Krita'
@@ -10,4 +10,4 @@ silentArgs    = "/S"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
-ls $toolsDir\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { Set-Content -Value "" -Path "$_.ignore" }}
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content -Value "" -Path "$_.ignore" }}
