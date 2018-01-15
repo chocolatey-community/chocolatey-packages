fdb051b6fdf525ee58551750b89f4a9c3b352b35
Chocolatey <chocolatey@realdimensions.net>
2018-01-15 15:55:23 +0000

    AU: 3 updated - cmake cmake.install cmake.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b82dfa8dc82f4fb883f5df7216450f700d6963d4

--- automatic/cmake.install/tools/chocolateyInstall.ps1
+++ automatic/cmake.install/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file           = "$toolsPath\cmake-3.9.6-win32-x86.msi"
-  file64         = "$toolsPath\cmake-3.9.6-win64-x64.msi"
+  file           = "$toolsPath\cmake-3.10.1-win32-x86.msi"
+  file64         = "$toolsPath\cmake-3.10.1-win64-x64.msi"
   softwareName   = 'CMake'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
@@ -14,4 +14,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-Get-ChildItem $toolsPath\*.msi | % { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
