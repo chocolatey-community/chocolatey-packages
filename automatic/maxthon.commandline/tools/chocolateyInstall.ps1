f5648c5776c780077d19c1f24d8bfa97e2d4c5be
Chocolatey <chocolatey@realdimensions.net>
2018-01-16 12:14:09 +0000

    AU: 3 updated - autohotkey autohotkey.install maxthon.commandline [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b3386807ae3f171a0de3dc6a175f6b11115912fa

--- automatic/maxthon.commandline/tools/chocolateyInstall.ps1
+++ automatic/maxthon.commandline/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  url          = 'https://dl.maxthon.com/mx5/maxthon_portable_5.1.5.1000.7z'
-  checksum     = '0ce03b62fc67c4f85fb9cf1512880a55d42fde4eafff8883339e78e1c03d4646'
+  url          = 'https://dl.maxthon.com/mx5/maxthon_portable_5.1.5.3000.7z'
+  checksum     = '8e23221b3f9b91e351c8cd844cebe7a43f6004d8dbac29391ee82f9f27179e60'
   checksumType = 'sha256'
   destination  = $toolsDir
 }
