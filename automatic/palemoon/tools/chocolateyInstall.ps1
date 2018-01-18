bde7c351e2c367167ebde87f760c9a5be2d0c69f
Chocolatey <chocolatey@realdimensions.net>
2018-01-18 12:14:07 +0000

    AU: 1 updated - palemoon [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/df98004bf9de63570ec46e7eb81edd31926f5359

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'http://rm-eu.palemoon.org/release/palemoon-27.7.0.win32.installer.exe'
-  url64         = 'http://rm-eu.palemoon.org/release/palemoon-27.7.0.win64.installer.exe'
+  url           = 'http://rm-eu.palemoon.org/release/palemoon-27.7.1.win32.installer.exe'
+  url64         = 'http://rm-eu.palemoon.org/release/palemoon-27.7.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '2a3665e0785471470de9f1e1db51d187fad2d2812c38cd5407b64851c4fab51a'
+  checksum      = '65455149c4e93611b638980113e1948e4ed13dc283fd218d365974dd0f448d81'
   checksumType  = 'sha256'
-  checksum64    = 'bdad51df07170cd096ecbce0d06e992b90cc35725d80a1886965aa2b81fc8b51'
+  checksum64    = 'a025f2bfef4ba407bca43538b3ed3e7c735b9eeaed63474d0bf64a6705dbad25'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
