49308d7009427c0c70b849efef717d6f3cbcad62
Chocolatey <chocolatey@realdimensions.net>
2018-01-21 12:15:04 +0000

    AU: 1 updated - wget [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1318ff4061c185cd49a9b09f3d165ef78c01352b

--- automatic/wget/tools/chocolateyinstall.ps1
+++ automatic/wget/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'wget'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$zipFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+$zipFile = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
          Write-Host "Getting x64 bit zip"; Get-Item "$toolsDir\*_x64.zip"
 } else { Write-Host "Getting x32 bit zip"; Get-Item "$toolsDir\*_x32.zip" }
 
