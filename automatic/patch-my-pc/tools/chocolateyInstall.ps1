ec39678591fe2efe99e903c43eb706ee4c28c46c
Chocolatey <chocolatey@realdimensions.net>
2018-01-23 00:32:49 +0000

    AU: 3 updated - git.portable patch-my-pc SQLite [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f50c2ffc397295bff1c6febaef4d6e961d21c26c

--- automatic/patch-my-pc/tools/chocolateyInstall.ps1
+++ automatic/patch-my-pc/tools/chocolateyInstall.ps1
@@ -11,7 +11,7 @@ $packageArgs = @{
   packageName    = 'patch-my-pc'
   file           = "$toolsDir\PatchMyPC.exe"
   url            = 'https://patchmypc.net/freeupdater/PatchMyPC.exe'
-  checksum       = '359e401e36e7ab39dbe8fce7b673e6af350616dd8225eb55c495006e1a770334'
+  checksum       = 'f314a244d14393d7355fde782ddcabe1edb97121a5ef19b7ab835ac6932c88af'
   checksumType   = 'sha256'
 }
 
--- automatic/sqlite/tools/chocolateyInstall.ps1
+++ automatic/sqlite/tools/chocolateyInstall.ps1
@@ -1,23 +1,23 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$embedded_path = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing 64 bit version"; gi "$toolsDir\*dll*win64*.zip"
-} else { Write-Host "Installing 32 bit version"; gi "$toolsDir\*dll*win32*.zip" }
+$embedded_path = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing 64 bit version"; Get-Item "$toolsDir\*dll*win64*.zip"
+} else { Write-Host "Installing 32 bit version"; Get-Item "$toolsDir\*dll*win32*.zip" }
 
 $packageArgs = @{
     PackageName  = 'sqlite'
     FileFullPath = $embedded_path
     Destination  = $toolsDir
 }
-ls $toolsDir\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
+Get-ChildItem $toolsDir\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
 Get-ChocolateyUnzip @packageArgs
 
 $pp = Get-PackageParameters
 if (!$pp.NoTools) {
     Write-Host "Installing tools"
-    $packageArgs.FileFullPath = gi "$toolsDir\*tools*win32*.zip"
+    $packageArgs.FileFullPath = Get-Item "$toolsDir\*tools*win32*.zip"
     Get-ChocolateyUnzip @packageArgs
 }
 
-rm $toolsDir\*.zip -ea 0
+Remove-Item $toolsDir\*.zip -ea 0
