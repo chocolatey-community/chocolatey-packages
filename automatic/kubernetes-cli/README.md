e5bb2518650f1b65dd95588d66c79287a248d5d2
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2018-01-24 10:41:10 +0100

    (kubernetes-cli) Add AU automatic package

---
+++ automatic/kubernetes-cli/README.md
@@ -0,0 +1,4 @@
+# <img src="http://cdn.rawgit.com/abejenaru/chocolatey-packages/16e4ddf69bd7dfc07597b7b3aaa7d8372eeec7fb/icons/kubernetes.png" width="48" height="48"/> [kubernetes-cli](https://chocolatey.org/packages/kubernetes-cli)
+
+Kubernetes is an open source system for managing containerized applications across multiple hosts, providing basic mechanisms for deployment, maintenance, and scaling of applications.
+Kubernetes builds upon a decade and a half of experience at Google running production workloads at scale using a system called Borg, combined with best-of-breed ideas and practices from the community.
---
+++ automatic/kubernetes-cli/update.ps1
@@ -0,0 +1,49 @@
+[CmdletBinding()]
+param($IncludeStream, [switch] $Force)
+
+Import-Module AU
+
+$releases = 'https://github.com/kubernetes/kubernetes/releases/latest'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>"   = "`${1}<$($Latest.ReleaseURL)>"
+      "(?i)(^\s*32\-bit software.*)\<.*\>"  = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*64\-bit software.*)\<.*\>"  = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"       = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"         = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"            = "`${1} $($Latest.Checksum64)"
+    }
+
+    "$($Latest.PackageName).nuspec" = @{
+      "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = '\.tar\.gz$'
+  $url = $download_page.links | ? href -match $re | % href | select -First 1
+
+  $version = (Split-Path ( Split-Path $url ) -Leaf).Substring(1)
+  $majorVersion = $version.Substring(0, 3)
+
+  $streams = @{}
+
+  $streams.Add($majorVersion, @{
+    Version     = $version
+    URL32       = "https://dl.k8s.io/v${version}/kubernetes-client-windows-386.tar.gz"
+    URL64       = "https://dl.k8s.io/v${version}/kubernetes-client-windows-amd64.tar.gz"
+    ReleaseNotes= "https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG-${majorVersion}.md"
+    ReleaseURL  = "https://github.com/kubernetes/kubernetes/releases/tag/v${version}"
+  })
+
+  return @{ Streams = $streams }
+}
+
+update -ChecksumFor none -IncludeStream $IncludeStream -Force:$Force
