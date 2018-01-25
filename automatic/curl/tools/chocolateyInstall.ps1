a02a59c996ebd429777207d88191827cda74a838
Chocolatey <chocolatey@realdimensions.net>
2018-01-25 06:13:23 +0000

    AU: 3 updated - chromium curl smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4690b21aee45b0a8d00f0b5132a3080f210001c9

--- automatic/curl/tools/chocolateyInstall.ps1
+++ automatic/curl/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
-$embedded_path = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing 64 bit version"; rm "$toolsDir\curl_x32" -Recurse
-} else { Write-Host "Installing 32 bit version"; rm "$toolsDir\curl_x64" -Recurse}
+$embedded_path = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing 64 bit version"; Remove-Item "$toolsDir\curl_x32" -Recurse
+} else { Write-Host "Installing 32 bit version"; Remove-Item "$toolsDir\curl_x64" -Recurse}
