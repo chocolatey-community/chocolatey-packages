c3afd1a1729d692640a25fe8795ca83b5d37bc2c
Chocolatey <chocolatey@realdimensions.net>
2018-01-26 06:14:05 +0000

    AU: 3 updated - calibre chromium thunderbird [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7c5be7b377fd41007805804c5a2e5a9667ea4803

--- automatic/thunderbird/tools/chocolateyInstall.ps1
+++ automatic/thunderbird/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'thunderbird'
 $softwareName = 'Mozilla Thunderbird'
 
-$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '52.5.2')
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '52.6.0')
 
 if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
   Write-Output $(
@@ -26,7 +26,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/52.5.2/win32/${locale}/Thunderbird%20Setup%2052.5.2.exe"
+    Url = "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/52.6.0/win32/${locale}/Thunderbird%20Setup%2052.6.0.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
