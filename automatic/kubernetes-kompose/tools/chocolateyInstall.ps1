e4e529ccc804ac6fac259a658e395e38b42b24f3
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2018-01-29 11:08:31 +0100

    (kubernetes-kompose) Add AU automatic package

---
+++ automatic/kubernetes-kompose/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'kubernetes-kompose'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$installLocation = "$(Get-ToolsLocation)\$packageName"
+
+$packageArgs1 = @{
+  PackageName    = $packageName
+  FileFullPath64 = Get-Item $toolsPath\*.exe.tar.gz
+  Destination    = $installLocation
+}
+Get-ChocolateyUnzip @packageArgs1
+
+$packageArgs2 = @{
+  PackageName    = $packageName
+  FileFullPath64 = Get-Item $installLocation\*.exe.tar
+  Destination    = $installLocation
+}
+Get-ChocolateyUnzip @packageArgs2
+
+New-Item -ItemType "directory" -Path "$installLocation\bin"
+Move-Item "$installLocation\kompose*.exe" "$installLocation\bin\kompose.exe"
+Install-ChocolateyPath "$installLocation\bin"
+
+Remove-Item "$installLocation\*.tar"
