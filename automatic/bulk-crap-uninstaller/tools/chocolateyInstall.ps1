caee1f74ab5d84fb3bd56d6e5d5c5c5414799b14
Chocolatey <chocolatey@realdimensions.net>
2018-01-29 00:37:10 +0000

    AU: 1 updated - bulk-crap-uninstaller [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2e986b50a7a542c9ab198f8b95cdf0ae76cc13b2

--- automatic/bulk-crap-uninstaller/tools/chocolateyInstall.ps1
+++ automatic/bulk-crap-uninstaller/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'bulk-crap-uninstaller'
   fileType       = 'exe'
-  file           = gi $toolsPath\*.exe
+  file           = Get-Item $toolsPath\*.exe
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
   softwareName   = 'BCUninstaller'
 }
 Install-ChocolateyInstallPackage @packageArgs
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" }}
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" }}
