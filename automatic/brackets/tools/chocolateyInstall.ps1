6f6c1600cf6f989153a003fc850a01a33d29cdf0
Chocolatey <chocolatey@realdimensions.net>
2018-01-30 12:14:12 +0000

    AU: 2 updated - Brackets diskdefragtouch [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/cd4c189482d16b391adcaf054560e572d38f6361

--- automatic/brackets/tools/chocolateyInstall.ps1
+++ automatic/brackets/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file           = "$toolsPath\Brackets.Release.1.11.msi"
+  file           = "$toolsPath\Brackets.Release.1.12.msi"
   softwareName   = 'brackets*'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
@@ -13,4 +13,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.msi | % { rm $_ -ea 0; if (Test-Path $_) { sc ".ignore" } }
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content ".ignore" } }
