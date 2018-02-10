fadaf1345134af60db9029686f1f588035052208
Chocolatey <chocolatey@realdimensions.net>
2018-02-10 00:39:03 +0000

    AU: 1 updated - vlc [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/00f036b26e771cf7c594e6f05975bce83dc6137d

--- automatic/vlc/tools/chocolateyInstall.ps1
+++ automatic/vlc/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@
 
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
-$installerFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing x64 bit version"; gi "$toolsDir\*_x64.exe"
-} else { Write-Host "Installing x32 bit version"; gi "$toolsDir\*_x32.exe" }
+$installerFile = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Installing x64 bit version"; Get-Item "$toolsDir\*_x64.exe"
+} else { Write-Host "Installing x32 bit version"; Get-Item "$toolsDir\*_x32.exe" }
 
 $packageArgs = @{
   packageName    = 'vlc'
@@ -14,13 +14,13 @@ $packageArgs = @{
   validExitCodes = @(0, 1223)
 }
 Install-ChocolateyInstallPackage @packageArgs
-rm ($toolsDir + '\*.' + $packageArgs.fileType)
+Remove-Item ($toolsDir + '\*.' + $packageArgs.fileType)
 
 $pp = Get-PackageParameters
 if ($pp.Language) {
     Write-Host 'Setting langauge to' $pp.Language
     mkdir -force HKCU:\Software\VideoLAN\VLC
-    sp HKCU:\Software\VideoLAN\VLC Lang $pp.Language
+    Set-ItemProperty HKCU:\Software\VideoLAN\VLC Lang $pp.Language
 }
 
 $packageName = $packageArgs.packageName
