dcd69e3879bc1b7b2936aef9176617337d5d8180
AdmiringWorm <kim.nordmo@gmail.com>
2018-02-12 23:56:56 +0100

    (curl) Included the curl certificate
    fixes #957

--- automatic/curl/tools/chocolateyInstall.ps1
+++ automatic/curl/tools/chocolateyInstall.ps1
@@ -2,5 +2,7 @@
 
 $toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
 $embedded_path = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true') {
-         Write-Host "Installing 64 bit version"; Remove-Item "$toolsDir\curl_x32" -Recurse
-} else { Write-Host "Installing 32 bit version"; Remove-Item "$toolsDir\curl_x64" -Recurse}
+         Write-Host "Installing 64 bit version"; Remove-Item "$toolsDir\curl_x32" -Recurse; "$toolsDir\curl_x64"
+} else { Write-Host "Installing 32 bit version"; Remove-Item "$toolsDir\curl_x64" -Recurse; "$toolsDir\curl_x32" }
+
+Move-Item -Path "$toolsDir\cacert.pem" -Destination "$embedded_path\curl-ca-bundle.crt" -Force
--- automatic/curl/update.ps1
+++ automatic/curl/update.ps1
@@ -25,6 +25,12 @@ function global:au_BeforeUpdate {
     7z e tools\*win32* curl.exe libcurl.dll -r -otools\curl_x32
     7z e tools\*win64* curl.exe libcurl.dll -r -otools\curl_x64
     rm tools\*.7z
+
+    # Download and include the ssl certificate, due to issue <https://github.com/chocolatey/chocolatey-coreteampackages/issues/957>
+
+    if (Test-Path "tools\cacert.pem") { remove-Item "tools\cacert.pem" }
+
+    Invoke-WebRequest -Uri "https://curl.haxx.se/ca/cacert.pem" -OutFile "tools\cacert.pem" -UseBasicParsing
 }
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
@@ -32,11 +38,13 @@ function global:au_GetLatest {
     $re    = '/vszakats/.+\.7z'
     $url   = $download_page.links | ? href -match $re | % href
     $version  = ($url[0] -split '/'  | select -Last 1) -split '-' | select -Index 1
+    $releaseNotes = $download_page.links | ? href -match "changes\.html" | select -first 1 -expand href
 
     @{
         Version      = $version
         URL32        = $url -match 'win32' | select -first 1
         URL64        = $url -notmatch 'win32' | select -first 1
+        ReleaseNotes = $releaseNotes
     }
 }
 
