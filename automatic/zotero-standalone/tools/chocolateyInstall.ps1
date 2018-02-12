78cb0a7730eef211166404dcee7b842f75a4c647
Chocolatey <chocolatey@realdimensions.net>
2018-02-12 12:14:01 +0000

    AU: 3 updated - activepresenter chromium zotero-standalone [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/038f057c719ef9ea5d07308dad0ed6648e02dbd9

--- automatic/zotero-standalone/tools/chocolateyInstall.ps1
+++ automatic/zotero-standalone/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero Standalone *'
-  file           = "$toolsPath\Zotero-5.0.35_setup.exe"
+  file           = "$toolsPath\Zotero-5.0.35.1_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
