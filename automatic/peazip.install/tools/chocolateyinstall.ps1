a6a9760ce09218c8b696beef18ba8bb2579930b1
Chocolatey <chocolatey@realdimensions.net>
2018-02-18 18:14:39 +0000

    AU: 3 updated - bulk-crap-uninstaller peazip peazip.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/178dbc8142d81d68d7c3925767fd5fc035b05efa

--- automatic/peazip.install/tools/chocolateyinstall.ps1
+++ automatic/peazip.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  file           = "$toolsDir\peazip-6.5.0.WINDOWS.exe"
-  file64         = "$toolsDir\peazip-6.5.0.WIN64.exe"
+  file           = "$toolsDir\peazip-6.5.1.WINDOWS.exe"
+  file64         = "$toolsDir\peazip-6.5.1.WIN64.exe"
   fileType       = 'exe'
   packageName    = 'peazip.install'
   softwareName   = 'PeaZip'
@@ -14,4 +14,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-Get-ChildItem -Path $toolsDir\*.exe | % { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
+Get-ChildItem -Path $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
