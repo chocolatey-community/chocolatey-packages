dcfa3d4c26653683eef452d4a0709b8f5b85abb4
Chocolatey <chocolatey@realdimensions.net>
2018-02-23 12:13:50 +0000

    AU: 1 updated - filezilla [skip ci]

--- automatic/filezilla/tools/chocolateyInstall.ps1
+++ automatic/filezilla/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'filezilla'
   fileType       = $fileType
-  file           = gi $toolsPath\*_x32.exe
-  file64         = gi $toolsPath\*_x64.exe
+  file           = Get-Item $toolsPath\*_x32.exe
+  file64         = Get-Item $toolsPath\*_x64.exe
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
   softwareName   = 'FileZilla Client*'
 }
 Install-ChocolateyInstallPackage @packageArgs
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { Set-Content -Value "" -Path "$_.ignore" }}
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content -Value "" -Path "$_.ignore" }}
