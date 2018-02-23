1e9e24aafc875cb010464443de0426b2902d73f7
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2018-02-23 12:01:58 +0100

    Future proof install script for tar.gz unpacking; Remove binary location from Path var on uninstall; Update README

--- automatic/kubernetes-kompose/README.md
+++ automatic/kubernetes-kompose/README.md
@@ -1,5 +1,13 @@
 # <img src="http://cdn.rawgit.com/abejenaru/chocolatey-packages/16e4ddf69bd7dfc07597b7b3aaa7d8372eeec7fb/icons/kubernetes.png" width="48" height="48"/> [kubernetes-kompose](https://chocolatey.org/packages/kubernetes-kompose)
 
-Kompose is a convenience tool to go from local Docker development to managing your application with Kubernetes.
+## Kubernetes + Compose = Kompose
+Kompose is a conversion tool for Docker Compose to container orchestrators such as Kubernetes (or OpenShift).
+It takes a Docker Compose file and translates it into Kubernetes resources.
 
 Transformation of the Docker Compose format to Kubernetes resources manifest may not be exact, but it helps tremendously when first deploying an application on Kubernetes.
+
+### Features
+* Simplify your development process with Docker Compose and then deploy your containers to a production cluster
+* Convert your docker-compose.yaml with one simple command kompose convert
+* Immediately bring up your cluster with kompose up
+* Bring it back down with kompose down
--- automatic/kubernetes-kompose/tools/chocolateyInstall.ps1
+++ automatic/kubernetes-kompose/tools/chocolateyInstall.ps1
@@ -5,21 +5,23 @@ $packageName = 'kubernetes-kompose'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $installLocation = "$(Get-ToolsLocation)\$packageName"
 
-$packageArgs1 = @{
+$packageArgs = @{
   PackageName    = $packageName
-  FileFullPath64 = Get-Item $toolsPath\*.exe.tar.gz
+  FileFullPath64 = Get-Item $toolsPath\kompose*.exe.tar.gz
   Destination    = $installLocation
 }
-Get-ChocolateyUnzip @packageArgs1
+Get-ChocolateyUnzip @packageArgs
 
-$packageArgs2 = @{
-  PackageName    = $packageName
-  FileFullPath64 = Get-Item $installLocation\*.exe.tar
-  Destination    = $installLocation
+if (!(Test-Path $installLocation -include "kompose*.exe")) {
+  $packageArgs2 = @{
+    PackageName    = $packageName
+    FileFullPath64 = Get-Item $installLocation\kompose*.exe.tar
+    Destination    = $installLocation
+  }
+  Get-ChocolateyUnzip @packageArgs2
 }
-Get-ChocolateyUnzip @packageArgs2
 
-New-Item -ItemType "directory" -Path "$installLocation\bin"
+New-Item -ItemType "directory" -Path "$installLocation\bin" -ErrorAction Ignore
 Move-Item "$installLocation\kompose*.exe" "$installLocation\bin\kompose.exe"
 Install-ChocolateyPath "$installLocation\bin"
 
