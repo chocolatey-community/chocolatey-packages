c4f337719fbc45f9a3f5a0b77a2b7f30ee1479ce
AdmiringWorm <kim.nordmo@gmail.com>
2018-02-26 20:04:46 +0100

    (1password) Migrated package

---
+++ automatic/1password/Readme.md
@@ -0,0 +1,5 @@
+# [<img src="https://cdn.rawgit.com/chocolatey/chocolatey-coreteampackages/0e94eff84f6fae272df90852605e5b7ec13e2df0/icons/1password.png" height="48" width="48" /> 1password](https://chocolatey.org/packages/1password)
+
+1Password – Too many passwords to remember?
+
+1Password can create strong, unique passwords for you, remember them, and restore them, all directly in your web browser.
--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -1,6 +1,14 @@
-﻿$packageId = '1password'
-$fileType = 'exe'
-$installArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = 'https://d13itkw33a7sus.cloudfront.net/dist/1P/win4/1Password-4.6.1.620.exe'
+﻿$ErrorActionPreference = 'Stop'
 
-Install-ChocolateyPackage $packageId $fileType $installArgs $url -validExitCodes @(0)
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = 'https://d13itkw33a7sus.cloudfront.net/dist/1P/win4/1Password-4.6.1.620.exe'
+  softwareName   = '1Password*'
+  checksum       = ''
+  checksumType   = ''
+  silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
