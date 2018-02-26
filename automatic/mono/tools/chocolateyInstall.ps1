ef18b56f2d3da02af8f4ad64c786b1fcc983e663
AdmiringWorm <kim.nordmo@gmail.com>
2018-02-26 18:41:15 +0100

    (mono) Initial Migration

---
+++ automatic/mono/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageName = 'mono'
+$url = 'http://download.mono-project.com/archive/4.4.2/windows-installer/mono-4.4.2.11-gtksharp-2.12.38-win32-0.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  url           = $url
+  silentArgs    = "/qn /norestart /l*v $env:TEMP\chocolatey\$packageName\install.log"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
