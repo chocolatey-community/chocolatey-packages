93b5a9763798cde42fd65bb5babae824e337e0e9
AdmiringWorm <kim.nordmo@gmail.com>
2018-02-26 19:54:39 +0100

    (1password) Initial Migration

---
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageId = '1password'
+$fileType = 'exe'
+$installArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$url = 'https://d13itkw33a7sus.cloudfront.net/dist/1P/win4/1Password-4.6.1.620.exe'
+
+Install-ChocolateyPackage $packageId $fileType $installArgs $url -validExitCodes @(0)
