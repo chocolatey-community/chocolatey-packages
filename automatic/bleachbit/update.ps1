0847a013cb7489c8c8a4001ff3bea9c6171ee6cc
AdmiringWorm <kim.nordmo@gmail.com>
2018-03-01 14:50:17 +0100

    (bleachbit*) Updated to fix latest version not grabbed

--- automatic/bleachbit/update.ps1
+++ automatic/bleachbit/update.ps1
@@ -4,30 +4,31 @@ $releases = 'https://www.bleachbit.org/download/windows'
 $betas = 'https://download.bleachbit.org/beta/'
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $filename = ($download_page.links | ? href -match '.exe$' | select -First 1 -expand href).Replace('/download/file/t?file=','')
-    $version = $filename -split '-' | select -First 1 -Skip 1
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $filename = ($download_page.links | ? href -match '.exe$' | select -First 1 -expand href).Replace('/download/file/t?file=', '')
+  $version = $filename -split '-' | select -First 1 -Skip 1
 
-    $page_betas    = Invoke-WebRequest -Uri $betas -UseBasicParsing
-    $path_beta     = $betas + ($page_betas.links | select -Last 1 -expand href)
-    $page_beta     = Invoke-WebRequest -Uri $path_beta -UseBasicParsing
-    $filename_beta = $page_beta.links | ? href -match 'setup.exe$' | select -First 1 -expand href
-    $version_beta  = $filename_beta -split '-' | select -First 1 -Skip 1
+  try {
+    # try figuring out if this is a beta release or a normal release.
+    $test_page = (iwr -UseBasicParsing "https://bleachbit.org/news/bleachbit-$($version -replace '\.','')" -Method Head)
+  }
+  catch {}
 
-    if ([version]$version_beta -gt [version]$version) {
-        @{
-            Version = $version_beta + '-beta'
-            URL32   = $path_beta + $filename_beta
-        }
-    } else {
-        @{
-            Version = $version
-            URL32   = 'https://download.bleachbit.org/' + $filename
-        }
-    }
+  if (!$test_page) {
+    # Until beta links works again we ignore beta releases
+    $beta_page = iwr -UseBasicParsing "https://bleachbit.org/news/bleachbit-$($version -replace '\.','')-beta" -Method Head
+  }
+
+  if ($beta_page) {
+    $filename = "beta/$version/$filename"
+    $version = $version + "-beta"
+  }
+
+  @{
+    Version = $version
+    URL32   = 'https://download.bleachbit.org/' + $filename
+  }
 }
-
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
   @{
@@ -37,4 +38,6 @@ function global:au_SearchReplace {
   }
 }
 
-update -ChecksumFor none
+if ($MyInvocation.InvocationName -ne '.') {
+  update -ChecksumFor none
+}
