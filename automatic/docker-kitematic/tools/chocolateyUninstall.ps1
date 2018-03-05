b422dcfaed67787575e481d92c68761506206fb3
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2018-03-05 17:25:29 +0100

    (docker-kitematic) Migrate Kitematic to Core team packages repo

---
+++ automatic/docker-kitematic/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿
+# Remove desktop shortcut
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$link = Join-Path $desktop "Kitematic.lnk"
+If (Test-Path $link) {
+    Remove-Item "$link"
+}
