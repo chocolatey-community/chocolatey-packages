d2ba3846157215dd14199887818129eea17950eb
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2018-03-06 09:25:31 +0100

    (minishift) Migrate Minishift to Core team packages repo

---
+++ automatic/minishift/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'minishift'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+Remove-Item "$toolsPath\minishift-*-windows-amd64" -Recurse -Force -ea 0
+
+$packageArgs = @{
+  PackageName    = $packageName
+  FileFullPath64 = Get-Item $toolsPath\minishift-*-windows-amd64.zip
+  Destination    = $toolsPath
+}
+Get-ChocolateyUnzip @packageArgs
---
+++ automatic/minishift/update.ps1
@@ -0,0 +1,41 @@
+[CmdletBinding()]
+param([switch] $Force)
+
+Import-Module AU
+
+$domain   = 'https://github.com'
+$releases = "$domain/minishift/minishift/releases/latest"
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseURL)>"
+      "(?i)(^\s*software.*)\<.*\>"        = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum\:).*"            = "`${1} $($Latest.Checksum64)"
+    }
+
+    "$($Latest.PackageName).nuspec" = @{
+      "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseURL)`${2}"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = '-windows-amd64\.zip$'
+  $url = $download_page.links | ? href -match $re | % href | select -First 1
+
+  $version = (Split-Path ( Split-Path $url ) -Leaf).Substring(1)
+
+  return @{
+    Version     = $version
+    URL64       = $domain + $url
+    ReleaseURL  = "$domain/minishift/minishift/releases/tag/v${version}"
+  }
+}
+
+update -ChecksumFor none -Force:$Force
