b091b872f1123f77e68599c21b7bfdaad249e375
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2018-03-06 09:28:33 +0100

    (openshift-cli) Migrate OpenShift Origin CLI to Core team packages repo

---
+++ automatic/openshift-cli/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'openshift-cli'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  PackageName    = $packageName
+  FileFullPath64 = Get-Item $toolsPath\openshift-origin-client-tools.zip
+  Destination    = $toolsPath
+}
+Get-ChocolateyUnzip @packageArgs
