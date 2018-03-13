988367a48be19b5f323df74a4cb5de1571678d84
Chocolatey <chocolatey@realdimensions.net>
2018-03-13 18:16:38 +0000

    AU: 4 updated - 360ts Firefox FirefoxESR tor-browser [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/13341ac0c2e45ad1924005fe8b34d4eec87617a3

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1255.exe'
-  checksum               = '216d81a2078e0eb972e46073f233e1c660ae88806e9988a20efbac079fc83442'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1270.exe'
+  checksum               = 'ab064dfe90c4d7b188e9f943a5504080e231e024f00f5b9698082c5ce589a134'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
