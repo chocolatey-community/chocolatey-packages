120c94f5534f703de1990a3a84ff3891231d8aba
Chocolatey <chocolatey@realdimensions.net>
2018-03-14 12:14:26 +0000

    AU: 1 updated - kitty [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/36aff9a2c90f2cdf6b3cdc8e2e5fbac3614a4079

--- automatic/kitty/tools/chocolateyInstall.ps1
+++ automatic/kitty/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ if ($pp.Portable) {
     Write-Host 'Setting portable mode'
     
     $iniPath = Join-Path $toolsPath 'kitty.ini'
-    if (Test-Path $iniPath) { $ini = gc $iniPath -Encoding ascii -ea 0 | Out-String }    
+    if (Test-Path $iniPath) { $ini = Get-Content $iniPath -Encoding ascii -ea 0 | Out-String }    
     # kitty ini file doesn't seem to have any other option now
     if ($ini -notmatch 'savemode=dir') { "[KiTTY]`nsavemode=dir" | Out-File -Encoding ascii $iniPath }
 }
