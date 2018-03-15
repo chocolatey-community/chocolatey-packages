47fd4c48dacf97b4ae734a067230ccc1f3dbedc8
Chocolatey <chocolatey@realdimensions.net>
2018-03-15 00:30:05 +0000

    AU: 1 updated - visualstudiocode [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1e77c518ccb54483c9b3b57d367851882997c8c5

--- automatic/visualstudiocode/Tools/ChocolateyInstall.ps1
+++ automatic/visualstudiocode/Tools/ChocolateyInstall.ps1
@@ -18,14 +18,14 @@ Get-Process code -ea 0 | Stop-Process  #in case gracefull shutdown did not succe
 $packageArgs = @{
   packageName    = 'visualstudiocode'
   fileType       = 'EXE'
-  url            = 'https://az764295.vo.msecnd.net/stable/9a199d77c82fcb82f39c68bb33c614af01c111ba/VSCodeSetup-ia32-1.21.0.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/9a199d77c82fcb82f39c68bb33c614af01c111ba/VSCodeSetup-x64-1.21.0.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/79b44aa704ce542d8ca4a3cc44cfca566e7720f1/VSCodeSetup-ia32-1.21.1.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/79b44aa704ce542d8ca4a3cc44cfca566e7720f1/VSCodeSetup-x64-1.21.1.exe'
 
   softwareName   = 'Microsoft Visual Studio Code'
 
-  checksum       = '394f1e5d2aebd24f0bac0c210a784318a848fdf375148757087347d6f9104a0f'
+  checksum       = 'e96df370f7335a3da269f336ed32d629bc8d98180c0cc1f5b67e0cef32a47b9f'
   checksumType   = 'sha256'
-  checksum64     = '07fe0d2433176fdf626a8a011bf6155d42b05124c8188540b6d7ad186778665c'
+  checksum64     = '7b539cf9c4520b68ad66645700cf3b045a25091b7cd0f9bd0c726f1e1de58e2a'
   checksumType64 = 'sha256'
 
   silentArgs     = "/verysilent /suppressmsgboxes /mergetasks=""$mergeTasks"" /log=""$env:temp\vscode.log"""
