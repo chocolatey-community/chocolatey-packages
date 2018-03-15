c55a5f1b4dd710d4aeb48de234366fe4e70f232c
Chocolatey <chocolatey@realdimensions.net>
2018-03-15 06:20:08 +0000

    AU: 3 updated - chromium tixati tixati.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/28a31268f9884eaff4c71d984390b776a24c42e6

--- automatic/tixati.portable/tools/chocolateyInstall.ps1
+++ automatic/tixati.portable/tools/chocolateyInstall.ps1
@@ -4,20 +4,20 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'tixati.portable'
-  url            = 'https://download2.tixati.com/download/tixati-2.56-1.portable.zip'
-  checksum       = 'ea652e6f56ab8a0d6f336b9b4c8039c7d2264605cdd7764248a44a0824518843'
+  url            = 'https://download2.tixati.com/download/tixati-2.57-1.portable.zip'
+  checksum       = '8ad033668958c225f2b3d197a3d3103bb29e26d6c27d38386c4731c7f06e20c8'
   checksumType   = 'sha256'
   unzipLocation  = $toolsPath
 }
 Install-ChocolateyZipPackage @packageArgs
 
-$is32bit = (Get-ProcessorBits 32) -or ($Env:chocolateyForceX86 -eq 'true')
+$is32bit = (Get-OSArchitectureWidth 32) -or ($Env:chocolateyForceX86 -eq 'true')
 $tixati_path = "$toolsPath\Tixati_portable"
-rm $tixati_path\tixati_Linux*
+Remove-Item $tixati_path\tixati_Linux*
 if ($is32bit) {
-    rm $tixati_path\tixati_Windows64bit.exe
-    mv $tixati_path\tixati_Windows32bit.exe $tixati_path\tixati.exe
+    Remove-Item $tixati_path\tixati_Windows64bit.exe
+    Move-Item $tixati_path\tixati_Windows32bit.exe $tixati_path\tixati.exe
 } else {
-    rm $tixati_path\tixati_Windows32bit.exe
-    mv $tixati_path\tixati_Windows64bit.exe $tixati_path\tixati.exe
+    Remove-Item $tixati_path\tixati_Windows32bit.exe
+    Move-Item $tixati_path\tixati_Windows64bit.exe $tixati_path\tixati.exe
 }
--- automatic/tixati/tools/chocolateyInstall.ps1
+++ automatic/tixati/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'tixati'
-$fileName = 'tixati-2.55-1.install.exe'
+$fileName = 'tixati-2.57-1.install.exe'
 $download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion"
 
 $packageArgs = @{
   packageName    = $packageName
   fileFullPath   = "$download_dir\$fileName"
-  url            = 'https://download1.tixati.com/download/tixati-2.55-1.win32-install.exe'
-  url64bit       = 'https://download1.tixati.com/download/tixati-2.55-1.win64-install.exe'
-  checksum       = 'aa1767518733f84dab3b4e14ba92566a83ce83fa34be9c55a10e43bdff094fe9'
-  checksum64     = 'eeb23e45af2fee7e370ace1da1515b8c4ef6efba6e4d7bfc4aabfefe4eb28d6b'
+  url            = 'https://download1.tixati.com/download/tixati-2.57-1.win32-install.exe'
+  url64bit       = 'https://download1.tixati.com/download/tixati-2.57-1.win64-install.exe'
+  checksum       = '08dcd33976f3122cc019c58452e26e00b3b8e861004b0aa2c2484a6ff3d0f07c'
+  checksum64     = 'e40265e57eaaa58ed75ef612b45dc840fae3795b4ddc0cdb20275912c38bbef7'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
 }
