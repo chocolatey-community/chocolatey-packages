31d3cf14294e44ad212d4a688a77ddbda6ccc6d3
Chocolatey <chocolatey@realdimensions.net>
2018-03-21 18:16:52 +0000

    AU: 6 updated - diskdefragtouch Ghostscript Ghostscript.app nodejs nodejs.install zotero-standalone [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0c5881e309bae39d9587dbc5c3ef5e2ccc7d95c2

--- automatic/ghostscript.app/tools/ChocolateyInstall.ps1
+++ automatic/ghostscript.app/tools/ChocolateyInstall.ps1
@@ -2,10 +2,10 @@
 
 $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$filePath32 = "$toolsPath\gs922w32.exe"
-$filePath64 = "$toolsPath\gs922w64.exe"
+$filePath32 = "$toolsPath\gs923w32.exe"
+$filePath64 = "$toolsPath\gs923w64.exe"
 
-$filePath = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne $true) {
+$filePath = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
   Write-Host "Installing 64 bit version" ; $filePath64
 } else { Write-Host "Installing 32 bit version" ; $filePath32 }
 
