168ee4acf77db4acd056028cc50b965d433d359e
Chocolatey <chocolatey@realdimensions.net>
2018-03-22 06:14:01 +0000

    AU: 3 updated - apache-httpd chromium gom-player [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a9e961b9be93fd032cfcb217b2b8aed86fcb0999

--- automatic/apache-httpd/tools/chocolateyInstall.ps1
+++ automatic/apache-httpd/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $pp = Get-PackageParameters
 
 $arguments = @{
     packageName = $env:chocolateyPackageName
-    file        = "$toolsDir\httpd-2.4.32-o102n-x86-vc14.zip"
-    file64      = "$toolsDir\httpd-2.4.32-o102n-x64-vc14.zip"
+    file        = "$toolsDir\httpd-2.4.33-o102n-x86-vc14.zip"
+    file64      = "$toolsDir\httpd-2.4.33-o102n-x64-vc14.zip"
     destination = if ($pp.installLocation) { $pp.installLocation } else { $env:APPDATA }
     port        = if ($pp.Port) { $pp.Port } else { 8080 }
     serviceName = if ($pp.serviceName) { $pp.serviceName } else { 'Apache' }
