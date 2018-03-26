4fd99f482269e6441fe510e38dbcc443db3fea12
Jakub Bereżański <kuba@berezanscy.pl>
2018-03-27 00:05:32 +0200

    (skype) import from ferventcoder/chocolatey-packages

---
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -0,0 +1,50 @@
+﻿#Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/3694814915aaa38100bfa0933f948e65/partner/59/SkypeSetup.exe'
+#Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' '{{DownloadUrl}}'
+#Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/SkypeSetupFull.exe'
+
+
+function isInstalled() {
+  return Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -match 'Skype\u2122 [\d\.]+$'}
+}
+
+$packageName = 'skype'
+$fileType = 'msi'
+# http://community.skype.com/t5/Windows-archive/Unattended-install/td-p/184628s
+$silentArgs = '/qn /norestart STARTSKYPE=FALSE TRANSFORMS=:RemoveDesktopShortcut.mst TRANSFORMS=:RemoveStartup.mst'
+
+$url = '{{DownloadUrl}}'
+
+try {
+
+  $appInstalled = isInstalled
+
+  if ($appInstalled) {
+      # If Skype (in any version) is already installed on the computer, remove it first, otherwise the
+      # installation of Skype will fail with an error.
+      $msiArgs = $('/x' + $appInstalled.IdentifyingNumber + ' ' + $silentArgs)
+      Write-Host "Uninstalling previous version of Skype, otherwise installing the new version won’t work."
+      Start-ChocolateyProcessAsAdmin $msiArgs 'msiexec'
+
+      # This loop checks every 5 seconds if Skype is already uninstalled.
+      # Then it proceeds with the download and installation of the Skype
+      # version specified in the package.
+      do {
+        Start-Sleep -Seconds 5
+        $i += 1
+
+        # Break if too much time passed
+        if ($i -gt 12) {
+          Write-Error 'Could not uninstall the previous version of Skype.'
+          break
+        }
+
+      } until (-not (isInstalled))
+  }
+
+  Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
+
