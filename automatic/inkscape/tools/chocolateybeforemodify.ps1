9ee7ec3496948e37aa211f70c8035b2cfdc9eecf
Jason Cumiskey <cumiskey@camosun.bc.ca>
2018-03-26 23:35:31 -0700

    fixes #550

---
+++ automatic/inkscape/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,7 @@
+﻿$app = Get-WmiObject -Class Win32_Product -Filter "Name Like 'Inkscape%'"
+
+if ($app) {
+  Write-Host "Inkscape already installed. Removing..."
+  $app.Uninstall()
+  Write-Host "Done. Continuing with update/uninstall..."
+}
