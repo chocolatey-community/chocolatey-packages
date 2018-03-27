045db5d3958f1c38f090a850df136147a9137a7b
Chocolatey <chocolatey@realdimensions.net>
2018-03-27 17:07:01 +0000

    AU: 3 updated - OpenSSL.Light typescript zotero-standalone [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b1c59cccc17309ee5377bd156d0bf85bd802960c

--- automatic/typescript/tools/chocolateyInstall.ps1
+++ automatic/typescript/tools/chocolateyInstall.ps1
@@ -4,4 +4,4 @@ Update-SessionEnvironment
 
 $npmPath = Get-Command npm | ForEach-Object { $_.Path }
 "Installing $env:chocolateyPackageName using nodejs..."
-Start-ChocolateyProcessAsAdmin $npmPath -statements install,"-g","typescript@2.7.2"
+Start-ChocolateyProcessAsAdmin $npmPath -statements install,"-g","typescript@2.8.1"
