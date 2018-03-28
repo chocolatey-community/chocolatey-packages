2d3e1a7c7d88927af9f636ef6009371daa33c9bf
Chocolatey <chocolatey@realdimensions.net>
2018-03-28 00:33:43 +0000

    AU: 2 updated - dropbox spotify [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/dea5ce72c42f3cf389b2c750c295f0a8300b341b

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2046.3.60%20Offline%20Installer.exe'
-    checksum        = '6cf0c7193dcc8d51613c091639aea31ddc2ca462f8c325676574eb348f3a452c'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2046.3.63%20Offline%20Installer.exe'
+    checksum        = '2b3f557e7c246271dcea8cf4cce73cf444a7712128a333f2fb6a363cfffe6f82'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
