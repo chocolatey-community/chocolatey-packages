e85e251a5f2e4e0d365ce920cd6a6de210ac03ac
Chocolatey <chocolatey@realdimensions.net>
2018-04-04 00:33:49 +0000

    AU: 1 updated - 1password [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4b6385be71d2586347a54882d33f476e9ca69dea

--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://cache.agilebits.com/dist/1P/win6/1PasswordSetup-6.8.496.exe'
+  url            = 'https://cache.agilebits.com/dist/1P/win6/1PasswordSetup-6.8.534.exe'
   softwareName   = '1Password*'
-  checksum       = '1660eee7cd822b47aeb49f656723014c27dbf0c029e0929255f05c33d492a1b7'
+  checksum       = 'c4d5ce45941ab9f69392f4b0276b8fb3930306d4cdcb8f0e4afaf47774e57203'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
