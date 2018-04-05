d4147589609baaa7eb82bbffe34e39e5a28bb9fa
AdmiringWorm <kim.nordmo@gmail.com>
2018-04-05 19:24:21 +0200

    (jitsi) Embedded package

--- automatic/jitsi/tools/chocolateyInstall.ps1
+++ automatic/jitsi/tools/chocolateyInstall.ps1
@@ -1,16 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
 $packageArgs = @{
-  packageName            = 'jitsi'
-  fileType               = 'MSI'
-  url                    = 'https://download.jitsi.org/jitsi/msi/jitsi-2.10.5550-x86.msi'
-  url64bit               = 'https://download.jitsi.org/jitsi/msi/jitsi-2.10.5550-x64.msi'
-  checksum               = 'f0aabb9f32e93183d6845f5b5d9e2f1f8b531133c92f3a26bf539a87f16b4572'
-  checksum64             = 'cf0afa658974af2ffc2683497419ba03a68448d69d3f972b0d29c89c629d0442'
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  silentArgs             = '/passive'
-  validExitCodes         = @(0)
-  registryUninstallerKey = $packageName
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'msi'
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
+  softwareName   = 'jitsi*'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
 }
-Install-ChocolateyPackage @packageArgs
+
+Install-ChocolateyInstallPackage @packageArgs
+
+ls $toolsPath\*.msi | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
--- automatic/jitsi/update.ps1
+++ automatic/jitsi/update.ps1
@@ -1,29 +1,55 @@
-import-module au
+﻿[CmdletBinding()]
+param($IncludeStream, [switch]$Force)
+Import-Module AU
 
 $releases = 'https://download.jitsi.org/jitsi/msi/'
 
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
+  @{
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re  = 'jitsi-\d.*'
-    $url = $download_page.links | ? href -match $re | ? href -notmatch 'latest' | select -First 2 -expand href
-    $url32 = $releases + ($url -match 'x86')
-    $url64 = $releases + ($url -match 'x64')
+  $re = 'jitsi-[\d\.]+\-x86'
+  $urls32 = $download_page.Links | ? href -match $re | select -expand href | % { $releases + $_ }
 
-    $version  = $url[0] -split '-' | select -Index 1
+  $re = 'jitsi-[\d\.]+\-x64'
+  $urls64 = $download_page.links | ? href -match $re | select -expand href | % { $releases + $_ }
 
-    @{ URL64=$url64; URL32 = $url32; Version = $version }
+  $streams = @{}
+  $urls32 | % {
+    $verRe = '-'
+    $version = $_ -split "$verRe" | select -last 1 -skip 1
+    $version = Get-Version $version
+    $url64 = $urls64 | ? { $_ -match "$version" | select -last 1 }
+    if (!($url64)) { throw "URL64 was not found for version $version" }
+
+    if (!($streams.ContainsKey($version.ToString(2)))) {
+      $streams.Add($version.ToString(2), @{
+          Version = $version.ToString()
+          URL32   = $_
+          URL64   = $url64
+        })
+    }
+  }
+
+  return @{ Streams = $streams }
 }
 
-update
+update -ChecksumFor none -IncludeStream $IncludeStream -Force:$Force
