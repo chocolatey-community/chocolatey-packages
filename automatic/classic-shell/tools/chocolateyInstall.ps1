e75ffc1b4d491bdc689adc61dc91ecdf23aacb66
Chocolatey <chocolatey@realdimensions.net>
2018-04-05 17:45:08 +0000

    AU: 2 updated - classic-shell jitsi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1e4041c73854a64436ef30063aa73b431da024b7

--- automatic/classic-shell/tools/chocolateyInstall.ps1
+++ automatic/classic-shell/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\"
+  file           = "$toolsPath\ClassicShellSetup_4_3_1.exe"
   softwareName   = 'classic-shell*'
   silentArgs     = '/passive'
   validExitCodes = @(0)
@@ -13,4 +13,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
--- automatic/jitsi/tools/chocolateyInstall.ps1
+++ automatic/jitsi/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file           = "$toolsPath\"
-  file64         = "$toolsPath\"
+  file           = "$toolsPath\jitsi-2.10.5550-x86.msi"
+  file64         = "$toolsPath\jitsi-2.10.5550-x64.msi"
   softwareName   = 'jitsi*'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
@@ -14,4 +14,4 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.msi | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
