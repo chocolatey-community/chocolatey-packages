03e2f0e94bf9dd06094a97502189efc1dabfc787
AdmiringWorm <kim.nordmo@gmail.com>
2018-04-05 08:09:16 +0200

    (hostsman) Embedded package

--- automatic/hostsman/tools/chocolateyInstall.ps1
+++ automatic/hostsman/tools/chocolateyInstall.ps1
@@ -1,10 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
 $packageArgs = @{
-  packageName    = 'hostsman'
-  url            = 'http://hostsman2.it-mate.co.uk/HostsMan_4.7.105.zip'
-  checksum       = 'b98f1155cda04e8a96cf29f6cc68497bdb28add7c8317cae79f6ec34c3bdc7fa'
-  checksumType   = 'sha256'
-  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'zip'
+  file           = "$toolsPath\"
+  softwareName   = 'hostsman*'
 }
-Install-ChocolateyZipPackage @packageArgs
+
+Get-ChocolateyUnzip @packageArgs
+rm $toolsPath\*.zip -ea 0
--- automatic/hostsman/update.ps1
+++ automatic/hostsman/update.ps1
@@ -1,25 +1,46 @@
-import-module au
+﻿[CmdletBinding()]
+param($IncludeStream, [switch]$Force)
+Import-Module AU
 
 $releases = 'http://hostsman2.it-mate.co.uk/'
 
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$(Get-RemoteChecksum $Latest.URL32)'"
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-        }
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*1\..+)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
     }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+    }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
-    $re    = '4[\.\d]+\.zip$'
-    $url   = $download_page.links | ? href -match $re | select -Last 1 -expand href | % { $releases + $_ }
-    @{
-        Version      = $url -split '\.zip|_' | select -Index 1
-        URL32        = $url
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re = '4[\.\d]+\.zip$'
+  $urls32 = $download_page.Links | ? href -match $re | select -expand href | % { 'http://hostsman2.it-mate.co.uk/' + $_ } | sort -Descending
+
+  $streams = @{}
+  $urls32 | % {
+    $verRe = '[_]|\.zip'
+    $version = $_ -split "$verRe" | select -last 1 -skip 1
+    $version = Get-Version $version
+
+    if (!($streams.ContainsKey($version.ToString(2)))) {
+      $streams.Add($version.ToString(2), @{
+        Version = $version.ToString()
+        URL32   = $_
+      })
     }
+  }
+
+  return @{ Streams = $streams }
 }
 
-update -ChecksumFor none
+update -ChecksumFor none -IncludeStream $IncludeStream -Force:$Force
