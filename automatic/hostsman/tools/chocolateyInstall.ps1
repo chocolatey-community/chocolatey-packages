2f052cd6d85820591ae8d4e3f10e8dd8f742a1f2
AdmiringWorm <kim.nordmo@gmail.com>
2018-04-05 21:04:47 +0200

    (hostsman) Fixed missing destination [AU hostsman\4.7]

--- automatic/hostsman/tools/chocolateyInstall.ps1
+++ automatic/hostsman/tools/chocolateyInstall.ps1
@@ -3,10 +3,11 @@
 $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'zip'
-  file           = "$toolsPath\HostsMan_4.0.95.zip"
-  softwareName   = 'hostsman*'
+  packageName  = $env:ChocolateyPackageName
+  fileType     = 'zip'
+  file         = "$toolsPath\HostsMan_4.0.95.zip"
+  destination  = $toolsPath
+  softwareName = 'hostsman*'
 }
 
 Get-ChocolateyUnzip @packageArgs
