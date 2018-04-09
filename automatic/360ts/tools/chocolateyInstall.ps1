636020fccb896861070566f8afab9660d620fa2e
Chocolatey <chocolatey@realdimensions.net>
2018-04-09 06:14:16 +0000

    AU: 3 updated - 360ts chromium smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/edea03d0d47fad8b3152c2664bda695e03ddcd56

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1283.exe'
-  checksum               = '0ddb2b8e942c9cfeb9b7532d5982a38f6581b72cc812903f44b2f1ab1630a344'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_9.6.0.1313.exe'
+  checksum               = '34a174e261a5711a4d1882f11bf1756fa736007c30231a1def0c49f9706dc940'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
