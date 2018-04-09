5602c3614bf3f1e74552474d88d87bc2a344eec9
Chocolatey <chocolatey@realdimensions.net>
2018-04-09 00:32:26 +0000

    AU: 2 updated - freecad youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0afc8e8d6784677368549785b4d2aef523cd718f

--- automatic/freecad/tools/chocolateyInstall.ps1
+++ automatic/freecad/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.16/FreeCAD.0.16.6706.f86a4e4-WIN-x86-installer.exe'
-  url64          = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.16.6712/FreeCAD-0.16.6712_x64_setup.exe'
+  url            = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.17/FreeCAD-0.17.13509.0258808-WIN-x86-installer.exe'
+  url64          = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.17/FreeCAD-0.17.13509.0258808-WIN-x64-installer.exe'
   softwareName   = 'FreeCAD*'
-  checksum       = '15a84512855adb0ce86e78c5424a9cb589201369aa070acc7b9467621bc434ab'
+  checksum       = '4b071a1f6ccb104055c5dfe18111dab1150b35a8e47fe0a99988262f6289456a'
   checksumType   = 'sha256'
-  checksum64     = 'a37d42ec379b8f23c54be5adb48141b7b13931827b84949e9d15698296ab856c'
+  checksum64     = 'dc71e49619ce4023ffd40b52e8a1e102a485de82ae61b20358343ddc03807938'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
