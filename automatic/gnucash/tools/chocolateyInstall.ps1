447be1a75d22262650f9c2b84020013acabb2b97
Chocolatey <chocolatey@realdimensions.net>
2018-04-11 06:13:24 +0000

    AU: 2 updated - chromium gnucash [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8ff89595fd151141a0afcae59f6d1b4a6c9b9309

--- automatic/gnucash/tools/chocolateyInstall.ps1
+++ automatic/gnucash/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\gnucash-3.0.setup.exe"
+  file           = "$toolsPath\gnucash-2.6.21-setup.exe"
 
   softwareName   = 'GnuCash*'
 
