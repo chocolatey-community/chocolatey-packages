0298046f8cbdc9fb1a4af9374ed268bbb5568b4d
Chocolatey <chocolatey@realdimensions.net>
2018-04-12 18:14:33 +0000

    AU: 2 updated - datagrip kubernetes-cli [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a10ff242dcc0a96722f673aff3f4979822dc6959

--- automatic/datagrip/tools/ChocolateyInstall.ps1
+++ automatic/datagrip/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments              = @{
     packageName         =  $env:ChocolateyPackageName
     softwareName        = 'JetBrains DataGrip*'
-    url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.1.exe'
-    checksum            = 'ba75b7c011bd193fa6e06af8da35fa4182a18568cdaa358353837e695a64950a'
+    url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.1.1.exe'
+    checksum            = '1e0207a23197fa6fd87bf04a58c55cb8548c54445b94b49e3699e12f0f106983'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = '/S'
