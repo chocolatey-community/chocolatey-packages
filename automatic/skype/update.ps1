1a9962083417db3bb328072ef73c7d8b2e24c4b4
AdmiringWorm <kim.nordmo@gmail.com>
2018-04-23 20:39:33 +0200

    (skype) Fixed update script
    fixes #1005

--- automatic/skype/update.ps1
+++ automatic/skype/update.ps1
@@ -1,48 +1,42 @@
 import-module au
+Import-Module "$env:ChocolateyInstall/helpers/chocolateyInstaller.psm1"
+
+$release = 'https://go.skype.com/classic.skype'
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.Url32)'"
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
+  }
 }
 
-function global:au_BeforeUpdate() {
-  $pp = $global:ProgressPreference
-  $global:ProgressPreference = 'SilentlyContinue'
-  try
-  {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
-    $Latest.ChecksumType32 = 'SHA256'
+function GetResultInformation([string]$url32) {
+  $dest = "$env:TEMP\skype.exe"
+  Get-WebFile $url32 $dest | Out-Null
+  $chType = 'SHA256'
+
+  try {
+    return @{
+      URL32          = $url32
+      Version        = Get-Item $dest | % { $_.VersionInfo.FileVersion }
+      Checksum32     = Get-FileHash $dest -Algorithm $chType | % Hash
+      ChecksumType32 = $chType
+    }
   }
-  finally
-  {
-    $global:ProgressPreference = $pp
+  finally {
+    Remove-Item -Force $dest
   }
 }
 
 function global:au_GetLatest {
-  $exeUrl = 'https://go.skype.com/classic.skype'
+  $url32 = Get-RedirectedUrl -url $release
 
-  $exePath = [System.IO.Path]::GetTempFileName()
-  $ProgressPreference = 'SilentlyContinue'
-  Invoke-WebRequest -Uri $exeUrl -OutFile $exePath -UseBasicParsing
-  try
-  {
-    $fileVersion = (Get-Item -Path $exePath).VersionInfo.FileVersion
-  }
-  finally
-  {
-    Remove-Item -Path $exePath -ErrorAction SilentlyContinue
-  }
-
-  @{
-    Url32 = 'https://download.skype.com/msi/SkypeSetup_{0}.msi' -f $fileVersion
-    Version = $fileVersion
-  }
+  return Update-OnETagChanged -execUrl $url32 -OnETagChanged {
+    GetResultInformation $url32
+  } -OnUpdate { @{ URL32 = $url32 }}
 }
 
 update -ChecksumFor none
