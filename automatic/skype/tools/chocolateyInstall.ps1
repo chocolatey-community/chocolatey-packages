c09043d37cd13c29e45f09b4ebb38c822172992c
Chocolatey <chocolatey@realdimensions.net>
2018-04-23 20:32:40 +0000

    AU: 1 updated - skype [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5b311a19c8fc0af67bd434934445df1dfb3008bd

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = ''
-  checksum       = ''
-  checksumType   = ''
+  url            = 'https://download.skype.com/5d7ac191986ccb485223c4e4e9e3e4ce/SkypeSetupFull.exe'
+  checksum       = '1E4CA5D441A00AC691C58E0AA5890C3FEB395D2DC53DBAE82FAEC7C9DF4E52FC'
+  checksumType   = 'SHA256'
   silentArgs     = '/qn /norestart STARTSKYPE=FALSE TRANSFORMS=:RemoveDesktopShortcut.mst TRANSFORMS=:RemoveStartup.mst'
   validExitCodes = @(0)
 }
