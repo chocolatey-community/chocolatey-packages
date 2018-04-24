495e424dd97a518b2add63654fec87a0709f1df2
AdmiringWorm <kim.nordmo@gmail.com>
2018-04-24 19:24:13 +0200

    (skype) Re-Added uninstallation before installing new version of skype

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -10,4 +10,23 @@ $packageArgs = @{
   validExitCodes = @(0)
 }
 
+# CHeck if the software is already installed
+[array]$key = Get-UninstallRegistryKey @packageArgs
+
+if ($key.Count -eq 1) {
+  $uninstallArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    file           = ''
+    fileType       = 'MSI'
+    silentArgs     = "$($key.PSChildName) /qn /norestart"
+    validExitCodes = $packageArgs.validExitCodes
+  }
+  Uninstall-ChocolateyPackage @uninstallArgs
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "This will most likely cause a 1603 error code when installing skype."
+}
+
 Install-ChocolateyPackage @packageArgs
