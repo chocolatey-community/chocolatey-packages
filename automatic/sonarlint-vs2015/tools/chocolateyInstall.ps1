d0d822dfb4de306a058d7bd5d669929cb26fcd83
Chocolatey <chocolatey@realdimensions.net>
2018-05-03 18:14:20 +0000

    AU: 3 updated - Brackets gpg4win sonarlint-vs2015 [skip ci]

--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-3.10.0.3095-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.0.0.3479-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
