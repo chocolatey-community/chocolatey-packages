4f90953e04c79119f203b7dbe7a91d96419e2044
Rui Lopes <rgl@ruilopes.com>
2018-05-04 22:18:06 +0100

    import existing gitextensions package sources

---
+++ manual/gitextensions/tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$packageName = 'gitextensions'
+$softwareName = 'Git Extensions*'
+$installerType = 'msi'
+$installArgs = '/quiet /norestart'
+$url = 'https://github.com/gitextensions/gitextensions/releases/download/v2.50.01/GitExtensions-2.50.01-Setup.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  silentArgs    = "/quiet /norestart"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+#------- ADDITIONAL SETUP -------#
+
+$osBitness = Get-ProcessorBits
+$is64bit = $osBitness -eq 64
+
+$progFiles = [System.Environment]::GetFolderPath('ProgramFiles')
+if ($is64bit -and $progFiles -notmatch 'x86') {$progFiles = "$progFiles (x86)"}
+
+$gitexPath = Join-Path $progFiles 'GitExtensions'
+Write-Host "Adding `'$gitexPath`' to the PATH so you can call gitex from the command line."
+Install-ChocolateyPath $gitexPath
+$env:Path = "$($env:Path);$gitexPath"
