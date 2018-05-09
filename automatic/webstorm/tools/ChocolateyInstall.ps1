929631768f9e7a981044131505de8c7a5a77388e
Chocolatey <chocolatey@realdimensions.net>
2018-05-09 12:22:09 +0000

    AU: 2 updated - libreoffice-fresh webstorm [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/cf9ede9dcde55f98d6e0a27f9fffef11f13b441b

--- automatic/webstorm/tools/ChocolateyInstall.ps1
+++ automatic/webstorm/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments              = @{
     packageName         =  $env:ChocolateyPackageName
     softwareName        = 'JetBrains WebStorm*'
-    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2018.1.2.exe'
-    checksum            = '4387792e0ed4d3d3263d5386b8e3f78c53fc27dae36434acf27f747c68d498f3'
+    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2018.1.3.exe'
+    checksum            = '3af0a9eb90a1f2c1767224280c4f3767eaef526da559a94b7c18306008ca3b48'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = '/S'
