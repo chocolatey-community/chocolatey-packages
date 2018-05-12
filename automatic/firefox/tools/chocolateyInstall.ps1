97d3835544b334939b4d1a49262f41204a2f39cc
Chocolatey <chocolatey@realdimensions.net>
2018-05-12 11:29:17 +0000

    AU: 2 updated - FirefoxESR krita [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ed0deccd7be6292b35d4477ca39ef6f7511dc29c

--- automatic/firefox/tools/chocolateyInstall.ps1
+++ automatic/firefox/tools/chocolateyInstall.ps1
@@ -4,10 +4,10 @@
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$packageName = 'Firefox'
-$softwareName = 'Mozilla Firefox'
+$packageName = 'FirefoxESR'
+$softwareName = 'Mozilla Firefox*ESR'
 
-$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '60.0')
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '52.8.0')
 
 if (Get-32bitOnlyInstalled -product $softwareName) {
   Write-Output $(
@@ -34,7 +34,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/60.0/win32/${locale}/Firefox%20Setup%2060.0.exe"
+    Url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/52.8.0esr/win32/${locale}/Firefox%20Setup%2052.8.0esr.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
@@ -43,7 +43,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
   if (!(Get-32bitOnlyInstalled($softwareName)) -and (Get-OSArchitectureWidth 64)) {
     $packageArgs.Checksum64 = $checksums.Win64
     $packageArgs.ChecksumType64 = 'sha512'
-    $packageArgs.Url64 = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/60.0/win64/${locale}/Firefox%20Setup%2060.0.exe"
+    $packageArgs.Url64 = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/52.8.0esr/win64/${locale}/Firefox%20Setup%2052.8.0esr.exe"
   }
 
   Install-ChocolateyPackage @packageArgs
