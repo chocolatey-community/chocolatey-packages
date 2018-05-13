c822cb862b0ee3643bb3ef9ed38ce6ecd4f50b8d
AdmiringWorm <kim.nordmo@gmail.com>
2018-05-13 14:12:26 +0200

    (skype) Fixed silent arguments [AU skype]

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   url            = 'https://endpoint920510.azureedge.net/s4l/s4l/download/win/Skype-8.21.0.9.exe'
   checksum       = '548261ce4c16771107617e3a567219f830e5c05fc9ecaf461faa73705c461fa3'
   checksumType   = 'sha256'
-  silentArgs     = '/qn /norestart STARTSKYPE=FALSE TRANSFORMS=:RemoveDesktopShortcut.mst TRANSFORMS=:RemoveStartup.mst'
+  silentArgs     = "/SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
 }
 
