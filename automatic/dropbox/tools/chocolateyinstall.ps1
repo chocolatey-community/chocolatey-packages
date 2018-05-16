706dd2795627baa42a2d8f90f928c9f41af3e008
Chocolatey <chocolatey@realdimensions.net>
2018-05-16 18:16:25 +0000

    AU: 2 updated - dropbox waterfox [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f0e0ab5460f82837ada05776f51e757e12bd8f55

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2050.3.67%20Offline%20Installer.exe'
-    checksum        = '36efd903c0dac0fe0d9a40abdc597c87e6bd7fe3d516b022114143e3a347ecc7'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2050.3.68%20Offline%20Installer.exe'
+    checksum        = 'cffe785bd4a2b01c2a9ab7563e6ca484a130feefd0bb2e1690c0df5faf5dceba'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
