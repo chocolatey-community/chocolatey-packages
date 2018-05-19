f60c1ffc3b1b37e6509a3b511df73ddb714f390d
RB <RedBaron2@users.noreply.github.com>
2018-05-18 18:18:13 -0700

    Update update.ps1
    This change will enable the finding of alpha/beta versions of the package too

--- automatic/360ts/update.ps1
+++ automatic/360ts/update.ps1
@@ -26,22 +26,27 @@ function global:au_SearchReplace {
 
 function Get360Version {
 param(
- [string]$releases,
+    [string]$releases,
 	[string]$PackageName,
 	[string]$Title
 )
 $regex = '(\d+\.\d+\.\d+\.\d+)'
-
-$HTML =  Invoke-WebRequest -UseBasicParsing -Uri $releases | Foreach-Object { $_ -match $regex } | Out-Null
+$pre_regex = "${regex}( Alpha)|${regex}( Beta)"
+$HTML = Invoke-WebRequest -UseBasicParsing -Uri $releases
+if ($HTML | Foreach-Object { $_ -match $pre_regex }) {
+$version = $Matches[0] -replace(' ','_')
+} else {
+$HTML | Foreach-Object { $_ -match $regex } | Out-Null
 $version = $Matches[0]
+}
 $url = "https://free.360totalsecurity.com/totalsecurity/${PackageName}_Setup_${version}.exe"
-
  @{
-    PackageName = ( $PackageName.ToLower() )
+    PackageName = $PackageName.ToLower()
     Title = $Title
-    Version = $version
+    Version = $version -replace('_','-')
     URL32   = $url
   }
+
 }
 
 $360_ts_url = 'https://www.360totalsecurity.com/en/version/360-total-security/'
