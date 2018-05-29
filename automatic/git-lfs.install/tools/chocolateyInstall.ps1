3c1b4760738dd93c57d19b02b91ccabcd65f6736
Chocolatey <chocolatey@realdimensions.net>
2018-05-29 06:09:37 +0000

    AU: 3 updated - chromium git-lfs git-lfs.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d32b3000bb44ce18c049dfa0619e2a1c844463da

--- automatic/git-lfs.install/tools/chocolateyInstall.ps1
+++ automatic/git-lfs.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\git-lfs-windows-2.4.1.exe"
+$filePath = "$toolsDir\git-lfs-windows-2.4.2.exe"
 
 $packageArgs = @{
   PackageName    = 'git-lfs'
