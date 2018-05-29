e915f9d994cbff68c7d746171fd61c5df8326fe2
Chocolatey <chocolatey@realdimensions.net>
2018-05-29 11:22:36 +0000

    AU: 2 updated - diskdefragtouch mattermost-desktop [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d3b7cfc920bb0c73be88c98cd2e01cf2d489774a

--- automatic/mattermost-desktop/tools/chocolateyInstall.ps1
+++ automatic/mattermost-desktop/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = 'mattermost-desktop'
   fileType       = 'exe'
-  file           = "$toolsPath\mattermost-setup-4.1.0-win32.exe"
-  file64         = "$toolsPath\mattermost-setup-4.1.0-win64.exe"
+  file           = "$toolsPath\mattermost-setup-4.1.2-win32.exe"
+  file64         = "$toolsPath\mattermost-setup-4.1.2-win64.exe"
   softwareName   = 'Mattermost*'
   # This is still Squirrel being used. Lets use --silent.
   silentArgs     = '/S --silent'
@@ -16,4 +16,4 @@ $packageArgs = @{
 Install-ChocolateyInstallPackage @packageArgs
 
 # Lets remove the installer and ignore files as there is no more need for them
-Get-ChildItem $toolsPath\*.exe | % { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
