ab77821edb6503a21c182b4894ef22ffea064109
Chocolatey <chocolatey@realdimensions.net>
2018-06-05 09:56:24 +0000

    AU: 3 updated - chromium lightscreen Opera [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b79121cb7abd2646d27f409f6d988867cb25a91d

--- automatic/lightscreen/tools/chocolateyInstall.ps1
+++ automatic/lightscreen/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
@@ -10,6 +10,6 @@ $packageArgs = @{
 
 Install-ChocolateyInstallPackage @packageArgs
 
-for ($i=0; $i -lt 3; $i++) { sleep 1; $p = ps LightScreen -ea 0; if ($p) { $p | kill; Write-Host "Process killed:" $p.Name; break }  }
+for ($i=0; $i -lt 3; $i++) { Start-Sleep 1; $p = Get-Process LightScreen -ea 0; if ($p) { $p | Stop-Process; Write-Host "Process killed:" $p.Name; break }  }
 
-Get-ChildItem -Path $toolsDir\*.exe | % { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
+Get-ChildItem -Path $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -5,17 +5,17 @@ $toolsPath = (Split-Path -Parent $MyInvocation.MyCommand.Definition)
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/53.0.2907.57/win/Opera_53.0.2907.57_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/53.0.2907.57/win/Opera_53.0.2907.57_Setup_x64.exe'
-  checksum       = '28f4f0415715d233ae7418c136d7fda91c8d6146324204c75f8bf99ed5b17d17'
-  checksum64     = '516809abfc8f02573fe387e6d3286f5449a777e4611ed7521b2c912a19645016'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/53.0.2907.88/win/Opera_53.0.2907.88_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/53.0.2907.88/win/Opera_53.0.2907.88_Setup_x64.exe'
+  checksum       = 'fb1c7fb42f0139bb2fc2898e5e9a7f34e8972abeef1673aca2033060927e9205'
+  checksum64     = '372b475380cdf1e401a5f61b0636d913a72f10b72e3a8531d8c305a978d9783d'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0'
   validExitCodes = @(0)
 }
 
-$version = '53.0.2907.57'
+$version = '53.0.2907.88'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
