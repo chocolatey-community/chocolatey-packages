4d5975551d0c419e94886fc7198dd26d824027fc
RB <RedBaron2@users.noreply.github.com>
2018-06-14 16:54:03 -0700

    Update update.ps1
    Changed the flow of the update file, and minor other requested changes

--- automatic/1password/update.ps1
+++ automatic/1password/update.ps1
@@ -14,52 +14,46 @@ function global:au_SearchReplace {
   }
 }
 
-function global:au_BeforeUpdate {
-  . "$PSScriptRoot/update_helper.ps1"
-    removeDependencies ".\*.nuspec"
-}
-
 function global:au_AfterUpdate {
   . "$PSScriptRoot/update_helper.ps1"
-  if ($Latest.PackageName -eq '1password') {
+  if ($Latest.PackageName -eq '1password4') {
+    removeDependencies ".\*.nuspec"
+  } else {
     addDependency ".\*.nuspec" 'dotnet4.6.1' '4.6.01055.20170308'
-  } 
+  }
 }
 
-function global:au_GetLatest {
+function Get-LatestOPW {
+param (
+	[string]$url,
+	[string]$kind,
+	[bool]$dependency
 
-  $streams = @{}
-  $res = @('OPWY';'OPW4';'OPW7')
-  $res | % {
-    $re = $_
-    switch -w ( $re ) {
-        'OPW4' {
-            $url32 = 'https://app-updates.agilebits.com/download/OPW4'
-			$kind = '1password4'
-        }
-        'OPW7' {
-            $url32 = 'https://app-updates.agilebits.com/download/OPW7'
-			$kind = '1password'
-        }
-        'OPWY' {
-            $url32 = 'https://app-updates.agilebits.com/download/OPW7/Y'
-			$kind = '1password'
-        }
-    }
-    $url32 = Get-RedirectedUrl $url32
+)
+
+    $url32 = Get-RedirectedUrl $url
     $verRe = '[-]|\.exe$'
     $version = $url32 -split $verRe | select -last 1 -skip 1
     $version = $version -replace('\.BETA',' beta')
     $version = Get-Version $version
     $major = $version.ToString(1)
 
-    if (!($streams.ContainsKey($kind))) {
-      $streams.Add($re, @{
+      @{
         URL32 = $url32
         Version = $version.ToString()
         PackageName = $kind
-      })
-    }
+      }
+  }
+  
+  $releases_opw4 = 'https://app-updates.agilebits.com/download/OPW4'
+  $kind_opw4     = '1password4'
+  $releases_opw  = 'https://app-updates.agilebits.com/download/OPW7/Y'
+  $kind_opw      = '1password'
+  
+function global:au_GetLatest {
+  $streams = [ordered] @{
+    OPW4 = Get-LatestOPW -url $releases_opw4 -kind $kind_opw4
+    OPW  = Get-LatestOPW -url $releases_opw -kind $kind_opw
   }
 
   return @{ Streams = $streams }
