95bd6aedc16e7ae69562556ba9fa96c12caef49d
Chocolatey <chocolatey@realdimensions.net>
2018-06-15 18:31:15 +0000

    AU: 2 updated - etcd webstorm [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5e7f8ad3b1975b0ba39913a6c2dd2776c257c703

--- automatic/webstorm/tools/ChocolateyInstall.ps1
+++ automatic/webstorm/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments              = @{
     packageName         =  $env:ChocolateyPackageName
     softwareName        = 'JetBrains WebStorm*'
-    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2018.1.4.exe'
-    checksum            = '8479d14d12f72b5c90bceb67c7bbc024f7dcf14ee79912dce324607be58a74a2'
+    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2018.1.5.exe'
+    checksum            = '5c3d1f3ab2bc6ace7a28d3339edf80e797e3cc33403735960911cc02377495e8'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = '/S'
