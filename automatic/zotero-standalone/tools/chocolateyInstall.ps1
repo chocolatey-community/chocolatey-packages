e3ddda3c3aab38004cd90deb5070654e8e1ea3be
Chocolatey <chocolatey@realdimensions.net>
2018-06-16 12:11:49 +0000

    AU: 1 updated - zotero-standalone [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f2e190f55f1fd8c646f69ade5f52208969c6a026

--- automatic/zotero-standalone/tools/chocolateyInstall.ps1
+++ automatic/zotero-standalone/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero Standalone *'
-  file           = "$toolsPath\Zotero-5.0.49_setup.exe"
+  file           = "$toolsPath\Zotero-5.0.51_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
