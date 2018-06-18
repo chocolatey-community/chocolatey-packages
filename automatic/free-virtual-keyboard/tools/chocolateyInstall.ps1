fa61e4661273bf0df8b3c36dc07491e00c0aa145
Chocolatey <chocolatey@realdimensions.net>
2018-06-18 18:33:36 +0000

    AU: 1 updated - free-virtual-keyboard [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/60f5ce493b82451ecb96a1bc9d0423b454b1c2ed

--- automatic/free-virtual-keyboard/tools/chocolateyInstall.ps1
+++ automatic/free-virtual-keyboard/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
   url            = 'http://freevirtualkeyboard.com/FreeVKSetup.exe'
-  checksum       = 'f5425f3d2ff11e8a0b98fc5fa652802a6e60b8c6361aa562002ea213d86d8aef'
+  checksum       = '2d39cfa60aa3c350919b60986c8c747e1106f29cee9ddb805f23249d7783d657'
   softwareName   = 'Free Virtual Keyboard'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
@@ -18,7 +18,7 @@ Install-ChocolateyPackage @packageArgs
 $installLocation = Get-AppInstallLocation $packageArgs.softwareName
 if ($installLocation) {
   Write-Host "$packageName installed to '$installLocation'"
-  $path = gci "$installLocation\*.exe" | select -First 1 -ExpandProperty FullName
+  $path = Get-ChildItem "$installLocation\*.exe" | Select-Object -First 1 -ExpandProperty FullName
   Register-Application $path -Name $packageName
   Write-Host "$packageName registered as $packageName"
 } else {
@@ -26,4 +26,4 @@ if ($installLocation) {
 }
 
 # Installer will start the process after installation, kill it
-ps freevk -ea 0 | kill
+Get-Process freevk -ea 0 | Stop-Process
