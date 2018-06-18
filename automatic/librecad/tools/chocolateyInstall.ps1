1092eafa764037521cdb874d65ee5f1ef6927104
Chocolatey <chocolatey@realdimensions.net>
2018-06-18 16:45:56 +0000

    AU: 1 updated - librecad [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b4218528916074ba2ba4ceb58e956120fed9e22c

--- automatic/librecad/tools/chocolateyInstall.ps1
+++ automatic/librecad/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\"
-  softwareName   = 'librecad*'
+  file           = "$toolsPath\LibreCAD-Installer-2.2.0-rc1.exe"
+  softwareName   = 'LibreCAD'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
