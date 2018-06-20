d45c1291d77ab7b464f16dc8d93823ebdc07f4cb
Chocolatey <chocolatey@realdimensions.net>
2018-06-20 14:49:44 +0000

    AU: 2 updated - datagrip freevideoeditor [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b4741be6cfebee1f1b425921018b190c37039304

--- automatic/datagrip/tools/ChocolateyInstall.ps1
+++ automatic/datagrip/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments              = @{
     packageName         =  $env:ChocolateyPackageName
     softwareName        = 'JetBrains DataGrip*'
-    url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.1.4.exe'
-    checksum            = 'd679a74ae8edaf96fee811c0ef5cc57c60ddefc42f23249baeff11733b302696'
+    url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.1.5.exe'
+    checksum            = '899a75e8e95a4d165a32b6badb77f61f75b6d0e109faa140d8818f0051a6d3bd'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = '/S'
