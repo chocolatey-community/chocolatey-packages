96a0cf853906f525bc4d7b7c162a82a351c1290c
Chocolatey <chocolatey@realdimensions.net>
2018-06-28 06:17:29 +0000

    AU: 4 updated - chromium python python3 smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d0a4fd2cf31d1638e1995019ea586c47d425e71b

--- automatic/python3/tools/chocolateyInstall.ps1
+++ automatic/python3/tools/chocolateyInstall.ps1
@@ -13,8 +13,8 @@ if ( $pp.InstallDir ) {
 $packageArgs = @{
     packageName    = 'python3'
     fileType       = 'exe'
-    file           = "$toolsPath\python-3.7.0rc1.exe"
-    file64         = "$toolsPath\python-3.7.0rc1-amd64.exe"
+    file           = "$toolsPath\python-3.7.0.exe"
+    file64         = "$toolsPath\python-3.7.0-amd64.exe"
     silentArgs     = '/quiet InstallAllUsers=1 PrependPath=1 TargetDir="{0}"' -f $installDir
     validExitCodes = @(0)
     softwareName   = 'Python*'
