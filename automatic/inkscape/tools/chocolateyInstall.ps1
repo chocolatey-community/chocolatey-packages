4344f2ee94adee87862904b727a1b04abe2228ee
AdmiringWorm <kim.nordmo@gmail.com>
2018-07-02 05:41:18 +0200

    (inkscape) Changed to embedded package
    ref #866

--- automatic/inkscape/tools/chocolateyInstall.ps1
+++ automatic/inkscape/tools/chocolateyInstall.ps1
@@ -1,43 +1,45 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName            = 'InkScape'
-  fileType               = 'msi'
-  url                    = 'https://inkscape.org/en/gallery/item/12193/inkscape-0.92.3-x86.msi'
-  url64bit               = 'https://inkscape.org/en/gallery/item/12190/inkscape-0.92.3-x64.msi'
-  checksum               = 'c64f3de51ff87e50b24ba82ee3b7275b80a71ba167e000ef9accaeb08543dd17'
-  checksum64             = '1d2f4d9edf221fe9dd1ad75c4a48c8809dd3adf5af0fb672f1383088b163516e'
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  silentArgs             = '/passive /norestart'
-  validExitCodes         = @(0)
-  softwareName           = 'InkScape*'
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'msi'
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
+  softwareName   = 'InkScape*'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0)
 }
+
 [array]$key = Get-UninstallRegistrykey $packageArgs.softwareName
 if ($key.Count -eq 1) {
   if ($key.DisplayVersion -eq '') {
     Write-Host "Software already installed"
     return
-  } else {
+  }
+  else {
     Uninstall-ChocolateyPackage -packageName $packageArgs.packageName `
       -fileType $packageArgs.fileType `
       -silentArgs "$($key.PSChildName) $($packageArgs.silentArgs)" `
       -validExitCodes $packageArgs.validExitCodes `
       -file ''
   }
-} elseif ($key.Count -gt 1) {
+}
+elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "This will most likely cause a 1603/1638 failure when installing InkScape."
   Write-Warning "Please uninstall InkScape before installing this package."
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+ls $toolsPath\*.msi | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
 
 $packageName = $packageArgs.packageName
 $installLocation = Get-AppInstallLocation $packageName
-if ($installLocation)  {
-    Write-Host "$packageName installed to '$installLocation'"
+if ($installLocation) {
+  Write-Host "$packageName installed to '$installLocation'"
 }
 else { Write-Warning "Can't find $PackageName install location" }
--- automatic/inkscape/update.ps1
+++ automatic/inkscape/update.ps1
@@ -3,23 +3,26 @@ Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $domain = 'https://inkscape.org'
 
-function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
-            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
-            "(?i)(DisplayVersion \-eq\s*)('.*')" = "`$1'$($Latest.RemoteVersion).0'"
-        }
+function global:au_BeforeUpdate { Get-RemoteFIles -Purge -NoSuffix }
 
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-        }
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.UpdateUrl)>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    "$($Latest.PackageName).nuspec" = @{
+      "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+    }
+  }
 }
 
 function global:au_GetLatest {
@@ -35,13 +38,14 @@ function global:au_GetLatest {
   $url64 = $64bit_page.links | ? href -match $re | select -first 1 -expand href | % { $domain + $_ }
 
   @{
-    Version      = $version
-    RemoteVersion= $version
-    URL32        = $url32
-    URL64        = $url64
-    ReleaseNotes = $redirUrl + "#left-column"
-    PackageName  = 'InkScape'
+    Version       = $version
+    RemoteVersion = $version
+    URL32         = $url32
+    URL64         = $url64
+    ReleaseNotes  = $redirUrl + "#left-column"
+    UpdateUrl     = $redirUrl + "windows"
+    PackageName   = 'InkScape'
   }
 }
 
-update
+update -ChecksumFor none
