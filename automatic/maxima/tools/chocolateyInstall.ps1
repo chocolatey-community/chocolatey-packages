118770ba05e6efb70f8b7fcf7ad15d1be5713039
AdmiringWorm <kim.nordmo@gmail.com>
2018-07-02 06:45:08 +0200

    (maxima) Changed to embedded package
    ref #866

--- automatic/maxima/tools/chocolateyInstall.ps1
+++ automatic/maxima/tools/chocolateyInstall.ps1
@@ -1,16 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
 $packageArgs = @{
-  packageName            = 'maxima'
-  fileType               = 'exe'
-  url                    = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.41.0-Windows/maxima-clisp-sbcl-5.41.0-win32.exe'
-  url64bit               = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.41.0-Windows/maxima-clisp-sbcl-5.41.0-win64.exe'
-  checksum               = '7d767879824590816ba3ba7c24ed2f494071b69a480df3e8e88288b67091f914'
-  checksum64             = '1fdbd75c9e67840924b61796fb4771a9fb19ab3ecf2b4c28e6f0c1fb0f6f6dbc'
-  checksumType           = 'sha256'
-  checksumType64         = 'sha256'
-  silentArgs             = '/S'
-  validExitCodes         = @(0)
-  softwareName           = 'maxima*'
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
+  softwareName   = 'maxima*'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
 }
-Install-ChocolateyPackage @packageArgs
+
+Install-ChocolateyInstallPackage @packageArgs
+
+ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
--- automatic/maxima/update.ps1
+++ automatic/maxima/update.ps1
@@ -1,34 +1,67 @@
-import-module au
+﻿Import-Module AU
 
 $releases = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows'
+$softwareName = 'maxima*'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameSkip 1 }
 
 function global:au_SearchReplace {
-   @{
-        "tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
-        }
-
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-        }
+  @{
+    ".\legal\VERIFICATION.txt"        = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'"         = "`${1}'$softwareName'"
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+    }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $version = ($download_page.Links | ? href -match "Windows\/" | select -expand href -first 1) -split '\-|\/' | select -last 1 -skip 2
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    @{
-        Version      = $version
-        URL32        = "https://sourceforge.net/projects/maxima/files/Maxima-Windows/${version}-Windows/maxima-clisp-sbcl-${version}-win32.exe"
-        URL64        = "https://sourceforge.net/projects/maxima/files/Maxima-Windows/${version}-Windows/maxima-clisp-sbcl-${version}-win64.exe"
-        ReleaseNotes = "https://sourceforge.net/p/maxima/code/ci/master/tree/ChangeLog-$($version -replace '\.\d+$').md"
-    }
+  $re = '[\d\.]+\-Windows\/$'
+  $releasesUrl = $download_page.Links | ? href -match $re | select -first 1 -expand href | % { 'https://sourceforge.net' + $_ }
+  $download_page = Invoke-WebRequest -Uri $releasesUrl -UseBasicParsing
+
+  $re = 'win32\.exe\/download$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
+
+  $re = 'win64\.exe\/download$'
+  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href
+
+  $verRe = '-'
+  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
+  $version64 = $url64 -split "$verRe" | select -last 1 -skip 1
+  if ($version32 -ne $version64) {
+    throw "32bit version do not match the 64bit version"
+  }
+  if ($version32 -match '[a-z]$') {
+    [char]$letter = $version32[$version32.Length - 1]
+    [int]$num = $letter - [char]'a'
+    $num++
+    $version32 = $version32 -replace $letter, ".$num"
+  }
+
+  $versionTwoPart = $version32 -replace '^(\d+\.\d+)\.[\d\.]+$',"`$1"
+
+
+  @{
+    URL32        = $url32
+    URL64        = $url64
+    Version      = $version32
+    ReleaseNotes = "https://sourceforge.net/p/maxima/code/ci/master/tree/ChangeLog-$($versionTwoPart).md"
+    FileType     = 'exe'
+  }
 }
 
-update
+update -ChecksumFor none
