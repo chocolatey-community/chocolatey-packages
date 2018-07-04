1cc739de9afec6784a7c4454f7800e7f19be60c5
Chocolatey <chocolatey@realdimensions.net>
2018-07-04 02:36:15 +0000

    AU: 1 updated - owncloud-client [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/bc37c95489800c401c83421951f07c478a86eb46

--- automatic/owncloud-client/tools/chocolateyInstall.ps1
+++ automatic/owncloud-client/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\"
-  softwareName   = 'owncloud-client*'
+  file           = "$toolsPath\ownCloud-2.5.0.9736alpha1-setup.exe"
+  softwareName   = 'ownCloud'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
