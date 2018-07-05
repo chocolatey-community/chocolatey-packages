405ed979b0e15bd1c70f61739e596949372e3331
Chocolatey <chocolatey@realdimensions.net>
2018-07-05 12:10:58 +0000

    AU: 1 updated - rapidee [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3c1b7f77e3d3dff556cdb406ff187f6a1a59263e

--- automatic/rapidee/tools/chocolateyInstall.ps1
+++ automatic/rapidee/tools/chocolateyInstall.ps1
@@ -1,17 +1,17 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     PackageName    = ''
-    FileFullPath   = gi $toolsPath\*_x32.zip
-    FileFullPath64 = gi $toolsPath\*_x64.zip
+    FileFullPath   = Get-Item $toolsPath\*_x32.zip
+    FileFullPath64 = Get-Item $toolsPath\*_x64.zip
     Destination    = $toolsPath
 }
 
-ls $toolsPath\* | ? { $_.PSISContainer } | rm -Recurse -Force #remove older package dirs
+Get-ChildItem $toolsPath\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
 Get-ChocolateyUnzip @packageArgs
-rm $toolsPath\*.zip -ea 0
+Remove-Item $toolsPath\*.zip -ea 0
 
 $pp = Get-PackageParameters
 if (!$pp.NoShortcut) {
