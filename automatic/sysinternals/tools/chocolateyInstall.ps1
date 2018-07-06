e30046761e58388dbde25c53d8e47a2bb0851a1a
Chocolatey <chocolatey@realdimensions.net>
2018-07-06 06:10:55 +0000

    AU: 3 updated - chromium smartftp sysinternals [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0b7078826385c286dd2c06153d8e25b9ea0cd9e9

--- automatic/sysinternals/tools/chocolateyInstall.ps1
+++ automatic/sysinternals/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ Write-Host "Sysinternals Suite is going to be installed in '$installDir'"
 $packageArgs = @{
   packageName    = 'sysinternals'
   url            = 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
-  checksum       = 'c6cd0795f6f5754ad2ceab576862e5142b8829b9e5004d5081822bfd3ba0ac10'
+  checksum       = 'b4be0d2e3b35b1c8b9bbc001a0dd3ae4db59bdb2a1ceeabd744a54002fb64c2c'
   checksumType   = 'sha256'
   unzipLocation  = $installDir
 }
@@ -18,7 +18,7 @@ Accept-Eula
 if ($installDir -ne $toolsPath) { Install-ChocolateyPath $installDir }
 if (Is-NanoServer) {
   $packageArgs.url = 'https://download.sysinternals.com/files/SysinternalsSuite-Nano.zip'
-  $packageArgs.checksum = '1217c890244a324628078bb6826496260e8e44eb40ab20761bc2d76398fa276c'
+  $packageArgs.checksum = 'bfff457db201fe022dc528a13200e080281b950ad2c6ca0c85957b6caef2cd2c'
  }
 
 $old_path = 'c:\sysinternals'
