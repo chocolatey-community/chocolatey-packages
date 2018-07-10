f9bf30f39d67f398e34ae80ddb700dbeae5ca6d7
Chocolatey <chocolatey@realdimensions.net>
2018-07-10 12:16:52 +0000

    AU: 1 updated - flashplayerppapi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4e109bb706d40599e3d394ebba6632bb4c38ed36

--- automatic/flashplayerppapi/tools/ChocolateyInstall.ps1
+++ automatic/flashplayerppapi/tools/ChocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'msi'
-  url           = 'https://download.macromedia.com/pub/flashplayer/pdc/30.0.0.113/install_flash_player_30_ppapi.msi'
+  url           = 'https://download.macromedia.com/pub/flashplayer/pdc/30.0.0.134/install_flash_player_30_ppapi.msi'
   silentArgs    = '/quiet /norestart REMOVE_PREVIOUS=YES'
   softwareName  = 'Flash Player PPAPI'
-  checksum      = 'c304f19a278bb749e578bdaa2f906762a2cf9db4e3001e16d51aa75d3aea0fcb'
+  checksum      = 'faf9b3d3ae777ca2b5e1e72ec3131a3edd47cbe0a46231dd8151b7a5d4359d0f'
   checksumType  = 'sha256'
 }
 
