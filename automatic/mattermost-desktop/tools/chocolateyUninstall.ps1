143229ccb430e414ec267f66dd8912e999f546b2
William Gathoye <william@gathoye.be>
2018-07-12 01:16:46 -0700

    Respect convention: use toolDir and 2 spaces

--- automatic/mattermost-desktop/tools/chocolateyUninstall.ps1
+++ automatic/mattermost-desktop/tools/chocolateyUninstall.ps1
@@ -10,7 +10,7 @@ $validExitCodes = @(0)
 if ($key.Count -eq 1) {
 
 	# We need to remove the --uninstall argument from the uninstall string.
-    $file = $key.UninstallString.Substring(0, $key.UninstallString.IndexOf(' '))
+  $file = $key.UninstallString.Substring(0, $key.UninstallString.IndexOf(' '))
 
 	Uninstall-ChocolateyPackage -PackageName $packageName `
 								-FileType $installerType `
@@ -19,10 +19,10 @@ if ($key.Count -eq 1) {
 								-File "$file"
 
 } elseif ($key.Count -eq 0) {
-    Write-Warning "$packageName has already been uninstalled by other means."
+  Write-Warning "$packageName has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
-    Write-Warning "$key.Count matches found!"
-    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-    Write-Warning "Please alert package maintainer the following keys were matched:"
-    $key | ForEach-Object {Write-Warning "- $_.DisplayName"}
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | ForEach-Object {Write-Warning "- $_.DisplayName"}
 }
