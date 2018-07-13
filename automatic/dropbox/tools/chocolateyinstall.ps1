e7cc7b9fdfce4689c997a54b76835dc3b9668efd
Chocolatey <chocolatey@realdimensions.net>
2018-07-13 18:11:55 +0000

    AU: 2 updated - dropbox smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/161294e0229715fbc00e85757f87b8c9e3137162

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2053.4.66%20Offline%20Installer.exe'
-    checksum        = 'e6f367be2506d0a599f19d6e80f968541f02971498c5d3862add7dff5cc7cea7'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2053.4.67%20Offline%20Installer.exe'
+    checksum        = 'c50b9b6ec6eba6aa29746bf3659eeb914061a496256f643733982ee70f6b1c1f'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
