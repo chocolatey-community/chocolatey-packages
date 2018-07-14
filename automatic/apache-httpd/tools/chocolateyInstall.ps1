61e8e084220f928f7135cd5be320da2f35b513db
Chocolatey <chocolatey@realdimensions.net>
2018-07-14 18:13:59 +0000

    AU: 1 updated - apache-httpd [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/aabf7fb4c1fdcb3d4cd4a5ff962876761107e97d

--- automatic/apache-httpd/tools/chocolateyInstall.ps1
+++ automatic/apache-httpd/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $pp = Get-PackageParameters
 
 $arguments = @{
     packageName = $env:chocolateyPackageName
-    file        = "$toolsDir\httpd-2.4.33-o102o-x86-vc14-r2.zip"
-    file64      = "$toolsDir\httpd-2.4.33-o102o-x64-vc14-r2.zip"
+    file        = "$toolsDir\httpd-2.4.34-o102o-x86-vc14.zip"
+    file64      = "$toolsDir\httpd-2.4.34-o102o-x64-vc14.zip"
     destination = if ($pp.installLocation) { $pp.installLocation } else { $env:APPDATA }
     port        = if ($pp.Port) { $pp.Port } else { 8080 }
     serviceName = if ($pp.NoService) { $null } elseif ($pp.serviceName) { $pp.serviceName } else { 'Apache' }
