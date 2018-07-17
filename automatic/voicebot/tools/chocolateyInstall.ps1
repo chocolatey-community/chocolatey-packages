faf3ce90a9c3ca2687a9f6ac9ed874cd0e169322
Chocolatey <chocolatey@realdimensions.net>
2018-07-17 00:11:49 +0000

    AU: 1 updated - voicebot [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/fa5e0518857df59cb4a7c5f987d0aa89233e653b

--- automatic/voicebot/tools/chocolateyInstall.ps1
+++ automatic/voicebot/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'VoiceBot'
-    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/123/VoiceBotSetup-3.3.1.exe'
-    checksum        = '5c30028a73d2250ca2368d2f4a25b3b659e203b204bedaa08fd031a0381a9626'
+    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/123/VoiceBotSetup-3.3.2.exe'
+    checksum        = '82f4cb7098f7aeaaa23d64a423860ab750df5b03fc5fa3a66da30c678ef2abe6'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/VERYSILENT /LAUNCHAFTER=0'
