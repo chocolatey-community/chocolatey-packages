2270e6818f5ffb92808cfa1efbe077ac7782fa0b
Chocolatey <chocolatey@realdimensions.net>
2018-07-23 12:12:13 +0000

    AU: 3 updated - filezilla webstorm wesnoth [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d06bf72a85054a1561cc90efd324dba6a67231dc

--- automatic/webstorm/tools/ChocolateyInstall.ps1
+++ automatic/webstorm/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments              = @{
     packageName         =  $env:ChocolateyPackageName
     softwareName        = 'JetBrains WebStorm*'
-    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2018.1.5.exe'
-    checksum            = '5c3d1f3ab2bc6ace7a28d3339edf80e797e3cc33403735960911cc02377495e8'
+    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2018.2.exe'
+    checksum            = 'aa2e2d70947b360e02d2d15c65dea44ab039f5a8485cde03c2fde233dd8c6291'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = '/S'
--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.14/wesnoth-1.14.3/wesnoth-1.14.3-win32.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.14/wesnoth-1.14.4/wesnoth-1.14.4-win32.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = '81799aa26e349e55c330cc6dc95096cf0e4573c07e4215c8bbad999a66f8665b'
+  checksum       = 'fe6c276970efa5d510a31976c58949b034aa3f393ee7ea75ccfbbfdfd4c5cc22'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
