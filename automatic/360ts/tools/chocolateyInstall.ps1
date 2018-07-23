0d8262eb7e1977e63611bf6f013c5ae743fff279
Chocolatey <chocolatey@realdimensions.net>
2018-07-23 18:11:03 +0000

    AU: 3 updated - 360ts filezilla minikube [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0eafce4fc90edd3664a103fb24b6078519ebe299

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.0.0.1159.exe'
-  checksum               = 'ef5850ecf62283e0108186deeb093e9f877bcb4b485219b1421bbe84be4585fb'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.0.0.1162.exe'
+  checksum               = 'd5fb4185831e20977e939bcfabbf13f71a147aa25ca90dd0b36cf0a209aaa02b'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
