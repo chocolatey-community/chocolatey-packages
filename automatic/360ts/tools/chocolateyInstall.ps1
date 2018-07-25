e1dd77dfbde8673c95add9b68c6d6354fa8a1277
Chocolatey <chocolatey@realdimensions.net>
2018-07-25 18:12:19 +0000

    AU: 2 updated - 360ts Opera [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9b0e0d3c80475cf1d88e6e19825d71d6ab933a3c

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.0.0.1162.exe'
-  checksum               = 'd5fb4185831e20977e939bcfabbf13f71a147aa25ca90dd0b36cf0a209aaa02b'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.0.0.1167.exe'
+  checksum               = 'aa5e949bd587a2457141252208065915091c2f4d0d89d60b1b638e8147cf7b67'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/54.0.2952.60/win/Opera_54.0.2952.60_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/54.0.2952.60/win/Opera_54.0.2952.60_Setup_x64.exe'
-  checksum       = 'd842b044e66f7f6bafc55b88275ac867dc6d77aa999e01f780bfc37688bd0031'
-  checksum64     = '0e8457330d070d4180ec1a458c1c57577db517c683a4aa80ab840769feb0d030'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/54.0.2952.64/win/Opera_54.0.2952.64_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/54.0.2952.64/win/Opera_54.0.2952.64_Setup_x64.exe'
+  checksum       = '07e0a94d5e24e13888d7bfd8030c78f3d04c3140d7874aebefa6f386b6bda49d'
+  checksum64     = '5ce1bf207a9ab2df4e37d80a0de42fbd832dd1ef666c86f54775e2e60130ac7f'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '54.0.2952.60'
+$version = '54.0.2952.64'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
