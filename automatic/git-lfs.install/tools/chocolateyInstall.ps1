35c9faead06d07dad4c559737facbeadd2418144
Chocolatey <chocolatey@realdimensions.net>
2018-07-27 00:11:07 +0000

    AU: 2 updated - git-lfs git-lfs.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/32b765a5db5d6c949c5e94219ea946d1c8f29837

--- automatic/git-lfs.install/tools/chocolateyInstall.ps1
+++ automatic/git-lfs.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\git-lfs-windows-2.4.2.exe"
+$filePath = "$toolsDir\git-lfs-windows-2.5.0.exe"
 
 $packageArgs = @{
   PackageName    = 'git-lfs'
