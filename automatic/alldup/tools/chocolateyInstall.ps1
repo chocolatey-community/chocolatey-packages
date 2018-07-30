46a73e79e38a07f25f220342617333ab51579030
AdmiringWorm <kim.nordmo@gmail.com>
2018-07-30 19:46:43 +0200

    (alldup) Updated to latest script guidelines

--- automatic/alldup/tools/chocolateyInstall.ps1
+++ automatic/alldup/tools/chocolateyInstall.ps1
@@ -1,17 +1,12 @@
-﻿$packageName  = 'alldup'
-$url          = 'http://www.alldup.info/download/AllDupSetup.exe'
-$checksum     = '038ca09820c340f5f6f230ea21c218bcc92f22805bbf400218f29411da16ad46'
-$checksumType = 'sha256'
+﻿$ErrorActionPreference = 'Stop'
 
-$packageArgs  = @{
-  packageName    = $packageName
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
+  url            = 'http://www.alldup.info/download/AllDupSetup.exe'
   softwareName   = 'AllDup*'
-
-  checksum       = $checksum
-  checksumType   = $checksumType
-  url            = $url
-
+  checksum       = '038ca09820c340f5f6f230ea21c218bcc92f22805bbf400218f29411da16ad46'
+  checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
 }
