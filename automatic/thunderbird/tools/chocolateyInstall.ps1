3f3eb756e3c2637e9ccdac784c103e05e543b2d3
Chocolatey <chocolatey@realdimensions.net>
2018-08-06 18:10:54 +0000

    AU: 3 updated - filezilla mkdocs-material thunderbird [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/cf6f7baf567bd2ea6f7521de63dbcd42e5227960

--- automatic/thunderbird/tools/chocolateyInstall.ps1
+++ automatic/thunderbird/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'thunderbird'
 $softwareName = 'Mozilla Thunderbird'
 
-$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '52.9.0')
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '60.0')
 
 if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
   Write-Output $(
@@ -26,7 +26,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/52.9.0/win32/${locale}/Thunderbird%20Setup%2052.9.0.exe"
+    Url = "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/60.0/win32/${locale}/Thunderbird%20Setup%2060.0.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
