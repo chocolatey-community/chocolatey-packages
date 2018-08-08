b7c8ba3a60f44a85ab1017d6e60efd9c85906d03
Chocolatey <chocolatey@realdimensions.net>
2018-08-08 06:11:22 +0000

    AU: 2 updated - kubernetes-cli sysinternals [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ec834c8706715e2f445d9242c2714044ab98873a

--- automatic/sysinternals/tools/chocolateyInstall.ps1
+++ automatic/sysinternals/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ Write-Host "Sysinternals Suite is going to be installed in '$installDir'"
 $packageArgs = @{
   packageName    = 'sysinternals'
   url            = 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
-  checksum       = 'bd5bd2019b4130644c859cbebc0a84123f808918fab4929593d37de0872b54b1'
+  checksum       = '533e40025ee69a1b4b436dc6c996c7b34c2b16cec1b45cea0199f19092e77810'
   checksumType   = 'sha256'
   unzipLocation  = $installDir
 }
@@ -18,7 +18,7 @@ Accept-Eula
 if ($installDir -ne $toolsPath) { Install-ChocolateyPath $installDir }
 if (Is-NanoServer) {
   $packageArgs.url = 'https://download.sysinternals.com/files/SysinternalsSuite-Nano.zip'
-  $packageArgs.checksum = 'bfff457db201fe022dc528a13200e080281b950ad2c6ca0c85957b6caef2cd2c'
+  $packageArgs.checksum = '879a99d8f7975109a7312618d9f2fc383a0baba0f911423d2c8188d644bbb953'
  }
 
 $old_path = 'c:\sysinternals'
