04c24e03f407acb60575a4481377bda119768453
Chocolatey <chocolatey@realdimensions.net>
2018-08-14 18:13:38 +0000

    AU: 2 updated - maxthon.commandline OpenSSL.Light [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/05396c3e25c40edaae4427781a438022f56b9f72

--- automatic/maxthon.commandline/tools/chocolateyInstall.ps1
+++ automatic/maxthon.commandline/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  url          = 'http://dl.maxthon.com/mx5/maxthon_portable_5.2.3.4000.7z'
-  checksum     = '3ddaf9ac551f59a62077f4d1693bb40172700c9437575fccc03cac021ef9a503'
+  url          = 'http://dl.maxthon.com/mx5/maxthon_portable_5.2.3.6000.7z'
+  checksum     = '9dee3256aa712defab63ca83dcf45b8de62b08bac577d0fa49c93e7463237804'
   checksumType = 'sha256'
   destination  = $toolsDir
 }
