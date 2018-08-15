27b7d1e1946da8987618f1a5aa68859d86e80f36
Chocolatey <chocolatey@realdimensions.net>
2018-08-15 18:20:08 +0000

    AU: 2 updated - datagrip octave.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9ab5e513f6688a596984e786b3a6a0e6dba9db12

--- automatic/datagrip/tools/ChocolateyInstall.ps1
+++ automatic/datagrip/tools/ChocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments              = @{
     packageName         =  $env:ChocolateyPackageName
     softwareName        = 'JetBrains DataGrip*'
-    url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.2.1.exe'
-    checksum            = '6ca718d80caf30f5a53baf9f08e5c9a571078b946352c25cd959bd6404d74ecb'
+    url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.2.2.exe'
+    checksum            = '77bcf7534d83de5e5ed9c75296b6e310ae3f912baffa373392a8b6037c10fd68'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = '/S'
