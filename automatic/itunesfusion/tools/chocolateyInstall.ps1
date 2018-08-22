f6fb3a13a3972b513df774f6af0698fe457ad32f
Chocolatey <chocolatey@realdimensions.net>
2018-08-22 18:13:20 +0000

    AU: 2 updated - itunesfusion mkdocs [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c939a39f581480da43dca18c6b180ab8324be029

--- automatic/itunesfusion/tools/chocolateyInstall.ps1
+++ automatic/itunesfusion/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'iTunesFusion'
-    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/102/iTunesFusionSetup-3.1.exe'
-    checksum        = '66cc4d7ad2d8e23c41013f20e0ca5109d452df7a04eb027a6cd1312764875ab9'
+    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/102/iTunesFusionSetup-3.2.exe'
+    checksum        = '1776bcb9621e1c36ace76f2e68d4c704d98a079f4caaf7f8e25bfe59b89106fc'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/VERYSILENT /LAUNCHAFTER=0'
--- automatic/mkdocs/tools/ChocolateyInstall.ps1
+++ automatic/mkdocs/tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿Update-SessionEnvironment
 
-$version = '1.0.1'
+$version = '1.0.2'
 
 $proxy = Get-EffectiveProxy
 if ($proxy) {
