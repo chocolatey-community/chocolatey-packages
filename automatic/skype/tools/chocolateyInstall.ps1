304a68a1d617e98aa734bc0a94fb4115b3790c9d
Chocolatey <chocolatey@realdimensions.net>
2018-08-28 00:11:15 +0000

    AU: 2 updated - skype youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0d4016fe4753670912e96696a1e052150b5ef211

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://endpoint920510.azureedge.net/s4l/s4l/download/win/Skype-8.28.0.41.exe'
-  checksum       = '29e65e1f11f634ece6ab1dcc5a5c8edc3a93514b5a189bbf76e63222859dc142'
+  url            = 'https://endpoint920510.azureedge.net/s4l/s4l/download/win/Skype-8.29.0.47.exe'
+  checksum       = '5d932a8f98b0e6de221ce6f3c29c7c552245ec55a51a1c1e11e566b6f7b8a694'
   checksumType   = 'sha256'
   silentArgs     = "/SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
