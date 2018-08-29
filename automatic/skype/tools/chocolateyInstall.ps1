eada23d1d81897c68b582ded436cd6d6215015e0
Chocolatey <chocolatey@realdimensions.net>
2018-08-29 06:12:35 +0000

    AU: 2 updated - skype spotify [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0d4500453210f4ee7cc58895ad32778817d26104

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://endpoint920510.azureedge.net/s4l/s4l/download/win/Skype-8.29.0.47.exe'
-  checksum       = '5d932a8f98b0e6de221ce6f3c29c7c552245ec55a51a1c1e11e566b6f7b8a694'
+  url            = 'https://endpoint920510.azureedge.net/s4l/s4l/download/win/Skype-8.29.0.50.exe'
+  checksum       = '12509d8d133cab7dd1bee14023f60cefa0dbb2609bd0479ea46a535f4dd33ad8'
   checksumType   = 'sha256'
   silentArgs     = "/SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
