aad10f8bf20da5b68cf1dd853753e6e158fb9978
Chocolatey <chocolatey@realdimensions.net>
2018-08-30 12:11:19 +0000

    AU: 1 updated - ccleaner [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1b2b5f749a9b6ba61520467919dce2e369c28cdb

--- automatic/ccleaner/tools/chocolateyInstall.ps1
+++ automatic/ccleaner/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'ccleaner'
-$url32       = 'https://download.ccleaner.com/ccsetup545.exe'
+$url32       = 'https://download.ccleaner.com/ccsetup546.exe'
 $url64       = $url32
-$checksum32  = '805b136fcedbf5451e6d86c89cb90c0198d0e68ff42daee8e95511aea744f543'
+$checksum32  = '6588472b3db3e25bba7fb0077d9a6f7dc38337034ebcdf33599f22071da70e79'
 $checksum64  = $checksum32
 
 if ($Env:ChocolateyPackageParameters -match '/UseSystemLocale') {
