720acf1092253e7bc7fd18c29d9c026551e166c6
Chocolatey <chocolatey@realdimensions.net>
2018-08-31 18:13:56 +0000

    AU: 7 updated - Cygwin dropbox gpg4win vlc winscp winscp.install winscp.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5c8e3f753b3db639ff30fb56154b828d8a2e98a0

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2057.3.85%20Offline%20Installer.exe'
-    checksum        = '35104853d8e2e26073c56deeaae0671f3e2708bdefd89f3ded21029ab4b559eb'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2057.3.86%20Offline%20Installer.exe'
+    checksum        = '42a0ffb82e00afbb4fa945cb1d89738983377fb4ae765033a5072a01f601b416'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
