a740c68c0d7fa52cea1224081438faa563f785a0
Chocolatey <chocolatey@realdimensions.net>
2018-09-04 12:13:26 +0000

    AU: 1 updated - maxthon.commandline [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3a763dbda7fbb8fe788960d323558b0e85c87f50

--- automatic/maxthon.commandline/tools/chocolateyInstall.ps1
+++ automatic/maxthon.commandline/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  url          = 'http://dl.maxthon.com/mx5/maxthon_portable_5.2.4.2000.7z'
-  checksum     = 'ee28c76d2b31963026897192ba6ce9f7ed89e6722ecfa090187bb17690ba3b5c'
+  url          = 'http://dl.maxthon.com/mx5/maxthon_portable_5.2.4.3000.7z'
+  checksum     = '1feb4ff4d9dc392ef372ff551bcc44d0e1cf40d78accecf4cb591a7d01d0f28f'
   checksumType = 'sha256'
   destination  = $toolsDir
 }
