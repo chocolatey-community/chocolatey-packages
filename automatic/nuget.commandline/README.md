0add6b4e7cad0b4dc59dcc2becd36087400e008b
Pascal Berger <pascal.berger@gmail.com>
2018-09-09 18:19:49 +0200

    (nuget.commandline)  Update using tools.json feed (#1106)

--- automatic/nuget.commandline/README.md
+++ automatic/nuget.commandline/README.md
@@ -2,3 +2,6 @@
 
 NuGet is the package manager for the Microsoft development platforms including .NET. NuGet gives you access to thousands of packages from other developers on [nuget.org](http://nuget.org), and the NuGet tools let you create, share, and host packages of your own.
 
+## Notes
+
+This package contains **preview** and **released** and **recommended** versions.
--- automatic/nuget.commandline/update.ps1
+++ automatic/nuget.commandline/update.ps1
@@ -1,17 +1,9 @@
 import-module au
 
-$packageName = 'NuGet.CommandLine'
+$releases = "https://dist.nuget.org/tools.json"
 
 function global:au_SearchReplace {
    @{
-        # Those might be useful for some other nuget packages
-
-        # ".\tools\chocolateyInstall.ps1" = @{
-        #     "(?i)(^\s*[$]packageName\s*=\s*)('.*')"= "`$1'$packageName'"
-        # }
-        # ".\tools\chocolateyUninstall.ps1" = @{
-        #     "(?i)(^\s*[$]packageName\s*=\s*)('.*')"= "`$1'$packageName'"
-        # }
         ".\legal\VERIFICATION.txt" = @{
           "(?i)(\s*download the.*)<.*>" = "`$1<$($Latest.URL32)>"
           "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
@@ -22,19 +14,16 @@ function global:au_SearchReplace {
 
 function global:au_BeforeUpdate {
     Get-RemoteFiles -Purge -NoSuffix
-
-    set-alias 7z $Env:chocolateyInstall\tools\7z.exe
-    rm tools\*.exe
-    7z e tools\*.nupkg NuGet.exe -r -otools
-    rm tools\*.nupkg
 }
 
 function global:au_GetLatest {
-    $package = Find-Package $packageName -provider "nuget" -Source http://www.nuget.org/api/v2/ -AllowPrereleaseVersions -Force
-    $version = $package.Version
+    $json = Invoke-WebRequest -UseBasicParsing -Uri $releases | ConvertFrom-Json
+
+    $latestVersion = $json."nuget.exe" | Sort-Object uploaded -Descending | Select-Object -First 1
+
     @{
-        Version = $version
-        Url32   = "https://api.nuget.org/packages/$($packageName.ToLower()).${version}.nupkg"
+        Version = $latestVersion.version
+        Url32   = $latestVersion.url
     }
 }
 
