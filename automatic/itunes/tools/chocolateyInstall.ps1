7a1e7e6159f4ffa5003f697f60a898c6b89616ad
Chocolatey <chocolatey@realdimensions.net>
2018-09-13 00:12:18 +0000

    AU: 1 updated - iTunes [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e75fe44fd5f9383846ca2bfd10f6d967535096f5

--- automatic/itunes/tools/chocolateyInstall.ps1
+++ automatic/itunes/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$version = '12.7.4'
+$version = '12.9.0.167'
 
 $packageArgs = @{
   packageName    = 'iTunes'
   fileType       = 'msi'
-  url            = 'https://secure-appldnld.apple.com/itunes12/091-76320-20180329-6D5B23D2-32F7-11E8-993D-9ABAB071F5CF/iTunesSetup.exe'
-  url64bit       = 'https://secure-appldnld.apple.com/itunes12/091-76333-20180329-6D5B026C-32F7-11E8-A675-99BAB071F5CF/iTunes64Setup.exe'
+  url            = 'https://secure-appldnld.apple.com/itunes12/041-02279-20180912-24D8EE3A-AC7A-11E8-BE19-C36F1B1141A5/iTunesSetup.exe'
+  url64bit       = 'https://secure-appldnld.apple.com/itunes12/041-02280-20180912-24D8CF68-AC7A-11E8-8B51-C26F1B1141A5/iTunes64Setup.exe'
   softwareName   = 'iTunes'
-  checksum       = 'CDD726CF9EC8E4A08E2CED4CCCE0B0B78F269F90B3DE476241560B1933FAE78E'
+  checksum       = '8DA31513D276AB82F3F9FDFEB28CE2ED127A648BE02AE7AEEF7470044766F480'
   checksumType   = 'sha256'
-  checksum64     = 'F4B923ABBB515D87F1D7B363C8D66E108001E0B914BDAE90DFCF9453E874891A'
+  checksum64     = 'e366b102f8b57958cd14457dbf3762322da6d86a044fc8e6d2a66e6d605347be'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 2010, 1641)
