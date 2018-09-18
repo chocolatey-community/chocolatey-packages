08ebd999cadb282d0934df2747b601fe1e6d5459
Chocolatey <chocolatey@realdimensions.net>
2018-09-18 00:11:07 +0000

    AU: 4 updated - git-lfs git-lfs.install GoogleChrome youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/df33d2379f1bc865d34b409d4da467bd12559f2e

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '69.0.3497.92'
+$version = '69.0.3497.100'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '1a8c21d1e6630b2644af179f127651ef8f9562e87e7e1ed35ade93f6ccdf4b18'
-  checksum64             = 'ce4639c79cbbef18db9c6f1d2750ea6521a0e71f93b20a01d9c39383401842df'
+  checksum               = '32ee4e493cba7db8df6612a9e73b9cb16d14c4a62c0483eef04ed9a2b144bba4'
+  checksum64             = '59e40c85f780f5a9f1d834345de27309ec5edb0b010b29297be8d483a9711481'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
