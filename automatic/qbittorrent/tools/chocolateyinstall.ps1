87b049b33bcd95fd83894da6107ef429eacbc7f6
Chocolatey <chocolatey@realdimensions.net>
2018-09-19 00:13:17 +0000

    AU: 2 updated - qbittorrent SQLite [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e47e00bd69e1de2c121019266798393ec978a534

--- automatic/qbittorrent/tools/chocolateyinstall.ps1
+++ automatic/qbittorrent/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   packageName    = 'qbittorrent'
   fileType       = 'exe'
   softwareName   = 'qBittorrent*'
-  file           = "$toolsDir\qbittorrent_4.1.2_setup.exe"
-  file64         = "$toolsDir\qbittorrent_4.1.2_x64_setup.exe"
+  file           = "$toolsDir\qbittorrent_4.1.3_setup.exe"
+  file64         = "$toolsDir\qbittorrent_4.1.3_x64_setup.exe"
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
 }
