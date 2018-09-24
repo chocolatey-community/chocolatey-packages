38cb8cb912dc674e602eea22fad9a0fb32797880
regexaurus <regexaurus@users.noreply.github.com>
2018-09-24 15:20:24 -0400

    Make Skype installs and updates even quieter (#1115)

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   url            = 'https://endpoint920510.azureedge.net/s4l/s4l/download/win/Skype-8.30.0.50.exe'
   checksum       = '5b769d80f0916e7e82a422481062b9e558449cf3542983883e4622d5e49618f8'
   checksumType   = 'sha256'
-  silentArgs     = "/SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
 }
 
