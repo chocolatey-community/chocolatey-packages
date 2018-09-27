5e2c98c5dfb4952783e6f5870b6e821251af0b0f
Chocolatey <chocolatey@realdimensions.net>
2018-09-27 00:10:58 +0000

    AU: 2 updated - mkvtoolnix skype [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/191b4c71ddba5fca723b2983c83b8fd17cf5e005

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://endpoint920510.azureedge.net/s4l/s4l/download/win/Skype-8.30.0.50.exe'
-  checksum       = '5b769d80f0916e7e82a422481062b9e558449cf3542983883e4622d5e49618f8'
+  url            = 'https://endpoint920510.azureedge.net/s4l/s4l/download/win/Skype-8.31.0.92.exe'
+  checksum       = 'd884654df5c8438a19bed2b82eb9d483868e86679e9dd9f5d77ff3243e5a2313'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
