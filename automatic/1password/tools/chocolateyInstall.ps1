64823402005de96d2c7551e28004483a7cfbcb7e
Chocolatey <chocolatey@realdimensions.net>
2018-10-02 00:10:24 +0000

    AU: 2 updated - 1password kubernetes-cli [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7ad6b5638bfe122c964dbc74af4d0f911cb2e32b

--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.2.581.BETA.exe'
+  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.2.581.exe'
   softwareName   = '1Password*'
   checksum       = '7cfcf2bc5a7130db698c30f02e5d5caa1ec9280e3190a9eb386c542c93004383'
   checksumType   = 'sha256'
