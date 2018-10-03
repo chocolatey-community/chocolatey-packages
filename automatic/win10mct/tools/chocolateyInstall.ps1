3cea0d48fb9ad813160092cc3b1390f17e26add3
Chocolatey <chocolatey@realdimensions.net>
2018-10-03 00:17:19 +0000

    AU: 2 updated - Firefox win10mct [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8cfd88fac62e808abcc024e9c086cf68c45f3469

--- automatic/win10mct/tools/chocolateyInstall.ps1
+++ automatic/win10mct/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   PackageName  = $env:chocolateyinstall
   FileFullPath = "$toolsDir\$exeName"
-  Url          = 'https://software-download.microsoft.com/download/pr/MediaCreationTool1803.exe'
-  Checksum     = '8960B2C95DB3A09AE2D7ABF820EA45E7100959F20CCD7EDCCA9EC5028D684B28DE3AC6ECDAE834150D40B91C1C264AD29BC2288D388B528B970C0F7531ACF909'
+  Url          = 'https://software-download.microsoft.com/download/pr/MediaCreationTool1809.exe'
+  Checksum     = '24BC375FC204020AA031403BCB536B5CCE92AE601F958C56F317158340CFBA6679EF765B7FBF0ABDFD1D32578DB9CCAA4826D1B1AA76ECF42913E659CE67F8A1'
   ChecksumType = 'sha512'
 }
 Get-ChocolateyWebFile @packageArgs
