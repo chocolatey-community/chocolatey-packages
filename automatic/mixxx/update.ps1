685fa3a7cdd8c679798f2bca4ae778238d091df1
Miodrag Milić <miodrag.milic@gmail.com>
2018-10-06 19:47:39 +0200

    (mixxx) update++

--- automatic/mixxx/update.ps1
+++ automatic/mixxx/update.ps1
@@ -18,17 +18,14 @@ function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
-    $re    = '\.exe$'
-    $url   = $download_page.links | ? href -match $re | select -First 2
-    $url32 = $url[0].href
-    $url64 = $url[1].href
-
-    $versionArr = $url32 -split 'x\-|\-[\d]|\-rel|\-win' | select -Last 2 -Skip 1
-    $versionRe = '^[\d\.]+(\-rc[\d]*)?$'
-    if ($versionArr[0] -match $versionRe) { $version = $versionArr[0] }
-    elseif ($versionArr[1] -match $versionRe) { $version = $versionArr[1] }
-
-    @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    $re    = 'win..\.exe$'
+    $url   = $download_page.links | ? href -match $re | select -First 2 -Expand href
+    $version = $url[0] -split '/' | select -Last 1 -Skip 1
+    @{  
+        Version = $version.Replace('mixxx-', '')
+        URL32   = $url | ? { $_ -like '*win32*' } | select -First 1
+        URL64   = $url | ? { $_ -like '*win64*' } | select -First 1
+    }
 }
 
 update -ChecksumFor none
