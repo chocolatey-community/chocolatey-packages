f01f7c73dcc032a76ff1307507504856e1da61a9
AdmiringWorm <kim.nordmo@gmail.com>
2018-10-17 17:18:08 +0200

    (defraggler) Fixed an issue were google chrome is still being installed

--- automatic/defraggler/update.ps1
+++ automatic/defraggler/update.ps1
@@ -2,19 +2,24 @@ import-module au
 
 $releases = 'http://www.piriform.com/defraggler/download/standard'
 
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum -Url $Latest.URL32 -Algorithm $Latest.ChecksumType32
+}
+
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
+  }
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re  = '\.exe$'
+  $re = '\.exe$'
   $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 
   $download_page = Invoke-WebRequest https://www.piriform.com/defraggler/version-history -UseBasicParsing
@@ -22,6 +27,7 @@ function global:au_GetLatest {
   $download_page.Content -match "\<h6\>v((?:[\d]\.)[\d\.]+)"
   $version = $Matches[1]
 
-  @{ URL32 = $url; Version = $version }
+  @{ URL32 = $url; Version = $version ; ChecksumType32 = 'sha256' }
 }
-update
+
+update -ChecksumFor none
