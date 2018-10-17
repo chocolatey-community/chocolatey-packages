765e6c460f4c6af5c12f31e969d03698d41a4007
AdmiringWorm <kim.nordmo@gmail.com>
2018-10-17 16:43:36 +0200

    (gom-player) Update++

--- automatic/gom-player/update.ps1
+++ automatic/gom-player/update.ps1
@@ -1,12 +1,12 @@
 ﻿Import-Module AU
 
 $releases     = 'https://www.gomlab.com/gomplayer-media-player/'
-$versions     = 'https://www.gomlab.com/gomplayer-media-player/?type=update'
+$versions     = 'https://www.gomlab.com/ajax/update.gom?page=1&lang=eng&product=GOMPLAYER'
 $softwareName = 'GOM Player'
 
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1"   = @{
+    ".\tools\chocolateyInstall.ps1" = @{
       "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
       "(?i)^(\s*url\s*=\s*)'.*'"          = "`${1}'$($Latest.URL32)'"
       "(?i)^(\s*checksum\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum32)'"
@@ -21,13 +21,13 @@ function global:au_GetLatest {
   $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
 
   $verRe = '((?:\d+\.){2,3}\d+)'
-  $Matches = $null
   $version_page = Invoke-WebRequest -Uri $versions -UseBasicParsing
-  $version_page.Content -match $verRe | Out-Null
-  $version32 = $Matches[1]
+  if ($version_page.Content -match $verRe) {
+    $version32 = $Matches[1]
+  }
 
   @{
-    URL32 = $url32
+    URL32   = $url32
     Version = $version32
   }
 }
