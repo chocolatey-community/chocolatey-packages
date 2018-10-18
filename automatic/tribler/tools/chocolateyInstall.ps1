d8031f0f2f83553692b84b34c37b2525149c8581
AdmiringWorm <kim.nordmo@gmail.com>
2018-10-18 18:08:41 +0200

    (tribler) Embedded package
    ref #866

--- automatic/tribler/tools/chocolateyInstall.ps1
+++ automatic/tribler/tools/chocolateyInstall.ps1
@@ -1,21 +1,17 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName  = 'tribler'
-$url          = 'https://github.com/Tribler/tribler/releases/download/v7.0.2/Tribler_7.0.2_x64.exe'
-$checksum     = 'd25470c7c4ce45eeddcd40301464e17238dd7020c69d5b35af0f76e21b978601'
-$checksumType = 'sha256'
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  softwareName   = 'Tribler'
-
-  checksum       = $checksum
-  checksumType   = $checksumType
-  url            = $url
-
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
+  softwareName   = 'tribler*'
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
+
+ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
--- automatic/tribler/update.ps1
+++ automatic/tribler/update.ps1
@@ -1,31 +1,52 @@
-import-module au
+﻿Import-Module AU
 
 $releases = 'https://www.tribler.org/download.html'
+$softwareName = 'Tribler'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
   @{
-    ".\tribler.nuspec" = @{
-      "(<releaseNotes>).+" = "`$1$($Latest.ReleaseNotes)</releaseNotes>"
+    ".\legal\VERIFICATION.txt"        = @{
+      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-      "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-      "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'"         = "`${1}'$softwareName'"
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    ".\tools\chocolateyUninstall.ps1" = @{
+      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
     }
   }
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re    = 'Tribler.*\.exe$'
-  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+  $re = 'x86\.exe$'
+  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
 
-  $tag = $url -split '\/' | select -Last 1 -Skip 1
-  $version  = $tag.TrimStart('v')
-  $releaseNotesUrl = "https://github.com/Tribler/tribler/releases/tag/${tag}"
+  $re = 'x64\.exe$'
+  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href
 
-  return @{ URL32 = $url; Version = $version; releaseNotes = $releaseNotesUrl }
+  $verRe = '\/v?'
+  $version32 = $url32 -split "$verRe" | select -last 1 -skip 1
+  $version64 = $url64 -split "$verRe" | select -last 1 -skip 1
+  if ($version32 -ne $version64) {
+    throw "32bit version do not match the 64bit version"
+  }
+  @{
+    URL32        = $url32
+    URL64        = $url64
+    Version      = $version32
+    ReleaseNotes = "https://github.com/Tribler/tribler/releases/tag/v${version32}"
+  }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor none
