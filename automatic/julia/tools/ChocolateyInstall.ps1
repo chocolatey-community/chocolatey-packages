dd01b072a60a5954413760b8e99ed51bbbd3d98d
drelatgithub <drel.sohu.com@gmail.com>
2018-10-21 13:57:54 -0400

    Rewrote ChocolateyInstall.ps1

---
+++ automatic/julia/tools/ChocolateyInstall.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
+  file32        = "$toolsDir\to-be-replaced.exe"
+  file64        = "$toolsDir\to-be-replaced.exe"
+
+  softwareName  = 'Julia*'
+
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+# Lets remove the installer as there is no more need for it
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
