68d8634e2d1364baa05659b615b23d3d5786c5d6
Chocolatey <chocolatey@realdimensions.net>
2018-11-12 00:11:47 +0000

    AU: 2 updated - chromium pspp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f695e5ffaa0b1ac80ef65909bb22d4067508e5b6

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "72.0.3608.0-snapshots"
+$version = "70.0.3538.102"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
