f10137d9d7252d09405bcd6cbca6220350621cd4
Chocolatey <chocolatey@realdimensions.net>
2018-11-14 00:15:28 +0000

    AU: 3 updated - dropbox notepadplusplus notepadplusplus.commandline [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/954baf77238e5df40bcbd65b4b6f09e48da1f165

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2061.3.92%20Offline%20Installer.exe'
-    checksum        = '675b3d7fe59ed72eb1a5b6358576c77fc3166b6bfe4a0ca40c6346e0d512fa86'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2062.3.93%20Offline%20Installer.exe'
+    checksum        = '43d31bdb87bdafb308705f1dae4fccc679a9d6b328618b58001dea499c125c32'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
