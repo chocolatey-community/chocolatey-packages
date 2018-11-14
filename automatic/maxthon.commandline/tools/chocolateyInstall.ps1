c8fc1eb137b251a693119db388cca449a8f600f4
Chocolatey <chocolatey@realdimensions.net>
2018-11-14 06:11:09 +0000

    AU: 3 updated - chromium gimp maxthon.commandline [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7387b442eb9d1e690246cc46bcf627e24a362cfd

--- automatic/maxthon.commandline/tools/chocolateyInstall.ps1
+++ automatic/maxthon.commandline/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  url          = 'http://dl.maxthon.com/mx5/maxthon_portable_5.2.5.3000.7z'
-  checksum     = '71f65a8330106d3519ef42060bff8ce280440134c61002d7b823259d402de263'
+  url          = 'http://dl.maxthon.com/mx5/maxthon_portable_5.2.5.4000.7z'
+  checksum     = 'ed36a8136ca497b1d25072f185c652c73f6969877eb2fd123d8ad1c7e96a64c4'
   checksumType = 'sha256'
   destination  = $toolsDir
 }
