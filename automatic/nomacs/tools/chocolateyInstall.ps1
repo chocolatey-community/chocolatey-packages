0cd84a5d526dffc3f5170ce0ff29e6543437ef33
Chocolatey <chocolatey@realdimensions.net>
2018-11-20 18:13:32 +0000

    AU: 2 updated - nomacs poi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4416b60d3be74f3cb967092755cacf4170bf0df5

--- automatic/nomacs/tools/chocolateyInstall.ps1
+++ automatic/nomacs/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'nomacs'
   fileType       = 'msi'
-  file           = gi $toolsPath\*-x86.msi
-  file64         = gi $toolsPath\*-x64.msi
+  file           = Get-Item $toolsPath\*-x86.msi
+  file64         = Get-Item $toolsPath\*-x64.msi
   silentArgs     = '/quiet /norestart /log "{0}/setup.log"' -f (Get-PackageCacheLocation)
   validExitCodes = @(0)
   softwareName   = 'nomacs*'
 }
 Install-ChocolateyInstallPackage @packageArgs
-rm $toolsPath\*.msi -ea 0
+Remove-Item $toolsPath\*.msi -ea 0
