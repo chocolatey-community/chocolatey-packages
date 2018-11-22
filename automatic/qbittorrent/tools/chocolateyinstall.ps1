23698c02791d8131321aaccd35f54192604b2b2d
Chocolatey <chocolatey@realdimensions.net>
2018-11-22 18:10:41 +0000

    AU: 2 updated - qbittorrent youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e16cb5d111b60f76532cac28b4a004a7790f962f

--- automatic/qbittorrent/tools/chocolateyinstall.ps1
+++ automatic/qbittorrent/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   packageName    = 'qbittorrent'
   fileType       = 'exe'
   softwareName   = 'qBittorrent*'
-  file           = "$toolsDir\qbittorrent_4.1.3_setup.exe"
-  file64         = "$toolsDir\qbittorrent_4.1.3_x64_setup.exe"
+  file           = "$toolsDir\qbittorrent_4.1.4_setup.exe"
+  file64         = "$toolsDir\qbittorrent_4.1.4_x64_setup.exe"
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
 }
