8a39ddfd2510d05ea5b521f0306a6c796dd094d6
AdmiringWorm <kim.nordmo@gmail.com>
2018-11-26 06:31:53 +0100

    (nodejs.install) Fixed silent arguments for v11+

--- automatic/nodejs.install/update.ps1
+++ automatic/nodejs.install/update.ps1
@@ -3,22 +3,31 @@ param($IncludeStream, [switch] $Force)
 
 import-module au
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if the script is not sourced
+if ($MyInvocation.InvocationName -ne '.') {
+  # run the update only if the script is not sourced
   function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 }
 
 function global:au_SearchReplace {
+  $version = [version]$Latest.Version
+  $silentArgs = if ($version -lt [version]'11.0') {
+    ',NodeEtwSupport'
+  }
+  $silentArgs = "/quiet ADDLOCAL=ALL REMOVE=NodePerfCtrSupport${silentArgs}"
+
+
   @{
     ".\tools\chocolateyInstall.ps1" = @{
       "(^[$]filePath32\s*=\s*`"[$]toolsPath\\)(.*)`"" = "`$1$($Latest.FileName32)`""
       "(^[$]filePath64\s*=\s*`"[$]toolsPath\\)(.*)`"" = "`$1$($Latest.FileName64)`""
+      "(?i)(^\s*SilentArgs\s*=\s*)'.*'"               = "`${1}'$silentArgs'"
     }
-    ".\legal\verification.txt" = @{
-      "(?i)(32-Bit.+)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(64-Bit.+)\<.*\>" = "`${1}<$($Latest.URL64)>"
+    ".\legal\verification.txt"      = @{
+      "(?i)(32-Bit.+)\<.*\>"      = "`${1}<$($Latest.URL32)>"
+      "(?i)(64-Bit.+)\<.*\>"      = "`${1}<$($Latest.URL64)>"
       "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-      "(?i)(checksum32:\s+).*" = "`${1}$($Latest.Checksum32)"
-      "(?i)(checksum64:\s+).*" = "`${1}$($Latest.Checksum64)"
+      "(?i)(checksum32:\s+).*"    = "`${1}$($Latest.Checksum32)"
+      "(?i)(checksum64:\s+).*"    = "`${1}$($Latest.Checksum64)"
     }
   }
 }
@@ -45,7 +54,7 @@ function global:au_GetLatest {
 
     $url32 = $msis | ? { $_ -match 'x86' } | select -first 1
     $version = $url32 -split '\-v?' | select -last 1 -skip 1
-    $versionTwoPart = $version -replace '(^\d+\.\d+).*',"`$1"
+    $versionTwoPart = $version -replace '(^\d+\.\d+).*', "`$1"
     if ($streams.ContainsKey($versionTwoPart)) { return ; }
 
     $url64 = $msis | ? { $_ -match "\-x64" } | select -first 1
@@ -58,6 +67,7 @@ function global:au_GetLatest {
   return @{ Streams = $streams }
 }
 
-if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+if ($MyInvocation.InvocationName -ne '.') {
+  # run the update only if script is not sourced
   update -ChecksumFor none -IncludeStream $IncludeStream -Force:$Force
 }
