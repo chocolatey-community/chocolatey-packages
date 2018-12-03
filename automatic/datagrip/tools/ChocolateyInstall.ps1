5f5ce50fb1568928a54c32f775faa5ec09ddd4e2
Maurice Kevenaar - Info.nl <maurice.kevenaar@info.nl>
2018-12-03 14:08:07 +0100

    * Changed the way datagrip is updated (faster, and use of SHA256 from upstream) * Added silent installation file * Added workaround for installer bug https://youtrack.jetbrains.com/issue/IDEA-202935

--- automatic/datagrip/tools/ChocolateyInstall.ps1
+++ automatic/datagrip/tools/ChocolateyInstall.ps1
@@ -1,5 +1,11 @@
 ﻿$ErrorActionPreference  = 'Stop'
 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+# Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
+$programfiles = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
+New-Item -ItemType Directory -Force -Path $programfiles\JetBrains
+ 
 $arguments              = @{
     packageName         =  $env:ChocolateyPackageName
     softwareName        = 'JetBrains DataGrip*'
@@ -7,7 +13,7 @@ $arguments              = @{
     checksum            = 'e6beb91ac0d5c71a68a6ce79c13784c9f1514816b98e313cf6a759a30d3bef1d'
     fileType            = 'exe'
     checksumType        = 'sha256'
-    silentArgs          = '/S'
+    silentArgs          = "/S /CONFIG=$toolsDir\silent.config "
     validExitCodes      = @(0, 1641, 3010)
 }
 
