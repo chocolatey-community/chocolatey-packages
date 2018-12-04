690fbd245be3d6213c13f35fdcbc71c316814a66
AdmiringWorm <kim.nordmo@gmail.com>
2018-12-04 13:09:07 +0100

    (datagrip) Corrected environment variable name

--- automatic/datagrip/tools/ChocolateyInstall.ps1
+++ automatic/datagrip/tools/ChocolateyInstall.ps1
@@ -4,18 +4,18 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
 $programFiles = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
-$pp = Get-PackageParameters 
+$pp = Get-PackageParameters
 
-$installDir = "$programFiles\JetBrains\DataGrip $env:ChocolatePackageVersion"
-if ($pp.InstallDir) { 
-    $installDir = $pp.InstallDir 
+$installDir = "$programFiles\JetBrains\DataGrip $env:ChocolateyPackageVersion"
+if ($pp.InstallDir) {
+    $installDir = $pp.InstallDir
 }
 
 $silentArgs   = "/S /CONFIG=$toolsDir\silent.config "
-$silentArgs   += "/D=`"$installDir`"" 
+$silentArgs   += "/D=`"$installDir`""
 
 New-Item -ItemType Directory -Force -Path $installDir
- 
+
 $arguments              = @{
     packageName         = $env:ChocolateyPackageName
     softwareName        = 'JetBrains DataGrip*'
