79834575ba0b80e9047a74bcc8550fe1138461da
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-04 12:50:58 +0100

    * changed README.md to include optional parameter * Added some powershell magic to define the installation folder ** NOTE ** WIP

--- automatic/datagrip/tools/ChocolateyInstall.ps1
+++ automatic/datagrip/tools/ChocolateyInstall.ps1
@@ -3,17 +3,27 @@
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # Workaround for https://youtrack.jetbrains.com/issue/IDEA-202935
-$programfiles = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
-New-Item -ItemType Directory -Force -Path $programfiles\JetBrains
+$programFiles = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
+$pp = Get-PackageParameters 
+
+$installDir = "$programFiles\JetBrains\DataGrip $env:ChocolatePackageVersion"
+if ($pp.InstallDir) { 
+    $installDir = $pp.InstallDir 
+}
+
+$silentArgs   = "/S /CONFIG=$toolsDir\silent.config "
+$silentArgs   += "/D=`"$installDir`"" 
+
+New-Item -ItemType Directory -Force -Path $installDir
  
 $arguments              = @{
-    packageName         =  $env:ChocolateyPackageName
+    packageName         = $env:ChocolateyPackageName
     softwareName        = 'JetBrains DataGrip*'
     url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.3.exe'
     checksum            = 'e6beb91ac0d5c71a68a6ce79c13784c9f1514816b98e313cf6a759a30d3bef1d'
     fileType            = 'exe'
     checksumType        = 'sha256'
-    silentArgs          = "/S /CONFIG=$toolsDir\silent.config "
+    silentArgs          = $silentArgs
     validExitCodes      = @(0, 1641, 3010)
 }
 
