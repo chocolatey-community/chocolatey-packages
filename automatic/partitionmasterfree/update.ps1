d9d21fa773956a7b6d2c232d574d31cf4c592a6f
AdmiringWorm <kim.nordmo@gmail.com>
2018-12-04 14:30:53 +0100

    (partitionmasterfree) Changed to detect changed installers
    fixes #1148

--- automatic/partitionmasterfree/update.ps1
+++ automatic/partitionmasterfree/update.ps1
@@ -1,27 +1,40 @@
 ﻿import-module au
+import-module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
 
 $releases = 'http://www.partition-tool.com/easeus-partition-manager/history.htm'
 
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
   }
 }
 
-function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  if ($download_page.Content -match 'EaseUS Partition Master Free ([0-9\.]+)') {
-    $version = $matches[1]
-  }
-  $url = "http://download.easeus.com/free/epm.exe"
+function GetResultInformation([string]$url32) {
+  $url32 = Get-RedirectedUrl $url32
 
-  @{
-    Version = $version
-    URL32   = $url
+  $dest = "$env:TEMP\epm.exe"
+  $checksumType = 'sha256'
+  $version = Get-Version (Get-Item $dest | % { $_.VersionInfo.ProductVersion })
+  $checksum32 = Get-FileHash $dest -Algorithm $checksumType | % { $_.Hash.ToLowerInvariant() }
+
+  return @{
+    URL32          = $url32
+    Version        = $version
+    Checksum32     = $checksum32
+    ChecksumType32 = $checksumType
   }
 }
 
-update -ChecksumFor 32
+function global:au_GetLatest {
+  $exec = "http://download.easeus.com/free/epm.exe"
+  Update-OnETagChanged -execUrl $exec `
+    -OnEtagChanged {
+    GetResultInformation $exec
+  } -OnUpdated { @{ URL32 = $exec } }
+}
+
+update -ChecksumFor none
