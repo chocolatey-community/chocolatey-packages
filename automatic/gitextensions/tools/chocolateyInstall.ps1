9f5cd5b6838b5f3fc81eb3b57e0454f3121f0a90
Chocolatey <chocolatey@realdimensions.net>
2018-12-07 19:29:25 +0000

    AU: 1 updated - gitextensions [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/73707a2050329945258cbb4ff3492dd26496a584

--- automatic/gitextensions/tools/chocolateyInstall.ps1
+++ automatic/gitextensions/tools/chocolateyInstall.ps1
@@ -6,13 +6,13 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = 'Git Extensions*'
     fileType       = 'msi'
-    file           = "$toolsDir\GitExtensions-3.00.00.03-RC2.msi"
+    file           = "$toolsDir\GitExtensions-3.00.00.4433.msi"
     silentArgs     = '/quiet /norestart ADDDEFAULT=ALL REMOVE=AddToPath'
     validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" '' } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
 
 Install-BinFile gitex "$(Get-AppInstallLocation GitExtensions)\gitex.cmd"
