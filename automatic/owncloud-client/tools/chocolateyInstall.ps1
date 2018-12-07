5f12b527754667eab84044d1ebf2825277ba28d4
Maurice Kevenaar <maurice@kevenaar.name>
2018-12-07 15:18:52 +0100

    * Switched to MSI installation * Removed chocolateyUninstall.ps1 * Removed vcredist2013 dependency * Renamed update.ps1 to restore AU

--- automatic/owncloud-client/tools/chocolateyInstall.ps1
+++ automatic/owncloud-client/tools/chocolateyInstall.ps1
@@ -4,13 +4,13 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileType       = 'exe'
+  fileType       = 'msi'
   file           = "$toolsPath\ownCloud-2.4.3.10188-setup.exe"
   softwareName   = 'ownCloud'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
+  silentArgs     = '/qn /norestart'
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
 
-Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
--- automatic/owncloud-client/update.ps1
+++ automatic/owncloud-client/update.ps1
@@ -19,9 +19,6 @@ function global:au_SearchReplace {
       "(?i)^(\s*softwareName\s*=\s*)'.*'"       = "`${1}'$softwareName'"
       "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
     }
-    ".\tools\chocolateyUninstall.ps1" = @{
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
-    }
   }
 }
 
@@ -32,13 +29,13 @@ function global:au_AfterUpdate {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re = 'ownCloud\-.*\.exe$'
+  $re = 'ownCloud\-.*\.msi$'
   $urls32 = $download_page.Links | ? href -match $re | select -expand href
 
   $streams = @{}
   $urls32 | % {
-    $verRe = '[-]|\.exe$'
-    $version = $_ -split "$verRe" | select -last 1 -skip 2
+    $verRe = '[-]|\.msi$'
+    $version = $_ -split "$verRe" | select -last 1 -skip 1
     $version = Get-Version $version
     $kind = $_ -split '\/' | select -last 1 -skip 1
 
