06047ed73595d6c38ebc0e572d0b0765deb870a4
AdmiringWorm <kim.nordmo@gmail.com>
2018-12-11 18:01:59 +0100

    (ruby) Fixed typos in install script
    fixes #1164

--- automatic/ruby/tools/chocolateyInstall.ps1
+++ automatic/ruby/tools/chocolateyInstall.ps1
@@ -4,10 +4,10 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $rubyDir =  'ruby' + ($Env:ChocolateyPackageVersion -replace '\.').Substring(0,2)
 
 $pp = Get-PackageParameters
-$installDir = if ($pp.InstallDir) { $pp.InstallDir } else { Join-Path (Get-ToolsLocation) $rubyDir }
+$installDir = if ($pp['InstallDir']) { $pp['InstallDir'] } else { Join-Path (Get-ToolsLocation) $rubyDir }
 
 $tasks = 'assocfiles', 'noridkinstall'
-if ( !$pp.NoPath ) { $tasks += 'modpath'  }
+if ( !$pp['NoPath'] ) { $tasks += 'modpath'  }
 
 Write-Host "Ruby is going to be installed in '$installDir'"
 
@@ -21,4 +21,4 @@ $packageArgs = @{
   softwareName   = 'ruby *'
 }
 Install-ChocolateyInstallPackage @packageArgs
-Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' }}
+Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' }}
