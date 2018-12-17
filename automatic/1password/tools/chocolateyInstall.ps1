344c27f03ca0a04d8a489cf2a50a0f50480c6811
Chocolatey <chocolatey@realdimensions.net>
2018-12-17 18:10:23 +0000

    AU: 2 updated - 1password chromium [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/da7118b205075685b7a5e97e42f9c8738ef802cd

--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.3.602.BETA.exe'
+  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.3.619.BETA.exe'
   softwareName   = '1Password*'
-  checksum       = '37a8a93208497499ce3ff7ab4c890f56f249c9b5cc34c3bea5ebec258a40e11b'
+  checksum       = '5f8523f221e1d55e87ca8d393d42e21a22dd3710a943067e37634eb55f9d67ad'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
