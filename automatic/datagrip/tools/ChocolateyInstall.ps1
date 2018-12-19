b0324b5600ee02b55943153f92385b3334822e6d
Chocolatey <chocolatey@realdimensions.net>
2018-12-19 12:11:52 +0000

    AU: 1 updated - datagrip [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/37ed3ef82ccd6ad5255cf05e2d7af8c2ab9e8253

--- automatic/datagrip/tools/ChocolateyInstall.ps1
+++ automatic/datagrip/tools/ChocolateyInstall.ps1
@@ -19,8 +19,8 @@ New-Item -ItemType Directory -Force -Path $installDir
 $arguments              = @{
     packageName         = $env:ChocolateyPackageName
     softwareName        = 'JetBrains DataGrip*'
-    url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.3.exe'
-    checksum            = 'e6beb91ac0d5c71a68a6ce79c13784c9f1514816b98e313cf6a759a30d3bef1d'
+    url                 = 'https://download.jetbrains.com/datagrip/datagrip-2018.3.1.exe'
+    checksum            = '8b27c34e6ea65dc2e19692ddf3538dd08d151586b0a7ec2a6a3e0fc503b4abc8'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = $silentArgs
