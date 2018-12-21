66d40e2306bde5636c5d9ac60e60c4ccf978b6cf
Chocolatey <chocolatey@realdimensions.net>
2018-12-21 00:11:45 +0000

    AU: 2 updated - dropbox thunderbird [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2bdb6f309ae1b113ccafff78378b5e00b0721180

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2063.4.107%20Offline%20Installer.exe'
-    checksum        = 'aa21b1738690eaa6db724466668c247af97ddb15fb13a48e34185d06da35d2e3'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2064.3.123%20Offline%20Installer.exe'
+    checksum        = '00a3c30ac3124d278c1584958b801a918f682d592ff664ff59a5a54e0bebff54'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
--- automatic/thunderbird/tools/chocolateyInstall.ps1
+++ automatic/thunderbird/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'thunderbird'
 $softwareName = 'Mozilla Thunderbird'
 
-$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '60.3.3')
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '60.4.0')
 
 if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
   Write-Output $(
@@ -26,7 +26,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/60.3.3/win32/${locale}/Thunderbird%20Setup%2060.3.3.exe"
+    Url = "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/60.4.0/win32/${locale}/Thunderbird%20Setup%2060.4.0.exe"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
