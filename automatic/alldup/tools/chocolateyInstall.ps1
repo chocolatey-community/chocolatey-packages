e10ab7cdce43080a74173446534aa0f13bf2d7c7
Chocolatey <chocolatey@realdimensions.net>
2018-12-23 12:10:25 +0000

    AU: 1 updated - alldup [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/02515bcb2e0db15a6247e8d7fc34263f78f8897f

--- automatic/alldup/tools/chocolateyInstall.ps1
+++ automatic/alldup/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'http://www.alldup.info/download/AllDupSetup.exe'
   softwareName   = 'AllDup*'
-  checksum       = '957b66797289feb2022bd04c0099fd7142087a622a8f5216ec398d871406eb5c'
+  checksum       = '673d713239c2d5421d3d640b824eb93b01a12cfefe86bbe7cef5e2ba9cf49a84'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
