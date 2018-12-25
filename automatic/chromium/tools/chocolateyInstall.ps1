37e99aa8048f2ecfb30f82684c0b6d6af41d4ddb
Chocolatey <chocolatey@realdimensions.net>
2018-12-25 00:10:57 +0000

    AU: 2 updated - chromium qbittorrent [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/817eb9d0535c08faebe3b35dea278d19834e4cec

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "73.0.3650.0-snapshots"
+$version = "73.0.3651.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
