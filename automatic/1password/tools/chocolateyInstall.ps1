ac6083cbeff56cc323d355e84d3352c2f137956c
Chocolatey <chocolatey@realdimensions.net>
2019-01-02 00:16:43 +0000

    AU: 1 updated - 1password [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/91a32a29341fedabfc9b61c37a5134076faed98f

--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.3.646.BETA.exe'
+  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.3.648.BETA.exe'
   softwareName   = '1Password*'
-  checksum       = 'b77ee9f3183b8f0c7084e4bfc85a63d8c61d8a2e9c5db3fbcd03f91f792485d9'
+  checksum       = 'b1a6bdba99bd4ba58c949b92fa81ec923f56c2356c21db7238517b7d219eac2f'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
