a564fa6b2e4846a5da99cc7f3c67a743ef1d49c5
Chocolatey <chocolatey@realdimensions.net>
2019-01-03 18:13:41 +0000

    AU: 3 updated - simplewall simplewall.install simplewall.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/24b0babfe23e83c6bcd5190d8e3be65fafef71e4

--- automatic/simplewall.install/tools/chocolateyInstall.ps1
+++ automatic/simplewall.install/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
   softwareName = "simplewall*"
-  file         = "$toolsDir\simplewall-2.3.12-setup.exe"
+  file         = "$toolsDir\simplewall-2.3.13-setup.exe"
   fileType     = "exe"
   silentArgs   = "/S"
 }
