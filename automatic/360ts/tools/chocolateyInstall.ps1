7565c794f732c01151bf4dcd713d7ab347a3674a
Chocolatey <chocolatey@realdimensions.net>
2019-01-04 12:11:23 +0000

    AU: 2 updated - 360ts calibre [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/00663bb214f713ed55ce13ade7f9e2ec3ab60163

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName            = '360tse'
+  packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TSE_Setup_8.8.0.1105.exe'
-  checksum               = 'ae8536c057899fbd28d2bb98be857a07647d808b643d7cabbac656b8d74e97c2'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.2.0.1251.exe'
+  checksum               = 'c43768c1104962650f1788dee0586695fba237cb4e400f09b737fb1d0824d095'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
-  softwareName           = '360 Total Security Essential'
+  softwareName           = '360 Total Security'
 }
 Install-ChocolateyPackage @packageArgs
