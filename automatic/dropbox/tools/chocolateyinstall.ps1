8e02192e76916c28a97cbc1dbb31e8feac40c33c
Chocolatey <chocolatey@realdimensions.net>
2019-01-09 00:16:04 +0000

    AU: 1 updated - dropbox [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ceec44662aeccad9b43e8c2b606696a36df6fde2

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2064.3.123%20Offline%20Installer.exe'
-    checksum        = '00a3c30ac3124d278c1584958b801a918f682d592ff664ff59a5a54e0bebff54'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2064.4.141%20Offline%20Installer.exe'
+    checksum        = '6440a66f1ca700ab12c68c78209ddc6229640e67410afb02ff13ee06f2bc9aae'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
