a20c583a26460e3bfd1cf6096586597667665d10
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-16 15:00:11 +0100

    (julia) Changed to remove shim file before upgrading
    fixes #1175

---
+++ automatic/julia/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,4 @@
+﻿Write-Host "Removing julia shim before upgrading/uninstalling..."
+
+$binFilePath = Get-UninstallRegistryKey -SoftwareName "Julia*" | select -First 1 -ExpandProperty DisplayIcon
+Uninstall-BinFile "julia" -path $binFilePath
