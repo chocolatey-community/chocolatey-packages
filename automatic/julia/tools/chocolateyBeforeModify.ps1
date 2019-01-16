be4eb72fa9a4b1d80dddac19162d6941e87330d7
Chocolatey <chocolatey@realdimensions.net>
2019-01-16 14:48:10 +0000

    AU: 2 updated - julia mp3tag [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5e34e207c170d38a2dfddbab96ed4bd838218bb9

--- automatic/julia/tools/chocolateyBeforeModify.ps1
+++ automatic/julia/tools/chocolateyBeforeModify.ps1
@@ -1,4 +1,4 @@
 ﻿Write-Host "Removing julia shim before upgrading/uninstalling..."
 
-$binFilePath = Get-UninstallRegistryKey -SoftwareName "Julia*" | select -First 1 -ExpandProperty DisplayIcon
+$binFilePath = Get-UninstallRegistryKey -SoftwareName "Julia*" | Select-Object -First 1 -ExpandProperty DisplayIcon
 Uninstall-BinFile "julia" -path $binFilePath
