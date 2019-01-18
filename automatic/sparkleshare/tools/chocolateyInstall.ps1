ec180edee5efb6c2c3b928b27e0e8f5cf0024532
jack1142 <6032823+jack1142@users.noreply.github.com>
2019-01-18 16:13:14 +0100

    (sparkleshare) Embedded package

--- automatic/sparkleshare/tools/chocolateyInstall.ps1
+++ automatic/sparkleshare/tools/chocolateyInstall.ps1
@@ -1,17 +1,18 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName   = 'sparkleshare'
+  packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
-  url           = 'https://bitbucket.org/hbons/sparkleshare/downloads/sparkleshare-windows-1.5.msi'
+  file          = "$toolsPath\"
 
   softwareName  = 'SparkleShare'
-
-  checksum      = '7c82078c4ed912d4ac5a0d93c9a638f15b9da5332f34d5088ec27ae14ccd085c'
-  checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
+
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
