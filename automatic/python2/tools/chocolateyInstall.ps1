01eb83e216e8faae7649440b11663e4680e62c42
jack1142 <6032823+jack1142@users.noreply.github.com>
2019-01-18 14:42:16 +0100

    (python2) Embedded package

--- automatic/python2/tools/chocolateyInstall.ps1
+++ automatic/python2/tools/chocolateyInstall.ps1
@@ -8,16 +8,14 @@ if ($Env:ChocolateyPackageParameters -match '/InstallDir:\s*(.+)') {
 }
 $installArgs  = '/qn /norestart ALLUSERS=1 ADDLOCAL=ALL TargetDir="{0}"' -f $installDir
 
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
 $params = @{
-  packageName    = 'python2'
+  packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
   silentArgs     = $installArgs
-  url            = 'https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi'
-  url64Bit       = 'https://www.python.org/ftp/python/2.7.15/python-2.7.15.amd64.msi'
-  checksum       = '1afa1b10cf491c788baa340066a813d5ec6232561472cfc3af1664dbc6f29f77'
-  checksum64     = '5e85f3c4c209de98480acbf2ba2e71a907fd5567a838ad4b6748c76deb286ad7'
-  checksumType   = 'sha256'
-  checksumType64 = 'sha256'
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
 }
 
 Install-ChocolateyPackage @params
@@ -29,3 +27,5 @@ if (($Env:PYTHONHOME -ne $null) -and ($Env:PYTHONHOME -ne $InstallDir)) {
 
 Write-Host "Adding $installDir to PATH if needed"
 Install-ChocolateyPath $installDir 'Machine'
+
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
