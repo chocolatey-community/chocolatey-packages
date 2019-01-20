f2dc562f2eda1483f7c9d912f478b1b3a22596b8
Chocolatey <chocolatey@realdimensions.net>
2019-01-20 12:10:19 +0000

    AU: 1 updated - gitextensions [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3f166111287140aac956080dfb25fc6a9238b5c2

--- automatic/gitextensions/tools/chocolateyInstall.ps1
+++ automatic/gitextensions/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = 'Git Extensions*'
     fileType       = 'msi'
-    file           = "$toolsDir\GitExtensions-3.00.00.4433.msi"
+    file           = "$toolsDir\GitExtensions-3.0.1.4933.msi"
     silentArgs     = '/quiet /norestart ADDDEFAULT=ALL REMOVE=AddToPath'
     validExitCodes = @(0, 3010, 1641)
 }
