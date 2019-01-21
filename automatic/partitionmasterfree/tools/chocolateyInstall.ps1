197cfd94713d12a66736d75b29798fbf15475455
Kim J. Nordmo <AdmiringWorm@users.noreply.github.com>
2019-01-21 17:57:01 +0100

    (partitionmasterfree) Corrected softwareName

--- automatic/partitionmasterfree/tools/chocolateyInstall.ps1
+++ automatic/partitionmasterfree/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@
   checksumType   = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
-  softwareName   = 'EaseUS Partition Master (Free)'
+  softwareName   = 'EaseUS Partition Master*'
 }
 
 Write-Host "This package is not completely silent." -ForegroundColor "White"
