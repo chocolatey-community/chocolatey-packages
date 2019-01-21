1fc4c8cfcc785e3ad82eeac56fe1023057cf6bb5
AdmiringWorm <kim.nordmo@gmail.com>
2019-01-21 18:02:34 +0100

    (partitionmasterfree) Added uninstall script to remave Todo Backup Free
    ref #1149

--- automatic/partitionmasterfree/tools/chocolateyInstall.ps1
+++ automatic/partitionmasterfree/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-﻿$packageArgs = @{
+$packageArgs = @{
   packageName    = 'partitionmasterfree'
   fileType       = 'EXE'
   url            = 'http://download.easeus.com/free/epm.exe'
   checksum       = 'b7f82106978ee2f633796c3f6273d8b8a981d27c9419552292055329cb00cbfe'
   checksumType   = 'sha256'
-  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes = @(0)
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /RESTARTEXITCODE=3010 /SP-'
+  validExitCodes = @(0, 3010)
   softwareName   = 'EaseUS Partition Master*'
 }
 
