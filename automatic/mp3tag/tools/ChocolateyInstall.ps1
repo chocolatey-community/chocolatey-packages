ede494cb985b7b3075695af6072716f0ff2e4f2a
Chocolatey <chocolatey@realdimensions.net>
2019-01-25 18:11:50 +0000

    AU: 2 updated - filezilla mp3tag [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e78d87c799a21b57cf9491b85c073378770f6408

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,7 +24,7 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv292setup.exe"
+  file           = "$toolsPath\mp3tagv292asetup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
