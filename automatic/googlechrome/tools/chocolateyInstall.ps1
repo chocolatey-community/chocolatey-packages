fa153e671325083b6545065abfbee0d88f422efa
Chocolatey <chocolatey@realdimensions.net>
2019-01-30 00:21:56 +0000

    AU: 8 updated - GoogleChrome musescore nodejs nodejs.install picard thunderbird tor-browser youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c64633622f4721af6d24c38b2953fdc0d8721454

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '71.0.3578.80'
+$version = '72.0.3626.81'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '3acbe447610b402c4c2df597dba67cc63d2c5453a571c6dd7e2408131af87c0c'
-  checksum64             = 'a4e5e5562b1ef9bd6a3b28ff66f37ddefba2d5b60e6f49f8a9981703bef0587e'
+  checksum               = '3fe6bb5fe7a3aa0748e245b37569542cab56688cd7bd9a45fecdd3316fb33d85'
+  checksum64             = 'c87d6bd5b07affee3707c2f7cbf4592ab0090e7a643737c5e14dc69cf3c460f3'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
