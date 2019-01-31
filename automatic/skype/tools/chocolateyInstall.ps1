3af648dedf9f19f7b62bdf75bf0722be3059ba38
Chocolatey <chocolatey@realdimensions.net>
2019-01-31 00:14:01 +0000

    AU: 3 updated - nodejs nodejs.install skype [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/166e6554078f448f637a91ac75576ca4df878e9c

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.37.0.98.exe'
-  checksum       = '4a356127b083e8b7840bc37fa07057c4ec40f3f992f4a7b1825a58c10f925289'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.38.0.138.exe'
+  checksum       = '244696af41f9c3b6a33906a8bab7476cfa8e58f17e74672d87b68fb04e060b5a'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
