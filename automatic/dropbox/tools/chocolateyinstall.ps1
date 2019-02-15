d87f8111681601f2418fa468db17daed97887d94
Chocolatey <chocolatey@realdimensions.net>
2019-02-15 00:13:15 +0000

    AU: 5 updated - dropbox juju nodejs nodejs.install yumi-uefi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/85a8e21df46613d353a6f1dd5bff2e1cf3bd0159

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2067.3.78%20Offline%20Installer.exe'
-    checksum        = '3db388d62cf37ee4969cc9394f5be249be7eaafc286a17ebc5cad4e9447d9f9f'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2067.3.79%20Offline%20Installer.exe'
+    checksum        = '9e2922e8049a8b2a3d59dd6fc106b64570203b97ba4f8b80f4820e970b4ba3a8'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
