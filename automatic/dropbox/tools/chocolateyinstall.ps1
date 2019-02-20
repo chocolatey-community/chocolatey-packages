23ee9ac3aafcd6d7edf744e4b2b28314f7f59489
Chocolatey <chocolatey@realdimensions.net>
2019-02-20 06:13:20 +0000

    AU: 2 updated - dropbox universal-usb-installer [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3a47c60c33c8a4a09c27c8819897c47b47344bf2

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2067.3.79%20Offline%20Installer.exe'
-    checksum        = '9e2922e8049a8b2a3d59dd6fc106b64570203b97ba4f8b80f4820e970b4ba3a8'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2068.3.84%20Offline%20Installer.exe'
+    checksum        = '980dea7c742dae6e7dd8be007ae5ccf624d3f85b22f3b76b032b7fbb65f1b54c'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
