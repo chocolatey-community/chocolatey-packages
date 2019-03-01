cfb953344aeffe4ee6fbffbf6f5bb07141a2b174
Chocolatey <chocolatey@realdimensions.net>
2019-03-01 00:13:47 +0000

    AU: 6 updated - dropbox Firefox nuget.commandline poi smartftp tortoisegit [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ba76b0274a40881acafb9ad01dc4bd4c206304e3

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs        = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = "Dropbox*"
-    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2067.4.83%20Offline%20Installer.exe'
-    checksum        = '3720b92af661ba1ebd6cf3df4bbadf3aa5cf19f6471f166e87e50f3208e9c198'
+    url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2068.3.95%20Offline%20Installer.exe'
+    checksum        = '6f66b2eeed41a255e3ab3a3c2f3dbfa3f7813647de7e6d92e014eb0acf1ba161'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/s'
--- automatic/poi/tools/chocolateyinstall.ps1
+++ automatic/poi/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\poi-setup-10.2.3.exe"
+  file          = "$toolsDir\poi-setup-10.2.4.exe"
 
   softwareName  = 'poi*'
 
