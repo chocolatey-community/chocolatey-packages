ee90e3653f089a41dc4caf2137a489ca843c55e7
Chocolatey <chocolatey@realdimensions.net>
2019-03-07 06:11:55 +0000

    AU: 2 updated - minikube php [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b8a9e92a0f4b1653b7ada978a944369fc3737f9e

--- automatic/php/tools/chocolateyInstall.ps1
+++ automatic/php/tools/chocolateyInstall.ps1
@@ -14,10 +14,10 @@ if ($installLocation) {
 $pp = Get-PackageParameters
 
 $filesInfo = @{
-  filets32  = "$toolsPath\php-7.3.2-Win32-VC15-x86.zip"
-  filets64  = "$toolsPath\php-7.3.2-Win32-VC15-x64.zip"
-  filents32 = "$toolsPath\php-7.3.2-nts-Win32-VC15-x86.zip"
-  filents64 = "$toolsPath\php-7.3.2-nts-Win32-VC15-x64.zip"
+  filets32  = "$toolsPath\php-7.3.3-Win32-VC15-x86.zip"
+  filets64  = "$toolsPath\php-7.3.3-Win32-VC15-x64.zip"
+  filents32 = "$toolsPath\php-7.3.3-nts-Win32-VC15-x86.zip"
+  filents64 = "$toolsPath\php-7.3.3-nts-Win32-VC15-x64.zip"
 }
 
 if ($pp.ThreadSafe) {
