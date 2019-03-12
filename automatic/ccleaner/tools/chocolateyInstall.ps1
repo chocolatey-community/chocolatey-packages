30650039fc5a034282e4ffc7a99c81dd0c5a2c31
Chocolatey <chocolatey@realdimensions.net>
2019-03-12 18:12:03 +0000

    AU: 3 updated - ccleaner dropbox smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/aded2253eb8d0bb46116e36b91f35c99ca2e1c83

--- automatic/ccleaner/tools/chocolateyInstall.ps1
+++ automatic/ccleaner/tools/chocolateyInstall.ps1
@@ -22,8 +22,8 @@ if ($Env:ChocolateyPackageParameters -match '/UseSystemLocale') {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
-  url            = 'https://download.ccleaner.com/ccsetup554.exe'
-  checksum       = '128a61cad17d0d75e1422729bdf224069e8c5c9c3c1bba113f1e6f4051c72ce8'
+  url            = 'https://download.ccleaner.com/ccsetup555.exe'
+  checksum       = 'ea2b0fe19acc526f8c634fe933f63b7f2a1911a27a74dc2d87a5ea6ac4a8f2b3'
   checksumType   = 'sha256'
   silentArgs     = "/S $locale"
   validExitCodes = @(0)
