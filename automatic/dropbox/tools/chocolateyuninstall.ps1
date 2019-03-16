2409c8a4884e1e8e36343601c91961f65834f518
jack1142 <6032823+jack1142@users.noreply.github.com>
2019-03-16 02:09:21 +0100

    cleaning up the code

--- automatic/dropbox/tools/chocolateyuninstall.ps1
+++ automatic/dropbox/tools/chocolateyuninstall.ps1
@@ -1,10 +1,23 @@
-. (Join-Path (Split-Path $MyInvocation.MyCommand.Definition -Parent) 'helpers.ps1')
+$ErrorActionPreference = 'Stop'
 
-$packageName = 'dropbox'
-$fileType = 'exe'
-$silentArgs = '/S'
-$uninstallProps = getDropboxRegProps
+[array]$key = Get-UninstallRegistryKey -SoftwareName "Dropbox"
 
-if ($uninstallProps -and $uninstallProps.UninstallString) {
-    Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $uninstallerPath.UninstallString
+if ($key.Count -eq 1) {
+    $key | ForEach-Object {
+        $packageArgs    = @{
+            packageName = $env:ChocolateyPackageName
+            fileType    = 'exe'
+            silentArgs  = '/S'
+            file        = ($_.UninstallString -split '"')[1]
+        }
+
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+} elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+    Write-Warning "$($key.Count) matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
