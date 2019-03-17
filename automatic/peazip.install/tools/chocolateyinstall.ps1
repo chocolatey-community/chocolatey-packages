75a04cc6fcd1ce4846fb36925e940c39df3dbcf0
Chocolatey <chocolatey@realdimensions.net>
2019-03-17 12:10:37 +0000

    AU: 2 updated - peazip peazip.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/6784c498168d718e7b318de9fef80edec7ab8108

--- automatic/peazip.install/tools/chocolateyinstall.ps1
+++ automatic/peazip.install/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  file           = "$toolsDir\peazip-6.7.0.WINDOWS.exe"
-  file64         = "$toolsDir\peazip-6.7.0.WIN64.exe"
+  file           = "$toolsDir\peazip-6.7.1.WINDOWS.exe"
+  file64         = "$toolsDir\peazip-6.7.1.WIN64.exe"
   fileType       = 'exe'
   packageName    = 'peazip.install'
   softwareName   = 'PeaZip'
