2c0fc9318084a6f41ec91b1e486f0caa7fff0c66
Chocolatey <chocolatey@realdimensions.net>
2019-03-18 06:12:45 +0000

    AU: 1 updated - tixati [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b0992dce302fd4e3bb54b0a70287b1223397b723

--- automatic/tixati/tools/chocolateyInstall.ps1
+++ automatic/tixati/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'tixati'
-$fileName = 'tixati-2.58-1.install.exe'
+$fileName = 'tixati-2.59-1.install.exe'
 $download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion"
 
 $packageArgs = @{
   packageName    = $packageName
   fileFullPath   = "$download_dir\$fileName"
-  url            = 'https://download1.tixati.com/download/tixati-2.58-1.win32-install.exe'
-  url64bit       = 'https://download1.tixati.com/download/tixati-2.58-1.win64-install.exe'
-  checksum       = 'b3041f3bcec00ac9fdd14a15f1e46cacc8d4c44890ceea9bf7d271ecf5091526'
-  checksum64     = 'fa26583def1264dfa6bb48d4948adb19ce1fe623da5237943e804b5762f8172c'
+  url            = 'https://download1.tixati.com/download/tixati-2.59-1.win32-install.exe'
+  url64bit       = 'https://download1.tixati.com/download/tixati-2.59-1.win64-install.exe'
+  checksum       = '1c86b561864ae86f06c90e28f76a75d131976227dc489664d05f85d99d5e8942'
+  checksum64     = '88462a661f367b236f539e4f2bd5ee8293b3b5bbfc4b6fd64268b5df824807ca'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
 }
