e923d83a9a9083fe045afbf317d8f7c26bdd7e6f
Chocolatey <chocolatey@realdimensions.net>
2019-03-21 12:11:39 +0000

    AU: 3 updated - alldup Opera smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/00867b782e4d3a8fe20d187635ab0639ae53e1bc

--- automatic/alldup/tools/chocolateyInstall.ps1
+++ automatic/alldup/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'http://www.alldup.info/download/AllDupSetup.exe'
   softwareName   = 'AllDup*'
-  checksum       = '7fed1f2c234431b3118590d35e2cf77d039deecebcca86d6574ccba8bacd4041'
+  checksum       = '092fae274b9059744540a393e079aada44895f9c6dab94349ed3b8a533e84057'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar 0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/58.0.3135.107/win/Opera_58.0.3135.107_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/58.0.3135.107/win/Opera_58.0.3135.107_Setup_x64.exe'
-  checksum       = '481550e4745f18fd50901aefa8c66acacf1bd9d75fb08f55bd74f2d75378ce35'
-  checksum64     = '82f1d6a847fe3372dd903c6048dfedf40dc70bae5c4963f8f2791ce37d580997'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/58.0.3135.117/win/Opera_58.0.3135.117_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/58.0.3135.117/win/Opera_58.0.3135.117_Setup_x64.exe'
+  checksum       = '677df46e119e74bf5c1642d50ed30bb94b75fd3909967c1d19ee89153209294e'
+  checksum64     = '02a7e68071118f12135c09f4a6e94f240a930fa2caf00978e43dbad0662668cd'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera 0 /setdefaultbrowser 0' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '58.0.3135.107'
+$version = '58.0.3135.117'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
