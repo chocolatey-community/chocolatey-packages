400b4a6aa063a1c827444b9c3d5fb9b288d8a0c4
Chocolatey <chocolatey@realdimensions.net>
2019-04-02 18:11:04 +0000

    AU: 3 updated - chromium gnucash keeweb [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8507b7dbf77395b1a0b468736f9ccbf67d53771e

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "75.0.3749.0-snapshots"
+$version = "75.0.3755.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
