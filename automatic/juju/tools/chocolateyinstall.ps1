db11bc2cc4cfd9ef794fe366fb25024538f5cbe8
Chocolatey <chocolatey@realdimensions.net>
2019-04-02 06:10:40 +0000

    AU: 2 updated - juju smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ced06950261b7d0d4b74019299049b2149c78ec0

--- automatic/juju/tools/chocolateyinstall.ps1
+++ automatic/juju/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName   = 'juju'
   fileType      = 'exe'
   softwareName  = 'Juju'
-  file          = "$toolsDir\juju-setup-2.5.3-signed.exe"
+  file          = "$toolsDir\juju-setup-2.5.4-signed.exe"
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"${env:TEMP}\${env:chocolateyPackageName}.${env:chocolateyPackageVersion}.Install.log`""
   validExitCodes= @(0)
 }
