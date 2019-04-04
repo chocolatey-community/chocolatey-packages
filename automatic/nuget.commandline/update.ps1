1b7ddc5673973deded74d353bf7152899acf3996
Pascal Berger <pascal.berger@gmail.com>
2019-04-04 21:27:54 +0200

    (nuget.commandline) Add more streams for different 4.x versions

--- automatic/nuget.commandline/update.ps1
+++ automatic/nuget.commandline/update.ps1
@@ -21,12 +21,32 @@ function global:au_GetLatest {
 
   $versions = $json."nuget.exe" | Sort-Object uploaded -Descending
   $preRelease = $versions | ? stage -EQ 'EarlyAccessPreview' | select -First 1
+  $stableRelease46 = $versions | ? stage -EQ 'ReleasedAndBlessed' | ? version -like '4.6.*' | select -First 1
+  $stableRelease47 = $versions | ? stage -EQ 'ReleasedAndBlessed' | ? version -like '4.7.*' | select -First 1
+  $stableRelease48 = $versions | ? stage -EQ 'ReleasedAndBlessed' | ? version -like '4.8.*' | select -First 1
+  $stableRelease49 = $versions | ? stage -EQ 'ReleasedAndBlessed' | ? version -like '4.9.*' | select -First 1
   $stableRelease = $versions | ? stage -EQ 'ReleasedAndBlessed' | select -First 1
 
   $streams = @{
     'pre'    = @{
       Version = $preRelease.version
       URL32   = $preRelease.url
+    }
+    'stable46' = @{
+      Version = $stableRelease46.version
+      URL32   = $stableRelease46.url
+    }
+    'stable47' = @{
+      Version = $stableRelease47.version
+      URL32   = $stableRelease47.url
+    }
+    'stable48' = @{
+      Version = $stableRelease48.version
+      URL32   = $stableRelease48.url
+    }
+    'stable49' = @{
+      Version = $stableRelease49.version
+      URL32   = $stableRelease49.url
     }
     'stable' = @{
       Version = $stableRelease.version
