f8f6a753a5959082f88e54ce43066fd8d4d50842
Chocolatey <chocolatey@realdimensions.net>
2019-04-10 18:29:50 +0000

    AU: 2 updated - Firefox mp3tag [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/80d24aa204702e2bb2a4ca53fc97600ada974776

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,7 +24,7 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv294csetup.exe"
+  file           = "$toolsPath\mp3tagv294dsetup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
