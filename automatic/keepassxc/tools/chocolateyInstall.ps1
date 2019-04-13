3ea2f7aed741906326a87e1a2ab44b46448b6096
Chocolatey <chocolatey@realdimensions.net>
2019-04-13 00:29:26 +0000

    AU: 2 updated - chromium keepassxc [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1596f42c00730d888a26507cfe710a7d260490f9

--- automatic/keepassxc/tools/chocolateyInstall.ps1
+++ automatic/keepassxc/tools/chocolateyInstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName    = 'keepassxc'
   softwareName   = 'KeePassXC'
   fileType       = 'msi'
-  file           = "$toolsDir\KeePassXC-2.4.0-Win32.msi"
-  file64         = "$toolsDir\KeePassXC-2.4.0-Win64.msi"
-  checksum       = '38F49720EBB73BAEAB5DA89BA16C4BDCD84BC703939A06EF288F0739DF2E8D54'
+  file           = "$toolsDir\KeePassXC-2.4.1-Win32.msi"
+  file64         = "$toolsDir\KeePassXC-2.4.1-Win64.msi"
+  checksum       = '709705D1AD1776EA17F67A6D844470B453A4B7C9623945126D49A0E443C32221'
   checksumType   = 'sha256'
-  checksum64     = '6939A4B64217BDDFB349431EC71B1878CA65D3EBEFAD4BA396CB19045FFB83E7'
+  checksum64     = 'C5ABEA61312F4D8AE3A7DC642A46E22718F54E36F64696A190523D644E346146'
   checksumType64 = 'sha256'
 
   # MSI
