55359592aee2675045b42f1a89ff9d78d044db4b
Chocolatey <chocolatey@realdimensions.net>
2019-04-16 00:35:24 +0000

    AU: 2 updated - 1password 360ts [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/fed812a5d40a4c4f78f40ab507a1c54a839ba827

--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.3.661.BETA.exe'
+  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.3.684.BETA.exe'
   softwareName   = '1Password*'
-  checksum       = '6c3937054c912eba1e8ca26eaab03c5776fab9eee6da890cf2fe39f62bb818e5'
+  checksum       = '8d4896a083d60171ba7db3c20f02656f48f1d65b6052598faef025da79cce94e'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1038.exe'
-  checksum               = '183e2d5d1ea4f300970f0d6aca425fc42e5c1cbb673effcd648a0708c7a3ee24'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1059.exe'
+  checksum               = '6466119423348c73ae071b27662c92546c39d01ac9c4191b12a0e4b54e409287'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
