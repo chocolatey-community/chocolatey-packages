cb1711416064528cfa5096b5e58687f5d0e46f97
AdmiringWorm <kim.nordmo@gmail.com>
2019-04-25 11:52:29 +0200

    (mp3tag) Updated to parse markdown for version information
    fixes #1267

--- automatic/mp3tag/update.ps1
+++ automatic/mp3tag/update.ps1
@@ -5,54 +5,55 @@ $releases = 'https://community.mp3tag.de/t/mp3tag-development-build-status/455'
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
-   @{
+    @{
         ".\legal\VERIFICATION.txt"      = @{
-          "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-          "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
-          "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-          "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
+            "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
+            "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+            "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
         }
         ".\tools\ChocolateyInstall.ps1" = @{
-            "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+            "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
         }
     }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest "$releases.json" | ConvertFrom-Json
-    $content = $download_page.post_stream.posts[0].cooked
+    $download_page = Invoke-WebRequest "$releases.json" -UseBasicParsing | ConvertFrom-Json
 
-    $result = $content -match "<strong>\s*Version:\s*</strong>\s*([\d\.]+)([a-z])?"
+    # We get the id of the first post so we can get json with markdown for that post
+    $id = $download_page.post_stream.stream[0]
+    $download_page = Invoke-WebRequest "https://community.mp3tag.de/posts/$id.json" -UseBasicParsing | ConvertFrom-Json
+    $content = $download_page.raw
 
-    if (!$result) {
-      throw "mp3tag version not found on $releases"
+    if ($content -notmatch "Version:[\* ]*(\d+\.[\d\.]+)([a-z])?") {
+        throw "mp3tag version not found on $releases"
     }
 
     $version = $Matches[1]
     $versionSuffix = $Matches[2]
 
-    $result = $content -match "<strong>\s*Status:\s*</strong>\s*<strong>\s*([a-z]+)\s*</strong>"
-
-    if (!$result) {
-      Write-Host "mp3tag status not found on $releases"
-      return 'ignore'
+    if ($content -notmatch "Status:[\* ]*([a-z]+)") {
+        Write-Host "mp3tag status not found on $releases"
+        return 'ignore'
     }
 
     $status = $Matches[1]
-    $url32   = 'http://download.mp3tag.de/mp3tagv<version>setup.exe'
+    $url32 = 'http://download.mp3tag.de/mp3tagv<version>setup.exe'
     $flatVersion = $version -replace '\.', ''
-    $url32   = $url32 -replace '<version>', "$flatVersion$versionSuffix"
+    $url32 = $url32 -replace '<version>', "$flatVersion$versionSuffix"
 
     if ($versionSuffix) {
-      [char]$letter = $versionSuffix
-      [int]$num = $letter - ([char]'a' - 1)
-      $version += ".$num"
+        [char]$letter = $versionSuffix
+        [int]$num = $letter - ([char]'a' - 1)
+        $version += ".$num"
     }
     if ($status -eq 'Beta') {
-      $version += "-beta"
-    } elseif ($status -ne 'Stable') {
-      Write-Host "mp3tag status is not recognizable"
-      return 'ignore'
+        $version += "-beta"
+    }
+    elseif ($status -ne 'Stable') {
+        Write-Host "mp3tag status is not recognizable"
+        return 'ignore'
     }
 
     return @{ URL32 = $url32; Version = $version }
