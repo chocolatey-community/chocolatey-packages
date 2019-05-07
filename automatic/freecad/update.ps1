71efd48b99b07846169d11592c2a6876635f4c86
RB <RedBaron2@users.noreply.github.com>
2019-05-07 16:36:55 -0700

    Update update.ps1
    update for streams

--- automatic/freecad/update.ps1
+++ automatic/freecad/update.ps1
@@ -1,55 +1,40 @@
 import-module au
-
-$releases = 'https://www.freecadweb.org/wiki/Download'
+ . ".\update_helper.ps1"
+ 
+$PreUrl = 'https://github.com'
+$releases = "$PreUrl/FreeCAD/FreeCAD/releases"
+$regex = "(\d+\.\d+\.\d+)"
 $softwareName = 'FreeCAD*'
 
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(?i)(^\s*fileType\s*=\s*)('.*')"       = "`$1'$($Latest.fileType)'"
+      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
       "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+      "(?i)^(\s*softwareName\s*=\s*)'.*'"     = "`${1}'$softwareName'"
     }
     ".\freecad.nuspec" = @{
       "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
     }
     ".\tools\chocolateyUninstall.ps1" = @{
       "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+      "(?i)(^\s*fileType\s*=\s*)('.*')"       = "`$1'$($Latest.fileType)'"
     }
   }
 }
+
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
-
-  $re = 'x(86|32).*\.exe$'
-  $url32 = $download_page.Links | ? href -match $re | select -first 1 -expand href
-
-  $re = 'x64.*\.exe$'
-  $url64 = $download_page.links | ? href -match $re | select -first 1 -expand href
-
-  $verRe = 'CAD\-|\.[\dA-Z]+\-WIN'
-  [version]$version32 = $url32 -split "$verRe" | select -last 1 -skip 1
-  [version]$version64 = $url64 -split "$verRe" | select -last 1 -skip 1
-  if ($version32.ToString(2) -ne $version64.ToString(2)) {
-    throw "32bit version do not match the 64bit version"
+  $streams = [ordered] @{
+    stable = Get-FreeCad -Title "FreeCAD" -kind "stable"
+    dev = Get-FreeCad -Title "FreeCAD" -kind "dev"
   }
-
-  if ($version32 -gt $version64) {
-    $version = $version32
-  } else {
-    $version = $version64
-  }
-
-  @{
-    URL32 = $url32
-    URL64 = $url64
-    Version = $version
-    ReleaseNotes = "https://www.freecadweb.org/wiki/Release_notes_$($version.Major)$($version.Minor)"
-  }
+  return @{ Streams = $streams }
 }
 
 update
+
