65a80341b5f1f0be3a6f274611c97e9d766537cc
RB <RedBaron2@users.noreply.github.com>
2019-05-07 16:04:47 -0700

    Update to read the Javascript generated release page

--- automatic/sourcetree/update.ps1
+++ automatic/sourcetree/update.ps1
@@ -1,6 +1,5 @@
 import-module au
-
-$releases = "https://www.sourcetreeapp.com/enterprise"
+ . ".\update_helper.ps1"
 
 function global:au_SearchReplace {
     @{
@@ -16,32 +15,7 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    # https://downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_2.5.5.msi
-    $re32  = "SourcetreeEnterpriseSetup_(.*).msi"
-
-    # There are multiple download links on the page.
-    # We need to find the one with the highest version number in case not all of them are the same.
-    $msiLinks = @()
-    $download_page.links | Where-Object href -match $re32 | ForEach-Object {
-        $msiLinks += New-Object PSObject -Property @{
-            'href' = $_.href
-            'versionString' = [regex]::Matches($_.href, $re32).Groups[1].Value
-            'version' = [version]([regex]::Matches($_.href, $re32).Groups[1]).Value
-        }
-    }
-
-    $newestMsi = $msiLinks | Sort-Object version -descending | Select-Object -First 1
-
-    $url32 = $newestMsi.href
-    $version32 = $newestMsi.versionString
-    @{
-        Version      = $version32
-        URL32        = $url32
-        PackageName  = 'SourceTree'
-        ReleaseNotes = "https://www.sourcetreeapp.com/update/windows/ga/ReleaseNotes_$version32.html"
-    }
+  return Get-JavaSiteUpdates -Package "sourcetree" -Title "Sourcetree for Windows Enterprise"
 }
 
-update -NoCheckChocoVersion
+update
