e1519940fef5bc6e50e07e75705a2e1235b52907
RB <RedBaron2@users.noreply.github.com>
2019-05-07 16:40:22 -0700

    Update chocolateyInstall.ps1
    update to handle `pp` and `--pre` streams

--- automatic/freecad/tools/chocolateyInstall.ps1
+++ automatic/freecad/tools/chocolateyInstall.ps1
@@ -1,17 +1,46 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
+
+$pp = Get-PackageParameters
+# Checking for Package Parameters
+if (!$pp['UnzipLocation']) { $pp['UnzipLocation'] = "${env:ChocolateyPackageFolder}\tools\${env:ChocolateyPackageTitle}" }
+if (!$pp['WorkingDirectory']) { $pp['WorkingDirectory'] = $pp.UnzipLocation }
+if (!$pp['TargetPath']) { $pp['TargetPath'] = $pp['WorkingDirectory']+"\bin\${env:ChocolateyPackageTitle}.exe" }
+if (!$pp['IconLocation']) { $pp['IconLocation'] = $pp['TargetPath'] }
+if (!$pp['Arguments']) { $pp['Arguments'] = "" }
+if (!$pp['ShortcutFilePath']) { $pp['ShortcutFilePath'] = ( [Environment]::GetFolderPath('Desktop') ) }
+if (!$pp['Shortcut']) { $pp['Shortcut'] = $true }
+if (!$pp['WindowStyle']) { $pp['WindowStyle'] = 1 }
+
+$packageParams = @{
+  ShortcutFilePath = $pp.ShortcutFilePath
+  TargetPath = $pp.TargetPath
+  WorkingDirectory = $pp.WorkingDirectory
+  Arguments = $pp.Arguments
+  IconLocation = $pp.IconLocation
+  Description = "FreeCAD Development ${env:ChocolateyPackageVersion}"
+  WindowStyle = $pp.WindowStyle
+}
+if ($pp['Taskbar']) { $packageParams.Add('PinToTaskbar', '') }
+if ($pp['Admin']) {  $packageParams.Add('RunAsAdmin','') }
+
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileType       = 'exe'
-  url            = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.18.1/FreeCAD-0.18.16110.f7dccfa-WIN-x32-installer.exe'
-  url64          = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.18.1/FreeCAD-0.18.16110.f7dccfa-WIN-x64-installer.exe'
+  fileType       = ''
+  url            = ''
+  url64          = ''
+  UnzipLocation	 = $pp.UnzipLocation
   softwareName   = 'FreeCAD*'
-  checksum       = 'd5d30471d0c79b9588f1504e606e146f61512b17371577f6bb12b32bc6cac2a0'
+  checksum       = ''
   checksumType   = 'sha256'
-  checksum64     = '1a4e842ce39850a2efeec3ed16ca59e4c8a720c16999ce0724e22a20c5db5540'
+  checksum64     = ''
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
-
+if ( $packageArgs.filetype -eq '7z' ) {
+Install-ChocolateyZipPackage @packageArgs
+if ( $pp.Shortcut ) { Install-ChocolateyShortcut @packageParams }
+} else {
 Install-ChocolateyPackage @packageArgs
+}
