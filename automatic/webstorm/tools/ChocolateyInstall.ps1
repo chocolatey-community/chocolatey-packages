084e92dfbdce3caa4506217883ac3e441fd40d1e
Chocolatey <chocolatey@realdimensions.net>
2019-05-28 12:12:28 +0000

    AU: 1 updated - webstorm [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/612ad7481da9c05bdafa7a52093aa7e5d2f5fd89

--- automatic/webstorm/tools/ChocolateyInstall.ps1
+++ automatic/webstorm/tools/ChocolateyInstall.ps1
@@ -19,8 +19,8 @@ New-Item -ItemType Directory -Force -Path $installDir
 $arguments              = @{
     packageName         = $env:ChocolateyPackageName
     softwareName        = 'JetBrains WebStorm*'
-    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2019.1.2.exe'
-    checksum            = 'c9d60348d879673d6a652488bfdf4c77b68a520a854aee52441dafc40928fbc7'
+    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2019.1.3.exe'
+    checksum            = 'd6116f19880e141a16f7bf78b877de62dafd583ec8f983783f60eab9456d4645'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = $silentArgs
