3347129dc3e633f710ad47b8dd6014c86ae912ad
Chocolatey <chocolatey@realdimensions.net>
2019-06-01 18:11:08 +0000

    AU: 1 updated - maxima [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/90eb14ca573fe3f574b6173a982e04977824700b

--- automatic/maxima/tools/chocolateyInstall.ps1
+++ automatic/maxima/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.42.1-Windows/maxima-clisp-sbcl-5.42.1-win32.exe/download'
-  url64bit       = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.42.1-Windows/maxima-clisp-sbcl-5.42.1-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.43.0-Windows/maxima-clisp-sbcl-5.43.0-win32.exe/download'
+  url64bit       = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.43.0-Windows/maxima-clisp-sbcl-5.43.0-win64.exe/download'
   softwareName   = 'maxima*'
-  checksum       = '3C23BBEC850BCB998C09BC73A518871A32C88991C14E8F68FB95A4AE080A92C8'
+  checksum       = '15e4f8ed26b763647c3870bac0cb371869764ee30878fd1767505dd30d54012f'
   checksumType   = 'sha256'
-  checksum64     = 'D4F14E263C21CE6518CD04C5A6AC9C99335FCE8756E2FD2DB6B6CB15A8C00576'
+  checksum64     = '52cb2fe1218287a5381d71a14ac1466661748267f1c3bd959ff7c50df47f05fd'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
