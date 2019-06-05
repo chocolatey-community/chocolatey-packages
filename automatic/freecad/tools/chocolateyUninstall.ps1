71deeace9bc216d107d011c5b172431d2f4d90b1
RB <RedBaron2@users.noreply.github.com>
2019-06-04 17:52:56 -0700

    Update chocolateyUninstall.ps1
    spacing and code

--- automatic/freecad/tools/chocolateyUninstall.ps1
+++ automatic/freecad/tools/chocolateyUninstall.ps1
@@ -1,28 +1,35 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pp = ( Get-Content "$toolsDir\pp.json" ) | ConvertFrom-Json 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'FreeCAD*'
-  fileType      = 'exe'
+  fileType      = '7z'
   silentArgs    = '/S'
   validExitCodes= @(@(0))
 }
 
-$uninstalled = $false
+if ( $packageArgs.fileType -match 'exe' ) {
+	$uninstalled = $false
 
-[array]$key = Get-UninstallRegistryKey @packageArgs
+	[array]$key = Get-UninstallRegistryKey @packageArgs
 
-if ($key.Count -eq 1) {
-  $key | ForEach-Object {
-    $packageArgs['file'] = "$($_.UninstallString)"
+	if ($key.Count -eq 1) {
+	  $key | ForEach-Object {
+		$packageArgs['file'] = "$($_.UninstallString)"
 
-    Uninstall-ChocolateyPackage @packageArgs
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$($key.Count) matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert the package maintainer that the following keys were matched:"
-  $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
+		Uninstall-ChocolateyPackage @packageArgs
+	  }
+	} elseif ($key.Count -eq 0) {
+	  Write-Warning "$packageName has already been uninstalled by other means."
+	} elseif ($key.Count -gt 1) {
+	  Write-Warning "$($key.Count) matches found!"
+	  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+	  Write-Warning "Please alert the package maintainer that the following keys were matched:"
+	  $key | ForEach-Object { Write-Warning "- $($_.DisplayName)" }
+	}
+} else {
+	Remove-Item -Path $pp.ShortcutFilePath
 }
