efaa52e9ca2a0e4230e7db344eaafdffbfb2212a
Chocolatey <chocolatey@realdimensions.net>
2019-06-05 12:12:00 +0000

    AU: 1 updated - krita [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f7e7cb705095417d869a009e3b83209fd9ab572f

--- automatic/krita/tools/chocolateyInstall.ps1
+++ automatic/krita/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$is64     = (Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true'
+$is64     = (Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne 'true'
 
 $packageArgs = @{
     packageName   = 'krita'
@@ -10,7 +10,7 @@ $packageArgs = @{
     url           = 'https://download.kde.org/stable/krita/4.2.0/krita-x86-4.2.0-setup.exe'
     checksum      = 'FC2A4C1E7644CB764C07806215885F243436686F2A9E5A782968BF2F420DBB97'
     checksumType  = 'sha256'
-    file64        = gi $toolsDir\*.exe
+    file64        = Get-Item $toolsDir\*.exe
 }
 
 # https://github.com/chocolatey-community/chocolatey-coreteampackages/issues/1289#issuecomment-498815481
