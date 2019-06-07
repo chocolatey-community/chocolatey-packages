1d4abbbdafa7bdd27d108cc8f599f62a68a64802
Miodrag Milic <miodrag.milic@gmail.com>
2019-06-07 14:00:55 +0200

    (1password) update++ #1186

--- automatic/1password/Readme.md
+++ automatic/1password/Readme.md
@@ -1,9 +1,9 @@
 # [<img src="https://cdn.jsdelivr.net/gh/chocolatey/chocolatey-coreteampackages@09451a71b28e4ee0b3ea3841ab130b1bbf46f9b0/icons/1password.png" height="48" width="48" /> 1password](https://chocolatey.org/packages/1password)
 
-1Password – Too many passwords to remember?
-
 1Password can create strong, unique passwords for you, remember them, and restore them, all directly in your web browser.
 
 ## Notes
 
 - This package provides only the latest version of 1password. If there is a need for the 4.x version of 1password, please install the [1password4](https://chocolatey.org/packages/1password4) package instead.
+
+![screenshot](https://github.com/chocolatey-community/chocolatey-coreteampackages/blob/master/automatic/1password/screenshot.png?raw=true)
--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -7,8 +7,24 @@ $packageArgs = @{
   softwareName   = '1Password*'
   checksum       = '8d4896a083d60171ba7db3c20f02656f48f1d65b6052598faef025da79cce94e'
   checksumType   = 'sha256'
-  silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  silentArgs     = '--log_path .'
   validExitCodes = @(0)
 }
 
+# Installer blocks at the end and never returns. Successifull installation is visible in the log file
+Start-Job -ScriptBlock { param($cache_dir)
+  rm $cache_dir\*.log -Recurse -ea 0
+  $seconds = 0; $max_seconds = 600
+
+  while ($seconds -lt $max_seconds) {
+    $logFilePath = ls $cache_dir\*.log -Recurse | select -First 1    
+    if (!$logFilePath) { sleep 1;  $seconds++; continue }
+    
+    $log = Get-Content $logFilePath
+    if ($log -like '*Installation successful!') {
+      ps $env:ChocolateyPackageName -ea 0 | kill
+      exit
+    }
+  }
+} -ArgumentList (Get-PackageCacheLocation)
 Install-ChocolateyPackage @packageArgs
