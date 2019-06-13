a5dc2f5b050ce58f1dbc991ebefd59559fe74bf4
Chocolatey <chocolatey@realdimensions.net>
2019-06-13 00:11:48 +0000

    AU: 2 updated - sysinternals vscode [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/98d5365701862ddafce8f35c7c20d0b30441f0a5

--- automatic/sysinternals/tools/chocolateyInstall.ps1
+++ automatic/sysinternals/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ Write-Host "Sysinternals Suite is going to be installed in '$installDir'"
 $packageArgs = @{
   packageName    = 'sysinternals'
   url            = 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
-  checksum       = 'be878f45a0287b0ce604daaea9d16ae35102c5354fb4f977f6779d185e0436d8'
+  checksum       = '709ea4cc692d8d963870c753f7d63a50f1fd7abd2a238ed70673b17e97491756'
   checksumType   = 'sha256'
   unzipLocation  = $installDir
 }
@@ -18,7 +18,7 @@ Accept-Eula
 if ($installDir -ne $toolsPath) { Install-ChocolateyPath $installDir }
 if (Is-NanoServer) {
   $packageArgs.url = 'https://download.sysinternals.com/files/SysinternalsSuite-Nano.zip'
-  $packageArgs.checksum = 'a68ec0329e7163667f4d0149d779acd317bdbdf0467953b9fa5ea6533308c68a'
+  $packageArgs.checksum = '7c3c10cc00d05a51d30523caf23240cc1e32aa6f47655e8789a91a0a8a0775b0'
  }
 
 $old_path = 'c:\sysinternals'
--- automatic/vscode/tools/ChocolateyInstall.ps1
+++ automatic/vscode/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.35.0'
+$version = '1.35.1'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/553cfb2c2205db5f15f3ee8395bbd5cf066d357d/VSCodeSetup-ia32-1.35.0.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/553cfb2c2205db5f15f3ee8395bbd5cf066d357d/VSCodeSetup-x64-1.35.0.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/c7d83e57cd18f18026a8162d042843bda1bcf21f/VSCodeSetup-ia32-1.35.1.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/c7d83e57cd18f18026a8162d042843bda1bcf21f/VSCodeSetup-x64-1.35.1.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = '28e7995ee90845fcc1ba5c123fc6827062378c66cd4fe274b46c45ab93603c25'
+  checksum       = '19c667821509533ef4a845f3c065340b0cb9c7612e7b21ccc0f08dbb961be6fe'
   checksumType   = 'sha256'
-  checksum64     = '47418a4d9d0921b5a392f205ebc1e367b6a73730ae0e35da4b50f96e17214da9'
+  checksum64     = '85ae566a84aec2005b8d1a0b14fab706dc74df9ca664b388cbdf0e9b14506c96'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
