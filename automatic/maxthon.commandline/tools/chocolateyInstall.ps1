958ed2007fa25c5ac26b25e6f7c0e4c2ec470f2c
Chocolatey <chocolatey@realdimensions.net>
2019-06-17 12:12:24 +0000

    AU: 3 updated - chromium maxthon.commandline spotify [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/77961010aa7b5665ac77d3f0d3d041d00f705563

--- automatic/maxthon.commandline/tools/chocolateyInstall.ps1
+++ automatic/maxthon.commandline/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  url          = 'http://dl.maxthon.com/mx5/maxthon_portable_5.2.7.3000.7z'
-  checksum     = '4a7f77951c91e8277d9f44224a45b563318cf4f4dfbd1e554c85a35d1481f2a0'
+  url          = 'http://dl.maxthon.com/mx5/maxthon_portable_5.2.7.4000.7z'
+  checksum     = '92c6869a39d80a203e442b596e622cb883dc8f1a7dfa575a82d070775add7841'
   checksumType = 'sha256'
   destination  = $toolsDir
 }
