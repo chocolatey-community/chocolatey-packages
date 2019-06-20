550f1597066945314a6609fd0b9beceabfb7de6d
Chocolatey <chocolatey@realdimensions.net>
2019-06-20 06:11:29 +0000

    AU: 3 updated - gom-player googlechromebeta juju [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/566371942d77a2cddcc108ed4716483906fbe9ec

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '76.0.3809.25-beta'
+$version = '76.0.3809.36-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '9cc9d651d77d90f7d905f2ae77072c86d4db76b5e0463f0f03f3dc9e6b782b60'
-  checksum64             = '85d3b81a5577c25634b331280bc51b5fde41599cbd2cdf7180f1fb0d640ac6e0'
+  checksum               = '7555aa889a7a57bb025c64a34baad5371daf4347319ad6b82a4799f7f4c36fbb'
+  checksum64             = '7afb315b8ebd75d7b480971685e43b67beac7d1bd0d6ea48331eb4ebcfee4761'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
