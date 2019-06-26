4efcc9178d58d75a46b0cda9216bbf912d04f51b
Chocolatey <chocolatey@realdimensions.net>
2019-06-26 13:21:20 +0000

    AU: 2 updated - chromium iTunes [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/cb223a04828177042aeefb5aa12571c5679b0a2b

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "77.0.3835.0-snapshots"
+$version = "77.0.3836.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
