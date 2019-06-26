cc51cb8536245b6748b63fd2f39ea76dffeb7a0c
Chocolatey <chocolatey@realdimensions.net>
2019-06-26 06:11:43 +0000

    AU: 1 updated - vagrant [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/96008c4d6b6d377710743a8978c2c42a49dcc201

--- automatic/vagrant/tools/chocolateyinstall.ps1
+++ automatic/vagrant/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName    = 'vagrant'
   fileType       = 'msi'
-  url            = 'https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_i686.msi'
-  url64bit       = 'https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.msi'
-  checksum       = '808496107341e4da0526d6b6c365324508a4bf1868084aca1d93c74a8e9c7854'
-  checksum64     = '0c8cf856ed8c794db2cceef96e8cbd5f0096eeafe851b024fd0dd4b308291026'
+  url            = 'https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_i686.msi'
+  url64bit       = 'https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.msi'
+  checksum       = '7cb6b2c1bf4c74b1b95d662e4c6c7dd2b7fbbefa0661b12f94b61eca3d80ec7d'
+  checksum64     = '8716bec78764f122354d0274448bab9124629c57e226f021e65bf7041cd8c659'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart"
