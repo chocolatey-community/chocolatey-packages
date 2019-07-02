6d8c05935f895264399f551774e3912983b2cd17
Chocolatey <chocolatey@realdimensions.net>
2019-07-02 12:13:13 +0000

    AU: 3 updated - brave spotify wps-office-free [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/783a6c1d8b2f03aa3fb9df26d185c892e4177b13

--- automatic/wps-office-free/tools/chocolateyInstall.ps1
+++ automatic/wps-office-free/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '11.2.0.8641'
+$version = '11.2.0.8668'
 $packageName = 'wps-office-free'
 $regName = $packageName -replace('\-',' ')
 $regName = $regName -replace('free','*')
@@ -8,11 +8,11 @@ $registry = Get-UninstallRegistryKey -SoftwareName $regName
 $packageArgs = @{
   packageName    = 'wps-office-free'
   fileType       = 'exe'
-  url            = 'http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.8641/WPSOffice_11.2.0.8641_Free.exe'
+  url            = 'http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.8668/WPSOffice_11.2.0.8668_Free.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = $regName
-  checksum       = '2843f8704688f13226834e8a0302bf3cd4921fb15a28c37e41e02f4b22d7adbb'
+  checksum       = '93ad326889681340436e4bd96877241f53faadbcdd3a4c7dbcec1b6c4956e4ea'
   checksumType   = 'sha256'
 }
 
