a393ed143a2daa17f3d20d6e37923f02f0e1a913
Chocolatey <chocolatey@realdimensions.net>
2019-07-04 12:12:22 +0000

    AU: 2 updated - brave libreoffice-fresh [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f73585401c03fa29f996799a3429a304ec094a04

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName            = 'libreoffice'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/6.2.4/win/x86/LibreOffice_6.2.4_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/6.2.4/win/x86_64/LibreOffice_6.2.4_Win_x64.msi'
-  checksum               = '7821443630c07ada6b2e901167ebf4c970811ef4c7919e4f2c22946181695a7f'
-  checksum64             = '45d3dace90f0572a26bfd22c870cc25eb5665c52f0c148f36c07a17638a4c7d0'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/6.2.5/win/x86/LibreOffice_6.2.5_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/6.2.5/win/x86_64/LibreOffice_6.2.5_Win_x64.msi'
+  checksum               = '717fb9e17a3feb8af1662e668b919db86fab343303b78f88c7859003056ee010'
+  checksum64             = '9b01f6f382dbb31367e12cfb0ad4c684546f00edb20054eeac121e7e036a5389'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/passive /norestart /l*v "{0}\install.log"' -f "$Env:TEMP\chocolatey\$Env:ChocolateyPackageName\$Env:ChocolateyPackageVersion"
