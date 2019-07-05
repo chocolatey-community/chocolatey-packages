41e6b7091b414299985b3c3e0a11fee447005280
Chocolatey <chocolatey@realdimensions.net>
2019-07-05 18:11:50 +0000

    AU: 5 updated - 1password chromium musescore php python3 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b9e55e6c1d2d281bd3268acec1f95c04be10de63

--- automatic/1password/Readme.md
+++ automatic/1password/Readme.md
@@ -1,4 +1,4 @@
-# [<img src="https://cdn.jsdelivr.net/gh/chocolatey/chocolatey-coreteampackages@09451a71b28e4ee0b3ea3841ab130b1bbf46f9b0/icons/1password.png" height="48" width="48" /> 1password](https://chocolatey.org/packages/1password)
+# [<img src="https://cdn.jsdelivr.net/gh/chocolatey-community/chocolatey-coreteampackages@09451a71b28e4ee0b3ea3841ab130b1bbf46f9b0/icons/1password.png" height="48" width="48" /> 1password](https://chocolatey.org/packages/1password)
 
 1Password can create strong, unique passwords for you, remember them, and restore them, all directly in your web browser.
 
--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.3.684.BETA.exe'
+  url            = 'https://c.1password.com/dist/1P/win6/1PasswordSetup-7.3.700.BETA.exe'
   softwareName   = '1Password*'
-  checksum       = '8d4896a083d60171ba7db3c20f02656f48f1d65b6052598faef025da79cce94e'
+  checksum       = '11cce95bc4d9e03e517ec1a9315fdbe403b9b098244f4e1e33075f8a64514c94'
   checksumType   = 'sha256'
   silentArgs     = '--log_path .'
   validExitCodes = @(0)
@@ -13,18 +13,18 @@ $packageArgs = @{
 
 # Installer blocks at the end and never returns. Successifull installation is visible in the log file
 Start-Job -ScriptBlock { param($cache_dir)
-  rm $cache_dir\*.log -Recurse -ea 0
+  Remove-Item $cache_dir\*.log -Recurse -ea 0
   $seconds = 0; $max_seconds = 600
 
   while ($seconds -lt $max_seconds) {
-    sleep 1; $seconds++
+    Start-Sleep 1; $seconds++
     
-    $logFilePath = ls $cache_dir\*.log -Recurse | select -First 1    
+    $logFilePath = Get-ChildItem $cache_dir\*.log -Recurse | Select-Object -First 1    
     if (!$logFilePath) { continue }
     
     $log = Get-Content $logFilePath
     if ($log -like '*Installation successful!') {
-      ps $env:ChocolateyPackageName -ea 0 | kill
+      Get-Process $env:ChocolateyPackageName -ea 0 | Stop-Process
       exit
     }
   }
