0421afe53459a7bb4a143a0daaebaa5996623223
Chocolatey <chocolatey@realdimensions.net>
2019-07-05 00:11:49 +0000

    AU: 4 updated - palemoon python smartftp tightvnc [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2d5d72a7734440be591e85dbafebd7c42b5b026a

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'http://rm-eu.palemoon.org/release/palemoon-28.6.0.win32.installer.exe'
-  url64         = 'http://rm-eu.palemoon.org/release/palemoon-28.6.0.win64.installer.exe'
+  url           = 'http://rm-eu.palemoon.org/release/palemoon-28.6.0.1.win32.installer.exe'
+  url64         = 'http://rm-eu.palemoon.org/release/palemoon-28.6.0.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = 'b1987fe3f2c8ef1c79a9fab0227e3a2c6cbff4d3b2e19261b46356fcd67cf079'
+  checksum      = 'd2af3b33f5f5c89792afa07d261a284b6dff2ae4d9f9f85f3cf55bfd74d7206c'
   checksumType  = 'sha256'
-  checksum64    = 'e0117d8f353e1cf10043593b9e45abe458e7572b6417a5f8eb64789712957ac3'
+  checksum64    = '9526941ff1d437ac94a5fc89ff8d0924a249de44c9413004d9a283b45f5debc2'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/tightvnc/tools/chocolateyInstall.ps1
+++ automatic/tightvnc/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file           = "$toolsPath\"
-  file64         = "$toolsPath\"
+  file           = "$toolsPath\tightvnc-2.8.23-gpl-setup-32bit.msi"
+  file64         = "$toolsPath\tightvnc-2.8.23-gpl-setup-64bit.msi"
   softwareName   = 'tightvnc*'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 2010, 1641)
@@ -25,4 +25,4 @@ if ($serviceStartMode -ne $null) {
   }
 }
 
-ls $toolsPath\*.msi | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" } }
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
