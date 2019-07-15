f7af52a91bae37ca85ca2d7f098993b83dba9957
Chocolatey <chocolatey@realdimensions.net>
2019-07-15 18:14:00 +0000

    AU: 2 updated - sonarlint-vs2015 youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c9f1c9ae5f89aa1429e0cbbb823a8601fb474082

--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-4.10.0.9879-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.11.0.0-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
