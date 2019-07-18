6debeb7a02ba8f4089e5f9bb2c3f9a3dd414460b
Chocolatey <chocolatey@realdimensions.net>
2019-07-18 18:12:40 +0000

    AU: 2 updated - Firefox sonarlint-vs2015 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/83913e254f7154842f1c28e9930614af736c7043

--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-4.11.0.0-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.11.1.0-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
