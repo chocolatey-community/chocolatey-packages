79ddcdf484ea6b8c3708e5f22357ca967dc9fdb3
Chocolatey <chocolatey@realdimensions.net>
2019-08-08 12:12:23 +0000

    AU: 2 updated - chromium wps-office-free [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a9f36763600f6cd4901b690f9ad997734b2eece4

--- automatic/wps-office-free/tools/chocolateyInstall.ps1
+++ automatic/wps-office-free/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '11.2.0.8684'
+$version = '11.2.0.8893'
 $packageName = 'wps-office-free'
 $regName = $packageName -replace('\-',' ')
 $regName = $regName -replace('free','*')
@@ -8,11 +8,11 @@ $registry = Get-UninstallRegistryKey -SoftwareName $regName
 $packageArgs = @{
   packageName    = 'wps-office-free'
   fileType       = 'exe'
-  url            = 'http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.8684/WPSOffice_11.2.0.8684_Free.exe'
+  url            = 'http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.8893/WPSOffice_11.2.0.8893_Free.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = $regName
-  checksum       = 'e576666b78cf03d8be7b7b5fbf3ffb7cb207c16c7349faaad15efb45a4403369'
+  checksum       = '4d0e37aa22b04be8fe72469b3dcc1b519308a37d1bafda366ca4b0b580e92a26'
   checksumType   = 'sha256'
 }
 
