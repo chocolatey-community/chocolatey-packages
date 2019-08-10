dff56f5203ce947916524e72d64cd9d504d9c500
Chocolatey <chocolatey@realdimensions.net>
2019-08-10 12:11:03 +0000

    AU: 2 updated - brave nmap [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2123f33157a0c70751a32fb88fd1d0adff564661

--- automatic/nmap/tools/chocolateyInstall.ps1
+++ automatic/nmap/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'nmap'
   fileType       = 'exe'
-  file           = "$toolsDir\nmap-7.70-setup.exe"
+  file           = "$toolsDir\nmap-7.80-setup.exe"
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
 }
