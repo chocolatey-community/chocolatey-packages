e454b3b56b35f1c201d76bf1399bdebdcf7ed95d
Chocolatey <chocolatey@realdimensions.net>
2019-08-14 09:02:38 +0000

    AU: 2 updated - brave chromium [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/65f3f273d9b3e850f79c92895afa948dfc0a0779

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "78.0.3880.0-snapshots"
+$version = "78.0.3883.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
