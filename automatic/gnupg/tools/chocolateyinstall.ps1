d3f033f32e5e444a5d150577f7a8f8742fd654e8
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-08-16 15:30:00 +0200

    chore: Update gnupg to version 2.2.17

--- automatic/gnupg/tools/chocolateyinstall.ps1
+++ automatic/gnupg/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'gnupg-modern'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.gnupg.org/ftp/gcrypt/binary/gnupg-w32-2.2.12_20181214.exe'
-$checksum32 = 'b0ffe2c0153046d4ce9ca2ef2bb68cdfede19bec9db597bad6ae0c84049b59a0'
+$url        = 'https://www.gnupg.org/ftp/gcrypt/binary/gnupg-w32-2.2.17_20190709.exe'
+$checksum32 = 'e5c99867591c1f72190f5540040ba027233f8cf5acc9ed627dba89a2461b57e0'
 
 $packageArgs = @{
   packageName   = $packageName
