42971dcf0cab5875d2334085b6019ab6064dabb5
Chocolatey <chocolatey@realdimensions.net>
2019-08-23 00:11:48 +0000

    AU: 2 updated - dropbox smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/fde994bf8599d13009039e08c180aa44a565146a

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
  if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '80.3.111'
+$version = '80.3.116'
 
 if (!(IsVersionAlreadyInstalled $version)) {
     $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) {$false} else {$true}
@@ -11,8 +11,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
     $packageArgs        = @{
         packageName     = $env:ChocolateyPackageName
         softwareName    = "Dropbox"
-        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2080.3.111%20Offline%20Installer.exe'
-        checksum        = '564fdce3094813cc4e8ae533c7e837553a0a86b7e2b0d004aff1e6e600062787'
+        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2080.3.116%20Offline%20Installer.exe'
+        checksum        = '478ae35165f7faabb62361787a3f61465ecc1ff94aed98d820bee023b48d9382'
         fileType        = 'exe'
         checksumType    = 'sha256'
         silentArgs      = '/s'
