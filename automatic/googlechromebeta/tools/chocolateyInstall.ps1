c376ffa9acb142a7d0a8fffaea46fba32a9a8147
Chocolatey <chocolatey@realdimensions.net>
2019-08-30 00:11:12 +0000

    AU: 2 updated - googlechromebeta mono [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d8a3324c62dc4be3433436f3034d6240e7a15e42

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '77.0.3865.42-beta'
+$version = '77.0.3865.56-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = 'ea70cddae15c430cbce31420efd5d7125e841d5f91b931bd3345e6956b0a60f1'
-  checksum64             = 'd8a1e7667ec884d024eb75b954d97f574fec95643db291ea73f5c49cae8357da'
+  checksum               = '01091d3ae1fed8d08a31995a6f136cf4ea1507881cbfeab39abca5d7d63cd098'
+  checksum64             = 'e6c51b0418fae75b11f014bd116ecce101f3a9b832ff9985fc3b500d1763a2ee'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/mono/tools/chocolateyInstall.ps1
+++ automatic/mono/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://download.mono-project.com/archive/6.0.0/windows-installer/mono-6.0.0.319-gtksharp-2.12.45-win32-0.msi'
-  url64bit       = 'https://download.mono-project.com/archive/6.0.0/windows-installer/mono-6.0.0.319-x64-0.msi'
+  url            = 'https://download.mono-project.com/archive/6.0.0/windows-installer/mono-6.0.0.327-gtksharp-2.12.45-win32-0.msi'
+  url64bit       = 'https://download.mono-project.com/archive/6.0.0/windows-installer/mono-6.0.0.327-x64-0.msi'
   softwareName   = 'Mono for Windows*'
-  checksum       = '24839c8b1772576ec4d28b90993125b35f26eb61819587be8ae35b2478477104'
+  checksum       = '9d104203aff4bc93e6a9bf9dbe241b2a11fe5b4b33a5550d613b06f8384cbe9c'
   checksumType   = 'sha256'
-  checksum64     = '29c495ee2b52c65e79d3a38670dcc66f155f1d1b8fe1cd19a7b357921863aade'
+  checksum64     = '0e9dc8749fed842ecb8316713159242220b78586505a610a6cb972563eb90149'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
