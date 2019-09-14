212be0d4b1e578032ebad862209f5271cb8b3ce8
drelatgithub <drel.sohu.com@gmail.com>
2019-09-14 09:51:12 -0400

    (Julia) Fix chocolateyinstall.ps1 binary shimming (#1327)
    * fix: julia shimming at installing
    * fix: (julia) bug in update.ps1
    * fix: (julia) bug in installer
    * fix: (julia) use real version number when updating

--- automatic/julia/tools/chocolateyinstall.ps1
+++ automatic/julia/tools/chocolateyinstall.ps1
@@ -13,14 +13,20 @@ $packageArgs = @{
   silentArgs    = '/S'
   validExitCodes= @(0)
 }
+$packageVersion = "1.2.0"
 
 Install-ChocolateyInstallPackage @packageArgs
 
 # Lets remove the installer as there is no more need for it
 Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
 
-$installLocation = Get-AppInstallLocation $packageArgs.softwareName
-if (!$installLocation)  { Write-Warning "Can't find Julia install location"; return }
-Write-Host "Julia installed to '$installLocation'"
+# Find the executable of current installed version
+[array]$keysCurrentVersion = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName'] | Where-Object {
+  ($_.DisplayName -split "\s+" | select -last 1) -eq $packageVersion
+}
 
-Install-BinFile 'julia' $installLocation\bin\julia.exe
+if ($keysCurrentVersion.Count -eq 0)  { Write-Warning "Can't find Julia install location"; return }
+$executableLocation = $($keysCurrentVersion | select -First 1).DisplayIcon
+Write-Host "Julia installed to '$executableLocation'"
+
+Install-BinFile 'julia' $executableLocation
