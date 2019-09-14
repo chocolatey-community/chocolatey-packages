cad09d821b064fb7866a58f5fbe3a6e8bf4397fa
Miodrag Milić <miodrag.milic@gmail.com>
2019-09-14 16:06:02 +0200

    (git.install) make exe removal run asap
    closes #1302

--- automatic/git.install/tools/chocolateyInstall.ps1
+++ automatic/git.install/tools/chocolateyInstall.ps1
@@ -17,13 +17,12 @@ $packageArgs = @{
     SilentArgs     = "/VERYSILENT", "/SUPPRESSMSGBOXES", "/NORESTART", "/NOCANCEL", "/SP-", "/LOG", (Get-InstallComponents $pp)
 }
 Install-ChocolateyInstallPackage @packageArgs
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
 
 $packageName = $packageArgs.packageName
 $installLocation = Get-AppInstallLocation $packageArgs.SoftwareName
 if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
 Write-Host "$packageName installed to '$installLocation'"
-
-Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
 
 if ($pp.NoCredentialManager) {
     Write-Host "Git credential manager will be disabled."
