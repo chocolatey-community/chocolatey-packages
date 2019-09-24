93b39db93f7f1384cc2dcefe2b1455dc2101fea1
Chocolatey <chocolatey@realdimensions.net>
2019-09-24 00:12:12 +0000

    AU: 2 updated - bulk-crap-uninstaller dropbox [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/39b4f3fe24030fd89842d6323f30b4e6c4452c98

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
  if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '82.3.133'
+$version = '82.3.138'
 
 if (!(IsVersionAlreadyInstalled $version)) {
     $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) {$false} else {$true}
@@ -11,8 +11,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
     $packageArgs        = @{
         packageName     = $env:ChocolateyPackageName
         softwareName    = "Dropbox"
-        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2082.3.133%20Offline%20Installer.exe'
-        checksum        = '1b2f3eb7b1a8c1e07ef52a11575608002b6d8ffcc52c3697fc48eee49e85fb65'
+        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2082.3.138%20Offline%20Installer.exe'
+        checksum        = '55104595f790b018a9a63466c15ab66629d55cd0eca2e5ad522179bbff47a9fd'
         fileType        = 'exe'
         checksumType    = 'sha256'
         silentArgs      = '/s'
