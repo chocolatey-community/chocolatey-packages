d6e268dfbb2b63ef01f6d1411c47cc2faaf3b7bc
Chocolatey <chocolatey@realdimensions.net>
2019-09-24 06:12:47 +0000

    AU: 2 updated - nuget.commandline wesnoth [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8a556201090964bff3df0a6fb49a84d98884910e

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.15.0/wesnoth-1.15.0-win32.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.14/wesnoth-1.14.9/wesnoth-1.14.9-win32.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = '17b8c1a1cea8742bca17b734c15d81b454d95a8a9e8cc72979316eb4e5c5d957'
+  checksum       = '17fd07ac2a48c9b8abc56d12f503557f007b306eb77a447a1c22d9a3e66371f9'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
