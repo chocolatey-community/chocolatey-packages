f907701b242988feb31a62bfb664b217bc8585b1
drelatgithub <drel.sohu.com@gmail.com>
2019-09-27 12:51:49 -0400

    feat: (protoc) try to add protoc for auto update

---
+++ automatic/protoc/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  FileFullPath   = "$toolsDir\TO-BE-REPLACED"
+  FileFullPath64 = "$toolsDir\TO-BE-REPLACED"
+
+  Destination    = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+# Install-BinFile 'protoc' $executableLocation
