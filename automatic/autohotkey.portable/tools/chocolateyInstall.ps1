de739a7bb79b5b0fe2f9c5ec938a80176dd8e9ee
Chocolatey <chocolatey@realdimensions.net>
2019-09-28 18:10:58 +0000

    AU: 3 updated - autohotkey autohotkey.install autohotkey.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9124cb6ca6d5a58c96a78c1990861cf187802af3

--- automatic/autohotkey.portable/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$fileName  = 'AutoHotkey_2.0-a105-acb6f3cb.zip'
+$fileName  = 'AutoHotkey_1.1.31.00.zip'
 $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $zip_path = "$toolsPath\$fileName"
 Remove-Item $toolsPath\* -Recurse -Force -Exclude $fileName
