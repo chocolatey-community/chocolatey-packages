e0f60e484832ae604381b32942fedeee14ade584
Miodrag Milic <miodrag.milic@gmail.com>
2019-09-30 10:50:02 +0200

    (notepadplusplus) update++ Fixed URLs

--- automatic/notepadplusplus/update.ps1
+++ automatic/notepadplusplus/update.ps1
@@ -15,17 +15,16 @@ function global:au_GetLatest {
     $new = (( $release.links -match "v\d+\.\d+\.\d+" ) -split " " | select -First 10 | Select -Last 1 )
     $new = $new.Substring(0,$new.Length-1)
     $releases = "https://notepad-plus-plus.org/downloads/$new"
-    $root          = "https://notepad-plus-plus.org"
     $download_page = Invoke-WebRequest $releases -UseBasicParsing
     $url_i         = $download_page.Links | ? href -match '.exe$' | Select-Object -Last 2 | % href
     $url_p         = $download_page.Links | ? href -match '.7z$' | % href
 
     @{
         Version = Split-Path (Split-Path $url_i[0]) -Leaf
-        URL32_i = $root + ($url_i -notmatch 'x64')
-        URL64_i = $root + ($url_i -match 'x64')
-        URL32_p = $root + ($url_p -notmatch 'x64' -notmatch 'minimalist')
-        URL64_p = $root + ($url_p -match 'x64' -notmatch 'minimalist')
+        URL32_i = $url_i -notmatch 'x64'
+        URL64_i = $url_i -match 'x64'
+        URL32_p = $url_p -notmatch 'x64' -notmatch 'minimalist'
+        URL64_p = $url_p -match 'x64' -notmatch 'minimalist'
     }
 }
 
