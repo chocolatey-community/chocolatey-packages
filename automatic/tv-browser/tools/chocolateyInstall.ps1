1f4a4b505a58d2b465f2d250ae80887d2cfc98d9
Miodrag Milic <miodrag.milic@gmail.com>
2019-09-30 20:58:26 +0200

    (tv-browser) setup adoptopenjdk11jre dependency

--- automatic/tv-browser/tools/chocolateyInstall.ps1
+++ automatic/tv-browser/tools/chocolateyInstall.ps1
@@ -12,3 +12,13 @@ $packageArgs = @{
 }
 Install-ChocolateyInstallPackage @packageArgs
 Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
+
+$jreDir = gi $Env:ProgramFiles\AdoptOpenJDK\jdk-11*-jre | select -First 1
+if (!$jreDir) { Write-Warning "Couldn't find AdoptOpenJDK JRE folder"; return }
+
+$installDir = Get-AppInstallLocation $packageArgs.softwareName
+if (!$installDir) { Write-Warning "Unable to find install directory"; return }
+
+Write-Host "Symlinking AdoptOpenJDK JRE"
+#New-Item -Type SymbolicLink -Path $installDir -Value $jreDir
+cmd.exe /C mklink /D "$installDir\java" $jreDir
