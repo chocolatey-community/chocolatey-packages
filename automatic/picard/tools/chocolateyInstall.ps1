a8b9234e779fb15a6ea9b9e2c7b2be26cfdb3418
Miodrag Milic <miodrag.milic@gmail.com>
2019-10-04 10:04:32 +0200

    [AU picard] uninstall existing installation prior to install closes #1352

--- automatic/picard/tools/chocolateyInstall.ps1
+++ automatic/picard/tools/chocolateyInstall.ps1
@@ -1,18 +1,22 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $filePath  = "$toolsPath\picard_x32.exe"
 
 $packageArgs = @{
   packageName    = 'picard'
   fileType       = 'exe'
-  file           = $filePath
   softwareName   = 'MusicBrainz Picard'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
 
+[array]$key = Get-UninstallRegistryKey $packageArgs.softwareName
+if ($key.Count -eq 1) {
+    Write-Host "Previous installation detected, uninstalling"
+    $packageArgs.file = $key[0].UninstallString
+    Uninstall-ChocolateyPackage @packageArgs
+}
+$packageArgs.file = $filePath
 Install-ChocolateyInstallPackage @packageArgs
-
 Remove-Item -Force -ea 0 $filePath,"$filePath.ignore"
