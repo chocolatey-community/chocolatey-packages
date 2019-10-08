54915ec88b3d8e8bed28883e6ff7754756d32066
Chocolatey <chocolatey@realdimensions.net>
2019-10-08 11:58:08 +0000

    AU: 4 updated - brave Opera skype smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0fb88e99965a27d829f5a44462f5eb716a6ff52d

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut)     { " /pintotaskbar=0"; Write-Host "
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/63.0.3368.71/win/Opera_63.0.3368.71_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/63.0.3368.71/win/Opera_63.0.3368.71_Setup_x64.exe'
-  checksum       = '3dad747bb1003daaa85ab5ec71e82e6fdd0ed483ba14999d168e37afeddfb5cf'
-  checksum64     = '549c3d3e9c8feb48b4a8a9d47e45e3417e905ec085bc9c5f3c07106eb435e15f'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/64.0.3417.47/win/Opera_64.0.3417.47_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/64.0.3417.47/win/Opera_64.0.3417.47_Setup_x64.exe'
+  checksum       = 'ac0f96bb0b8c3163a6fb551e18bce124d70cc5c0a4044ff503f7158832153416'
+  checksum64     = '08d0c0a1057513c78c29c19bb9dc903ac1a2c57fa7a4b1700afc4c07bb753970'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '63.0.3368.71'
+$version = '64.0.3417.47'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 } else {
--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.52.0.138.exe'
-  checksum       = '3f65ee95598c59280aff8143a5a60df27962c30eaff3d508e26ad7a8b34675ef'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.53.0.85.exe'
+  checksum       = '25df7db0bcf5de01eaf51e32d3fc28933fbe232b21215ac375d6769f7a8f3559'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
