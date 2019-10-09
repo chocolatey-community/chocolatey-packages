6f3760a97dd1f96359f68956f193b78db5a04cec
William Gathoye <william@gathoye.be>
2019-10-09 15:27:44 -0700

    Try to fix the log location for msi installer

--- automatic/libreoffice-streams/update.ps1
+++ automatic/libreoffice-streams/update.ps1
@@ -30,7 +30,7 @@ function global:au_SearchReplace {
   if ($Latest.FileType -eq "exe") {
     $linesToPatch["(?i)(^\s*silentArgs\s*=\s*)('.*')"] = "`$1'/S'"
   } else {
-    $linesToPatch["(?i)(^\s*silentArgs\s*=\s*)('.*')"] = "`$1'/passive /norestart /l*v `"`$Env:TEMP\chocolatey\`$Env:ChocolateyPackageName\`$Env:ChocolateyPackageVersion\install.log`"'"
+    $linesToPatch["(?i)(^\s*silentArgs\s*=\s*)('.*')"] = "`$1'/qn /norestart /l*v `"$(`$env:TEMP)\$(`$packageName).$(`$env:chocolateyPackageVersion).MsiInstall.log`"'"
   }
   $filesToPatchHashTable[".\tools\chocolateyInstall.ps1"] = $linesToPatch
 
