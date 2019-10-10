ed10bc1b1dec184fa72b3c108709be7c50cfcb93
Chocolatey <chocolatey@realdimensions.net>
2019-10-10 18:13:40 +0000

    AU: 2 updated - GoogleChrome keeweb [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9db8761bae709121db67c91de43ee4a2ecb988c5

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '77.0.3865.90'
+$version = '77.0.3865.120'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '9d0403e362629f4b7e51081723e85cb9831cbb2b76f409d5d57d9b4014d87d12'
-  checksum64             = 'b714d0e46e7e029e2f0e174ceddf62043207d3f61584247199ae43c10a8b5d48'
+  checksum               = 'fa39c979b0df2349849f280a23c923c4252f2bd18ae4f826133ca58e3d829c4b'
+  checksum64             = '774e3da4d2f5150a511158fb43a04123ee603bc6955b7c8353fd64fd697682ce'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
