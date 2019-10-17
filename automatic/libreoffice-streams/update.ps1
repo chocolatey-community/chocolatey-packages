381876f9f04bb9fe9e7c39d9be3c22fc42122c45
William Gathoye <william@gathoye.be>
2019-10-17 09:41:52 -0700

    Ensure patching is replacing not appending - attempt 3

--- automatic/libreoffice-streams/update.ps1
+++ automatic/libreoffice-streams/update.ps1
@@ -31,7 +31,7 @@ function global:au_SearchReplace {
   if ($Latest.FileType -eq "exe") {
     $linesToPatch["(?i)(^\s*silentArgs\s*=\s*)('.*')"] = "`$1'/S'"
   } else {
-    $linesToPatch["(?i)(^\s*silentArgs\s*=\s*)('.*')"] = "`$1'/qn /passive /norestart /l*v `"{0}\install.log`"' -f `"`$Env:TEMP\chocolatey\`$Env:ChocolateyPackageName\`$Env:ChocolateyPackageVersion`""
+    $linesToPatch["(?i)(^\s*silentArgs\s*=\s*)('.*')"] = "`$1'/qn /passive /norestart /l*v `"`$(`$env:TEMP)\`$(`$packageName).`$(`$env:chocolateyPackageVersion).MsiInstall.log`"'"
   }
   $filesToPatchHashTable[".\tools\chocolateyInstall.ps1"] = $linesToPatch
 
