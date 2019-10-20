062f09f0a6053a536c125f9b71d34343ce3aaba1
William Gathoye <william@gathoye.be>
2019-10-20 15:33:02 -0700

    Switch Mattermost Desktop to stable URL and msi installer

--- automatic/mattermost-desktop/tools/chocolateyInstall.ps1
+++ automatic/mattermost-desktop/tools/chocolateyInstall.ps1
@@ -3,17 +3,20 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName    = 'mattermost-desktop'
-  fileType       = 'exe'
-  file           = "$toolsDir\mattermost-setup-4.2.3-win32.exe"
-  file64         = "$toolsDir\mattermost-setup-4.2.3-win64.exe"
-  softwareName   = 'Mattermost*'
-  # This is still Squirrel being used. Lets use --silent.
-  silentArgs     = '/S --silent'
-  validExitCodes = @(0)
+  packageName            = 'mattermost-desktop'
+  fileType               = 'msi'
+  file                   = "$toolsDir\"
+  file64                 = "$toolsDir\"
+  checksum               = ''
+  checksum64             = ''
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes         = @(0, 3010, 1641)
+  softwareName           = 'Mattermost*'
 }
 
 Install-ChocolateyInstallPackage @packageArgs
 
 # Lets remove the installer and ignore files as there is no more need for them
-Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
+Get-ChildItem $toolsDir\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
