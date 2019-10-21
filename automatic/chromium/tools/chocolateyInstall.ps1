ba64d78bf411513fe49bb8284a1afcd19dbe8038
Chocolatey <chocolatey@realdimensions.net>
2019-10-21 12:12:28 +0000

    AU: 10 updated - brave calibre chromium cmake.install cmake.portable mattermost-desktop nodejs nodejs.install prey smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2589eaf09bc7e74e14a30bc046fcab409e56925b

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "79.0.3945.0-snapshots"
+$version = "80.0.3947.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
