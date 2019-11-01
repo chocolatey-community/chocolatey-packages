c653078364a34ef9b334ac08e1ba9ead077645d6
ImportTaste <53661808+ImportTaste@users.noreply.github.com>
2019-11-01 07:33:29 -0500

    vcredist140 14.24.28127.4

--- automatic/vcredist140/tools/data.ps1
+++ automatic/vcredist140/tools/data.ps1
@@ -1,12 +1,12 @@
 ﻿$installData32 = @{
-  Url = 'https://download.visualstudio.microsoft.com/download/pr/9565895b-35a6-434b-a881-11a6f4beec76/4A8157B2FF422C259DDAA2D0E568C0C0AFAB940E1F6E0E482EF83E90DDBAD2D6/VC_redist.x86.exe'
-  Checksum = '4a8157b2ff422c259ddaa2d0e568c0c0afab940e1f6e0e482ef83e90ddbad2d6'
+  Url = 'https://download.visualstudio.microsoft.com/download/pr/348da5f2-c5d4-4fbf-8360-d1b907780672/E59AE3E886BD4571A811FE31A47959AE5C40D87C583F786816C60440252CD7EC/VC_redist.x86.exe'
+  Checksum = 'e59ae3e886bd4571a811fe31a47959ae5c40d87c583f786816c60440252cd7ec'
   ChecksumType = 'sha256'
 }
 
 $installData64 = @{
-  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/9565895b-35a6-434b-a881-11a6f4beec76/EE84FED2552E018E854D4CD2496DF4DD516F30733A27901167B8A9882119E57C/VC_redist.x64.exe'
-  Checksum64 = 'ee84fed2552e018e854d4cd2496df4dd516f30733a27901167b8a9882119e57c'
+  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/3b070396-b7fb-4eee-aa8b-102a23c3e4f4/40EA2955391C9EAE3E35619C4C24B5AAF3D17AEAA6D09424EE9672AA9372AEED/VC_redist.x64.exe'
+  Checksum64 = '40ea2955391c9eae3e35619c4c24b5aaf3d17aeaa6d09424ee9672aa9372aeed'
   ChecksumType64 = 'sha256'
 }
 
@@ -15,7 +15,7 @@ $uninstallData = @{
 }
 
 $otherData = @{
-  ThreePartVersion = [version]'14.23.27820'
+  ThreePartVersion = [version]'14.24.28127.4'
   FamilyRegistryKey = '14.0'
   PackageName = 'vcredist140'
 }
