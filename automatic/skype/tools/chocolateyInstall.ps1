398054e204e5f2889c3a8e381ddd8a06480e0fe7
Chocolatey <chocolatey@realdimensions.net>
2019-11-04 18:13:11 +0000

    AU: 3 updated - dropbox juju skype [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/af691841df42e441fccd7ce12373e9dc21f44626

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.53.0.85.exe'
-  checksum       = '25df7db0bcf5de01eaf51e32d3fc28933fbe232b21215ac375d6769f7a8f3559'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.54.0.85.exe'
+  checksum       = '2d0190434bbe58b960a49804ec2da4df86b3f38526c6d2fa7fe638dbb17e4ff5'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
