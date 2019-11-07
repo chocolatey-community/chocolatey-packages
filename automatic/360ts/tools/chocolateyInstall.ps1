71fb33f47f9c78381c742380324fd563446a96b4
Chocolatey <chocolatey@realdimensions.net>
2019-11-07 18:12:58 +0000

    AU: 3 updated - 360ts juju thunderbird [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4429b328415e2521e28fb93c5a31f4e190a6c57e

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1223.exe'
-  checksum               = '1b487c0bfe8aae70fab877d5fb71a3b679a84c9683827e352dbe795a334270c4'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1238.exe'
+  checksum               = 'eb87841848c4084a84bd87d9da637ac4be454ed255123636209a046621497da5'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
