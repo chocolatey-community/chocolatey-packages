498673420b9c410ba97349afecc1ed985c1f8f71
Chocolatey <chocolatey@realdimensions.net>
2019-11-15 06:12:11 +0000

    AU: 1 updated - dropbox [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3b9ed448351584afea62c2a8c00e5ebbcf6dd55a

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
  if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '86.3.127'
+$version = '85.4.155'
 
 if (!(IsVersionAlreadyInstalled $version)) {
     $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) {$false} else {$true}
@@ -11,8 +11,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
     $packageArgs        = @{
         packageName     = $env:ChocolateyPackageName
         softwareName    = "Dropbox"
-        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2086.3.127%20Offline%20Installer.exe'
-        checksum        = '0c133a99b9c5c979a3415cf776e5b9acad9f8f01751ce51e8ea5aad8cc411d80'
+        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2085.4.155%20Offline%20Installer.exe'
+        checksum        = 'f8501b330dd9fb21287d09716a0f2f18312cd90d95c79ace4b9e365cb82a9373'
         fileType        = 'exe'
         checksumType    = 'sha256'
         silentArgs      = '/s'
