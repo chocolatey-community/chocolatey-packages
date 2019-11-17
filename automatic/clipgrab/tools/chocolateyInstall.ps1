55565ec622b6c24799f90528465ecbee11ab315a
Chocolatey <chocolatey@realdimensions.net>
2019-11-17 06:14:43 +0000

    AU: 2 updated - autohotkey.portable clipgrab [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3154b2be44f1f34bd815477847bc6f4190966edf

--- automatic/clipgrab/tools/chocolateyInstall.ps1
+++ automatic/clipgrab/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\clipgrab-3.8.5-portable.exe"
+  file           = "$toolsPath\clipgrab-3.8.6-portable.exe"
   softwareName   = 'ClipGrab*'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
