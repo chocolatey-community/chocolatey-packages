2696b2570621568f615f1108b6aa4f629268f2c1
Chocolatey <chocolatey@realdimensions.net>
2019-11-20 18:15:41 +0000

    AU: 7 updated - chromium freevideoeditor juju Opera seafile-client sonarlint-vs2015 wps-office-free [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/182e478dc3cda002d1fb2d99057e7eb3ecba6dd4

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/65.0.3467.42/win/Opera_65.0.3467.42_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/65.0.3467.42/win/Opera_65.0.3467.42_Setup_x64.exe'
-  checksum       = '6e67d3a6e3012160e76decbc88357f29fac369d2c53d141ce02b6b4a108fc0dc'
-  checksum64     = '05e19ec627d7f3ec7c4a0b585f887331fbfce799fb51fc669d1251b87f68d814'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/65.0.3467.48/win/Opera_65.0.3467.48_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/65.0.3467.48/win/Opera_65.0.3467.48_Setup_x64.exe'
+  checksum       = '1eb56072913c3709d67e2ee68169c411c5639f8097aebc1a58f75839728baa8c'
+  checksum64     = '1f83196fd8460208f36692676a44931b17bd48eee8a4ad54507b5ce4879ba23d'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '65.0.3467.42'
+$version = '65.0.3467.48'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
--- automatic/wps-office-free/tools/chocolateyInstall.ps1
+++ automatic/wps-office-free/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '11.2.0.9031'
+$version = '11.2.0.9052'
 $packageName = 'wps-office-free'
 $regName = $packageName -replace('\-',' ')
 $regName = $regName -replace('free','*')
@@ -8,11 +8,11 @@ $registry = Get-UninstallRegistryKey -SoftwareName $regName
 $packageArgs = @{
   packageName    = 'wps-office-free'
   fileType       = 'exe'
-  url            = 'http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.9031/WPSOffice_11.2.0.9031.exe'
+  url            = 'http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.9052/WPSOffice_11.2.0.9052.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = $regName
-  checksum       = 'e2d5a357bbdeb6e31dec4742d662307bc739def7398771754157bddebe8aa5ce'
+  checksum       = '468fe5d91c3074578f85429bf03319492f8dcadd8017d13d01c07ead65482e1d'
   checksumType   = 'sha256'
 }
 
