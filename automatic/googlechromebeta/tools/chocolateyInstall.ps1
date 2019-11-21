be86883a17592a44e38b80e89667f62ce150dbf3
Chocolatey <chocolatey@realdimensions.net>
2019-11-21 00:13:48 +0000

    AU: 4 updated - brave googlechromebeta php protoc [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/134ba8be1c78fdfb56ed0ecbe78e73e27e254fb3

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '79.0.3945.36-beta'
+$version = '79.0.3945.45-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '596a4e582829f8f0dd61c4e7132cd024b93be19b729b940f117f622c8e679b66'
-  checksum64             = '14dbf22516ef018fa3e421bc93dff72588c4a3e2900010052777f25e7f756296'
+  checksum               = '0a6175c0dc0e49dccca4d810e05f8afbfc76539f6a96e58da6ab457d7edb0777'
+  checksum64             = 'd1f5c827f9d299030c12a2ff44223f00920c7a1b64ffb5d636dcd3fae86ae659'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/php/tools/chocolateyInstall.ps1
+++ automatic/php/tools/chocolateyInstall.ps1
@@ -14,10 +14,10 @@ if ($installLocation) {
 $pp = Get-PackageParameters
 
 $filesInfo = @{
-  filets32  = "$toolsPath\php-7.3.12-Win32-VC15-x86.zip"
-  filets64  = "$toolsPath\php-7.3.12-Win32-VC15-x64.zip"
-  filents32 = "$toolsPath\php-7.3.12-nts-Win32-VC15-x86.zip"
-  filents64 = "$toolsPath\php-7.3.12-nts-Win32-VC15-x64.zip"
+  filets32  = "$toolsPath\php-7.2.25-Win32-VC15-x86.zip"
+  filets64  = "$toolsPath\php-7.2.25-Win32-VC15-x64.zip"
+  filents32 = "$toolsPath\php-7.2.25-nts-Win32-VC15-x86.zip"
+  filents64 = "$toolsPath\php-7.2.25-nts-Win32-VC15-x64.zip"
 }
 
 if ($pp.ThreadSafe) {
