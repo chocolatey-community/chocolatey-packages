a5260fe7190d2e6900980f042a1d91cc16db43b7
Chocolatey <chocolatey@realdimensions.net>
2019-11-22 00:13:10 +0000

    AU: 3 updated - nodejs vim youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0a44aafe6c4e5539d81478e9546af7dd6981703f

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.1.2321_x86.zip"
-  file64        = "$toolsDir\gvim_8.1.2321_x64.zip"
+  file          = "$toolsDir\gvim_8.1.2333_x86.zip"
+  file64        = "$toolsDir\gvim_8.1.2333_x64.zip"
 }
 
 $installArgs = @{
