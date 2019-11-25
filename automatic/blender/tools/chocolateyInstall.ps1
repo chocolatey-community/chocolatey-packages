4441ee98c83556b066f0682d3a8755792d33aa9a
Chocolatey <chocolatey@realdimensions.net>
2019-11-25 12:12:06 +0000

    AU: 4 updated - blender brave chromium freevideoeditor [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/52540f6b664aa4d470e52abbe109593dc81b10cb

--- automatic/blender/tools/chocolateyInstall.ps1
+++ automatic/blender/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName    = 'blender'
   softwareName   = 'Blender'
   fileType       = 'msi'
-  file64         = "$toolsPath\blender-2.80-windows64.msi"
+  file64         = "$toolsPath\blender-2.81-windows64.msi"
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0, 2010, 1641)
 }
