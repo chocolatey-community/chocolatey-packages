3bc1a3911b2b6824bf4cb0307a4588984cd276dc
Chocolatey <chocolatey@realdimensions.net>
2019-12-08 00:11:29 +0000

    AU: 2 updated - autohotkey.portable vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ab26d40e1c3d15d686561067d33c25f2c1d1af90

--- automatic/autohotkey.portable/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$fileName  = 'AutoHotkey_1.1.32.00.zip'
+$fileName  = 'AutoHotkey_2.0-a108-a2fa0498.zip'
 $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $zip_path = "$toolsPath\$fileName"
 Remove-Item $toolsPath\* -Recurse -Force -Exclude $fileName
