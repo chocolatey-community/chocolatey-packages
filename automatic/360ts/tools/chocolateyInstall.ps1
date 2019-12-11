2d8a7cd49f847e4984747b5e8ad93be859b1a87b
Chocolatey <chocolatey@realdimensions.net>
2019-12-11 12:11:56 +0000

    AU: 5 updated - 360ts chromium git.portable kitty skype [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2ed6873348074a47e459a3d04b1630729ba44022

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1259.exe'
-  checksum               = '28af82f8ed7cce0c5534708bda1b622acd6a5abdb589102937e1943d3e618d9d'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1283.exe'
+  checksum               = 'c763c5292651dce746566cdd645cd56743c044467fdc4efc3939620a4af9bce8'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.55.0.123.exe'
-  checksum       = 'f2e6079057af281421f4b59dee8faeb048f4a268e1f68bb24bd7c36f122ddd9e'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.55.0.135.exe'
+  checksum       = '19c281b07dced1501e4547ec7fed86400912b866f12f3ec6620eb95e6a14e93d'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
