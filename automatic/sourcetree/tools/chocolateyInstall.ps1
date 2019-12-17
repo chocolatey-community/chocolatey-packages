f79193d830cab0cc63abc55ee4a4ff83eb1c256d
Chocolatey <chocolatey@realdimensions.net>
2019-12-17 00:12:35 +0000

    AU: 7 updated - brave cmake cmake.install cmake.portable Cygwin sourcetree vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7c7b661a66e30b67bb775aa96a7a6f0bf5141fc3

--- automatic/sourcetree/tools/chocolateyInstall.ps1
+++ automatic/sourcetree/tools/chocolateyInstall.ps1
@@ -15,8 +15,8 @@ $packageArgs = @{
   fileType      = 'msi'
   silentArgs    = "/qn /norestart ACCEPTEULA=1 /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
   validExitCodes= @(0,1641,3010)
-  url           = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_3.2.6.msi'
-  checksum      = 'c8b34688d7f69185b41f9419d8c65d63a2709d9ec59752ce8ea57ee6922cbba4'
+  url           = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_3.3.6.msi'
+  checksum      = 'f62d6498fd4433ee55bee7e9dc0af452bed990ae39b3badd9de64a38b3e39dcf'
   checksumType  = 'sha256'
   url64bit      = ''
   checksum64    = ''
