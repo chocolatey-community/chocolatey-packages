c8e69d5b0ee3f8add703770e1470096bea468bb9
Chocolatey <chocolatey@realdimensions.net>
2019-12-20 00:12:37 +0000

    AU: 11 updated - chromium clipboardfusion cmake.install cmake.portable logfusion nodejs.install python3 vim voicebot vscode vscode.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0e28b6926dcdd02ef19f39ea1cc77c346b5a2d23

--- automatic/voicebot/tools/chocolateyInstall.ps1
+++ automatic/voicebot/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'VoiceBot'
-    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/123/VoiceBotSetup-3.5.1.exe'
-    checksum        = 'bfa2b86aac4191347830220fad2c1eccb8cbe5943642c3566aa06dd4f99de2a9'
+    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/123/VoiceBotSetup-3.6.exe'
+    checksum        = 'cb81a36bd1a0c49d19ffbc44fba12d56ac69a001e524bef4cf7bc7995cb35b1d'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/VERYSILENT /LAUNCHAFTER=0'
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.41.0'
+$version = '1.41.1'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/9579eda04fdb3a9bba2750f15193e5fafe16b959/VSCodeSetup-ia32-1.41.0.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/9579eda04fdb3a9bba2750f15193e5fafe16b959/VSCodeSetup-x64-1.41.0.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/26076a4de974ead31f97692a0d32f90d735645c0/VSCodeSetup-ia32-1.41.1.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/26076a4de974ead31f97692a0d32f90d735645c0/VSCodeSetup-x64-1.41.1.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = '4362c7c22ec5f7d486e2f1a380dde1d9eaa640e6ba75f470b1d1e9006065b165'
+  checksum       = 'd72dfa1e4644e0bbe7ebe7243a2ac59f51b2d2ca261bb24936f2879ad1d6aac3'
   checksumType   = 'sha256'
-  checksum64     = '4d99e19f39f21cdeea644f099cd031a3f041f350e6dcb4c4646290fe7f9562a4'
+  checksum64     = 'cfde2bc9e601d3d1ab4a1f43c4a5d8c352b41a82cc97732a0c07ccaed3d2c086'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
