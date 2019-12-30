ea62d069382b85003c54e49b54b603466f0bda99
Chocolatey <chocolatey@realdimensions.net>
2019-12-30 12:11:56 +0000

    AU: 2 updated - 360ts alldup [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/57d78fa7f43d43e9c142db400b672aa45d9c0112

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName            = '360tse'
+  packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TSE_Setup_8.8.0.1119.exe'
-  checksum               = 'b6b7bfc5e429b97ebc1857b6bb73119bf48ce4657328e5aabea7933bb2564b65'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1300.exe'
+  checksum               = 'ef83b9b178e6a4c17721d75fa3f9523dea0bf52d83afd864d11cdbe40b5a6680'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
-  softwareName           = '360 Total Security Essential'
+  softwareName           = '360 Total Security'
 }
 Install-ChocolateyPackage @packageArgs
