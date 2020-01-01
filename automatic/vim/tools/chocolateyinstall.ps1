513cbf1612bfb8cce2650932f7ecec6ad293502e
Chocolatey <chocolatey@realdimensions.net>
2020-01-01 06:10:57 +0000

    AU: 2 updated - vim youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/667e07aad54dfba63e3d8cabcd909ef465654d62

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.0065_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.0065_x64.zip"
+  file          = "$toolsDir\gvim_8.2.0069_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.0069_x64.zip"
 }
 
 $installArgs = @{
