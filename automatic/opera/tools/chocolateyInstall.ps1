a9a3f515bf9266346053e852a5b287b9539e3a16
Chocolatey <chocolatey@realdimensions.net>
2020-01-08 12:12:34 +0000

    AU: 6 updated - brave chromium docker-kitematic nodejs.install Opera zotero [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/bde4707dc88102ad1ecde1942fe39d762465aaba

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/65.0.3467.78/win/Opera_65.0.3467.78_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/65.0.3467.78/win/Opera_65.0.3467.78_Setup_x64.exe'
-  checksum       = '252733d6e322d832d1c90e7e0d64c0ec5d08fb09eb5f722cc5a2a9030e4f648f'
-  checksum64     = 'faf3e4019989c32cf999f62b41775f3f8b5cfa1e101156516da8119c7f030a1a'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/66.0.3515.27/win/Opera_66.0.3515.27_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/66.0.3515.27/win/Opera_66.0.3515.27_Setup_x64.exe'
+  checksum       = '8b3a4cd721218e52853884c7cc5f3812d15f5355e22db3b61819bda7a78d1aa0'
+  checksum64     = '28114b8c6a110a80042f81b343997889aa78da495011453ef736a85bbee13429'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '65.0.3467.78'
+$version = '66.0.3515.27'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
--- automatic/zotero/tools/chocolateyInstall.ps1
+++ automatic/zotero/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero Standalone *'
-  file           = "$toolsPath\Zotero-5.0.79_setup.exe"
+  file           = "$toolsPath\Zotero-5.0.81_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
