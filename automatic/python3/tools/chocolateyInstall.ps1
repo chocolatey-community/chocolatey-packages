ba784a91b2153daa8ebbeeacbf4b0c3f2414dd41
Chocolatey <chocolatey@realdimensions.net>
2020-01-10 13:27:57 +0000

    AU: 2 updated - python python3 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/86d46deb90a0092cd5900ca260e92115d425eb03

--- automatic/python3/tools/chocolateyInstall.ps1
+++ automatic/python3/tools/chocolateyInstall.ps1
@@ -17,8 +17,8 @@ else {
 $packageArgs = @{
   packageName    = 'python3'
   fileType       = 'exe'
-  file           = "$toolsPath\python-3.5.4.exe"
-  file64         = "$toolsPath\python-3.5.4-amd64.exe"
+  file           = "$toolsPath\python-3.7.6.exe"
+  file64         = "$toolsPath\python-3.7.6-amd64.exe"
   silentArgs     = '/quiet InstallAllUsers=1 PrependPath=1 TargetDir="{0}"' -f $installDir
   validExitCodes = @(0)
   softwareName   = 'Python*'
