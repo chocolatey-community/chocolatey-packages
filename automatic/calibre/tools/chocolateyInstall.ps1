aabbd35a4726d364d5d94b7c378b1c24f5c7bd10
AdmiringWorm <kim.nordmo@gmail.com>
2020-01-10 11:42:02 +0100

    (calibre) Changed to download 32bit during installation

--- automatic/calibre/tools/chocolateyInstall.ps1
+++ automatic/calibre/tools/chocolateyInstall.ps1
@@ -5,12 +5,20 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'MSI'
-  file           = "$toolsPath\calibre-4.6.0.msi"
-  file64         = "$toolsPath\calibre-64bit-4.6.0.msi"
+  url            = ''
+  checksum       = ''
+  checksumType   = ''
+  file64         = "$toolsPath\calibre-64bit-4.8.0.msi"
   softwareName   = 'calibre*'
   silentArgs     = '/quiet'
   validExitCodes = @(0, 3010, 1641)
 }
-Install-ChocolateyInstallPackage @packageArgs
+
+if ((Get-ProcessorBits -compare 32) -or ($env:chocolateyForceX86 -eq $true)) {
+  Install-ChocolateyPackage @packageArgs
+}
+else {
+  Install-ChocolateyInstallPackage @packageArgs
+}
 
 Remove-Item -Force -ea 0 "$toolsPath\*.msi","$toolsPath\*.ignore"
--- automatic/calibre/update.ps1
+++ automatic/calibre/update.ps1
@@ -3,7 +3,10 @@ Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = 'https://download.calibre-ebook.com/4.html'
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix
+  Remove-Item "$PSScriptRoot\tools\$($Latest.FileName32)"
+}
 
 function global:au_AfterUpdate {
   Update-ChangelogVersion -version $Latest.Version
@@ -11,16 +14,16 @@ function global:au_AfterUpdate {
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
+    ".\legal\VERIFICATION.txt"      = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*Url\s*=\s*)''"                    = "`${1}'$($Latest.URL32)'"
+      "(?i)(^\s*Checksum\s*=\s*)''"               = "`${1}'$($Latest.Checksum32)'"
+      "(?i)(^\s*ChecksumType\s*=\s*)''"           = "`${1}'$($Latest.ChecksumType32)'"
       "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
   }
@@ -29,7 +32,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
-    $versionHyperlink = $download_page.links | select -First 1
+    $versionHyperlink = $download_page.links | Select-Object -First 1
     if ($versionHyperlink.Title -notmatch 'Release (4[\d\.]+)' ) { throw "Calibre version 3.x not found on $releases" }
 
     $version = $versionHyperlink.InnerText
