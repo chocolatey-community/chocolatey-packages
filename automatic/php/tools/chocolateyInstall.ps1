d6b37ee9e48710e01f5903180767f4d6d6690413
Chocolatey <chocolatey@realdimensions.net>
2020-01-21 18:13:28 +0000

    AU: 5 updated - cmake cmake.install cmake.portable php smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/96bd6d99ac3a82dda9b2177ee42c9f0e007a73a1

--- automatic/php/tools/chocolateyInstall.ps1
+++ automatic/php/tools/chocolateyInstall.ps1
@@ -14,10 +14,10 @@ if ($installLocation) {
 $pp = Get-PackageParameters
 
 $filesInfo = @{
-  filets32  = "$toolsPath\php-7.4.1-Win32-vc15-x86.zip"
-  filets64  = "$toolsPath\php-7.4.1-Win32-vc15-x64.zip"
-  filents32 = "$toolsPath\php-7.4.1-nts-Win32-vc15-x86.zip"
-  filents64 = "$toolsPath\php-7.4.1-nts-Win32-vc15-x64.zip"
+  filets32  = "$toolsPath\php-7.3.14-Win32-VC15-x86.zip"
+  filets64  = "$toolsPath\php-7.3.14-Win32-VC15-x64.zip"
+  filents32 = "$toolsPath\php-7.3.14-nts-Win32-VC15-x86.zip"
+  filents64 = "$toolsPath\php-7.3.14-nts-Win32-VC15-x64.zip"
 }
 
 if ($pp.ThreadSafe) {
