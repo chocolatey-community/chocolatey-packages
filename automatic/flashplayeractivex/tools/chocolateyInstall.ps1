5329d59c900cf996499eb1ae628cea647765cd09
Chocolatey <chocolatey@realdimensions.net>
2020-01-21 12:11:50 +0000

    AU: 7 updated - brave chromium flashplayeractivex flashplayerplugin flashplayerppapi keepassxc PDFCreator [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7833119980f6855f9d6797464e9a3bc9e33f56bc

--- automatic/flashplayeractivex/tools/chocolateyInstall.ps1
+++ automatic/flashplayeractivex/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'flashplayeractivex'
-$version = '32.0.0.314'
+$version = '32.0.0.321'
 $majorVersion = '32'
 $registry = ( Get-UninstallRegistryKey -SoftwareName "Adobe Flash Player $majorVersion ActiveX" ).DisplayVersion
 $checking = ( $registry -eq $version )
@@ -29,10 +29,10 @@ if ($allRight) {
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'msi'
-  url           = 'https://download.macromedia.com/pub/flashplayer/pdc/32.0.0.314/install_flash_player_32_active_x.msi'
+  url           = 'https://download.macromedia.com/pub/flashplayer/pdc/32.0.0.321/install_flash_player_32_active_x.msi'
   silentArgs    = '/quiet /norestart REMOVE_PREVIOUS=YES'
   softwareName  = 'Adobe Flash Player ActiveX'
-  checksum      = 'fb888286bf33242335ce2fdca7e9147505229ce5ed716fe85bded252556a2dcf'
+  checksum      = 'cb641820f87f67569a10cd30f9d58f560eaacd0ff37ccc95f9904f7c8b8200fc'
   checksumType  = 'sha256'
 }
   Install-ChocolateyPackage @packageArgs
--- automatic/flashplayerplugin/tools/chocolateyInstall.ps1
+++ automatic/flashplayerplugin/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'flashplayerplugin'
   fileType               = 'msi'
-  url                    = 'https://download.macromedia.com/get/flashplayer/pdc/32.0.0.314/install_flash_player_32_plugin.msi'
-  checksum               = '9b7ea97ec2c64b3e7ccbf6f0f18bc75f9fe36dc6936601378a468b94c3786b83'
+  url                    = 'https://download.macromedia.com/get/flashplayer/pdc/32.0.0.321/install_flash_player_32_plugin.msi'
+  checksum               = '87f807ec28b8a04cb8e346c68ffa5a87cfc5d4a745ac3b277efae34d39bb835f'
   checksumType           = 'sha256'
   silentArgs             = '/quiet /norestart REMOVE_PREVIOUS=YES'
   validExitCodes         = @(0)
