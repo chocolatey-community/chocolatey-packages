7087fd72c7a720494d2334ad0b534bea74c13cad
Chocolatey <chocolatey@realdimensions.net>
2020-01-25 00:12:28 +0000

    AU: 4 updated - dropbox nuget.commandline thunderbird vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0f09bba3a79c83dfcfc12be23207d77a5a1ab7f5

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
  if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '89.4.278'
+$version = '90.3.292'
 
 if (!(IsVersionAlreadyInstalled $version)) {
     $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) {$false} else {$true}
@@ -11,8 +11,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
     $packageArgs        = @{
         packageName     = $env:ChocolateyPackageName
         softwareName    = "Dropbox"
-        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2089.4.278%20Offline%20Installer.exe'
-        checksum        = '6218007f6ec41a01e66cf3fe6eed7195bb64acafbc4f375a7a6c24cb20d2da3d'
+        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2090.3.292%20Offline%20Installer.exe'
+        checksum        = 'a97239fc010028a0f7ac09ad119a7e9fdf64f31f0460b4e3c4193f2d8fe4acd9'
         fileType        = 'exe'
         checksumType    = 'sha256'
         silentArgs      = '/s'
