6aca7f5fc1fbc8b3bbb8fd5558babaea3d8a59a0
Chocolatey <chocolatey@realdimensions.net>
2020-01-27 00:11:48 +0000

    AU: 2 updated - ffmpeg vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/08a866fc470bfafd8107e22c35c90c7aa3fdd5df

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.0148_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.0148_x64.zip"
+  file          = "$toolsDir\gvim_8.2.0158_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.0158_x64.zip"
 }
 
 $installArgs = @{
