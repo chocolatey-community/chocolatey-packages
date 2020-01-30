43393358e23f0b71cf412d0687b3b430c5d0cda7
Chocolatey <chocolatey@realdimensions.net>
2020-01-30 12:12:15 +0000

    AU: 2 updated - chromium skype [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f7cd9f1c5db0751678eea58f55f72ea1f6c000ff

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.55.0.141.exe'
-  checksum       = '67bdccd21492781e3b74689b2bbf2a6f40dc21590a8d2373ec09ca89da95faec'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.56.0.103.exe'
+  checksum       = 'a18bf240bfdadb7d55f5a9140b403ed01b68ad7c4be33e2fafcf044d7bb73ce1'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
