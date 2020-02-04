0bf19e8d3f1bd38127e15fc1ecece7c56567575e
Chocolatey <chocolatey@realdimensions.net>
2020-02-04 18:13:12 +0000

    AU: 5 updated - cmake cmake.install cmake.portable palemoon sonarlint-vs2015 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c3596ded2070645d5c9867bfcbe2bf87d0ab2973

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.8.2.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.8.2.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.8.2.1.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.8.2.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '6e8bf9affb9668f4b4e957ae0f8cc326055c9ff8d0e3ca3eb5d3e3390b3d0586'
+  checksum      = '06c21da264b9849bbf8ac211c186a5e53fa55d457d314ea6e54b71ba21c804be'
   checksumType  = 'sha256'
-  checksum64    = '73408894d7342cc276f1f3b465691e9f486e9a40f19d690a89a31dfbebf01403'
+  checksum64    = 'c9fef0818f56f871010cabd92e77513ee9de315f94018541286e8daa70c02d69'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-4.16.0.14364-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.17.0.14702-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
