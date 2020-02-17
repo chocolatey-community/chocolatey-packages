ce7da8cb86c3b4a3a336a1871b9d73ebb8f8471b
Chocolatey <chocolatey@realdimensions.net>
2020-02-17 12:11:46 +0000

    AU: 3 updated - 360ts brave spotify [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/59a715fd106a2c7893b2bec4a296545ecec08e77

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1335.exe'
-  checksum               = '50e4c0fa45d86fae8868673b492c8c50a757a5e56c1c183235757d35212d7044'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1338.exe'
+  checksum               = 'd4d424313cda67c3c4b408b85bd828ecbee6ca71fc05f54e53546f5e0783045f'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
