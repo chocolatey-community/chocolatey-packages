c8e5a468783c779fd37fa16bcc7c0db402cfefe1
Chocolatey <chocolatey@realdimensions.net>
2020-02-28 18:13:31 +0000

    AU: 1 updated - tixati.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1b7bc3cf09b00ee542d322aa094949a0b72f1b1a

--- automatic/tixati.portable/tools/chocolateyInstall.ps1
+++ automatic/tixati.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'tixati.portable'
-  url            = 'https://download2.tixati.com/download/tixati-2.67-1.portable.zip'
-  checksum       = '7070b2c6a2b413949416727aa67cb72da34ae0e137d39627d51be3a82cdb9109'
+  url            = 'https://download2.tixati.com/download/tixati-2.68-1.portable.zip'
+  checksum       = 'd98917223e39354068b6e4ce16ecc75d0ac88c6c52cd7b7dc5eb9dfabea39851'
   checksumType   = 'sha256'
   unzipLocation  = $toolsPath
 }
