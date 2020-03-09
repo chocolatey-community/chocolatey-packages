5cc2a9bd0bcde511baa89e542ec722115620774c
Chocolatey <chocolatey@realdimensions.net>
2020-03-09 00:12:30 +0000

    AU: 4 updated - chromium mkvtoolnix peazip.install vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5fc389e763d2c6a1101523d0029563a11a561a5b

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.0360_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.0360_x64.zip"
+  file          = "$toolsDir\gvim_8.2.0364_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.0364_x64.zip"
 }
 
 $installArgs = @{
