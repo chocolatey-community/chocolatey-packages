d753fd08cd3279a663aa37604547dd7cfbae6f82
Chocolatey <chocolatey@realdimensions.net>
2020-03-12 18:12:47 +0000

    AU: 7 updated - cmake cmake.portable Ghostscript nodejs nodejs.install Opera spotify [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b9a266b0818e5872f05c0ea43aee8aa34f67527d

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/67.0.3575.53/win/Opera_67.0.3575.53_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/67.0.3575.53/win/Opera_67.0.3575.53_Setup_x64.exe'
-  checksum       = '8af5da925d3e59f522f8d3848617cee4f8fcb9dda14d3e529bf713ab50c59343'
-  checksum64     = 'c3e5d5a516e8f34d0c748cd911fa327194597d4d8777e377d52b3da7816f2cf6'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/67.0.3575.79/win/Opera_67.0.3575.79_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/67.0.3575.79/win/Opera_67.0.3575.79_Setup_x64.exe'
+  checksum       = '19a60575bdfab5ae4fe9f1abba2a5399b5719e6914206fca73b454b3cd519e3a'
+  checksum64     = '3c1db5fe63f91d0de56d04cd00c09b7566cc4d211badc27285e3b0eef636697c'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '67.0.3575.53'
+$version = '67.0.3575.79'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
