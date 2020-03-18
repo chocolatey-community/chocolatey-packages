833f8152a28aa2c249d380342337ae0514742fd6
Matteo Dell'Aquila <matteo.dellaquila@gmail.com>
2020-03-18 05:31:20 +0100

    k9s package added
    Signed-off-by: Matteo Dell'Aquila <matteo.dellaquila@gmail.com>

---
+++ automatic/k9s/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName   = $env:ChocolateyPackageName
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  PackageName    = $packageName
+  FileFullPath   = Get-Item $toolsDir\*_i386.tar.gz
+  FileFullPath64 = Get-Item $toolsDir\*_x86_64.gz
+  Destination    = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$packageArgs2 = @{
+  PackageName    = $packageName
+  FileFullPath   = Get-Item $toolsDir\*_i386.tar
+  FileFullPath64 = Get-Item $toolsDir\*_x86_64.tar
+  Destination    = $toolsDir
+}
+
+Get-ChocolateyUnzip @packageArgs2
+
+Remove-Item "$toolsDir\k9s_Windows_*.tar"
