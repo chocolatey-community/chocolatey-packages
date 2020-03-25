2d1734bea32431910daa55fac63f92595b8bf29d
Chocolatey <chocolatey@realdimensions.net>
2020-03-25 00:13:41 +0000

    AU: 4 updated - palemoon vim vscode vscode.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/587242933e55a9aa4284769c8aeade4c0bab080b

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.9.0.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.9.0.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.9.0.1.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.9.0.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = 'fd4d2bce872a88991a00744b1dc59e31c6f369ce36cebc3f9adcae580584dbe8'
+  checksum      = '1ae7e312276b563d42f985ae1e359c36e2faf53454cd37f62e6ac00e00143d6c'
   checksumType  = 'sha256'
-  checksum64    = '6f814cc61a46ba35e63e803892c05a36f618868795effe139decf9ef79aed72a'
+  checksum64    = 'c3f08e435110e883f043e600507f9b93ac5b66d80c6c1acee66db638195a1e6e'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.43.1'
+$version = '1.43.2'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/fe22a9645b44368865c0ba92e2fb881ff1afce94/VSCodeSetup-ia32-1.43.1.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/fe22a9645b44368865c0ba92e2fb881ff1afce94/VSCodeSetup-x64-1.43.1.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/0ba0ca52957102ca3527cf479571617f0de6ed50/VSCodeSetup-ia32-1.43.2.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/0ba0ca52957102ca3527cf479571617f0de6ed50/VSCodeSetup-x64-1.43.2.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = 'a66f472d4d107206d1f89ff399f9a168544dc06f4c18f14a9658db15bdc35832'
+  checksum       = 'c66712e0d55727fbb94d4c665a33f071cb6165278617a9ee9a51d5ded172df08'
   checksumType   = 'sha256'
-  checksum64     = 'a9aad0cb2cf46c76d59f654fabc5cb23e7fccec35b8e0a1c94b575ed0ace0d24'
+  checksum64     = '598c24e8db07b61346c7966601d458e2134abdeaba22f83431594dc734bfbc4b'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
