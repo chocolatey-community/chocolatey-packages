9557cdc44d68d334c7a79fabbcda4d78c51772d7
Chocolatey <chocolatey@realdimensions.net>
2020-03-26 00:12:29 +0000

    AU: 2 updated - kubernetes-cli palemoon [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8f09c1af4dddec18f4951bf81021c6e6f3425254

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.9.0.1.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.9.0.1.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.9.0.2.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.9.0.2.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '1ae7e312276b563d42f985ae1e359c36e2faf53454cd37f62e6ac00e00143d6c'
+  checksum      = 'd7c79918c773405e7298718ead0210ecf876fac451820e7217ba209fdb2937d4'
   checksumType  = 'sha256'
-  checksum64    = 'c3f08e435110e883f043e600507f9b93ac5b66d80c6c1acee66db638195a1e6e'
+  checksum64    = 'c06e08e0264ef9a1421ad07a33bbb353df7bfd657aee3f4ef327bd8fd8e44f8f'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
