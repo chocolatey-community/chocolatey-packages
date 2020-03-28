81db84279735ceef96425068b0c020538e7fa0c9
Chocolatey <chocolatey@realdimensions.net>
2020-03-28 00:12:06 +0000

    AU: 2 updated - mp3tag vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/6828ed6252a3ade3adcdc42c500a7eacbc64421e

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,7 +24,7 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv300dsetup.exe"
+  file           = "$toolsPath\mp3tagv301setup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
