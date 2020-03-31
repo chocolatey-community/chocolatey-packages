93cba76794ca9d9d716db67f48fb1d13dc7cf905
Chocolatey <chocolatey@realdimensions.net>
2020-03-31 12:13:25 +0000

    AU: 3 updated - chromium codeblocks gnucash [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/33876f91ee9e48a65fbd8200bf8c55cc7f8ddd5b

--- automatic/gnucash/tools/chocolateyInstall.ps1
+++ automatic/gnucash/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\gnucash-3.7.setup.exe"
+  file           = "$toolsPath\gnucash-3.9.setup.exe"
 
   softwareName   = 'GnuCash*'
 
