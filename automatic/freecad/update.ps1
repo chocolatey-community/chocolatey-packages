042fd522a8a877bd3faf765ce8ba48f4e601b40f
AdmiringWorm <kim.nordmo@gmail.com>
2020-04-01 14:34:05 +0200

    (freecad) Corrected style of of files
    This was necessary to do as git would not allow to continue without fixing up
    the different style issues in these files

--- automatic/freecad/update.ps1
+++ automatic/freecad/update.ps1
@@ -1,16 +1,16 @@
-import-module au
+﻿import-module au
 
- if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+if (!$PSScriptRoot) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\update_helper.ps1"
- 
+
 $PreUrl = 'https://github.com'
 $releases = "$PreUrl/FreeCAD/FreeCAD/releases"
 $softwareName = 'FreeCAD'
 
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*packageName\s*=\s*)'.*'"	  = "`$1'$($Latest.PackageName)'"
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)(^\s*packageName\s*=\s*)'.*'"      = "`$1'$($Latest.PackageName)'"
       "(?i)(^\s*fileType\s*=\s*)('.*')"       = "`$1'$($Latest.fileType)'"
       "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
       "(?i)(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
@@ -20,10 +20,10 @@ function global:au_SearchReplace {
       "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
       "(?i)(^\s*softwareName\s*=\s*)'.*'"     = "`$1'$($softwareName)'"
     }
-    ".\freecad.nuspec" = @{
-	  "(?i)(^\s*\<id\>).*(\<\/id\>)"                     = "`${1}$($Latest.PackageName)`${2}"
-	  "(?i)(^\s*\<title\>).*(\<\/title\>)"               = "`${1}$($Latest.Title)`${2}"
-	  "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+    ".\freecad.nuspec"                = @{
+      "(?i)(^\s*\<id\>).*(\<\/id\>)"                     = "`${1}$($Latest.PackageName)`${2}"
+      "(?i)(^\s*\<title\>).*(\<\/title\>)"               = "`${1}$($Latest.Title)`${2}"
+      "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
     }
     ".\tools\chocolateyUninstall.ps1" = @{
       "(?i)(^\s*packageName\s*=\s*)'.*'"  = "`$1'$($Latest.PackageName)'"
@@ -34,19 +34,19 @@ function global:au_SearchReplace {
 }
 
 function global:au_BeforeUpdate {
-    Get-RemoteFiles -Purge -FileNameBase "$($Latest.PackageName)"
-	$types = @("7z","exe","json")
-	foreach ($file in $types) {
-	Remove-Item ".\tools\*.${file}" -Force # Removal of downloaded files	
-	}
+  Get-RemoteFiles -Purge -FileNameBase "$($Latest.PackageName)"
+  $types = @("7z", "exe", "json")
+  foreach ($file in $types) {
+    Remove-Item ".\tools\*.${file}" -Force # Removal of downloaded files
+  }
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $streams = [ordered] @{
-    dev = Get-FreeCad -Title "${softwareName}" -kind "dev"
-    stable = Get-FreeCad -Title "${softwareName}"
-	portable = Get-FreeCad -Title "${softwareName}" -kind "portable"
+    dev      = Get-FreeCad -Title "${softwareName}" -kind "dev"
+    stable   = Get-FreeCad -Title "${softwareName}"
+    portable = Get-FreeCad -Title "${softwareName}" -kind "portable"
   }
   return @{ Streams = $streams }
 }
