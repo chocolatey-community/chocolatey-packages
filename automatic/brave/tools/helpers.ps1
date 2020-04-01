04e36467a943c1e38e06237f95f5ddbd4af6b23b
TakataSanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2020-04-02 01:34:22 +0900

    move the Get-UninstallRegistryKey inside a function

--- automatic/brave/tools/helpers.ps1
+++ automatic/brave/tools/helpers.ps1
@@ -1,9 +1,9 @@
-[array]$key = Get-UninstallRegistryKey -SoftwareName 'Brave*'
-$installedVersion = $key.Version[3..($key.Version.length - 1)]
-$installedVersion = "$installedVersion" -replace '\s',''
-
 if (-not $env:ChocolateyForce) {
   try {
+    [array]$key = Get-UninstallRegistryKey -SoftwareName 'Brave*'
+    $installedVersion = $key.Version[3..($key.Version.length - 1)]
+    $installedVersion = "$installedVersion" -replace '\s',''
+
     if ($installedVersion -ge [Version]::Parse($env:ChocolateyPackageVersion))
     {
       $alreadyInstalled = $installedVersion
