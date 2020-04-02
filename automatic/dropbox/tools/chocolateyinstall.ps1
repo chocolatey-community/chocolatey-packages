8d988574ddd17dbc3e54207a8250061208535deb
Chocolatey <chocolatey@realdimensions.net>
2020-04-02 00:13:15 +0000

    AU: 5 updated - brave chromium dropbox etcd juju [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/caa013003bbc7f44ec9c60afe904eb7233395885

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
  if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '95.3.425'
+$version = '94.4.384'
 
 if (!(IsVersionAlreadyInstalled $version)) {
     $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) {$false} else {$true}
@@ -11,8 +11,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
     $packageArgs        = @{
         packageName     = $env:ChocolateyPackageName
         softwareName    = "Dropbox"
-        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2095.3.425%20Offline%20Installer.exe'
-        checksum        = 'a813ea4f72792e11ebef99ee8a1480c2bb90bf16f4d3053e8263c269f734f932'
+        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2094.4.384%20Offline%20Installer.exe'
+        checksum        = '69193537721ad4e10c4b264c5afa9aeb10e73657b82be38d629d05a5765f8c1f'
         fileType        = 'exe'
         checksumType    = 'sha256'
         silentArgs      = '/s'
