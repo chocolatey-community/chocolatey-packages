4bd4f6327b5fdd4bd0c2fc7d1bfb39d68f22c017
AdmiringWorm <kim.nordmo@gmail.com>
2020-04-02 04:34:11 +0200

    (cutepdf) Added Get-FixVersion helper call

--- automatic/cutepdf/update_helper.ps1
+++ automatic/cutepdf/update_helper.ps1
@@ -1,11 +1,11 @@
-
+﻿
 function GetETagIfChanged() {
   param([string]$url)
 
   if (!(Test-Path $PSScriptRoot\info)) {
-	New-Item $PSScriptRoot\info -ItemType file
+  New-Item $PSScriptRoot\info -ItemType file
   }
-  
+
   if (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
     $existingETag = Get-Content "$PSScriptRoot\info" -Encoding "UTF8" | Select -First 1 | Foreach { $_ -split '\|' } | Select -First 1
   }
@@ -27,6 +27,7 @@ function GetResultInformation() {
   $dest = "$env:TEMP\$file"
   Invoke-WebRequest -UseBasicParsing -Uri $url32 -OutFile $dest
   $version = Get-Item $dest | Foreach { $_.VersionInfo.FileVersion -replace '^(\d+(\.[\d]+){1,3}).*', '$1' }
+  $version = Get-FixVersion -Version $version -OnlyFixBelowVersion "4.0.1"
 
   $result = @{
     URL32          = $url32
