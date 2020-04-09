3d7c2a8894a9d8d1893a499acd2280408bfc4812
Chocolatey <chocolatey@realdimensions.net>
2020-04-09 00:20:45 +0000

    AU: 6 updated - kubernetes-cli nodejs nodejs.install thunderbird vim vscode [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/fa73456e7e6f36694c81ed5b05d5a74964be104d

--- automatic/thunderbird/tools/chocolateyInstall.ps1
+++ automatic/thunderbird/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageName = 'thunderbird'
 $softwareName = 'Mozilla Thunderbird'
 
-$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '68.5.0')
+$alreadyInstalled = (AlreadyInstalled -product $softwareName -version '68.7.0')
 
 if (Get-32bitOnlyInstalled -product $softwareName) {
   Write-Output $(
@@ -34,7 +34,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
 
     Checksum = $checksums.Win32
     ChecksumType = 'sha512'
-    Url = "https://download.mozilla.org/?product=thunderbird-68.5.0-SSL&os=win&lang=${locale}"
+    Url = "https://download.mozilla.org/?product=thunderbird-68.7.0-SSL&os=win&lang=${locale}"
 
     silentArgs = '-ms'
     validExitCodes = @(0)
@@ -43,7 +43,7 @@ if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
   if (!(Get-32bitOnlyInstalled($softwareName)) -and (Get-OSArchitectureWidth 64)) {
     $packageArgs.Checksum64 = $checksums.Win64
     $packageArgs.ChecksumType64 = 'sha512'
-    $packageArgs.Url64 = "https://download.mozilla.org/?product=thunderbird-68.5.0-SSL&os=win64&lang=${locale}"
+    $packageArgs.Url64 = "https://download.mozilla.org/?product=thunderbird-68.7.0-SSL&os=win64&lang=${locale}"
   }
 
   Install-ChocolateyPackage @packageArgs
