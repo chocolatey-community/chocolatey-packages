bd897d6695f57c47acae0533ba3cc6986a996820
AdmiringWorm <kim.nordmo@gmail.com>
2020-04-11 22:53:09 +0200

    (brave) Corrected the grabbing of existing version in helper.ps1

--- automatic/brave/tools/helpers.ps1
+++ automatic/brave/tools/helpers.ps1
@@ -1,10 +1,12 @@
 ﻿function Get-InstalledVersion() {
   [array]$key = Get-UninstallRegistryKey -SoftwareName 'Brave*'
-  $installedVersion = $key.Version[3..($key.Version.length - 1)]
-  $installedVersion = "$installedVersion" -replace '\s', ''
+  if ($key.Length -ge 1) {
+    $installedVersion = $key.Version[3..($key.Version.length - 1)]
+    $installedVersion = "$installedVersion" -replace '\s', ''
 
-  if ($installedVersion -and (-not $env:ChocolateyForce)) {
-    return [version]$installedVersion
+    if ($installedVersion -and (-not $env:ChocolateyForce)) {
+      return [version]$installedVersion
+    }
   }
 
   return $null
