53230f79a13cfbdbeda225a7d73d87e4704ec960
Chocolatey <chocolatey@realdimensions.net>
2020-04-21 18:14:59 +0000

    AU: 5 updated - nodejs notepadplusplus notepadplusplus.commandline notepadplusplus.install sonarlint-vs2015 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5e65135d546e10923e0ae35c41d15f1b5c6cf7cf

--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-4.19.0.15817-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.21.0.16909-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
