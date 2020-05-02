73cea76dc08132f012b7e63091a610fd01f6764b
RB <RedBaron2@users.noreply.github.com>
2020-05-01 21:56:51 -0700

    Update update_helper.ps1
    fixed the spacing issue no more tabs, and made the package check the checksum to determine a force.

--- automatic/cutepdf/update_helper.ps1
+++ automatic/cutepdf/update_helper.ps1
@@ -1,86 +1,64 @@
 
-function GetETagIfChanged() {
-  param([string]$url)
-
-  if (!(Test-Path $PSScriptRoot\info)) {
-	New-Item $PSScriptRoot\info -ItemType file
-  }
-  
-  if (($global:au_Force -ne $true) -and (Test-Path $PSScriptRoot\info)) {
-    $existingETag = Get-Content "$PSScriptRoot\info" -Encoding "UTF8" | Select -First 1 | Foreach { $_ -split '\|' } | Select -First 1
-  }
-  else {
-    $existingETag = $null
-  }
-
-  $etag = Invoke-WebRequest -Method Head -Uri $url -UseBasicParsing
-  # $etag = $etag | Foreach { $_.Headers.ETag }
-  $etag = $etag | Foreach { $_.Headers["Content-Length"] }
-  if ($etag -eq $existingETag) { return $null }
-
-  return $etag
-}
-
 function Compare-Hashes {
 param (
-	[string]$my_path = "$PSScriptRoot",
-	[string]$reg_chksum = '\bchecksum\b'
+  [string]$my_path = "$PSScriptRoot",
+  [string]$reg_chksum = '\bchecksum\b',
+  [string]$checksumType = '\bchecksumType\b'
 )
-	$gud = $false
-	$current_checksum = (gi "$my_path\tools\chocolateyInstall.ps1" | sls $reg_chksum) -split "=|'" | Select -Last 1 -Skip 1
-	if ($current_checksum.Length -ne 128) { throw "Can't find current checksum" }
-	$remote_checksum  = Get-RemoteChecksum $url
-	if ($current_checksum -ne $remote_checksum) {
-		Write-Host 'Remote checksum is different then the current one, forcing update'
-		$gud = $true
-	}
+$global:au_force=$gud = $false;
+$current_checksumType = (gi "$my_path\tools\chocolateyInstall.ps1" | sls $checksumType) -split "=|'" | Select -Last 1 -Skip 1
+$current_checksum = (gi "$my_path\tools\chocolateyInstall.ps1" | sls $reg_chksum) -split "=|'" | Select -Last 1 -Skip 1
+if ($current_checksumType -eq "SHA512"){ $characters = "128" }; if ($current_checksumType -eq "SHA256"){ $characters = "64"}
+if ($current_checksum.Length -ne $characters) { throw "Can't find current checksum" }
+$remote_checksum  = Get-RemoteChecksum $url -Algorithm $current_checksumType
+ if ($current_checksum -ne $remote_checksum) {
+  Write-Host 'Remote checksum is different then the current one, forcing update'
+  $global:au_old_force = $global:au_force; $global:au_force = $true; $gud = $true
+ }
 return $gud
 }
 
-function Get-PaddingNumber {
-	$tags= Invoke-WebRequest -Method Head -Uri $url -UseBasicParsing
-	$date = $tags | Foreach { $_.Headers["Date"] }
-	$lastChange = $tags | Foreach { $_.Headers["Last-Modified"] }
-	$ts = New-TimeSpan -Start $lastChange -End $date
-return $ts
-}
-
-function Get-FixedVersion {
+function Get-ETagIfChanged {
 param(
-	[string]$new,
-	[string]$etag,
-	[string]$infoPath = "$PSScriptRoot\info"
+  [string]$url,
+  [string]$tag = "Content-Length",
+  [string]$packageName = "$PSScriptRoot\info"
+) 
+ (Compare-Hashes) | Out-Null
+ if (!(Test-Path "$packageName")) {
+   New-Item "$PSScriptRoot$packageName" -ItemType file
+ }  
+ if (($global:au_Force -ne $true) -and (Test-Path "$packageName")) {
+  $existingETag = Get-Content "$packageName" -Encoding "UTF8" | Select -First 1 | Foreach { $_ -split '\|' } | Select -First 1
+ } else {
+  $existingETag = $null
+ }
+ $etag = Invoke-WebRequest -Method Head -Uri $url -UseBasicParsing
+ $etag = $etag | Foreach { $_.Headers.$tag }
+ if ($etag -eq $existingETag) { return $null }
+return $etag
+}
+Set-Alias GetETagIfChanged Get-ETagIfChanged
+
+function Get-ResultInformation {
+param(
+  [string]$url32,
+  [string]$file = "install.exe",
+  [string]$version = "FileVersion",
+  [string]$algorithm = "SHA512"
 )
-$me = ( $MyInvocation.MyCommand ); $padVersion = "4.0.0.2"
-$version = Get-Content $infoPath -Encoding "UTF8" | select -First 1 | Foreach { $_ -split '\|' } | select -Last 1
-  if (($new -ge $padVersion) -and ($version -ge $new)) {
-	if ([string]::IsNullOrEmpty($etag)) {
-	$etag = Get-Content $infoPath -Encoding "UTF8" | Select -First 1 | Foreach { $_ -split '\|' } | Select -First 1
-	} 
-	if ( Compare-Hashes ) {
-	$regex = @{$true=".{2}$";$false=""}[ ( (( $version -split "\.")[-1]) -match "\d{3}") ]
-	$padding = @{$true=(( Get-PaddingNumber ).Days ).ToString("00");$false=""}[ ( (( $version -split "\.")[-1]) -match "\d{3}") ]
-	$version = ($version -replace $regex ) + $padding
-	}     
-  } else {
-   $version = $new
-  }
-return $version
+$dest = "$env:TEMP\$file"
+Invoke-WebRequest -UseBasicParsing -Uri $url32 -OutFile $dest
+$version = Get-Item $dest | Foreach { $_.VersionInfo.$version -replace '^(\d+(\.[\d]+){1,3}).*', '$1' }
+$version = ( Get-FixVersion $version )
+
+ $result = @{
+  URL32          = $url32
+  Version        = $version
+  Checksum32     = Get-FileHash $dest -Algorithm $algorithm | Foreach Hash
+  ChecksumType32 = $algorithm
+ }
+Remove-Item -Force $dest
+return $result
 }
-
-function GetResultInformation() {
-  param([string]$url32,[string]$file)
-
-  $dest = "$env:TEMP\$file"
-  Invoke-WebRequest -UseBasicParsing -Uri $url32 -OutFile $dest
-  $version = Get-Item $dest | Foreach { $_.VersionInfo.FileVersion -replace '^(\d+(\.[\d]+){1,3}).*', '$1' }
-
-  $result = @{
-    URL32          = $url32
-    Version        = Get-FixedVersion $version
-    Checksum32     = Get-FileHash $dest -Algorithm "SHA512" | Foreach Hash
-    ChecksumType32 = 'sha512'
-  }
-  Remove-Item -Force $dest
-  return $result
-}
+Set-Alias GetResultInformation Get-ResultInformation
