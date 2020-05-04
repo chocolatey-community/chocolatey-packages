10393aa364458c08e13501e014b007f454e7606f
Chocolatey <chocolatey@realdimensions.net>
2020-05-04 12:13:06 +0000

    AU: 3 updated - brave chromium keeweb [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/51d57372bf7cd0d8747f2a0804652d4acf55faef

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\BraveBrowserSilentBetaSetup32.exe"
-  file64      = "$toolsPath\BraveBrowserSilentBetaSetup.exe"
+  file        = "$toolsPath\BraveBrowserStandaloneSilentSetup32.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.9.50'
+[version]$softwareVersion = '1.8.89'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
