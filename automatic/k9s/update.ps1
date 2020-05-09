0108e1f4685e3fe45628a02a8790c451460f73e1
Matteo Dell'Aquila <matteo.dellaquila@gmail.com>
2020-05-09 06:21:31 +0200

    support for 32bit version removed
    Signed-off-by: Matteo Dell'Aquila <matteo.dellaquila@gmail.com>

--- automatic/k9s/update.ps1
+++ automatic/k9s/update.ps1
@@ -6,7 +6,6 @@ Import-Module AU
 $domain   = 'https://github.com'
 $releases = "$domain/derailed/k9s/releases/latest"
 
-$filename32 = 'k9s_Windows_i386.tar.gz'
 $filename64 = 'k9s_Windows_x86_64.tar.gz'
 
 function global:au_BeforeUpdate {
@@ -16,10 +15,8 @@ function global:au_BeforeUpdate {
 function global:au_SearchReplace {
   @{
     ".\legal\VERIFICATION.txt" = @{
-      "(?i)(^\s*32\-bit software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(^\s*64\-bit software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*"      = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*"        = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum\s*type\:).*"      = "`${1} $($Latest.ChecksumType64)"
       "(?i)(^\s*checksum(64)?\:).*"        = "`${1} $($Latest.Checksum64)"
     }
 
@@ -39,18 +36,14 @@ function global:au_GetLatest {
 
   $checksumAsset = $domain + ($download_page.Links | ? href -match "checksums\.txt$" | select -first 1 -expand href)
   $checksum_page = Invoke-WebRequest -Uri $checksumAsset -UseBasicParsing
-  $checksum32 = [regex]::Match($checksum_page, "([a-f\d]+)\s*$([regex]::Escape($filename32))").Groups[1].Value
   $checksum64 = [regex]::Match($checksum_page, "([a-f\d]+)\s*$([regex]::Escape($filename64))").Groups[1].Value
   
   return @{
     Version        = $version
-    URL32          = "$domain/derailed/k9s/releases/download/v${version}/${filename32}"
     URL64          = "$domain/derailed/k9s/releases/download/v${version}/${filename64}"
     ReleaseNotes   = "$domain/derailed/k9s/blob/v${version}/change_logs/release_v${version}.md"
     ReleaseURL     = "$domain/derailed/k9s/releases/tag/v${version}"
-    Checksum32     = $checksum32
     Checksum64     = $checksum64
-    ChecksumType32 = "sha256"
     ChecksumType64 = "sha256"
   }
 }
