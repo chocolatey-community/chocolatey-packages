ae1af716af9c16500e7f6f45d650f1dbf3d372fd
Felipe Santos <felipecassiors@gmail.com>
2020-05-09 01:55:38 +0000

    (vscode-insiders*) Add vscode-insiders* package (#1517)

---
+++ automatic/vscode-insiders.install/tools/ChocolateyInstall.ps1
@@ -0,0 +1,34 @@
+$ErrorActionPreference = 'Stop'
+
+$pp = Get-PackageParameters
+function Get-MergeTasks {
+  $t = "!runCode"
+  $t += ', ' + '!' * $pp.NoDesktopIcon + 'desktopicon'
+  $t += ', ' + '!' * $pp.NoQuicklaunchIcon + 'quicklaunchicon'
+  $t += ', ' + '!' * $pp.NoContextMenuFiles + 'addcontextmenufiles'
+  $t += ', ' + '!' * $pp.NoContextMenuFolders + 'addcontextmenufolders'
+  $t += ', ' + '!' * $pp.DontAssociateWithFiles + 'associatewithfiles'
+  $t += ', ' + '!' * $pp.DontAddToPath + 'addtopath'
+
+  Write-Host "Merge Tasks: $t"
+  $t
+}
+
+$packageArgs = @{
+  packageName    = "$env.ChocolateyPackageName"
+  fileType       = 'exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/d487078dc7fc1c276657cadb61b4f63833a8df55/VSCodeSetup-ia32-1.46.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/d487078dc7fc1c276657cadb61b4f63833a8df55/VSCodeSetup-x64-1.46.0-insider.exe'
+
+  softwareName   = 'Microsoft Visual Studio Code Insiders'
+
+  checksum       = '642736d0b4ae4e18dcb4db0c05072f9befb05497cc217312ab465c93cde90dab'
+  checksumType   = 'sha256'
+  checksum64     = '1dc7dba1cbd2db3db041ecadc4f0f3b3e15a88aecfc3c41fab006626986afecf'
+  checksumType64 = 'sha256'
+
+  silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/vscode-insiders.install/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,3 @@
+﻿Get-Process "Code - Insiders" -ea 0 | ForEach-Object { $_.CloseMainWindow() | Out-Null }
+Start-Sleep 1
+Get-Process "Code - Insiders" -ea 0 | Stop-Process  #in case gracefull shutdown did not succeed, try hard kill
