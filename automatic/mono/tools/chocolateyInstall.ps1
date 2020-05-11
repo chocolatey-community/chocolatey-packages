08990a1bfbf174a8e59db332bb9c7176ecbdf6af
Chocolatey <chocolatey@realdimensions.net>
2020-05-11 18:14:42 +0000

    AU: 3 updated - kitty mattermost-desktop mono [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7292abfb5262e9d26931e53c591455fe569f0157

--- automatic/mono/tools/chocolateyInstall.ps1
+++ automatic/mono/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://download.mono-project.com/archive/6.8.0/windows-installer/mono-6.8.0.105-gtksharp-2.12.45-win32-0.msi'
-  url64bit       = 'https://download.mono-project.com/archive/6.8.0/windows-installer/mono-6.8.0.105-x64-0.msi'
+  url            = 'https://download.mono-project.com/archive/6.8.0/windows-installer/mono-6.8.0.123-gtksharp-2.12.45-win32-0.msi'
+  url64bit       = 'https://download.mono-project.com/archive/6.8.0/windows-installer/mono-6.8.0.123-x64-0.msi'
   softwareName   = 'Mono for Windows*'
-  checksum       = '2e9431255d103bd63124fa43bd3a1b0f5a73f1dafb3e2fce5c102b2329b0be39'
+  checksum       = '8d545125d22bf695ba1491cef517bdec9394b409d88aaf1a7d42a9da2363a4ac'
   checksumType   = 'sha256'
-  checksum64     = 'dcbe043636f44623e397e43c8a552789cc747959bfcfbc202556cb6f32b47f1c'
+  checksum64     = '81755fe866f2cdb8f697d35a70aecb17ea04694e20b4b2463ea617f2e1eac61d'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
