bc52e0108a9e0132512237b084406801a92fc5b8
Chocolatey <chocolatey@realdimensions.net>
2020-05-14 12:14:14 +0000

    AU: 4 updated - brave freecad python python3 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ddbad94b8ab0412abcee6f59c835c498cc2ccc74

--- automatic/python3/tools/chocolateyInstall.ps1
+++ automatic/python3/tools/chocolateyInstall.ps1
@@ -17,8 +17,8 @@ else {
 $packageArgs = @{
   packageName    = 'python3'
   fileType       = 'exe'
-  file           = "$toolsPath\python-3.8.3rc1.exe"
-  file64         = "$toolsPath\python-3.8.3rc1-amd64.exe"
+  file           = "$toolsPath\python-3.8.3.exe"
+  file64         = "$toolsPath\python-3.8.3-amd64.exe"
   silentArgs     = '/quiet InstallAllUsers=1 PrependPath=1 TargetDir="{0}"' -f $installDir
   validExitCodes = @(0)
   softwareName   = 'Python*'
