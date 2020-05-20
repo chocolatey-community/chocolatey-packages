8b66f2efc1b53a2eba8c919bba88b658320ae10e
Chocolatey <chocolatey@realdimensions.net>
2020-05-20 00:12:26 +0000

    AU: 6 updated - brave freecad nodejs nodejs.install typescript vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/cfa07604dde56e78969b1907490921527cdc9f66

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.9.68'
+[version]$softwareVersion = '1.9.71'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
