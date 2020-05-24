a693b1413c78b6549d4f240aac21c14d5e2a87ec
Chocolatey <chocolatey@realdimensions.net>
2020-05-24 18:12:14 +0000

    AU: 1 updated - poi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/dc64d3a111562800649d485fb93d909ebfee303b

--- automatic/poi/tools/chocolateyinstall.ps1
+++ automatic/poi/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\poi-setup-10.6.0-beta.2.exe"
+  file          = "$toolsDir\poi-setup-10.6.0-beta.3.exe"
 
   softwareName  = 'poi*'
 
