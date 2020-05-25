f4d2742058bb7443b6623ff83cec844e53772e26
AdmiringWorm <kim.nordmo@gmail.com>
2020-05-25 11:45:14 +0200

    (lightworks) Updated to get correct url from website

--- automatic/lightworks/update.ps1
+++ automatic/lightworks/update.ps1
@@ -1,36 +1,41 @@
-import-module au
+﻿import-module au
 
-$releases = 'https://www.lwks.com/index.php?option=com_lwks&view=download&Itemid=206&tab=0'
+$domain = 'https://www.lwks.com'
+$releases = "$domain/index.php?option=com_lwks&view=download&Itemid=206&tab=0"
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
+  }
+}
+
+function Get-UrlFromPage([string]$url) {
+  $dlPage = Invoke-WebRequest $url -UseBasicParsing
+
+  $url = $dlPage.Links | ? href -match "doc_download" | % href
+
+  return Get-RedirectedUrl $url
 }
 
 function global:au_GetLatest {
-    [System.Net.ServicePointManager]::SecurityProtocol = 'Ssl3,Tls,Tls11,Tls12' #https://github.com/chocolatey/chocolatey-coreteampackages/issues/366
-    $download_page = Invoke-WebRequest $releases
+  [System.Net.ServicePointManager]::SecurityProtocol = 'Ssl3,Tls,Tls11,Tls12' #https://github.com/chocolatey/chocolatey-coreteampackages/issues/366
+  $download_page = Invoke-WebRequest $releases -UseBasicParsing
 
-    $version = $download_page.AllElements | ? class -eq 'release_table' | % InnerText | Select -First 1
-    $version = ($version -split ":|\n" | select -Index 1).Trim()
-    $verTwoPart = (Get-Version $version).ToString(2) -replace '\.','-'
-    if ($version -match "^\d+\.\d+$") {
-      $versionFull = "$version.0"
-    } else {
-      $versionFull = $version
-    }
+  $url32 = Get-UrlFromPage ($download_page.Links | ? href -match "win_public_32" | % { $domain + $_.href })
+  $url64 = Get-UrlFromPage ($download_page.Links | ? href -match "win_public_64" | % { $domain + $_.href })
 
-    @{
-        Version = $version
-        URL32   = "https://downloads.lwks.com/v$verTwoPart/lightworks_v${versionFull}_full_32bit_setup.exe"
-        URL64   = "https://downloads.lwks.com/v$verTwoPart/lightworks_v${versionFull}_full_64bit_setup.exe"
-    }
+  $version = $url64 -split '_' | select -last 1 -Skip 3
+
+  @{
+    Version = $version
+    URL32   = $url32
+    URL64   = $url64
+  }
 }
 
 update
