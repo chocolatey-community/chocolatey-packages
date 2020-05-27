41063d465087670abad31d463a0aaa3752419660
Chocolatey <chocolatey@realdimensions.net>
2020-05-27 00:11:55 +0000

    AU: 3 updated - protoc smartftp vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5421211a57e72712f7db4da290340069209a6802

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.0825_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.0825_x64.zip"
+  file          = "$toolsDir\gvim_8.2.0830_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.0830_x64.zip"
 }
 
 $installArgs = @{
