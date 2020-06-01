a2fd6879855f698fc8a8e3aa1220d066e04015ad
Chocolatey <chocolatey@realdimensions.net>
2020-06-01 12:11:49 +0000

    AU: 3 updated - 360ts Cygwin spotify [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/fc6c8430ebd81d240f70f0a51628ef114605f324

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.6.0.1411.exe'
-  checksum               = '4b835012d9c792ea9419d3eb19c21995e04a941dc62f10e28103a26fe65d83ee'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1021.exe'
+  checksum               = '8e6b86fb2026baf6793521ab3251f0910b487508fb4882a8fc4a1e6099dd7be6'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
