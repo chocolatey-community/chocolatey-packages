dd89a10b42bc3da1ab104a49308255f2d1b96e96
Chocolatey <chocolatey@realdimensions.net>
2020-06-03 06:12:02 +0000

    AU: 2 updated - chromium protoc [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/6e13d589d23800d27820a9f8e99405ca343577e2

--- automatic/protoc/tools/chocolateyinstall.ps1
+++ automatic/protoc/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   PackageName    = $Env:ChocolateyPackageName
-  FileFullPath   = "$toolsPath\protoc-3.12.2-win32.zip"
-  FileFullPath64 = "$toolsPath\protoc-3.12.2-win64.zip"
+  FileFullPath   = "$toolsPath\protoc-3.12.3-win32.zip"
+  FileFullPath64 = "$toolsPath\protoc-3.12.3-win64.zip"
   Destination    = $toolsPath
 }
 Get-ChocolateyUnzip @packageArgs
