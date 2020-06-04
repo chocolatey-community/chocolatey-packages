ae0c9d09b11a88cfbf631c48d288f9ce36a03dbb
Chocolatey <chocolatey@realdimensions.net>
2020-06-04 00:12:21 +0000

    AU: 6 updated - cutepdf Firefox smartftp thunderbird vim waterfox-classic [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/692351f90923d2de30a96dc820bce64f88e76967

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.0884_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.0884_x64.zip"
+  file          = "$toolsDir\gvim_8.2.0896_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.0896_x64.zip"
 }
 
 $installArgs = @{
