b5367aaba84a703578014caed59dbdbaf5b289c9
Chocolatey <chocolatey@realdimensions.net>
2020-06-08 00:11:39 +0000

    AU: 5 updated - cutepdf freecad keeweb poi vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/bb3aa205b27478629e23aa64dd98e9931fc830ba

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.0916_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.0916_x64.zip"
+  file          = "$toolsDir\gvim_8.2.0931_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.0931_x64.zip"
 }
 
 $installArgs = @{
