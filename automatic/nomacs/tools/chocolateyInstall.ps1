7ba75314ffc80b439d651c28760400484f9a9d4c
Manfred Wallner <schusterfredl@mwallner.net>
2020-06-09 08:38:02 +0200

    cleanup unused 32bit artifact handling

--- automatic/nomacs/tools/chocolateyInstall.ps1
+++ automatic/nomacs/tools/chocolateyInstall.ps1
@@ -1,11 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
+  throw "This package doesn't support 32bit architecture"
+}
+
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'nomacs'
   fileType       = 'msi'
-  file           = Get-Item $toolsPath\*-x86.msi
   file64         = Get-Item $toolsPath\*-x64.msi
   silentArgs     = '/quiet /norestart /log "{0}/setup.log"' -f (Get-PackageCacheLocation)
   validExitCodes = @(0)
