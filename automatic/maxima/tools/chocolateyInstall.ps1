ca2192e2cc2d9c8c5c3f46be15eeffa927ac7f8f
Chocolatey <chocolatey@realdimensions.net>
2020-06-09 00:12:18 +0000

    AU: 6 updated - dropbox maxima notepadplusplus notepadplusplus.commandline notepadplusplus.install vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2092e36e5ed1679f5c76ca1f1651658d63cf1b4a

--- automatic/maxima/tools/chocolateyInstall.ps1
+++ automatic/maxima/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.43.2-Windows/maxima-clisp-sbcl-5.43.2-win32.exe/download'
-  url64bit       = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.43.2-Windows/maxima-clisp-sbcl-5.43.2-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.44.0-Windows/maxima-clisp-sbcl-5.44.0-win32.exe/download'
+  url64bit       = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.44.0-Windows/maxima-clisp-sbcl-5.44.0-win64.exe/download'
   softwareName   = 'maxima*'
-  checksum       = 'b7b97f7b3cffc9ab79446dd898d22fccdbedddab38af63627ea3941342d0ca4d'
+  checksum       = 'f0d9cbb8b639d75f1972cdab070fe8670a5375c7110e0c3a89f9d1f426653c93'
   checksumType   = 'sha256'
-  checksum64     = '75e34f693f5158063e698071eca6555fb8125dfd1f4f1f271a5504a1fc48305c'
+  checksum64     = '14fca2a76d0307f8f5955cd43defe975fa55abd13a9aa32e854bf1b51c7d8af1'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
