37e24b8f54cef0062210066071bc8fee334f939e
Chocolatey <chocolatey@realdimensions.net>
2020-06-10 06:12:34 +0000

    AU: 6 updated - brave chromium dropbox keeweb php python3 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f58e188ef4d3bd88b5dc28d284b4e5eeaf820d16

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "85.0.4167.0-snapshots"
+$version = "85.0.4169.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
  if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '99.3.496'
+$version = '99.4.501'
 
 if (!(IsVersionAlreadyInstalled $version)) {
     $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) {$false} else {$true}
@@ -11,8 +11,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
     $packageArgs        = @{
         packageName     = $env:ChocolateyPackageName
         softwareName    = "Dropbox"
-        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2099.3.496%20Offline%20Installer.exe'
-        checksum        = '1c2fcc756be81e243954574e7522fd1e51d432a403c27c36b95eaf962880a6fa'
+        url             = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2099.4.501%20Offline%20Installer.exe'
+        checksum        = 'f8b83cc9b7172002f2767c53696ec8e1a84af21d4d19bd6d9151c03d4e2521ea'
         fileType        = 'exe'
         checksumType    = 'sha256'
         silentArgs      = '/s'
