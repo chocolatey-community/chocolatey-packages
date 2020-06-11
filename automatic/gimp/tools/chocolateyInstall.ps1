4416fe8a7c252a4fc7bbdc25bd487dc560979d0a
Chocolatey <chocolatey@realdimensions.net>
2020-06-11 18:13:02 +0000

    AU: 1 updated - gimp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4d307fe2a35473f95cd5fdb8ffbf94c0f4f1a046

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.18-setup-2.exe'
+  url            = 'https://download.gimp.org/mirror/pub/gimp/v2.10/windows/gimp-2.10.20-setup.exe'
   softwareName   = 'GIMP'
-  checksum       = 'bf6ce59587e63e1e8bab923e7dfdaa0f2c8bd3ac4d0ced602f3c063f9e32465c'
+  checksum       = 'de68eafd3bf88bb868e015e3613b7415fcfe94cc20946c0b64ccd37f94e22017'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /RESTARTEXITCODE=3010 /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
