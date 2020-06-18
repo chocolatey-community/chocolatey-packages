49d9dada55d7479645b9e786ec8bbc25f1b5bd59
Chocolatey <chocolatey@realdimensions.net>
2020-06-18 00:11:31 +0000

    AU: 2 updated - chromium vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/39aae7991f82a85c5e43ec6c99f4eccac52b6be3

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "85.0.4176.0-snapshots"
+$version = "83.0.4103.106"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.0992_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.0992_x64.zip"
+  file          = "$toolsDir\gvim_8.2.0998_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.0998_x64.zip"
 }
 
 $installArgs = @{
