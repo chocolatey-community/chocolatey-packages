e4dbfbd67fdb81f923f2f938c153f523d290c0ce
AdmiringWorm <kim.nordmo@gmail.com>
2020-06-24 10:16:26 +0200

    (itunesfusion) Add information about retired package

--- automatic/itunesfusion/update.ps1
+++ automatic/itunesfusion/update.ps1
@@ -2,17 +2,17 @@
 Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 function global:au_GetLatest {
-    $downloadEndPointUrl = 'https://www.binaryfortress.com/Data/Download/?package=itunesfusion&log=102'
-    $versionRegEx = 'iTunesFusionSetup-([0-9\.\-]+)\.exe'
+  $downloadEndPointUrl = 'https://www.binaryfortress.com/Data/Download/?package=retired_itunesfusion_retired&log=100'
+  $versionRegEx = 'iTunesFusionSetup-([0-9\.\-]+)\.exe'
 
-    $downloadUrl = Get-RedirectedUrl $downloadEndPointUrl
-    $versionInfo = $downloadUrl -match $versionRegEx
+  $downloadUrl = Get-RedirectedUrl $downloadEndPointUrl
+  $versionInfo = $downloadUrl -match $versionRegEx
 
-    if ($matches) {
-        $version = $matches[1]
-    }
+  if ($matches) {
+    $version = $matches[1]
+  }
 
-    return @{ Url32 = $downloadUrl; Version = $version }
+  return @{ Url32 = $downloadUrl; Version = $version }
 }
 
 function global:au_AfterUpdate {
@@ -20,13 +20,13 @@ function global:au_AfterUpdate {
 }
 
 function global:au_SearchReplace {
-    return @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.Url32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-        }
+  return @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.Url32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
+  }
 }
 
 Update -ChecksumFor 32
