7dba15bd62fcce564ad889e62af9f19e20d5ce46
Chocolatey <chocolatey@realdimensions.net>
2020-06-26 00:11:35 +0000

    AU: 3 updated - freecad mpc-hc-clsid2 vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/89e7785984cc589bf1b21d25e7d9ab2ef5f2369a

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.1052_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.1052_x64.zip"
+  file          = "$toolsDir\gvim_8.2.1058_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.1058_x64.zip"
 }
 
 $installArgs = @{
