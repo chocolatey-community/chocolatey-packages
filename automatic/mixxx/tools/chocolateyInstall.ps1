eb34f1142a94814bb82ebec49bfbe4d79f372be1
Chocolatey <chocolatey@realdimensions.net>
2020-06-26 18:11:51 +0000

    AU: 1 updated - mixxx [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0c4a2195def71adc05f228117272498e461966b8

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -5,14 +5,14 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://downloads.mixxx.org/mixxx-2.2.3/mixxx-2.2.3-win32.exe'
-  url64bit       = 'https://downloads.mixxx.org/mixxx-2.2.3/mixxx-2.2.3-win64.exe'
+  url            = 'https://downloads.mixxx.org/mixxx-2.2.4/mixxx-2.2.4-win32.exe'
+  url64bit       = 'https://downloads.mixxx.org/mixxx-2.2.4/mixxx-2.2.4-win64.exe'
 
   softwareName   = 'Mixxx *'
 
-  checksum       = '616143ceb66c3bd6f2ddb22f69533612feed4d28e3312a2b708fef908b6a8747014bab84c986a4562a4fe8cb2ee5c55428bb39c13232e84a498751751f5e7b38'
+  checksum       = 'af6fbce38e90a5b2f4e6d5a13912418cb461643432a74c90d292fdea5331b4527d026a11f4078e2775c943bdf5352a47297eaa351d7fe7e5eb1a3f10a554370e'
   checksumType   = 'sha512'
-  checksum64     = '32cf658920e15d4ad7603b943133da3d562b6ced67791a0eebd1fd78356a0c4763c78abbc82c95111a10eff9e7873dbd8e5dbbf85f05155e20f208953ec5a067'
+  checksum64     = 'ede500af569a8ac1197e0055db54e0d561613ffde8b013302a4f6f1eac3274524cf3ecca15002f5a892d01831aea3d2f932275c1d0c474f197ce72e531299b54'
   checksumType64 = 'sha512'
 
   silentArgs     = '/quiet'
