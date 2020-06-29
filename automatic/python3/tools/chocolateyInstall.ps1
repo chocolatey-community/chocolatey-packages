8e652152d2568fb60f50f10567d36af0596ac75c
Chocolatey <chocolatey@realdimensions.net>
2020-06-29 18:12:58 +0000

    AU: 2 updated - python3 yumi-uefi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/175f11cee8b9cab99cdbf6a70c71c6b91b62c63d

--- automatic/python3/tools/chocolateyInstall.ps1
+++ automatic/python3/tools/chocolateyInstall.ps1
@@ -17,8 +17,8 @@ else {
 $packageArgs = @{
   packageName    = 'python3'
   fileType       = 'exe'
-  file           = "$toolsPath\python-3.9.0b3.exe"
-  file64         = "$toolsPath\python-3.9.0b3-amd64.exe"
+  file           = "$toolsPath\python-3.7.8.exe"
+  file64         = "$toolsPath\python-3.7.8-amd64.exe"
   silentArgs     = '/quiet InstallAllUsers=1 PrependPath=1 TargetDir="{0}"' -f $installDir
   validExitCodes = @(0)
   softwareName   = 'Python*'
