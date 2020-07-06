0de7c8537753588d4094ee5947902c9ceb8ead52
Chocolatey <chocolatey@realdimensions.net>
2020-07-06 12:14:22 +0000

    AU: 2 updated - autohotkey.portable sonarlint-vs2015 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/6f48ed74321e0cc29b4a2765c5fd62d4fcb18fb4

--- automatic/autohotkey.portable/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$fileName  = 'AutoHotkey_2.0-a114-65fd90d8.zip'
+$fileName  = 'AutoHotkey_2.0-a115-f7c71ea8.zip'
 $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $zip_path = "$toolsPath\$fileName"
 Remove-Item $toolsPath\* -Recurse -Force -Exclude $fileName
--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-4.22.0.18519-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.23.0.19399-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
