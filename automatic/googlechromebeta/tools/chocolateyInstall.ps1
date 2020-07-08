f645dafd5bb95cc579a97cd36d9a0e48e40b93f8
Chocolatey <chocolatey@realdimensions.net>
2020-07-08 00:12:42 +0000

    AU: 3 updated - freecad googlechromebeta vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/aea3180de89087debc85269a821ea670acbf4bca

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '84.0.4147.68-beta'
+$version = '84.0.4147.85-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '244798e33379a09d0a4a6dd27ed2f2a8426d068d61109aaa29abc3cc69fa0400'
-  checksum64             = '096d2d905a8febe8ec231266cf16d0284f0f75b33b970f8de2a42267583cb654'
+  checksum               = 'b4dcfd8a0a3584eb677aa78c833ba6d7f9c6e2d2653f4b215d7aa3fa04dfba9b'
+  checksum64             = '3153ff5ca05e4a7f25a4fcc05e978e3672b228e5e3886e4b5e9676145470da05'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.1149_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.1149_x64.zip"
+  file          = "$toolsDir\gvim_8.2.1153_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.1153_x64.zip"
 }
 
 $installArgs = @{
