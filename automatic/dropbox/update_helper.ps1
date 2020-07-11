fd15d34cda297a752f437a7bd4e75b697c04071f
RB <RedBaron2@users.noreply.github.com>
2020-07-10 19:33:10 -0700

    (dropbox) regex self generating and spacing
    This will generate a up to date regex for the builds gotten from the forum. This is made possible from using Get-Version in update.

--- automatic/dropbox/update_helper.ps1
+++ automatic/dropbox/update_helper.ps1
@@ -1,29 +1,25 @@
 
 function drpbx-compare {
-	param( 
-[Parameter(Position = 0)][string]$_version, [string]$build )
-    if ($build -eq '') {$build = "Stable"}
+param( [Parameter(Position = 0)][string]$_version, [string]$build )
+    if ([string]::IsNullOrEmpty($build)) { $build = 'stable' }
     $releases = 'https://www.dropboxforum.com/t5/Desktop-client-builds/bd-p/101003016'
     $HTML = (Invoke-WebRequest -UseBasicParsing -Uri $releases).Links`
      | where {($_ -match $build)} | Select -First 6 | out-string
-    $re_dash = '-'; $re_dot = '.'; $re_non = ''; $re_build = $build + "-Build-";
-    $version = (drpbx-builds -hrefs $HTML -testVersion $_version);
-    if ( $version -eq $null ) { $version=$_version; }
+    $re_dash = '-'; $re_dot = '.'; $re_non = ''; $re_build = $build + "build";
+    $version = (  (drpbx-builds -hrefs $HTML -testVersion $_version) );
     return $version
 }
 
 function drpbx-builds {
-	param( [string]$default = '27.3.21', [string]$hrefs, [string]$testVersion )
+param( [string]$default = '27.3.21', [string]$hrefs, [string]$testVersion )
     $links = $hrefs -split ( '\/' ); $build = @(); $regex = ($re_build);
-    foreach($_ in $links) {
-        foreach($G in $_) {
-            if ($G -match '([\d]{2}[\-]{1}[\d]{1,2}[\-]{1}[\d]{2})') {
-                $G = $G -replace($regex,$re_non) -replace($re_dash,$re_dot) -replace('New.',$re_non);
-					if (($G -ge $default) -and ($G -ge $testVersion)) { $build += $G + ";"; }
-					}
-			if (($build | measure).Count -ge '6') { $build = ($build | measure -Maximum).Maximum; break; }
-            }
-        }
-	
-	return ($build -split(";") | select -First 1)
+    $Maj = ($vers.Major.ToString()).length; $Min = ($vers.Minor.ToString()).length;
+    $Bui = ($vers.build.ToString()).length;
+    foreach($_ in $links) { foreach($G in $_) {
+        if ($G -match "([\d]{$Maj}[\-]{1}[\d]{$Min}[\-]{1}[\d]{$Bui})") {
+            $G = $G -replace($regex,$re_non) -replace($re_dash,$re_dot) -replace('New.',$re_non);
+            if (($G -ge $default) -and ($G -ge $testVersion)) { $build += $G + ";" } }
+        if (($build | measure).Count -ge '6') { $build = ($build | measure -Maximum).Maximum; break; }
+    } }
+	return ($build | select -First 1)
 }
