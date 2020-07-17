81f915a0e3adb3d0b0be145026815e11a26dc613
Chocolatey <chocolatey@realdimensions.net>
2020-07-17 06:11:42 +0000

    AU: 3 updated - 360ts yumi yumi-uefi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4e8269fc349c2d9847c0d608f8a28a2db1a99158

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1049.exe'
-  checksum               = '06365e1433d90c601b19cef3e06b6b6c87fe392e9408a8fc127406d03d12646c'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1060.exe'
+  checksum               = 'e1703b40ce1eeaebe5d425263618e6bc85c9c956e111304004993bb59fa486e9'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
