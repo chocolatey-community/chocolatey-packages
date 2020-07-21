d4b46ae7a7e985ce760ff12ca1fbf28296d1070d
AdmiringWorm <kim.nordmo@gmail.com>
2020-07-21 13:17:00 +0200

    (graphviz) Update++

--- automatic/graphviz/update.ps1
+++ automatic/graphviz/update.ps1
@@ -1,28 +1,28 @@
-import-module au
+﻿import-module au
 
-$releases = 'https://graphviz.gitlab.io/_pages/Download/Download_windows.html'
+$releases = 'https://www2.graphviz.org/Packages/stable/windows/10/msbuild/Release/Win32/'
 
 function global:au_SearchReplace {
-   @{
-        ".\legal\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"     = "`${1} $($Latest.URL32)"
-          "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
-        }
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(\s+x32:).*"     = "`${1} $($Latest.URL32)"
+      "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
     }
+  }
 }
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re    = '\.msi$'
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $version  =  $url -split '-|\.msi' | select -Last 1 -Skip 1
-    @{
-        Version      = $version
-        URL32        = "https://graphviz.gitlab.io/_pages/Download/" + $url
-    }
+  $re = '\.msi$'
+  $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+  $version = $url -split '-' | select -Last 1 -Skip 1
+  @{
+    Version = $version
+    URL32   = $releases + $url
+  }
 }
 
 update -ChecksumFor none
