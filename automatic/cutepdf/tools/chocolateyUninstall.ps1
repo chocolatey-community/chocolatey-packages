ea48c271e988324c00ab381282ec8c768ddbc2d0
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-07-21 08:46:06 -0500

    (CutePDF) Add uninstall script and fix install exit code 1 (#1507)
    Adds a uninstalls script because the autouninstaller does not find a
    silent switch and is thus not silent.
    
    Fixes GH-1500 where the cutepdf installer fails with exit code 1 by
    adding code to uninstall old versions of cutepdf.

---
+++ automatic/cutepdf/tools/chocolateyUninstall.ps1
@@ -0,0 +1,26 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = 'cutepdf'
+  fileType      = 'exe'
+  silentArgs    = '/uninstall /s'
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+  softwareName  = 'cutepdf*'
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['file'] = "$($_.UninstallString.Split('/') | select -First 1)"	
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
