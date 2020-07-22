eccbe60dc20515bcccb6cbdd06b9dc3b7ecfd18e
Chocolatey <chocolatey@realdimensions.net>
2020-07-22 00:12:46 +0000

    AU: 6 updated - dropbox nodejs nodejs.install protoc smartftp vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/786ead4a92efe3ab684ce4debf72601903699652

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '101.4.434'
+$version = '102.4.431'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%20101.4.434%20Offline%20Installer.exe'
-    checksum       = '8e0d9ffe7114906f6c2d4385eb856dfde4e400da3fca57330323de219b3019ff'
+    url            = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%20102.4.431%20Offline%20Installer.exe'
+    checksum       = 'aaba4fc262088610637557a49a83c9db105d4e589b1d7956e7a15cfe479cfd3e'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
--- automatic/protoc/tools/chocolateyinstall.ps1
+++ automatic/protoc/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   PackageName    = $Env:ChocolateyPackageName
-  FileFullPath   = "$toolsPath\protoc-4.0.0-rc-1-win32.zip"
-  FileFullPath64 = "$toolsPath\protoc-4.0.0-rc-1-win64.zip"
+  FileFullPath   = "$toolsPath\protoc-4.0.0-rc-2-win32.zip"
+  FileFullPath64 = "$toolsPath\protoc-4.0.0-rc-2-win64.zip"
   Destination    = $toolsPath
 }
 Get-ChocolateyUnzip @packageArgs
