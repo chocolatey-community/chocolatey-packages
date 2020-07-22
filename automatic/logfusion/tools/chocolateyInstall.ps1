276fc75d518459ab8ae1ef176608908d7e4667d6
Chocolatey <chocolatey@realdimensions.net>
2020-07-22 18:12:17 +0000

    AU: 5 updated - clipboardfusion logfusion nodejs nodejs.install smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5943caaaf805e9fac32eef7ef804c8801ce74602

--- automatic/logfusion/tools/chocolateyInstall.ps1
+++ automatic/logfusion/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'LogFusion'
-    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/117/LogFusionSetup-6.4.exe'
-    checksum        = '8ce6c363092b5cd1c2e26c2197326ebc735d11a4f08d035d1c00666728530e1e'
+    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/117/LogFusionSetup-6.4.1.exe'
+    checksum        = 'f35d2c5002e006e5a43760f34136ecd6ddd3171159987db6a266c5fc0a124142'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/VERYSILENT /LAUNCHAFTER=0'
