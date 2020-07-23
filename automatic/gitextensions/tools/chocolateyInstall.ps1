379cdd76d42917f51ba5be3ff816b2c0a6478964
Chocolatey <chocolatey@realdimensions.net>
2020-07-23 07:24:00 +0000

    AU: 2 updated - gitextensions gom-player [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2b4cfa02b467a83daab0a108eae644f844e53207

--- automatic/gitextensions/tools/chocolateyInstall.ps1
+++ automatic/gitextensions/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = 'Git Extensions*'
     fileType       = 'msi'
-    file           = "$toolsDir\GitExtensions-3.4.2.9737-082cfd97d.msi"
+    file           = "$toolsDir\GitExtensions-3.4.3.9999-d4b0f48bb.msi"
     silentArgs     = '/quiet /norestart ADDDEFAULT=ALL REMOVE=AddToPath'
     validExitCodes = @(0, 3010, 1641)
 }
