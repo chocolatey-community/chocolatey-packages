e864b862364be1dfcb7948f8d1596644422a103d
Chocolatey <chocolatey@realdimensions.net>
2020-07-28 12:12:38 +0000

    AU: 4 updated - brave git Opera universal-usb-installer [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/755e0da391cb10b7b3134560600d9430c01f4177

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/69.0.3686.95/win/Opera_69.0.3686.95_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/69.0.3686.95/win/Opera_69.0.3686.95_Setup_x64.exe'
-  checksum       = 'f584219b3e87729e4565659c73eb47105b17c98213cc181e7f0137e6bd4cce8a'
-  checksum64     = '7b56d84cf8226bb5833811eb5f3a446272f4568849308fe76ca08b1b003b7d3d'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/70.0.3728.71/win/Opera_70.0.3728.71_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/70.0.3728.71/win/Opera_70.0.3728.71_Setup_x64.exe'
+  checksum       = '6fa316c444a8b5d2ba79af874d5b5f5fb76c2e1394f60ab87d740ee656618bf7'
+  checksum64     = 'c98fac6e9be38e057e731e10e64a28d827ef98652272e0c45c71cf6406a48296'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '69.0.3686.95'
+$version = '70.0.3728.71'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
