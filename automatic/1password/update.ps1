e7851715a1b2f5c292d8ffbc2fa5964e68d6472e
AdmiringWorm <kim.nordmo@gmail.com>
2020-08-04 02:18:28 +0200

    (1password) Correct quoting of silent arguments

--- automatic/1password/update.ps1
+++ automatic/1password/update.ps1
@@ -4,10 +4,10 @@ import-module $PSScriptRoot\..\..\extensions\chocolatey-core.extension\extension
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)^(\s*url\s*=\s*)'.*'"          = "`${1}'$($Latest.URL32)'"
-      "(?i)^(\s*checksum\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum32)'"
-      "(?i)^(\s*checksumType\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType32)'"
-      "(?i)^(\s*silentArgs\s*=\s*)'.*'"   = "`${1}'$($Latest.SilentArgs)'"
+      "(?i)^(\s*url\s*=\s*)'.*'"                = "`${1}'$($Latest.URL32)'"
+      "(?i)^(\s*checksum\s*=\s*)'.*'"           = "`${1}'$($Latest.Checksum32)'"
+      "(?i)^(\s*checksumType\s*=\s*)'.*'"       = "`${1}'$($Latest.ChecksumType32)'"
+      "(?i)^(\s*silentArgs\s*=\s*)['`"].*['`"]" = "`${1}`"$($Latest.SilentArgs)`""
     }
   }
 }
