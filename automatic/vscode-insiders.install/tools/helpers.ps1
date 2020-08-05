4329cf6d7bb40c69220265f0c1db1e4fb8eb6b90
Felipe Santos <felipecassiors@gmail.com>
2020-08-05 17:19:13 -0300

    (vscode-insiders.install) Warn user when closing

--- automatic/vscode-insiders.install/tools/helpers.ps1
+++ automatic/vscode-insiders.install/tools/helpers.ps1
@@ -1,6 +1,7 @@
 ﻿function Close-VSCodeInsiders {
   $processName = 'Code - Insiders'
   if (Get-Process $processName -ErrorAction SilentlyContinue) {
+    Write-Host "Closing $processName"
     Get-Process $processName -ErrorAction SilentlyContinue | ForEach-Object { $_.CloseMainWindow() | Out-Null }
     Start-Sleep 1
     Get-Process $processName -ErrorAction SilentlyContinue | Stop-Process  #in case gracefull shutdown did not succeed, try hard kill
