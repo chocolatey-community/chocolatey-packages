6879fbc3dec75a32af8a4b4f9966b58e7afba2a0
Felipe Santos <felipecassiors@gmail.com>
2020-08-05 17:15:28 -0300

    (vscoder-insiders.install) Prevent spending one or two seconds when not open

--- automatic/vscode-insiders.install/tools/helpers.ps1
+++ automatic/vscode-insiders.install/tools/helpers.ps1
@@ -1,5 +1,8 @@
 ﻿function Close-VSCodeInsiders {
-  Get-Process "Code - Insiders" -ea 0 | ForEach-Object { $_.CloseMainWindow() | Out-Null }
-  Start-Sleep 1
-  Get-Process "Code - Insiders" -ea 0 | Stop-Process  #in case gracefull shutdown did not succeed, try hard kill
+  $processName = 'Code - Insiders'
+  if (Get-Process $processName -ErrorAction SilentlyContinue) {
+    Get-Process $processName -ErrorAction SilentlyContinue | ForEach-Object { $_.CloseMainWindow() | Out-Null }
+    Start-Sleep 1
+    Get-Process $processName -ErrorAction SilentlyContinue | Stop-Process  #in case gracefull shutdown did not succeed, try hard kill
+  }
 }
