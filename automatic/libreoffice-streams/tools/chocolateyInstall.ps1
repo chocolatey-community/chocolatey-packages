8e64b44b42ee12b3f76e10e9ff55b364251b07cf
Chocolatey <chocolatey@realdimensions.net>
2020-08-14 12:14:32 +0000

    AU: 2 updated - chromium libreoffice-fresh [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a962b55b2ea8f23ac6f81e5fed20b53d1b385d63

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = 'libreoffice-fresh'
-  version                = '6.4.5'
+  version                = '6.4.6'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/6.4.5/win/x86/LibreOffice_6.4.5_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/6.4.5/win/x86_64/LibreOffice_6.4.5_Win_x64.msi'
-  checksum               = '617abddc16b329b9d2dcc38b2df7204856af2aef8f2f308367e320450fd2e126'
-  checksum64             = '3d80c1d8f3bf4540a6b845d43e54d3e6940058a9b2e5456234ac0fa39527ebcf'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/6.4.6/win/x86/LibreOffice_6.4.6_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/6.4.6/win/x86_64/LibreOffice_6.4.6_Win_x64.msi'
+  checksum               = '64c1e57096f034557724397cac4a329bd1d47c4347f3e531612b79739e77baaa'
+  checksum64             = 'ac0ec1a2f4d4138c90a7ff3af849560702147e13f7ed83334871280c6dbbce64'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
