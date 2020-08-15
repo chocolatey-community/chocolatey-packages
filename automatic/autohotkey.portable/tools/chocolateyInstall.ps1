1086bda619c426465b769e9300fab3dcae0f1022
Chocolatey <chocolatey@realdimensions.net>
2020-08-15 06:13:04 +0000

    AU: 2 updated - autohotkey.portable SQLite [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b1257b51076acbcc36f8a4af3997821c53ee0aab

--- automatic/autohotkey.portable/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$fileName  = 'AutoHotkey_2.0-a121-7e074bc4.zip'
+$fileName  = 'AutoHotkey_2.0-a122-f595abc2.zip'
 $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $zip_path = "$toolsPath\$fileName"
 Remove-Item $toolsPath\* -Recurse -Force -Exclude $fileName
