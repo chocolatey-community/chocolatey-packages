e1f60199ae42b698f2a3b0798f08e665910d0ed6
Chocolatey <chocolatey@realdimensions.net>
2020-08-17 12:13:48 +0000

    AU: 2 updated - chromium sonarlint-vs2015 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ffbaa0b2fd3d0e07cb3c1c0509590364d8c8da03

--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-4.23.0.19399-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.25.0.20544-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
