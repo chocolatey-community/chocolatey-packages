4e88447121eedac80ff2c0e87bca23b348419a01
AdmiringWorm <kim.nordmo@gmail.com>
2020-08-17 16:28:10 +0200

    (defraggler) Updated metadata urls

--- automatic/defraggler/update.ps1
+++ automatic/defraggler/update.ps1
@@ -22,7 +22,7 @@ function global:au_GetLatest {
   $re = '\.exe(\?[a-f\d]+)?$'
   $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 
-  $download_page = Invoke-WebRequest https://www.piriform.com/defraggler/version-history -UseBasicParsing
+  $download_page = Invoke-WebRequest https://www.ccleaner.com/defraggler/version-history -UseBasicParsing
   $Matches = $null
   $download_page.Content -match "\<h6\>v((?:[\d]\.)[\d\.]+)"
   $version = $Matches[1]
