a99647d5a8e4f101f869af4bff1e0b5b20944dce
AdmiringWorm <kim.nordmo@gmail.com>
2020-08-17 16:51:22 +0200

    (speccy) Updated metadata

--- automatic/speccy/update.ps1
+++ automatic/speccy/update.ps1
@@ -17,7 +17,7 @@ function global:au_GetLatest {
   $re = '\.exe(\?[a-f\d]+)?$'
   $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 
-  $download_page = Invoke-WebRequest https://www.piriform.com/speccy/version-history -UseBasicParsing
+  $download_page = Invoke-WebRequest https://www.ccleaner.com/speccy/version-history -UseBasicParsing
   $Matches = $null
   $download_page.Content -match "\<h6\>v((?:[\d]\.)[\d\.]+)"
   $version = $Matches[1]
