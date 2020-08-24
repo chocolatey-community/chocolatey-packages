5ae9607cf510d46216d910cdc9c2a4d16e6c98f1
Chocolatey <chocolatey@realdimensions.net>
2020-08-24 00:13:18 +0000

    AU: 2 updated - freecad vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e80e3dbaef462cd83f68879b13a1897cac1ea496

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.1512_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.1512_x64.zip"
+  file          = "$toolsDir\gvim_8.2.1520_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.1520_x64.zip"
 }
 
 $installArgs = @{
