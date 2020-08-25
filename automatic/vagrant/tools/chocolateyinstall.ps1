9e70cb73a97a3017dd1be0ae2eb11c24dfcca06d
Chocolatey <chocolatey@realdimensions.net>
2020-08-25 06:13:05 +0000

    AU: 2 updated - kitty vagrant [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3ab32b3f075469d5db52f0dea451d2854aebd3fd

--- automatic/vagrant/tools/chocolateyinstall.ps1
+++ automatic/vagrant/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName    = 'vagrant'
   fileType       = 'msi'
-  url            = 'https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_i686.msi'
-  url64bit       = 'https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.msi'
-  checksum       = '7b0e16043d906b6cafa8ae4f8748c8df7a8b6a2ea0e24bdfe0721e3ee2cb8daf'
-  checksum64     = '5d337db06a94870e01f8e8905455501b663268777e83783fe5d1f65e8543fb1a'
+  url            = 'https://releases.hashicorp.com/vagrant/2.2.10/vagrant_2.2.10_i686.msi'
+  url64bit       = 'https://releases.hashicorp.com/vagrant/2.2.10/vagrant_2.2.10_x86_64.msi'
+  checksum       = 'ecf9b202fb022f122eb67a324b0d052a612f2b5dcad6e72444298c02446be2d8'
+  checksum64     = '9e034ad6a1e0c81588e75f18e1911704091caf44e0cda96de492998c308573c3'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart"
