8bc9695c1798acd64af1f77ce38694e8f4174f7b
Chocolatey <chocolatey@realdimensions.net>
2020-08-31 18:14:39 +0000

    AU: 2 updated - blender Opera [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/acdb442d0d0a2ab44e6995dcd0d8ad4ebf7bb631

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/70.0.3728.133/win/Opera_70.0.3728.133_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/70.0.3728.133/win/Opera_70.0.3728.133_Setup_x64.exe'
-  checksum       = '6a72791443c0cc43dc0405e4a60992ac161a489e0767eaeccb5d1cff424896fa'
-  checksum64     = '66932d622fef349cbfce15276f14cbc063d24238fb804191c2d07e89013c81eb'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/70.0.3728.144/win/Opera_70.0.3728.144_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/70.0.3728.144/win/Opera_70.0.3728.144_Setup_x64.exe'
+  checksum       = 'd3fc0dc3cd35f5ccc8a54d32aaff3430ee52de578edf432165d08d89940ac38c'
+  checksum64     = '620f49b83b792598260335073aae96418a3787e54c397c59de6d2a855b8531e0'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '70.0.3728.133'
+$version = '70.0.3728.144'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
