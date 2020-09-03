0193ea1ea4e25878224a0b2a4fe150340dbcf04d
Chocolatey <chocolatey@realdimensions.net>
2020-09-03 18:14:26 +0000

    AU: 1 updated - gnupg [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4b652bf9e0b912d8aec202df0426dcf9e3f884cd

--- automatic/gnupg/tools/chocolateyInstall.ps1
+++ automatic/gnupg/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName    = $Env:ChocolateyPackageName
   fileType       = 'EXE'
   softwareName   = 'GNU Privacy Guard*'
-  file           = "$toolsDir\gnupg-w32-2.2.22_20200827.exe"
+  file           = "$toolsDir\gnupg-w32-2.2.23_20200903.exe"
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
