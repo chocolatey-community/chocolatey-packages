fadce49429055f2e4a3183ad6f78aa352ad43241
Chocolatey <chocolatey@realdimensions.net>
2020-09-09 18:14:39 +0000

    AU: 3 updated - chromium googlechromebeta kubernetes-cli [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9776d22affce5a3b5b054d59f039be2e12a62a8a

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "87.0.4260.0-snapshots"
+$version = "85.0.4183.102"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '86.0.4240.22-beta'
+$version = '86.0.4240.30-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = 'cb09d77ba2e3d1812c2a5470a61bd23bb069785b04e2bee02575de969b9b6a07'
-  checksum64             = 'd43fafb5457c229a3ade40a79c2ef6a0e99085ef64ba380c6e6cefea979bf286'
+  checksum               = 'ef3aa89f3748cf9b3b775eacd51279ee1b80fca83db3e3d4eb9fdcffb7e942e7'
+  checksum64             = '8325bea4a764d5bf73f0500e6b169c373abc5557a74bd394ea58a9f7db70cecb'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
