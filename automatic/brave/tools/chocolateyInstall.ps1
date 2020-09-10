81e8dfcc63e9efc51bb03f4c23bddee487c3948e
Chocolatey <chocolatey@realdimensions.net>
2020-09-10 12:15:41 +0000

    AU: 3 updated - brave chromium freecad [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e84d4480eabb73416a6e6135c5edb1172ad686b0

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\BraveBrowserSilentBetaSetup32.exe"
-  file64      = "$toolsPath\BraveBrowserSilentBetaSetup.exe"
+  file        = "$toolsPath\BraveBrowserStandaloneSilentSetup32.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.14.78'
+[version]$softwareVersion = '1.13.86'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
