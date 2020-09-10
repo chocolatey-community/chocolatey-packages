c84d8dc1d75b6b0f912c697ef22d386146c0669f
Chocolatey <chocolatey@realdimensions.net>
2020-09-10 18:12:47 +0000

    AU: 5 updated - Ghostscript Ghostscript.app mp3tag nodejs nodejs.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/64a8979fc9a57a73230593c6ecfbca9ac6996a82

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,7 +24,7 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv302setup.exe"
+  file           = "$toolsPath\mp3tagv302asetup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
