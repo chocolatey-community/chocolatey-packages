ca18ccddd4088946dbce091f0fb053f0fcdf1f98
Chocolatey <chocolatey@realdimensions.net>
2020-09-12 00:13:05 +0000

    AU: 2 updated - brave mattermost-desktop [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/77d27a51eb36d02b9be01f5c9de3c894d5589e5a

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.14.80'
+[version]$softwareVersion = '1.14.81'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
