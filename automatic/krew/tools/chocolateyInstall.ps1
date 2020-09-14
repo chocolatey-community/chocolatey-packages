247a123f85c53ebe945eac8b340eac1efd00192f
Joseph Petersen <josephp90@gmail.com>
2020-09-14 23:23:28 +0200

    (krew) add package for krew cli

---
+++ automatic/krew/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'krew'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  Name = $packageName
+  Path = "$toolsPath\$packageName.exe"
+}
+
+Install-BinFile @packageArgs
