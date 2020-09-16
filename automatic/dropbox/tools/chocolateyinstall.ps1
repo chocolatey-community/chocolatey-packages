9ce0c4555fca8150dfa51ff0971490c40e23f5fd
Chocolatey Community <chocolatey@realdimensions.net>
2020-09-16 00:13:26 +0000

    AU: 4 updated - dropbox nodejs nodejs.install vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4ad765ca1b4a97095e12daaf1f13c60320ee5c4f

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '106.3.349'
+$version = '106.3.351'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%20106.3.349%20Offline%20Installer.exe'
-    checksum       = '5f8b5e47a17fe1194e5d5e13c816a29aa41209ab168592371995039dd91d3f0c'
+    url            = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%20106.3.351%20Offline%20Installer.exe'
+    checksum       = 'c8211d2e261dfce6e2bad95f03366a314bc2a5df7dcca913d48eae0071a2b3b7'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
