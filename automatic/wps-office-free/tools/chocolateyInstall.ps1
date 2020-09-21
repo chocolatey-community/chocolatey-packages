a788a13e0aeaede538b9a0051e2404e9a149c293
Chocolatey Community <chocolatey@realdimensions.net>
2020-09-21 12:13:59 +0000

    AU: 1 updated - wps-office-free [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/267c1c720dea577c392c8a9c10d1de840593daf5

--- automatic/wps-office-free/tools/chocolateyInstall.ps1
+++ automatic/wps-office-free/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '11.2.0.9665'
+$version = '11.2.0.9684'
 $packageName = 'wps-office-free'
 $regName = $packageName -replace('\-',' ')
 $regName = $regName -replace('free','*')
@@ -8,11 +8,11 @@ $registry = Get-UninstallRegistryKey -SoftwareName $regName
 $packageArgs = @{
   packageName    = 'wps-office-free'
   fileType       = 'exe'
-  url            = 'http://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.9665/WPSOffice_11.2.0.9665.exe'
+  url            = 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.9684/WPSOffice_11.2.0.9684.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = $regName
-  checksum       = '12d7408f15b1df95766d8bd736a557c5f9201c642cef4d067c76a27fd23c855d'
+  checksum       = '9b0c16b758551a4c1bd5b2c84ef2fab3a7e12bd65411a7b27f32f06e6551d00c'
   checksumType   = 'sha256'
 }
 
