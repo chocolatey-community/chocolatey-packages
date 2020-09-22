b44b8966052eaccfc8a435f9064e8efeabe97302
Chocolatey Community <chocolatey@realdimensions.net>
2020-09-22 00:13:39 +0000

    AU: 2 updated - GoogleChrome vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3cf74a6bc9892f77a07b5f4e1ae96c92f265e968

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '85.0.4183.102'
+$version = '85.0.4183.121'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '5ac765973e517cf29740c580892d84f43c27d5813fa0b9416e108dc4dcbdf058'
-  checksum64             = 'ea90b2d7962b7698134199fde63bccb06165631a4051e3cf2c8d6624c26f3e59'
+  checksum               = '47f6db3115e7693ee42b37f41ca4078ab09f80f6bcdc4e33da79ff0ac9b550bc'
+  checksum64             = '43f1e6481a9f537d1591469124afb9f804c830c6d38af2820971947469dc2f3a'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
