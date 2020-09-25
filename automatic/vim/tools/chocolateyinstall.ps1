4df79bdb74974cc51e7e8cf5e3abf9a3c0d0d0d9
Chocolatey Community <chocolatey@realdimensions.net>
2020-09-25 00:13:51 +0000

    AU: 4 updated - thunderbird vim vscode vscode.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7007cbaab4471f2789bb8e75ba14e89af547b694

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.1736_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.1736_x64.zip"
+  file          = "$toolsDir\gvim_8.2.1738_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.1738_x64.zip"
 }
 
 $installArgs = @{
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.49.1'
+$version = '1.49.2'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/58bb7b2331731bf72587010e943852e13e6fd3cf/VSCodeSetup-ia32-1.49.1.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/58bb7b2331731bf72587010e943852e13e6fd3cf/VSCodeSetup-x64-1.49.1.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/e5e9e69aed6e1984f7499b7af85b3d05f9a6883a/VSCodeSetup-ia32-1.49.2.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/e5e9e69aed6e1984f7499b7af85b3d05f9a6883a/VSCodeSetup-x64-1.49.2.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = 'f62af086962328813e375c15f5c584df3a810fbac027f57c2a6680f08251fd10'
+  checksum       = '11e9194b59ef4bb1edd964acabba12b67e7f2114c2652949a6ed2062236f70c7'
   checksumType   = 'sha256'
-  checksum64     = '884241b8a4cbfc0684cd144f0794d06beb4ef415f095c87740eef307f853e354'
+  checksum64     = '0d49771b69477caa989ed4644d20ba26d8bc55c4cee12d0a694268eb0742dea8'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
