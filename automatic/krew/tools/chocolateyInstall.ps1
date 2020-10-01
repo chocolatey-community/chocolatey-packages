1deb974b614ecd59aeacbbf42f0f03d7aa224a7a
Joseph Petersen <josephp90@gmail.com>
2020-10-01 08:57:55 +0200

    (krew) add package for krew cli (#1537)
    * add krew.svg
    
    * (krew) add package for krew cli
    
    * fix version to 0.0.0
    
    * add `.krew\bin` to path
    
    * update README.md

---
+++ automatic/krew/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'krew'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+Install-BinFile $packageName "$toolsPath\$packageName.exe"
+Install-ChocolateyPath "%USERPROFILE%\.krew\bin" Machine
