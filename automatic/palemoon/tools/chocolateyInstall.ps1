b30b45fd78423a7e2fb0343a7588ac92897bf290
Chocolatey Community <chocolatey@realdimensions.net>
2020-10-02 12:13:43 +0000

    AU: 1 updated - palemoon [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/fc25690a443fc3b485dc1c9e5e912e20c5feaea5

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.14.1.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.14.1.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.14.2.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.14.2.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = 'c589bbbdeade7af663d6dcad3209dda80a8cbaa9bfd8ea74871ece6cf49e12e1'
+  checksum      = '1335fc7f0a329c3e8723041769fefb2583ecdd0c265f11cf0e1ee20792e61897'
   checksumType  = 'sha256'
-  checksum64    = 'b4200e5ba30c390a059e0278a4dbedbf26545f78fc9c9e739fd74a9dc097bf16'
+  checksum64    = '9c1ef72b7d04a37ec5ff01dfca8b62c35b3b4f52ea323d3486f3a1a835549f92'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
