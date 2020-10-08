030c7499e418dca3a562ace467b7782e1a1acb3b
Chocolatey Community <chocolatey@realdimensions.net>
2020-10-08 00:15:12 +0000

    AU: 5 updated - chromium mono nodejs nodejs.install vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7c5a25060aff6a8fdc14a274f47fbd39468cd35d

--- automatic/mono/tools/chocolateyInstall.ps1
+++ automatic/mono/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://download.mono-project.com/archive/6.10.0/windows-installer/mono-6.10.0.104-gtksharp-2.12.45-win32-0.msi'
-  url64bit       = 'https://download.mono-project.com/archive/6.10.0/windows-installer/mono-6.10.0.104-x64-0.msi'
+  url            = 'https://download.mono-project.com/archive/6.12.0/windows-installer/mono-6.12.0.98-gtksharp-2.12.45-win32-0.msi'
+  url64bit       = 'https://download.mono-project.com/archive/6.12.0/windows-installer/mono-6.12.0.98-x64-0.msi'
   softwareName   = 'Mono for Windows*'
-  checksum       = '8c681dbe0490da1a412c2d8674c16522927ee204707c34494c5538b4032465d5'
+  checksum       = '01fb215a26f462d39c7add813289f1d371834ed2e2aea2148aff57e4d2ec23a0'
   checksumType   = 'sha256'
-  checksum64     = '8c52108ac8cff1bd045601bdfbc3e93419b8a53f123c5cf4d2e597b7416b2fbf'
+  checksum64     = '37d1d9b4a5d0590ead7b9c1f9f72b9b795271aa3fa3b409679222731ce83f849'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
