0ccc8349a42d404646015ac841c30050bf006d5d
Chocolatey Community <chocolatey@realdimensions.net>
2020-10-12 18:14:45 +0000

    AU: 4 updated - Minikube smartftp sonarlint-vs2015 universal-usb-installer [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-4.26.0.22454-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.27.0.22695-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
