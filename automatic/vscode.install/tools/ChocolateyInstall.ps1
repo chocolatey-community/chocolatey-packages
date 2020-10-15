e9c967b9d36e0c8feded428f062fa1ceea4eea91
Chocolatey Community <chocolatey@realdimensions.net>
2020-10-15 00:13:17 +0000

    AU: 4 updated - universal-usb-installer vim vscode vscode.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.50.0'
+$version = '1.50.1'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/93c2f0fbf16c5a4b10e4d5f89737d9c2c25488a3/VSCodeSetup-ia32-1.50.0.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/93c2f0fbf16c5a4b10e4d5f89737d9c2c25488a3/VSCodeSetup-x64-1.50.0.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/d2e414d9e4239a252d1ab117bd7067f125afd80a/VSCodeSetup-ia32-1.50.1.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/d2e414d9e4239a252d1ab117bd7067f125afd80a/VSCodeSetup-x64-1.50.1.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = '9e512056dc120512c7802d603c38167a7d4ab0810afc30435f92d43f6775c48c'
+  checksum       = '1faa43c20c2b6a317de5f5fa20066d0edf77aca952e9f1f2c731b5d4f3aa60c9'
   checksumType   = 'sha256'
-  checksum64     = '44a354fbf8a6c70eca8ce255c60727d6e56edd044a0208a2aa676c8877dd5221'
+  checksum64     = '0180288d4a51497224997ce0491b1f7335747df3ce858ae9f73458f81f782156'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
