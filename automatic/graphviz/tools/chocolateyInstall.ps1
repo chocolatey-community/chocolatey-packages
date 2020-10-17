9c220bb1b15a38fc631f3f48af18a1ce2e7251d1
RB <RedBaron2@users.noreply.github.com>
2020-10-16 18:50:56 -0700

    setup for 32/64 bit builds

--- automatic/graphviz/tools/chocolateyInstall.ps1
+++ automatic/graphviz/tools/chocolateyInstall.ps1
@@ -1,1 +1,18 @@
+$ErrorActionPreference = 'Stop'
 
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$file = ''
+$file64 = ''
+
+$packageArgs = @{
+  packageName    = ''
+  fileType       = ''
+  file           = "$toolsPath\$file"
+  file64         = "$toolsPath\$file64"
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+  softwareName   = ''
+}
+
+Install-ChocolateyPackage @packageArgs
+Remove-Item $toolsPath\*.exe -ea 0
