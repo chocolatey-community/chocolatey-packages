f53bbfd605df563080cd12e687406847a9c56571
Chocolatey Community <chocolatey@realdimensions.net>
2020-10-19 00:13:03 +0000

    AU: 3 updated - qbittorrent vim wesnoth [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.15.5/wesnoth-1.15.5-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.15.6/wesnoth-1.15.6-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = '082c1acd50a0c0522aace1da0d399e152969f84538821910893e17e37ed41cb0'
+  checksum       = 'e96b8abd723572aac6d06140ed0b14d7f1b936537aad37d552212c7c46b72cf3'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
