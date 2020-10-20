4ced018d98ff1e9126d7a4c0cc37738f8f4f6b5f
RB <RedBaron2@users.noreply.github.com>
2020-10-20 09:56:57 -0700

    As per reviewer dot needs to be on the path

--- automatic/graphviz/tools/chocolateyInstall.ps1
+++ automatic/graphviz/tools/chocolateyInstall.ps1
@@ -16,3 +16,10 @@ $packageArgs = @{
 
 Install-ChocolateyPackage @packageArgs
 Remove-Item $toolsPath\*.exe -ea 0
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
+
+@('dot') |ForEach-Object {Install-BinFile $_ "$installLocation\bin\$_.exe"}
