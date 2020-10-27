26638704ca82d4bf1032eb88cbbc114ab39efe3d
Chocolatey Community <chocolatey@realdimensions.net>
2020-10-27 18:14:04 +0000

    AU: 6 updated - clipgrab Firefox nodejs nodejs.install palemoon php [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.14.2.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.14.2.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.15.0.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.15.0.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '1335fc7f0a329c3e8723041769fefb2583ecdd0c265f11cf0e1ee20792e61897'
+  checksum      = 'cdce26e2f8a4f63d621b76fa062715ebf4a311bb21c2eb8d93426bf75d7c0391'
   checksumType  = 'sha256'
-  checksum64    = '9c1ef72b7d04a37ec5ff01dfca8b62c35b3b4f52ea323d3486f3a1a835549f92'
+  checksum64    = '68cb58bb04997e1adb9679ce777474d00db882cf21e4aede84d404843c0e18f4'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
