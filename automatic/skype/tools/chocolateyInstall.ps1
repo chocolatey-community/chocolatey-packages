9b87607d4d238d0ef946dd4cc202dd9dd356eaee
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-02 18:14:41 +0000

    AU: 2 updated - displayfusion skype [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.65.0.78.exe'
-  checksum       = 'e516a7a80ed10c4e33e0886a814f8ec795abe5675781c2cb7a0fb4920c3cdaf8'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.66.0.74.exe'
+  checksum       = '8378fca75dda4b217df3b8390b2e42599e099687803ec22afda2c3cb7453b8c9'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
