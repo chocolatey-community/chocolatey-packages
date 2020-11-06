6e3db13a5e1d89670e551196f93205e81d9fa1e3
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-06 00:13:12 +0000

    AU: 5 updated - clipboardfusion displayfusion k9s mkdocs-material vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/mkdocs-material/tools/ChocolateyInstall.ps1
+++ automatic/mkdocs-material/tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿Update-SessionEnvironment
 
-$version = '6.1.2'
+$version = '6.1.3'
 
 $proxy = Get-EffectiveProxy
 if ($proxy) {
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.1955_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.1955_x64.zip"
+  file          = "$toolsDir\gvim_8.2.1961_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.1961_x64.zip"
 }
 
 $installArgs = @{
