3f488e39b2d3f45c5c38e96312dab5caa150968e
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-09 12:14:08 +0000

    AU: 3 updated - absolute-uninstaller chromium Opera [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/72.0.3815.207/win/Opera_72.0.3815.207_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/72.0.3815.207/win/Opera_72.0.3815.207_Setup_x64.exe'
-  checksum       = '3e981fbe6e2c5a38448892a01afd74ec03ad5747757ece9f7f6643361d9134b1'
-  checksum64     = '570f0376e50c5ac81bc21002ff6f61a1534b5f3fd5bfbbc0ca9e85bc2b4b489a'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/72.0.3815.211/win/Opera_72.0.3815.211_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/72.0.3815.211/win/Opera_72.0.3815.211_Setup_x64.exe'
+  checksum       = '3cab4a52917b7e8e45db3a3eeb535a780091e108312264ca9e4bc69d40e7d0e4'
+  checksum64     = '49015bd30bc6942f4952fca2383fe3394dfb4fe14ca3ce9ee83b1bd29b1d59ff'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '72.0.3815.207'
+$version = '72.0.3815.211'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
