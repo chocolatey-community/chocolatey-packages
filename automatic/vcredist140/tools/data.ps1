20307a89bb3edac1c91e83a88e941c7a2dce6ff4
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-11 00:18:01 +0000

    AU: 5 updated - julia nodejs nodejs.install tor-browser vcredist140 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/vcredist140/tools/data.ps1
+++ automatic/vcredist140/tools/data.ps1
@@ -1,12 +1,12 @@
 ﻿$installData32 = @{
-  Url = 'https://download.visualstudio.microsoft.com/download/pr/48431a06-59c5-4b63-a102-20b66a521863/CAA38FD474164A38AB47AC1755C8CCCA5CCFACFA9A874F62609E6439924E87EC/VC_redist.x86.exe'
-  Checksum = 'caa38fd474164a38ab47ac1755c8ccca5ccfacfa9a874f62609e6439924e87ec'
+  Url = 'https://download.visualstudio.microsoft.com/download/pr/8ecb9800-52fd-432d-83ee-d6e037e96cc2/50A3E92ADE4C2D8F310A2812D46322459104039B9DEADBD7FDD483B5C697C0C8/VC_redist.x86.exe'
+  Checksum = '50a3e92ade4c2d8f310a2812d46322459104039b9deadbd7fdd483b5c697c0c8'
   ChecksumType = 'sha256'
 }
 
 $installData64 = @{
-  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/48431a06-59c5-4b63-a102-20b66a521863/4B5890EB1AEFDF8DFA3234B5032147EB90F050C5758A80901B201AE969780107/VC_redist.x64.exe'
-  Checksum64 = '4b5890eb1aefdf8dfa3234b5032147eb90f050c5758a80901b201ae969780107'
+  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/89a3b9df-4a09-492e-8474-8f92c115c51d/B1A32C71A6B7D5978904FB223763263EA5A7EB23B2C44A0D60E90D234AD99178/VC_redist.x64.exe'
+  Checksum64 = 'b1a32c71a6b7d5978904fb223763263ea5a7eb23b2c44a0d60e90d234ad99178'
   ChecksumType64 = 'sha256'
 }
 
@@ -15,7 +15,7 @@ $uninstallData = @{
 }
 
 $otherData = @{
-  ThreePartVersion = [version]'14.27.29112'
+  ThreePartVersion = [version]'14.28.29325'
   FamilyRegistryKey = '14.0'
   PackageName = 'vcredist140'
 }
