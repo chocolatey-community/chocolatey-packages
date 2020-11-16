e020ef0ca980c3562131aff86822181fb86f5c0f
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-16 00:13:25 +0000

    AU: 3 updated - mp3tag picard vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.1980_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.1980_x64.zip"
+  file          = "$toolsDir\gvim_8.2.1992_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.1992_x64.zip"
 }
 
 $installArgs = @{
