24652b4efc5c25b2f0522969626367d09af84fbb
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-17 06:12:59 +0000

    AU: 2 updated - dropbox Minikube [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '110.3.425'
+$version = '110.4.458'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20110.3.425%20Offline%20Installer.exe'
-    checksum       = '926e8f2c6658ea35ae6bf9c16b727b083b2ac47525b6b21c6128e8cebdff8456'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20110.4.458%20Offline%20Installer.exe'
+    checksum       = '87d75dc5cf3d90fb4b04da21c2adfb79867b86cb4b1c437e974f8210d03dcf8a'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
