7f4c731f3fa7aa00997121404b57d1a9462a1fe3
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-20 00:14:09 +0000

    AU: 5 updated - dropbox freecad smartftp typescript yumi-uefi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '110.4.458'
+$version = '111.3.437'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20110.4.458%20Offline%20Installer.exe'
-    checksum       = '87d75dc5cf3d90fb4b04da21c2adfb79867b86cb4b1c437e974f8210d03dcf8a'
+    url            = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%20111.3.437%20Offline%20Installer.exe'
+    checksum       = 'bf28ee9e340f7fd07393dcda5921f623545d9a696fdfdd982a27ce8a03bb87a5'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
