296e1373cda3f1f5b9f229a52694dce56bec7018
Miodrag Milic <miodrag.milic@gmail.com>
2020-11-21 17:04:01 +0100

    (graphviz) refactor sloppy work

--- automatic/graphviz/update_helper.ps1
+++ automatic/graphviz/update_helper.ps1
@@ -1,23 +1,22 @@
-
 function Set-ReadMeFile {
-[CmdletBinding()]
-param(
-    [string]$file = "$pwd\README.tmp",
-    [string[]]$keys,
-    [string[]]$new_info
-)
-$me = ( $MyInvocation.MyCommand );
+    [CmdletBinding()]
+    param(
+        [string]$file = "$pwd\README.tmp.md",
+        [string[]]$keys,
+        [string[]]$new_info
+    )
 
-$keys = $keys.split(" |,")
-$new_info = $new_info.split(" |,")
-$data = Get-Content -Path $file; $i=0
-foreach( $item in $keys ) {
-$data = $data  -replace( "<$item>" , $($new_info[$i]) )
-$i++
-}
+    $me = ( $MyInvocation.MyCommand );
 
-Write-Verbose "$me data -$data-"
-$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
-[System.IO.File]::WriteAllLines("$pwd/README.md", $data, $Utf8NoBomEncoding)
+    $keys = $keys.split(" |,")
+    $new_info = $new_info.split(" |,")
+    $data = Get-Content -Path $file; $i=0
+    foreach( $item in $keys ) {
+        $data = $data  -replace( "<$item>" , $($new_info[$i]) )
+        $i++
+    }
 
+    Write-Verbose "$me data -$data-"
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+    [System.IO.File]::WriteAllLines("$pwd/README.md", $data, $Utf8NoBomEncoding)
 }
