7acc4b819f685e21119163957177957028a51128
RB <RedBaron2@users.noreply.github.com>
2020-11-21 06:25:39 -0800

    (graphviz) update++
    * deleted due to bad naming
    
    this repository uses only lowercase names
    
    * easier to delete than rename and update
    
    * file is AU prepared from tmp
    
    * update of files for new PR
    
    * updated spacing and missing bracket
    
    * forgot the dependency ...
    
    to the chocolatey-core.extension

--- automatic/graphviz/tools/chocolateyUninstall.ps1
+++ automatic/graphviz/tools/chocolateyUninstall.ps1
@@ -1,3 +1,18 @@
-﻿$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'Stop'
 
-@('dot','circo','sfdp','twopi') |ForEach-Object {Uninstall-BinFile $_}
+# All arguments for the Uninstallation of this package
+$packageArgs = @{
+  PackageName    = ''
+  FileType       = ''
+  SilentArgs     = "/S"
+  validExitCodes = @(0)
+}
+
+$name = ( $packageArgs.packageName -split "-" )[0]
+$registry = Get-UninstallRegistryKey -SoftwareName "$name*"
+$file = $registry.UninstallString
+$packageArgs.Add( "File", $file )
+# Now to Uninstall the Package
+Uninstall-ChocolateyPackage @packageArgs
+
+@('dot') |ForEach-Object {Install-BinFile $_ "$installLocation\bin\$_.exe"}
