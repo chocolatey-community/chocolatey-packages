8632b821ca416bfc52bc62bf5305a4e8526fcd60
Miodrag Milic <miodrag.milic@gmail.com>
2020-11-21 16:06:46 +0100

    (owncloud) update++
    Removed streams, added screenshot

--- automatic/owncloud-client/README.md
+++ automatic/owncloud-client/README.md
@@ -1,7 +1,14 @@
 # <img src="https://cdn.jsdelivr.net/gh/chocolatey-community/chocolatey-coreteampackages@edba4a5849ff756e767cba86641bea97ff5721fe/icons/owncloud.png" width="48" height="48"/> [owncloud-client](https://chocolatey.org/packages/owncloud-client)
 
-
 ownCloud gives you universal access to your files through a web interface or WebDAV. It also provides a platform to easily view & sync your contacts, calendars and bookmarks across all your devices and enables basic editing right on the web. Installation has minimal server requirements, doesn't need special permissions and is quick. ownCloud is extendable via a simple but powerful API for applications and plugins.
 
-This package installs the ownCloud Sync Client on your computer.
+## Features
 
+- File sync and share
+- Access to community apps
+- Access to community forum
+- Desktop & mobile apps
+- Self-managed, on-premises
+
+
+![screenshot](https://cdn.rawgit.com/chocolatey/chocolatey-coreteampackages/master/automatic/owncloud-client/screenshot.jpg)
--- automatic/owncloud-client/update.ps1
+++ automatic/owncloud-client/update.ps1
@@ -11,47 +11,27 @@ function global:au_SearchReplace {
   @{
     ".\legal\VERIFICATION.txt"      = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
-      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
+      "(?i)(^\s*checksum\:).*"            = "`${1} $($Latest.Checksum64)"
     }
     ".\tools\chocolateyInstall.ps1" = @{
       "(?i)^(\s*softwareName\s*=\s*)'.*'"       = "`${1}'$softwareName'"
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
-  }
-}
-
-function global:au_AfterUpdate {
-  Update-Metadata -data @{
-    title   = $Latest.Title
-    docsUrl = "https://doc.owncloud.org/desktop/$($Latest.Version.ToString(2))/"
   }
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re = 'ownCloud\-[.\d]+\.msi$'
-  $urls32 = $download_page.Links | ? href -match $re | select -expand href
+  $re = 'ownCloud-.+\.x64\.msi$'
+  $url = $download_page.Links | ? href -match $re | select -expand href
 
-  $streams = @{}
-  $urls32 | % {
-    $verRe = '[-]|(?:\.[a-z]{2}\-[a-z]{2})?\.msi$'
-    $version = $_ -split "$verRe" | select -last 1 -skip 1
-    $version = Get-Version $version
-    $kind = $_ -split '\/' | select -last 1 -skip 1
-
-    if (!($streams.ContainsKey($kind))) {
-      $streams.Add($kind, @{
-          Version = $version
-          URL32   = $_
-          Title   = if ($_ -match 'testing') { "ownCloud Windows Client (Technical Preview)" } else { "ownCloud Windows Client" }
-        })
-    }
+  @{
+    Version = $url -split "/" | select -last 1 -skip 2
+    URL64   = $url
   }
-
-  return @{ Streams = $streams }
 }
 
-update -ChecksumFor none -IncludeStream $IncludeStream -Force:$Force
+update -ChecksumFor none
