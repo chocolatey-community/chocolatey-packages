1c333f3b21471686ae263210afc88d77d5f2d239
Miodrag Milic <miodrag.milic@gmail.com>
2020-11-22 15:50:29 +0100

    simplified updater

--- automatic/partitionmasterfree/installer_download.ahk
+++ automatic/partitionmasterfree/installer_download.ahk
@@ -1,8 +1,15 @@
 #SingleInstance, force
+log = %TEMP%\chocolatey\au\partitionmagicfree_updater.log
+FileCreateDir, %TEMP%\chocolatey\au
+
+FileAppend, started script`n, %log%
 Run %1%
+FileAppend, started %1%`n, %log%
 WinWait, EPM_Installer
-Sleep 5000
+FileAppend, detected installer`n, %log%
+Sleep 3000
 WinActivate,
 MouseClick,,400,360
-Sleep 2000
+Sleep 1000
 MouseClick,,650,450
+FileAppend, finished script`n, %log%
--- automatic/partitionmasterfree/update.ps1
+++ automatic/partitionmasterfree/update.ps1
@@ -1,8 +1,6 @@
 ﻿import-module au
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
-Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$exec = "https://down.easeus.com/product/epm_free"
+$downloadUrl = "https://down.easeus.com/product/epm_free"
 
 function global:au_SearchReplace {
   @{
@@ -17,40 +15,32 @@ function global:au_SearchReplace {
   }
 }
 
-function GetResultInformation([string]$url32) {
+function global:au_GetLatest {
     Remove-Item $PSScriptRoot\tools\*.exe
 
-    $url32 = Get-RedirectedUrl $url32
-    $name = $url32 -split '/' | Select -Last 1
-    $dest = Join-Path "$PSScriptRoot\tools" $name
+    $downloaderPath = "$PSScriptRoot\tools\downloader.exe"
+    Invoke-WebRequest $downloadUrl -OutFile $downloaderPath
 
-    Get-WebFile $url32 $dest
-    & $PSScriptRoot\installer_download.ahk $dest
+    & $PSScriptRoot\installer_download.ahk $downloaderPath
     for ($i=0; $i -lt 60; $i++) {
-      Sleep 1
-      $installer = ls -Exclude $name tools\*.exe | select -First 1
+      Start-Sleep 1
+      $installer = ls -Exclude downloader.exe tools\*.exe | select -First 1
       if ($installer) { Get-Process EDownloader -ea 0 | kill; break }
     }
     if (!$installer) { throw "Can't download installer via AHK"}
-    Remove-Item $PSScriptRoot\tools\$name
+    Remove-Item $downloaderPath
 
-    $version = Get-Item $installer | % { $_.VersionInfo.ProductVersion }
+    $version = (Get-Item $installer).VersionInfo.ProductVersion
 
     $checksumType = 'sha256'
     @{
-        URL32          = $url32
+        URL32          = $downloadUrl
         Version        = Get-Version $version
         Checksum32     = Get-FileHash $installer -Algorithm $checksumType | % { $_.Hash.ToLowerInvariant() }
         ChecksumType32 = $checksumType
         PackageName    = 'PartitionMasterFree'
         FileName32     = $installer.Name
     }
-}
-
-function global:au_GetLatest {
-    Update-OnETagChanged -execUrl $exec -OnEtagChanged {
-        GetResultInformation $exec
-    } -OnUpdated { @{ URL32 = $exec } }
 }
 
 update -ChecksumFor none
