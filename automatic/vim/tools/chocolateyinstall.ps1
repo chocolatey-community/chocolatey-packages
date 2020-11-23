19678af4b29bb0ee272d83e05f9ffcd2e08ea8ca
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-23 00:13:15 +0000

    AU: 3 updated - clipgrab freecad vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2030_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2030_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2033_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2033_x64.zip"
 }
 
 $installArgs = @{
