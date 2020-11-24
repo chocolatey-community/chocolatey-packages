a788befe7c2e2ec2544e54e6ebac67d640e7ddb2
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-24 12:13:06 +0000

    AU: 2 updated - 360ts palemoon [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8c27cf65bbee390caaaf56879b7b7c4d809ba179

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1170.exe'
-  checksum               = '07e70ac687b856b175f4220dc3e5dc7e64ae384b6de5d3f52c822c95e4ba74ae'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1200.exe'
+  checksum               = 'd239f21a6fc17f93c77f26ac6492d94b2cddadab1e074fee749193bb882c84d3'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.15.0.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.15.0.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-28.16.0.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-28.16.0.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = 'cdce26e2f8a4f63d621b76fa062715ebf4a311bb21c2eb8d93426bf75d7c0391'
+  checksum      = '1116fd126532e0ef1dda6ed8ac00c20e968e41ca0b59dbdc657e5578bf9e7234'
   checksumType  = 'sha256'
-  checksum64    = '68cb58bb04997e1adb9679ce777474d00db882cf21e4aede84d404843c0e18f4'
+  checksum64    = '8d56e89088a5dc88fd39a3e759a290d97e857732b3081c87ff314f5e459452d5'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
