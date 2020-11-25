305cce11c28d7835ed3dbb01035ceab445b7f6fa
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-25 12:13:51 +0000

    AU: 2 updated - activepresenter sysinternals [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/3a10f68a50583d55b04549bec3b6445a40c29faa

--- automatic/sysinternals/tools/chocolateyInstall.ps1
+++ automatic/sysinternals/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ Write-Host "Sysinternals Suite is going to be installed in '$installDir'"
 $packageArgs = @{
   packageName    = 'sysinternals'
   url            = 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
-  checksum       = 'ac93286995ec1e881632797a781c2c01db9fb09f3e2a6beeee9831322f224e88'
+  checksum       = '8c016173b23c3006ae3c427582266121f0e643d0b539cf13a2cb0a67ca60a4ba'
   checksumType   = 'sha256'
   unzipLocation  = $installDir
 }
@@ -18,7 +18,7 @@ Accept-Eula
 if ($installDir -ne $toolsPath) { Install-ChocolateyPath $installDir }
 if (Is-NanoServer) {
   $packageArgs.url = 'https://download.sysinternals.com/files/SysinternalsSuite-Nano.zip'
-  $packageArgs.checksum = 'd130f432ff126e367edb1727fc001f6a2ca66660c62290a7317ae658c6341f5e'
+  $packageArgs.checksum = 'e933dc70dc8ba90b1cd3bd0f52b5019f8fdbeebe1c90870e143dc9948c67d178'
  }
 
 $old_path = 'c:\sysinternals'
