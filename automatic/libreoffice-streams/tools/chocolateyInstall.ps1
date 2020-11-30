7e0d450299e24378c1e39a718aac62c4453627b4
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-30 12:13:16 +0000

    AU: 2 updated - chromium libreoffice-still [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4f70cc417f29acb1cc5e3c22ea28f6d689e9663f

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = 'libreoffice-still'
-  version                = '6.4.7'
+  version                = '7.0.0'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/6.4.7/win/x86/LibreOffice_6.4.7_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/6.4.7/win/x86_64/LibreOffice_6.4.7_Win_x64.msi'
-  checksum               = 'd2ac8a5bb4febfce7b920c00a536c4df54109874e29923c2506105c59ce7fcb3'
-  checksum64             = 'c4701958d57458dbdfa6cc6c80ab92e545aec8b4f1a15e358f16ad7ca8937ca9'
+  url                    = 'https://downloadarchive.documentfoundation.org/libreoffice/old/7.0.0.3/win/x86/LibreOffice_7.0.0.3_Win_x86.msi'
+  url64bit               = 'https://downloadarchive.documentfoundation.org/libreoffice/old/7.0.0.3/win/x86_64/LibreOffice_7.0.0.3_Win_x64.msi'
+  checksum               = '6aa63e23906d466de9e1971f80e5b3dc5350cbcf0d8342eba4e0bfa62cf61b04'
+  checksum64             = '7e779fc8e0ec162fc3c6bbd96779ea7c4578e4ae43c2591516a24043752b78f6'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
