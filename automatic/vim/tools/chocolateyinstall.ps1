00d4448a82fc441c6071b237c0a3a5018d230888
Chocolatey Community <chocolatey@realdimensions.net>
2020-11-30 00:13:03 +0000

    AU: 2 updated - universal-usb-installer vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b029c39602fed351ca008a1883861843d7bf3236

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2067_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2067_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2069_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2069_x64.zip"
 }
 
 $installArgs = @{
