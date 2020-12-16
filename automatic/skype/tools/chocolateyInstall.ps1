d8048ef6fd5c36420f3913ecb4c158125519beb0
Chocolatey Community <chocolatey@realdimensions.net>
2020-12-16 18:13:59 +0000

    AU: 7 updated - cmake cmake.install cmake.portable Opera skype Waterfox yumi [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f4d52215dff009de7b286076a380061d9b93cddb

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.67.0.87.exe'
-  checksum       = '41921ea067f09c8a972f703317f077d95b1de2e17f2a7bf9f65ead631cd5c7ff'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.67.0.96.exe'
+  checksum       = '158ac7a0a9db06be9a1849d2a29d57372c19f98b416de848911d640f81329079'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
