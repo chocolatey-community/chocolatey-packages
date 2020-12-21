2a7b332686abb8872b7919f55ac8afaac33a7150
Chocolatey Community <chocolatey@realdimensions.net>
2020-12-21 12:14:54 +0000

    AU: 3 updated - skype wps-office-free zotero [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/640123ff4e5504dd4d3377f627999ffb8cb8297b

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.67.0.96.exe'
-  checksum       = '158ac7a0a9db06be9a1849d2a29d57372c19f98b416de848911d640f81329079'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.67.0.97.exe'
+  checksum       = '3952e0bc6a0a05dfed51563c7dc7f3f4b8caf30804ed61e4496731fd2b774ce2'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
--- automatic/wps-office-free/tools/chocolateyInstall.ps1
+++ automatic/wps-office-free/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '11.2.0.9747'
+$version = '11.2.0.9906'
 $packageName = 'wps-office-free'
 $regName = $packageName -replace('\-',' ')
 $regName = $regName -replace('free','*')
@@ -8,11 +8,11 @@ $registry = Get-UninstallRegistryKey -SoftwareName $regName
 $packageArgs = @{
   packageName    = 'wps-office-free'
   fileType       = 'exe'
-  url            = 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.9747/WPSOffice_11.2.0.9747.exe'
+  url            = 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.9906/WPSOffice_11.2.0.9906.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = $regName
-  checksum       = 'e1168c57249230cd9e93ef0cf22783b6105fccf3b6ae86b91e82b20803b667fb'
+  checksum       = '7bafc81db43749393f4b9352faa432f948f708bd04dee032f764853b3f822ec9'
   checksumType   = 'sha256'
 }
 
