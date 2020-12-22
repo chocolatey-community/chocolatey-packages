d10c3ee8838b5c4060f8e2e9a15d9c9535fb0122
Chocolatey Community <chocolatey@realdimensions.net>
2020-12-22 00:13:30 +0000

    AU: 8 updated - chromium dropbox gnupg owncloud-client python python3 vim youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ba3ba372beac435e0808601e41cbac2efb372722

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '112.4.321'
+$version = '113.3.427'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20112.4.321%20Offline%20Installer.exe'
-    checksum       = 'c7c062797391e48db06a11e38692b98f6ea6edb56ab23ed107bd6f890207dcaf'
+    url            = 'https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%20113.3.427%20Offline%20Installer.exe'
+    checksum       = '3c8069079ebc9ef09997c02e9f1d3da74f0f025e6c7a401bb1adbf4414bbcdfc'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
