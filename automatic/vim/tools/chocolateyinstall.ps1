9fd4cf847d444b5a996ed56d9609123ef6839077
Chocolatey Community <chocolatey@realdimensions.net>
2020-12-29 00:13:18 +0000

    AU: 6 updated - git git.install git.portable kitty vim youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/96bc5407fdff6e325f17b005854f3463781bae17

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2232_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2232_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2241_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2241_x64.zip"
 }
 
 $installArgs = @{
