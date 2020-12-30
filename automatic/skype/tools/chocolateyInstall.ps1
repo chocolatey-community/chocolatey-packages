330b2017af3a43895b53bd50974a36467428b814
Chocolatey Community <chocolatey@realdimensions.net>
2020-12-30 18:13:04 +0000

    AU: 3 updated - keeweb skype smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5aa0306ff6b11bf937dfc07c01a3c3872422adb1

--- automatic/skype/tools/chocolateyInstall.ps1
+++ automatic/skype/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'Skype*'
-  url            = 'https://download.skype.com/s4l/download/win/Skype-8.67.0.97.exe'
-  checksum       = '3952e0bc6a0a05dfed51563c7dc7f3f4b8caf30804ed61e4496731fd2b774ce2'
+  url            = 'https://download.skype.com/s4l/download/win/Skype-8.67.0.99.exe'
+  checksum       = '302b803ee59da2c30005d57082ba3c3f826b95196e427517c4526299e22e3caf'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
