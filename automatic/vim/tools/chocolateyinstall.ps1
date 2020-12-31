7e7915657dc664102bcd476e3d653b3d7b78ee22
Chocolatey Community <chocolatey@realdimensions.net>
2020-12-31 00:13:12 +0000

    AU: 3 updated - smartftp vim youtube-dl [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/cbbf4dc52af981992dacb3a74fb4b35e6eb4b571

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2246_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2246_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2251_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2251_x64.zip"
 }
 
 $installArgs = @{
