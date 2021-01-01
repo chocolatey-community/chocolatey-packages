36a03b8f8c55215b311fab0178e3e8ec6bb636d5
Chocolatey Community <chocolatey@realdimensions.net>
2021-01-01 12:13:23 +0000

    AU: 2 updated - chromium freeciv [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b40e86413d9aa42db87d5149a3be32d4c5349c87

--- automatic/freeciv/tools/chocolateyInstall.ps1
+++ automatic/freeciv/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'Freeciv*'
 
-  checksum       = '194c140fe7ac016af236b5073495b5789b992572811c6a43ea42b80cf430818a'
+  checksum       = '56643d8e044f32d086d9dd613b53d85d23fff3129621ef976c062d77df2f0009'
   checksumType   = 'sha256'
-  url            = 'http://files.freeciv.org/packages/windows/Freeciv-2.6.2.1-win32-gtk3-setup.exe'
+  url            = 'http://files.freeciv.org/packages/windows/Freeciv-2.6.3-win32-gtk3-setup.exe'
 
   silentArgs     = '/S'
   validExitCodes = @(0)
