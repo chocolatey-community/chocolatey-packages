b559c48631886ed586a26a86a9dba2dad2d5581e
AdmiringWorm <kim.nordmo@gmail.com>
2021-01-12 13:17:51 +0100

    (kitty) Updated to use uncompressed edition of kitty
    fixes #1463

--- automatic/kitty/update.ps1
+++ automatic/kitty/update.ps1
@@ -4,10 +4,14 @@ $releases = 'https://github.com/cyd01/KiTTY/releases/latest'
 $padVersionUnder = '0.71.1'
 
 function global:au_SearchReplace {
-   @{ }
+  @{ }
 }
 
 function downloadFile($url, $filename) {
+  if ($filename.EndsWith("_nocompress.exe")) {
+    $filename = $filename -replace "_nocompress\.exe$", ".exe"
+  }
+
   $filePath = "$PSScriptRoot\tools\$filename"
   Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $filePath | Out-Null
   return Get-FileHash $filePath -Algorithm SHA256 | % Hash
@@ -20,32 +24,38 @@ function global:au_BeforeUpdate {
   $Latest.GetEnumerator() | ? { $_.Key.StartsWith("URL") } | % {
     $fileName = $_.Key.Substring(3)
     $checksum = downloadFile $_.Value $fileName
-    $value = [string]::Format("| {0,-12} | {1} | {2,-71} |", $fileName, $checksum, $_.Value)
+    $value = [string]::Format("| {0,-20} | {1} | {2,-79} |", $fileName, $checksum, $_.Value)
     $sb.AppendLine($value) | Out-Null
   }
-  $value = "".PadLeft(155,'-')
+  $value = "".PadLeft(171, '-')
   $sb.Append("|$value|") | Out-Null
 
   $sb.ToString() | Out-file -Encoding utf8 -FilePath "$PSScriptRoot\legal\VERIFICATION.txt"
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
-    $allFiles = $download_page.Links | ? { $_.href -match "\.exe$" -and $_.href -NotMatch "nocompress|portable" } | % href
+  [array]$uncompressedFiles = $download_page.Links | ? { $_.href -match "_nocompress\.exe" } | % href
+  $allFiles = ($download_page.Links | ? { $_.href -match "\.exe$" -and $_.href -notmatch "nocompress|portable" } | ? {
+      $name = (Split-Path -Leaf $_.href) -replace "\.exe$", "_nocompress.exe"
+      !($uncompressedFiles | ? { $_.EndsWith($name) })
+    } | % href) + $uncompressedFiles
 
-    $version = ($allFiles | select -First 1) -split "\/v?" | select -Last 1 -Skip 1
+  #$allFiles = $download_page.Links | ? { $_.href -match "\.exe$" -and $_.href -NotMatch "nocompress|portable" } | % href
 
-    $result = @{
-      Version = Get-FixVersion $version -OnlyFixBelowVersion $padVersionUnder
-    }
+  $version = ($allFiles | select -First 1) -split "\/v?" | select -Last 1 -Skip 1
 
-    $allFiles | % {
-      $fileName = $_ -split "\/" | select -Last 1
-      $result["URL" + $fileName] = New-Object uri([uri]$releases, $_)
-    }
+  $result = @{
+    Version = Get-FixVersion $version -OnlyFixBelowVersion $padVersionUnder
+  }
 
-    return $result
+  $allFiles | % {
+    $fileName = $_ -split "\/" | select -Last 1
+    $result["URL" + $fileName] = New-Object uri([uri]$releases, $_)
+  }
+
+  return $result
 }
 
 update -ChecksumFor none
