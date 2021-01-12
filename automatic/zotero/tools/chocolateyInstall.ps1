c26ffbb80265636c128def28bb2a21626bf23533
Chocolatey Community <chocolatey@realdimensions.net>
2021-01-12 00:13:24 +0000

    AU: 4 updated - gnupg kitty vim zotero [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/265a0233fe035d0c2522d74994c26eebcf248101

--- automatic/zotero/tools/chocolateyInstall.ps1
+++ automatic/zotero/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero'
-  file           = "$toolsPath\Zotero-5.0.94_setup.exe"
+  file           = "$toolsPath\Zotero-5.0.95_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
