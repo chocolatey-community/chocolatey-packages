8e2f8feff0d068aaf8ce97beaa459c261f39cc0c
Chocolatey Community <chocolatey@realdimensions.net>
2021-01-20 18:13:35 +0000

    AU: 3 updated - blender chromium sonarlint-vs2015 [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "90.0.4395.0-snapshots"
+$version = "88.0.4324.96"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
