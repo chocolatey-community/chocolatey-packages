f2be915fcbe7f91d9978c0078da2eca468e3849c
Chocolatey Community <chocolatey@realdimensions.net>
2021-01-25 00:13:01 +0000

    AU: 2 updated - paint.net vim [skip ci]

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2398_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2398_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2406_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2406_x64.zip"
 }
 
 $installArgs = @{
