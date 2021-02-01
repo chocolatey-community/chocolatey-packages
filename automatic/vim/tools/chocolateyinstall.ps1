91cd2c804ebb8c98a6874271963e18132202ce54
Chocolatey Community <chocolatey@realdimensions.net>
2021-02-01 00:13:11 +0000

    AU: 2 updated - vim Waterfox [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/27913e6a38572dd188b34f8a46d98aca05e208fd

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2434_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2434_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2445_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2445_x64.zip"
 }
 
 $installArgs = @{
