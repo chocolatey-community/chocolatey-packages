8de135a0df68fdd5ef18d37e94e95047f8c16fa3
Chocolatey Community <chocolatey@realdimensions.net>
2021-02-09 18:14:12 +0000

    AU: 10 updated - Firefox git git.install git.portable nodejs nodejs.install Opera owncloud-client vscode vscode.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/bec07aec396ca06979c29b484545722eb336db44

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/74.0.3911.75/win/Opera_74.0.3911.75_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/74.0.3911.75/win/Opera_74.0.3911.75_Setup_x64.exe'
-  checksum       = 'f9c889f29405b75288a54fd7b1a43389d2c96093a753723bf00e9a5513ebb48d'
-  checksum64     = 'fa8ec74a29175415ba729281102b94942255eeb42d5c31a6106dfe7353bdb5ab'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/74.0.3911.107/win/Opera_74.0.3911.107_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/74.0.3911.107/win/Opera_74.0.3911.107_Setup_x64.exe'
+  checksum       = '068ade8862ed52c3d1957d55301d245a04576128397a0588131b02d124188126'
+  checksum64     = '4d2834d602639033d1955b2a6ea1b8cfb9c833a119fdc3f6702c6073f81f66cd'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '74.0.3911.75'
+$version = '74.0.3911.107'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.53.0'
+$version = '1.53.1'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/8490d3dde47c57ba65ec40dd192d014fd2113496/VSCodeSetup-ia32-1.53.0.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/8490d3dde47c57ba65ec40dd192d014fd2113496/VSCodeSetup-x64-1.53.0.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/5d424b828ada08e1eb9f95d6cb41120234ef57c7/VSCodeSetup-ia32-1.53.1.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/5d424b828ada08e1eb9f95d6cb41120234ef57c7/VSCodeSetup-x64-1.53.1.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = '64b3d4c07d89dac6af167f0224aa6853fd0d6f4f3d5bbf78cc6d9d6825ba3f79'
+  checksum       = '62398285c37a0ad1caa8f721ae8388c2d682edb8c33674fd430d10333b53d9fb'
   checksumType   = 'sha256'
-  checksum64     = 'd20c9bc29804a16c7de1c80c71798dc10d3899f7a19f426527d8020ce2db1357'
+  checksum64     = '9ac575795e1708c386969f6fee5de9b4ca931185d254bbbd316bd3e0837de66e'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
