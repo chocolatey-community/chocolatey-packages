a3203422010879f497e81582b2d6a908336a345c
Chocolatey Community <chocolatey@realdimensions.net>
2021-02-13 20:42:09 +0000

    AU: 6 updated - 360ts codeblocks gobby graphviz lightworks maxthon.commandline [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8e79f0c63a7db6782aab1c9ff59bbfebe776eb7d

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1213.exe'
-  checksum               = '54a4cae6d8a9e4e53f2ee847c615f32fa22d1ad3c0f7eb7e67509a6b21370773'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1262.exe'
+  checksum               = '8e425204b79b672e21ed16922c1a7e5bb331204aa4763d7952f848f0204c8e68'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/maxthon.commandline/tools/chocolateyInstall.ps1
+++ automatic/maxthon.commandline/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  url          = 'https://dl.maxthon.com/mx6/maxthon_portable_6.1.0.2000_x86.7z'
-  checksum     = '76d1de0cdd0e4a926153b85f980a4e7dfea7c128f316df6d13265abad4475117'
+  url          = 'https://dl.maxthon.com/mx6/maxthon_portable_6.1.1.1000_x86.zip'
+  checksum     = '9bd24c106adeee7936866d8ccc463fa27e4d34e3f815915a0c3299d1de29875a'
   checksumType = 'sha256'
   destination  = $toolsDir
 }
