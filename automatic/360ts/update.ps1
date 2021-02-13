6ae6cff4edea46df1a1b2dd4480f5ac09193ed46
AdmiringWorm <kim.nordmo@gmail.com>
2021-02-13 20:04:22 +0100

    (360ts) Remove TSE edition of the package
    This software edition has been removed from the
    website, and thus can not be updated anymore.

--- automatic/360ts/update.ps1
+++ automatic/360ts/update.ps1
@@ -25,35 +25,37 @@ function global:au_SearchReplace {
 }
 
 function Get360Version {
-param(
+  param(
     [string]$releases,
-	[string]$PackageName,
-	[string]$Title
-)
-$regex = '(\d+\.\d+\.\d+\.\d+)|( Beta)'
-$HTML = Invoke-WebRequest -UseBasicParsing -Uri $releases
-( $HTML | ForEach-Object { ($_ -match $regex )} ) | select -First 2 | Out-Null
-$version = $Matches[0];
-if ( $Matches[2] -ne $null ) {
-$version = $version + $Matches[2]
-$version -replace('','_')
-}
-$url = "https://free.360totalsecurity.com/totalsecurity/${PackageName}_Setup_${version}.exe"
+    [string]$PackageName,
+    [string]$Title
+  )
 
- @{
+  $regex = '(\d+\.\d+\.\d+\.\d+)|( Beta)'
+  $HTML = Invoke-WebRequest -UseBasicParsing -Uri $releases
+  ( $HTML | ForEach-Object { ($_ -match $regex )} ) | select -First 2 | Out-Null
+  $version = $Matches[0];
+  if ( $Matches[2] -ne $null ) {
+    $version = $version + $Matches[2]
+    $version -replace('','_')
+  }
+
+  $url = "https://free.360totalsecurity.com/totalsecurity/${PackageName}_Setup_${version}.exe"
+
+  @{
     PackageName = $PackageName.ToLower()
     Title = $Title
     Version = $version -replace('_','-')
     URL32   = $url
   }
 }
+
 $360_ts_url = 'https://www.360totalsecurity.com/en/version/360-total-security/'
 $360_tse_url = 'https://www.360totalsecurity.com/en/version/360-total-security-essential/'
 
-
 function global:au_GetLatest {
   $streams = [ordered] @{
-    tse = Get360Version -releases $360_tse_URL -PackageName "360TSE" -Title "360 Total Security Essential"
+    #tse = Get360Version -releases $360_tse_URL -PackageName "360TSE" -Title "360 Total Security Essential" # Software removed from website, kept here as a historic reference
     ts = Get360Version -releases $360_ts_URL -PackageName "360TS" -Title "360 Total Security"
   }
 
