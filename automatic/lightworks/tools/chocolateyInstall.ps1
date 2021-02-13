9e25ae11012d91aa7401f994c2496cb6af5e6cd9
AdmiringWorm <kim.nordmo@gmail.com>
2021-02-13 21:15:32 +0100

    (lightworks) Updated to only support 64bit edition

--- automatic/lightworks/tools/chocolateyInstall.ps1
+++ automatic/lightworks/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName            = 'lightworks'
   fileType               = 'EXE'
-  url                    = 'https://cdn.lwks.com/releases/2020.1.1/lightworks_2020.1.1_r124942_32bit_setup.exe'
+  url                    = ''
   url64bit               = 'https://cdn.lwks.com/releases/2020.1.1/lightworks_2020.1.1_r124942_64bit_setup.exe'
-  checksum               = '9a53e7c8211922a7abb6f38de865f9c54b2b51380063ccbabda5f62fada986b8'
+  checksum               = ''
   checksum64             = '27d1f7ef922638a62fea38c7a1918d51a0e75c7408ab213835e1f09016f51b66'
-  checksumType           = 'sha256'
+  checksumType           = ''
   checksumType64         = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/lightworks/update.ps1
+++ automatic/lightworks/update.ps1
@@ -6,9 +6,7 @@ $releases = "$domain/index.php?option=com_lwks&view=download&Itemid=206&tab=0"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
       "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
       "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
   }
@@ -26,16 +24,15 @@ function global:au_GetLatest {
   [System.Net.ServicePointManager]::SecurityProtocol = 'Ssl3,Tls,Tls11,Tls12' #https://github.com/chocolatey/chocolatey-coreteampackages/issues/366
   $download_page = Invoke-WebRequest $releases -UseBasicParsing
 
-  $url32 = Get-UrlFromPage ($download_page.Links | ? href -match "win_public_32" | % { $domain + $_.href })
-  $url64 = Get-UrlFromPage ($download_page.Links | ? href -match "win_public_64" | % { $domain + $_.href })
+  #$url32 = Get-UrlFromPage ($download_page.Links | ? href -match "win_public_32" | % { [uri]::new($domain, $_.href) })
+  $url64 = Get-UrlFromPage ($download_page.Links | ? href -match "win_public_64" | select -first 1 | % { $domain + $_.href })
 
   $version = $url64 -split '_' | select -last 1 -Skip 3
 
   @{
     Version = $version
-    URL32   = $url32
     URL64   = $url64
   }
 }
 
-update
+update -checksumFor 64
