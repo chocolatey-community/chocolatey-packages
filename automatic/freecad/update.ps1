697b8459fda0583b5db5fd260f51c79dbe7158f8
RB <RedBaron2@users.noreply.github.com>
2021-02-17 16:51:52 -0800

    update to cleanup the blanks in the install file

--- automatic/freecad/update.ps1
+++ automatic/freecad/update.ps1
@@ -1,4 +1,4 @@
-﻿import-module au
+import-module au
 
 if (!$PSScriptRoot) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
 . "$PSScriptRoot\update_helper.ps1"
@@ -8,29 +8,52 @@ $releases = "$PreUrl/FreeCAD/FreeCAD/releases"
 $softwareName = 'FreeCAD'
 
 function global:au_SearchReplace {
-  @{
-    ".\tools\chocolateyInstall.ps1"   = @{
-      "(?i)(^\s*packageName\s*=\s*)'.*'"      = "`$1'$($Latest.PackageName)'"
-      "(?i)(^\s*fileType\s*=\s*)('.*')"       = "`$1'$($Latest.fileType)'"
-      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
-      "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
-      "(?i)(^\s*softwareName\s*=\s*)'.*'"     = "`$1'$($softwareName)'"
+    if ( [string]::IsNullOrEmpty($Latest.URL32) ) {
+        @{
+            ".\tools\chocolateyInstall.ps1"   = @{
+                "(?i)(^\s*packageName\s*=\s*)'.*'"                 = "`$1'$($Latest.PackageName)'"
+                "(?i)(^\s*fileType\s*=\s*)('.*')"                  = "`$1'$($Latest.fileType)'"
+                "(?i)(^\s*url64\s*=\s*)('.*')"                     = "`$1'$($Latest.URL64)'"
+                "(?i)(^\s*checksum64\s*=\s*)('.*')"                = "`$1'$($Latest.Checksum64)'"
+                "(?i)(^\s*checksumType64\s*=\s*)('.*')"            = "`$1'$($Latest.ChecksumType64)'"
+                "(?i)(^\s*softwareName\s*=\s*)'.*'"                = "`$1'$($softwareName)'"
+            }
+            ".\freecad.nuspec"                = @{
+                "(?i)(^\s*\<id\>).*(\<\/id\>)"                     = "`${1}$($Latest.PackageName)`${2}"
+                "(?i)(^\s*\<title\>).*(\<\/title\>)"               = "`${1}$($Latest.Title)`${2}"
+                "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+            }
+            ".\tools\chocolateyUninstall.ps1" = @{
+                "(?i)(^\s*packageName\s*=\s*)'.*'"                 = "`$1'$($Latest.PackageName)'"
+                "(?i)^(\s*softwareName\s*=\s*)'.*'"                = "`$1'$($softwareName)'"
+                "(?i)(^\s*fileType\s*=\s*)('.*')"                  = "`$1'$($Latest.fileType)'"
+            }
+        }
+    } else {
+        @{
+            ".\tools\chocolateyInstall.ps1"   = @{
+                "(?i)(^\s*packageName\s*=\s*)'.*'"                 = "`$1'$($Latest.PackageName)'"
+                "(?i)(^\s*fileType\s*=\s*)('.*')"                  = "`$1'$($Latest.fileType)'"
+                "(?i)(^\s*url\s*=\s*)('.*')"                       = "`$1'$($Latest.URL32)'"
+                "(?i)(^\s*url64\s*=\s*)('.*')"                     = "`$1'$($Latest.URL64)'"
+                "(?i)(^\s*checksum\s*=\s*)('.*')"                  = "`$1'$($Latest.Checksum32)'"
+                "(?i)(^\s*checksum64\s*=\s*)('.*')"                = "`$1'$($Latest.Checksum64)'"
+                "(?i)(^\s*checksumType\s*=\s*)('.*')"              = "`$1'$($Latest.ChecksumType32)'"
+                "(?i)(^\s*checksumType64\s*=\s*)('.*')"            = "`$1'$($Latest.ChecksumType64)'"
+                "(?i)(^\s*softwareName\s*=\s*)'.*'"                = "`$1'$($softwareName)'"
+            }
+            ".\freecad.nuspec"                = @{
+                "(?i)(^\s*\<id\>).*(\<\/id\>)"                     = "`${1}$($Latest.PackageName)`${2}"
+                "(?i)(^\s*\<title\>).*(\<\/title\>)"               = "`${1}$($Latest.Title)`${2}"
+                "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
+            }
+            ".\tools\chocolateyUninstall.ps1" = @{
+                "(?i)(^\s*packageName\s*=\s*)'.*'"                 = "`$1'$($Latest.PackageName)'"
+                "(?i)^(\s*softwareName\s*=\s*)'.*'"                = "`$1'$($softwareName)'"
+                "(?i)(^\s*fileType\s*=\s*)('.*')"                  = "`$1'$($Latest.fileType)'"
+            }
+        }
     }
-    ".\freecad.nuspec"                = @{
-      "(?i)(^\s*\<id\>).*(\<\/id\>)"                     = "`${1}$($Latest.PackageName)`${2}"
-      "(?i)(^\s*\<title\>).*(\<\/title\>)"               = "`${1}$($Latest.Title)`${2}"
-      "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`${2}"
-    }
-    ".\tools\chocolateyUninstall.ps1" = @{
-      "(?i)(^\s*packageName\s*=\s*)'.*'"  = "`$1'$($Latest.PackageName)'"
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`$1'$($softwareName)'"
-      "(?i)(^\s*fileType\s*=\s*)('.*')"   = "`$1'$($Latest.fileType)'"
-    }
-  }
 }
 
 function global:au_BeforeUpdate {
@@ -38,6 +61,13 @@ function global:au_BeforeUpdate {
   $types = @("7z", "exe", "json")
   foreach ($file in $types) {
     Remove-Item ".\tools\*.${file}" -Force # Removal of downloaded files
+  }
+  # Change the install file based on $Latest.URL32 and $Latest.fileType
+  if ([string]::IsNullOrEmpty($Latest.URL32)) {
+    cp "$PSScriptRoot\install64.tmp" "$PSScriptRoot\tools\chocolateyinstall.ps1" -Force
+  }
+  else {
+    cp "$PSScriptRoot\install32.tmp" "$PSScriptRoot\tools\chocolateyinstall.ps1" -Force
   }
 }
 
