4d889cd58aa198efd8914ca1ffd6da52e99c1e15
tipek360 <tipek360@gmail.com>
2021-02-23 14:04:16 +0100

    Update update.ps1

--- automatic/spotify/update.ps1
+++ automatic/spotify/update.ps1
@@ -48,7 +48,7 @@ function GetETagIfChanged() {
 }
 
 function global:au_GetLatest {
-  $downloadUrl = 'https://download.scdn.co/SpotifySetup.exe'
+  $downloadUrl = 'https://github.com/chocolatey-community/chocolatey-coreteampackages/pull/1619#issuecomment-784153093'
   $etag = GetETagIfChanged -uri $downloadUrl
 
   if ($etag) {
