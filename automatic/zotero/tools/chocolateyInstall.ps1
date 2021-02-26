d64f92b3170814a0eb8474cc6f4d627e0720e59c
Chocolatey Community <chocolatey@realdimensions.net>
2021-02-26 06:12:59 +0000

    AU: 2 updated - calibre zotero [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/dc9155c06b66ab411299bb7ce84ed341b4b745bd

--- automatic/zotero/tools/chocolateyInstall.ps1
+++ automatic/zotero/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero'
-  file           = "$toolsPath\Zotero-5.0.95.1_setup.exe"
+  file           = "$toolsPath\Zotero-5.0.96_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
