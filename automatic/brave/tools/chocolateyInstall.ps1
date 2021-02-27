c1a3bef8e914f30ad4410237286e014dda9c155b
Chocolatey Community <chocolatey@realdimensions.net>
2021-02-27 00:13:04 +0000

    AU: 5 updated - brave etcd freecad mkvtoolnix vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c711a34d6e8c8d870fe54e2678d0507ef4b1d4c5

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.21.69'
+[version]$softwareVersion = '1.21.70'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2549_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2549_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2553_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2553_x64.zip"
 }
 
 $installArgs = @{
