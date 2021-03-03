4a5aecd0608701a5fa03d9e228c3edb524d7d992
Chocolatey Community <chocolatey@realdimensions.net>
2021-03-03 12:14:19 +0000

    AU: 6 updated - autohotkey.portable brave chromium nodejs nodejs.install wps-office-free [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c160cd689eb5cf601f4682ff62f1b5d94b18699a

--- automatic/autohotkey.portable/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$fileName  = 'AutoHotkey_1.1.33.04.zip'
+$fileName  = 'AutoHotkey_2.0-a127-a43811bd.zip'
 $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $zip_path = "$toolsPath\$fileName"
 Remove-Item $toolsPath\* -Recurse -Force -Exclude $fileName
--- automatic/wps-office-free/tools/chocolateyInstall.ps1
+++ automatic/wps-office-free/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '11.2.0.9984'
+$version = '11.2.0.10017'
 $packageName = 'wps-office-free'
 $regName = $packageName -replace('\-',' ')
 $regName = $regName -replace('free','*')
@@ -8,11 +8,11 @@ $registry = Get-UninstallRegistryKey -SoftwareName $regName
 $packageArgs = @{
   packageName    = 'wps-office-free'
   fileType       = 'exe'
-  url            = 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.9984/WPSOffice_11.2.0.9984.exe'
+  url            = 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.10017/WPSOffice_11.2.0.10017.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = $regName
-  checksum       = '5edb7bb48d46ed2790eb7340a7f880345bb64053a235ec296d83c6336e674ce7'
+  checksum       = 'f8786a89d97de1edaaf7c1ea84a0d77ae26a7212c38292993e9d4220d37111c1'
   checksumType   = 'sha256'
 }
 
