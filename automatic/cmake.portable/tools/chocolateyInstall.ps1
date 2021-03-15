e4fa8253e8173baad426a443e611eb2a3fd52c27
Chocolatey Community <chocolatey@realdimensions.net>
2021-03-15 18:13:28 +0000

    AU: 5 updated - alldup brave cmake cmake.install cmake.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/33b3f3d6a155dcadda08150277b7fc1ea3e4d449

--- automatic/cmake.portable/tools/chocolateyInstall.ps1
+++ automatic/cmake.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\cmake-3.20.0-rc4-windows-i386.zip"
-  file64      = "$toolsPath\cmake-3.20.0-rc4-windows-x86_64.zip"
+  file        = "$toolsPath\cmake-3.19.7-win32-x86.zip"
+  file64      = "$toolsPath\cmake-3.19.7-win64-x64.zip"
   destination = $toolsPath
 }
 
