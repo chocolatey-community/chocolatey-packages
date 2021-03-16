242782efd91a3c1f042c50187b75dc5c29960853
Chocolatey Community <chocolatey@realdimensions.net>
2021-03-16 06:13:01 +0000

    AU: 3 updated - git.install git.portable graphviz [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8b934e2c3f1710d306fffeb876d043d574832729

--- automatic/git.install/tools/chocolateyInstall.ps1
+++ automatic/git.install/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ Set-InstallerRegistrySettings $pp
 
 Stop-GitSSHAgent
 
-$fileName32 = 'Git-2.30.2-32-bit.exe'
-$fileName64 = 'Git-2.30.2-64-bit.exe'
+$fileName32 = 'Git-2.31.0-32-bit.exe'
+$fileName64 = 'Git-2.31.0-64-bit.exe'
 $packageArgs = @{
     PackageName    = 'git.install'
     FileType       = 'exe'
