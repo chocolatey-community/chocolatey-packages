8b4541b9b5a1c5680bd9cac29f7071f6ddd8aaf9
Chocolatey Community <chocolatey@realdimensions.net>
2021-03-18 00:13:11 +0000

    AU: 7 updated - googlechromebeta nodejs nodejs.install smartftp sonarlint-vs2015 SQLite vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1072175d1a43a5ab9c057e4c43d073b814049df8

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '90.0.4430.19-beta'
+$version = '90.0.4430.30-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '27e0fa75bcb8789b6f6df4f35d9bf1fc25a909292fd152a67efb2086341b9e32'
-  checksum64             = '1388001d7d8aec92e19822d739ae4ce55c5ce3f091f9eee441cfa1b05de40a2f'
+  checksum               = 'b393874e9e5870cc0c5660cb43dc48c9598843665937984741168c1a1dfbd1a2'
+  checksum64             = 'b5e9656bba777acc501bf44c898c8c0649fa47dbdb0dc98a8d1e6a17359df13e'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
+++ automatic/sonarlint-vs2015/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$filePath = "$toolsPath\SonarLint.VSIX-4.33.0.28535-2015.vsix"
+$filePath = "$toolsPath\SonarLint.VSIX-4.33.1.28913-2015.vsix"
 
 $vsixUrl =  "file://" + $filePath.Replace("\", "/")
 
