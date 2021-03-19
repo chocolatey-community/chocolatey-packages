db742805608d83ededd981a6d3a76e4277d2c272
Chocolatey Community <chocolatey@realdimensions.net>
2021-03-19 00:13:04 +0000

    AU: 3 updated - freecad kubernetes-cli vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/2540e1529fb41ec56389de518f9c7f73cc243764

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2619_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2619_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2622_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2622_x64.zip"
 }
 
 $installArgs = @{
