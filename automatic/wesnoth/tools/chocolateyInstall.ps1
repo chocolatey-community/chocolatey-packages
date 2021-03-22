696c41e63a1072d77fd2d8d033dbb113f591b74a
Chocolatey Community <chocolatey@realdimensions.net>
2021-03-22 06:15:59 +0000

    AU: 5 updated - brave chromium kubelogin vim wesnoth [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/67c0c6176ffc086f3ae52bd4102b633c6998d766

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.15.10/wesnoth-1.15.10-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.14/wesnoth-1.14.16/wesnoth-1.14.16-win32.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = 'ea2e47d42ea809e3ab35e8d445e9c4a42901abc76e2f7ee7a2825cbd4137b88f'
+  checksum       = '2334c632d59bbb32e1d12b33c2423bf24e20da3e0d956d5a20e6064e0ac67067'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
