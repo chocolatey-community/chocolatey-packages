bc2a2a276e619cc2195253f14a397ba11fb605f3
Chocolatey Community <chocolatey@realdimensions.net>
2021-03-29 12:13:11 +0000

    AU: 2 updated - 360ts PartitionMasterFree [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f1a123cbe740bbe9f301737675ac600c7d07bd31

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1279.exe'
-  checksum               = '70aff63bee534729dafabb948cf88d15cab203405fb8a39b62134229996d8986'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1286.exe'
+  checksum               = '7d7b94960e894c4994fa4a4a4d9341b7cb5006aa7ed078ef8cebd8c8237f1962'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
