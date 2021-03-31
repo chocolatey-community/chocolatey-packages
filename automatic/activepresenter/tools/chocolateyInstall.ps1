567aba0f0ea615161dac790df415ba495ca7df44
AdmiringWorm <kim.nordmo@gmail.com>
2021-03-31 16:57:03 +0200

    (activepresenter) Updated to use 'Get-RemoteFiles' instead of custom solution

--- automatic/activepresenter/tools/chocolateyInstall.ps1
+++ automatic/activepresenter/tools/chocolateyInstall.ps1
@@ -1,13 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$fileType      = 'exe'
-$toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
-$embedded_path = Get-Item "$toolsDir\*.$fileType"
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'activepresenter'
-  fileType       = $fileType
-  file           = $embedded_path
+  fileType       = 'exe'
+  file           = "$toolsDir\"
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0)
 }
@@ -16,7 +14,7 @@ Remove-Item $embedded_path -ea 0
 
 $packageName = $packageArgs.packageName
 $installLocation = Get-AppInstallLocation $packageName
-if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+if (!$installLocation) { Write-Warning "Can't find $packageName install location"; return }
 Write-Host "$packageName installed to '$installLocation'"
 
 Register-Application "$installLocation\$packageName.exe"
