04fcf965c10e4e02ae80042be18195d2c793e3a3
AdmiringWorm <kim.nordmo@gmail.com>
2021-03-31 17:33:07 +0200

    (codelite) Updated to use main website for urls

--- automatic/codelite/tools/chocolateyInstall.ps1
+++ automatic/codelite/tools/chocolateyInstall.ps1
@@ -1,14 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsPath      = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'codelite'
   fileType       = 'exe'
-  file64         = Get-Item $toolsPath\*.exe
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
   silentArgs     = '/VERYSILENT /SP- /SUPPRESSMSGBOXES'
   validExitCodes = @(0)
   softwareName   = 'CodeLite'
 }
 Install-ChocolateyInstallPackage @packageArgs
-Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" "" }}
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" "" } }
--- automatic/codelite/update.ps1
+++ automatic/codelite/update.ps1
@@ -1,13 +1,19 @@
 ﻿Import-Module AU
 
-$releases = "https://github.com/eranif/codelite/releases/latest"
+$releases = "https://downloads.codelite.org/"
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+    }
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType64)"
-      "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
+      "(?i)(^\s*checksum32\:).*"          = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
 
     "$($Latest.PackageName).nuspec" = @{
@@ -16,26 +22,25 @@ function global:au_SearchReplace {
   }
 }
 
-function global:au_BeforeUpdate { 
-  Get-RemoteFiles -Purge -NoSuffix
-  set-alias 7z $Env:chocolateyInstall\tools\7z.exe
-  $path = gi tools\*.7z
-  7z x $path -otools -aoa
-  rm $path
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -FileNameBase "codelite"
 }
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re      = 'exe\.7z$'
-  $domain  =  $releases -split '(?<=//.+)/' | select -First 1
-  $url     = $download_page.links | ? href -match $re | select -First 1 -Expand href | % { $domain + $_ }
-  $version = $url -split '/' | select -last 1 -skip 1
+  $re = 'windows$'
+  $re64 = 'windows_64$'
+  $url = $download_page.links | ? href -match $re | select -First 1 -Expand href | % { Get-RedirectedUrl $_  3>$null }
+  $url64 = $download_page.links | ? href -match $re64 | select -First 1 -Expand href { % Get-RedirectedUrl $_  3>$null }
+  $version = $download_page.content -match "CodeLite ([\d\.]+) - Stable Release" | select -first 1 | % { $Matches[1] }
 
   @{
-    URL64        = $url
+    URL64        = $url64
+    URL32        = $url
     Version      = $version
     ReleaseNotes = "https://github.com/eranif/codelite/releases/tag/$version"
+    FileType     = "exe"
   }
 }
 
