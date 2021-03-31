9ef29591345aa97a6991b76259bc94b39701809f
AdmiringWorm <kim.nordmo@gmail.com>
2021-03-31 18:07:58 +0200

    (yo) Make version semver 1 compatible

--- automatic/yo/update.ps1
+++ automatic/yo/update.ps1
@@ -5,7 +5,7 @@ $releases = 'https://github.com/yeoman/yo/releases'
 function global:au_SearchReplace {
   @{
     'tools\ChocolateyInstall.ps1' = @{
-    "(^[$]version\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"
+      "(^[$]version\s*=\s*)('.*')" = "`$1'$($Latest.RemoteVersion)'"
     }
   }
 }
@@ -14,14 +14,15 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
   #https://github.com/yeoman/yo/archive/v1.8.5.zip
-  $re  = "(.*).zip"
+  $re = "(.*).zip"
   $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
   $file = $url -split 'v' | Select-Object -last 1
 
   $version = [IO.Path]::GetFileNameWithoutExtension($file)
 
   @{
-    Version = $version
+    Version       = Get-Version $version
+    RemoteVersion = $version
   }
 }
 
