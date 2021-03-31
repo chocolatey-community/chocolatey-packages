43b6df44e9e52a13de785d862764928bfb984813
AdmiringWorm <kim.nordmo@gmail.com>
2021-03-31 17:59:54 +0200

    (vscode*) Corrected api url to check for updates

--- automatic/vscode.install/update.ps1
+++ automatic/vscode.install/update.ps1
@@ -1,8 +1,8 @@
 import-module au
 import-module "$PSScriptRoot\..\..\extensions\chocolatey-core.extension\extensions\chocolatey-core.psm1"
 
-$releases32 = 'https://vscode-update.azurewebsites.net/api/update/win32/stable/VERSION'
-$releases64 = 'https://vscode-update.azurewebsites.net/api/update/win32-x64/stable/VERSION'
+$releases32 = 'https://update.code.visualstudio.com/api/update/win32/stable/VERSION'
+$releases64 = 'https://update.code.visualstudio.com/api/update/win32-x64/stable/VERSION'
 
 if ($MyInvocation.InvocationName -ne '.') {
   function global:au_BeforeUpdate {
@@ -12,34 +12,34 @@ if ($MyInvocation.InvocationName -ne '.') {
 }
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"    = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^[$]version\s*=\s*)('.*')"     = "`$1'$($Latest.RemoteVersion)'"
-        }
-     }
+  @{
+    'tools\chocolateyInstall.ps1' = @{
+      "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
+      "(?i)(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*url64bit\s*=\s*)('.*')"    = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^[$]version\s*=\s*)('.*')"     = "`$1'$($Latest.RemoteVersion)'"
+    }
+  }
 }
 
 function global:au_GetLatest {
-    $json32 = Invoke-WebRequest -UseBasicParsing -Uri $releases32 | ConvertFrom-Json
-    $json64 = Invoke-WebRequest -UseBasicParsing -Uri $releases64 | ConvertFrom-Json
+  $json32 = Invoke-WebRequest -UseBasicParsing -Uri $releases32 | ConvertFrom-Json
+  $json64 = Invoke-WebRequest -UseBasicParsing -Uri $releases64 | ConvertFrom-Json
 
-    if ($json32.productVersion -ne $json64.productVersion) {
-        throw "Different versions for 32-Bit and 64-Bit detected."
-    }
+  if ($json32.productVersion -ne $json64.productVersion) {
+    throw "Different versions for 32-Bit and 64-Bit detected."
+  }
 
-    @{
-        Version       = $json32.productVersion
-        RemoteVersion = $json32.productVersion
-        URL32         = $json32.Url
-        URL64         = $json64.Url
-    }
+  @{
+    Version       = $json32.productVersion
+    RemoteVersion = $json32.productVersion
+    URL32         = $json32.Url
+    URL64         = $json64.Url
+  }
 }
 
 if ($MyInvocation.InvocationName -ne '.') {
-    update -ChecksumFor none
+  update -ChecksumFor none
 }
