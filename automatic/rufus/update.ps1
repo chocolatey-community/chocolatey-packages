11dacf1747a0915a1c32a4211b114841306b8129
AdmiringWorm <kim.nordmo@gmail.com>
2021-03-31 17:44:35 +0200

    (rufus) Corrected website to parse for releases

--- automatic/rufus/update.ps1
+++ automatic/rufus/update.ps1
@@ -1,30 +1,30 @@
 import-module au
 
-$releases = 'https://rufus.ie/en_IE.html'
+$releases = 'https://rufus.ie/en/'
 
 function global:au_SearchReplace {
-   @{
-        ".\legal\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
-          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-        }
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(\s+x32:).*"     = "`${1} $($Latest.URL32)"
+      "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
     }
+  }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase 'rufus' -NoSuffix}
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameBase 'rufus' -NoSuffix }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re    = '\.exe$'
-    $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+  $re = '\.exe$'
+  $url = $download_page.links | ? href -match $re | select -First 1 -expand href
 
-    $version  = $url -split '-|.exe' | select -Last 1 -Skip 1
+  $version = $url -split '-|.exe' | select -Last 1 -Skip 1
 
-    @{
-        Version  = $version
-        URL32    = New-Object uri([uri]$releases, $url)
-    }
+  @{
+    Version = $version
+    URL32   = New-Object uri([uri]$releases, $url)
+  }
 }
 
 update -ChecksumFor none
