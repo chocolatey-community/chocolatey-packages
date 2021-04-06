da1a1fc1a494afe4bb4990bc59eb3939e8cf54f0
Chocolatey Community <chocolatey@realdimensions.net>
2021-04-06 18:13:41 +0000

    AU: 2 updated - brave waterfox-classic [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9c9490fa6509896c7bb1287e72b359d6d2dc90bc

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.23.58'
+[version]$softwareVersion = '1.23.64'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
