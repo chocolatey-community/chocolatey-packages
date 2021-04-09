b6968422f83d7bbca505a57c0ac131ce4b286dab
AdmiringWorm <kim.nordmo@gmail.com>
2021-04-09 08:56:08 +0200

    [PUSH lockhunter]
    Checksum has changed

--- manual/lockhunter/README.md
+++ manual/lockhunter/README.md
@@ -1,4 +1,4 @@
-# <img src="https://cdn.rawgit.com/chocolatey/chocolatey-coreteampackages/3feb5644/icons/lockhunter.png" width="48" height="48"/> [lockhunter](https://chocolatey.org/packages/lockhunter)
+# <img src="https://cdn.jsdelivr.net/gh/chocolatey-community/chocolatey-coreteampackages@33b62b2ce8da379ba364675e5104019ef0734c4c/icons/lockhunter.png" width="48" height="48"/> [lockhunter](https://chocolatey.org/packages/lockhunter)
 
 LockHunter is a foolproof file unlocker. It is a free tool to delete files blocked by something you do not know. LockHunter is useful for fighting against malware, and other programs that are blocking files without a reason. Unlike other similar tools it deletes files into the recycle bin so you may restore them if deleted by mistake.
 
@@ -15,4 +15,4 @@ LockHunter is a foolproof file unlocker. It is a free tool to delete files block
 - It deletes files into the recycle bin, so you may restore them if deleted by mistake
 - Supports both 32 and 64 bit Windows
 
-![screenshot](http://lockhunter.com/screenshots/mainScreenshotRes.png)
+![screenshot](https://lockhunter.com/assets/screenshots/mainScreenshotFull.png)
--- manual/lockhunter/tools/chocolateyInstall.ps1
+++ manual/lockhunter/tools/chocolateyInstall.ps1
@@ -1,20 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName            = $env:chocolateyPackageName
-  fileType               = 'EXE'
-  url                    = 'https://lockhunter.com/assets/exe/lockhuntersetup_3-3-4.exe'
-  checksum               = 'A91CA5B5D4517666F65A43E39DA56FDD6C13E1BCB9F22FAD1790ACC5326680C6'
-  checksumType           = 'sha256'
-  silentArgs             = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes         = @(0)
-  softwareName           = 'LockHunter*'
+  packageName    = $env:chocolateyPackageName
+  fileType       = 'EXE'
+  url            = 'https://lockhunter.com/assets/exe/lockhuntersetup_3-3-4.exe'
+  checksum       = '46984b34649cd28076e171799819afbab8a25034830eda7bdf25ef9e2025db81'
+  checksumType   = 'sha256'
+  silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  validExitCodes = @(0)
+  softwareName   = 'LockHunter*'
 }
 Install-ChocolateyPackage @packageArgs
 
 $packageName = $packageArgs.packageName
 $installLocation = Get-AppInstallLocation $packageArgs.softwareName
-if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+if (!$installLocation) { Write-Warning "Can't find $packageName install location"; return }
 Write-Host "$packageName installed to '$installLocation'"
 
 Register-Application "$installLocation\$packageName.exe"
