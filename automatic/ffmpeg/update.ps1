677a80b0dad88d956b174b4de1eb88bfe3d2c54c
VuiMuich <jm.spam@gmx.net>
2021-04-12 13:13:39 +0200

    update to use remote provided version refference

--- automatic/ffmpeg/update.ps1
+++ automatic/ffmpeg/update.ps1
@@ -18,10 +18,10 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest   {
-  $download_page = Invoke-WebRequest -Uri "$releases/packages/" -UseBasicParsing -Header @{ Referer = $releases }
-  $re = 'essentials_build\.7z$'
-  $url64 = $download_page.links | ? href -match $re | Select -Expand href -Last 1
-  $version = ($url64 -split '/ffmpeg-')[-1] -split '-' | select -First 1
+#  $download_page = Invoke-WebRequest -Uri "$releases/packages/" -UseBasicParsing -Header @{ Referer = $releases }
+#  $re = 'essentials_build\.7z$'
+#  $url64 = $download_page.links | ? href -match $re | Select -Expand href -Last 1
+  $version = Invoke-WebRequest -Uri "$releases/release-version"
 
   @{
     URL64 = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.7z";
