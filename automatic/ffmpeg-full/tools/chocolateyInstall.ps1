d1de5dd497703c0b13ef44d07c74c16f8fc72f7e
VuiMuich <jm.spam@gmx.net>
2021-04-12 13:10:13 +0200

    add ffmpeg-full package

---
+++ automatic/ffmpeg-full/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    PackageName    = 'ffmpeg-full'
+    FileFullPath64 = Get-Item $toolsPath\*.7z
+    Destination    = $toolsPath
+}
+
+Get-ChildItem $toolsPath\* | Where-Object { $_.PSISContainer } | Remove-Item -Recurse -Force #remove older package dirs
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsPath\*.7z -ea 0
+
+Move-Item $toolsPath\ffmpeg-* $toolsPath\ffmpeg
---
+++ automatic/ffmpeg-full/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'https://www.gyan.dev/ffmpeg/builds'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+        }
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+
+function global:au_GetLatest   {
+  $download_page = Invoke-WebRequest -Uri "$releases/packages/" -UseBasicParsing -Header @{ Referer = $releases }
+  $re = 'full_build\.7z$'
+  $url64 = $download_page.links | ? href -match $re | Select -Expand href -Last 1
+  $version = Invoke-WebRequest -Uri "$releases/release-version"
+
+  @{
+    URL64 = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z";
+    Version = $version
+  }
+}
+
+update -ChecksumFor none
