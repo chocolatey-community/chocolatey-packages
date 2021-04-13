9267672d00efc1592dfd79dbe1f1c6a8bfa34c86
Chocolatey Community <chocolatey@realdimensions.net>
2021-04-13 00:13:26 +0000

    AU: 3 updated - googlechromebeta smartftp vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c31c305dcd58fe575ec5464d9b0cbcb33c73efa4

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '90.0.4430.61-beta'
+$version = '90.0.4430.70-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = 'd83ab9c2b068831dae4a816aa1363764ca9145b558032ad0735a7812e9889542'
-  checksum64             = '823fd002397e4fe6429928c017dbdcfbf9f752f9e9db2b3d1e932b53a107570e'
+  checksum               = 'f440aee6419bff1bf6020d763ecd6c10b1dbc36206c08bf59974b0d0b0c76efa'
+  checksum64             = 'ee1c74567c8beed3ac68c96b45807b8af8c8f97c0bef6d69dd5019c097958b03'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2756_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2756_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2758_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2758_x64.zip"
 }
 
 $installArgs = @{
