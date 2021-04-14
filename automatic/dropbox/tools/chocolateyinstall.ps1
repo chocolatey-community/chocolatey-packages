d558bdb1be1246cb847f3867c5b4d0cbf908c105
Chocolatey Community <chocolatey@realdimensions.net>
2021-04-14 18:14:49 +0000

    AU: 6 updated - curl dropbox gimp googlechromebeta vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e0f856588509d2895d1070e3913372426bc328f6

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '120.3.4579'
+$version = '121.3.4241'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20120.3.4579%20Offline%20Installer.exe'
-    checksum       = '8d511a6a006b815e6b2ffbebd7c6c91ca67ac1cdaff4aaa1b20573e91ee7148c'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20121.3.4241%20Offline%20Installer.exe'
+    checksum       = '7a72a083e88be1572c9fe60db4f42b871a42fbcef549ba0a74e42e6991a48aff'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/48387dfc3d691558404cff1ea2582e3862a40080/VSCodeSetup-ia32-1.56.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/48387dfc3d691558404cff1ea2582e3862a40080/VSCodeSetup-x64-1.56.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/94d369e27d19f598228961e9feb0413f2edabe5d/VSCodeSetup-ia32-1.56.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/94d369e27d19f598228961e9feb0413f2edabe5d/VSCodeSetup-x64-1.56.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = '2efa81653d3e67b0077781bae12bb11367305662c724ae0524d1a1eadc87b8e82f0a623af3078e4eac54aa9f1f5b331ef833c087104790908180a381f607c86b'
+  checksum       = 'f86836d134a0f70b3819ea556e755d6ebd856f9dcf39c942bf0bc1ea79c154d3eeff2248d5d3b4382f9633d2674090de7c790586d364bf9a6f05ef39879c6a46'
   checksumType   = 'sha512'
-  checksum64     = 'b6602a620f3ec0c3e9d745a54fbb0b0fc5da45279cdc15774a233e0bd601feea348be7cb4ca9a2d5c1fc18416155572281c04dca7ca01cbdd0c9ac87965743e8'
+  checksum64     = '1c1acbc40e8ef90358d0baa37d1825da6932153531f408f8b9decdc441d677b1357d127be26a7819aaa89fec1ce1044d1c17565086fba6690128eb96504380c5'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
