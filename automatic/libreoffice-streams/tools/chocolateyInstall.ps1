fc50fdfc88671751b5e2bc5191b8df7b5a3e084b
Chocolatey Community <chocolatey@realdimensions.net>
2021-04-16 12:13:36 +0000

    AU: 3 updated - libreoffice-fresh Waterfox zotero [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a098fb8cb4d8d5fe0e01c9b7f7e2297cd14c0299

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -4,13 +4,13 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
 $packageArgs = @{
-  packageName            = 'libreoffice-still'
-  version                = '7.0.5'
+  packageName            = 'libreoffice-fresh'
+  version                = '7.1.2'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.0.5/win/x86/LibreOffice_7.0.5_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.0.5/win/x86_64/LibreOffice_7.0.5_Win_x64.msi'
-  checksum               = '6a3820c0bba7443e1e2d208e22274367a7b4f9d3be03c996ae187ce2c0c7c00c'
-  checksum64             = 'd5cc1613f56f54058b02701c8f7bea80ec20a452b2ba48099bd49350e2f05955'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.1.2/win/x86/LibreOffice_7.1.2_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.1.2/win/x86_64/LibreOffice_7.1.2_Win_x64.msi'
+  checksum               = 'e2df79d507b9eccfe01d7e734f027161e6abac798a64876d930d76f3ce7632a4'
+  checksum64             = '3131ce7d052706feffef3121f9a77d0d6c6c894fe6dee9141dc4aa3a68641c0b'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
