c11ce541530868dc301120f18d393ca01d5c4156
Chocolatey Community <chocolatey@realdimensions.net>
2021-04-21 06:14:21 +0000

    AU: 2 updated - gnupg GoogleChrome [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b34b01aace168870bab88ac474bfd45945b1a2ce

--- automatic/gnupg/tools/chocolateyInstall.ps1
+++ automatic/gnupg/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName    = $Env:ChocolateyPackageName
   fileType       = 'EXE'
   softwareName   = 'GNU Privacy Guard*'
-  file           = "$toolsDir\gnupg-w32-2.3.0_20210407.exe"
+  file           = "$toolsDir\gnupg-w32-2.3.1_20210420.exe"
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '90.0.4430.72'
+$version = '90.0.4430.85'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '41c53eb53f87fd4c0003c4db9920b2a26ddaa484f229f57650ae8c848413c57d'
-  checksum64             = '8ac8fb866387497105c9845729cfd619000bfecde5a2db4bf86340d29964955f'
+  checksum               = '02fa2012f5f9675cf9792b0a39a5ff202a7ef9808b3827cf3a5df3f71dfe0aba'
+  checksum64             = '0e71d6c17fe70e207526a042bc80fbac7d0938de825562c4d6939b04ebe6fe89'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
