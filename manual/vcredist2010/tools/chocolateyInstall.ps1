b3004b3778a7b8670fb918383b6e0ca82d708970
Jakub Bereżański <kuba@berezanscy.pl>
2021-04-24 18:21:13 +0200

    (vcredist2010) Refactor install script

--- manual/vcredist2010/tools/chocolateyInstall.ps1
+++ manual/vcredist2010/tools/chocolateyInstall.ps1
@@ -1,6 +1,22 @@
-﻿$processor = Get-WmiObject Win32_Processor
-$is64bit = $processor.AddressWidth -eq 64
-Install-ChocolateyPackage 'vcredist2010' 'exe' '/Q' 'http://download.microsoft.com/download/C/6/D/C6D0FD4E-9E53-4897-9B91-836EBA2AACD3/vcredist_x86.exe'
-if($is64bit) {
-  Install-ChocolateyPackage 'vcredist2010_x64' 'exe' '/Q' 'http://download.microsoft.com/download/A/8/0/A80747C3-41BD-45DF-B505-E9710D2744E0/vcredist_x64.exe'
+﻿$params = @{
+  PackageName    = 'vcredist2010'
+  FileType       = 'exe'
+  Url            = 'http://download.microsoft.com/download/C/6/D/C6D0FD4E-9E53-4897-9B91-836EBA2AACD3/vcredist_x86.exe'
+  Url64bit       = 'http://download.microsoft.com/download/A/8/0/A80747C3-41BD-45DF-B505-E9710D2744E0/vcredist_x64.exe'
+  Checksum       = ''
+  Checksum64     = ''
+  ChecksumType   = ''
+  ChecksumType64 = ''
+  SilentArgs     = '/Q'
+  ValidExitCodes = @(0)
+}
+Install-ChocolateyPackage @params
+
+# Install both 32bit and 64bit on a 64bit OS
+# If a program is compiled as x86 and the 32bit version of vcredist isn't installed, then the program would fail to start.
+if (Get-ProcessorBits 64) {
+  $originalChocolateyForceX86 = $Env:chocolateyForceX86
+  $Env:chocolateyForceX86 = $true
+  Install-ChocolateyPackage @params
+  $Env:chocolateyForceX86 = $originalChocolateyForceX86
 }
