405a3fabec726574c1afb7c4082be2e2bcbb7b8f
RB <RedBaron2@users.noreply.github.com>
2021-04-27 11:28:00 -0700

    regex adjustment and newer dev find

--- automatic/freecad/update_helper.ps1
+++ automatic/freecad/update_helper.ps1
@@ -1,19 +1,22 @@
-
 function Get-FreeCad {
 param(
   [string]$Title,
   [string]$kind = 'stable',
-  [int]$filler = "0"
+  [int]$filler = "0",
+  [string]$uri = $releases
 )
-  $mobile = @{$true="portable";$false="installer"}[($kind -eq 'portable')]; $portable = @{$true="vc14.x-x86_64";$false="$mobile"}[($kind -eq 'dev')]
+  $download_page = Invoke-WebRequest -Uri $uri -UseBasicParsing
+  $mobile = @{$true="portable";$false="installer"}[($kind -eq 'portable')]; $portable = @{$true="vc(17|14)\.x\-x86(\-|_)64";$false="$mobile"}[($kind -eq 'dev')]
   $try = (($download_page.Links | ? href -match "CAD\-|\.[\dA-Z]+\-WIN" | select -First 1 -expand href).Split("/")[-1]).Split(".")[-1]
   $ext = @{$true='7z';$false='exe'}[( $kind -match 'dev' ) -or ( $portable -match 'portable' )]
   $re32 = "(WIN)\-x32\-($portable)\.$ext";
   # cybrwshl use of (\-[0-9])? regex updated to (\-\d+)? for installer with possible digit(s) before extension
-  $re64 =  @{$true="(WIN|Win)\-(Conda|x64)(\-|_)$portable+(\-\d+)?(\.$ext)$";$false="(WIN)\-x64\-($portable)(\-\d)?\.$ext$"}[( $kind -match 'dev' )]
+  $re64 =  @{$true="(WIN|Win)\-(LPv\d+\.\d+\.\d+|Conda|x64)(\-|_)($portable)(\-|.)?(\d+)?(\.$ext)$";$false="(WIN)\-x64\-($portable)(\-|.)?(\d)?\.$ext$"}[( $kind -match 'dev' )]
   $url32 = $download_page.Links | ? href -match $re32 | select -first 1 -expand href
   $url64 = $download_page.Links | ? href -match $re64 | select -first 1 -expand href
+  Write-Warning "url64 -${PreUrl}${url64}-"
   $checking = (($url64.Split('\/'))[-1]) -replace($re64,'') -replace('\-','.'); $version = ( Get-Version $checking ).Version
+  Write-Host "version check -$checking-"
   # Version Matching for all kind except dev since it is only 64bit
   if ($kind -ne 'dev'){ 
       $32bitchk = (($url32.Split('\/'))[-1]) -replace($re32,'') -replace('\-','.'); $ver32 = ( Get-Version $32bitchk ).Version
