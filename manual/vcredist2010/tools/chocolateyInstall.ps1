88f2abf0b5f6be366b127395dc4893ff94a69c1a
Jakub Bereżański <kuba@berezanscy.pl>
2021-05-13 23:50:18 +0200

    (vcredist2010) Add update script

--- manual/vcredist2010/tools/chocolateyInstall.ps1
+++ manual/vcredist2010/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
   PackageName    = 'vcredist2010'
   FileType       = 'exe'
   Url            = 'https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe'
-  Url64bit       = 'https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe'
+  Url64          = 'https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe'
   Checksum       = '67313B3D1BC86E83091E8DE22981F14968F1A7FB12EB7AD467754C40CD94CC3D'
   Checksum64     = 'CC7EC044218C72A9A15FCA2363BAED8FC51095EE3B2A7593476771F9EBA3D223'
   ChecksumType   = 'sha256'
