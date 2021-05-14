a7777b1acb0548ae21c4457130bbce101d356d74
Jakub Bereżański <kuba@berezanscy.pl>
2021-05-14 21:39:17 +0200

    (vcredist2005) Update style to match vcredist2010 and vcredist2008

--- manual/vcredist2005/tools/chocolateyInstall.ps1
+++ manual/vcredist2005/tools/chocolateyInstall.ps1
@@ -1,14 +1,22 @@
-﻿try { 
-  $processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
-  Install-ChocolateyPackage 'vcredist2005' 'exe' '/Q' 'http://download.microsoft.com/download/e/1/c/e1c773de-73ba-494a-a5ba-f24906ecf088/vcredist_x86.exe'
-  if($is64bit) {
-  	Install-ChocolateyPackage 'vcredist2005_x64' 'exe' '/Q' 'http://download.microsoft.com/download/d/4/1/d41aca8a-faa5-49a7-a5f2-ea0aa4587da0/vcredist_x64.exe'
-  }
+﻿$params = @{
+  PackageName    = 'vcredist2005'
+  FileType       = 'exe'
+  Url            = 'https://download.microsoft.com/download/e/1/c/e1c773de-73ba-494a-a5ba-f24906ecf088/vcredist_x86.exe'
+  Url64          = 'https://download.microsoft.com/download/d/4/1/d41aca8a-faa5-49a7-a5f2-ea0aa4587da0/vcredist_x64.exe'
+  Checksum       = ''
+  Checksum64     = ''
+  ChecksumType   = 'sha256'
+  ChecksumType64 = 'sha256'
+  SilentArgs     = '/Q'
+  ValidExitCodes = @(0,3010) # http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+}
+Install-ChocolateyPackage @params
 
-  # the following is all part of error handling
-  Write-ChocolateySuccess 'vcredist2005'
-} catch {
-  Write-ChocolateyFailure 'vcredist2005' "$($_.Exception.Message)"
-  throw 
+# Install both 32bit and 64bit on a 64bit OS
+# If a program is compiled as x86 and the 32bit version of vcredist isn't installed, then the program would fail to start.
+if (Get-ProcessorBits 64) {
+  $originalChocolateyForceX86 = $Env:chocolateyForceX86
+  $Env:chocolateyForceX86 = $true
+  Install-ChocolateyPackage @params
+  $Env:chocolateyForceX86 = $originalChocolateyForceX86
 }
