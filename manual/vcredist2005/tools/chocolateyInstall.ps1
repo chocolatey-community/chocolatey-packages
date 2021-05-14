adc16c9aad362403f97e3e1f0709e45374f1ce2b
Jakub Bereżański <kuba@berezanscy.pl>
2021-05-14 21:09:44 +0200

    (vcredist2005) Import from MarkRobertJohnson/ChocolateyPackages

---
+++ manual/vcredist2005/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿try { 
+  $processor = Get-WmiObject Win32_Processor
+  $is64bit = $processor.AddressWidth -eq 64
+  Install-ChocolateyPackage 'vcredist2005' 'exe' '/Q' 'http://download.microsoft.com/download/e/1/c/e1c773de-73ba-494a-a5ba-f24906ecf088/vcredist_x86.exe'
+  if($is64bit) {
+  	Install-ChocolateyPackage 'vcredist2005_x64' 'exe' '/Q' 'http://download.microsoft.com/download/d/4/1/d41aca8a-faa5-49a7-a5f2-ea0aa4587da0/vcredist_x64.exe'
+  }
+
+  # the following is all part of error handling
+  Write-ChocolateySuccess 'vcredist2005'
+} catch {
+  Write-ChocolateyFailure 'vcredist2005' "$($_.Exception.Message)"
+  throw 
+}
