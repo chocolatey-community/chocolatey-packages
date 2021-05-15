f70103fcab17b1d4f8cb227e8fcdc5ca06fb6da7
Chocolatey Community <chocolatey@realdimensions.net>
2021-05-15 00:13:02 +0000

    AU: 2 updated - dropbox keeweb [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e8c315ccf9c9ce7b87c1cda9923cc7331332d256

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '122.4.4867'
+$version = '123.3.4808'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20122.4.4867%20Offline%20Installer.exe'
-    checksum       = 'e6cce6baf7d7284947d86f817d9579746b392375ebfad414a312ef7100df1401'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20123.3.4808%20Offline%20Installer.exe'
+    checksum       = '87d25c25fd9549b875d76bdfd789b3b513be0e60710e984662829959285f6e16'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
