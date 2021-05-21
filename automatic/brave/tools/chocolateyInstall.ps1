03a9df9da1693876cbd8a061c87a11d6f851e20d
Chocolatey Community <chocolatey@realdimensions.net>
2021-05-21 12:14:04 +0000

    AU: 5 updated - brave chromium dropbox mp3tag smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/384551335b30b4e451418d066d3dc5498084a605

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup32.exe"
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
+  file        = "$toolsPath\BraveBrowserStandaloneSilentSetup32.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.26.34'
+[version]$softwareVersion = '1.25.65'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
