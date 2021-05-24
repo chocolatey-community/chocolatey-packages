59823537c6b8ceced8505bdce5415cbba1e5489a
Ragnar-F <1332321+Ragnar-F@users.noreply.github.com>
2021-05-24 08:27:52 +0200

    (autohotkey.portable) Consider new filenames for v2

--- automatic/autohotkey.portable/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.portable/tools/chocolateyInstall.ps1
@@ -18,9 +18,13 @@ Remove-Item "$toolsPath/AutoHotkeyA32.exe" -ea 0
 if ((Get-OSArchitectureWidth 64) -and ($Env:chocolateyForceX86 -ne 'true')) {
     Write-Verbose "Removing UNICODE-32 version"
     Remove-Item "$toolsPath/AutoHotkeyU32.exe" -ea 0
-    Move-Item "$toolsPath/AutoHotkeyU64.exe" "$toolsPath/AutoHotkey.exe" -Force
+    Move-Item "$toolsPath/AutoHotkeyU64.exe" "$toolsPath/AutoHotkey.exe" -Force -ea 0
+    Remove-Item "$toolsPath/AutoHotkey32.exe" -ea 0
+    Move-Item "$toolsPath/AutoHotkey64.exe" "$toolsPath/AutoHotkey.exe" -Force -ea 0
 } else {
     Write-Verbose "Removing UNICODE-64 version"
     Remove-Item "$toolsPath/AutoHotkeyU64.exe" -ea 0
-    Move-Item "$toolsPath/AutoHotkeyU32.exe" "$toolsPath/AutoHotkey.exe" -Force
+    Move-Item "$toolsPath/AutoHotkeyU32.exe" "$toolsPath/AutoHotkey.exe" -Force -ea 0
+    Remove-Item "$toolsPath/AutoHotkey64.exe" -ea 0
+    Move-Item "$toolsPath/AutoHotkey32.exe" "$toolsPath/AutoHotkey.exe" -Force -ea 0
 }
