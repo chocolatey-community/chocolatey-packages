e4ffe75c00572dd3d2a95b617ed65985daa60a5e
Chocolatey Community <chocolatey@realdimensions.net>
2021-05-26 06:13:21 +0000

    AU: 3 updated - apache-httpd brave etcd [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/42bcdf073025a3df0aa5ee315268b615c2b69476

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.25.66'
+[version]$softwareVersion = '1.25.68'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
