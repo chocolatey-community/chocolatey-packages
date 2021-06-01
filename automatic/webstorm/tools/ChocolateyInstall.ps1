b1cce959ab05490729061db5bb9788ca8ee95e16
Chocolatey Community <chocolatey@realdimensions.net>
2021-06-01 18:13:28 +0000

    AU: 4 updated - datagrip FirefoxESR sysinternals webstorm [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/41532a359775b75e979a83ea39272414a520d593

--- automatic/webstorm/tools/ChocolateyInstall.ps1
+++ automatic/webstorm/tools/ChocolateyInstall.ps1
@@ -19,8 +19,8 @@ New-Item -ItemType Directory -Force -Path $installDir
 $arguments              = @{
     packageName         = $env:ChocolateyPackageName
     softwareName        = 'JetBrains WebStorm*'
-    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2021.1.1.exe'
-    checksum            = '11d2c1bfd44717963a5c4ee35f93a74a243d7a800b3898b040ca8a5a01894f3f'
+    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2021.1.2.exe'
+    checksum            = '1892065785b338c5b9ec68d9b28377cd1867e98a0c624d45dc69ca065bb84981'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = $silentArgs
