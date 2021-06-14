7d0fafaac35525d518116403264cbdcfeb32d350
Chocolatey Community <chocolatey@realdimensions.net>
2021-06-14 00:13:07 +0000

    AU: 2 updated - flightgear vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c7ccb6b2fc81184b75b68a6d8a08cf24306323a3

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.2984_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.2984_x64.zip"
+  file          = "$toolsDir\gvim_8.2.2994_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.2994_x64.zip"
 }
 
 $installArgs = @{
