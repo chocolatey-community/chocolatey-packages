3ab18a5fc701ffb3a501bde39c8b106c9e928239
Chocolatey Community <chocolatey@realdimensions.net>
2021-06-17 00:13:29 +0000

    AU: 5 updated - brave googlechromebeta nuget.commandline typescript vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b5ae6145aa8b7be3816bd28a3316691c67a64d8c

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '92.0.4515.51-beta'
+$version = '92.0.4515.59-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = 'b48aaaf3477968e690296e3fecd7131b70bbf2d51829bd80b5ee0ab3bbad0e14'
-  checksum64             = '53af76968134cdb8dac7d9e72c032b9412b63abafffff6cf323b96bf1ea77ad4'
+  checksum               = 'dc02a56d5c272d541c23f30cd400db49837159203f35ae3087111069cf5c55bc'
+  checksum64             = '6e4c83579ccc6910d804587eaeb8805010bb275826e27a3408fbbd280c22bcc6'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
