33569a492c6c25fdc1d4fda53a77f2bd4a18eb66
Chocolatey Community <chocolatey@realdimensions.net>
2021-06-22 06:13:20 +0000

    AU: 1 updated - maxima [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/59f10ef7bfe6e24df2edc11d88b45b978a9feb14

--- automatic/maxima/tools/chocolateyInstall.ps1
+++ automatic/maxima/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.45.0-Windows/maxima-5.45.0-win32.exe/download'
-  url64bit       = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.45.0-Windows/maxima-5.45.0-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.45.1-Windows/maxima-5.45.1-win32.exe/download'
+  url64bit       = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.45.1-Windows/maxima-5.45.1-win64.exe/download'
   softwareName   = 'maxima*'
-  checksum       = '394ae838a1e35e9e7e86b7bedf786e89790834cb9fed14d64c0e537db0dda637'
+  checksum       = 'b2ab4d399ec7ecde4ff6bfb0f64f51bfdf52305733a834fe3d5b591922330714'
   checksumType   = 'sha256'
-  checksum64     = '068c2c6628fcf6479a686b97a3c0c6a010b0e821b987cf1ad88438c6b5a78e02'
+  checksum64     = '245611bdc5c43267ac444a377d40663962265e94db0c59c4831d84d05ddfeadc'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
