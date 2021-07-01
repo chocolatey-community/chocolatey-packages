7d4e4ed22f4b1e2230ffc29996af7bc0610a7378
Chocolatey Community <chocolatey@realdimensions.net>
2021-07-01 00:13:15 +0000

    AU: 6 updated - chromium mp3tag typescript vim Waterfox winscp.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0c7b62200fe12b7aa1e25123c95b14997018c14b

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.3075_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.3075_x64.zip"
+  file          = "$toolsDir\gvim_8.2.3081_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.3081_x64.zip"
 }
 
 $installArgs = @{
