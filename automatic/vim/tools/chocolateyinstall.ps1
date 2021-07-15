8e2acc37c25270d0000783098ca74a21d158e454
Chocolatey Community <chocolatey@realdimensions.net>
2021-07-15 00:13:06 +0000

    AU: 2 updated - julia vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/18abb4f694c7a5e45a55c516de7c98237fca3221

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.3159_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.3159_x64.zip"
+  file          = "$toolsDir\gvim_8.2.3161_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.3161_x64.zip"
 }
 
 $installArgs = @{
