ff5d131343130b29f6d882fae744a0e85fd21fb5
Chocolatey Community <chocolatey@realdimensions.net>
2021-07-16 18:13:13 +0000

    AU: 1 updated - chromium [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/085687e7308683cae950f2693aa9966ea61af593

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "94.0.4578.0-snapshots"
+$version = "91.0.4472.164"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
