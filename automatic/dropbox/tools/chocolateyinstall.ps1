bb81f6b48d00eed2807c53604185aed657248c79
Chocolatey Community <chocolatey@realdimensions.net>
2021-07-21 00:13:43 +0000

    AU: 3 updated - dropbox GoogleChrome vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/40dde832326a074a57347a9448ab4ddc3ec6c2c9

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '127.3.4263'
+$version = '127.4.4265'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20127.3.4263%20Offline%20Installer.exe'
-    checksum       = '2be47b31b30c480ef6f2a17542c765da82f5e8341d4d4fe9a3eeeb5a78cd2a91'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20127.4.4265%20Offline%20Installer.exe'
+    checksum       = 'fd256ed39db0ffb236ceeb86402b942a097d8f65aa0b230e14e861e8d4afa0d4'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '91.0.4472.164'
+$version = '92.0.4515.107'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '6c419de691ac74c1fb4c7cd8aceab5ac75a629dce784db51ed544836452f2141'
-  checksum64             = '72d5c0b9053c858be0b8ab0fbcd1c0a05185553cd381806d0ab71b00def87c56'
+  checksum               = '2b9f09399e3333c778a75a8137cadf6c53b133df16655606928fc5d5d8b625b9'
+  checksum64             = '2047c566831992cf85296157a16beeb25cccb706831e0aa9d0dc52c4c29f47cc'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
