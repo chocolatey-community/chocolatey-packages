a32f1dad96d00cc4d0a70d200abe518b4485baa3
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-01 06:13:08 +0000

    AU: 2 updated - chromium smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5ab32f10a075479c4c98fbeedeab1a6a45900466

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "94.0.4594.0-snapshots"
+$version = "94.0.4595.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
