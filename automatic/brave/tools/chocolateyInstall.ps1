2d9ca670d9ee25fbb55899eee8f95e7a923fd96a
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-04 12:13:56 +0000

    AU: 3 updated - brave chromium flightgear [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8a7c22228006a59fefd5c4089b73f8c45b969afe

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup32.exe"
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
+  file        = "$toolsPath\BraveBrowserStandaloneSilentSetup32.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.28.100'
+[version]$softwareVersion = '1.27.110'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
