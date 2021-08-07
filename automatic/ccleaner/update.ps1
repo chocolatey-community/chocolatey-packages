7031850cf79464a6e21d7460eaa0b6e736ab1793
AdmiringWorm <kim.nordmo@gmail.com>
2021-08-07 19:15:18 +0200

    (ccleaner) Update++

--- automatic/ccleaner/update.ps1
+++ automatic/ccleaner/update.ps1
@@ -1,25 +1,25 @@
-﻿import-module au
+﻿Import-Module au
 
-$releases = 'https://www.piriform.com/ccleaner/download/standard'
+$releases = 'https://www.ccleaner.com/ccleaner/download/standard'
 
 function global:au_BeforeUpdate {
   $tmpFile = "$env:TEMP\ccleaner.exe"
   Invoke-WebRequest -Uri $Latest.URL32 -OutFile $tmpFile -UseBasicParsing
 
-  $Latest.Checksum32 = Get-FileHash $tmpFile -Algorithm $Latest.ChecksumType32 | % Hash
-  [version]$fileVersion = Get-Item $tmpFile | % { $_.VersionInfo.FileVersion }
+  $Latest.Checksum32 = Get-FileHash $tmpFile -Algorithm $Latest.ChecksumType32 | ForEach-Object Hash
+  [version]$fileVersion = Get-Item $tmpFile | ForEach-Object { $_.VersionInfo.FileVersion }
 
   if ($fileVersion.ToString(2) -ne $Latest.RemoteVersion.ToString(2)) {
     # We only care about major and minor parts
-    throw "Executable have not yet been updated"
+    throw 'Executable have not yet been updated'
   }
 
-  rm $tmpFile -Force
+  Remove-Item $tmpFile -Force
 }
 
 function global:au_SearchReplace {
   @{
-    ".\tools\chocolateyInstall.ps1" = @{
+    '.\tools\chocolateyInstall.ps1' = @{
       "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
       "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
       "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
@@ -28,15 +28,12 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-  $re = '\.exe$'
-  $url = $download_page.links | ? href -match $re | select -First 1 -expand href
-
   $download_page = Invoke-WebRequest https://www.piriform.com/ccleaner/version-history -UseBasicParsing
   $Matches = $null
-  $download_page.Content -match "\<h6\>v((?:[\d]\.)[\d\.]+)"
+  $download_page.Content -match '\<h6\>v((?:[\d]\.)[\d\.]+)'
   $version = $Matches[1]
+
+  $url = 'https://download.ccleaner.com/ccsetup{0}.exe' -f ($version -replace '^(\d+)\.(\d+).*$', '$1$2')
 
   @{ URL32 = $url -replace 'http:', 'https:'; Version = $version ; RemoteVersion = [version]$version ; ChecksumType32 = 'sha256' }
 }
