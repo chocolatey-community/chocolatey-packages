953c453ee73bd07efdfd5864fffe79a480012d6f
AdmiringWorm <kim.nordmo@gmail.com>
2021-08-07 22:24:04 +0200

    (transifex-client) Update to support Python 3.7 versions

--- automatic/transifex-client/update.ps1
+++ automatic/transifex-client/update.ps1
@@ -3,32 +3,40 @@ Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 $releases = 'https://github.com/transifex/transifex-client/releases'
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameBase tx }
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix -FileNameBase tx
+  if (!(Test-Path "$PSScriptRoot\tools\x64")) {
+    mkdir "$PSScriptRoot\tools\x64"
+  }
+
+  Move-Item "$PSScriptRoot\tools\$($Latest.FileName64)" "$PSScriptRoot\tools\x64\$($Latest.FileName64)"
+}
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt"        = @{
-      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseUrl)>"
-      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
+    '.\legal\VERIFICATION.txt'        = @{
+      '(?i)(^\s*location on\:?\s*)\<.*\>' = "`${1}<$($Latest.ReleaseUrl)>"
+      '(?i)(\s*64\-Bit Software.*)\<.*\>' = "`${1}<$($Latest.URL64)>"
+      '(?i)(^\s*checksum\s*type\:).*'     = "`${1} $($Latest.ChecksumType64)"
+      '(?i)(^\s*checksum64\:).*'          = "`${1} $($Latest.Checksum64)"
     }
     ".\$($Latest.PackageName).nuspec" = @{
-      "(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseUrl)`${2}"
+      '(?i)(^\s*\<releaseNotes\>).*(\<\/releaseNotes\>)' = "`${1}$($Latest.ReleaseUrl)`${2}"
     }
   }
 }
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $re = 'tx\.py36.*\.exe$'
-  $urls = $download_page.Links | ? href -match $re | select -first 2 -expand href | % { 'https://github.com' + $_ }
+  $re = 'tx\.py37.*\.exe$'
+  $urls = $download_page.Links | Where-Object href -Match $re | Select-Object -First 2 -expand href | ForEach-Object { 'https://github.com' + $_ }
 
   $verRe = '\/'
-  $version = $urls[0] -split "$verRe" | select -last 1 -skip 1
+  $version = $urls[0] -split "$verRe" | Select-Object -Last 1 -Skip 1
+  $url64 = ($urls | Where-Object { $_ -match $version -and $_ -match '3\d+[-.]x64' } | Select-Object -First 1)
 
   @{
-    URL64      = [uri]($urls | ? { $_ -match $version -and $_ -match '3\d+\.x64' } | select -First 1)
+    URL64      = [uri]$url64
     Version    = [version]$version
     ReleaseUrl = "$releases/tag/$version"
   }
