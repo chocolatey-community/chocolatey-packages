052da16dada9fd6aeed2910ae7776609f79e59d1
AdmiringWorm <kim.nordmo@gmail.com>
2021-08-07 17:40:45 +0200

    (mkvtoolnix) Change to use exact paths instead of globbing [AU mkvtoolnix]
    fixes #1688

--- automatic/mkvtoolnix/tools/chocolateyInstall.ps1
+++ automatic/mkvtoolnix/tools/chocolateyInstall.ps1
@@ -1,22 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'mkvtoolnix'
   fileType       = 'exe'
-  file           = Get-Item $toolsPath\*-32-*.exe
-  file64         = Get-Item $toolsPath\*-64-*.exe
+  file           = "$toolsPath\"
+  file64         = "$toolsPath\"
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'mkvtoolnix*'
 }
 Install-ChocolateyInstallPackage @packageArgs
-Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' }}
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
 
 $packageName = $packageArgs.packageName
 $installLocation = Get-AppInstallLocation $packageName
-if (!$installLocation)  { Write-Warning "Can't find $PackageName install location"; return }
+if (!$installLocation) { Write-Warning "Can't find $PackageName install location"; return }
 Write-Host "$packageName installed to '$installLocation'"
 
 Write-Host 'Adding to PATH if needed'
--- automatic/mkvtoolnix/update.ps1
+++ automatic/mkvtoolnix/update.ps1
@@ -1,4 +1,4 @@
-import-module au
+﻿Import-Module au
 
 $domain = 'https://mkvtoolnix.download'
 $releases = "$domain/windows/releases"
@@ -6,11 +6,16 @@ $releases = "$domain/windows/releases"
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt" = @{
-      "(?i)(\s+x32:).*"     = "`${1} $($Latest.URL32)"
-      "(?i)(\s+x64:).*"     = "`${1} $($Latest.URL64)"
-      "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
-      "(?i)(checksum64:).*" = "`${1} $($Latest.Checksum64)"
+    '.\legal\VERIFICATION.txt'      = @{
+      '(?i)(\s+x32:).*'     = "`${1} $($Latest.URL32)"
+      '(?i)(\s+x64:).*'     = "`${1} $($Latest.URL64)"
+      '(?i)(checksum32:).*' = "`${1} $($Latest.Checksum32)"
+      '(?i)(checksum64:).*' = "`${1} $($Latest.Checksum64)"
+
+    }
+    '.\tools\chocolateyInstall.ps1' = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
   }
 }
@@ -20,18 +25,18 @@ function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $versions = $download_page.links | ? href -match '\/\d+\.[\d\.]+\/' | % { $_.href -split '\/' | select -last 1 -skip 1 }
+  $versions = $download_page.links | Where-Object href -Match '\/\d+\.[\d\.]+\/' | ForEach-Object { $_.href -split '\/' | Select-Object -Last 1 -Skip 1 }
 
   $versionSort = { [version]($_.TrimEnd('/')) }
   $version = $versions `
-    | sort $versionSort -Descending | select -first 1 | % { $_.TrimEnd('/') }
+  | Sort-Object $versionSort -Descending | Select-Object -First 1 | ForEach-Object { $_.TrimEnd('/') }
 
   $releases = "$releases/$version/"
   $re = 'mkvtoolnix-.+\.exe$'
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $url = $download_page.links | ? href -match $re | % href | unique
-  $url32 = $url -match '32\-?bit' | select -first 1
-  $url64 = $url -match '64\-?bit' | select -first 1
+  $url = $download_page.links | Where-Object href -Match $re | ForEach-Object href | Get-Unique
+  $url32 = $url -match '32\-?bit' | Select-Object -First 1
+  $url64 = $url -match '64\-?bit' | Select-Object -First 1
 
   @{
     URL32   = "$domain" + $url32
