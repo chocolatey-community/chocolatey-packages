d965dce77c7f34d5355f82faa1550c1e1ebcb067
AdmiringWorm <kim.nordmo@gmail.com>
2021-08-07 18:51:34 +0200

    (bleachbit) Ignore URLS for fosshub

--- automatic/bleachbit.install/update.ps1
+++ automatic/bleachbit.install/update.ps1
@@ -5,16 +5,24 @@ $releases = 'https://www.bleachbit.org/download/windows'
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $filename = ($download_page.links | Where-Object href -Match '.exe$' |
+    Where-Object href -NotMatch 'fosshub' |
     Select-Object -First 1 -expand href) -Replace '.*file=', ''
+
+  if (!$filename) {
+    # Most likely we only got a fosshub url in this case,
+    # as such we just do an ignore
+    return 'ignore'
+  }
+
   $version = $filename -split '-' | Select-Object -First 1 -Skip 1
 
   # figure out if this is a beta release or a normal release.
-  $BetasPage = Invoke-WebRequest -UseBasicParsing "https://download.bleachbit.org/beta"
+  $BetasPage = Invoke-WebRequest -UseBasicParsing 'https://download.bleachbit.org/beta/'
   $IsBeta = $BetasPage.links | Where-Object { $_.innertext -eq "$version/" }
 
   if ($IsBeta) {
     $filename = "beta/$version/$filename"
-    $version = $version + "-beta"
+    $version = $version + '-beta'
   }
 
   @{
@@ -25,15 +33,15 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
   $replacements = @{
-    ".\legal\VERIFICATION.txt" = @{
-      "(?i)(x86:).*"        = "`${1} $($Latest.URL32)"
-      "(?i)(checksum32:).*" = "`${1} $($Latest.Checksum32)"
-      "(?i)(type:).*"       = "`${1} $($Latest.ChecksumType32)"
+    '.\legal\VERIFICATION.txt' = @{
+      '(?i)(x86:).*'        = "`${1} $($Latest.URL32)"
+      '(?i)(checksum32:).*' = "`${1} $($Latest.Checksum32)"
+      '(?i)(type:).*'       = "`${1} $($Latest.ChecksumType32)"
     }
   }
 
-  if ($MyInvocation.InvocationName -ne ".") {
-    $replacements[".\tools\chocolateyInstall.ps1"] = @{
+  if ($MyInvocation.InvocationName -ne '.') {
+    $replacements['.\tools\chocolateyInstall.ps1'] = @{
       "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName32)`""
     }
   }
