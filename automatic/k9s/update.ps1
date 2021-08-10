7e188aec8640f5617289ea4994bf06a0e1ba2db8
Miodrag Milić <miodrag.milic@gmail.com>
2021-08-10 21:04:17 +0200

    (k9s) update++

--- automatic/k9s/update.ps1
+++ automatic/k9s/update.ps1
@@ -34,12 +34,11 @@ function global:au_GetLatest {
 
   $checksumAsset = $domain + ($download_page.Links | ? href -match "checksums\.txt$" | select -first 1 -expand href)
   $checksum_page = Invoke-WebRequest -Uri $checksumAsset -UseBasicParsing
-  $filename64 = -join('k9s_v', $version, '_Windows_x86_64.tar.gz')
   $checksum64 = [regex]::Match($checksum_page, "([a-f\d]+)\s*$([regex]::Escape($filename64))").Groups[1].Value
 
   return @{
     Version        = $version
-    URL64          = "$domain/derailed/k9s/releases/download/v${version}/${filename64}"
+    URL64          = "$domain/derailed/k9s/releases/download/v${version}/k9s_Windows_x86_64.tar.gz"
     ReleaseNotes   = "$domain/derailed/k9s/blob/v${version}/change_logs/release_v${version}.md"
     ReleaseURL     = "$domain/derailed/k9s/releases/tag/v${version}"
     Checksum64     = $checksum64
