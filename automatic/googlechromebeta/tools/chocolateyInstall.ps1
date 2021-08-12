35326fdafd2ac5f686c9021245211fa6ad29c5cf
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-12 18:13:29 +0000

    AU: 5 updated - googlechromebeta notepadplusplus notepadplusplus.commandline notepadplusplus.install thunderbird [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8277b5279dc6b40241b9e4406512f1512ba303bc

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '93.0.4577.25-beta'
+$version = '93.0.4577.42-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '20b60df0f6d813f8a00e2403e4db71adb3125247d3f26cd0a7170aaf5f3343cd'
-  checksum64             = 'd1ae52633e472c9523cd2e23cf8da3f222655e76cf425ee3f8eeea71f6de3098'
+  checksum               = '961ef4e0f8157e07b5f92c9ea8bf8f81c6e84d6388da69021b610f65375c7d47'
+  checksum64             = '5ee9f07658634dd02a110fd2d4f80980b64ad017aa8dbfb26e30e242a61eae99'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
