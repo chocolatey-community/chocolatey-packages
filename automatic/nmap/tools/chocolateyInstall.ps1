ea2634355bf3b628ecf9ffbcdf477d72a5c06b31
Miodrag Milić <miodrag.milic@gmail.com>
2021-08-19 20:32:25 +0200

    [AU nmap:7.92.0.20210815] fix invalid shim
    closes #1557

--- automatic/nmap/tools/chocolateyInstall.ps1
+++ automatic/nmap/tools/chocolateyInstall.ps1
@@ -4,3 +4,5 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $file = "$toolsPath\nmap-7.92-setup.exe"
 Start-Process -Wait $toolsPath\install.ahk $file
+
+ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" "" }}
