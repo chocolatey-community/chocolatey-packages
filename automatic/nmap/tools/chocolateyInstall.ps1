2588b590bab95addcc3d64719bef508f27a7b253
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-19 18:38:42 +0000

    AU: 1 updated - nmap [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e7460832f25ec73bfa81dfb572df0244e0ef6475

--- automatic/nmap/tools/chocolateyInstall.ps1
+++ automatic/nmap/tools/chocolateyInstall.ps1
@@ -5,4 +5,4 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $file = "$toolsPath\nmap-7.92-setup.exe"
 Start-Process -Wait $toolsPath\install.ahk $file
 
-ls $toolsPath\*.exe | % { rm $_ -ea 0; if (Test-Path $_) { sc "$_.ignore" "" }}
+Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" "" }}
