9bdce7764e5235ec104b0003028a5de6d12780d4
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-23 00:13:10 +0000

    AU: 2 updated - vim wesnoth [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/cb1b187e54fd3f1bdd63198cb32c188c16ea1f94

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.15.15/wesnoth-1.15.15-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.15.16/wesnoth-1.15.16-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = '4b67fb56804263d8cf814ab2f8bff5b0f1314d686124cf2e10f51efae55820a2'
+  checksum       = '1566096c0a955545a41a0e81f89a3f754c7656709478f771489484265b1394b4'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
