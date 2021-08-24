2db1149674309a2e34ce70ca54acd4b4be62aca9
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-24 18:13:43 +0000

    AU: 5 updated - Firefox googlechromebeta simplewall simplewall.install simplewall.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9925316485e38b9488c580cd8f238d2786b679e2

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '93.0.4577.51-beta'
+$version = '93.0.4577.58-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '28894e47f5032631750cae6e4abe937fb0081b85e418f2b008a6030df9e6d123'
-  checksum64             = '054b8196fa9a718e9623bee28c2a37dc31f1cbe8fb6f4c875572c5231544ecba'
+  checksum               = 'c6893859c3a029d7fb9d4d358005a604f747eb3ffd8217b96433e269e0ed5553'
+  checksum64             = '3aa57952f03155049d013239ed866ef7116c1e41f7dce1e82350a61625386e07'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
