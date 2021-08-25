6c0a5767708ec0a506a1609281f476f7e0d9535d
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-25 06:13:34 +0000

    AU: 5 updated - gnupg notepadplusplus notepadplusplus.commandline notepadplusplus.install vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a9622b554f5c2994535ee89d78f13bc04afb1eda

--- automatic/notepadplusplus.commandline/tools/chocolateyInstall.ps1
+++ automatic/notepadplusplus.commandline/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\npp.8.1.3.portable.7z"
-  file64      = "$toolsPath\npp.8.1.3.portable.x64.7z"
+  file        = "$toolsPath\npp.8.1.4.portable.7z"
+  file64      = "$toolsPath\npp.8.1.4.portable.x64.7z"
   destination = $toolsPath
 }
 
