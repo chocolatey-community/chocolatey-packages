1fcc2b7598f1c9adafe515c2386f7969e473c71b
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-26 00:13:07 +0000

    AU: 5 updated - chromium nodejs nodejs.install thunderbird vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c7b83453f42689309bdc92f553010e5a30264d40

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "95.0.4622.0-snapshots"
+$version = "95.0.4623.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
