e6360e387a1ff7ad7f1ed375a35f164a0abef1d6
Chocolatey Community <chocolatey@realdimensions.net>
2021-08-27 06:13:16 +0000

    AU: 2 updated - calibre dropbox [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4de7a566b4aecc4b5817ff10337cbb4f1fa069a1

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '130.3.4948'
+$version = '130.3.4951'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20130.3.4948%20Offline%20Installer.exe'
-    checksum       = 'c9e4d697c816414bf913f11126c8028194ee06ebef098cf9b83c7afc7a92c25c'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20130.3.4951%20Offline%20Installer.exe'
+    checksum       = '4e653f2b8b01c7c4311da35e21d99dbd5543215acfaa2e4ce2825255e78e4a8d'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
