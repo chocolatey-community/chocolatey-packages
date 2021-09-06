a2f2acf92d63a50226bfc8482441c5e68c1086b8
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-06 07:38:05 +0000

    AU: 1 updated - rdcman [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d634ed429ee84b1e4b4f1c6f4521df4feffe9463

--- automatic/rdcman/tools/chocolateyInstall.ps1
+++ automatic/rdcman/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@
 
 $packageArgs = @{
   packageName    = 'rdcman'
-  url            = ''
-  checksum       = ''
-  checksumType   = ''
+  url            = 'https://download.sysinternals.com/files/RDCMan.zip'
+  checksum       = 'ff56304da13c6bd2f2ef30ebf68d78cc889d1db8bc4f38e8174d0dad6af0f817'
+  checksumType   = 'sha256'
   unzipLocation  = $installDir
 }
 Install-ChocolateyZipPackage @packageArgs
