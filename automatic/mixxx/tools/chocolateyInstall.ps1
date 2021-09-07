3b42022a3a15250a75b6e24196c1c19ddd62dc2b
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-07 18:13:44 +0000

    AU: 4 updated - Firefox mixxx smartftp thunderbird [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/aa2a519c4018d85ea3e48c5cec0f930fa43864e3

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-792-g97cd3df821-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-793-g169cd3e4ac-win64.msi"
 
   softwareName   = 'Mixxx *'
 
