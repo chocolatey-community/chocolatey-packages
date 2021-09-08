846da8fdb850fa6b7ec03c88c5c9f6d47ee4438a
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-08 18:17:51 +0000

    AU: 4 updated - ccleaner googlechromebeta mixxx tor-browser [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ac21d7d668070d45835a50328045b9eac2114401

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '94.0.4606.31-beta'
+$version = '94.0.4606.41-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '605be5620c6383725fadcbe8c884585b747a74dad03d870034d03768c214f666'
-  checksum64             = 'ca041e2863724d5bb1671d6f0d56e416cbf291ddab116a2fbf0443c9e4b0ade7'
+  checksum               = '9e55afdeec4553d6a988bf44fc46f0702d7a2c591d0abef8f80fc19fbcb4eb92'
+  checksum64             = 'e9fdad8223c62b72640e12b636b4f5578d88a8562ccde629a8a494b99b2c50b5'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
