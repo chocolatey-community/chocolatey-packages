5d239f02c29bdec51b71a4ff5951f8cdfd50283f
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-09 12:13:57 +0000

    AU: 5 updated - brave chromium OpenSSL.Light owncloud-client vscode-insiders [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0ca4eacb2cede7d716a076ece2faf2ff4a265014

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\BraveBrowserStandaloneSilentSetup32.exe"
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
+  file        = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup32.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.29.79'
+[version]$softwareVersion = '1.30.68'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
