6b54e1ccc65ca3741ce9e6a62dac526d134ffc40
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-11 00:13:37 +0000

    AU: 5 updated - nodejs nodejs.install typescript vim wps-office-free [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a9daa470e6a50a3b687afafb2de30a8b1a143b9e

--- automatic/wps-office-free/tools/chocolateyInstall.ps1
+++ automatic/wps-office-free/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$version = '11.2.0.10294'
+$version = '11.2.0.10296'
 $packageName = 'wps-office-free'
 $regName = $packageName -replace('\-',' ')
 $regName = $regName -replace('free','*')
@@ -8,11 +8,11 @@ $registry = Get-UninstallRegistryKey -SoftwareName $regName
 $packageArgs = @{
   packageName    = 'wps-office-free'
   fileType       = 'exe'
-  url            = 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.10294/WPSOffice_11.2.0.10294.exe'
+  url            = 'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/11.2.0.10296/WPSOffice_11.2.0.10296.exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = $regName
-  checksum       = 'f26ab749b082e37fae5068f39712da87b87469700fb1d352446e1f99e362254f'
+  checksum       = 'e72d924b68444719b95504f9caa92e3f96b26ea2c884adbca2d63052b7a92a8b'
   checksumType   = 'sha256'
 }
 
