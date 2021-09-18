fe8414d0d533ac88091646e7638c457fae99f1c0
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-18 00:13:33 +0000

    AU: 4 updated - brave Minikube mixxx vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9f00e90eb5904f11c72cbf76514dc72a1892a441

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-817-g07f675a404-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-821-gddbf8af267-win64.msi"
 
   softwareName   = 'Mixxx *'
 
