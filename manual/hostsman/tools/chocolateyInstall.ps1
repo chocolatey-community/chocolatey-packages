4b0a1565754db56edb9792c31119e1a89c9ced5f
Miodrag Milić <miodrag.milic@gmail.com>
2021-09-19 17:04:37 +0200

    (hostsman) move to manual packages
    As last release was almost 5 years ago.

---
+++ manual/hostsman/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$fileName = 'HostsMan_Setup.exe'
+
+$packageArgs = @{
+  packageName    = $Env:ChocolateyPackageName
+  fileType       = 'exe'
+  file           = gi $toolsPath\$fileName
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+  softwareName   = 'Hostsman*'
+}
+Install-ChocolateyInstallPackage @packageArgs
+rm $toolsPath\$fileName -ea 0; if (Test-Path $toolsPath\$fileName) { sc "$toolsPath\$fileName.ignore" "" }
