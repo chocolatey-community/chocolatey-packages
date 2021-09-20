a3c408bc01f50faa919bc1b03421aa63bd2c5365
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-20 18:14:51 +0000

    AU: 5 updated - cmake cmake.install cmake.portable googlechromebeta mixxx [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/79fefcb8b2716fe63e3f80b05eb209fcb62d05b5

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '94.0.4606.50-beta'
+$version = '94.0.4606.54-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '295e3e569a7a699c27a871f6a19b5621580641d2379f88ae8e22b09aa099aabb'
-  checksum64             = '6d28882d310b16e9fc3189856e4104b20119ca159d94bc476ae34aabbb1713dc'
+  checksum               = '5266a13d12a791c2d7a70f310fc89cf755c47860cda9a4085754bb08ace95023'
+  checksum64             = '8231459eba3f4856cb3aa4b5d51ce1928495f70cf0777c9e8a77c4656e642d4e'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-839-g4ef9b6c136-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-840-g158a254929-win64.msi"
 
   softwareName   = 'Mixxx *'
 
