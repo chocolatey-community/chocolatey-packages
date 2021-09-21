b857775792eab3e2ea90296c5567fa0991361774
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-21 00:17:12 +0000

    AU: 2 updated - iTunes vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c6ba090bc50a85ab4c856de80677abe5ae296399

--- automatic/itunes/tools/chocolateyInstall.ps1
+++ automatic/itunes/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$version = '12.11.4.15'
+$version = '12.12.0.6'
 
 $packageArgs = @{
   packageName    = 'iTunes'
   fileType       = 'msi'
-  url            = 'https://secure-appldnld.apple.com/itunes12/071-25049-20210809-F69E52C8-F94A-11EB-8496-03B06A2FDD26/iTunesSetup.exe'
-  url64bit       = 'https://secure-appldnld.apple.com/itunes12/071-25047-20210809-F69EB204-F94A-11EB-9CEE-02B06A2FDD26/iTunes64Setup.exe'
+  url            = 'https://secure-appldnld.apple.com/itunes12/071-30323-20210915-123B0E28-1678-11EC-AB36-CEF157CD27C4/iTunesSetup.exe'
+  url64bit       = 'https://secure-appldnld.apple.com/itunes12/071-30327-20210915-123B7FE8-1678-11EC-8C42-CFF157CD27C4/iTunes64Setup.exe'
   softwareName   = 'iTunes'
-  checksum       = 'B8DC7922CFC2DC5704F1E64037DB0366465ED1A37E5C405587825F04EA629AE8'
+  checksum       = 'C958879020DC5BB41E26D6879AC7D14C4AE920186D6F8E84845765C4C27E96E6'
   checksumType   = 'sha256'
-  checksum64     = '0f9beca9148f5ced1ab77b4024d462c54846c4b3e8a4e1dcbeff730b5e5c66c8'
+  checksum64     = '76fcc844621a9e87f158784b657dd9a2198251e2d8bdf62f7f242262c276ea63'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 2010, 1641)
