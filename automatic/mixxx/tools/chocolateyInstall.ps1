1eb4db5546d640b86d033d5a6cc25cd3d441cfe1
Chocolatey Community <chocolatey@realdimensions.net>
2021-09-28 00:13:21 +0000

    AU: 3 updated - juju mixxx paint.net [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8bb0b7f2e823f68aed00022d70827cc87fbe0b97

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-847-geff27abd57-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-848-g8edf18d69c-win64.msi"
 
   softwareName   = 'Mixxx *'
 
