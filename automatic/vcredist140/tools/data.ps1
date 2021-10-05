248d4334e01c34ab8a8328cd2525b8ae1af7decc
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-05 20:25:37 +0000

    AU: 7 updated - apache-httpd chromium protoc simplewall simplewall.install simplewall.portable vcredist140 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c0f70a5e3a1e20eeea6338627155a11acddb9537

--- automatic/vcredist140/tools/data.ps1
+++ automatic/vcredist140/tools/data.ps1
@@ -1,12 +1,12 @@
 ﻿$installData32 = @{
-  Url = 'https://download.visualstudio.microsoft.com/download/pr/221ed2ae-1269-497b-a962-e113045001fa/1ACD8D5EA1CDC3EB2EB4C87BE3AB28722D0825C15449E5C9CEEF95D897DE52FA/VC_redist.x86.exe'
-  Checksum = '1acd8d5ea1cdc3eb2eb4c87be3ab28722d0825c15449e5c9ceef95d897de52fa'
+  Url = 'https://download.visualstudio.microsoft.com/download/pr/73b58d04-0049-47d1-9f54-1784792c71cd/80C7969F4E05002A0CD820B746E0ACB7406D4B85E52EF096707315B390927824/VC_redist.x86.exe'
+  Checksum = '80c7969f4e05002a0cd820b746e0acb7406d4b85e52ef096707315b390927824'
   ChecksumType = 'sha256'
 }
 
 $installData64 = @{
-  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/7239cdc3-bd73-4f27-9943-22de059a6267/003063723B2131DA23F40E2063FB79867BAE275F7B5C099DBD1792E25845872B/VC_redist.x64.exe'
-  Checksum64 = '003063723b2131da23f40e2063fb79867bae275f7b5c099dbd1792e25845872b'
+  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/d3cbdace-2bb8-4dc5-a326-2c1c0f1ad5ae/9B9DD72C27AB1DB081DE56BB7B73BEE9A00F60D14ED8E6FDE45DAB3E619B5F04/VC_redist.x64.exe'
+  Checksum64 = '9b9dd72c27ab1db081de56bb7b73bee9a00f60d14ed8e6fde45dab3e619b5f04'
   ChecksumType64 = 'sha256'
 }
 
@@ -15,7 +15,7 @@ $uninstallData = @{
 }
 
 $otherData = @{
-  ThreePartVersion = [version]'14.29.30133'
+  ThreePartVersion = [version]'14.29.30135'
   FamilyRegistryKey = '14.0'
   PackageName = 'vcredist140'
 }
