db4f866f5aa633ff29dcbaf2fc9a191912d0e28a
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-05 00:13:09 +0000

    AU: 5 updated - mixxx paint.net python python3 vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/edba77845f308f275e43c5b21430205b4feab3dd

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.3469_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.3469_x64.zip"
+  file          = "$toolsDir\gvim_8.2.3475_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.3475_x64.zip"
 }
 
 $installArgs = @{
