84d5b786c2cb075b768e94505e2fdfd8170598fa
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-10-06 15:34:06 -0500

    (win10mct) Add parameters for start menu and desktop shortcuts
    Adds /startmenushortcut and /desktopshortcut parameters to create start
    menu and desktop shortcut respectively.
    Fixes chocolatey-community/chocolatey-coreteampackages#1694

--- automatic/win10mct/tools/chocolateyUninstall.ps1
+++ automatic/win10mct/tools/chocolateyUninstall.ps1
@@ -1,6 +1,28 @@
 ﻿$exeName = "MediaCreationTool.exe"
 $AppPathKey = "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\$exeName"
 
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$shortcutName = 'Windows Media 10 Creation Tool.lnk'
+$exePath = Join-Path $toolsDir $exeName 
+
 #Uninstall-ChocolateyZipPackage $packageName windows10-media-creation-tool.zip
 
 If (Test-Path $AppPathKey) {Remove-Item "$AppPathKey" -Force -Recurse -EA SilentlyContinue | Out-Null}
+
+$desktopshortcut = (Join-Path ([System.Environment]::GetFolderPath("Desktop")) $shortcutName)
+$startshortcut = (Join-Path ([System.Environment]::GetFolderPath("Programs")) $shortcutName)
+
+if (Test-Path $desktopshortcut) {
+	Remove-Item $desktopshortcut
+	Write-Host -ForegroundColor white 'Removed ' $desktopshortcut
+} else {
+	Write-Host -ForegroundColor yellow 'Did not find ' $desktopshortcut 'to remove'
+}
+
+if (Test-Path $startshortcut) {
+	Remove-Item $startshortcut
+	Write-Host -ForegroundColor white 'Removed ' $startshortcut
+} else {
+	Write-Host -ForegroundColor yellow 'Did not find ' $startshortcut 'to remove'
+}
