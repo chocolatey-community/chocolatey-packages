8201c0b7e92731829d3db441925a249b1839b569
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-08 00:14:00 +0000

    AU: 3 updated - mixxx thunderbird vscode [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/66ffe7298eb82ee1caf4934d4faa09da7037cf7a

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-868-g64a90497fc-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-870-g7618abfad7-win64.msi"
 
   softwareName   = 'Mixxx *'
 
