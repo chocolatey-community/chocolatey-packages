d4731d6cf1f7e590abcd4e1b8be3dc1b3db1c9bd
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-09 12:13:07 +0000

    AU: 2 updated - chromium maxthon.commandline [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/34f58cacee28233a12ebbc311f42eb773801fad9

--- automatic/maxthon.commandline/tools/chocolateyInstall.ps1
+++ automatic/maxthon.commandline/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
-  url          = 'https://dl.maxthon.com/mx6/maxthon_portable_6.1.2.1000_x86.zip'
-  checksum     = 'd9e1d4e63d7b28e5fd1317c1f4a7f8b4bf04d8b5d066dfb65222333ea1964405'
+  url          = 'https://dl.maxthon.com/mx6/maxthon_portable_6.1.2.3000_x86.zip'
+  checksum     = '9ede7e558fadcd17e9f937161f059b7178a95d48d26bf23eb47bc57cfa06ac5e'
   checksumType = 'sha256'
   destination  = $toolsDir
 }
