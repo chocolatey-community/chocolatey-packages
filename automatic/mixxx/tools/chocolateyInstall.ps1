d7c502f5cffe0f9d21d01acff21840a90dd41830
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-14 16:57:26 +0000

    AU: 5 updated - cmake cmake.install cutepdf mixxx owncloud-client [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/31ca6910a51da64eba2515d3ac554a38ba8a24c3

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-913-gcdb881ecd6-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-915-g74a0ec9a02-win64.msi"
 
   softwareName   = 'Mixxx *'
 
