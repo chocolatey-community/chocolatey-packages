42927d163b8d46d74211aabdaf90eb3a699710e8
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-18 00:13:02 +0000

    AU: 2 updated - mixxx vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/bee5d961c932c106fcb56f032e6d324ac244ecc7

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-942-gf83869ba54-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-946-g581803a5af-win64.msi"
 
   softwareName   = 'Mixxx *'
 
