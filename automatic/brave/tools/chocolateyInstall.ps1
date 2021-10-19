13f1624943c27cbb92b0997a783a2cf45cac2760
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-19 12:14:03 +0000

    AU: 2 updated - brave vscode-insiders [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/64fe243dd5aa6687890e9a625cd6e87452c8d560

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.31.86'
+[version]$softwareVersion = '1.31.87'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
