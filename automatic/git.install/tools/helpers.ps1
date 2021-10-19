6b29aa1e3d258a6020fcd24932eb46b7d0d7b6a4
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2021-10-19 06:26:11 +0200

    (git.install) add new package parameters

--- automatic/git.install/tools/helpers.ps1
+++ automatic/git.install/tools/helpers.ps1
@@ -1,11 +1,30 @@
 ﻿function Get-InstallOptions( [HashTable]$pp )
 {
+    # Options are defined in this [file](https://github.com/git-for-windows/build-extra/blob/main/installer/install.iss)
+    # You can see [here](https://github.com/git-for-windows/build-extra/blob/4490974c504f1bbc07327b885ea3607ad019f736/installer/install.iss#L1140) how they are interpreted, it is all parameters passed in the ReplayChoice method without spaces.
     $options = @()
     if ($pp.GitOnlyOnPath)             { $options += "/o:PathOption=Cmd" }
     elseif ($pp.GitAndUnixToolsOnPath) { $options += "/o:PathOption=CmdTools" }
     if ($pp.WindowsTerminal)           { $options += "/o:BashTerminalOption=ConHost" }
     if ($pp.NoAutoCrlf)                { $options += "/o:CRLFOption=CRLFCommitAsIs" }
     if ($pp.SChannel)                  { $options += "/o:CURLOption=WinSSL" }
+    if ($pp.NoOpenSSH)                 { $options += "/o:SSHOption=ExternalOpenSSH" }
+    if ($pp.Symlinks)                  { $options += "/o:EnableSymlinks=Enabled" }
+    if ($pp.DefaultBranchName)         { $options += "/o:DefaultBranchOption=" + $pp.DefaultBranchName }
+    if ($pp.PseudoConsoleSupport)      { $options += "/o:EnablePseudoConsoleSupport=Enabled" }
+    if ($pp.FSMonitor)                 { $options += "/o:EnableFSMonitor=Enabled" }
+
+    if($pp.Editor)
+    {
+      if ($pp.Editor -in @('Atom', 'Nano', 'Notepad', 'Notepad++', 'SublimeText', 'VIM', 'VisualStudioCode', 'VisualStudioCodeInsiders', 'VSCodium', 'Wordpad'))
+      {
+        $options += "/o:EditorOption=" + $pp.Editor
+      }
+      else {
+        $options += "/o:EditorOption=CustomEditor"
+        $options += "/o:CustomEditorPath=" + $pp.Editor
+      }
+    }
     return $options
 }
 
@@ -19,6 +38,8 @@ function Get-InstallComponents( [HashTable]$pp )
         Write-Host "Using Git LFS"
         $res += 'gitlfs'
     }
+
+    if ($pp.WindowsTerminalProfile ) { $res += "windowsterminal" }
 
     # Make our install work properly when running under SYSTEM account (Chef Client Service, Puppet Service, etc)
     $isSystem = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsSystem
