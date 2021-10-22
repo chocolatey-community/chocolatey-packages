f19f03a191ba23aebe55b031d757c80a634f4379
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-22 18:13:52 +0000

    AU: 2 updated - dropbox mattermost-desktop [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/848f32ef332ab29c3427b89d26efe4eff92ebf3b

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '134.3.4109'
+$version = '134.3.4111'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20134.3.4109%20Offline%20Installer.exe'
-    checksum       = '928e5c9dfa1136b35c21b15dd01d8455f5b1e7f4a28c330f7251fbb53d6d5d40'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20134.3.4111%20Offline%20Installer.exe'
+    checksum       = '99c57486a95ffb33f6046db259c9bbe7136f9adc47619d7c24231953677d23aa'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
