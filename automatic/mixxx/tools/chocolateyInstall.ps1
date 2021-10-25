abdb7ff5ff71cc2901c02144814ffb9fe65d418f
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-25 00:13:16 +0000

    AU: 5 updated - mixxx vim winscp winscp.install winscp.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5e17f8b3e7b2f7939616badf77d62993f253bddd

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-959-g6e04d58712-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-961-gc99b8ba50c-win64.msi"
 
   softwareName   = 'Mixxx *'
 
