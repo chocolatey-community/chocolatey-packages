d9d144c57e87af7bf8c9305a1cbcec24a176e51b
RB <RedBaron2@users.noreply.github.com>
2021-10-27 18:55:44 -0700

    Update update_helper.ps1
    Updated the helper to find the url and version better due to upstream site changes

--- automatic/wps-office-free/update_helper.ps1
+++ automatic/wps-office-free/update_helper.ps1
@@ -17,18 +17,41 @@ return $PackageUrl
 
 }
 
+function Get-ResultInformation() {
+param(
+	[string]$url32,
+	[string]$file,
+	[string]$algorithm = 'sha512',
+	[switch]$info
+)
+
+  $dest = "$env:TEMP\$file"
+  Invoke-WebRequest -UseBasicParsing -Uri $url32 -OutFile $dest
+  $version = Get-Item $dest | Foreach { $_.VersionInfo.ProductVersion -replace '^(\d+(\.[\d]+){1,3}).*', '$1' }
+
+  Remove-Item -Force $dest
+  if ($info){
+    return $version
+  } else {
+    $result = @{
+    URL32          = $url32
+    Version        = $version
+    Checksum32     = (Get-FileHash $dest -Algorithm $algorithm | Foreach Hash)
+    ChecksumType32 = $algorithm
+    }
+    return $result
+  }
+}
+
 function Get-JavaSiteUpdates {
 # $wait number can be adjusted per the package needs
- param(
+param(
 	[string]$package,
-  [string]$Title,
-  [string]$padVersionUnder = '10.2.1',
-  [string]$wait = 4
+	[string]$Title,
+	[string]$padVersionUnder = '10.2.1',
+	[string]$wait = 4
  )
- 
-$OS_caption = ( Get-CimInstance win32_operatingsystem -Property Caption )
-$check = @{$true=$true;$false=$false}[ ( $OS_caption -match 'Server' ) ]
-$regex = '([\d]{0,2}[\.][\d]{0,2}[\.][\d]{0,2}[\.][\d]{0,5})'
+
 $url = Get-PackageName $package
 $ie = New-Object -comobject InternetExplorer.Application
 $ie.Navigate2($url) 
@@ -36,36 +59,20 @@ $ie.Visible = $false
 while($ie.ReadyState -ne $wait) {
  start-sleep -Seconds 20
 }
-	if ( $check ) {
-		$url32 = $ie.Document.IHTMLDocument3_getElementsByTagName("a") | % { $_.href } | where { $_ -match $regex } | select -First 1
-	} else {
-		$url32 = $ie.Document.getElementsByTagName("a") | % { $_.href } | where { $_ -match $regex } | select -First 1
-	}
-  
-    foreach ( $_ in $ie.Document.getElementsByTagName("a") ) {
-     $url = $_.href;
-         if ( $url -match $regex) {
-            $yes = $url | select -last 1
-            $version = $Matches[0]
-            $the_match = $yes -match( $rev_regex );
-            $revision = $Matches[0];
-            break;
-        }
-    }
-$ie.quit()
-if ($package -match 'wps') {
-$version = $version
-} else {
-$version = $version + $revision
-}
+# Server detection not needed anymore currently 10/27/2021
+$url = $ie.Document.getElementsByTagName("a") | % { $_.href } | select -First 2 | Select -Last 1
 
+# New Version from Get-ResultInformation due to downloading file
+$version = ((Get-ResultInformation $url -file "${package}.exe" -info) -replace(",","."))
+
+$ie.quit()
 
 	@{    
 		PackageName = $package
 		Title       = $Title
 		fileType    = 'exe'
 		Version		= Get-FixVersion $version -OnlyFixBelowVersion $padVersionUnder
-		URL32		= $yes
+		URL32		= $url
     }
 
 }
