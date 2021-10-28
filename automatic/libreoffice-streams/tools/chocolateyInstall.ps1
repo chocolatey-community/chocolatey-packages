132f1e3ae66c8540c6ba5a69ec1d257c2b2da946
Chocolatey Community <chocolatey@realdimensions.net>
2021-10-28 18:13:21 +0000

    AU: 6 updated - cmake.install cmake.portable libreoffice-fresh smartftp tixati tixati.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e187e60aca27035379864eed5c12fb35fbebda5b

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -4,13 +4,13 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
 $packageArgs = @{
-  packageName            = 'libreoffice-still'
-  version                = '7.1.0'
+  packageName            = 'libreoffice-fresh'
+  version                = '7.2.2'
   fileType               = 'msi'
-  url                    = 'https://downloadarchive.documentfoundation.org/libreoffice/old/7.1.0.3/win/x86/LibreOffice_7.1.0.3_Win_x86.msi'
-  url64bit               = 'https://downloadarchive.documentfoundation.org/libreoffice/old/7.1.0.3/win/x86_64/LibreOffice_7.1.0.3_Win_x64.msi'
-  checksum               = '258bae103cc76b01b943814c25fd37d1373c2b2e23a1f6c92d268510e708ed5f'
-  checksum64             = '5ddb51955849dc4fe39933047c1b65d044ec5b01ff88e8b81e0a15d8de638aed'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.2.2/win/x86/LibreOffice_7.2.2_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.2.2/win/x86_64/LibreOffice_7.2.2_Win_x64.msi'
+  checksum               = '4486203b94812a7f4da62111bfbfed46dccf6402be036a5f8efebf76635f46d2'
+  checksum64             = '422e733813bd853f37ef7731d75e554e73d3230c78b9bd58e39c0a8c2b6a45d0'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
