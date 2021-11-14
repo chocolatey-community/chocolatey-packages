187b0b366ee6023a4547ea7abc04fdbdd9bc84a6
Chocolatey Community <chocolatey@realdimensions.net>
2021-11-14 00:13:23 +0000

    AU: 8 updated - bleachbit bleachbit.install mixxx notepadplusplus notepadplusplus.commandline notepadplusplus.install smartftp vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8ef1c1614b0bfde813ace78597b81ac880b140e6

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-alpha-985-gbfd5b1d5ee-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4-alpha-988-gdea9f71785-win64.msi"
 
   softwareName   = 'Mixxx *'
 
