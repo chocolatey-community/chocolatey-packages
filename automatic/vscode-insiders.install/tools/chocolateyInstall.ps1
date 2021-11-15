8bb877dc0409a0e032edbccf85bdf63a0ae362fa
Chocolatey Community <chocolatey@realdimensions.net>
2021-11-15 12:14:05 +0000

    AU: 1 updated - vscode-insiders.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f9f36a9315ad8b563d696b60a7b07e72b394db55

--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/3bd29c1d6c6ef5b6265614def99b7257b671ae19/VSCodeSetup-ia32-1.63.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/3bd29c1d6c6ef5b6265614def99b7257b671ae19/VSCodeSetup-x64-1.63.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/035eaab2cb31798c6ae90a35928c62117cb52fcd/VSCodeSetup-ia32-1.63.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/035eaab2cb31798c6ae90a35928c62117cb52fcd/VSCodeSetup-x64-1.63.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = 'fc53176d08c474a2c758af95b1dc78f2bd3d98c5ba817ca7161c1911c6aa0a407127475e7d3686d525aea160c20c35ba807869943115db8ec69c26d2a6eea8b4'
+  checksum       = 'b45f647b716555f32cc58cc5bdb109a6903c045e8ec5f9440092eea4004e60de2c21a06dcdf5b82b15b16dfa5e3ba113680f055dddfd01fcc30365259fff88ec'
   checksumType   = 'sha512'
-  checksum64     = '08d7713c51daa3f95101f3d3fbeda1d36a0c2e91f8639a6d36160d94929d193832530de84596b7269a07dd8e993774caadcaffd5296dad3d1f5436732c2f59d9'
+  checksum64     = '060f01423a4e3083e391c54e2b259eeb0e1d3fc2a313f5b8677feb466410f57fcc75c883389d1d34d6cb01e9002d52ef0bfd83d711452eeebfc8e1fa2c90f6a2'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
