ce59378cd506ebef9882ac9ab50b6f3189594b74
Chocolatey Community <chocolatey@realdimensions.net>
2021-11-22 00:14:39 +0000

    AU: 4 updated - chromium mixxx paint.net vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ccfd006b002819dc7c58606547d342e9fdd6fa99

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.3635_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.3635_x64.zip"
+  file          = "$toolsDir\gvim_8.2.3640_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.3640_x64.zip"
 }
 
 $installArgs = @{
