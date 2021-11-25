a7c32e6441c7c921c9f155c64531edab64ea08cd
Chocolatey Community <chocolatey@realdimensions.net>
2021-11-25 00:13:12 +0000

    AU: 3 updated - brave smartftp vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/fce38d3a7367cfdacbf978b00dd0c4140bfd1930

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.32.112'
+[version]$softwareVersion = '1.32.113'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
