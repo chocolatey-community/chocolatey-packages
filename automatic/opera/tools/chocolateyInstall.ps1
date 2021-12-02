296125f58db7d2e0f0f382faf2dc204bf23a7376
Chocolatey Community <chocolatey@realdimensions.net>
2021-12-02 12:13:46 +0000

    AU: 4 updated - brave Opera vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e90e9c1a742181b9661c452dc74a2660f128cb98

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/81.0.4196.60/win/Opera_81.0.4196.60_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/81.0.4196.60/win/Opera_81.0.4196.60_Setup_x64.exe'
-  checksum       = '59e483395c733dda13bd42e3b731b5098ac74bf51b530540c77e85fd9ddbd69c'
-  checksum64     = '7503a19801652cc5c661bda8e2739be1d13849ccb232d056bff1f80ed21ad3a3'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/82.0.4227.23/win/Opera_82.0.4227.23_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/82.0.4227.23/win/Opera_82.0.4227.23_Setup_x64.exe'
+  checksum       = 'a977afad9417891b3c741127babfb0a19ea700fe9c9da120d397e119528af446'
+  checksum64     = 'aa879604f1dbb29bcee0fb2192d51fb09e75d133f551d9489bc4e8902517a544'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '81.0.4196.60'
+$version = '82.0.4227.23'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/0cc0904c565399781defa830facf43141db8b6f3/VSCodeSetup-ia32-1.63.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/0cc0904c565399781defa830facf43141db8b6f3/VSCodeSetup-x64-1.63.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/68a1e2f872f8869b004eec284917782474730e0b/VSCodeSetup-ia32-1.63.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/68a1e2f872f8869b004eec284917782474730e0b/VSCodeSetup-x64-1.63.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = '6c53bef17cda01f0050807b9da379706d2eec23e165b677b559475297cb00ac7830c70325993d5463ca2c98f55ac42e1bfa29168ce75f8744852b0880ed8ad24'
+  checksum       = '1ff2036654120b6f558372cb907588841de59d6c39c596d9aaca6868c7dd5e976306c165b54d02e1addd591f1598521038a042ff759a3c2eb27d79bef15cd264'
   checksumType   = 'sha512'
-  checksum64     = 'adb596d4ed46565758ec4a44a01125a86adc2d4145a02f54dec3f91acd8b27e34e7f01b019b592ce885dc1387e7ed38b89f475565cc9116b42529af822945444'
+  checksum64     = 'c2acdc6244e957c8e14246acf199ed89e721828ef34523179241c38252c145b03510e58a82c21093758eb6bf42d72746dc4b6f7d9f56b6f36b53e95212ddf00a'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
