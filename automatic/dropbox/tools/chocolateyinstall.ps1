cd2b3061d29c0ab7893268a00d2b296ac63a93ce
Chocolatey Community <chocolatey@realdimensions.net>
2021-12-03 18:30:41 +0000

    AU: 3 updated - chromium dropbox owncloud-client [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0057bb78e525d5c3e4989330937cdc9c51f78630

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '137.3.6396'
+$version = '137.3.6403'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20137.3.6396%20Offline%20Installer.exe'
-    checksum       = '94f22e02280e06e452d5b82ae90abe5e5688ba0ee798aa4271dd1102eca9f1a7'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20137.3.6403%20Offline%20Installer.exe'
+    checksum       = 'd9ca5c9a85dc72a0f06cfe21a36a7d9c74c7669a11fcbba45685e0b49dfea02a'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
