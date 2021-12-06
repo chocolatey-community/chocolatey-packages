bed074e7646e03580c139fb144413b074d2357c9
Chocolatey Community <chocolatey@realdimensions.net>
2021-12-06 06:27:27 +0000

    AU: 3 updated - brave chromium libreoffice-still [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d8b02f9ad3def5078dd9a7112c3a1bdf144ee88c

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.33.99'
+[version]$softwareVersion = '1.33.100'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = 'libreoffice-still'
-  version                = '7.1.7'
+  version                = '7.1.8'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.1.7/win/x86/LibreOffice_7.1.7_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.1.7/win/x86_64/LibreOffice_7.1.7_Win_x64.msi'
-  checksum               = '1ec0e016820f2165b147120057d888c121fdccfd95406ec6a0f4eb5a6b9c8a06'
-  checksum64             = '7a15d4d26ce04d65d6d4e1b58919168f1c1b8ef2eb2147785bdad4e1fff25092'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.1.8/win/x86/LibreOffice_7.1.8_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.1.8/win/x86_64/LibreOffice_7.1.8_Win_x64.msi'
+  checksum               = '1a7fd97895d7070b84110fb5f156fdc05c599c01a5c683cf56a94d00fedc68a3'
+  checksum64             = '79e5427c8b02777717c0c714ebbc37c6ca5e9a267e4de6d0d3962b78297e92f5'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
