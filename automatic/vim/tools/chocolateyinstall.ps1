c7103e0da7181cbccd9c8f5ad037d87a5186acb5
Chocolatey Community <chocolatey@realdimensions.net>
2021-12-10 00:20:53 +0000

    AU: 3 updated - chromium typescript vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e16b7bf1b23486ab546451ce7504b41331aa2da9

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.3761_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.3761_x64.zip"
+  file          = "$toolsDir\gvim_8.2.3770_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.3770_x64.zip"
 }
 
 $installArgs = @{
