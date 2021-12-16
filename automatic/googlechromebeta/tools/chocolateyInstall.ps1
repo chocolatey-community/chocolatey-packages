3c3f3dd4fc6be4d26ed3b5acf9b3f7f1e214ef72
Chocolatey Community <chocolatey@realdimensions.net>
2021-12-16 00:14:09 +0000

    AU: 4 updated - chromium googlechromebeta smartftp vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/a045ab0ccd1fe69d0a4f568a8f7ae56d9bd08455

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '97.0.4692.45-beta'
+$version = '97.0.4692.56-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = '631f91ee721475809b2f437db7b27ff6013c5aa3eb32299ac3c1fd94caf1387f'
-  checksum64             = '972c25bf506d62941cde7673b348a89227ea764db05cbeab6fe15201d96cba11'
+  checksum               = 'e235bec8c8225fe09aabd728e2710308b30bb024c3657dcf6060029e2b8c9a4a'
+  checksum64             = 'b80ca98b3cdfb244bc06bdd0b0b92b3c4b42a30d699289fb98143fc4b8de9d68'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.3811_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.3811_x64.zip"
+  file          = "$toolsDir\gvim_8.2.3821_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.3821_x64.zip"
 }
 
 $installArgs = @{
