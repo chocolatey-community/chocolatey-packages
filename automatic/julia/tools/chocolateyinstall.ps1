64d31f4dc7f27eaa6e2c0fcaa9401ba7158b8144
Chocolatey Community <chocolatey@realdimensions.net>
2021-12-24 00:13:21 +0000

    AU: 3 updated - chromium julia vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ad8528d0a811680c5604e14901168d6911b42f77

--- automatic/julia/tools/chocolateyinstall.ps1
+++ automatic/julia/tools/chocolateyinstall.ps1
@@ -5,15 +5,15 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\julia-1.7.0-win32.exe"
-  file64        = "$toolsDir\julia-1.7.0-win64.exe"
+  file          = "$toolsDir\julia-1.7.1-win32.exe"
+  file64        = "$toolsDir\julia-1.7.1-win64.exe"
 
   softwareName  = 'Julia*'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
-$packageVersion = "1.7.0"
+$packageVersion = "1.7.1"
 
 Install-ChocolateyInstallPackage @packageArgs
 
