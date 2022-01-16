418cbbf4ade22570b4dbe64f816a426d6932fbee
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2022-01-16 15:31:54 -0600

    (ruby) AU streams for minor releases

--- automatic/ruby/update.ps1
+++ automatic/ruby/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://rubyinstaller.org/downloads'
+$releases = 'https://rubyinstaller.org/downloads/archives/'
 
 function global:au_SearchReplace {
    @{
@@ -21,17 +21,41 @@ function global:au_SearchReplace {
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
 
+function GetStreams() {
+  param($releaseUrls)
+  $streams = @{ }
+  
+  $re64           = 'x64\.exe$'
+  $x64releaseUrls = $releaseUrls | ? href -match $re64
+
+  $x64releaseUrls | % {
+    $version = $_ -replace '\-([\d]+)','.$1' -replace 'rubyinstaller.' -replace 'ruby.' -split '/' | select -Last 1 -Skip 1
+    if ($version -match '[a-z]') { Write-Host "Skipping prerelease: '$version'"; return }
+    $versionTwoPart = $version -replace '([\d]+\.[\d]+).*', "`$1"
+
+    if ($streams.$versionTwoPart) { return }
+    
+    $url64 = $_ | Select-Object -ExpandProperty href
+    $url32 = $releaseUrls | ? href -notmatch $re64 | ? href -match $version | Select-Object -ExpandProperty href
+    
+    if (!$url32 -or !$url64) {
+      Write-Host "Skipping due to missing installer: '$version'"; return
+    }
+
+    $streams.$versionTwoPart = @{ URL32 = $url32 ; URL64 = $url64 ; Version = Get-FixVersion -Version $version -OnlyFixBelowVersion "2.5.4" }
+  }
+
+  Write-Host $streams.Count 'streams collected'
+  $streams
+}
+
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $re    = '\.exe$'
-    $url   = $download_page.links | ? href -match $re | ? {$_ -notmatch 'devkit'} | select -First 2 -expand href
-    $version = $url[0] -replace '\-([\d]+)','.$1' -replace 'rubyinstaller.' -split '/' | select -Last 1 -Skip 1
 
-    @{
-        Version      = Get-FixVersion -Version $version -OnlyFixBelowVersion "2.5.4"
-        URL32        = $url -notmatch 'x64' | select -first 1
-        URL64        = $url -match 'x64'    | select -first 1
-    }
+    $re            = '\.exe$'
+    $releaseUrls   = $download_page.links | ? href -match $re | ? {$_ -notmatch 'devkit'} 
+    
+    @{ Streams = GetStreams $releaseUrls }
 }
 
 update -ChecksumFor none
