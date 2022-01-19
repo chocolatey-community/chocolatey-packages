0b50f5c46e59e3dc317e788698a9c834da46f89a
Chocolatey Community <chocolatey@realdimensions.net>
2022-01-19 00:13:56 +0000

    AU: 3 updated - msys2 nodejs vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b4a3d7b79c8922b33a5171685a9f2900a937c8aa

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.4127_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.4127_x64.zip"
+  file          = "$toolsDir\gvim_8.2.4141_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.4141_x64.zip"
 }
 
 $installArgs = @{
