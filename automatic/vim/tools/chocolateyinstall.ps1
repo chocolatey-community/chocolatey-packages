ad6163dbdae26f7a9201bab031b34b885fb4f7c9
Chocolatey Community <chocolatey@realdimensions.net>
2022-01-26 06:13:38 +0000

    AU: 2 updated - kubernetes-cli vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/478eb70d4e74ce3fd21dbeb804d3577211b567cf

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.4212_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.4212_x64.zip"
+  file          = "$toolsDir\gvim_8.2.4219_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.4219_x64.zip"
 }
 
 $installArgs = @{
