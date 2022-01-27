3de5a044428aa7e1311e7030771ab4aa2a76296d
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2022-01-27 16:51:14 -0600

    (ruby.portable) update according to PR review comments

--- automatic/ruby.portable/tools/chocolateyInstall.ps1
+++ automatic/ruby.portable/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  FileFullPath   = Join-Path $toolsDir 'rubyinstaller-3.1.0-1-x86_x32.7z'
-  FileFullPath64 = Join-Path $toolsDir 'rubyinstaller-3.1.0-1-x64_x64.7z'
-  Destination    = Join-Path $toolsDir 'ruby'
-  PackageName    = 'ruby.portable'
+  File        = "$toolsPath\"
+  File64      = "$toolsPath\"
+  Destination = "$toolsDir\ruby"
+  PackageName = $env:ChocolateyPackageName
 }
 
 Get-ChocolateyUnzip @packageArgs
 
-Get-ChildItem $toolsDir\*.7z | ForEach-Object { Remove-Item $_ -ea 0 }
+Get-ChildItem $toolsPath\*.7z | ForEach-Object { Remove-Item $_ -ea 0 }
--- automatic/ruby.portable/update.ps1
+++ automatic/ruby.portable/update.ps1
@@ -3,42 +3,41 @@ import-module au
 $releases = 'https://rubyinstaller.org/downloads/archives/'
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            '(?i)(^\s*FileFullPath\s*=\s*)(.*)' = "`$1Join-Path `$toolsDir '$($Latest.FileName32)'"
-            '(?i)(^\s*FileFullPath64\s*=\s*)(.*)' = "`$1Join-Path `$toolsDir '$($Latest.FileName64)'"
-        }
-
-        ".\legal\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
-          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
-          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
-          "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
+
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+      "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+      "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+      "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL64)"
+    }
+  }
 }
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function GetStreams() {
   param($releaseUrls)
   $streams = @{ }
-  
-  $re64           = 'x64\.7z$'
+
+  $re64 = 'x64\.7z$'
   $x64releaseUrls = $releaseUrls | ? href -match $re64
 
   $x64releaseUrls | % {
-    $version = $_ -replace '\-([\d]+)','.$1' -replace 'rubyinstaller.' -replace 'ruby.' -split '/' | select -Last 1 -Skip 1
+    $version = $_ -replace '\-([\d]+)', '.$1' -replace 'rubyinstaller.' -replace 'ruby.' -split '/' | select -Last 1 -Skip 1
     if ($version -match '[a-z]') { Write-Host "Skipping prerelease: '$version'"; return }
     $versionTwoPart = $version -replace '([\d]+\.[\d]+).*', "`$1"
 
     if ($streams.$versionTwoPart) { return }
-    
+
     $url64 = $_ | Select-Object -ExpandProperty href
     $url32 = $releaseUrls | ? href -notmatch $re64 | ? href -match $version | Select-Object -ExpandProperty href
-    
+
     if (!$url32 -or !$url64) {
       Write-Host "Skipping due to missing installer: '$version'"; return
     }
@@ -51,15 +50,12 @@ function GetStreams() {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re            = '\.7z$'
-    $releaseUrls   = $download_page.links | ? href -match $re | ? {$_ -notmatch 'doc'} 
-    
-    @{ Streams = GetStreams $releaseUrls }
+  $re = '\.7z$'
+  $releaseUrls = $download_page.links | ? href -match $re | ? { $_ -notmatch 'doc' }
+
+  @{ Streams = GetStreams $releaseUrls }
 }
 
-if ($MyInvocation.InvocationName -ne '.') {
-  # run the update only if script is not sourced by the virtual package ruby
-  update -ChecksumFor none
-}
+update -ChecksumFor none
