a9f1cb6f325a7c459b749827d97584847b29ec94
Chocolatey Community <chocolatey@realdimensions.net>
2022-02-01 00:13:43 +0000

    AU: 4 updated - chromium tortoisegit vim Waterfox [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/10c0bcf4876c4294ec835dd1170d1dc3448bd726

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.4266_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.4266_x64.zip"
+  file          = "$toolsDir\gvim_8.2.4277_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.4277_x64.zip"
 }
 
 $installArgs = @{
