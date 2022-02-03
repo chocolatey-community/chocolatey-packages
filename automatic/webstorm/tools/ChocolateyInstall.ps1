3b1da04091cd873db1bf151b4c75f5842a352d62
Chocolatey Community <chocolatey@realdimensions.net>
2022-02-03 18:13:48 +0000

    AU: 3 updated - brave chromium webstorm [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4ceb5e9cdb7819c841b6833f60774d60bc4a32d0

--- automatic/webstorm/tools/ChocolateyInstall.ps1
+++ automatic/webstorm/tools/ChocolateyInstall.ps1
@@ -19,8 +19,8 @@ New-Item -ItemType Directory -Force -Path $installDir
 $arguments              = @{
     packageName         = $env:ChocolateyPackageName
     softwareName        = 'JetBrains WebStorm*'
-    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2021.3.1.exe'
-    checksum            = '235bba60a1640c88f6c42b0df86acbd51200c99b747888af61bc0dd029054941'
+    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2021.3.2.exe'
+    checksum            = '98b8f5901299bb6ad74913104e72bdaf275a1dc6a1ba9664149b802f10ad26e9'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = $silentArgs
