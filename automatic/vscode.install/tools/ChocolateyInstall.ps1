fd29eac58a056bca323108694052a876d1452e25
Chocolatey Community <chocolatey@realdimensions.net>
2022-02-08 18:14:27 +0000

    AU: 5 updated - cmake cmake.install cmake.portable Firefox vscode.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ef3a152f8ffd3ec6e5f69c2647a7d6ef3e1349ae

--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.64.0'
+$version = '1.64.1'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/5554b12acf27056905806867f251c859323ff7e9/VSCodeSetup-ia32-1.64.0.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/5554b12acf27056905806867f251c859323ff7e9/VSCodeSetup-x64-1.64.0.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/d6ee99e4c045a6716e5c653d7da8e9ae6f5a8b03/VSCodeSetup-ia32-1.64.1.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/d6ee99e4c045a6716e5c653d7da8e9ae6f5a8b03/VSCodeSetup-x64-1.64.1.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = '8c17e9df40e47ec1f6c42c13e77748c42387cf802af775efd26c2ceb140ce0e1'
+  checksum       = '7380307a2db822aad8cbc827cef9add76e538566e26adfb47e33f33236e191a2'
   checksumType   = 'sha256'
-  checksum64     = '6999b6031217b5e5817bbff3335713d6fa341720b517f81796860e03d89ea77e'
+  checksum64     = 'd31a70f5f31c2c3ecb10e5056bd3aab09d600f8699cab6f5f06ba82eb765ee60'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
