6ae3ff6f7da26eb54073be2117051ee4cc80d936
Chocolatey Community <chocolatey@realdimensions.net>
2022-02-17 07:20:49 +0000

    AU: 3 updated - brave chromium ruby.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4a2d26eedf3883792e4c9ea5bcf0b83adb47e581

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.36.94'
+[version]$softwareVersion = '1.36.97'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
--- automatic/ruby.portable/tools/chocolateyInstall.ps1
+++ automatic/ruby.portable/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  File        = "$toolsPath\rubyinstaller-3.1.0-1-x86.7z"
-  File64      = "$toolsPath\rubyinstaller-3.1.0-1-x64.7z"
+  File        = "$toolsPath\rubyinstaller-3.0.3-1-x86.7z"
+  File64      = "$toolsPath\rubyinstaller-3.0.3-1-x64.7z"
   Destination = "$toolsDir\ruby"
   PackageName = $env:ChocolateyPackageName
 }
