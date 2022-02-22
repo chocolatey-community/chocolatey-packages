da0949023992e9b0ebe8876b5e50b4e1c672644e
Chocolatey Community <chocolatey@realdimensions.net>
2022-02-22 18:14:48 +0000

    AU: 4 updated - wesnoth winscp winscp.install winscp.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f6b3804160378cf113260b6268c5af9e3f96e43e

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.16/wesnoth-1.16.2/wesnoth-1.16.2-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.1/wesnoth-1.17.1-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = 'c890bb91fbf38c26881ad1f2f5ceb3d9cb5b5565da8ccb43fac2bd407ce167b3'
+  checksum       = '0791532d16385e8529f45711573000f56f1fed7e54ef0c6da6a8d186dd1921c9'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
