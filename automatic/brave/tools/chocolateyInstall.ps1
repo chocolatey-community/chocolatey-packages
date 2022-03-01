5cbc737330beca85f7ce6cb61c4b417a5825fad2
Chocolatey Community <chocolatey@realdimensions.net>
2022-03-01 12:15:15 +0000

    AU: 1 updated - brave [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f196e5f24ad7a14706a91717e49626d5e094e804

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.37.72'
+[version]$softwareVersion = '1.37.77'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
