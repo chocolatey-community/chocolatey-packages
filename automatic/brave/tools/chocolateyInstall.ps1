44055eb1cc8ef2ac1179d23c1be4b6bef626af5c
Chocolatey Community <chocolatey@realdimensions.net>
2022-03-03 12:13:42 +0000

    AU: 3 updated - brave chromium spotify [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b37e36bdad9b9026624323dfa1487eaa3dd4cbbf

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.37.77'
+[version]$softwareVersion = '1.37.80'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
