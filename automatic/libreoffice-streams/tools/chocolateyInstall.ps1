eee2df69979b9feb8f52f87ea3dae31b1123f0cb
Chocolatey Community <chocolatey@realdimensions.net>
2022-03-09 18:32:33 +0000

    AU: 4 updated - blender libreoffice-still nodejs nodejs.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1ffffc4736ab538a7d42c7ee73d33d794bc5cab1

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -4,13 +4,13 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
 $packageArgs = @{
-  packageName            = 'libreoffice-fresh'
-  version                = '7.2.5'
+  packageName            = 'libreoffice-still'
+  version                = '7.2.0'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.2.5/win/x86/LibreOffice_7.2.5_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.2.5/win/x86_64/LibreOffice_7.2.5_Win_x64.msi'
-  checksum               = '34d3514e33ea1f9acb029219381c1446d7d29a95d6c81ad0fae95035ec62c04f'
-  checksum64             = 'dfbf02fd97515e06e55c53d5dc0108fb680f92146b093cd4ba9c43e87fdd0dc5'
+  url                    = 'https://downloadarchive.documentfoundation.org/libreoffice/old/7.2.0.4/win/x86/LibreOffice_7.2.0.4_Win_x86.msi'
+  url64bit               = 'https://downloadarchive.documentfoundation.org/libreoffice/old/7.2.0.4/win/x86_64/LibreOffice_7.2.0.4_Win_x64.msi'
+  checksum               = 'e3d5ff07a7e2aa0a8775c1ca303618d645ecce1496a1c179c232e38caa691cae'
+  checksum64             = 'd2969415c10ca60a9dbcfe175df48bc38209690f69326af2639c3c75853db590'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
