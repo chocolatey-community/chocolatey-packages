479f8483c5fa634e86ef1621a9d7b7ee8dc15b79
Chocolatey Community <chocolatey@realdimensions.net>
2022-03-11 18:13:24 +0000

    AU: 2 updated - mp3tag rufus [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/851c7cbf4ffff446564e8186d02d68640267e0ea

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,7 +24,7 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv312bsetup.exe"
+  file           = "$toolsPath\mp3tagv312dsetup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
