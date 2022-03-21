b9c5695970b6b9f5f307c0b605ffe837377af01d
Chocolatey Community <chocolatey@realdimensions.net>
2022-03-21 00:31:55 +0000

    AU: 3 updated - palemoon tor-browser vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/f81d2d6bd7b497153dc046789f7f295438dd3862

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-30.0.0.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-30.0.0.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-30.0.1.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-30.0.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = 'e844e30bf3a554907dda2eaf282e87d16a24993fbe5187f3d973aaf2745b1d09'
+  checksum      = '850893092bbb5c1e7ff01dd869a00c1e606be843ea1b2edceaf223cc3763b67a'
   checksumType  = 'sha256'
-  checksum64    = 'cecb33f8c238c82d9a4fd0076b573624d35b476fd984d0ee2c6747b8724d1304'
+  checksum64    = 'f2adf999be923d2c426f233d59d92ac0ce01b9ce4c8672bff597c462f49c1e7f'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
