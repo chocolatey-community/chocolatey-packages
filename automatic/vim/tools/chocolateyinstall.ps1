7252304213431b972388a4b48ec640dcf9711ede
Chocolatey Community <chocolatey@realdimensions.net>
2022-03-22 00:13:30 +0000

    AU: 2 updated - juju vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/8d444f07ed25ca727ed70ca99510004a909e6a49

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.4602_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.4602_x64.zip"
+  file          = "$toolsDir\gvim_8.2.4606_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.4606_x64.zip"
 }
 
 $installArgs = @{
