e8e049c5f0dd221737342950c1e7877033717e38
Chocolatey Community <chocolatey@realdimensions.net>
2022-03-23 18:14:05 +0000

    AU: 5 updated - chromium cmake.install cmake.portable Firefox Opera [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e36f0eb937cf75484247f129d77b9db3576f3b26

--- automatic/cmake.portable/tools/chocolateyInstall.ps1
+++ automatic/cmake.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\cmake-3.23.0-rc4-windows-i386.zip"
-  file64      = "$toolsPath\cmake-3.23.0-rc4-windows-x86_64.zip"
+  file        = "$toolsPath\cmake-3.23.0-rc5-windows-i386.zip"
+  file64      = "$toolsPath\cmake-3.23.0-rc5-windows-x86_64.zip"
   destination = $toolsPath
 }
 
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/84.0.4316.42/win/Opera_84.0.4316.42_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/84.0.4316.42/win/Opera_84.0.4316.42_Setup_x64.exe'
-  checksum       = '764ab82807ec7681cf773ced0a0c852f4ce4fdfc063558a81c4ca0a9e4d3efbf'
-  checksum64     = '8937237644fea9a97b1822a1c844bc813f3ab1432341fd567212a063b7693c41'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/85.0.4341.18/win/Opera_85.0.4341.18_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/85.0.4341.18/win/Opera_85.0.4341.18_Setup_x64.exe'
+  checksum       = '45cb7e3f0dcf1d91b3a6b0f9a0ee79d6d7e7cf171ec3ced6d63c2df294344ca4'
+  checksum64     = '040e9a7fe6acc2c4118017c528fb4b8742dc2ae8cda2f28185e9b9e6bbe4184a'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '84.0.4316.42'
+$version = '85.0.4341.18'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
