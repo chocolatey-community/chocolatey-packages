6a26e8720075cc28a978756b69a9b10728a7acad
RB <RedBaron2@users.noreply.github.com>
2022-03-24 16:30:56 -0700

    Mistake in thinking the software was installed as exe
    This will download the msi file.

--- automatic/sourcetree/update_helper.ps1
+++ automatic/sourcetree/update_helper.ps1
@@ -5,7 +5,7 @@ function Get-PackageName() {
 
   switch -w ( $a ) {
 	  'sourcetree' {
-		  $PackageUrl = "https://www.atlassian.com/software/sourcetree"
+		  $PackageUrl = "https://www.sourcetreeapp.com/enterprise"
 	  }
   }
 
@@ -15,20 +15,19 @@ function Get-PackageName() {
 function Get-JavaSiteUpdates {
   param(
     [string]$Package,
-    [string]$Title,
-    [string]$Wait = 4
+    [string]$Title
   )
 
   $regex = '([\d]{0,2}[\.][\d]{0,2}[\.][\d]{0,2}[\.][\d]{0,5})'
   $page = Invoke-WebRequest -Uri "https://www.atlassian.com/software/sourcetree" -UseBasicParsing
-  $url32 = $page.Links | ? href -match $regex | select -expand href | select -First 1
+  $url32 = ($page.Links | ? href -match $regex | select -expand href | select -First 1) -replace(".exe",".msi")
   $version = (Get-Version $url32)
 
   @{
-    PackageName  = $Package
+    PackageName    = $Package
 	  Title        = $Title
-	  Version	     = $version
-	  URL32		     = $url32
+	  Version	   = $version
+	  URL32		   = $url32
 	  ReleaseNotes = "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/ReleaseNotes_$version.html"
   }
 }
