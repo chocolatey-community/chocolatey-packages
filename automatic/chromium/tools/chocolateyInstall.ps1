b7e3b9ad68e309b9d696ac1e513aea55f9aa46a3
Chocolatey Community <chocolatey@realdimensions.net>
2022-03-28 00:14:00 +0000

    AU: 2 updated - chromium gnucash [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7456758f3327d155cf534565077e150146003205

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "102.0.4968.0-snapshots"
+$version = "102.0.4969.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
