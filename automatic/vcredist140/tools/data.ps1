d787f1afff68967a8c9d30841216a70e8cfab7b0
Chocolatey Community <chocolatey@realdimensions.net>
2022-04-02 14:36:10 +0000

    AU: 1 updated - vcredist140 [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b2b8c0d4dc990bf4d2132e1c1d153f7deaf3047e

--- automatic/vcredist140/tools/data.ps1
+++ automatic/vcredist140/tools/data.ps1
@@ -1,21 +1,21 @@
 ﻿$installData32 = @{
-  Url = 'https://download.visualstudio.microsoft.com/download/pr/144a5711-f076-44fa-bf55-f7e0121eb30c/B7AE307237F869E09F7413691A2CD1944357B5CEE28049C0A0D3430B47BB3EDC/VC_redist.x86.exe'
-  Checksum = 'b7ae307237f869e09f7413691a2cd1944357b5cee28049c0a0d3430b47bb3edc'
+  Url = 'https://download.visualstudio.microsoft.com/download/pr/7e8edcf6-f2c1-41b1-a712-3f9cd8f58db4/4C6C420CF4CBF2C9C9ED476E96580AE92A97B2822C21329A2E49E8439AC5AD30/VC_redist.x86.exe'
+  Checksum = '4c6c420cf4cbf2c9c9ed476e96580ae92a97b2822c21329a2e49e8439ac5ad30'
   ChecksumType = 'sha256'
 }
 
 $installData64 = @{
-  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/d22ecb93-6eab-4ce1-89f3-97a816c55f04/37ED59A66699C0E5A7EBEEF7352D7C1C2ED5EDE7212950A1B0A8EE289AF4A95B/VC_redist.x64.exe'
-  Checksum64 = '37ed59a66699c0e5a7ebeef7352d7c1c2ed5ede7212950a1b0a8ee289af4a95b'
+  Url64 = 'https://download.visualstudio.microsoft.com/download/pr/b929b7fe-5c89-4553-9abe-6324631dcc3a/296F96CD102250636BCD23AB6E6CF70935337B1BBB3507FE8521D8D9CFAA932F/VC_redist.x64.exe'
+  Checksum64 = '296f96cd102250636bcd23ab6e6cf70935337b1bbb3507fe8521d8d9cfaa932f'
   ChecksumType64 = 'sha256'
 }
 
 $uninstallData = @{
-  SoftwareName = 'Microsoft Visual C++ 2015-2022 Redistributable*'
+  SoftwareName = 'Microsoft Visual C++ 2015-2019 Redistributable*'
 }
 
 $otherData = @{
-  ThreePartVersion = [version]'14.31.31103'
+  ThreePartVersion = [version]'14.29.30139'
   FamilyRegistryKey = '14.0'
   PackageName = 'vcredist140'
 }
