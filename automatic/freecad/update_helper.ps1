7a2b6414d00ed8426ccfbd4b32807ee05285a33e
RB <RedBaron2@users.noreply.github.com>
2022-04-08 13:12:29 -0700

    Cleanup and Update
    This cleans the code and removes the 32bit entirely. FreeCAD developers have started to only develop for 64bit versions. This will also update the version based on the installer number.

--- automatic/freecad/update_helper.ps1
+++ automatic/freecad/update_helper.ps1
@@ -1,51 +1,53 @@
 
 function Get-FreeCad {
-  param(
-    [string]$Title,
-    [string]$kind = 'stable',
-    [int]$filler = "0",
-    [string]$uri = $releases
-  )
+param(
+  [string]$Title,
+  [string]$kind = 'stable',
+  [int]$filler = "0",
+  [string]$uri = $releases
+)
   $download_page = Invoke-WebRequest -Uri $uri -UseBasicParsing
-  $mobile = @{$true = "portable"; $false = "installer" }[($kind -eq 'portable')]; $portable = @{$true = "vc(17|14)\.x\-x86(\-|_)64"; $false = "$mobile" }[($kind -eq 'dev')]
-  $try = (($download_page.Links | ? href -match "CAD\-|\.[\dA-Z]+\-WIN" | select -First 1 -expand href).Split("/")[-1]).Split(".")[-1]
-  $ext = @{$true = '7z'; $false = 'exe' }[( $kind -match 'dev' ) -or ( $portable -match 'portable' )]
-  $re32 = "(WIN)\-x32\-($portable)\.$ext";
-  # cybrwshl use of (\-[0-9])? regex updated to (\-\d+)? for installer with possible digit(s) before extension
-  $re64 = @{$true = "(WIN|Win)\-(LPv\d+\.\d+\.\d+|Conda|x64)(\-|_)($portable)(\-|.)?(\d+)?(\.$ext)$"; $false = "(WIN)\-x64\-($portable)(\-|.)?(\d)?\.$ext$" }[( $kind -match 'dev' )]
-  $url32 = $download_page.Links | ? href -match $re32 | select -first 1 -expand href
-  $url64 = $download_page.Links | ? href -match $re64 | select -first 1 -expand href
+  $mobile = @{$true="portable";$false="installer"}[($kind -eq 'portable')]
+  $portable = @{$true="vc(19|17|14)\.x\-x86(\-|_)64";$false="$mobile"}[($kind -eq 'dev')]
+  $ext = @{$true='7z';$false='exe'}[( $kind -match 'dev' ) -or ( $portable -match 'portable' )]
+  # cybrwshl use of (\-[0-9])? regex updated to (\-\d+)? for installer with possiblity of any number of digit(s) before extension
+  $re64 =  @{$true="(WIN|Win)\-(LPv\d+\.\d+\.\d+|Conda|x64)(\-|_)($portable)(\-|.)?(\d+)?(\.$ext)$";$false="(WIN)\-x64\-($portable)(\-|.)?(\d+)?\.$ext$"}[( $kind -match 'dev' )]
+  # FreeCad Website only makes mention of a 64-bit version for download as of 2022-2-16
+  $url64 = @{$true=( $download_page.Links | ? href -match $re64 | Select-Object -First 1 -ExpandProperty 'href' )`
+  ;$false=( $download_page.Links | ? href -match $re64 | Sort-Object -Property 'href' -Descending | Select-Object -First 1 -ExpandProperty 'href' )}[($kind -eq 'dev')]
   # Logic to find filler value from url
   $fill = (($url64.Split('\/'))[-2]).Split('.')[-1]
   if ( (!([string]::IsNullOrEmpty($fill) ) ) -and ($fill -is [int]) ) {
-    Write-Warning "fill -$fill-"
+    "fill -$fill-" | Write-Warning
     if ($fill -ge $filler) { $filler = $fill }
   }
-  $checking = (($url64.Split('\/'))[-1]) -replace ($re64, '') -replace ('\-', '.'); $version = ( Get-Version $checking ).Version
-  # Version Matching for all kind except dev since it is only 64bit
-  if ($kind -ne 'dev') {
-    $32bitchk = (($url32.Split('\/'))[-1]) -replace ($re32, '') -replace ('\-', '.'); $ver32 = ( Get-Version $32bitchk ).Version
-    if ($ver32 -ne $version) {
-      Write-Warning "ver32 -$ver32- version -$version-"
-      # This is commented due to the added Notes in the Readme
-      #	    $url32 = $null
-    }
-  }
-  switch ($kind) {
-    'portable' {
-      $PackageName = "$Title.$kind"
-      $Title = "$Title (Portable)"
-    }
-    default {
-      $PackageName = "$Title"
-      $Title = "$Title"
-    }
-  }
+  "url64 -${PreUrl}${url64}-" | Write-Warning
+  $chking = (($url64.Split('\/'))[-1])
+  $checking = $chking -replace($re64,'') -replace('\-','.')
+  "version check -$checking-" | Write-Warning
+  $version = ( Get-Version $checking ).Version
   # Check if version is using a revision and build correctly
   if ( ($version.Build) -match "\d{5}" ) {
-    [version]$version = ( ( ($version.Major), ($version.Minor), ($filler), ($version.Build) ) -join "." )
+    [version]$version = ( ( ($version.Major),($version.Minor),($filler),($version.Build) ) -join "." )
   }
-  $vert = @{$true = "$version-$kind"; $false = "$version" }[( $kind -eq 'dev' )]
+  $vert = @{$true="$version-$kind";$false="$version"}[( $kind -eq 'dev' )]
+  # Due to Non Uniform Releases of the package in the past
+  $NonUniformRegex = "(?:\-)(\d+)(?:.${ext})"
+  if (($chking -match $NonUniformRegex) -and ($kind -ne 'dev')) { if (!([string]::IsNullOrEmpty( $Matches[1] ))) { $digits = $Matches[1] } "digits -$digits-" | Write-Warning }
+  # If digits are found in the fileName they will be addded to the version number
+  # This approach is done due a four segment version needed for Get-FixVersion
+  if ((!([string]::IsNullOrEmpty($digits) ) ) -and ($digits -match "(\d+)")) {  $vert = "${vert}.${digits}" }
+  "vert -$vert-" | Write-Warning
+  switch ($kind) {
+    'portable' {
+      $PackageName  = "$Title.$kind"
+      $Title        = "$Title (Portable)"
+    }
+    default {
+      $PackageName  = "$Title"
+      $Title        = "$Title"
+    }
+  }
  	$package = @{
     PackageName  = ($PackageName).ToLower()
     Title        = $Title
@@ -53,7 +55,6 @@ function Get-FreeCad {
     Version      = $vert
     fileType     = ($url64.Split("/")[-1]).Split(".")[-1]
     ReleaseNotes = "https://www.freecadweb.org/wiki/Release_notes_$($version.Major).$($version.Minor)"
-  }
-  if (![string]::IsNullOrEmpty($url32)) { $package.Add( "URL32", $PreUrl + $url32 ) }
-  return $package
+	}
+return $package
 }
