38fe5eb5c61f908cf56c7a7d20ade7b80fcaec0d
Chocolatey Community <chocolatey@realdimensions.net>
2022-04-15 00:14:01 +0000

    AU: 5 updated - brave chromium GoogleChrome maxima vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/68dd1ee7e45e8f2ae468c318d771fc400f424ba0

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '100.0.4896.88'
+$version = '100.0.4896.127'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '6211109b294a8165443537c8e68dc45ed297cb6765c3066ef1df6d968e47fc7f'
-  checksum64             = '3843e171474f3722b77a67ea022ca4158819977bc6c6191183d5bb7361636c5b'
+  checksum               = '5a9c11c4aaa3f4d26f27fadb908a292531f529cdeeaf937348e226678ffd839f'
+  checksum64             = '37802e4cce13e22ba5d0e039f7e31c0c62077eeaf69e0f43b0ca4dcd7875c121'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/maxima/tools/chocolateyInstall.ps1
+++ automatic/maxima/tools/chocolateyInstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.45.1-Windows/maxima-5.45.1s-win32.exe/download'
-  url64bit       = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.45.1-Windows/maxima-5.45.1s-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.46.0-Windows/maxima-5.46.0-win32.exe/download'
+  url64bit       = 'https://sourceforge.net/projects/maxima/files/Maxima-Windows/5.46.0-Windows/maxima-5.46.0-win64.exe/download'
   softwareName   = 'maxima*'
-  checksum       = 'fe385011593da096be0929426fe93e0ccd32f4fc4a7bf8c3f0bbdae78d73317b'
+  checksum       = 'a57d91429a8da5741ab170c130137a93cd8431b8efdf13e7da774808601fe3e4'
   checksumType   = 'sha256'
-  checksum64     = '60be72a45376e1256a554d79e7da35f7d91cfc86b12b9a9a5501c0f5ed8b6de0'
+  checksum64     = '93a877a51193f3987b3ec49daa6f231adbcdfc146ba3a7b07e7c284c29aca3c5'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
