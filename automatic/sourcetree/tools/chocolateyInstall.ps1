dace8be2960da299894975ff8fb31fb148598ab4
meek2100 <34908880+meek2100@users.noreply.github.com>
2022-04-16 21:26:03 -0700

    Update chocolateyInstall.ps1
    Fix sourcetree automatic updates.

--- automatic/sourcetree/tools/chocolateyInstall.ps1
+++ automatic/sourcetree/tools/chocolateyInstall.ps1
@@ -1,4 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
+$checksum = 'd9794507e3f319971e27da1679de2d09c7306ee78b0b2445ee118855abec1704'
+$url = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_3.4.6.msi'
 
 # Check if Sourcetree standard (with Squirrel installer) is installed 
 [array] $key = Get-UninstallRegistryKey "sourcetree" | Where-Object { -Not ($_.WindowsInstaller) }
@@ -13,14 +15,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Sourcetree*'
   fileType      = 'msi'
+  url            = $url
   silentArgs    = "/qn /norestart ACCEPTEULA=1 /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
   validExitCodes= @(0,1641,3010)
-  url           = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_3.4.1.msi'
-  checksum      = '6720d2278c309d0fb9117451cf92ed2af25a26588fbd39a08da5266578004e74'
-  checksumType  = 'sha256'
-  url64bit      = ''
-  checksum64    = ''
-  checksumType64= 'sha256'
+  checksum       = $checksum
+  checksumType   = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
