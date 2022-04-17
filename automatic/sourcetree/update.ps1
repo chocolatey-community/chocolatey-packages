c6848bde4980fc78cc86ccf432b6d14ec6286713
meek2100 <34908880+meek2100@users.noreply.github.com>
2022-04-16 21:24:49 -0700

    Update update.ps1
    Fix source tree to run fetch latest version going forward.

--- automatic/sourcetree/update.ps1
+++ automatic/sourcetree/update.ps1
@@ -1,21 +1,36 @@
+﻿$ErrorActionPreference = 'Stop'
 import-module au
- . ".\update_helper.ps1"
+
+$download_page_url = 'https://www.sourcetreeapp.com/download-archives'
+$url_part1 = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_'
+$url_part2 = '.msi'
+$release_notes_part1 = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/ReleaseNotes_'
+$release_notes_part2 = '.html'
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
-            "(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
         }
-
-        "$($Latest.PackageName).nuspec" = @{
+		'sourcetree.nuspec' = @{
             "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
         }
      }
 }
 
 function global:au_GetLatest {
-  return Get-JavaSiteUpdates -Package "sourcetree" -Title "Sourcetree for Windows Enterprise"
+    $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
+
+     # Get Version
+    $homepage_content -match 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_\d+\.\d+\.\d+'| Out-Null
+	$version = $matches[0] -replace "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_", ""
+    $url = $url_part1 + $version + $url_part2
+	$release_notes = $release_notes_part1 + $version + $release_notes_part2
+    
+
+    $Latest = @{ URL = $url; Version = $version; ReleaseNotes = $release_notes }
+    return $Latest
 }
 
 update
