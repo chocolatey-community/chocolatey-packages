55921699e62ed3b1e486a7f3cb18fa1f37d18256
Chocolatey Community <chocolatey@realdimensions.net>
2022-04-20 12:14:05 +0000

    AU: 9 updated - autohotkey autohotkey.install autohotkey.portable chromium Opera smartftp vlc vlc.install vlc.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/9f435c4e9908181174ad194e144372e8b63b5d9c

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/85.0.4341.60/win/Opera_85.0.4341.60_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/85.0.4341.60/win/Opera_85.0.4341.60_Setup_x64.exe'
-  checksum       = 'd7e932ea701c7e9418e4195a7e64c420fd931f895ed56a48018617f8009c657d'
-  checksum64     = '5e0b636a5c5d83b70bc738be361aa610b943a337dc5f26d095d6e2f51f6229c2'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/85.0.4341.75/win/Opera_85.0.4341.75_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/85.0.4341.75/win/Opera_85.0.4341.75_Setup_x64.exe'
+  checksum       = '9050b60a98911a917de2fb94e5484706bcd62887ca2d8ba199832ecbac8fd9d9'
+  checksum64     = '4f3de0ea960ff824bbb55a7e1bc1992eaaac744e6443f951389b04e6e205220f'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '85.0.4341.60'
+$version = '85.0.4341.75'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
