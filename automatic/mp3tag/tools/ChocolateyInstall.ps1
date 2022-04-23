14aed744d4574124f3c20768245aa87d254a1b31
Chocolatey Community <chocolatey@realdimensions.net>
2022-04-23 15:38:16 +0000

    AU: 2 updated - mp3tag vlc.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/cd65a2e9ef1b6c501d8e81468dd7ed227c4349bd

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,7 +24,7 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv314csetup.exe"
+  file           = "$toolsPath\mp3tagv314dsetup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
