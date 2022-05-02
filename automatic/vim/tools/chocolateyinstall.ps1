32087f5e7cebce7024cad1bafd6ebc1d757751e4
Chocolatey Community <chocolatey@realdimensions.net>
2022-05-02 00:13:41 +0000

    AU: 2 updated - smartftp vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/fc9e181319f2b5e0e16cd78d1c26d98d378cb022

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.4850_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.4850_x64.zip"
+  file          = "$toolsDir\gvim_8.2.4853_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.4853_x64.zip"
 }
 
 $installArgs = @{
