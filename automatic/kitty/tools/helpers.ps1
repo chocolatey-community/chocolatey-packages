a91c034ce0fe461c2455bcc4acf1664cf7be0758
Younes El-karama <yelkarama@users.noreply.github.com>
2022-05-06 11:18:34 -0400

    Simplity StopProcesses function

--- automatic/kitty/tools/helpers.ps1
+++ automatic/kitty/tools/helpers.ps1
@@ -1,20 +1,9 @@
-﻿function StopProcesses() {
+function StopProcesses() {
   $processNames = @("genpass", "gensfc", "kageant", "cygtermd", "kitty", "kittygen", "klink", "kscp", "ksftp")
   $processes = Get-Process  $processNames -ea SilentlyContinue
 
   if ($processes) {
-    $activeProcesses = ($processes | Select-Object -ExpandProperty name | Sort-Object -Unique) -join ","
-    Write-Warning "Stopping processes: $activeProcesses to prevent upgrade/uninstall failure..."
-    Stop-Process -InputObject $processes
-
-    Start-Sleep -Seconds 3
-
-    $processes = Get-Process  $processNames -ea SilentlyContinue
-    if ($processes) {
-      $activeProcesses = ($processes | Select-Object -ExpandProperty name | Sort-Object -Unique) -join ","
-      Write-Warning "Killing processes: $activeProcesses to prevent upgrade/uninstall failure..."
-      Stop-Process -InputObject $processes -Force
-    }
+    Write-Warning "Stopping processes to prevent upgrade/uninstall failure..."
+    Stop-Process -InputObject $processes -Force
   }
 }
-
