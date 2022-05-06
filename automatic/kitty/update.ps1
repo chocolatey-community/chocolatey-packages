35cfd80f81a164c2c35dcdc95a592e40a6d11f50
Younes El-karama <yelkarama@users.noreply.github.com>
2022-05-06 15:39:29 -0400

    Use standard update.ps1 and VERIFICATION.txt
    Signed-off-by: Younes El-karama <yelkarama@users.noreply.github.com>

--- automatic/kitty/update.ps1
+++ automatic/kitty/update.ps1
@@ -3,44 +3,29 @@ import-module au
 $latestRelease = 'https://api.github.com/repos/cyd01/KiTTY/releases/latest'
 
 function global:au_SearchReplace {
-  @{ }
-}
-
-function downloadFile($url, $filename) {
-  if ($filename.EndsWith("_nocompress.exe")) {
-    $filename = $filename -replace "_nocompress\.exe$", ".exe"
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(\s*zip file\:).*"           = "`${1} $($Latest.URL32)"
+      "(?i)(^\s*SHA256\:).*"            = "`${1} $($Latest.Hash)"
+      "(?i)(^\s*Get-RemoteChecksum ).*" = "`${1} $($Latest.URL32)"
+    }
   }
-
-  $filePath = "$PSScriptRoot\tools\$filename"
-  Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $filePath | Out-Null
-  return Get-FileHash $filePath -Algorithm SHA256 | % Hash
 }
 
-function global:au_BeforeUpdate {
-  $text = Get-Content -Encoding UTF8 -Path "$PSScriptRoot\VERIFICATION_Source.txt" -Raw
-  $sb = New-Object System.Text.StringBuilder($text)
-
-  $Latest.GetEnumerator() | ? { $_.Key.StartsWith("URL") } | % {
-    $fileName = $_.Key.Substring(3)
-    $checksum = downloadFile $_.Value $fileName
-    $value = [string]::Format("| {0,-20} | {1} | {2,-79} |", $fileName, $checksum, $_.Value)
-    $sb.AppendLine($value) | Out-Null
-  }
-  $value = "".PadLeft(171, '-')
-  $sb.Append("|$value|") | Out-Null
-
-  $sb.ToString() | Out-file -Encoding utf8 -FilePath "$PSScriptRoot\legal\VERIFICATION.txt"
-}
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_GetLatest {
   $assets = (Invoke-RestMethod $latestRelease).assets
   $fileName = $assets[0].name
   $version = $fileName.Replace("kitty-bin-", "").Replace(".zip", "")
-  $result = @{
-    Version = $version
+  $wc = [System.Net.WebClient]::new()
+  $Hash = (Get-FileHash -InputStream ($wc.OpenRead($assets[0].browser_download_url)) | Select-Object -ExpandProperty hash).ToLower()
+
+  @{
+    URL32      = $assets[0].browser_download_url
+    Version    = $version
+    Hash       = $Hash
   }
-  $result["URL" + $fileName] = New-Object uri($assets[0].browser_download_url)
-  return $result
 }
 
 update -ChecksumFor none
