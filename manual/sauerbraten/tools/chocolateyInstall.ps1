64fd302968101c37e753d474357f2e323e042238
Janek <27jf@pm.me>
2022-05-14 23:26:04 +0200

    Update sauerbraten to 2020 version

--- manual/sauerbraten/tools/chocolateyInstall.ps1
+++ manual/sauerbraten/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
 
   checksum       = '3424EB2B7DA089B91D3F3B2259F737B4BD507E5C12B28EA1B7FC4D5777755B26'
   checksumType   = 'sha256'
-  url            = 'https://sourceforge.net/projects/sauerbraten/files/sauerbraten/2013_01_04/sauerbraten_2013_02_03_collect_edition_windows.exe/download'
+  url            = 'https://sourceforge.net/projects/sauerbraten/files/sauerbraten/2020_11_29/sauerbraten_2020_12_21_windows.exe/download'
 
   silentArgs     = '/S'
   validExitCodes = @(0)
