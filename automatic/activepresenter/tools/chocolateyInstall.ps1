6e7c0d45cf8d44b341c4d09833976833200d9603
Chocolatey Community <chocolatey@realdimensions.net>
2022-05-16 18:14:56 +0000

    AU: 4 updated - activepresenter Firefox mattermost-desktop wesnoth [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/e97eb26b39fc515606d5d1aa544e5b39906e16b8

--- automatic/activepresenter/tools/chocolateyInstall.ps1
+++ automatic/activepresenter/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'activepresenter'
   fileType       = 'exe'
-  file           = "$toolsDir\ActivePresenter_v8.5.6_setup.exe"
+  file           = "$toolsDir\ActivePresenter_v8.5.7_setup.exe"
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0)
 }
--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.3/wesnoth-1.17.3-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.4/wesnoth-1.17.4-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = 'a742eab6e65d700c3c1acc12ffa98dd1c9f11560bdbd947aac911690e60f8188'
+  checksum       = 'b1a2822ae0763426ce52026b07c438c786203ce580db460b92d5edcdcabe6b8c'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
