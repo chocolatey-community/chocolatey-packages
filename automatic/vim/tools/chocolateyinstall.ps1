37898cdabc5e1bf26fd6585741693ef2a7affa1a
Chocolatey Community <chocolatey@realdimensions.net>
2022-05-24 00:19:53 +0000

    AU: 3 updated - chromium tor-browser vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7e936474b4c7c9b9acea4cb775275a542db3fa61

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_8.2.5006_x86.zip"
-  file64        = "$toolsDir\gvim_8.2.5006_x64.zip"
+  file          = "$toolsDir\gvim_8.2.5010_x86.zip"
+  file64        = "$toolsDir\gvim_8.2.5010_x64.zip"
 }
 
 $installArgs = @{
