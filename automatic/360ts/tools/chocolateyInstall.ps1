bef09a2f54225383c54f58c31b0720565ae7811a
Chocolatey Community <chocolatey@realdimensions.net>
2022-05-25 18:13:43 +0000

    AU: 5 updated - 360ts chromium git-lfs git-lfs.install qbittorrent [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d6d81880cc1f79e2a5018320a342c3a165a0dde6

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1451.exe'
-  checksum               = 'ab84a074f4c36d59c3a46818d5c4832cad78404c2436f2bed53bb7551d3dfa40'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1456.exe'
+  checksum               = 'c672d72865d94a1fa9249adb2de1831e54d807f811957c9689fdede783aff2d4'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/git-lfs.install/tools/chocolateyInstall.ps1
+++ automatic/git-lfs.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\git-lfs-windows-v3.1.4.exe"
+$filePath = "$toolsDir\git-lfs-windows-v3.2.0.exe"
 
 $packageArgs = @{
   PackageName    = 'git-lfs'
