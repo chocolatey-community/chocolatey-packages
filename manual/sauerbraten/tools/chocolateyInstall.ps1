80811ee0aadea4ffbaa422dd1ec299786fd78281
xeruf <27jf@pm.me>
2022-06-10 00:55:27 +0200

    sauerbraten: move to main sourceforge from mirror

--- manual/sauerbraten/tools/chocolateyInstall.ps1
+++ manual/sauerbraten/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
 
   checksum       = '89842b3fe5cf591b761b84948e97e3a68e2fa451f5a0afb102a9347efca0a41e'
   checksumType   = 'sha256'
-  url            = 'https://jztkft.dl.sourceforge.net/project/sauerbraten/sauerbraten/2020_11_29/sauerbraten_2020_12_21_windows.exe'
+  url            = 'https://sourceforge.net/projects/sauerbraten/files/sauerbraten/2020_11_29/sauerbraten_2020_12_21_windows.exe/download'
   
   silentArgs     = '/S'
   validExitCodes = @(0)
