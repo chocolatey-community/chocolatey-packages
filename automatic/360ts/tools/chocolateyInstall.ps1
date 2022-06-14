30844c3280d5251ea6cab9323837c2f826c73550
Chocolatey Community <chocolatey@realdimensions.net>
2022-06-14 18:13:56 +0000

    AU: 4 updated - 360ts mkdocs-material nuget.commandline smartftp [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c0708cb8c918d38a453faed3d02d81c9f3490c7b

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1456.exe'
-  checksum               = 'c672d72865d94a1fa9249adb2de1831e54d807f811957c9689fdede783aff2d4'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1458.exe'
+  checksum               = '6e42e996e76af53f8dbb9e8897ceab45861d78913b28101c08d064dd26cc0042'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/mkdocs-material/tools/ChocolateyInstall.ps1
+++ automatic/mkdocs-material/tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿Update-SessionEnvironment
 
-$version = '8.3.4'
+$version = '8.3.5'
 
 $proxy = Get-EffectiveProxy
 if ($proxy) {
