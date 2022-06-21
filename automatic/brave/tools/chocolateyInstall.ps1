22c98cf568cc3c445885c93a5d6194ea2929cad2
Chocolatey Community <chocolatey@realdimensions.net>
2022-06-21 12:15:13 +0000

    AU: 4 updated - brave chromium vscode-insiders wesnoth [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0941882fce4bad45fea481e7883a758893f7b612

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.40.100'
+[version]$softwareVersion = '1.41.74'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.16/wesnoth-1.16.3/wesnoth-1.16.3-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.5/wesnoth-1.17.5-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = '6c7ce087640aba804cb9720e1553eb4e8dd91a2a9938d42eeae21b5668423efa'
+  checksum       = '3ac4ac6cff769b3143b23f91ca182c763a2c4cfc9969fe0a51e74053ba15d63f'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
