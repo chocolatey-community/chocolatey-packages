bd3c9a2a7acfbac4011e8b8dd680acf71d5104ad
Chocolatey Community <chocolatey@realdimensions.net>
2022-06-24 06:14:30 +0000

    AU: 2 updated - juju zotero [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/76d0f6fc5c065ef184a9e5be53a5c41a38f7dc9f

--- automatic/zotero/tools/chocolateyInstall.ps1
+++ automatic/zotero/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero'
-  file           = "$toolsPath\Zotero-6.0.8_setup.exe"
+  file           = "$toolsPath\Zotero-6.0.9_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
