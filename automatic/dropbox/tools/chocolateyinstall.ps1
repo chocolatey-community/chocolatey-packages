d5b46eb36c80b147f05610ba93cb7ea800f78a75
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-06 18:14:06 +0000

    AU: 4 updated - blender dropbox Firefox totalcommander [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c4f0983dd9f8c4808140d252331d3434b184fb27

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '152.4.4880'
+$version = '153.3.3924'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20152.4.4880%20Offline%20Installer.x86.exe'
-    checksum       = '151cc9733c4c51398da88f5fea7d45085b797e9edc5319f37283003a5c8f705a'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20153.3.3924%20Offline%20Installer.x86.exe'
+    checksum       = '0c8e3964e2fa28dab2772a7d9d692ebdb94501b25ee30bc2008bc92430cef7e3'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
