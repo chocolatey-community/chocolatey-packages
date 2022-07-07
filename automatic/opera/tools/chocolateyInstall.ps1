7dff4435467085feb9464729c6381ec7b8069c87
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-07 12:14:38 +0000

    AU: 4 updated - chromium Opera palemoon picard [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b85912a0d40d6ddeca9a0b612011763a190430ae

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/88.0.4412.74/win/Opera_88.0.4412.74_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/88.0.4412.74/win/Opera_88.0.4412.74_Setup_x64.exe'
-  checksum       = 'e0e9c586e9bc4b3c4ad681f448e698e08fde0d3d4389d1209a300e02f289b80a'
-  checksum64     = '880b8bbcf8ee994767142309e68a111e7556d49987f220983b0204e39f113239'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/89.0.4447.38/win/Opera_89.0.4447.38_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/89.0.4447.38/win/Opera_89.0.4447.38_Setup_x64.exe'
+  checksum       = 'a99ba9fd6fab7d25ef0016932cbd6b74c2904aea188b5fa00bf0fc6afc156f37'
+  checksum64     = '37a56ac0f714ed73853f86e85776c214d8177258e6350c144ab6363aaca2e489'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '88.0.4412.74'
+$version = '89.0.4447.38'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-31.1.0.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-31.1.0.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-31.1.1.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-31.1.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '57a197c1c619071a4918302d80e2bacd460562cf79627b33d9ae3a81d37979e8'
+  checksum      = 'eb727f3012c58872c9dd4b0e67a3f4cbe37d536ad507ad43a74f781a5f8920fb'
   checksumType  = 'sha256'
-  checksum64    = '9b76d7cfaefa41e62853e67b2b9d26e2d432fdcc63959db3d7f14e4db0001df3'
+  checksum64    = '4f67a44182286d9dd44c5629b35fa49c0d04ecf7fd356bda93bf18a5e627e576'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
