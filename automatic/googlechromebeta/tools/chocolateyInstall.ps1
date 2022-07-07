d105b9a78e4385f874446a4b7d445d1c9b1bc170
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-07 18:14:47 +0000

    AU: 9 updated - cmake cmake.install cmake.portable googlechromebeta nodejs nodejs.install selenium-chrome-driver vscode vscode.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/dbce534b2d4522681eca920cf726e1cefdadbeb6

--- automatic/googlechromebeta/tools/chocolateyInstall.ps1
+++ automatic/googlechromebeta/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '104.0.5112.29-beta'
+$version = '104.0.5112.39-beta'
 if ($version -eq (Get-ChromeBetaVersion)) {
   Write-Host "Google Chrome Beta $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/beta/googlechromebetastandaloneenterprise64.msi'
-  checksum               = 'e51abb395ec2620211f121779b6fe853fe34011dacdab429fbdb3ff8fc34e558'
-  checksum64             = '5f8a9cd650fba806972936254a4bc6ad5f146bf1e0f1a8d9e24736801ed8fa81'
+  checksum               = 'e2ca58fde4637160a6d624968adcb701fa3f1ad588ab7cd5c1d00b5ae43e4bf2'
+  checksum64             = 'fa7dd51d6f1ac93adb7ccba48136bb97a5e6d843f0f1774276840d54c92bd679'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.68.1'
+$version = '1.69.0'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/30d9c6cd9483b2cc586687151bcbcd635f373630/VSCodeSetup-ia32-1.68.1.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/30d9c6cd9483b2cc586687151bcbcd635f373630/VSCodeSetup-x64-1.68.1.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/92d25e35d9bf1a6b16f7d0758f25d48ace11e5b9/VSCodeSetup-ia32-1.69.0.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/92d25e35d9bf1a6b16f7d0758f25d48ace11e5b9/VSCodeSetup-x64-1.69.0.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = 'a45ead6465646d08f6d3ec727a399844e98b41715c8e0ec3077bb2ec20840be3'
+  checksum       = '9091db7bd2122e51056d2d7adfdc09ade62eef010265f54570d7cce59ec20c03'
   checksumType   = 'sha256'
-  checksum64     = '7e42465a9509b518a5b8a8f33612f41d4ad46156d913dd026d56c2ad26e94424'
+  checksum64     = '2db764d9ccd989d0bf2b47bd50e4fcbaaa8c7e1c3e39791098bcf8df6268dd4f'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
