80ad9e04ef8cf0cc55403873bdc70ef200852a45
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-11 18:14:10 +0000

    AU: 2 updated - gnupg mp3tag [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/45e99b416ce80ea3b6e1739f1656b34bc58b12f4

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,8 +24,8 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv316bsetup.exe"
-  file64         = "$toolsPath\mp3tagv316b-x64-setup.exe"
+  file           = "$toolsPath\mp3tagv316csetup.exe"
+  file64         = "$toolsPath\mp3tagv316c-x64-setup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
