7444042b1ceaad48e6f764fede950a133a1e178f
xxyzz <gitpull@protonmail.com>
2022-07-13 07:36:15 +0800

    (Calibre) Update AU script for 6.x releases

--- automatic/calibre/tools/chocolateyInstall.ps1
+++ automatic/calibre/tools/chocolateyInstall.ps1
@@ -5,20 +5,12 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'MSI'
-  url            = 'https://download.calibre-ebook.com/5.44.0/calibre-5.44.0.msi'
-  checksum       = '21903563B5BB5817EE33F3A3EA6B0F3B71C3EAC1793069F89674D70A99F0F080'
-  checksumType   = 'sha256'
   file64         = "$toolsPath\calibre-64bit-5.44.0.msi"
   softwareName   = 'calibre*'
   silentArgs     = '/quiet'
   validExitCodes = @(0, 3010, 1641)
 }
 
-if ((Get-OSArchitectureWidth -compare 32) -or ($env:chocolateyForceX86 -eq $true)) {
-  Install-ChocolateyPackage @packageArgs
-}
-else {
-  Install-ChocolateyInstallPackage @packageArgs
-}
+Install-ChocolateyInstallPackage @packageArgs
 
 Remove-Item -Force -ea 0 "$toolsPath\*.msi","$toolsPath\*.ignore"
