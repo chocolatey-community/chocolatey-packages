4c95fe49ae1c96a8ffc66ca50d69a76c41c0831a
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-15 12:15:15 +0000

    AU: 3 updated - 360ts chromium vscode-insiders [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/6f904962fd28fd77612f2f75129322a38f070007

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1465.exe'
-  checksum               = '21ff441ddd83620168c7c2f71de719d0cd682421d89cc77575c805aa72766ceb'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1468.exe'
+  checksum               = '4c367c4ad46f71fa282594747baf73f388610ab15c21825a6d5adc98de825dc3'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
