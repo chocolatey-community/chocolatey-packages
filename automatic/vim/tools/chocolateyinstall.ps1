ff1a5cfb5560641f9dc98b13de8c7f2c6d209758
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-16 00:18:40 +0000

    AU: 4 updated - chromium universal-usb-installer vim wesnoth [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c82b8a9b19c1a9b5a166d19d15e871b527d263eb

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.0053_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.0053_x64.zip"
+  file          = "$toolsDir\gvim_9.0.0054_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.0054_x64.zip"
 }
 
 $installArgs = @{
