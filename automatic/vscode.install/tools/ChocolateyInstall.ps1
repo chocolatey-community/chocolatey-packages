fa18ba13b83ecb8d21e8fc37aa5869385a128577
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-19 00:14:07 +0000

    AU: 4 updated - chromium vim vscode vscode.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c6daa810103298603de6333c18e9afd136edb0ab

--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.69.1'
+$version = '1.69.2'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/b06ae3b2d2dbfe28bca3134cc6be65935cdfea6a/VSCodeSetup-ia32-1.69.1.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/b06ae3b2d2dbfe28bca3134cc6be65935cdfea6a/VSCodeSetup-x64-1.69.1.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/3b889b090b5ad5793f524b5d1d39fda662b96a2a/VSCodeSetup-ia32-1.69.2.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/3b889b090b5ad5793f524b5d1d39fda662b96a2a/VSCodeSetup-x64-1.69.2.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = 'f53f48238c23e8682122e830bf92ac284cad50c588e3e30ef876b03f8ffd7656'
+  checksum       = '630e1c110048762707c49d0a9d87754b67eee215d9f99768ac871734bc05109a'
   checksumType   = 'sha256'
-  checksum64     = '008a2a04a52feafc4e62e1dfb34244f87d87889b91ac55612d72cc8b426996ff'
+  checksum64     = '577444da696e8e6f067c109257096aa28e60647c6515e085f3636312ae3da1f1'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
