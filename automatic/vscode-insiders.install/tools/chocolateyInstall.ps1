5280c77a80827449dce466be8c9207ac2560b784
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-22 12:13:53 +0000

    AU: 4 updated - chromium spotify vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ebe1827058cc5bc6eab07f2bb79c81600f75ccb6

--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/62469028a6dce9db5b7c4639be7f447f4ab0e35c/VSCodeSetup-ia32-1.70.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/62469028a6dce9db5b7c4639be7f447f4ab0e35c/VSCodeSetup-x64-1.70.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/13ba7bb446a638d37ebccb1a7d74e31c32bb9790/VSCodeSetup-ia32-1.70.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/13ba7bb446a638d37ebccb1a7d74e31c32bb9790/VSCodeSetup-x64-1.70.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = '45d0fd97a285049bdec5b0c0e76b8c25ea24e57ec71b422457d98b8b5c6e55147b21dfce345e88f04a2d018db2f26ff7377bcb5da7b9baacd42a9af052ee60b4'
+  checksum       = '4c373d7afed1612a1a0af380ad87d8bfd26228fc7e8c0aa77de8ecc6d2649deb8586b4875336c9b393a792ffc460a8232a66cc5264c00519cf4ff28eb886ba1c'
   checksumType   = 'sha512'
-  checksum64     = 'b6979b10aafd117beef27bcc3265414611857055143d9d566a4e4025fcda6ef0b6818aabe66e4e12b126aa55b7254fd337c5e888db1e01132246ecadfecaccee'
+  checksum64     = 'a96aaf3d512818cca42c64a15f8cc46813a470d758652288cbb966d89475f7bea16a262636a1b4167c7e3c47e8def974d9e7aeb0fc060472a46c16a18755d678'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
