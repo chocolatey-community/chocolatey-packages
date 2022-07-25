0247b305f4127b6c2bcf686dcba06f2da55a1fef
Corné Speekenbrink <70575069+cspeekenbrink@users.noreply.github.com>
2022-07-25 21:47:13 +0200

    (filezilla) Update chocolateyInstall.ps1
    Change name of software to be FileZilla 3* rather than FileZilla Client*
    which reflects the name in Add/Remove Programs. This was a change in a
    recent version of the FileZilla software.

--- automatic/filezilla/tools/chocolateyInstall.ps1
+++ automatic/filezilla/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   file64         = Get-Item $toolsPath\*_x64.exe
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
-  softwareName   = 'FileZilla Client*'
+  softwareName   = 'FileZilla 3*'
 }
 Install-ChocolateyInstallPackage @packageArgs
 Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content -Value "" -Path "$_.ignore" }}
