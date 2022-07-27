4292270116312c62f99d201106c3ab47eebf68a1
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-27 00:20:38 +0000

    AU: 6 updated - brave chromium nodejs nodejs.install tor-browser vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/7474e437853d49e40ae8126b6d95fa389f7e4540

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.0073_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.0073_x64.zip"
+  file          = "$toolsDir\gvim_9.0.0089_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.0089_x64.zip"
 }
 
 $installArgs = @{
