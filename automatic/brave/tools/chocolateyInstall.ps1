a4106f10fc5cebf2f18867028b0b5da0bc84587d
Chocolatey Community <chocolatey@realdimensions.net>
2022-07-28 12:14:22 +0000

    AU: 3 updated - brave chromium vscode-insiders [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/12b8e148eb90466a3e04f84a48b1d56c0cfb4e22

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.42.78'
+[version]$softwareVersion = '1.43.51'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
