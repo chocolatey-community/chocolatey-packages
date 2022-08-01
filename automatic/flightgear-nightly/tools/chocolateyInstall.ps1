979d0ab3be8bad857d37f4dab1f91094d9173db7
Roman Lazunin <rlazunin@gmail.com>
2022-08-01 23:29:39 +0200

    added flightgear-nightly package to install flightgear latest build

---
+++ automatic/flightgear-nightly/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$version = '2020.4.0-latest-nightly'
+$softwareName = "FlightGear v$version"
+
+$packageArgs = @{
+  packageName = 'flightgear'
+  fileType    = 'exe'
+  url         = 'http://download.flightgear.org/builds/nightly/FlightGear-latest-nightly-full.exe'
+
+  softwareName = $softwareName
+
+  silentArgs   = '/VERYSILENT'
+  validExitCodes = @(0)
+}
+
+$key = UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key) {
+  Write-Host "FlightGear $version is already installed. Skipping download and installation."
+} else {
+  Install-ChocolateyPackage @packageArgs
+}
---
+++ automatic/flightgear-nightly/update.ps1
@@ -0,0 +1,46 @@
+﻿import-module au
+
+# We can't use https for the url, otherwise powershell throws an error and closes the window.
+# Oddly it works when choco auto redirects http to https
+$downloads = 'http://download.flightgear.org/builds'
+$changelog = 'http://wiki.flightgear.org/Changelog_'
+$versions = 'http://www.flightgear.org/'
+
+http://download.flightgear.org/builds/nightly/FlightGear-latest-nightly-full.exe
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+      "(?i)(^[$]version\s*=\s*)('.*')"        = "`$1'$($Latest.RemoteVersion)'"
+    }
+    ".\$($Latest.PackageName).nuspec" = @{
+      "(?i)(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`$1$($Latest.ReleaseNotes)`$2"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $version_page = Invoke-WebRequest -UseBasicParsing -Uri $versions
+  $re = "(?i)Current stable release: (?<stable>[0-9\.]+)"
+  if($version_page.Content -match $re)
+  {
+    $version = $Matches.stable
+    $short_version =  $version.Substring(0, $version.LastIndexOf("."))
+  } else
+  {
+    throw "Cannot obtain the latest version from FlightGear's homepage, please update the `"update.ps1`" script."
+  }
+
+  $url = "$downloads/nightly/FlightGear-latest-nightly-full.exe"
+
+  $releaseNotes = "$changelog$short_version"
+
+  @{
+    URL32 = $url
+    Version = $version
+    RemoteVersion = $version
+    ReleaseNotes = $releaseNotes
+  }
+}
+
+update -ChecksumFor none
