37cc527bb8ba7ba704ee6f8f4ed746a4a3e25dab
Chocolatey Community <chocolatey@realdimensions.net>
2022-08-03 18:13:53 +0000

    AU: 6 updated - blender brave chromium dropbox palemoon rufus [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/594210c51ae47ba5572b828e3c2bc66761384dfe

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '154.4.5363'
+$version = '155.3.5473'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20154.4.5363%20Offline%20Installer.x86.exe'
-    checksum       = '290398e754335dfc7305c20db7adf55a0afed6d353611bd33d8142c0b0b225cc'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20155.3.5473%20Offline%20Installer.x86.exe'
+    checksum       = 'bc56b462df1400fe5e639e1375a8db3302769b5fddbae2872e488b54cab60778'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-31.2.0.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-31.2.0.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-31.2.0.1.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-31.2.0.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = 'ae2662796c286e8498a49f052b8ff9ab2448e28e268aad3ecaae147cac4f28bc'
+  checksum      = 'd9220c8a8262cb1a7dad3060e30a69c0af7f59cf00da4244e63e1de274406a96'
   checksumType  = 'sha256'
-  checksum64    = '3f1c728932019cb56ec3fb8857e0aa0042bf8299329c784d97ad3263169ffe7d'
+  checksum64    = '35aae95e609e7e693498a5809b0d7baa399ec2c2508f3ed396e5d773f137bc52'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
