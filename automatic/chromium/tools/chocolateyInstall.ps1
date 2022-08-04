f6fa1a1d9a55ee075ed67dd996d226d410b3042d
Chocolatey Community <chocolatey@realdimensions.net>
2022-08-04 00:14:00 +0000

    AU: 2 updated - chromium vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/391c14eb0ee2cb0711ca64aa95c4b1d85d01e7eb

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "106.0.5217.0-snapshots"
+$version = "104.0.5112.81"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
