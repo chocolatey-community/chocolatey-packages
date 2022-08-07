6ae76bbb35c26a19750c95ff6879e3ef1c084875
Chocolatey Community <chocolatey@realdimensions.net>
2022-08-07 00:13:49 +0000

    AU: 3 updated - brave chromium vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/abf947e0f079eb28cb86628f25efbcb8aca5e84f

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.42.86'
+[version]$softwareVersion = '1.42.88'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.0148_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.0148_x64.zip"
+  file          = "$toolsDir\gvim_9.0.0161_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.0161_x64.zip"
 }
 
 $installArgs = @{
