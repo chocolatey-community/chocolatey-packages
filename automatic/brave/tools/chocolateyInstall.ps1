f1c1d8102be6e878f0b725186a7e00ba03e85614
Chocolatey Community <chocolatey@realdimensions.net>
2022-08-09 08:12:14 +0000

    AU: 3 updated - brave calibre chromium [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/791ebc5c6d0a0030dda97aa1dfaead80bccf7715

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\BraveBrowserStandaloneSilentSetup32.exe"
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
+  file        = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup32.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.42.88'
+[version]$softwareVersion = '1.43.63'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
