d48f87013aed18889ddcf0678a8e044d98c063fd
Chocolatey Community <chocolatey@realdimensions.net>
2022-08-13 00:14:19 +0000

    AU: 3 updated - chromium mp3tag vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5f01e73ca9df991a144e1d4fb16f7f71d3eed38f

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.0193_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.0193_x64.zip"
+  file          = "$toolsDir\gvim_9.0.0195_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.0195_x64.zip"
 }
 
 $installArgs = @{
