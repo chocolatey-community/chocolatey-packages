a3d19e4f47140a3103b21cdc369eb57b371e1fc7
Chocolatey Community <chocolatey@realdimensions.net>
2022-08-16 00:14:19 +0000

    AU: 5 updated - brave chromium poi vim zotero [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/91504ace86e65d32b8bf74167a5efa5d958066b0

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup32.exe"
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
+  file        = "$toolsPath\BraveBrowserStandaloneSilentSetup32.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.43.67'
+[version]$softwareVersion = '1.42.95'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
