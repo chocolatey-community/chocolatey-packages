e4920aa141f2f5cd63d7452c6c1ccd8c381f8315
Chocolatey Community <chocolatey@realdimensions.net>
2022-08-18 12:14:24 +0000

    AU: 6 updated - 360ts brave chromium julia krita vscode-insiders [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/01f3a92931421b4de446082548b4fe3750f83f68

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1469.exe'
-  checksum               = 'ae51c37704958ab505fed0f465c51286ec1e7358b60deaecc6859c8ea1d00f76'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_10.8.0.1482.exe'
+  checksum               = 'f48268d5fcfd6693a5b2488a206380b483372791af74ccc1fbc19946209615b4'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/krita/tools/chocolateyInstall.ps1
+++ automatic/krita/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   softwareName = 'Krita'
 
   checksumType = 'sha256'
-  file64       = "$toolsDir\krita-x64-5.0.6-setup.exe"
+  file64       = "$toolsDir\krita-x64-5.1.0-setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
