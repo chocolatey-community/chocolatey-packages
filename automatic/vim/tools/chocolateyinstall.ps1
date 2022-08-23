80fabbb93c17c8e17679310797b849d6c08fc013
Chocolatey Community <chocolatey@realdimensions.net>
2022-08-23 00:13:57 +0000

    AU: 3 updated - chromium cutepdf vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4c8e509f8248e857fdd7845ff33d48fa737fce28

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.0242_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.0242_x64.zip"
+  file          = "$toolsDir\gvim_9.0.0246_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.0246_x64.zip"
 }
 
 $installArgs = @{
