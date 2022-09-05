6a0f748196e63f93b1c3b5d78be5a258eb9ca169
Chocolatey Community <chocolatey@realdimensions.net>
2022-09-05 12:14:33 +0000

    AU: 4 updated - freecad poweriso vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/ffcb9d4944150816b00cc6965de2300361465f12

--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/c4722f1bb292284e0437eb7547808a5788fe6430/VSCodeSetup-ia32-1.72.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/c4722f1bb292284e0437eb7547808a5788fe6430/VSCodeSetup-x64-1.72.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/d17726fe4beae5abe87ba9e9429cd298be8b53c4/VSCodeSetup-ia32-1.72.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/d17726fe4beae5abe87ba9e9429cd298be8b53c4/VSCodeSetup-x64-1.72.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = '4042cda638fdda4ccafae7fc3f4fa6890d864c85830b146535df61078ffba3d37ce3ba12e7a5c3ca78cd233f06877530d6ba26782d45c3f7d70593ed02ee3127'
+  checksum       = 'b45e2bd3e20ee8466ef481bd06e476648f116246eff7e44cf6ba0396bb97e11ded6af38b4f3438a12463604a76a3510cade99f8166dfe104a18e5d183a20792f'
   checksumType   = 'sha512'
-  checksum64     = '1bbcb53ded91cf2114ea7612c2aff7b5d0d3150f7d20108098492b82a7fd529440ca81a9deb5938007cc4bef959e9762c31ea62f87f7ab9c1838064f0a705fb5'
+  checksum64     = '932cbe4ab2dc5272a57fa75f36df669180353c90161f766b35e391594809595ffddadd68ff7ef64063f5f1880ece2c1a3f4936ec3f7bee77001b0dbaf13087ba'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
