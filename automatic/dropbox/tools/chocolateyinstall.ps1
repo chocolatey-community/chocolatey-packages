96e8a915d949f0cbdd4bd3a81264313fccf7cbae
Chocolatey Community <chocolatey@realdimensions.net>
2022-09-22 18:22:30 +0000

    AU: 2 updated - dropbox spotify [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/046fcb437cc4109d76177e7ea757182f0b0e815b

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '157.4.4808'
+$version = '158.3.4547'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20157.4.4808%20Offline%20Installer.x86.exe'
-    checksum       = '3c5f34ab81ef7c57ea73f9289ab678a06c3a262818d748f7f29333db2cda81c6'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20158.3.4547%20Offline%20Installer.x86.exe'
+    checksum       = '00826c3adf30dabfc005d841026c9a73ba74cd6b10a74567e079260008bc688e'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
