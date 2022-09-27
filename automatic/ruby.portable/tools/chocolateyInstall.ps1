11ff7606b4178ed6e6c1bd64ad48cdc795c7edb5
Kim J. Nordmo <kim@chocolatey.io>
2022-09-27 12:07:02 +0200

    (ruby.portable) Fix incorrect variable used as destination

--- automatic/ruby.portable/tools/chocolateyInstall.ps1
+++ automatic/ruby.portable/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   File        = "$toolsPath\rubyinstaller-3.1.2-1-x86.7z"
   File64      = "$toolsPath\rubyinstaller-3.1.2-1-x64.7z"
-  Destination = "$toolsDir\ruby"
+  Destination = "$toolsPath\ruby"
   PackageName = $env:ChocolateyPackageName
 }
 
