78feb68f5e9c57df594582ed6b481ce3f34b9295
Chocolatey Community <chocolatey@realdimensions.net>
2022-09-30 00:14:37 +0000

    AU: 3 updated - SQLite sysinternals vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1dddfd0dc5269a3488c42ef2863b1001076bd782

--- automatic/sysinternals/tools/chocolateyInstall.ps1
+++ automatic/sysinternals/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ Write-Host "Sysinternals Suite is going to be installed in '$installDir'"
 $packageArgs = @{
   packageName    = 'sysinternals'
   url            = 'https://download.sysinternals.com/files/SysinternalsSuite.zip'
-  checksum       = '69e75f54a70a76277dd9a4b6f1d739d7f4237e606391dcfe56890bacabe77dd7'
+  checksum       = '4356ef2832d2382c36e37d3e05a3a9dbe3b8f658646eb417b232f212d52ce793'
   checksumType   = 'sha256'
   unzipLocation  = $installDir
 }
@@ -18,7 +18,7 @@ Accept-Eula
 if ($installDir -ne $toolsPath) { Install-ChocolateyPath $installDir }
 if (Is-NanoServer) {
   $packageArgs.url = 'https://download.sysinternals.com/files/SysinternalsSuite-Nano.zip'
-  $packageArgs.checksum = 'f270d19689d9763c70d6b4ea559cfb9dda8104036fe62123de9e9c650314a5d3'
+  $packageArgs.checksum = 'c3f0e3ebc9039840b07ebb6f6dd6196ad12a93f29fdd6b25f0785b132cafbe2c'
  }
 
 $old_path = 'c:\sysinternals'
