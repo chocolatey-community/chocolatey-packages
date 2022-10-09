2f51ff71ebd23f4ba7ee5b8c4ef7d5d353c42c41
Chocolatey Community <chocolatey@realdimensions.net>
2022-10-09 12:14:15 +0000

    AU: 5 updated - chromium freeciv mkvtoolnix peazip peazip.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1079aa8b585026e38c5fe0c2fa6751132a495fca

--- automatic/freeciv/tools/chocolateyInstall.ps1
+++ automatic/freeciv/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'Freeciv*'
 
-  checksum       = 'e601b328c113e6f276a107b637f59f6ef42ba760cab52fa2d1e57a47893788e3'
+  checksum       = '4f4f09302c6457cc8a3d67ed371969ae344a133ffdb9c1e5471c9720dc5220cd'
   checksumType   = 'sha256'
-  url            = 'https://files.freeciv.org/packages/windows/Freeciv-3.0.3-msys2-win64-gtk3.22-setup.exe'
+  url            = 'https://files.freeciv.org/packages/windows/Freeciv-3.0.4-msys2-win64-gtk3.22-setup.exe'
 
   silentArgs     = '/S'
   validExitCodes = @(0)
