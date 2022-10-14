c1454e4e768c8a3067ecd979f7353e07de05e91e
James Ruskin <github@justpowerdown.com>
2022-10-14 22:45:51 +0100

    (#2007) Adds a Latest GitHub Release script
    Adds a script to address the updates to GitHub Release parsing.
    Updates #2007

---
+++ scripts/Get-LatestGitHubRelease.ps1
@@ -0,0 +1,35 @@
+﻿function Get-LatestGitHubRelease {
+  <#
+      .SYNOPSIS
+          Gets the latest release of a given GitHub repository
+
+      .EXAMPLE
+          Get-LatestGitHubRelease cloudflare cloudflared
+  #>
+  [CmdletBinding()]
+  param(
+      # Repository owner
+      [Parameter(Mandatory, Position=0)]
+      [string]$Owner,
+
+      # Repository name
+      [Parameter(Mandatory, Position=1)]
+      [string]$Name,
+
+      # GitHub token, used to reduce rate-limiting or access private repositories (needs repo scope)
+      [string]$Token = "$($env:github_api_key)"
+  )
+  end {
+      $Request = @{
+          Uri = "https://api.github.com/repos/$Owner/$Name/releases/latest"
+      }
+
+      if (-not [string]::IsNullOrEmpty($Token)) {
+        $Request.Headers = @{
+            Authorization = "Bearer $($Token)"
+        }
+      }
+
+      Invoke-RestMethod @Request
+  }
+}
