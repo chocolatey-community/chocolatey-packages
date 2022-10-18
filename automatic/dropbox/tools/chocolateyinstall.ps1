6147bfdaec44c6d8202c466c24209f10c85364f9
RB <RedBaron2@users.noreply.github.com>
2022-10-18 16:48:15 -0700

    (dropbox) Add support for 64bit executable

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -14,9 +14,12 @@ if (!(IsVersionAlreadyInstalled $version)) {
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
     url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20162.3.5368%20Offline%20Installer.x86.exe'
+    url64          = ''
     checksum       = 'ca55cc7b54e7087a4dcf5eead4e41c33ffbd6248ab2fb8aa8259afe0777f9d21'
+    checksum64     = ''
     fileType       = 'exe'
     checksumType   = 'sha256'
+    checksumType64 = ''
     silentArgs     = '/s'
     validExitCodes = @(0, 1641, 3010)
   }
--- automatic/dropbox/update.ps1
+++ automatic/dropbox/update.ps1
@@ -5,10 +5,13 @@ import-module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(^[$]version\s*=\s*)('.*')"          = "`$1'$($Latest.RemoteVersion)'"
-      "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(^[$]version\s*=\s*)('.*')"            = "`$1'$($Latest.RemoteVersion)'"
+      "(?i)(^\s*url\s*=\s*)('.*')"            = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+      "(?i)(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
     }
   }
 }
@@ -16,6 +19,8 @@ function global:au_SearchReplace {
 function global:au_BeforeUpdate {
   $Latest.ChecksumType32 = 'sha256'
   $Latest.Checksum32 = Get-RemoteChecksum -Algorithm $Latest.ChecksumType32 -Url $Latest.URL32
+  $Latest.ChecksumType64 = 'sha256'
+  $Latest.Checksum64 = Get-RemoteChecksum -Algorithm $Latest.ChecksumType64 -Url $Latest.URL64
 }
 
 function GetDropbox {
@@ -30,10 +35,13 @@ function GetDropbox {
   $beta = Get-Version $beta | select -expand Version
   # URL no longer valid as of 02/23/2018 $url = "https://dl-web.dropbox.com/u/17/Dropbox%20${beta}.exe"
   $url32 = Get-RedirectedUrl "https://www.dropbox.com/download?build=${beta}&plat=win&type=full"
+  # Adding URL64 on 10/12/2022 due to it now being available
+  $url64 = Get-RedirectedUrl "https://www.dropbox.com/download?build=${beta}&plat=win&type=full&arch=x64"
   $version = -join ($beta , $build)
   @{
     Title         = $Title
     URL32         = $url32
+    URL64         = $url64
     Version       = $version
     RemoteVersion = $beta
   }
