d306517a43797573360eca53c6b77cbcc380eb32
Chocolatey Community <chocolatey@realdimensions.net>
2022-10-21 00:14:19 +0000

    AU: 6 updated - chromium dropbox Firefox googlechromebeta vim virtualbox [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/4295bcb3e3ddc881d3c89f13a361b8afe4f84d6d

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '160.3.4633'
+$version = '160.3.4650'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,8 +13,8 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20160.3.4633%20Offline%20Installer.x86.exe'
-    checksum       = '6bf709a10c20bc58568ce0c178087390680e5544564c2a52e11f42a2c98b2a0e'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20160.3.4650%20Offline%20Installer.x86.exe'
+    checksum       = '5b44fd8d90bad402eb429e0b1d3b90314d85643f1f06115bbe4b840b5027df8e'
     fileType       = 'exe'
     checksumType   = 'sha256'
     silentArgs     = '/s'
