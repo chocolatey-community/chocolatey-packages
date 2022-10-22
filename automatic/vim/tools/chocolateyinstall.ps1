ae8993a5afe9d103eaaa2def04b43abe870227d9
Chocolatey Community <chocolatey@realdimensions.net>
2022-10-22 00:14:02 +0000

    AU: 3 updated - chromium patch-my-pc vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b280340bfc0c465a68a878acff1184213d1e49b9

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.0809_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.0809_x64.zip"
+  file          = "$toolsDir\gvim_9.0.0813_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.0813_x64.zip"
 }
 
 $installArgs = @{
