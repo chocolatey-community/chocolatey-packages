73c91213510a6ce4c806fa7ec8bdc0ec9e8e46a5
Chocolatey Community <chocolatey@realdimensions.net>
2022-10-25 14:59:37 +0000

    AU: 2 updated - php selenium-chrome-driver [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/199b5f7888a8a1c6242f02e3b916cea444f7bc46

--- automatic/php/tools/chocolateyInstall.ps1
+++ automatic/php/tools/chocolateyInstall.ps1
@@ -14,10 +14,10 @@ if ($installLocation) {
 $pp = Get-PackageParameters
 
 $filesInfo = @{
-  filets32  = "$toolsPath\php-7.4.32-Win32-vc15-x86.zip"
-  filets64  = "$toolsPath\php-7.4.32-Win32-vc15-x64.zip"
-  filents32 = "$toolsPath\php-7.4.32-nts-Win32-vc15-x86.zip"
-  filents64 = "$toolsPath\php-7.4.32-nts-Win32-vc15-x64.zip"
+  filets32  = "$toolsPath\php-8.0.25-Win32-vs16-x86.zip"
+  filets64  = "$toolsPath\php-8.0.25-Win32-vs16-x64.zip"
+  filents32 = "$toolsPath\php-8.0.25-nts-Win32-vs16-x86.zip"
+  filents64 = "$toolsPath\php-8.0.25-nts-Win32-vs16-x64.zip"
 }
 
 if ($pp.ThreadSafe) {
