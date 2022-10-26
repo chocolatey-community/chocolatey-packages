71b913f7200998f04a23b12a1250302db04bb6cd
James Ruskin <github@justpowerdown.com>
2022-10-26 21:11:38 +0100

    (kubernetes-kompose) Updates Update.ps1 with Get-GitHubRelease
    Updates to use the GitHub REST API for releases.
    This adds to #2007.

--- automatic/kubernetes-kompose/update.ps1
+++ automatic/kubernetes-kompose/update.ps1
@@ -3,9 +3,6 @@ param([switch] $Force)
 
 Import-Module AU
 
-$domain   = 'https://github.com'
-$releases = "$domain/kubernetes/kompose/releases/latest"
-
 function global:au_BeforeUpdate {
   Get-RemoteFiles -Purge -NoSuffix -FileNameBase "kompose"
 }
@@ -22,17 +19,12 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-  $re = '\.exe$'
-  $url = $download_page.links | ? href -match $re | % href | select -First 1
-
-  $version = (Split-Path ( Split-Path $url ) -Leaf).Substring(1)
+  $LatestRelease = Get-GitHubRelease kubernetes kompose
 
   return @{
-    Version     = $version
-    URL64       = $url
-    ReleaseURL  = "$domain/kubernetes/kompose/releases/tag/v${version}"
+    Version     = $LatestRelease.tag_name.TrimStart("v")
+    URL64       = $LatestRelease.assets | Where-Object {$_.name.EndsWith(".exe")} | Select-Object -ExpandProperty browser_download_url
+    ReleaseURL  = $LatestRelease.html_url
   }
 }
 
