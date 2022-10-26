b5d40713f18ffc27ebd2d5cc174c6d46aad9343c
James Ruskin <github@justpowerdown.com>
2022-10-26 21:08:10 +0100

    (paint.net) Updates Update.ps1 with Get-GitHubRelease
    Updates to use the GitHub REST API for releases.
    This adds to #2007.

--- automatic/paint.net/update.ps1
+++ automatic/paint.net/update.ps1
@@ -1,7 +1,5 @@
 ﻿Import-Module AU
 
-$releases = 'https://github.com/paintdotnet/release/releases'
-
 function global:au_SearchReplace {
   @{
       ".\legal\VERIFICATION.txt" = @{
@@ -20,15 +18,11 @@ function global:au_BeforeUpdate {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $domain  = $releases -split '(?<=//.+)/' | select -First 1
-    $re = 'paint.net.+.install.x64.zip'
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
-    $version = $url -split '/' | select -Last 1 -Skip 1
+    $LatestRelease = Get-GitHubRelease paintdotnet release
 
     @{
-        Version = $version.Substring(1)
-        Url64   = $domain + $url
+        Version = $LatestRelease.tag_name.TrimStart("v")
+        Url64   = $LatestRelease.assets | Where-Object {$_.name -match 'paint.net.+.install.x64.zip'} | Select-Object -ExpandProperty browser_download_url
     }
 }
 
