f0f498e82180021a31759ad1a0f6fc3b4161e193
James Ruskin <github@justpowerdown.com>
2022-10-26 21:08:10 +0100

    (poi) Updates Update.ps1 with Get-GitHubRelease
    Updates to use the GitHub REST API for releases.
    This adds to #2007.

--- automatic/poi/update.ps1
+++ automatic/poi/update.ps1
@@ -1,7 +1,5 @@
 import-module au
 
-$releases = 'https://github.com/poooi/poi/releases'
-
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
@@ -21,20 +19,12 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-
-
-    $download_page = Invoke-WebRequest -Uri "$releases$lastVersionString" -UseBasicParsing
-    $urls = $download_page.links | ? href -match 'poi-setup-.+\.exe$' | % href
-    $url = $urls | select -First 1
-
-    $exeSep  = (Split-Path($url) -Leaf) -split 'poi-setup-|\.exe'
-    $version = $exeSep | select -Last 1 -Skip 1
-    $validVersion = $version -replace "beta\.", "beta"
+    $LatestRelease = Get-GitHubRelease poooi poi
 
     @{
-        URL32   = 'https://github.com' + $url
-        Version = $validVersion
-        ReleaseNotes = "$releases/tag/v${version}"
+        URL32   = $LatestRelease.assets | Where-Object {$_.name -match '^poi-setup-(?<Version>.+)\.exe$'} | Select-Object -ExpandProperty browser_download_url
+        Version = $LatestRelease.tag_name.TrimStart("v") -replace "beta\.", "beta"
+        ReleaseNotes = $LatestRelease.html_url
     }
 }
 
