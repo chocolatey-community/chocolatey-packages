b8a25d7bf5fb8224c27efaefb765b6dd78134e75
Chocolatey Community <chocolatey@realdimensions.net>
2022-10-26 06:14:18 +0000

    AU: 2 updated - activepresenter GoogleChrome [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d65ef432cb56ddadb1d0a34ff9dcd9eefa3b8f1c

--- automatic/activepresenter/tools/chocolateyInstall.ps1
+++ automatic/activepresenter/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'activepresenter'
   fileType       = 'exe'
-  file           = "$toolsDir\ActivePresenter_v9.0.1_setup.exe"
+  file           = "$toolsDir\ActivePresenter_v9.0.2_setup.exe"
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0)
 }
--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '106.0.5249.119'
+$version = '107.0.5304.63'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = 'c17a0e7c02696ac19c582dcdd68828cf4e8c379746d921bab3c8ca9955399dda'
-  checksum64             = 'f388d363a166463a655c88419c9ad13f1b0412e93b5a1e3ea8763d8fc8338b4c'
+  checksum               = 'ed6055ed6b81fe270cee0b90ef367ef2907002666074f9369ddc30033dd4308c'
+  checksum64             = '132f618eea2e2ef89356d66dcdcfc2394ddfb13b384159691e3fff7ed1a8f1ba'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
