dd361112410c43d803835230b4f9a640284f2930
James Ruskin <github@justpowerdown.com>
2022-10-26 21:07:45 +0100

    (yo) Updates Update.ps1 with Get-GitHubRelease
    Updates to use the GitHub REST API for releases.
    This adds to #2007.

--- automatic/yo/update.ps1
+++ automatic/yo/update.ps1
@@ -1,7 +1,5 @@
 import-module au
 
-$releases = 'https://github.com/yeoman/yo/releases'
-
 function global:au_SearchReplace {
   @{
     'tools\ChocolateyInstall.ps1' = @{
@@ -11,18 +9,12 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-  #https://github.com/yeoman/yo/archive/v1.8.5.zip
-  $re = "(.*).zip"
-  $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
-  $file = $url -split 'v' | Select-Object -last 1
-
-  $version = [IO.Path]::GetFileNameWithoutExtension($file)
+  $LatestRelease = Get-GitHubRelease yeoman yo
+  $Version = $LatestRelease.tag_name.TrimStart("v")
 
   @{
-    Version       = Get-Version $version
-    RemoteVersion = $version
+    Version       = Get-Version $Version
+    RemoteVersion = $Version
   }
 }
 
