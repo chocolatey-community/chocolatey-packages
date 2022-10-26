9667c839be1a4e0df624b2f3afc23132153b98ae
James Ruskin <github@justpowerdown.com>
2022-10-26 21:11:37 +0100

    (k9s) Updates Update.ps1 with Get-GitHubRelease
    Updates to use the GitHub REST API for releases.
    This adds to #2007.

--- automatic/k9s/update.ps1
+++ automatic/k9s/update.ps1
@@ -3,9 +3,6 @@ param([switch] $Force)
 
 Import-Module AU
 
-$domain = 'https://github.com'
-$releases = "$domain/derailed/k9s/releases/latest"
-
 function global:au_BeforeUpdate {
   Get-RemoteFiles -Purge -NoSuffix
 }
@@ -25,24 +22,17 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $LatestRelease = Get-GitHubRelease derailed k9s
 
-  $re = '_Windows_x86_64\.tar.gz$'
-  $url = $download_page.links | ? href -match $re | % href | select -First 1
-
-  $version = (Split-Path ( Split-Path $url ) -Leaf).Substring(1)
-  $filename64 = Split-Path $url -Leaf
-
-  $checksumAsset = $domain + ($download_page.Links | ? href -match "checksums\.txt$" | select -first 1 -expand href)
+  $checksumAsset = $LatestRelease.assets | Where-Object {$_.name -eq 'checksums.txt'} | Select-Object -ExpandProperty browser_download_url
   $checksum_page = Invoke-WebRequest -Uri $checksumAsset -UseBasicParsing
-
   $checksum64 = [regex]::Match($checksum_page, "([a-f\d]+)\s*$([regex]::Escape($filename64))").Groups[1].Value
 
   return @{
-    Version        = $version
-    URL64          = "$domain/derailed/k9s/releases/download/v${version}/${filename64}" 
-    ReleaseNotes   = "$domain/derailed/k9s/blob/v${version}/change_logs/release_v${version}.md"
-    ReleaseURL     = "$domain/derailed/k9s/releases/tag/v${version}"
+    Version        = $LatestRelease.tag_name.TrimStart("v")
+    URL64          = $LatestRelease.assets | Where-Object {$_.name -eq 'k9s_Windows_x86_64.tar.gz'} | Select-Object -ExpandProperty browser_download_url
+    ReleaseNotes   = "https://github.com/derailed/k9s/blob/$($LatestRelease.tag_name)/change_logs/release_$($LatestRelease.tag_name).md"
+    ReleaseURL     = $LatestRelease.html_url
     Checksum64     = $checksum64
     ChecksumType64 = "sha256"
   }
