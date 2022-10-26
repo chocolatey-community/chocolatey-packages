c4610ca91eed091260b5c15876f71c73fbd7dfe3
James Ruskin <github@justpowerdown.com>
2022-10-26 21:11:40 +0100

    (simplewall) Updates Update.ps1 with Get-GitHubRelease
    Updates to use the GitHub REST API for releases.
    This adds to #2007.

--- automatic/simplewall/update.ps1
+++ automatic/simplewall/update.ps1
@@ -1,17 +1,12 @@
 import-module au
 
-$releases = 'https://github.com/henrypp/simplewall/releases/latest'
-
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url_i = $download_page.links | ? href -match '.exe$' | select -First 1 -expand href
-    $url_p = $download_page.links | ? href -match '.zip$' | select -First 1 -expand href
-    $version = $url_i -split '-' | select -First 1 -Skip 1
+    $LatestRelease = Get-GitHubRelease henrypp simplewall
 
     @{
-        Version = $version
-        URL32_i = 'https://github.com' + $url_i
-        URL32_p = 'https://github.com' + $url_p
+        Version = $LatestRelease.tag_name.TrimStart("v.")  # Tags have a "v." prefix, not a typo
+        URL32_i = $LatestRelease.assets | Where-Object {$_.name.EndsWith(".exe")} | Select-Object -ExpandProperty browser_download_url
+        URL32_p = $LatestRelease.assets | Where-Object {$_.name.EndsWith(".zip")} | Select-Object -ExpandProperty browser_download_url
     }
 }
 
