fe5babdad3dd5cb4d067b6678784cfcf04bcd570
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2022-10-26 21:23:08 -0500

    (notepadplusplus) Update AU script with GH release script
    Also fixes notepadplusplus.install and .commandline

--- automatic/notepadplusplus/update.ps1
+++ automatic/notepadplusplus/update.ps1
@@ -10,22 +10,14 @@ function global:au_SearchReplace {
  }
 
 function global:au_GetLatest {
-    $tags = "https://github.com/notepad-plus-plus/notepad-plus-plus/tags"
-    $release = Invoke-WebRequest $tags -UseBasicParsing
-    $new = (( $release.links -match "\/v\d+\.\d+(\.\d+)?" ) -split " " | select -First 10 | Select -Last 1 )
-    $new = $new.Substring(1,$new.Length-2)
-    $https = "https://github.com"
-    $releases = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/tag/v$new/"
-    $download_page = Invoke-WebRequest $releases -UseBasicParsing
-    $url_i         = $download_page.Links | ? href -match '.exe$' | Select-Object -Last 2 | % { $https + $_.href }
-    $url_p         = $download_page.Links | ? href -match '.7z$' | % { $https + $_.href }
+    $LatestRelease = Get-GitHubRelease -Owner "notepad-plus-plus" -Name "notepad-plus-plus"
 
     @{
-        Version = Get-Version ( $url_i -notmatch 'x64' | select -First 1 )
-        URL32_i = $url_i -notmatch 'x64' | select -First 1
-        URL64_i = $url_i -match 'x64'  | select -First 1
-        URL32_p = $url_p -notmatch 'x64' -notmatch 'minimalist'  | select -First 1
-        URL64_p = $url_p -match 'x64' -notmatch 'minimalist'  | select -First 1
+        Version = $LatestRelease.tag_name.Trim("v")
+        URL32_i = $LatestRelease.assets | Where-Object {$_.name.EndsWith("Installer.exe")} | Select-Object -ExpandProperty browser_download_url
+        URL64_i = $LatestRelease.assets | Where-Object {$_.name.EndsWith("x64.exe")} | Select-Object -ExpandProperty browser_download_url
+        URL32_p = $LatestRelease.assets | Where-Object {$_.name.EndsWith("portable.7z")} | Select-Object -ExpandProperty browser_download_url
+        URL64_p = $LatestRelease.assets | Where-Object {$_.name.EndsWith("portable.x64.7z")} | Select-Object -ExpandProperty browser_download_url
     }
 }
 
