c3094ffd0ebbd2d12e2e225807a177b401a296df
Chocolatey Community <chocolatey@realdimensions.net>
2022-11-02 00:14:53 +0000

    AU: 11 updated - chromium cmake cmake.install cmake.portable mkdocs nodejs nodejs.install OpenSSL.Light vim vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/d01831f39f49e35c88c3b86c6ceb25becd99df19

--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/9ad069358e9f09d1f49a90db36b73c953505438a/VSCodeSetup-ia32-1.73.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/9ad069358e9f09d1f49a90db36b73c953505438a/VSCodeSetup-x64-1.73.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/8fa188b2b301d36553cbc9ce1b0a146ccb93351f/VSCodeSetup-ia32-1.73.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/8fa188b2b301d36553cbc9ce1b0a146ccb93351f/VSCodeSetup-x64-1.73.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = '6cc34cbc78972ad7bd040785be830290b028a2cb59e3e60183043f8d81ed473cb93b3db1f59b114dda8c7720f25a26163b3563b576480f54772f7c8074d0660e'
+  checksum       = '729d0debc619a570f96f7e02c4f75d4878c2bf17985b8a37805a7d87f8659c7b0a99d170ec314da884809556fc26fe01ecad4e76e815f7606140d0133db1dddc'
   checksumType   = 'sha512'
-  checksum64     = 'a90728b0f9d9a22476b6d199a1a8ebfe6e62f063b38a0dd4712f85bf10cff2642f9cccbf0b105473aa52ad24bf0bb9c789dbfcd5b1e135ea20c3c676d83e0bd7'
+  checksum64     = '8927cacc6abbf1a47482f9a2a612ec43f337bb4e6399c445645bf1c45f8030c1fe8a999181a0edddcd4fe00030b373d6b1188c24a250f6664420fbe21f7c64e8'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
