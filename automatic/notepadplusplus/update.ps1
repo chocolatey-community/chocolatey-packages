fd5e005e5568853626dabfab166bf859b8510520
Alex Lopez <12056704+virtualex-itv@users.noreply.github.com>
2022-11-03 12:03:41 -0400

    (notepadplusplus) updated to use api

--- automatic/notepadplusplus/update.ps1
+++ automatic/notepadplusplus/update.ps1
@@ -1,5 +1,6 @@
 import-module au
 
+$releases = "https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases"
 
 function global:au_SearchReplace {
   @{
@@ -10,22 +11,20 @@ function global:au_SearchReplace {
  }
 
 function global:au_GetLatest {
-    $tags = "https://github.com/notepad-plus-plus/notepad-plus-plus/tags"
-    $release = Invoke-WebRequest $tags -UseBasicParsing
-    $new = (( $release.links -match "\/v\d+\.\d+(\.\d+)?" ) -split " " | select -First 10 | Select -Last 1 )
-    $new = $new.Substring(1,$new.Length-2)
-    $https = "https://github.com"
-    $releases = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/tag/v$new/"
-    $download_page = Invoke-WebRequest $releases -UseBasicParsing
-    $url_i         = $download_page.Links | ? href -match '.exe$' | Select-Object -Last 2 | % { $https + $_.href }
-    $url_p         = $download_page.Links | ? href -match '.7z$' | % { $https + $_.href }
+    $header = @{
+        "Authorization" = "token $env:github_api_key"
+      }
+
+    $download_page = Invoke-WebRequest $releases -UseBasicParsing -Headers $header | ConvertFrom-Json
+    $url_i         = $download_page.assets.browser_download_url -match '.exe$' | select -First 2 -Skip 1
+    $url_p         = $download_page.assets.browser_download_url -match '.7z$' | select -First 6
 
     @{
         Version = Get-Version ( $url_i -notmatch 'x64' | select -First 1 )
         URL32_i = $url_i -notmatch 'x64' | select -First 1
         URL64_i = $url_i -match 'x64'  | select -First 1
-        URL32_p = $url_p -notmatch 'x64' -notmatch 'minimalist'  | select -First 1
-        URL64_p = $url_p -match 'x64' -notmatch 'minimalist'  | select -First 1
+        URL32_p = $url_p -notmatch 'x64' -notmatch 'minimalist' -notmatch 'arm64' | select -First 1
+        URL64_p = $url_p -match 'x64' -notmatch 'minimalist' -notmatch 'arm64' | select -First 1
     }
 }
 
