c789815a6140ae0409126cabf8093370fc803002
Matteo Dell'Aquila <matteo.dellaquila@gmail.com>
2022-11-09 15:00:11 +0100

    (k9s) Switch AU to Get-GitHubRelease
    Signed-off-by: Matteo Dell'Aquila <matteo.dellaquila@gmail.com>

--- automatic/k9s/update.ps1
+++ automatic/k9s/update.ps1
@@ -25,15 +25,15 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $LatestRelease = Get-GitHubRelease -Owner "derailed" -Name "k9s"
 
   $re = '_Windows_x86_64\.tar.gz$'
-  $url = $download_page.links | ? href -match $re | % href | select -First 1
+  $url = $latestrelease.assets.browser_download_url | Where-Object { $_ -match $re } | select -First 1
 
   $version = (Split-Path ( Split-Path $url ) -Leaf).Substring(1)
   $filename64 = Split-Path $url -Leaf
 
-  $checksumAsset = $domain + ($download_page.Links | ? href -match "checksums\.txt$" | select -first 1 -expand href)
+  $checksumAsset = $latestrelease.assets.browser_download_url | Where-Object { $_ -match "checksums\.txt$" } | select -First 1
   $checksum_page = Invoke-WebRequest -Uri $checksumAsset -UseBasicParsing
 
   $checksum64 = [regex]::Match($checksum_page, "([a-f\d]+)\s*$([regex]::Escape($filename64))").Groups[1].Value
