031200def98c0d2aeed73e79b3f9033e68bd1fb7
Chocolatey Community <chocolatey@realdimensions.net>
2022-11-17 22:13:09 +0000

    AU: 6 updated - chromium dropbox julia notepadplusplus notepadplusplus.commandline notepadplusplus.install [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/54ad6a53b161753195a6fee8ba9aa5447acbc710

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '162.3.5368'
+$version = '162.3.5401'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,13 +13,13 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20162.3.5368%20Offline%20Installer.x86.exe'
-    url64          = ''
-    checksum       = 'ca55cc7b54e7087a4dcf5eead4e41c33ffbd6248ab2fb8aa8259afe0777f9d21'
-    checksum64     = ''
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20162.3.5401%20Offline%20Installer.x86.exe'
+    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20162.3.5401%20Offline%20Installer.x64.exe'
+    checksum       = '68fc8d6728acda0e3901b88e9f69a4f96024bb32fd03591b3a08507346792e06'
+    checksum64     = 'a967d1c4ee8cf9ff9c86ad21e2ec1062d5c77d72449008c0d40b327a4ff32f0f'
     fileType       = 'exe'
     checksumType   = 'sha256'
-    checksumType64 = ''
+    checksumType64 = 'sha256'
     silentArgs     = '/s'
     validExitCodes = @(0, 1641, 3010)
   }
