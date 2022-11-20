a508162fbb8c6cdcb3090637ccbf115c8b4d1a86
Chocolatey Community <chocolatey@realdimensions.net>
2022-11-20 12:14:08 +0000

    AU: 2 updated - autohotkey.portable chromium [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/06c87f7c5abe95c8dac5c0b129bd3c339cfab433

--- automatic/autohotkey.portable/tools/chocolateyInstall.ps1
+++ automatic/autohotkey.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$fileName  = 'AutoHotkey_1.1.35.00.zip'
+$fileName  = 'AutoHotkey_2.0-rc.1.zip'
 $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $zip_path = "$toolsPath\$fileName"
 Remove-Item $toolsPath\* -Recurse -Force -Exclude $fileName
