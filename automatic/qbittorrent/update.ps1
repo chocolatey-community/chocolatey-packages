952c78bb95b0a4cedf2c02199066283307bc6c34
gobalius <obal.goran@gmail.com>
2022-12-08 21:43:54 +0100

    Removed Search&Replace for 32-bit, adjusted link parsing for 64-bit

--- automatic/qbittorrent/update.ps1
+++ automatic/qbittorrent/update.ps1
@@ -7,14 +7,11 @@ function global:au_BeforeUpdate { Get-RemoteFiles -Purge -FileNameSkip 1 -NoSuff
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName32)`""
       "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\).*" = "`${1}$($Latest.FileName64)`""
     }
     ".\tools\verification.txt" = @{
-      "(?i)(32-Bit.+)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(64-Bit.+)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-      "(?i)(checksum32:\s+).*" = "`${1}$($Latest.Checksum32)"
+      "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"
       "(?i)(checksum64:\s+).*" = "`${1}$($Latest.Checksum64)"
     }
   }
@@ -33,22 +30,14 @@ function global:au_GetLatest {
   }
 
   $re    = 'setup\.exe\/download$'
-  $urls  = $download_page.links | ? href -match $re | select -First 2 -expand href
-  $url32 = $urls | ? { $_ -notmatch "x64" }  | select -first 1
+  $urls  = $download_page.links | ? href -match $re | select -First 1 -expand href
   $url64 = $urls | ? { $_ -match "x64" } | select -first 1
 
-  $version   = $url32 -split '[_]' | select -Last 1 -Skip 1
   $version64 = $url64 -split '[_]' | select -Last 1 -Skip 2
 
-#  Beginning with v4.5.0, only 64-bit version is avalable, so this check is not needed anymore.
-#  if ($version -ne $version64) {
-#    throw "32-bit and 64-bit version do not match. Please investigate."
-#  }
-
   return @{
-    URL32    = $url32
     URL64    = $url64
-    Version  = $version
+    Version  = $version64
     FileType = 'exe'
   }
 }
