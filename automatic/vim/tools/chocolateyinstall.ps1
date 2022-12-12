790cc37f2beb46fdfa9cc872a48eeded92235a6b
Chocolatey Community <chocolatey@realdimensions.net>
2022-12-12 00:14:04 +0000

    AU: 1 updated - vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/494925246c2041037b6aff25caeea7feb31e8892

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.1046_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.1046_x64.zip"
+  file          = "$toolsDir\gvim_9.0.1047_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.1047_x64.zip"
 }
 
 $installArgs = @{
