df9e8133346368eafe6898a0b39579b34b328769
Chocolatey Community <chocolatey@realdimensions.net>
2022-12-16 12:54:47 +0000

    AU: 1 updated - audacity [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/1e80f6b00882da13758d6c72ccedba21a6ef8d85

--- automatic/audacity/tools/chocolateyInstall.ps1
+++ automatic/audacity/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'audacity'
   fileType       = 'exe'
-  file           = "$toolsDir\audacity-win-3.2.1-32bit.exe"
-  file64         = "$toolsDir\audacity-win-3.2.1-64bit.exe"
+  file           = "$toolsDir\audacity-win-3.2.2-x32.exe"
+  file64         = "$toolsDir\audacity-win-3.2.2-x64.exe"
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0, 1223)
 }
