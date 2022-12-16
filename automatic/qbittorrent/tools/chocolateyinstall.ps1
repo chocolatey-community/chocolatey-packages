ed1459cfec3a74509c4672d47d8c5432bb3610c1
gobalius <99242665+gobalius@users.noreply.github.com>
2022-12-16 13:12:10 +0100

    Update chocolateyinstall.ps1

--- automatic/qbittorrent/tools/chocolateyinstall.ps1
+++ automatic/qbittorrent/tools/chocolateyinstall.ps1
@@ -10,7 +10,6 @@ $packageArgs = @{
   packageName    = 'qbittorrent'
   fileType       = 'exe'
   softwareName   = 'qBittorrent*'
-  file           = "$toolsDir\qbittorrent_4.4.5_setup.exe"
   file64         = "$toolsDir\qbittorrent_4.4.5_x64_setup.exe"
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
