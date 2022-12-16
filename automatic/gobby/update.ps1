a0ee44d40b5baf3a3abf35f85fe746af1cded632
Kim J. Nordmo <kim@chocolatey.io>
2022-12-16 15:44:15 +0100

    (gobby) Fix github releases not being found

--- automatic/gobby/update.ps1
+++ automatic/gobby/update.ps1
@@ -1,13 +1,11 @@
-﻿Import-Module AU
-
-$releases     = 'https://github.com/gobby/gobby/releases'
+Import-Module AU
 
 function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
 
 function global:au_SearchReplace {
   @{
-    ".\legal\VERIFICATION.txt"        = @{
-      "(?i)(download location on\s*)<.*>" = "`${1}<$releases>"
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(download location on\s*)<.*>" = "`${1}<$($Latest.ReleaseNotesUrl)>"
       "(?i)(\s*1\..+)\<.*\>"              = "`${1}<$($Latest.URL32)>"
       "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
       "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
@@ -18,16 +16,16 @@ function global:au_SearchReplace {
   }
 }
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $LatestRelease = Get-GitHubRelease 'gobby' 'gobby'
 
   $re = 'gobby\-[0-9\.]+\-x64.exe$'
-  $url32 = $download_page.Links | ? href -match $re | select -Last 1 | % { [uri]::new([uri]$releases, $_.href) }
+  $url32 = $LatestRelease.assets | ? name -match $re | select -Last 1 -ExpandProperty browser_download_url
 
-  $verRe = '\/v?'
-  $version32 = $url32 -split "$verRe" | select -Last 1 -Skip 1
+  $version32 = $LatestRelease.tag_name.TrimStart('v')
   @{
-    URL32 = $url32
-    Version = $version32
+    URL32           = $url32
+    Version         = $version32
+    ReleaseNotesUrl = $LatestRelease.html_url
   }
 }
 
