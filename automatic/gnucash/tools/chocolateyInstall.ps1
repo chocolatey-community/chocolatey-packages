8bb7efb85c6e6ac8f20e3a6f8f7aaa6add23bca7
Chocolatey Community <chocolatey@realdimensions.net>
2022-12-19 18:14:10 +0000

    AU: 1 updated - gnucash [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/89c734a3ada3c15744820fca63291a2928d32cb3

--- automatic/gnucash/tools/chocolateyInstall.ps1
+++ automatic/gnucash/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\gnucash-4.12.setup.exe"
+  file           = "$toolsPath\gnucash-4.13.setup.exe"
 
   softwareName   = 'GnuCash*'
 
