480a40eafaa266cc46aa9d9d00170b945aeb9914
Chocolatey Community <chocolatey@realdimensions.net>
2023-01-03 00:13:56 +0000

    AU: 3 updated - chromium mkvtoolnix vim [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/c52f2659e30cd3df2971121c518a37e5003f3665

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.1128_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.1128_x64.zip"
+  file          = "$toolsDir\gvim_9.0.1136_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.1136_x64.zip"
 }
 
 $installArgs = @{
