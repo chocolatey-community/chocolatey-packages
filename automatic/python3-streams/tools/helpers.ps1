248a864d36d3917bce480358b7456d1d1ae54f59
Chocolatey Community <chocolatey@realdimensions.net>
2023-01-03 09:43:04 +0000

    AU: 3 updated - chromium mkdocs-material python39 [skip ci]

--- automatic/python3-streams/tools/helpers.ps1
+++ automatic/python3-streams/tools/helpers.ps1
@@ -37,9 +37,9 @@ function Install-Python {
   }
 
   $packageArgs = @{
-    packageName    = 'python311'
+    packageName    = 'python39'
     fileType       = 'exe'
-    file           = "$toolsPath\python-3.11.1.exe"
+    file           = "$toolsPath\python-3.9.13.exe"
     silentArgs     = '/quiet InstallAllUsers=1 PrependPath={0} TargetDir="{1}"' -f $prependPath, $installDir
     validExitCodes = @(0)
   }
@@ -48,7 +48,7 @@ function Install-Python {
   $packageArgs['softwareName'] = "Python 3.$minor_version.*"
 
   if (!$only32Bit) {
-    $packageArgs['file64'] = "$toolsPath\python-3.11.1-amd64.exe"
+    $packageArgs['file64'] = "$toolsPath\python-3.9.13-amd64.exe"
   }
   else {
     $packageArgs['packageName'] = "32-bit $($packageArgs['packageName'])"
