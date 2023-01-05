2f53ae1596674a896bf29652ead8ab952f021645
Chocolatey Community <chocolatey@realdimensions.net>
2023-01-05 18:13:53 +0000

    AU: 2 updated - Firefox krita [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/86d5e9e078c5e391b57323a988a1aa0fbffff9a0

--- automatic/krita/tools/chocolateyInstall.ps1
+++ automatic/krita/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   softwareName = 'Krita'
 
   checksumType = 'sha256'
-  file64       = "$toolsDir\krita-x64-5.1.4-setup.exe"
+  file64       = "$toolsDir\krita-x64-5.1.5-setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
