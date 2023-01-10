190c0c97980fdc895a02a7f66f474720c124d1ae
Chocolatey Community <chocolatey@realdimensions.net>
2023-01-10 06:13:58 +0000

    AU: 1 updated - julia [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/0a32318cef55b114ffda1f8e6f1a67ed493b017f

--- automatic/julia/tools/chocolateyinstall.ps1
+++ automatic/julia/tools/chocolateyinstall.ps1
@@ -5,15 +5,15 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\julia-1.8.4-win32.exe"
-  file64        = "$toolsDir\julia-1.8.4-win64.exe"
+  file          = "$toolsDir\julia-1.8.5-win32.exe"
+  file64        = "$toolsDir\julia-1.8.5-win64.exe"
 
   softwareName  = 'Julia*'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
-$packageVersion = "1.8.4"
+$packageVersion = "1.8.5"
 
 Install-ChocolateyInstallPackage @packageArgs
 
