8f9c817b02a6f16c69cb8fdd26bd5759179e14e4
Chocolatey Community <chocolatey@realdimensions.net>
2023-01-23 18:14:33 +0000

    AU: 5 updated - vscode-insiders vscode-insiders.install winscp winscp.install winscp.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/14adeb9fc9a1620970a84542a1f277f50a2448c0

--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://vscode.blob.core.windows.net/insider/9ccc2b3b30c122afe45bc6722bef0e3901fb2806/VSCodeSetup-ia32-1.75.0-insider.exe'
-  url64bit       = 'https://vscode.blob.core.windows.net/insider/9ccc2b3b30c122afe45bc6722bef0e3901fb2806/VSCodeSetup-x64-1.75.0-insider.exe'
+  url            = 'https://vscode.blob.core.windows.net/insider/f1b09237cd400d92987cf7a9681995e8011ef54c/VSCodeSetup-ia32-1.75.0-insider.exe'
+  url64bit       = 'https://vscode.blob.core.windows.net/insider/f1b09237cd400d92987cf7a9681995e8011ef54c/VSCodeSetup-x64-1.75.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = 'f4111dd190e31abaa8d8965be285da540c7fbd6fe0dc59bd12254ace0f86e0d37a206edeecb051eac9c12e3165a158324803904f19e219056f52eb5112cbc8e4'
+  checksum       = '425fba7477dea3baad702a4587940bcd2f65b6bb1b3a34bd4ce6e698a7334e7a5d9eb396a2724658e38b49120aef799036b26cc649ae90ca94f4acd0b9a3fdb9'
   checksumType   = 'sha512'
-  checksum64     = 'ea8a1f40a4c894cd9cc136e9a709b0c50bc8e5c7c54aec005599cac043ebea9690f76219f3c7fa43055213ae21f0608216779f016e634f1d1eeeebf2dcaaa520'
+  checksum64     = '56b36dda8d80f4af4906d10bc1126f74aed3263f4924644d2923ca4021cd8c2c55bf6a8a83a0a03714b18ff19e4b473c8ce8b726790e454b475cfa36db9bd8e6'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
