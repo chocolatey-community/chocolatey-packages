a146207668f9643f8bb832a0a1294492867417e7
Chocolatey Community <chocolatey@realdimensions.net>
2023-02-06 00:14:15 +0000

    AU: 2 updated - juju tortoisegit [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/bd9d6c08755d3b1b133e2a84eff0da46a5c241c1

--- automatic/juju/tools/chocolateyinstall.ps1
+++ automatic/juju/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName   = 'juju'
   fileType      = 'exe'
   softwareName  = 'Juju'
-  file          = "$toolsDir\juju-setup-2.9.38.exe"
+  file          = "$toolsDir\juju-setup-3.1.0.exe"
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"${env:TEMP}\${env:chocolateyPackageName}.${env:chocolateyPackageVersion}.Install.log`""
   validExitCodes= @(0)
 }
