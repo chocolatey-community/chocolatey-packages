e13944a70bce1c1ba8203a3685e151454d3bab7c
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2023-03-01 15:35:18 +0100

    (sourcetree) Fix update script to get new versions

--- automatic/sourcetree/update.ps1
+++ automatic/sourcetree/update.ps1
@@ -1,21 +1,39 @@
-import-module au
- . ".\update_helper.ps1"
+Import-Module au
+
+function global:au_BeforeUpdate {
+  $Latest.ChecksumType32 = 'sha256'
+  $Latest.Checksum32 = Get-RemoteChecksum -Algorithm $Latest.ChecksumType32 -Url $Latest.URL32
+}
 
 function global:au_SearchReplace {
-    @{
-        "tools\chocolateyInstall.ps1" = @{
-            "(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-        }
-
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-        }
-     }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)^(\s*url\s*=\s*)'.*'"                = "`${1}'$($Latest.URL32)'"
+      "(?i)^(\s*checksum\s*=\s*)'.*'"           = "`${1}'$($Latest.Checksum32)'"
+      "(?i)^(\s*checksumType\s*=\s*)'.*'"       = "`${1}'$($Latest.ChecksumType32)'"
+    }
+  }
 }
 
 function global:au_GetLatest {
-  return Get-JavaSiteUpdates -Package "sourcetree" -Title "Sourcetree for Windows Enterprise"
+	$downloadSite = "https://www.sourcetreeapp.com/download/"
+
+	$Splat = @{
+		Method = 'GET'
+		Uri = $downloadSite
+	}
+
+	$results = Invoke-RestMethod @Splat
+
+	$urlRegEx = '"enterpriseURL":"(.*?)"'
+
+	if ($results -match $URLregEx){
+		$url = $matches[1]
+		$versionFromUrl = ($url -split "/")[-1]
+		$version = $versionFromUrl -replace "^(\w+)_", "" -replace ".msi$", ""
+	}
+
+  @{ URL32 = $url; Version = $version }
 }
 
-update
+update -ChecksumFor none
