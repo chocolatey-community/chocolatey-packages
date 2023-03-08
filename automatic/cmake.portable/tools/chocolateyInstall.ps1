30ded5dca359b880a0b24d832d80f4a2335afad4
Chocolatey Community <chocolatey@realdimensions.net>
2023-03-08 18:14:45 +0000

    AU: 3 updated - cmake cmake.install cmake.portable [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/5dee8306b82d2e1470f9a5496df2d3e5e8051c7e

--- automatic/cmake.portable/tools/chocolateyInstall.ps1
+++ automatic/cmake.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\cmake-3.24.4-windows-i386.zip"
-  file64      = "$toolsPath\cmake-3.24.4-windows-x86_64.zip"
+  file        = "$toolsPath\cmake-3.25.3-windows-i386.zip"
+  file64      = "$toolsPath\cmake-3.25.3-windows-x86_64.zip"
   destination = $toolsPath
 }
 
