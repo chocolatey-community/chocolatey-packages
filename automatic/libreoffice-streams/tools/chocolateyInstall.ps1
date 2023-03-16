83417f2a45414ab0b8cb7f88bfb85bdf72c1f27d
Chocolatey Community <chocolatey@realdimensions.net>
2023-03-16 15:47:33 +0000

    AU: 3 updated - freecad libreoffice-still skype [skip ci] https://gist.github.com/a14b1e5bfaf70839b338eb1ab7f8226f/b4aed5c50f33e766f2a2c1d9f9e6c3e0524133e0

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = 'libreoffice-still'
-  version                = '7.4.5'
+  version                = '7.4.6'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.4.5/win/x86/LibreOffice_7.4.5_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.4.5/win/x86_64/LibreOffice_7.4.5_Win_x64.msi'
-  checksum               = '0e3fa8d7d4aa090e9708f27099f0e0a1f2edde88735e00fad6a1c3b864ca7aa1'
-  checksum64             = 'aacdb6ec15cf1b76287cd089582e8d76fe769b218c1dbcbc7496cda5c3cd662b'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.4.6/win/x86/LibreOffice_7.4.6_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.4.6/win/x86_64/LibreOffice_7.4.6_Win_x64.msi'
+  checksum               = '4f4eec66cd8d9af67fa2d536de0d88569cc4b4e85be75f71f05ce56c819ea767'
+  checksum64             = '870a0c2a550c03754ae7974ccc277806a772488e4fd51ea5e146580f8141dae8'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
