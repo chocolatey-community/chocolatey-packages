15e75be8e2293ada6bd99dcb6c1770c5fa629e5d
Chocolatey Community <chocolatey@realdimensions.net>
2023-03-25 06:14:28 +0000

    AU: 2 updated - chromium selenium-chromium-edge-driver [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/58727f5a84f4fbebfc24dc6068ea64c681484f03

--- automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
+++ automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = 'selenium-chromium-edge-driver'
-  url            = 'https://msedgedriver.azureedge.net/111.0.1661.51/edgedriver_win32.zip'
-  url64          = 'https://msedgedriver.azureedge.net/111.0.1661.51/edgedriver_win64.zip'
-  checksum       = '940bedd974b58a7e50c86c66c56c26905e6fdf0b0c7ccc3ad0b2328c00c2dd67'
-  checksum64     = '3795f47a26e21f265c514f4d2c7771a54c04054f4cf56541568de7a1ab082ac2'
+  url            = 'https://msedgedriver.azureedge.net/111.0.1661.54/edgedriver_win32.zip'
+  url64          = 'https://msedgedriver.azureedge.net/111.0.1661.54/edgedriver_win64.zip'
+  checksum       = 'a521f161fc14b417fba89d1c85fa30f84a8bc42e59233356d166293147270cf2'
+  checksum64     = '97bd1acfeb5595218db4694fbbd48605b9112ea191dab499f4ba3a4b258d9d23'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = $seleniumDir
