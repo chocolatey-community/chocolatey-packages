016950462f3023834c79284aee877471d8009b5d
Chocolatey Community <chocolatey@realdimensions.net>
2023-03-27 16:25:03 +0000

    AU: 3 updated - brave Ghostscript Ghostscript.app [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/68d28c0683d890d062f11f5b77f84d2eb562dce7

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -1,15 +1,15 @@
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = ''
-  checksum    = ''
-  checksumType= ''
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.49.128/BraveBrowserStandaloneSilentSetup32.exe'
+  checksum    = '0C517B98C47EB85F430DF42721DDDD3F3B673F00D440E6D00E4F1C7E7AA5C57A'
+  checksumType= 'sha256'
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.45.123'
+[version]$softwareVersion = '1.49.128'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
