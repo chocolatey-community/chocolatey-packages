8aee477edcec3e896da01cfecde85bdfa11debd7
Chocolatey Community <chocolatey@realdimensions.net>
2023-04-11 18:15:00 +0000

    AU: 9 updated - Firefox smartftp thunderbird vscode vscode.install vscode-insiders vscode-insiders.install winscp winscp.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/db7a18c01f9f49d1de0bb8f02e3699331f74678b

--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/7f03ec6f7a48791d8027761b22773b454fc52478/VSCodeSetup-ia32-1.78.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/7f03ec6f7a48791d8027761b22773b454fc52478/VSCodeSetup-x64-1.78.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/079272e4a37ceecd22dcebc3566bb46c8580a13c/VSCodeSetup-ia32-1.78.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/079272e4a37ceecd22dcebc3566bb46c8580a13c/VSCodeSetup-x64-1.78.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = '444d9fbb65fe18e3b285227b8af6dfbe67ea6326f8132845e3fdf9faf9456b8cb58e94be9eeeba26b191dbc7bd1ef102927d7082ea6481e79740e54c7d115fe9'
+  checksum       = 'bb251a7148191090c870face427becaf7960e16ebb636ca3186161d63e2d813b51ed90c829bf1497de4aab318feda2876be3d76d89a1236e8fb020aacdf12eda'
   checksumType   = 'sha512'
-  checksum64     = 'd30332d6483f2b5044054d48e24f8090d4df56a52ef8287b0d6187e8c364dde14969f1e014d204216f2abe1c9e9e8ef219bb2485ec963cdea5776ccff5778efc'
+  checksum64     = 'f821f71e770eac884b5b1a9eef2299de223671bedfd4db1f22480b2aec6c892ccf00f754b861435b9b8d74ceddc562e54fc7ee33264c2740f783eb0bb7cb3e37'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.77.1'
+$version = '1.77.2'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/b7886d7461186a5eac768481578c1d7ca80e2d21/VSCodeSetup-ia32-1.77.1.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/b7886d7461186a5eac768481578c1d7ca80e2d21/VSCodeSetup-x64-1.77.1.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/e344f1f539a80912a0e9357cec841f36ce97a4e2/VSCodeSetup-ia32-1.77.2.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/e344f1f539a80912a0e9357cec841f36ce97a4e2/VSCodeSetup-x64-1.77.2.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = '12b327000d58c5026a81cb947d8dbc175460351cdfc5cdbeba2163d6ee06cbd1'
+  checksum       = 'a0ddfd672bce71100c5ef482349c38b3c6b92202803acc4fb55ad70957f65233'
   checksumType   = 'sha256'
-  checksum64     = '0de15dd5232398cb68ea96966dce7425e2c1063b95554f24b81e6821fc9c0a40'
+  checksum64     = '12e8a4888cf03acece34a36e5ed41ef81ae465d6db5a7a9a583b267d44d64e2e'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
