26bf33f65c6ee1b60cb7aca964976d54c6c08d76
Chocolatey Community <chocolatey@realdimensions.net>
2023-04-19 12:14:53 +0000

    AU: 5 updated - chromium skype vscode-insiders vscode-insiders.install zotero [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/c5a0ef709e15bc6252deec7e7e2b6503f110d0ca

--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/1e774371f2ca5f6618b4f40fdc72ce7518443014/VSCodeSetup-ia32-1.78.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/1e774371f2ca5f6618b4f40fdc72ce7518443014/VSCodeSetup-x64-1.78.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/545d981034e010e2e31396faf224996c31eceab1/VSCodeSetup-ia32-1.78.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/545d981034e010e2e31396faf224996c31eceab1/VSCodeSetup-x64-1.78.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = '15ab4e1d3a1f783fa1caee24dfd84cd267bb05d62cdf5070b234bc478c8403e3d07862cd9b79922b3340bb952b4006be6a6b888432f752f318b497435bc1ead8'
+  checksum       = '4894e5e02f0f6332eb9253868d40ca4b04ba8570d518472177937d2353304b9ae2129a71085f3aa29bd0423927e1f5047ce5d79373e385e562a05273714fd146'
   checksumType   = 'sha512'
-  checksum64     = '571c2478783db064138bde7b32c574b280e87e98563b65d281d18c210f2115d78a5bacad62919bfce8b4dc2d773a9d9034faf3cf1142422d35f4e9d886c78f26'
+  checksum64     = 'cc6033643ced0b43e99383476d5540cfc381facd0c92c3a6a5ddff564e2aecfe812bbdc24e35fecdc12b8ed263d880a91c430a177b4d2b4fce6a707df5ac312e'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
--- automatic/zotero/tools/chocolateyInstall.ps1
+++ automatic/zotero/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero'
-  file           = "$toolsPath\Zotero-6.0.24_setup.exe"
+  file           = "$toolsPath\Zotero-6.0.25_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
