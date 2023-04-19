9cb637da9270577c9d98a50146c13d222897f373
Chocolatey Community <chocolatey@realdimensions.net>
2023-04-19 00:14:34 +0000

    AU: 3 updated - chromium GoogleChrome vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/c96d6c9d0f5bfebfb1285a54199c382cf760fe27

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '112.0.5615.121'
+$version = '112.0.5615.138'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '29696ca1af025e37d9bad28e72708a8d655d2b95e3052691aba3b73a0f531356'
-  checksum64             = '2f090aa5a2c33e288e418a5b7d423b482dffc1ebfd9ea96af6796611181c213c'
+  checksum               = '35c10bdc56ab894166cc3e4232e9c582c769583dbf496c81e07d3a4032d0a580'
+  checksum64             = 'f3dea4e71c70fcd7f176cc03d64d50f544355ad1a2acfe2bf092ad2f3787433a'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.1464_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.1464_x64.zip"
+  file          = "$toolsDir\gvim_9.0.1469_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.1469_x64.zip"
 }
 
 $installArgs = @{
