558f7c6d65dc8b9de8427e51bfb598780a930bfe
Chocolatey Community <chocolatey@realdimensions.net>
2023-04-22 00:13:59 +0000

    AU: 3 updated - chromium displayfusion vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/e423de1efb78ee0976d40d89c3e45db9723ccd84

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.1471_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.1471_x64.zip"
+  file          = "$toolsDir\gvim_9.0.1474_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.1474_x64.zip"
 }
 
 $installArgs = @{
