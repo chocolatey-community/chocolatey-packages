e2318a45fc9e4b830adc7c060f8783f2bc0aade9
Chocolatey Community <chocolatey@realdimensions.net>
2023-04-26 00:14:20 +0000

    AU: 3 updated - chromium rufus vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/b6c647fc98d7e4f95c61a3956307d377d21d998f

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.1479_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.1479_x64.zip"
+  file          = "$toolsDir\gvim_9.0.1488_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.1488_x64.zip"
 }
 
 $installArgs = @{
