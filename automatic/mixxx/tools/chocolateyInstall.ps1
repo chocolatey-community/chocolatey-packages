17ac42e2accf27e1872c9a5a355eef6a7f7e57ce
Chocolatey Community <chocolatey@realdimensions.net>
2023-04-29 00:13:53 +0000

    AU: 3 updated - chromium mixxx vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/1ba0c91801dbc98621b16288388fa4391ae1359d

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-alpha-26-g6b1844e469-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-alpha-29-gf36cd4f290-win64.msi"
 
   softwareName   = 'Mixxx *'
 
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.1494_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.1494_x64.zip"
+  file          = "$toolsDir\gvim_9.0.1496_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.1496_x64.zip"
 }
 
 $installArgs = @{
