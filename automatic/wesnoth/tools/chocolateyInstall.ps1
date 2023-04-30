bb6bbca7b0c3ab34831c736fe654f41c62bd2fc9
Chocolatey Community <chocolatey@realdimensions.net>
2023-04-30 18:19:30 +0000

    AU: 3 updated - freecad texmaker wesnoth [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/31a700f729c1f77e12c915d2ea60177e9ff49f7d

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.16/wesnoth-1.16.9/wesnoth-1.16.9-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.16/wesnoth-1.17.16-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = '6c2b68d6bfb7ce823dd93d8dd4b5aa665ce03d2d8ddacd3007aa1549d931a884'
+  checksum       = 'bb32908541243bddf72c3ca2623ea7eaa77b307d315485ec5bd7ae653bc3ea01'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
