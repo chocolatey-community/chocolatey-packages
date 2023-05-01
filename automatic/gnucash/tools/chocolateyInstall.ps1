305b1d532f02add6e4317d656524cccadf473863
Chocolatey Community <chocolatey@realdimensions.net>
2023-05-01 00:14:17 +0000

    AU: 3 updated - gnucash smartftp Waterfox [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/762198e4c5fa8b8c5313730e0444e2d914c10313

--- automatic/gnucash/tools/chocolateyInstall.ps1
+++ automatic/gnucash/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\gnucash-5.0.setup.exe"
+  file           = "$toolsPath\gnucash-5.1.setup.exe"
 
   softwareName   = 'GnuCash*'
 
