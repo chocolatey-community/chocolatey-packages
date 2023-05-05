5b785747aa8d457959b51af3a496e8d23ad4f8d0
Chocolatey Community <chocolatey@realdimensions.net>
2023-05-05 11:37:24 +0000

    AU: 4 updated - alldup chromium graphviz mixxx [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/d876264c7b879c5bcfe5d7932d2fdd4d432be66f

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-alpha-29-gf36cd4f290-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-alpha-30-gd993ec5bab-win64.msi"
 
   softwareName   = 'Mixxx *'
 
