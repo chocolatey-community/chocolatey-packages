2038ec23e0d075f00e8d7aaa28b0d2fe59aa38d1
Chocolatey Community <chocolatey@realdimensions.net>
2023-05-10 06:14:12 +0000

    AU: 3 updated - Everything julia openshift-cli [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/9fb6444980e292bc899f1b28a4e9915b3290fe96

--- automatic/julia/tools/chocolateyinstall.ps1
+++ automatic/julia/tools/chocolateyinstall.ps1
@@ -5,15 +5,15 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  file          = "$toolsDir\julia-1.8.5-win32.exe"
-  file64        = "$toolsDir\julia-1.8.5-win64.exe"
+  file          = "$toolsDir\julia-1.9.0-win32.exe"
+  file64        = "$toolsDir\julia-1.9.0-win64.exe"
 
   softwareName  = 'Julia*'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
-$packageVersion = "1.8.5"
+$packageVersion = "1.9.0"
 
 Install-ChocolateyInstallPackage @packageArgs
 
