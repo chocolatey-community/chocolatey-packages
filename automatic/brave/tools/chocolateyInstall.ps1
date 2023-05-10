11a80bd69a602c0b6042fd956e2946866b19426a
Chocolatey Community <chocolatey@realdimensions.net>
2023-05-10 00:14:18 +0000

    AU: 5 updated - brave chromium php vim Waterfox [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/8009a66174d0df867ed8dff20c0831c05db98dc0

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.51.110/BraveBrowserStandaloneSilentSetup32.exe'
-  checksum    = '57E5D2C0F8CBFF3D0A92F02A66A03E47775AEE5638D7CB79F2CF85F95C785160'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.51.114/BraveBrowserStandaloneSilentSetup32.exe'
+  checksum    = 'E5360B2BA3021B6EE3A361144DE04C51080A51C3AC8CBFB4B0C3459580E936AF'
   checksumType= 'sha256'
   file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.51.110'
+[version]$softwareVersion = '1.51.114'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
