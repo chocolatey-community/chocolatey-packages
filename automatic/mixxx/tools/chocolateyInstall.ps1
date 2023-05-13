750839b7880dae72f39d1bb1c68b6ee2bb1605f6
Chocolatey Community <chocolatey@realdimensions.net>
2023-05-13 00:14:06 +0000

    AU: 4 updated - chromium mixxx smartftp vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/5bfa7ecef2288cd4ade53fd880fbf286f0046624

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-alpha-32-g92f1414b0a-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-alpha-33-g3e500b1abc-win64.msi"
 
   softwareName   = 'Mixxx *'
 
