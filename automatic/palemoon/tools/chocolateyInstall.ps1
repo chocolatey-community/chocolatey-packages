57e295e84b7c8edef10d8cbb8007a69c2a383318
Chocolatey Community <chocolatey@realdimensions.net>
2023-05-16 12:14:42 +0000

    AU: 7 updated - chromium owncloud-client palemoon smartftp spotify vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/7f90be904cfa52d1fdeca5fe99e0e8085bcb57ec

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-32.1.1.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-32.1.1.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-32.2.0.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-32.2.0.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '127b11e55c236112eed552c79ea61da3c6307c6c105e023b75aebeac90d5e478'
+  checksum      = '27ba42ce0b77c1d2971ccb228de3cfb119f897a9fa623b9bb297e5a5d70af046'
   checksumType  = 'sha256'
-  checksum64    = 'e4dd3b9821bfe1002f28a4977affec54f7f02809ecb637aeac1ca36a82daaed8'
+  checksum64    = 'fc997eec917d784666f4ef306923fdf74c5662d444c62620fabf971f6241ded5'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/9084e081d4e89ed8ab67fce340d573c4e1378939/VSCodeSetup-ia32-1.79.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/9084e081d4e89ed8ab67fce340d573c4e1378939/VSCodeSetup-x64-1.79.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/9233de805c8fb05fad4d9d1c15f22fbca85430ad/VSCodeSetup-ia32-1.79.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/9233de805c8fb05fad4d9d1c15f22fbca85430ad/VSCodeSetup-x64-1.79.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = '413b361ad3eb9d7cd6abbaaf4d4316a813a1d9acf14cebd3b85b1473f40a25d4acdac8c99bd4e67c1f1ebc3fb98e8f0700f9c11ee75bbc67c5b5a7e771355194'
+  checksum       = '6205bf205aba403d45959ee7579886c2758a273b1daa1a9279f35e3f51b7bb11c4dbf0568dc53b6ff08da041c768f12e74fc49d4d7767e4046cf8d47be1a7ab4'
   checksumType   = 'sha512'
-  checksum64     = '6bf0c864d6b38f12d6e1bcce4c7f3328251e8ee5c3164af89e27d035cd28882bfb4c35a7fd24be11fdd90ac8f837d3451d65789cf24cf42a536f3c4cde1d3f1f'
+  checksum64     = 'd5d0e16fc8d64a233b065b7a423d30b9e5b778048edc36b31654fb7d9155acbc66970614e99d648a90e00a81bb98c1b4032121c5e4d29d01e2b7e99a56c69736'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
