da3efbc83ddf7fb6f1ba1a0671cfe0a9c0dbe4ca
Chocolatey Community <chocolatey@realdimensions.net>
2023-05-18 18:14:09 +0000

    AU: 3 updated - chromium freecad Opera [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/969c4b45c43732b5792fb9b935e26c53770de271

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "115.0.5779.0-snapshots"
+$version = "113.0.5672.127"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -10,17 +10,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/99.0.4788.9/win/Opera_99.0.4788.9_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/99.0.4788.9/win/Opera_99.0.4788.9_Setup_x64.exe'
-  checksum       = 'a1980aa1077ab916cbddd93e2db4156b9dacf8b9f33d514247ae10ac01698528'
-  checksum64     = '6e4b336edb2c074b76d4307bf9c15dea33fa14db6c3fd052412a8fc6d7f499e1'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/99.0.4788.13/win/Opera_99.0.4788.13_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/99.0.4788.13/win/Opera_99.0.4788.13_Setup_x64.exe'
+  checksum       = 'f857c761491f18770046f430bf1a91559c73b1acec2590bba05ddffcad478d75'
+  checksum64     = '97d56029d5494354aa058c121d23beadd4358b5f9486f8a7caf2187d18cc291c'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '99.0.4788.9'
+$version = '99.0.4788.13'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
