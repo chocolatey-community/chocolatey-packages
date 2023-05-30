1817b12a11cd58d14130912d49c8001cb3bcfd43
Chocolatey Community <chocolatey@realdimensions.net>
2023-05-30 00:14:09 +0000

    AU: 4 updated - chromium displayfusion mixxx vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/1a8cb3a712643294c901e703f4a54a28ea98b722

--- automatic/displayfusion/update.ps1
+++ automatic/displayfusion/update.ps1
@@ -24,12 +24,12 @@ function global:au_GetLatest {
   )
 
   $streams = @{}
-  $urls | % {
+  $urls | ForEach-Object {
     $releaseUrl = $_
     try {
       $url = Get-RedirectedUrl $releaseUrl 3>$null
       $verRe = 'Setup-|\.exe$'
-      $version = $url -split "$verRe" | select -last 1 -skip 1
+      $version = $url -split "$verRe" | Select-Object -last 1 -skip 1
       if (!$version) { return }
       $version = Get-Version $version
 
--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-alpha-40-g24eee4547c-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-alpha-42-g11146c6e5e-win64.msi"
 
   softwareName   = 'Mixxx *'
 
