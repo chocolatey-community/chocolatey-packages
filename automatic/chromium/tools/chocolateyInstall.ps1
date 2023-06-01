af885734ebffa2eba77fb0133c2206c8f508d722
Chocolatey Community <chocolatey@realdimensions.net>
2023-06-01 12:18:59 +0000

    AU: 3 updated - chromium vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/309ca58f33d9d01d10ba9f0c1023c47385729d98

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "116.0.5805.0-snapshots"
+$version = "114.0.5735.91"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/fa8eefda18bf099d82b58453f8fe5d3010196fa0/VSCodeSetup-ia32-1.79.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/fa8eefda18bf099d82b58453f8fe5d3010196fa0/VSCodeSetup-x64-1.79.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/c3867859cf3e1a73f3ddce5ad36b3791b7883d88/VSCodeSetup-ia32-1.79.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/c3867859cf3e1a73f3ddce5ad36b3791b7883d88/VSCodeSetup-x64-1.79.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = 'b67adb937a88f8a6cd8de5fb3619d9de86defe6d51baf7ceb1b4d6962906a15d5da5cdea2a4988036afa66e7bb91a00723f60914c7daeee33bd2c33249a2a1b1'
+  checksum       = 'b48f17cd85a3cf3df70411b84ff1ab7f6ca62731a9cb6a69abf15d72ef9cb5d79e79d1138047aa50e7b592fb482b05cbbb59497ce947e85112065a23a226ee10'
   checksumType   = 'sha512'
-  checksum64     = 'cbbb9179fe74235b555fc798cae08068809e01fa63a70b1cc14e6afaa5bca005317f9fbd47d90aaf110456606d5ca8086e3200169cf4cef3377c6a8007caeac9'
+  checksum64     = 'e400e0a57d28362158972a5d5a5ed0862bcca8634193033babb664a25bbf82ad955a9fc5c78536c68e7830f18d0434d16fddb4c6f6ca4da9159ce3171e30175a'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
