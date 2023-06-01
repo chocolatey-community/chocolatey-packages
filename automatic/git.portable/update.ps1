a142219f255e6f05e90fed46e71e38a664c63530
Chocolatey Community <chocolatey@realdimensions.net>
2023-06-01 18:15:08 +0000

    AU: 4 updated - freecad git.install git.portable typescript [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/a7189e79ed60afbf52cf65f4a746ab4121d7241d

--- automatic/git.portable/update.ps1
+++ automatic/git.portable/update.ps1
@@ -1,13 +1,13 @@
-import-module au
+﻿import-module au
 
 $domain = 'https://github.com'
 $releases = "$domain/git-for-windows/git/releases/latest"
 
 function global:au_BeforeUpdate {
-  $releaseAssets = Get-GitHubRelease -Owner 'git-for-windows' -Name 'git' -Tag $Latest.TagName | % assets
+  $releaseAssets = Get-GitHubRelease -Owner 'git-for-windows' -Name 'git' -Tag $Latest.TagName | ForEach-Object assets
 
-  $Latest.URL32 = $releaseAssets | ? name -match "PortableGit-.+-32-bit.7z.exe" | % browser_download_url
-  $Latest.URL64 = $releaseAssets | ? name -match "PortableGit-.+-64-bit.7z.exe" | % browser_download_url
+  $Latest.URL32 = $releaseAssets | Where-Object name -match "PortableGit-.+-32-bit.7z.exe" | ForEach-Object browser_download_url
+  $Latest.URL64 = $releaseAssets | Where-Object name -match "PortableGit-.+-64-bit.7z.exe" | ForEach-Object browser_download_url
 
   if (!$Latest.URL32 -or !$Latest.URL64) {
     throw "64bit or 32bit URL is missing"
@@ -31,10 +31,10 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $tagUrl = $download_page.Links | Where-Object href -match 'releases/tag/.*windows' | select -First 1 -ExpandProperty href
-  $tagName = $tagUrl -split '\/' | Select -Last 1
+  $tagUrl = $download_page.Links | Where-Object href -match 'releases/tag/.*windows' | Select-Object -First 1 -ExpandProperty href
+  $tagName = $tagUrl -split '\/' | Select-Object -Last 1
 
-  $version = $tagName -split '^v|\.windows' | select -Last 1 -Skip 1
+  $version = $tagName -split '^v|\.windows' | Select-Object -Last 1 -Skip 1
 
   @{
       Version = $version
