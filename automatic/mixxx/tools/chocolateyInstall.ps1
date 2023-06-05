3022a839cbf883f5878cd665f9efa59a68d1738a
Chocolatey Community <chocolatey@realdimensions.net>
2023-06-05 18:14:53 +0000

    AU: 4 updated - mixxx smartftp vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/39e2bff8ba6110f7c31543d70dadc01f47bb12d0

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4-beta-7-g27292f7cec-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-alpha-44-g585519d0ef-win64.msi"
 
   softwareName   = 'Mixxx *'
 
--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,14 +22,14 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/insider/05ae089c791aa5c8ec05abd5687c9a31517579bc/VSCodeSetup-ia32-1.79.0-insider.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/insider/05ae089c791aa5c8ec05abd5687c9a31517579bc/VSCodeSetup-x64-1.79.0-insider.exe'
+  url            = 'https://az764295.vo.msecnd.net/insider/666396ddec7708df2b986dcc178863faaa67c14d/VSCodeSetup-ia32-1.79.0-insider.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/insider/666396ddec7708df2b986dcc178863faaa67c14d/VSCodeSetup-x64-1.79.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum       = 'fb6fa01561a62de60c7d51281742e246362a4c3c631d06e0adb258ece88f8452c41ec4055531de551590c9ee59b3a5c08f49c16401f818ee5051091727351d0a'
+  checksum       = '0817de3b8f5a27b110d4a3674b7ef9aedcf9a5b307722259759e098df87cba1b261cf46f71a69923ae49324fbc077e9a5e737b4c967c278a5ff39f714f4d4535'
   checksumType   = 'sha512'
-  checksum64     = 'b56fde3846d59b0bf730e1dfbabf9df1cd45190d7c36d48ac8b714ed05cc5cef419dedb9487162fee808011cfa3f3654ea174dee061172690775a673aa8bf685'
+  checksum64     = '83cc063d63d0ab649a43140730c1e7824102e5363e7a5e888cb7634c64d5fdd0c5a3a6e296681dd317b6e9eb322a66cb909055766de3e8f55ebadb339f904ece'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
