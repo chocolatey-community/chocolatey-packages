0c97e98947bb96aad961ebe1d72b38da153a9015
Chocolatey Community <chocolatey@realdimensions.net>
2023-06-06 00:14:46 +0000

    AU: 4 updated - chromium GoogleChrome mixxx smartftp [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/48ea463e3c68cec8dc972a7174d92643cb93e64d

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '114.0.5735.91'
+$version = '114.0.5735.110'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = 'c3a4497416bbdec2cf35de51a3137636e5a92a15884eb22c2f50141f528e93fb'
-  checksum64             = '311d3ea8c9d1dd3aa0a37353eb45af8c7b1370db8295222bb99d7caa452eee4c'
+  checksum               = '404fe51ff402875940d9f3839eb98a9ff2f7f47f4610acef79c5374812c35806'
+  checksum64             = 'dfa65c383d340c6884f2b5fc5726c1669de7b46a5ac821d31e8f1ab25eae3802'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-alpha-44-g585519d0ef-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-alpha-47-g018cb521b2-win64.msi"
 
   softwareName   = 'Mixxx *'
 
