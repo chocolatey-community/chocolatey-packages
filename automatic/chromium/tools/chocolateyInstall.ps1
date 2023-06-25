861358ee7de9c851010631e67fcf51b34424f174
Chocolatey Community <chocolatey@realdimensions.net>
2023-06-25 00:14:19 +0000

    AU: 3 updated - chromium paint.net vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/ddd7bccf3c3da2b1c61face2035f22bd9bf5917c

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $scriptDir=$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 . (Join-Path $scriptDir 'helper.ps1')
 
-$version = "117.0.5851.0-snapshots"
+$version = "117.0.5852.0-snapshots"
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.0.1655_x86.zip"
-  file64        = "$toolsDir\gvim_9.0.1655_x64.zip"
+  file          = "$toolsDir\gvim_9.0.1664_x86.zip"
+  file64        = "$toolsDir\gvim_9.0.1664_x64.zip"
 }
 
 $installArgs = @{
