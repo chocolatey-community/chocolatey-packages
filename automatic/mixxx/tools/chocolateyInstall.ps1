d772cb4497f22c60d7b858998587c55e1d31f192
Chocolatey Community <chocolatey@realdimensions.net>
2023-07-04 00:14:24 +0000

    AU: 2 updated - mixxx Waterfox [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/72981fb5cd0f11e722d982fdcd9da960532f6efc

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-alpha-58-g63c55acc73-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-alpha-59-g4e5df61c4f-win64.msi"
 
   softwareName   = 'Mixxx *'
 
