cd1ff3b5dabe9a453aeba9a3d79668faba6ff8d9
Chocolatey Community <chocolatey@realdimensions.net>
2023-07-07 12:14:19 +0000

    AU: 3 updated - 360ts graphviz spotify [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/a90b2e80494654fc46ecfa384ba5a631eae224d2

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_11.0.0.1020.exe'
-  checksum               = 'dddc006f7e833598d26dcb381df191425aec0209f1b5878532502706b1762c50'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_11.0.0.1022.exe'
+  checksum               = '82522183e0c27f2d3047e1645bab04a4a6cbc17ced50cbf7c29f2cb51b827a46'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/graphviz/update.ps1
+++ automatic/graphviz/update.ps1
@@ -1,4 +1,4 @@
-import-module au
+﻿import-module au
 
 $releases = "https://graphviz.org/download"
 
@@ -21,12 +21,12 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
   $re  = "windows_10_cmake_Release.+-win64\.exe"
-  $link = $download_page.links | ? outerHtml -match $re | select -first 1
+  $link = $download_page.links | Where-Object outerHtml -match $re | Select-Object -first 1
   $link.outerHtml -match '>(.+)</a>' | Out-Null
   $fileName = $Matches[1]
 
   @{
-    Version = $filename -split '-| ' | select -First 1 -Skip 1
+    Version = $filename -split '-| ' | Select-Object -First 1 -Skip 1
     URL64 = $link.href
     FileName = $fileName
     FileType = 'exe'
