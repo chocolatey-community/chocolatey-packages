a6136e13b71c32857ef125cd93e180f120be146a
Chocolatey Community <chocolatey@realdimensions.net>
2023-07-14 00:15:00 +0000

    AU: 3 updated - selenium-chromium-edge-driver vscode vscode.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/ae2ff4ef9131b836a0b715811471ac8c4d7a9632

--- automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
+++ automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = 'selenium-chromium-edge-driver'
-  url            = 'https://msedgedriver.azureedge.net/114.0.1823.79/edgedriver_win32.zip'
-  url64          = 'https://msedgedriver.azureedge.net/114.0.1823.79/edgedriver_win64.zip'
-  checksum       = '008a30e1ac4ca19979700aa0c06af251fc4d9ed0a0607835b6fc23f5cb9bec3c'
-  checksum64     = 'f300ff25a814cd736cf163e73466ee27f7c7dd5110f5660faad03ca371f127de'
+  url            = 'https://msedgedriver.azureedge.net/114.0.1823.82/edgedriver_win32.zip'
+  url64          = 'https://msedgedriver.azureedge.net/114.0.1823.82/edgedriver_win64.zip'
+  checksum       = '783f40b5dc1efdddf704e101d1a4b4695762e2e3164ae7daf64a617caea2fd31'
+  checksum64     = '620f3c0d3fd8c652e1b278a0d521284805607e872f8d7dcca66042aed603c3d2'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = $seleniumDir
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.80.0'
+$version = '1.80.1'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://az764295.vo.msecnd.net/stable/660393deaaa6d1996740ff4880f1bad43768c814/VSCodeSetup-ia32-1.80.0.exe'
-  url64bit       = 'https://az764295.vo.msecnd.net/stable/660393deaaa6d1996740ff4880f1bad43768c814/VSCodeSetup-x64-1.80.0.exe'
+  url            = 'https://az764295.vo.msecnd.net/stable/74f6148eb9ea00507ec113ec51c489d6ffb4b771/VSCodeSetup-ia32-1.80.1.exe'
+  url64bit       = 'https://az764295.vo.msecnd.net/stable/74f6148eb9ea00507ec113ec51c489d6ffb4b771/VSCodeSetup-x64-1.80.1.exe'
 
   softwareName   = "$softwareName"
 
-  checksum       = '8c7546f28bc47b90ba877009925f820a17db713344f50caa7ebc38510be16ef2'
+  checksum       = 'bc4e3ffa69e4137ddd6e945f7849a9a5e977f20402f4cc23ea6a324b3cf7b27f'
   checksumType   = 'sha256'
-  checksum64     = '90bf319e15ab3818616292d864b2b5e41edc75020b9b1c4d6a0a5271ef8aec38'
+  checksum64     = '1b541228f6e2158d32c3afcfb9e42e596abdf3e5eea4647e0b89ef065311bcc5'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
