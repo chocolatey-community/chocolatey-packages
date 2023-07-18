da74eb9f2e2e22dfb9a86e261f014d085f7ccd5d
Chocolatey Community <chocolatey@realdimensions.net>
2023-07-18 12:18:21 +0000

    AU: 2 updated - brave palemoon [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/257cd7c5710bae4779b0a56e483cfa2262581bdb

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.57.13/BraveBrowserStandaloneSilentBetaSetup32.exe'
-  checksum    = '10CAED524B99C3A04B609B26F503D2E1C2BE2E2525BFC889A2690C2960A1CE89'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.57.16/BraveBrowserStandaloneSilentBetaSetup32.exe'
+  checksum    = '701F71D40DDB69493F6638471B7CF275C59C718C3A9445C74417852C5C0EEE90'
   checksumType= 'sha256'
   file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.57.13'
+[version]$softwareVersion = '1.57.16'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-32.3.0.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-32.3.0.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-32.3.1.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-32.3.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '07ede1b8f21be171667149c90afd78029bb0a1d0d2172df8bb03ee8c2d0e4d19'
+  checksum      = 'c18ae5f0248f54abe5264da4ede4f930dcb81bd251949b8cbb7f3f61bac4fc9f'
   checksumType  = 'sha256'
-  checksum64    = '8672953c76f72a41ea6cc945973067c05b68c571651eab31ac394b201cf1868a'
+  checksum64    = 'd7bbf6c97a228296caa659d63ffc56fd582922eb46ef846d3eec656325485d4a'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
