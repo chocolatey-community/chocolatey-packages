76a512ee22da02830d35db2e9469d605890d421c
Chocolatey Community <chocolatey@realdimensions.net>
2023-07-21 06:16:00 +0000

    AU: 2 updated - freecad GoogleChrome [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/499ca327daae3712fd1f86c7b1e0b3f02ebab95c

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '115.0.5790.99'
+$version = '115.0.5790.102'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '8998fa6a594b629657c34e65ccfcff8bce5d0cb009c8717f84176e34b250ea87'
-  checksum64             = '3769c7f1779f7c34489e44ac722ba5c0f90e8371be467179116f9c5838e642f3'
+  checksum               = '05eb367019e5233117418d984dbef933383637836d1097a067fdc79bdec56ba4'
+  checksum64             = '3e8c24f18ef619a8d5a9d745ebcca044c33772e32b67fd2c05608071a1d54dfc'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
