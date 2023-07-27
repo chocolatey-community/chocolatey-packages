682d5617da6fa414cd3c8fe6b38cf91d4e693133
Chocolatey Community <chocolatey@realdimensions.net>
2023-07-27 18:15:38 +0000

    AU: 4 updated - freecad mkdocs-material nexus-repository smartftp [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/d3b6c81fd284de55cf6b2a7e43a92cb992bc35ae

--- automatic/mkdocs-material/tools/ChocolateyInstall.ps1
+++ automatic/mkdocs-material/tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿Update-SessionEnvironment
 
-$version = '9.1.20'
+$version = '9.1.21'
 
 $proxy = Get-EffectiveProxy
 if ($proxy) {
--- automatic/nexus-repository/tools/chocolateyinstall.ps1
+++ automatic/nexus-repository/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ if (Get-OSArchitectureWidth 32) {
   throw "Sonatype Nexus Repository 3.0 and greater only supports 64-bit Windows."
 }
 
-$Version = '3.58.1-01'
+$Version = '3.58.1-02'
 $NexusVersionedFolder = "nexus-$Version"
 $TargetFolder = "$env:ProgramData\nexus"
 $ExtractFolder = "$env:TEMP\NexusExtract"
@@ -55,8 +55,8 @@ if (Test-Path "$ExtractFolder") {
 $PackageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $ExtractFolder
-  url64          = 'https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.58.1-01-win64.zip'
-  checksum64     = 'e3e843600686e5bee5f313e935a14b08adf02e784be71323d2113dec2b585878'
+  url64          = 'https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.58.1-02-win64.zip'
+  checksum64     = '336415ea85169f7cf4dc74ca5adb6f84ed5f18121f4ab6bc136bbcea1c8ce00a'
   checksumType64 = 'SHA256'
 }
 
