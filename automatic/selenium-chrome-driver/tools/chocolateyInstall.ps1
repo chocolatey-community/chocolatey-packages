4b43483cdffaf1c65558ed2663fe986d3492755e
Eetu Narhi <eetu.narhi@eficode.com>
2023-08-03 16:56:31 +0300

    Fix unzipping

--- automatic/selenium-chrome-driver/tools/chocolateyInstall.ps1
+++ automatic/selenium-chrome-driver/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   checksum        = '53c2e1f1b9f2a7571483284ea156958674501e8571cfe34be01282b4152e0770138d9aaef12876f109db9d77c1216362d0576aa40b4f19f039d4b97a15054c98'
   checksumType    = 'sha512'
   destination     = $seleniumDir
-  specificFolder  = 'chromedriver-win32'
+  specificFolder  = 'chromedriver-win32/chromedriver.exe'
 }
 
 Get-ChocolateyUnzip @packageArgs
