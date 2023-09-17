9b837fc10d5e151813115e55e895177c09f5361c
Chocolatey Community <chocolatey@realdimensions.net>
2023-09-17 00:14:21 +0000

    AU: 2 updated - brave vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/5d854679650096f3856df6fb9815b059487cf14c

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.58.124/BraveBrowserStandaloneSilentSetup32.exe'
-  checksum    = '8D1B3B7E772231B7E47B936A1803B2202DCA8C7929E4B5C8FA859C49F2A17529'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.59.90/BraveBrowserStandaloneSilentBetaSetup32.exe'
+  checksum    = '8BE666394ED707A6CC1409DDEF2073081F69EE87EFD636EA7F1BEF07CCD03A55'
   checksumType= 'sha256'
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.58.124'
+[version]$softwareVersion = '1.59.90'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
