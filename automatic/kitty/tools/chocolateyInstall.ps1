9a5d340f1f868b6fb74d841e42c293f3755536cd
Chocolatey Community <chocolatey@realdimensions.net>
2023-09-17 18:14:16 +0000

    AU: 2 updated - kitty smartftp [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/e52bab218cd43d56991f296ea3ccb47d3ae74765

--- automatic/kitty/tools/chocolateyInstall.ps1
+++ automatic/kitty/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ StopProcesses
 
 $packageArgs = @{
     PackageName    = 'kitty'
-    FileFullPath   = "$toolsPath\kitty-bin-0.76.1.12.zip"
+    FileFullPath   = "$toolsPath\kitty-bin-0.76.1.13.zip"
     Destination    = $toolsPath
   }
 
