37ad5c4cdd54fab7fc6177890a6f50e097cb95c6
Chocolatey Community <chocolatey@realdimensions.net>
2023-09-23 18:23:10 +0000

    AU: 2 updated - selenium-chromium-edge-driver wesnoth [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/fd6048916b07ec2a0aabcf36a8cf7335ec3f106c

--- automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
+++ automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = 'selenium-chromium-edge-driver'
-  url            = 'https://msedgedriver.azureedge.net/117.0.2045.40/edgedriver_win32.zip'
-  url64          = 'https://msedgedriver.azureedge.net/117.0.2045.40/edgedriver_win64.zip'
-  checksum       = 'c59a95a1e347300ebb083a7015ad22b406923bc613725eebdb05ddb5b293cfcf'
-  checksum64     = '694dbb8f38c8b4dec4ff31e2685bd631c288c65740367ed2154aaa5c6d20e619'
+  url            = 'https://msedgedriver.azureedge.net/117.0.2045.41/edgedriver_win32.zip'
+  url64          = 'https://msedgedriver.azureedge.net/117.0.2045.41/edgedriver_win64.zip'
+  checksum       = '69f9ea6f790c922e1a9f642496f41e42914c909f5edc35180bb64ed17865433e'
+  checksum64     = '32ac12aea3e785dafc04fb77968b397560ea5e00c6d660fa465793e4244733f0'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = $seleniumDir
--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.19/wesnoth-1.17.19-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.21/wesnoth-1.17.21-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = 'ee2063e1924c02959c7af2df26fe38d60cbde3de34ed6fedc1f1e0c493b1c526'
+  checksum       = '71576bf14d9e38f4ad0d4018d44ae669257374a867c58fd1df6943bb797bbde2'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
