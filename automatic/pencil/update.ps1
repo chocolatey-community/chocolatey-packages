9ea09c322959a849964fbe8cb7fece01781b04ec
Josh King <josh@chocolatey.io>
2023-10-01 18:00:29 +1300

    (#2187) Fix pencil pkg auto update

--- automatic/pencil/update.ps1
+++ automatic/pencil/update.ps1
@@ -12,10 +12,8 @@ function global:au_SearchReplace {
     ".\legal\VERIFICATION.txt" = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$($Latest.ReleaseURL)>"
       "(?i)(^\s*software.*)\<.*\>"        = "`${1}<$($Latest.URL32)>"
-      "(?i)(^\s*checksum32\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum64\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
-      "(?i)(^\s*checksum32\:).*"            = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*"            = "`${1} $($Latest.Checksum64)"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(^\s*checksum\:).*"            = "`${1} $($Latest.Checksum32)"
     }
 
     "$($Latest.PackageName).nuspec" = @{
@@ -31,13 +29,12 @@ function global:au_GetLatest {
   $re = '\.exe$'
   $url = $download_page.links | ? href -match $re | % { $domain + $_.href }
 
-  $version = $url[0] -split '/' | select -last 1 -Skip 1
+  $version = $url -split '/' | select -last 1 -Skip 1
   $version = $version.Substring(1) -replace '\.\w+$'
 
   @{
     Version     = $version
-    URL32       = $url -match 'i386' | select -first 1
-    URL64       = $url -notmatch 'i386' | select -first 1
+    Url32       = $url
     ReleaseURL  = $download_page.links.href | ? { $_ -like "*github*/release/*"} | select -first 1
   }
 }
