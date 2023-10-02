bac7f7f2fe5189b25d56f1f8004f16a48ea2e342
Chocolatey Community <chocolatey@realdimensions.net>
2023-10-02 18:15:14 +0000

    AU: 6 updated - python python3 python312 skype vscode vscode.install [skip ci]

--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.82.2'
+$version = '1.82.3'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,14 +16,14 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url            = 'https://update.code.visualstudio.com/1.82.2/win32/stable'
-  url64bit       = 'https://update.code.visualstudio.com/1.82.2/win32-x64/stable'
+  url            = 'https://update.code.visualstudio.com/1.82.3/win32/stable'
+  url64bit       = 'https://update.code.visualstudio.com/1.82.3/win32-x64/stable'
 
   softwareName   = "$softwareName"
 
-  checksum       = '76cf4fbe3d371402d94e79840d16e65c7e0ada7e5c12ac002a7fc8b285d2770c'
+  checksum       = '31b55f6e3cd23850a4931a0270e9efb89b9344b40e3320ef860d7d3e3f54c957'
   checksumType   = 'sha256'
-  checksum64     = '9dcd084c2e666af8b6bfbc0f3f45610250d6cae14d7a1570e611fb8c88867df7'
+  checksum64     = 'b704f7647e0efb2ce8d264e3c75846b11d07491ae6b1d9d140b8d02803b3d9fc'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
