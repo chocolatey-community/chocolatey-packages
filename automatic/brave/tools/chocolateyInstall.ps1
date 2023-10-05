3f9a55fd0d8ee223537f33dceda80dcacd31ad18
Chocolatey Community <chocolatey@realdimensions.net>
2023-10-05 12:14:54 +0000

    AU: 2 updated - brave smartftp [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/4236b8b3378c0dae2eaa4957ed53f3017e32399c

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.58.137/BraveBrowserStandaloneSilentSetup32.exe'
-  checksum    = '3BBEA36919A97534D50B2D2265188A27BEE2DCBC155069B5C097EDF34590A19B'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.60.81/BraveBrowserStandaloneSilentBetaSetup32.exe'
+  checksum    = '9C64A1B58BC090267263E0F129D503F25E287AECB2860C5C9C872629EEC2670F'
   checksumType= 'sha256'
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.58.137'
+[version]$softwareVersion = '1.60.81'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
