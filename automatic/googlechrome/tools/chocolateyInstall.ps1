3a1af2240012b8c5ef8ba632981b61e309dc0f6c
Chocolatey Community <chocolatey@realdimensions.net>
2023-10-11 00:14:53 +0000

    AU: 5 updated - freevideoeditor GoogleChrome nodejs nodejs.install thunderbird [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/45cc13559c82887223240305ef2d6c832b530bf8

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '117.0.5938.150'
+$version = '118.0.5993.71'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = '2e073b85ce0e84f534fc14475abb49f86140008295f2e9472e90ed3f7e4395ff'
-  checksum64             = '66040e12f5bbf5f0cdf8aef36d433d175bf103c862462c40e26a4a39c76a8ae3'
+  checksum               = 'b1c48fc706c7f00ccdfe6082779637115e6d24e5d04fee2eefcf62a6a1c11359'
+  checksum64             = '35343ea8c206905e25f7134eb3534c99fe5eeadb7f2156d140adf61aad15ce1c'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
