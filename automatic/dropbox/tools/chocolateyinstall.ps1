ed7ca793b1ef1ef07bf0ebb78f78430dcc983103
Chocolatey Community <chocolatey@realdimensions.net>
2023-10-23 18:15:03 +0000

    AU: 3 updated - dropbox filezilla Opera [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/7bd75a48d6cc6eea5a3fa7b40288e1add55609fc

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '186.3.6103'
+$version = '186.3.6141'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,10 +13,10 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20186.3.6103%20Offline%20Installer.x86.exe'
-    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20186.3.6103%20Offline%20Installer.x64.exe'
-    checksum       = '7b6a03ae607c5eea5d6c3eda4c8414d3c427d6d1f147b7e77c78a713324d9b85'
-    checksum64     = 'cc51c6cee52eafb701175c6ef8e7f405994c69fc2dfd0749735da991f322b1a4'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20186.3.6141%20Offline%20Installer.x86.exe'
+    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20186.3.6141%20Offline%20Installer.x64.exe'
+    checksum       = '26ad50407d349ce2b2b681fe118daaac287a26c06d5559487ac13f558011703d'
+    checksum64     = '7ff884bc34bf968f49058d33f3766bfe4a5cc9fc5507ba2d6f7e9815ffe2b554'
     fileType       = 'exe'
     checksumType   = 'sha256'
     checksumType64 = 'sha256'
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -11,17 +11,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/103.0.4928.34/win/Opera_103.0.4928.34_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/103.0.4928.34/win/Opera_103.0.4928.34_Setup_x64.exe'
-  checksum       = 'aaf98551ba7c42d3f9beedc970da32351a9275245f25995bd3e7ee5990db48a5'
-  checksum64     = '3a6ac26b0d085c23916b2440319b2afa64a4f9cf56e9a325482f50df7e7b2e4d'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/104.0.4944.23/win/Opera_104.0.4944.23_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/104.0.4944.23/win/Opera_104.0.4944.23_Setup_x64.exe'
+  checksum       = 'cd5258825395cab0d877f420e57aadbd59579709e8902a7de893841567583409'
+  checksum64     = '6bbab68ec83059fe165ef7ced0b664ec50f0615baac71bea263a57f71928e20b'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '103.0.4928.34'
+$version = '104.0.4944.23'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
