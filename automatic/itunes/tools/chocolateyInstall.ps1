70f7c169cb23b29f2f7b87e1928053509ef21190
Chocolatey Community <chocolatey@realdimensions.net>
2023-10-24 18:14:45 +0000

    AU: 4 updated - Firefox iTunes nodejs nodejs.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/162791f457c9c9a9d790109628edc8d8996d3fd1

--- automatic/itunes/tools/chocolateyInstall.ps1
+++ automatic/itunes/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$version = '12.12.10.1'
+$version = '12.13.0.9'
 
 $packageArgs = @{
   packageName    = 'iTunes'
   fileType       = 'msi'
-  url            = 'https://secure-appldnld.apple.com/itunes12/042-32486-20230911-150844F3-FE7A-4355-AA15-03182082D824/iTunesSetup.exe'
-  url64bit       = 'https://secure-appldnld.apple.com/itunes12/042-32480-20230911-CC326CC1-E6C1-4F7C-A717-18493F59FE2E/iTunes64Setup.exe'
+  url            = 'https://secure-appldnld.apple.com/itunes12/042-62514-20231023-50B51FD0-68B9-4F27-989D-B226D7A42BEC/iTunesSetup.exe'
+  url64bit       = 'https://secure-appldnld.apple.com/itunes12/042-62516-20231023-4B775F51-D1D0-4728-A168-77A5EFB3D51D/iTunes64Setup.exe'
   softwareName   = 'iTunes'
-  checksum       = '2B30F6045BA27488A97E1B45AA224A794527E9087877ADD93780A659F5024F65'
+  checksum       = 'B29C86AAA25A5DD6473293F7CF12310F9F8F405E80CC466F2591FE5CD91F504D'
   checksumType   = 'sha256'
-  checksum64     = '31465167704b2fd795aafc14cf5c04261d7c3ae663d087f63842e431aa204abc'
+  checksum64     = '9659783e209012386d6654ce448d58cf9a77efd9cf1d71aa767c9da08ca9adcf'
   checksumType64 = 'sha256'
   silentArgs     = "/qn /norestart"
   validExitCodes = @(0, 2010, 1641)
