f9856aa5bc68ea5c4e4c425b4bf93ca0abd2edc0
Cory Knox <me@coryknox.dev>
2023-10-30 15:57:35 -0700

    (azcopy10) Initial commit from source repository

---
+++ automatic/azcopy10/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+ 
+$packageName = 'azcopy10'
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    packageName    = $packageName
+    url64          = 'https://azcopyvnext.azureedge.net/releases/release-10.21.1-20231025/azcopy_windows_amd64_10.21.1.zip'
+    checksum64     = '12927c211b3da7ea0df28277d4a69ff7a6046cd8ad3163b40fc2b31a96d66e81'
+    checksumType64 = 'sha256'
+    url            = 'https://azcopyvnext.azureedge.net/releases/release-10.21.1-20231025/azcopy_windows_386_10.21.1.zip'
+    checksum       = '461990141b1ff495a2cf25f2809169c541bd53364ce32fc43b912f4c664b9a23'
+    checksumType   = 'sha256'
+    destination    = $toolsDir
+}
+
+#Manage azcopy installation
+Install-ChocolateyZipPackage @packageArgs
+
+$targetPath = Join-Path -Path $toolsDir -ChildPath 'azcopy'
+If (Test-Path -Path $targetPath -PathType:Container) {
+    Remove-Item -Path $targetPath -Force -Recurse
+}
+
+Get-ChildItem -Path $toolsDir -Directory -Filter "azcopy*" | Rename-Item  -NewName 'azcopy' -Force
