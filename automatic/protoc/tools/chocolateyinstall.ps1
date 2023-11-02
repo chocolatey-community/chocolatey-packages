df19a4257786118b911b41755ac2d95247db9148
Chocolatey Community <chocolatey@realdimensions.net>
2023-11-02 00:14:26 +0000

    AU: 4 updated - protoc vlc vlc.install vlc.portable [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/d27ef0609ace59fd1e328608ab40c3f50fe3573a

--- automatic/protoc/tools/chocolateyinstall.ps1
+++ automatic/protoc/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   PackageName    = $Env:ChocolateyPackageName
-  FileFullPath   = "$toolsPath\protoc-24.4-win32.zip"
-  FileFullPath64 = "$toolsPath\protoc-24.4-win64.zip"
+  FileFullPath   = "$toolsPath\protoc-25.0-win32.zip"
+  FileFullPath64 = "$toolsPath\protoc-25.0-win64.zip"
   Destination    = $toolsPath
 }
 Get-ChocolateyUnzip @packageArgs
