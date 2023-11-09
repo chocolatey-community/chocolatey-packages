4d9581a222fd50a57cb515ad78490eadfd64258e
Daniel Rose <daniel.rose@Evolutionizer.com>
2023-11-09 16:00:30 +0100

    (vscode.install) 32-bit version not available anymore after 1.83
    From https://code.visualstudio.com/docs/supporting/faq#_previous-release-versions:
    
    > Windows x86 32-bit versions are no longer actively supported after release 1.83 and could pose a security risk.

--- automatic/vscode.install/update.ps1
+++ automatic/vscode.install/update.ps1
@@ -4,7 +4,6 @@ Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
 if ($MyInvocation.InvocationName -ne '.') {
   function global:au_BeforeUpdate {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
     $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
   }
 }
@@ -13,9 +12,7 @@ function global:au_SearchReplace {
   @{
     'tools\chocolateyInstall.ps1' = @{
       "(?i)(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
-      "(?i)(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
       "(?i)(^\s*url64bit\s*=\s*)('.*')"    = "`$1'$($Latest.URL64)'"
-      "(?i)(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
       "(?i)(^\s*checksum64\s*=\s*)('.*')"  = "`$1'$($Latest.Checksum64)'"
       "(?i)(^[$]version\s*=\s*)('.*')"     = "`$1'$($Latest.RemoteVersion)'"
     }
@@ -26,13 +23,11 @@ function global:au_GetLatest {
   $latestRelease = Get-GitHubRelease microsoft vscode
   $version = $latestRelease.tag_name
   # URLs are documented here: https://code.visualstudio.com/docs/supporting/faq#_previous-release-versions
-  $url32 = "https://update.code.visualstudio.com/$version/win32/stable"
   $url64 = "https://update.code.visualstudio.com/$version/win32-x64/stable"
 
   @{
     Version       = $version
     RemoteVersion = $version
-    URL32         = $url32
     URL64         = $url64
   }
 }
