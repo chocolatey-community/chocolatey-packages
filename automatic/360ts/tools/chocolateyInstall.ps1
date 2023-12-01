c70be6d3b753e959ee82b09948a9d2376984fd3d
Chocolatey Community <chocolatey@realdimensions.net>
2023-12-01 18:14:52 +0000

    AU: 3 updated - 360ts mp3tag owncloud-client [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/ca8e97799b4a6c9a986fceec33864f4662b21bdd

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_11.0.0.1058.exe'
-  checksum               = 'd9a9d37d128f199cb4529ee5248d0a6695385b419d56e62b310e2eaddb7ede49'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_11.0.0.1061.exe'
+  checksum               = '9d74776657078d72623f87aea1bba390eb6773a0a60ee56cc0194bdff2097cd9'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
