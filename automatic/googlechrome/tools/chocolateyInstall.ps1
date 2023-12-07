021569fdd899aac4baf0bbc0a116a8dde81b713f
GraphicHealer <raymondella2001@gmail.com>
2023-12-07 12:54:54 -0500

    Update chocolateyInstall.ps1
    Fix Google Chrome Download Link

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ if ($version -eq (Get-ChromeVersion)) {
 $packageArgs = @{
   packageName            = 'googlechrome'
   fileType               = 'MSI'
-  url                    = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
-  url64bit               = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
+  url                    = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise.msi'
+  url64bit               = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi'
   checksum               = '72ebc4f59405a5cb497d2fdd1e8989b07a1808ed0a43759802859ef351cf231d'
   checksum64             = 'f0a7e673d2da6da8005726c0a1e040bde7201241a5f760e99182c12b025698b2'
   checksumType           = 'sha256'
