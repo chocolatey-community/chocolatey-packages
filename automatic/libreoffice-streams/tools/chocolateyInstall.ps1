17d774e0b230ab234b4e713839efd7a6024a7401
Chocolatey Community <chocolatey@realdimensions.net>
2023-12-08 18:14:53 +0000

    AU: 2 updated - libreoffice-still skype [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/1dd4c7f0dd3bb37b0518cba6dce93af56cf5058e

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = 'libreoffice-still'
-  version                = '7.5.8'
+  version                = '7.5.9'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.5.8/win/x86/LibreOffice_7.5.8_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.5.8/win/x86_64/LibreOffice_7.5.8_Win_x86-64.msi'
-  checksum               = 'd274d141ec8e196ce8e21f805d7282830cf42ce83b0535eb1a134aa5d1d285b1'
-  checksum64             = '862ad100ef602e1f1c98f7cfbcff1844eb31b700132b59b1d320a9dbd316dd1c'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/7.5.9/win/x86/LibreOffice_7.5.9_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/7.5.9/win/x86_64/LibreOffice_7.5.9_Win_x86-64.msi'
+  checksum               = '4589fb6d7588b66baaac0ebdffe509d315e94d2281b20e7f19107f562a408d8f'
+  checksum64             = '82d744eb8c3b19bc05ce3f2f363d7a6987432cdba2f495f0c019f5d0b83dfa62'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
