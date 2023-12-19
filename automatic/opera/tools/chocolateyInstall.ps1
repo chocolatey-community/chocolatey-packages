c139e6cc83865bf2d298a529183e015ad6acbafe
Chocolatey Community <chocolatey@realdimensions.net>
2023-12-19 18:14:33 +0000

    AU: 4 updated - Firefox openshift-cli Opera thunderbird [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/da3ee3d626b051c13f0092035f91fd8cbdc8f13f

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -11,17 +11,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/105.0.4970.48/win/Opera_105.0.4970.48_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/105.0.4970.48/win/Opera_105.0.4970.48_Setup_x64.exe'
-  checksum       = 'c45d6e7f485dd2aea3f6907ad7f6c718ffccf3ab2aba5d9c3f7cc1715d98cf0d'
-  checksum64     = '602b98274bd2a2eeb221db5989bac5d05c5d282b650f1c5dd228de4635893433'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/106.0.4998.16/win/Opera_106.0.4998.16_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/106.0.4998.16/win/Opera_106.0.4998.16_Setup_x64.exe'
+  checksum       = 'de75d8f6ee01375c770b87ea03a126a839476219e2fbdb5f18aa5809d2dd97c7'
+  checksum64     = 'ac5ed75f6096ba22aa091b5560bc6c9891193902b4ae964e9c1fbce70de7a125'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '105.0.4970.48'
+$version = '106.0.4998.16'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
