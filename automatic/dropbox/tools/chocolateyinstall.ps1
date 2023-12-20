fed11c02c1f3f7704780e6d46c6ee122a13e843a
Chocolatey Community <chocolatey@realdimensions.net>
2023-12-20 18:15:26 +0000

    AU: 5 updated - dropbox filezilla kubernetes-cli php Waterfox [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/baa0afb59531e85ba40a32e4866632f8bc676275

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '189.4.8395'
+$version = '190.3.6210'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,10 +13,10 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20189.4.8395%20Offline%20Installer.x86.exe'
-    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20189.4.8395%20Offline%20Installer.x64.exe'
-    checksum       = 'f42f31124617e905668064e09fb9143084ed9f4b43571bb67fa40606dd48f389'
-    checksum64     = 'd0d816ee8264a7b29a5966f47d2ce67375fe7f1fb417e5b707323da271a9e28f'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20190.3.6210%20Offline%20Installer.x86.exe'
+    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20190.3.6210%20Offline%20Installer.x64.exe'
+    checksum       = 'a72114cb05c1c2275bfa062d9bf2751f617b78a6afd84dc75b39d0fb95f936ac'
+    checksum64     = '6e18f53f441906efe0027fd99cbcebe01d19a94f2c084a878e8b3c6d78d6fda2'
     fileType       = 'exe'
     checksumType   = 'sha256'
     checksumType64 = 'sha256'
