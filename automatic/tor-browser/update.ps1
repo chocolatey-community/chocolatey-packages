cc449fe2a1470f6eecc25f569baafe8bf13417c8
Recurzion <4392567+Recurzion@users.noreply.github.com>
2023-12-30 22:57:58 -0500

    updating PR with suggested changes

--- automatic/tor-browser/update.ps1
+++ automatic/tor-browser/update.ps1
@@ -1,7 +1,7 @@
 ﻿import-module au
 
-$releases = "https://archive.torproject.org/tor-package-archive/torbrowser/13.0.6/"
-$baseUrl  = "https://www.torproject.org"
+$releases = "https://www.torproject.org/download/languages/"
+$baseUrl  = "https://archive.torproject.org/tor-package-archive/torbrowser/"
 
 function global:au_SearchReplace {
   @{ 
@@ -23,14 +23,14 @@ function global:au_GetLatest {
   $download_page    = Invoke-WebRequest -Uri $releases -UseBasicParsing
     
   $allExes          = $download_page.Links | Where-Object href -match "\.exe$" | Select-Object -expand href
-  $url32            = $releases + ($allExes | Where-Object { $_ -match "tor-browser-windows-x86_64-portable-\d.*.exe$" } | Select-Object -First 1)
-  $url64            = $releases + ($allExes | Where-Object { $_ -match "tor-browser-windows-x86_64-portable-\d.*.exe$" } | Select-Object -First 1)
-  $version          = $releases -split '\/' | Select-Object -last 1 -skip 1
+  $url32            = (($allExes | Where-Object { $_ -match "tor-browser-windows-i686-portable-\d.*.exe$" } | Select-Object -First 1) -split("\/",5) | Select -Index 4)
+  $url64            = (($allExes | Where-Object { $_ -match "tor-browser-windows-x86_64-portable-\d.*.exe$" } | Select-Object -First 1) -split("\/",5) | Select -Index 4)
+  $version          = $url64 -split '\/' | Select-Object -last 1 -skip 1
   
   @{
     Version         = "$version"
-    URL32           = $url32
-    URL64           = $url64
+    URL32           = $baseUrl + $url32
+    URL64           = $baseUrl + $url64
   }
 }
 
