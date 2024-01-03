a41283109cc60f08e6071152368fea8faac27e84
Chocolatey Community <chocolatey@realdimensions.net>
2024-01-03 14:18:43 +0000

    AU: 3 updated - simplewall simplewall.install simplewall.portable [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/b40789cb032968442dd0ebba64464be1aa7777fd

--- automatic/simplewall.portable/tools/chocolateyInstall.ps1
+++ automatic/simplewall.portable/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\simplewall-3.7.6-bin.zip"
+  file        = "$toolsDir\simplewall-3.7.7-bin.zip"
   destination = $toolsDir
 }
 
