7322286f6a203483dea93688b8ba8dd1f3d669bd
Chocolatey Community <chocolatey@realdimensions.net>
2024-01-09 11:20:32 +0000

    AU: 5 updated - k9s ruby ruby.install ruby.portable vim [skip ci]

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.1.0_x86.zip"
-  file64        = "$toolsDir\gvim_9.1.0_x64.zip"
+  file          = "$toolsDir\gvim_9.1.0016_x86.zip"
+  file64        = "$toolsDir\gvim_9.1.0016_x64.zip"
 }
 
 $installArgs = @{
