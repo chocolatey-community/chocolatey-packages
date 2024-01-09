068e9dd0f3f45126ff4367ceffaf04cfc91735dc
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2024-01-09 10:23:02 -0600

    (scribus) Switch to half-embedded
    If both installers are embedded, the package is over 200mb.

--- automatic/scribus/update.ps1
+++ automatic/scribus/update.ps1
@@ -2,21 +2,23 @@
 
 $releases = 'https://sourceforge.net/projects/scribus/files/scribus/'
 
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameSkip 1 }
+function global:au_BeforeUpdate {
+  Get-RemoteFiles -Purge -NoSuffix -FileNameSkip 1
+  Remove-Item "$PSScriptRoot\tools\$($Latest.FileName32)"
+}
 
 function global:au_SearchReplace {
   @{
     ".\legal\VERIFICATION.txt"      = @{
       "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
       "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*"       = "`${1} $($Latest.Checksum32)"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
       "(?i)(^\s*checksum64\:).*"          = "`${1} $($Latest.Checksum64)"
     }
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
       "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
+      "(?i)(^\s*Url\s*=\s*)''"                    = "`${1}'$($Latest.URL32)'"
+      "(?i)(^\s*Checksum\s*=\s*)''"               = "`${1}'$($Latest.Checksum32)'"
     }
   }
 }
