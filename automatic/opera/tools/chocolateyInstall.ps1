0149fbf36ecf9d1f513d917f5045c6566bd52874
Chocolatey Community <chocolatey@realdimensions.net>
2024-01-18 18:15:01 +0000

    AU: 5 updated - k9s kubernetes-kompose Opera vscode vscode.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/e97c3fc178124983af16667405c9102499a7ed51

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -11,17 +11,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/106.0.4998.41/win/Opera_106.0.4998.41_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/106.0.4998.41/win/Opera_106.0.4998.41_Setup_x64.exe'
-  checksum       = '6913140dd145e90edd92f18ee9b41885d238e93813bdad891fdf1c59194a9ad7'
-  checksum64     = 'af57cb277ebfa73f883cbb609c161f979842fc0d2e75b5cc79a15569e1e351a2'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/106.0.4998.52/win/Opera_106.0.4998.52_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/106.0.4998.52/win/Opera_106.0.4998.52_Setup_x64.exe'
+  checksum       = 'ff109164933428124bc0c9df96a97c949fd4d8562bfb678aa0ba0d2d74d98c57'
+  checksum64     = 'e4ece354dff568fae1b00298b71201f63a89d11ac646d34259f50f7bcd519f92'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '106.0.4998.41'
+$version = '106.0.4998.52'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.85.1'
+$version = '1.85.2'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,11 +16,11 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url64bit       = 'https://update.code.visualstudio.com/1.85.1/win32-x64/stable'
+  url64bit       = 'https://update.code.visualstudio.com/1.85.2/win32-x64/stable'
 
   softwareName   = "$softwareName"
 
-  checksum64     = '3fa59820ba446b022af006750bcc7e32248e7bff17b1c319949a570bcbc85512'
+  checksum64     = '76fe04ff2721f5586e853b61f116e2bf1bdaa15f436a9b4eed1f9a97bb1fe149'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
