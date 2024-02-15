a050e31a159db5b49dc09c1124a40215ea0269a8
Chocolatey Community <chocolatey@realdimensions.net>
2024-02-15 18:19:30 +0000

    AU: 3 updated - mpc-hc-clsid2 vscode vscode.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/31c9d26029db86178125e975ceac7b3d77bb0ebf

--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.86.1'
+$version = '1.86.2'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,11 +16,11 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url64bit       = 'https://update.code.visualstudio.com/1.86.1/win32-x64/stable'
+  url64bit       = 'https://update.code.visualstudio.com/1.86.2/win32-x64/stable'
 
   softwareName   = "$softwareName"
 
-  checksum64     = 'a2342fb33fa5a9d91db4a913a1d2516fa4442e6dd85615d9796137f9ccc66453'
+  checksum64     = 'b8524d2e12d6c16a7ff80442acdfab63e8063791c6c35668d401b02cb90436ed'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
