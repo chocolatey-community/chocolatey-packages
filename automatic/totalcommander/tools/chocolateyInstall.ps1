8e8436ffe7eeadd5378f5c85e908b0811da90325
Chocolatey Community <chocolatey@realdimensions.net>
2024-02-21 18:15:05 +0000

    AU: 5 updated - jenkins totalcommander winscp winscp.install winscp.portable [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/9a5b560d3c952bbfaec47ffa7629d587eb7ccda8

--- automatic/totalcommander/tools/chocolateyInstall.ps1
+++ automatic/totalcommander/tools/chocolateyInstall.ps1
@@ -25,7 +25,7 @@ if ($pp.InstallPath) {
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'exe'
-    file           = "$toolsPath\tcmd1102x32_64.exe"
+    file           = "$toolsPath\tcmd1103x32_64.exe"
     silentArgs     = $installArgs
     validExitCodes = @(0)
     softwareName   = 'Total Commander*'
