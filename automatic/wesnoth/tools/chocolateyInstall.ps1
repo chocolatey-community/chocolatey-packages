2533dce0bf4c1576905ddd725b2c965eee39168b
Chocolatey Community <chocolatey@realdimensions.net>
2024-02-24 06:15:37 +0000

    AU: 2 updated - mkdocs-material wesnoth [skip ci]

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.25/wesnoth-1.17.25-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.26/wesnoth-1.17.26-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = '55db5087365e174c43335ab0e3ba4031923e6c6c56c19458804865011becff7f'
+  checksum       = 'c497b4daa7598e955ca26390924e4180a1aaea86f8973477cb7f95009bff114e'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
