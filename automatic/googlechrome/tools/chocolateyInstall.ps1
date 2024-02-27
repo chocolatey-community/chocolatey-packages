74661972293a01661c69f6ecd02014e77475d284
Chocolatey Community <chocolatey@realdimensions.net>
2024-02-27 00:18:13 +0000

    AU: 2 updated - GoogleChrome vim [skip ci]

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '122.0.6261.69'
+$version = '122.0.6261.70'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
