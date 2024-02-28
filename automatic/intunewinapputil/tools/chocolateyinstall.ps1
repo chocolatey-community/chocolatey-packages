1a4683eb851bcd1b99704f009c08e0242833848f
Chocolatey Community <chocolatey@realdimensions.net>
2024-02-28 06:34:21 +0000

    AU: 2 updated - intunewinapputil vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/ef6f247b02d6eb84959db03ea1cb27031f816817

--- automatic/intunewinapputil/tools/chocolateyinstall.ps1
+++ automatic/intunewinapputil/tools/chocolateyinstall.ps1
@@ -10,9 +10,9 @@ foreach ($folder in $foldersToRemove) {
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/archive/refs/tags/v1.8.5.zip'
+  url           = 'https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/archive/refs/tags/v1.8.6.zip'
   softwareName  = 'intunewinapputil*'
-  checksum      = '8cd8372116ea3181016bd8be9628a42b4776d60c55de5bb296d03f3983258a22'
+  checksum      = '3bfd2c93a3500bc2b99c007a5d2352af021df9a043851b77f01ea788fd878851'
   checksumType  = 'sha256'
 }
 
