32ae6c04205fe5d3f477e1f8960eb2c0bdca196c
Chocolatey Community <chocolatey@realdimensions.net>
2024-03-12 18:21:04 +0000

    AU: 5 updated - vscode vscode.install winscp winscp.install winscp.portable [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/7b15853ef110c7b13c8b05da26a900d31d0916b7

--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.87.1'
+$version = '1.87.2'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,11 +16,11 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url64bit       = 'https://update.code.visualstudio.com/1.87.1/win32-x64/stable'
+  url64bit       = 'https://update.code.visualstudio.com/1.87.2/win32-x64/stable'
 
   softwareName   = "$softwareName"
 
-  checksum64     = 'd2d3e7e75ea42a1b609321b58d7085df947bf25bfd0d1c364fb2ebcf85a477f2'
+  checksum64     = 'c49f51562a99e19412d968a81ad653960c4861e95f7cd04e49e15c42e139a9ee'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
