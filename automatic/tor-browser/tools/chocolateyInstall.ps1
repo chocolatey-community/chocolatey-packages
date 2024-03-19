9be37bc6503f6bfa97137733f43939b0c2c4bec6
Chocolatey Community <chocolatey@realdimensions.net>
2024-03-19 18:17:51 +0000

    AU: 4 updated - Firefox skype thunderbird tor-browser [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/86f2c1fd9f62f7b6e56b29efeb3fe21d16e3af5e

--- automatic/tor-browser/tools/chocolateyInstall.ps1
+++ automatic/tor-browser/tools/chocolateyInstall.ps1
@@ -8,10 +8,10 @@ $destinationFolder = GetInstallDirectory -toolsPath $toolsDir
 $packageArgs = @{
   PackageName  = 'tor-browser'
   FileType     = 'exe'
-  Url          = 'https://archive.torproject.org/tor-package-archive/torbrowser/13.0.11/tor-browser-windows-i686-portable-13.0.11.exe'
-  Url64        = 'https://archive.torproject.org/tor-package-archive/torbrowser/13.0.11/tor-browser-windows-x86_64-portable-13.0.11.exe'
-  Checksum     = 'e86f9e5124e35ae83f8afe6c4532c69a5d293a2e89b49f26e6982817049dd5ff'
-  Checksum64   = 'f43310557553a73fc667e9b5e3925b1a7da99975f606d43c5e84b0642c34689c'
+  Url          = 'https://archive.torproject.org/tor-package-archive/torbrowser/13.0.12/tor-browser-windows-i686-portable-13.0.12.exe'
+  Url64        = 'https://archive.torproject.org/tor-package-archive/torbrowser/13.0.12/tor-browser-windows-x86_64-portable-13.0.12.exe'
+  Checksum     = '63aca8004848f0abd32f436032a01faf86226cc138bce114c9d65e2f77aa2a2c'
+  Checksum64   = '6586e985380d261b08d7cf47fc37319cf06cf3c36e3d6c1350c1380552d25759'
   ChecksumType = 'sha256'
   SilentArgs   = "/S","/D=$destinationFolder"
 }
