a5f07ac5c1baccf806cc8b5b0bff6c1a859930a6
Chocolatey Community <chocolatey@realdimensions.net>
2024-03-22 06:22:24 +0000

    AU: 3 updated - cmake.portable vim wesnoth [skip ci]

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.17.26/wesnoth-1.17.26-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.18/wesnoth-1.18.0/wesnoth-1.18.0-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = 'c497b4daa7598e955ca26390924e4180a1aaea86f8973477cb7f95009bff114e'
+  checksum       = '5920aaa24828ff9048f59bed49ab1ba670209333aa114a98d1643a88cfc7c7f8'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
