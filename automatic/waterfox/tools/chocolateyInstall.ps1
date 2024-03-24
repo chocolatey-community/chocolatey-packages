013dc57f9538c567c54b142dc8f899562ba91e22
Chocolatey Community <chocolatey@realdimensions.net>
2024-03-24 18:22:26 +0000

    AU: 1 updated - Waterfox [skip ci]

--- automatic/waterfox/tools/chocolateyInstall.ps1
+++ automatic/waterfox/tools/chocolateyInstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   packageName   = 'Waterfox'
   fileType      = 'exe'
   softwareName  = 'Waterfox*'
-  file          = "$toolsDir\Waterfox Setup G6.0.10_x64.exe"
+  file          = "$toolsDir\Waterfox Setup G6.0.11_x64.exe"
   silentArgs    = "/S"
   validExitCodes= @(0)
 }
