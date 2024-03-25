5fe176fdb996a2ef1f7d3a8312a491fc0e6ee971
Chocolatey Community <chocolatey@realdimensions.net>
2024-03-25 06:24:05 +0000

    AU: 2 updated - qbittorrent vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/29e917e85bcce667542a4da769801aa3c14afe76

--- automatic/qbittorrent/tools/chocolateyinstall.ps1
+++ automatic/qbittorrent/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
   packageName    = 'qbittorrent'
   fileType       = 'exe'
   softwareName   = 'qBittorrent*'
-  file64         = "$toolsDir\qbittorrent_4.6.3_x64_setup.exe"
+  file64         = "$toolsDir\qbittorrent_4.6.4_x64_setup.exe"
   silentArgs     = '/S'
   validExitCodes = @(0, 1223)
 }
