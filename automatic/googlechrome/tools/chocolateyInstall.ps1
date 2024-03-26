a046628ddcb0121cbe7c7166365c854e36695bcd
Chocolatey Community <chocolatey@realdimensions.net>
2024-03-26 00:20:05 +0000

    AU: 1 updated - GoogleChrome [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/cf46cb97218a026f13b223562b959dcd8aeeb26d

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '123.0.6312.58'
+$version = '123.0.6312.59'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
