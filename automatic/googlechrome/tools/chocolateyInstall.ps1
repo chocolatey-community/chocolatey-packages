be7e9734443163ba35b0e6d0628147c40a0b6997
Chocolatey Community <chocolatey@realdimensions.net>
2024-04-03 00:19:10 +0000

    AU: 3 updated - GoogleChrome nexus-repository vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/ccb8440b52250456b6c26d4b78abab6761b1e7a0

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '123.0.6312.86'
+$version = '123.0.6312.105'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = 'a7c26c676c8502aa7b3e43976393334a7e40773d9ca6bf747c7530cfd2c1097d'
-  checksum64             = 'dc1e28cda8a6e8b443e460b59087076adef23d50c84be18aead4c2a778111219'
+  checksum               = 'd9ef908e44785c8aae7850f90f6384fdd808dd72538332e443fe5dd2b424b9fe'
+  checksum64             = 'f9f7e6211ff117ba49020b31ab84537f8ccb506ac7b3630d688ecc57613d960e'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/nexus-repository/tools/chocolateyinstall.ps1
+++ automatic/nexus-repository/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ if (Get-OSArchitectureWidth 32) {
   throw "Sonatype Nexus Repository 3.0 and greater only supports 64-bit Windows."
 }
 
-$Version = '3.66.0-02'
+$Version = '3.67.0-03'
 $NexusVersionedFolder = "nexus-$Version"
 $TargetFolder = "$env:ProgramData\nexus"
 $ExtractFolder = "$env:TEMP\NexusExtract"
@@ -55,8 +55,8 @@ if (Test-Path "$ExtractFolder") {
 $PackageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $ExtractFolder
-  url64          = 'https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.66.0-02-win64.zip'
-  checksum64     = '96f28c96148bf81a30ee7f206eff81592dfcd82a0556218a3ac17956be14a251'
+  url64          = 'https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.67.0-03-win64.zip'
+  checksum64     = '3316e0f8a29660baa9b763966856ed0467d5cce24188ff4f9dcfee97894240ea'
   checksumType64 = 'SHA256'
 }
 
