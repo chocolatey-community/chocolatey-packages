d1a73c20d260cc064a7b0b63a933650dd8b7daca
Chocolatey Community <chocolatey@realdimensions.net>
2024-04-16 00:21:42 +0000

    AU: 5 updated - filezilla putty putty.install putty.portable vim [skip ci]

--- automatic/putty.install/tools/chocolateyInstall.ps1
+++ automatic/putty.install/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
     PackageName    = "putty.install"
     FileType       = "msi"
     SoftwareName   = "PuTTY"
-    File           = "$toolsPath\putty-0.80-installer.msi"
-    File64         = "$toolsPath\putty-64bit-0.80-installer.msi"
+    File           = "$toolsPath\putty-0.81-installer.msi"
+    File64         = "$toolsPath\putty-64bit-0.81-installer.msi"
     SilentArgs     = '/qn /norestart /l*v "{0}\install.log"' -f "$Env:TEMP\chocolatey\$Env:ChocolateyPackageName\$Env:ChocolateyPackageVersion"
     ValidExitCodes = @(0)
 }
