5d7f7bd00ea6fe96d69ca85646636adf70fc6d4e
Chocolatey Community <chocolatey@realdimensions.net>
2024-04-24 00:19:24 +0000

    AU: 5 updated - displayfusion GoogleChrome openshift-cli smartftp vim [skip ci]

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '124.0.6367.61'
+$version = '124.0.6367.78'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = 'c2d47ea33d9c740f28031c9090901462bb80da9534a6dbcd0f3db7bd0bb405d5'
-  checksum64             = '4b0ec825c78a7c7310fb4f2cc83ab8b57f258827df0ec5aeb8e63956c7a31398'
+  checksum               = '5806709e466a26565143777a2d6a60bda7047c33854f3035a07660b6d1060adc'
+  checksum64             = 'da03d5b4a6f9acbb7a27e592cfd9161a11e61a149d22fcc35091b9b154a5e191'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.1.0366_x86.zip"
-  file64        = "$toolsDir\gvim_9.1.0366_x64.zip"
+  file          = "$toolsDir\gvim_9.1.0368_x86.zip"
+  file64        = "$toolsDir\gvim_9.1.0368_x64.zip"
 }
 
 $installArgs = @{
