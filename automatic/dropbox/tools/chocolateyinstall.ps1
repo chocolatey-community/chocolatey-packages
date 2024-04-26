6bf25a8b4c4a5e432495886d629c0126b20e9e40
Chocolatey Community <chocolatey@realdimensions.net>
2024-04-26 22:51:43 +0000

    AU: 2 updated - dropbox selenium-chromium-edge-driver [skip ci]

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '197.4.7629'
+$version = '198.3.7548'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,10 +13,10 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20197.4.7629%20Offline%20Installer.x86.exe'
-    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20197.4.7629%20Offline%20Installer.x64.exe'
-    checksum       = 'e02e935386bbb105c711d06820ad2f589e437c5d95eeb8cf52e520c0f59819c3'
-    checksum64     = '33363ebd2214f2249c69d4ed1c3a8412f4e2d308deade4ee9be8b5961ec569cd'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20198.3.7548%20Offline%20Installer.x86.exe'
+    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20198.3.7548%20Offline%20Installer.x64.exe'
+    checksum       = '656a94e7bd7c9214a71d95cd825f255ae09733480b2286329819ca5c2c208ac6'
+    checksum64     = '72d6169dd1ade2924e5fa2ff9c9b88c346fcaccf065165e4c3157e64cbf5404d'
     fileType       = 'exe'
     checksumType   = 'sha256'
     checksumType64 = 'sha256'
--- automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
+++ automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = 'selenium-chromium-edge-driver'
-  url            = 'https://msedgedriver.azureedge.net/123.0.2420.97/edgedriver_win32.zip'
-  url64          = 'https://msedgedriver.azureedge.net/123.0.2420.97/edgedriver_win64.zip'
-  checksum       = 'eafee7f92e134b9c8d5dad98bd5b7641f377ec8db5f34deccfd669788de5fec2'
-  checksum64     = 'd889ca13091daff652e0536f8f6af2ede599aeb184956290275d78de8eb6c034'
+  url            = 'https://msedgedriver.azureedge.net/124.0.2478.67/edgedriver_win32.zip'
+  url64          = 'https://msedgedriver.azureedge.net/124.0.2478.67/edgedriver_win64.zip'
+  checksum       = '89d6566aa1e181a1687546cd120d4d116f0df2580eb7038ba016e185c40196bd'
+  checksum64     = 'eee0e078907261f41d4f829bd4d68b16cc660ee52d9e5fd0b1995c645b08d8e9'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = $seleniumDir
