fff63fd58d285805f21bce58cced440c3f06f2e9
Chocolatey Community <chocolatey@realdimensions.net>
2024-04-26 12:19:13 +0000

    AU: 2 updated - mp3tag smartftp [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/9132463d7e97ecc26ef6c315ac756177ccb5328d

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,8 +24,8 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv325setup.exe"
-  file64         = "$toolsPath\mp3tagv325-x64-setup.exe"
+  file           = "$toolsPath\mp3tagv325asetup.exe"
+  file64         = "$toolsPath\mp3tagv325a-x64-setup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
