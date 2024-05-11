910a431c4cecffe246b599715b5a44a82d9875f3
Chocolatey Community <chocolatey@realdimensions.net>
2024-05-11 00:18:56 +0000

    AU: 8 updated - chromium mixxx notepadplusplus notepadplusplus.commandline notepadplusplus.install selenium-chromium-edge-driver smartftp vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/a54eceb4ec4cd6199ca417e30866bacf118ddda5

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4.0-win64.msi"
+  file64         = "$toolsPath\mixxx-2.4.1-win64.msi"
 
   softwareName   = 'Mixxx *'
 
