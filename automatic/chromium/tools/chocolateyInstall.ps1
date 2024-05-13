38ddb80d42671f33e85a01ea1a45099f744d2c5b
Chocolatey Community <chocolatey@realdimensions.net>
2024-05-13 12:17:38 +0000

    AU: 1 updated - chromium [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '126.0.6476.0-snapshots'
+$version = '126.0.6477.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1299803/mini_installer.exe'
-  checksum      = '020CF4D6593E3885156668575C48A48F9CEC07E92F03234427FF4CD8D605343E'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1299932/mini_installer.exe'
+  checksum      = 'A12F8FE20F5223E80C324F60D88976CD5460FC69FBEA1273B0162C7538885680'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
