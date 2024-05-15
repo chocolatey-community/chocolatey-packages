e23a6d9e309b7a92c9e7ebd1e276700cfbceb02b
Chocolatey Community <chocolatey@realdimensions.net>
2024-05-15 12:20:50 +0000

    AU: 8 updated - 7zip 7zip.install 7zip.portable chromium jenkins kubernetes-cli vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/e7d554ab3ab732f4f2a70ae31b431b7567d424c9

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '124.0.6367.208'
+$version = '127.0.6480.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://github.com/Hibbiki/chromium-win32/releases/download/v109.0.5414.120-r1070088/mini_installer.sync.exe'
-  checksum      = 'C1AABD6EEF38D759BBD92CA9516353D88C463A1E8389A875B4D12D53DB467F98'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1301156/mini_installer.exe'
+  checksum      = '4129AFFCC2D7ABC294917AF0C86E1BB0D2097BC398B881B1916CD5195A24A0F9'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,11 +22,11 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url64bit       = 'https://vscode.download.prss.microsoft.com/dbazure/download/insider/bbc4ba1eff648ee542972589216f211b7a6a08f2/VSCodeSetup-x64-1.90.0-insider.exe'
+  url64bit       = 'https://vscode.download.prss.microsoft.com/dbazure/download/insider/5e68ffd760038bb156894a87f896910be510accd/VSCodeSetup-x64-1.90.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum64     = '24051d700afa0286bf6f667809c55b66874eba6fb29805403df665ff6251871de9e09fac4fc48e7eaac6e3498388a87e07c189be04c2146babe428432b39462b'
+  checksum64     = '394c8cb7d651b7631d8311374a8b8c44d970f7c7b5af80a7f0a6de0774a4868238403a9d2a14df1018a3ff0cfb3d536b033f0eea7f6fda5ed897e31f020011d4'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
