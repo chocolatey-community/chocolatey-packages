489d5c919d62e2e28d06abeb61f663ccf4e10594
Radosław Serba <radoslaw@serba.ovh>
2024-05-30 23:37:14 +0200

    fix: remove redundant symbol

--- automatic/mumble/update.ps1
+++ automatic/mumble/update.ps1
@@ -27,7 +27,6 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $url32 = $LatestRelease.assets | Where-Object {$_.name.StartsWith("mumble_client")} | Where-Object {$_.name.EndsWith(".msi")} | Select-Object -ExpandProperty browser_download_url
   $releaseNotes = $download_page.Links | ? href -match "www.mumble.info\/blog" | Select-Object -first 1 -expand href
-  }
 
   @{
     URL32        = $url32
