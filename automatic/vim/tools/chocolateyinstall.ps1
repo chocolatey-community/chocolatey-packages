7663b9995f6b7ebdc6cb03963d2e55c5eeef7093
Chocolatey Community <chocolatey@realdimensions.net>
2024-06-03 00:17:42 +0000

    AU: 3 updated - kubernetes-kompose smartftp vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/cd98ec10838b4194194647cbbf70dff28f8843ed

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.1.0457_x86.zip"
-  file64        = "$toolsDir\gvim_9.1.0457_x64.zip"
+  file          = "$toolsDir\gvim_9.1.0461_x86.zip"
+  file64        = "$toolsDir\gvim_9.1.0461_x64.zip"
 }
 
 $installArgs = @{
