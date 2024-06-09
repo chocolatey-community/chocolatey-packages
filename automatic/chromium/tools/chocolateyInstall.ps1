0b6cce386e1bb82233553272fc759ea871292f26
Chocolatey Community <chocolatey@realdimensions.net>
2024-06-09 12:19:00 +0000

    AU: 4 updated - chromium vlc vlc.install vlc.portable [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/eabd80b85acc8e837af94fa12043b26364b7ae23

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '127.0.6529.0-snapshots'
+$version = '127.0.6530.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1312478/mini_installer.exe'
-  checksum      = '434D7D80C4DF264979662441DAC32FC8A656D68191D139F91FE89271B9D08B30'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1312520/mini_installer.exe'
+  checksum      = 'A85807635E2BE10747E966D5E5716C940B2296F19A25DEDF809795C2CF947B55'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
