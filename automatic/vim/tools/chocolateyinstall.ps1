05b11ccb58c6d632487087e0d7f3c425f7c7b31b
Chocolatey Community <chocolatey@realdimensions.net>
2024-06-11 00:20:27 +0000

    AU: 2 updated - chromium vim [skip ci]

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.1.0472_x86.zip"
-  file64        = "$toolsDir\gvim_9.1.0472_x64.zip"
+  file          = "$toolsDir\gvim_9.1.0474_x86.zip"
+  file64        = "$toolsDir\gvim_9.1.0474_x64.zip"
 }
 
 $installArgs = @{
