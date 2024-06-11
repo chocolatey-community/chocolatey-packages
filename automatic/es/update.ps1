3a32924c46eace899dc7de24b5a344ad73618f20
Chocolatey Community <chocolatey@realdimensions.net>
2024-06-11 01:02:01 +0000

    AU: 1 updated - es [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/93c2fc9ab69193c3d9c7cc1e55f90147e5a8daea

--- automatic/es/update.ps1
+++ automatic/es/update.ps1
@@ -27,9 +27,9 @@ function global:au_BeforeUpdate {
 function global:au_GetLatest {
     $download_page  = Invoke-WebRequest -Uri $releases -UseBasicParsing
     $re             = "ES-[\d\.]+\.x86.zip"
-    $urlPath        = $download_page.links | ? href -match $re | Select-Object -First 1 -Expand href
+    $urlPath        = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -Expand href
     $re64           = "ES-[\d\.]+\.x64.zip"
-    $urlPath64      = $download_page.links | ? href -match $re64 | Select-Object -First 1 -Expand href
+    $urlPath64      = $download_page.links | Where-Object href -match $re64 | Select-Object -First 1 -Expand href
     $version        = ($urlPath -split '-' | Select-Object -Last 1).trim(".x86.zip")
     @{
         Version      = $version
