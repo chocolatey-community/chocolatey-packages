c6be0d80d159f525dd139c135c270fb70268c59b
Chocolatey Community <chocolatey@realdimensions.net>
2024-06-18 18:18:13 +0000

    AU: 1 updated - libreoffice-still [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/ac68b0b6ab58bfaf9128a33b25d4f60ccfe3adef

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -4,13 +4,13 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
 $packageArgs = @{
-  packageName            = 'libreoffice-fresh'
-  version                = '24.2.3'
+  packageName            = 'libreoffice-still'
+  version                = '24.2.4'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/24.2.3/win/x86/LibreOffice_24.2.3_Win_x86.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/24.2.3/win/x86_64/LibreOffice_24.2.3_Win_x86-64.msi'
-  checksum               = 'fbbec994b6ff8fcf28b94675304762214e8d5c330f9406c9ff0b316d9eeb6134'
-  checksum64             = '12b988318452120d31a4c618bc6ac314ad3bf99157584d0c2bf6029801a4cf31'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/24.2.4/win/x86/LibreOffice_24.2.4_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/24.2.4/win/x86_64/LibreOffice_24.2.4_Win_x86-64.msi'
+  checksum               = 'fbcb2b4b4cd12a357a9d47f7ae9f52ba2e738405c727d5bcfdc5fe6dbe515c55'
+  checksum64             = '23dde11f10fc4ec4fac592d50a2448331fa9bedc68ae4ee39ed63201032d0d6d'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
