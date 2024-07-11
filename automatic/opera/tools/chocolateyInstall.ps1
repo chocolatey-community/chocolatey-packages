e5b50339292d4b79a030d9ec0a0facead2573292
Chocolatey Community <chocolatey@realdimensions.net>
2024-07-11 18:18:12 +0000

    AU: 4 updated - Opera vscode vscode.install youtube-dl [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/8872b2e184165831f48523e897178716f1bb4643

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -11,17 +11,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/111.0.5168.61/win/Opera_111.0.5168.61_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/111.0.5168.61/win/Opera_111.0.5168.61_Setup_x64.exe'
-  checksum       = 'accec6a3cf8eb3d11f5d62eb31ad5f8194f1d822af2353034486023dfd1380a4'
-  checksum64     = '5833187113ece6e3c4f97f4e970576381cd9fce22c311fb388105fddd598e7ee'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/112.0.5197.24/win/Opera_112.0.5197.24_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/112.0.5197.24/win/Opera_112.0.5197.24_Setup_x64.exe'
+  checksum       = '0e8c475ca03cf4f798958df5650dc35f32c25260aeb6760868449f805b1b262e'
+  checksum64     = 'e4212d61f72963c1522fb1d5a9e5b62b7c4057eff181950441e3e58fdbb2a825'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '111.0.5168.61'
+$version = '112.0.5197.24'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
