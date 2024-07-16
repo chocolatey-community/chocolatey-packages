dc72d35adbef62d2bc0f2b268f469d7d51d37b35
Chocolatey Community <chocolatey@realdimensions.net>
2024-07-16 00:19:24 +0000

    AU: 4 updated - freecad mixxx palemoon vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/9b96df883e061642f3475d00e1c8790cb3444c83

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.6-alpha-43-g94bc90997d-win64.msi"
+  file64         = "$toolsPath\mixxx-2.6-alpha-44-gfe878b99a0-win64.msi"
 
   softwareName   = 'Mixxx *'
 
--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-33.2.0.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-33.2.0.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-33.2.1.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-33.2.1.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = 'a1d444370757d9d43e642cc524f1aff08b6de696352f965973d2e4b73ae1ecfb'
+  checksum      = '2367d67d53915acd52b8f8e50e2dcb4c791e7210a15fd755438704ecf2beed9c'
   checksumType  = 'sha256'
-  checksum64    = '7e67d157327ccf464a36a29ccbb6d87c47d6e43f2025e4ab38c38f0d7dab78c1'
+  checksum64    = 'b4cd50b3c47283e141a9c70e918ec3957a7b9c20450153a5c6e4658909d02e93'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
