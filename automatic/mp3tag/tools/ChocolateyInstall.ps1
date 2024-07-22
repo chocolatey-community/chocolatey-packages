6c74aa932523136fe290d3570df3ce4a4f845a21
Chocolatey Community <chocolatey@realdimensions.net>
2024-07-22 18:18:45 +0000

    AU: 2 updated - mp3tag universal-usb-installer [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/c69837e98674750909fae26ee86820bc491173b5

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,8 +24,8 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv326csetup.exe"
-  file64         = "$toolsPath\mp3tagv326c-x64-setup.exe"
+  file           = "$toolsPath\mp3tagv326dsetup.exe"
+  file64         = "$toolsPath\mp3tagv326d-x64-setup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
