3eff89d7a78e8e2c04fe968adda2b902055bb2b8
Chocolatey Community <chocolatey@realdimensions.net>
2024-07-30 00:18:39 +0000

    AU: 8 updated - chromium cutepdf freecad git git.install git.portable prey vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/ddbeabb67b69963ee0f882d4143533a7cbe2dfdb

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '129.0.6625.0-snapshots'
+$version = '129.0.6626.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1334119/mini_installer.exe'
-  checksum      = '5B4C9FF2B318F65DEB3E0AA13AECD6D82DAD5580F6A2884E798E6735EAEA06F8'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1334453/mini_installer.exe'
+  checksum      = '1595D703E154DCFDDF297EDBED8C4A48D0C9ECFBB1B9F25D4737C18FAED785D6'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
