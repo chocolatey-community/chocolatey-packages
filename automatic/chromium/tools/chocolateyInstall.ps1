723e8b53776baf2754b6a1677a18790aaa681f17
Chocolatey Community <chocolatey@realdimensions.net>
2024-08-01 18:18:19 +0000

    AU: 3 updated - chromium vscode vscode.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/38b6bfba4067e37ca36c8d9a68e1346a7a49a9d6

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '129.0.6631.0-snapshots'
+$version = '129.0.6632.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1335960/mini_installer.exe'
-  checksum      = '821E9D3A59E357E72E9380B87D5C5726A2F1DFB589F317A4E5729846EB49CC09'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1336027/mini_installer.exe'
+  checksum      = '887F5928402F564529AC439724B81F50AB01B96FE9A3814E95604159FE414536'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.91.1'
+$version = '1.92.0'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,11 +16,11 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url64bit       = 'https://update.code.visualstudio.com/1.91.1/win32-x64/stable'
+  url64bit       = 'https://update.code.visualstudio.com/1.92.0/win32-x64/stable'
 
   softwareName   = "$softwareName"
 
-  checksum64     = 'c15a0b510aee7c91ee488c76012c936f08a0c7f36afb54375ac917d887c02e5d'
+  checksum64     = '7c72fc02bd1045090d34d2808999f75d61d23005448525af5b64ac5fafa8b3a3'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
