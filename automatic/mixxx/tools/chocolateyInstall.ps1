d229bf9a75d9bab82b6c051a6f1a9856f03a5a4a
Chocolatey Community <chocolatey@realdimensions.net>
2024-08-02 00:18:04 +0000

    AU: 3 updated - mixxx selenium-chromium-edge-driver vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/71f0d62d967e72b0a290fed4b6040efbe83569ca

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-beta-29-gcd959a6b98-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-beta-30-g22be583cf7-win64.msi"
 
   softwareName   = 'Mixxx *'
 
--- automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
+++ automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = 'selenium-chromium-edge-driver'
-  url            = 'https://msedgedriver.azureedge.net/127.0.2651.74/edgedriver_win32.zip'
-  url64          = 'https://msedgedriver.azureedge.net/127.0.2651.74/edgedriver_win64.zip'
-  checksum       = '440adb7d996c11e790b8b839365ac8fd0e96fcf2085aff0b17423f6730397a93'
-  checksum64     = 'bb9b4af841b41086ac322c34d75fab544d5894c74c45b07f86baf3fcef1c3d18'
+  url            = 'https://msedgedriver.azureedge.net/129.0.2751.0/edgedriver_win32.zip'
+  url64          = 'https://msedgedriver.azureedge.net/129.0.2751.0/edgedriver_win64.zip'
+  checksum       = 'e9076f091ef362d1fc5d8a27548c90b54ce2ac4f9bf59f4ff5c75c1f0b43166b'
+  checksum64     = '809be169deafb991e99ca48a0e266d9aeb57acef8908bf08a137c8b7d829d74e'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = $seleniumDir
