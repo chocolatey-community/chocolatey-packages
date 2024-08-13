b99cf1df6879d92547d53a4c6d42b77b2a670fbe
Chocolatey Community <chocolatey@realdimensions.net>
2024-08-13 12:19:46 +0000

    AU: 6 updated - Firefox palemoon picard SQLite vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/8f8a33f9a5066eecbaf49fdfef7b3fe70d2a85e7

--- automatic/palemoon/tools/chocolateyInstall.ps1
+++ automatic/palemoon/tools/chocolateyInstall.ps1
@@ -3,14 +3,14 @@
 $packageArgs = @{
   packageName   = 'palemoon'
   fileType      = 'exe'
-  url           = 'https://rm-eu.palemoon.org/release/palemoon-33.2.1.win32.installer.exe'
-  url64         = 'https://rm-eu.palemoon.org/release/palemoon-33.2.1.win64.installer.exe'
+  url           = 'https://rm-eu.palemoon.org/release/palemoon-33.3.0.win32.installer.exe'
+  url64         = 'https://rm-eu.palemoon.org/release/palemoon-33.3.0.win64.installer.exe'
 
   softwareName  = 'Pale Moon*'
 
-  checksum      = '2367d67d53915acd52b8f8e50e2dcb4c791e7210a15fd755438704ecf2beed9c'
+  checksum      = 'eae4e69e1fec5831e70cdf24e28039071a6337b6bf6b36b5be3f1697ca2ba6ea'
   checksumType  = 'sha256'
-  checksum64    = 'b4cd50b3c47283e141a9c70e918ec3957a7b9c20450153a5c6e4658909d02e93'
+  checksum64    = '6e34125dc22439a7480d17cb3c21c2b4b0213d11883869c950e3f699391b1469'
   checksumType64= 'sha256'
 
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,11 +22,11 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url64bit       = 'https://vscode.download.prss.microsoft.com/dbazure/download/insider/b60fe002f59dbc364ba3af905cefd6575bfe717b/VSCodeSetup-x64-1.93.0-insider.exe'
+  url64bit       = 'https://vscode.download.prss.microsoft.com/dbazure/download/insider/922413f6d97e16c05b565398f33d95c306b1ceb7/VSCodeSetup-x64-1.93.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum64     = 'aca7cf0dfdf022a11d7139bd38905d1ee35505ea8cba08313a08d94993a8b272b724c291dbda77111ba37ffa65eac49a01f5a3938aa06957485e2ba605598ea5'
+  checksum64     = '6649471c76364f516514e7bb296d8b693031f86045122c8c2086f431f1d8efc050cb1050507bd01693416bc0552c084c0ff14be0d45f77ea2908e9ef916adbc5'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
