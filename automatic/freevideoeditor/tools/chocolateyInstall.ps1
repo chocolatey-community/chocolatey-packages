b00384ed4a7f153c273ba5131c1d8fbfdf50a423
AdmiringWorm <kim@chocolatey.io>
2024-08-19 11:37:23 +0200

    (freevideoeditor) Make package partially embedded
    The package is now too large to be properly embedded, as such we need
    to only embed the 64bit executable while downloading the 32bit version.

--- automatic/freevideoeditor/tools/chocolateyInstall.ps1
+++ automatic/freevideoeditor/tools/chocolateyInstall.ps1
@@ -5,13 +5,19 @@ $toolsPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\video_editor_x32.exe"
+  url            = ''
   file64         = "$toolsPath\video_editor_x64.exe"
   softwareName   = 'VSDC Free Video Editor*'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
+  checksum       = ''
+  checksumType   = ''
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+if ((Get-OSArchitectureWidth -compare 32) -or ($env:ChocolateyForceX86 -eq $true)) {
+  Install-ChocolateyPackage @packageArgs
+} else {
+  Install-ChocolateyInstallPackage @packageArgs
+}
 
 Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
