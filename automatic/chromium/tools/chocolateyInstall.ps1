430f7dc4c52fc46dc3e88fc0cfdc8000876b0835
Chocolatey Community <chocolatey@realdimensions.net>
2024-09-08 12:18:25 +0000

    AU: 2 updated - chromium mixxx [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/403f8058ed53a931bcf926f4387e1f8a105cb0c5

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '130.0.6705.0-snapshots'
+$version = '130.0.6706.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1352467/mini_installer.exe'
-  checksum      = 'DDAE77E1EFD23FF9014FE17AC6A2DCDCEA1AA33385A6CACAB9BD2791AC6C8C69'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1352487/mini_installer.exe'
+  checksum      = '13D994004FF77FF85AC3400060F7A1A1C90E87F411DD0C389C9426CE40E8B26B'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.6-alpha-84-g8790b5fbf2-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-beta-49-g5750104365-win64.msi"
 
   softwareName   = 'Mixxx *'
 
