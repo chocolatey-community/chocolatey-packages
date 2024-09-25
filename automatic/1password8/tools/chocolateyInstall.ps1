c1a3d39ea26e859ef0f4c1f1f9eacd95b340c332
AdmiringWorm <kim@chocolatey.io>
2024-09-25 12:34:32 +0200

    (1password8) Create package for 1password 8.x
    This creates a new package to handle installations of the 1password 8.x
    range.
    
    This uses a similar approach as the 1password7 package, but makes us of
    the machine wide MSI instead of the User level exe file, as well as
    requiring to use the release notes page to get the version as these are
    not provided through the normal update service anymore.

---
+++ automatic/1password8/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'msi'
+  url            = ''
+  softwareName   = '1Password*'
+  checksum       = ''
+  checksumType   = 'sha256'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
