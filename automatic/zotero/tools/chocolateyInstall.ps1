f7be67637b231a9e9026c7592f033a6b6d688baa
teknowledgist <teknowledgist@users.noreply.github.com>
2024-09-30 12:57:33 -0400

    (zotero) add 64-bit installer
    This adds the new install option for 64-bit Zotero and simplifies the update a bit.

--- automatic/zotero/tools/chocolateyInstall.ps1
+++ automatic/zotero/tools/chocolateyInstall.ps1
@@ -1,16 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+$Installers = Get-ChildItem -Path $toolsPath -Filter '*.exe' |
+                  Sort-Object lastwritetime | 
+                  Select-Object -Last 2 -ExpandProperty FullName
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  fileType       = 'exe'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
-  softwareName   = 'Zotero'
-  file           = "$toolsPath\Zotero-7.0.5_win32_setup.exe"
+   packageName    = $env:ChocolateyPackageName
+   softwareName   = "$env:ChocolateyPackageName*"
+   fileType       = 'exe'
+   file           = $Installers | Where-Object {$_ -match "win32"}
+   file64         = $Installers | Where-Object {$_ -match "x64"}
+   silentArgs     = '/S'
+   validExitCodes = @(0)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
 
-Remove-Item -Force -ea 0 "$toolsPath\*.exe","$toolsPath\*.ignore"
+Remove-Item $Installers -Force -ea 0
