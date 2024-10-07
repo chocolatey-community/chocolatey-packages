260972354f233bcc91da8ec472639907b0332673
Chocolatey Community <chocolatey@realdimensions.net>
2024-10-07 18:22:26 +0000

    AU: 4 updated - bleachbit bleachbit.install mixxx Opera [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/4955a4c2a876a79dd983cdd44afc4195e44137e2

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.6-alpha-98-g60bdaca11e-win64.msi"
+  file64         = "$toolsPath\mixxx-2.6-alpha-99-gcff6de731a-win64.msi"
 
   softwareName   = 'Mixxx *'
 
