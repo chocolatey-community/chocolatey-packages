d6ad32db8da02da47a5c9b00822f378685da220d
Chocolatey Community <chocolatey@realdimensions.net>
2024-10-08 00:24:30 +0000

    AU: 3 updated - chromium freecad vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/9491a85c35a1c415160d0242e7564496209957a4

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '131.0.6763.0-snapshots'
+$version = '131.0.6764.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1364852/mini_installer.exe'
-  checksum      = 'B770764428897942367D6DB7C26968C38AFE5A0AFE50B280ADE772CCC40B9A54'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1365196/mini_installer.exe'
+  checksum      = '35AB1586EB7E9148597DE27B236DFAD52FB93F0148626149B03F7695977BD640'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.1.0764_x86.zip"
-  file64        = "$toolsDir\gvim_9.1.0764_x64.zip"
+  file          = "$toolsDir\gvim_9.1.0766_x86.zip"
+  file64        = "$toolsDir\gvim_9.1.0766_x64.zip"
 }
 
 $installArgs = @{
