3e9c34b8cc50c04b15137abd024a38fa4935d911
staeglis <staeglis@informatik.uni-freiburg.de>
2024-10-09 09:39:45 +0200

    Add silentArg ALLUSERS=1
    If silentArg ALLUSERS=1 is not set the package is installed as user only

--- automatic/mattermost-desktop/tools/chocolateyInstall.ps1
+++ automatic/mattermost-desktop/tools/chocolateyInstall.ps1
@@ -11,7 +11,7 @@ $packageArgs = @{
   checksum64             = '2E8592BC18E1AA18A59365547135778FE462A76549ECC4A8FA005452F74BF162'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
-  silentArgs             = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs             = "/ALLUSERS=1 /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes         = @(0, 3010, 1641)
   softwareName           = 'Mattermost*'
 }
