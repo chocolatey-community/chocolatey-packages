4bc4d4b3c1f6884668b85e1738057dc7291bd30a
Chocolatey Community <chocolatey@realdimensions.net>
2024-10-18 06:19:10 +0000

    AU: 2 updated - chromium zotero [skip ci]

--- automatic/zotero/tools/chocolateyInstall.ps1
+++ automatic/zotero/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   silentArgs     = '/S'
   validExitCodes = @(0)
   softwareName   = 'Zotero'
-  file           = "$toolsPath\Zotero-7.0.7_win32_setup.exe"
+  file           = "$toolsPath\Zotero-7.0.8_win32_setup.exe"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
