30208b7e0bccd5db9bb1296c041ad78ace13fde2
Chocolatey Community <chocolatey@realdimensions.net>
2024-11-05 00:19:04 +0000

    AU: 4 updated - clipboardfusion mixxx thunderbird vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/2861e98a6eeb21c28d2cc099ebb46eafc8c95ba0

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-beta-68-g9bd837ed89-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-beta-74-g2d2237ec2f-win64.msi"
 
   softwareName   = 'Mixxx *'
 
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.1.0837_x86.zip"
-  file64        = "$toolsDir\gvim_9.1.0837_x64.zip"
+  file          = "$toolsDir\gvim_9.1.0842_x86.zip"
+  file64        = "$toolsDir\gvim_9.1.0842_x64.zip"
 }
 
 $installArgs = @{
