6c0f196bc36e49208f1d54525cc9f5fe35946ac5
Chocolatey Community <chocolatey@realdimensions.net>
2024-11-11 00:19:29 +0000

    AU: 4 updated - chromium freecad mixxx vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/d7ee115902b69009b9ed61ce5acfe8b2cc0d0e77

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.5-beta-79-g26e4175031-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-beta-82-gcf954819de-win64.msi"
 
   softwareName   = 'Mixxx *'
 
