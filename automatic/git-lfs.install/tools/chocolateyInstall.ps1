aaa6e7fbfd9d9e590df514d489e2a56e3a0db5c5
Chocolatey Community <chocolatey@realdimensions.net>
2024-11-21 06:19:19 +0000

    AU: 4 updated - git-lfs git-lfs.install gom-player kubernetes-cli [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/9a34f358ded4468a4f949f1bf46e69ff02d29011

--- automatic/git-lfs.install/tools/chocolateyInstall.ps1
+++ automatic/git-lfs.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$filePath = "$toolsDir\git-lfs-windows-v3.5.1.exe"
+$filePath = "$toolsDir\git-lfs-windows-v3.6.0.exe"
 
 $packageArgs = @{
   PackageName    = 'git-lfs'
