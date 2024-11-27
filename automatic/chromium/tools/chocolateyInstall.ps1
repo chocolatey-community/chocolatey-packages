d36aa2314434f411b1fa0f962042426c521f0d85
Chocolatey Community <chocolatey@realdimensions.net>
2024-11-27 18:21:00 +0000

    AU: 3 updated - chromium jenkins Opera [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/b54dd04fb6109184c623ba5825d9ac9bed9369c7

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '133.0.6863.0-snapshots'
+$version = '133.0.6864.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1388708/mini_installer.exe'
-  checksum      = '46AF97F4EBF826E55DEC4CC8E2D30C00E62E2EA2DDC433833BEAA9077F39C8C6'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1388834/mini_installer.exe'
+  checksum      = '3E11E591F94F6C2452A53C2FB5ADFC58BE032B9388EC96DC89FB452B7113D2D9'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -11,17 +11,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/114.0.5282.222/win/Opera_114.0.5282.222_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/114.0.5282.222/win/Opera_114.0.5282.222_Setup_x64.exe'
-  checksum       = '57d1e227088a62b6bb0122645b16f1003690b874b988c322aadaa54ea6d4c2b8'
-  checksum64     = '618a129e3a8edbb1443d7be23d1a8304d2be8e8629f60907a53a7c3131dbbf1d'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/115.0.5322.68/win/Opera_115.0.5322.68_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/115.0.5322.68/win/Opera_115.0.5322.68_Setup_x64.exe'
+  checksum       = '7d6344bd3792524711d807e063b181357338351c88f3865932edd808b6a44910'
+  checksum64     = '421745f4c3700c31e88ad939f879bd96da5a18596d7c95300362fbc5c8f9379a'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '114.0.5282.222'
+$version = '115.0.5322.68'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
