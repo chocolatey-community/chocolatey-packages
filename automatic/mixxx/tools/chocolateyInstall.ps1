1bdf00d318075c1687aa5e003f15bdf30d96f0fc
Chocolatey Community <chocolatey@realdimensions.net>
2024-11-28 00:19:15 +0000

    AU: 6 updated - mixxx protoc putty putty.install putty.portable vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/86d3b11ebd838ca47b577d9646985248bdb22242

--- automatic/mixxx/tools/chocolateyInstall.ps1
+++ automatic/mixxx/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ if ((Get-OSArchitectureWidth 32) -or ($env:chocolateyForceX86 -eq 'true')) {
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  file64         = "$toolsPath\mixxx-2.4.2-win64.msi"
+  file64         = "$toolsPath\mixxx-2.5-beta-92-gb1301eaac2-win64.msi"
 
   softwareName   = 'Mixxx *'
 
