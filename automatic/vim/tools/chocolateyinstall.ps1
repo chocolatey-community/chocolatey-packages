7015380c80f63c4ddfee298113af5977c39b9403
Chocolatey Community <chocolatey@realdimensions.net>
2024-12-08 00:19:18 +0000

    AU: 2 updated - SQLite vim [skip ci]

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.1.0909_x86.zip"
-  file64        = "$toolsDir\gvim_9.1.0909_x64.zip"
+  file          = "$toolsDir\gvim_9.1.0912_x86.zip"
+  file64        = "$toolsDir\gvim_9.1.0912_x64.zip"
 }
 
 $installArgs = @{
