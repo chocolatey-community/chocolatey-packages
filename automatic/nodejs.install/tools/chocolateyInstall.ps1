cd5252834218fdb47cd23af664dd93a87abc93c2
Chocolatey Community <chocolatey@realdimensions.net>
2024-12-10 18:21:47 +0000

    AU: 5 updated - blender Firefox nodejs nodejs.install voicebot [skip ci]

--- automatic/nodejs.install/tools/chocolateyInstall.ps1
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   PackageName    = 'nodejs.install'
   FileType       = 'msi'
   SoftwareName   = 'Node.js'
-  File           = "$toolsPath\node-v22.12.0-x86.msi"
-  File64         = "$toolsPath\node-v22.12.0-x64.msi"
+  File           = ''
+  File64         = "$toolsPath\node-v23.4.0-x64.msi"
   SilentArgs     = '/quiet ADDLOCAL=ALL'
   ValidExitCodes = @(0)
 }
--- automatic/voicebot/tools/chocolateyInstall.ps1
+++ automatic/voicebot/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'VoiceBot'
-    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/123/VoiceBotSetup-3.9.7.exe'
-    checksum        = '892dba6f78eac82d2e23932e5d7719509c83e0b027a35e339d03d5ec11541d6b'
+    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/123/VoiceBotSetup-3.9.8.exe'
+    checksum        = 'e0b90613196ba7479f8fbec2a9e1075467f78f2fb4c3a23ba2e939036687d1bd'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/VERYSILENT /LAUNCHAFTER=0'
