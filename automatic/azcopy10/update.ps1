eee96e55930cf94d272609dd9929d3d6a8b4b1ee
robmoss2k <15984835+robmoss2k@users.noreply.github.com>
2024-12-17 12:44:30 +0000

    Remove redundant URL redirection function from azcopy update script

--- automatic/azcopy10/update.ps1
+++ automatic/azcopy10/update.ps1
@@ -2,34 +2,6 @@
 
 $releases = 'https://aka.ms/downloadazcopy-v10-windows'
 
-# Define a function to handle the 301 redirection and retrieve headers
-function Get-RedirectedUrl {
-  param (
-      [string]$Url
-  )
-  # Use .NET HttpClient for better control
-  $handler = New-Object System.Net.Http.HttpClientHandler
-  $handler.AllowAutoRedirect = $false  # Prevent auto-following redirects
-
-  $client = New-Object System.Net.Http.HttpClient($handler)
-
-  try {
-      # Create a HEAD request
-      $request = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Head, $Url)
-      $response = $client.SendAsync($request).Result
-
-      if ($response.StatusCode -eq [System.Net.HttpStatusCode]::MovedPermanently -or `
-          $response.StatusCode -eq [System.Net.HttpStatusCode]::Found) {
-          return $response.Headers.Location.AbsoluteUri
-      } else {
-          throw "Unexpected status code: $($response.StatusCode)"
-      }
-  }
-  finally {
-      $client.Dispose()
-  }
-}
-
 function global:au_GetLatest {
     $url64 = Get-RedirectedUrl -Url $releases
     $url32 = $url64 -replace "amd64", "386"
