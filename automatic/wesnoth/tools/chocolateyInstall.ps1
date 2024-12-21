0ccede6032737cd6b1c4456ef38fd8ff96f435ce
Chocolatey Community <chocolatey@realdimensions.net>
2024-12-21 06:19:18 +0000

    AU: 4 updated - cmake cmake.install cmake.portable wesnoth [skip ci]

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.19.6/wesnoth-1.19.6-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.19.7/wesnoth-1.19.7-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = 'cdffa5bedab426f37d4aedaab78e8a92e164e128249bef27e7185686a7f0a2d3'
+  checksum       = '8601a0869fb470e77f412b952dfa730babf768c9f50fb0386fbbd7be10c38600'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
