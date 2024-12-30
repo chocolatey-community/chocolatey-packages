443cca73ab8017f1327838c5e85ef40ec27e2e52
Chocolatey Community <chocolatey@realdimensions.net>
2024-12-30 18:21:04 +0000

    AU: 4 updated - chromium ruby ruby.install ruby.portable [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '133.0.6928.0-snapshots'
+$version = '133.0.6929.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1400840/mini_installer.exe'
-  checksum      = '1211B35146208BFBB7E40522E41EABE94A1C63EAF41F888F24B058CCCA860AD6'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1400926/mini_installer.exe'
+  checksum      = '43655316AA2ADD65E47544EAD1CDD3527660448E1286CBFD53495A13AC12E837'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
