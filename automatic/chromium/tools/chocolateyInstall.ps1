7fde49ca450273ffb8ed75f83749f88e287f06b5
Chocolatey Community <chocolatey@realdimensions.net>
2025-01-04 00:19:57 +0000

    AU: 4 updated - chromium dropbox freeciv vim [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/29f6d3257aff3ff489621fe1a18c6211be8c3f78

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '133.0.6936.0-snapshots'
+$version = '133.0.6937.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1401681/mini_installer.exe'
-  checksum      = '8583A5D9C0348E2886C203ED788FCDAFF809D4141203CAC98DE5C5C63934CB88'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1401959/mini_installer.exe'
+  checksum      = '6569A7FD86D018CAE8214834FB7F4EB0E3384B52203DF864A235045D208CBE58'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '215.3.7023'
+$version = '215.3.7173'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,10 +13,10 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20215.3.7023%20Offline%20Installer.x86.exe'
-    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20215.3.7023%20Offline%20Installer.x64.exe'
-    checksum       = 'af2b001992f5b310e55159629f92b6115dd77cde8b7fa6c5204d8ef11ffdb6df'
-    checksum64     = 'c464ab11c24f0c3793b89e3c2dfe033211626505b836dab7ec435882c292c3c3'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20215.3.7173%20Offline%20Installer.x86.exe'
+    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20215.3.7173%20Offline%20Installer.x64.exe'
+    checksum       = 'bd1a852192486219563d220024d518c1ccb1d8f2d25fba449fd7005b1ce4f202'
+    checksum64     = '3e3374d4287a479e4302ed5ece4c8c2b939e4fd6dc91f41d3924453aee0a9b5d'
     fileType       = 'exe'
     checksumType   = 'sha256'
     checksumType64 = 'sha256'
