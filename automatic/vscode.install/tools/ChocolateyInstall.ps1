49ef2240856f405de77bc7b654bd7caeb946cb7e
Chocolatey Community <chocolatey@realdimensions.net>
2025-01-13 18:22:06 +0000

    AU: 2 updated - vscode vscode.install [skip ci]

--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.96.2'
+$version = '1.96.3'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,11 +16,11 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url64bit       = 'https://update.code.visualstudio.com/1.96.2/win32-x64/stable'
+  url64bit       = 'https://update.code.visualstudio.com/1.96.3/win32-x64/stable'
 
   softwareName   = "$softwareName"
 
-  checksum64     = 'dabbeb4bf2cde12560a7dcfac12f78cb8cc924fd65d68704248434ac8ed04bd0'
+  checksum64     = 'dae685ed2708caf087c89a028818bae8c59983549b6d052164c1b3df21465cb3'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
