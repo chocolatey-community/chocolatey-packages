ae718b62835e2b71e56bfd6962335eb765d42ccb
Chocolatey Community <chocolatey@realdimensions.net>
2025-01-30 18:21:38 +0000

    AU: 3 updated - nodejs nodejs.install spotify [skip ci]

--- automatic/nodejs.install/tools/chocolateyInstall.ps1
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   FileType       = 'msi'
   SoftwareName   = 'Node.js'
   File           = ''
-  File64         = "$toolsPath\node-v23.6.1-x64.msi"
+  File64         = "$toolsPath\node-v23.7.0-x64.msi"
   SilentArgs     = '/quiet ADDLOCAL=ALL'
   ValidExitCodes = @(0)
 }
