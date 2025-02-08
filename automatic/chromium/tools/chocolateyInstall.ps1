27d65ebe45d95c9a34284849b8eba63768443bfd
Chocolatey Community <chocolatey@realdimensions.net>
2025-02-08 12:18:42 +0000

    AU: 4 updated - chromium putty putty.install putty.portable [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '135.0.7005.0-snapshots'
+$version = '135.0.7006.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1417574/mini_installer.exe'
-  checksum      = '5CBDAF7E3B3441C843C8A14677C633F7108CFF62FAFC7E872B7D1C52DC22AAFE'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1417742/mini_installer.exe'
+  checksum      = 'C23A71CF639C564B43E032962651759D8CB399776ABD7397D71E89B195B956BC'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
