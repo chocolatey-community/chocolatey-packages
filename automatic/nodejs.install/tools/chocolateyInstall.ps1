30fa9e2fbc727c2b02900cc26f08f237d9e5071e
Chocolatey Community <chocolatey@realdimensions.net>
2025-02-10 18:18:26 +0000

    AU: 2 updated - nodejs nodejs.install [skip ci]

--- automatic/nodejs.install/tools/chocolateyInstall.ps1
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   PackageName    = 'nodejs.install'
   FileType       = 'msi'
   SoftwareName   = 'Node.js'
-  File           = ''
-  File64         = "$toolsPath\node-v23.7.0-x64.msi"
+  File           = "$toolsPath\node-v20.18.3-x86.msi"
+  File64         = "$toolsPath\node-v20.18.3-x64.msi"
   SilentArgs     = '/quiet ADDLOCAL=ALL'
   ValidExitCodes = @(0)
 }
