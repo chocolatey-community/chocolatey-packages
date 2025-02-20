d6c2fcf121b7a47446fda3f0802f3d4dc4ac8e7f
Chocolatey Community <chocolatey@realdimensions.net>
2025-02-20 18:19:37 +0000

    AU: 2 updated - 1password 1password8 [skip ci]

--- automatic/1password8/tools/chocolateyInstall.ps1
+++ automatic/1password8/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.10.60.msi'
+  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.10.62.msi'
   softwareName   = '1Password*'
-  checksum       = '9d400ae86d769e9b2f10bc81105e98a87427f9c1dc05ec48be2ff102b7b8bc9c'
+  checksum       = 'e78af4c46eb3490c8358cff83966f6610c72d0479a88955d7212340435718df3'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 1641, 3010)
