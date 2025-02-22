2a74f764c60a21a586022ab8d717fbf140a54341
Chocolatey Community <chocolatey@realdimensions.net>
2025-02-22 12:18:05 +0000

    AU: 3 updated - chromium peazip peazip.install [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '135.0.7029.0-snapshots'
+$version = '135.0.7030.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1423412/mini_installer.exe'
-  checksum      = 'C7CC30CCCA14120B501C8CC8D7B1ECFC7035B29D920F9FC186F3D09ED9AFDDDC'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1423562/mini_installer.exe'
+  checksum      = '92038B008070C25A981A58B0196B86FE35B4360E24BB472EA00915040B36BD90'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
