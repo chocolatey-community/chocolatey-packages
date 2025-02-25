145d3145e81f58c20e1340c21d8cc4a50b81fb8d
Chocolatey Community <chocolatey@realdimensions.net>
2025-02-25 06:18:59 +0000

    AU: 4 updated - cmake cmake.install cmake.portable libreoffice-still [skip ci]

--- automatic/libreoffice-streams/tools/chocolateyInstall.ps1
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -5,12 +5,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = 'libreoffice-still'
-  version                = '24.8.0'
+  version                = '24.8.5'
   fileType               = 'msi'
-  url                    = 'https://downloadarchive.documentfoundation.org/libreoffice/old/24.8.0.3/win/x86/LibreOffice_24.8.0.3_Win_x86.msi'
-  url64bit               = 'https://downloadarchive.documentfoundation.org/libreoffice/old/24.8.0.3/win/x86_64/LibreOffice_24.8.0.3_Win_x86-64.msi'
-  checksum               = '3a0e133ec4e86dab1894fe881558078a55d10ac6af8bb37d11b2748ab0d018b4'
-  checksum64             = '3c28f090c9921d10460a2db266bb024b690ad67fda178746278ca9aa1f60301e'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/24.8.5/win/x86/LibreOffice_24.8.5_Win_x86.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/24.8.5/win/x86_64/LibreOffice_24.8.5_Win_x86-64.msi'
+  checksum               = 'c4f81d04db578b5d587d4cf49995161419a9defe7765e62644c84a38a9685543'
+  checksum64             = 'ef3077c919fb05a5778818a00b7f9b329841691e6892bfb664d117e83ea27c9c'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
