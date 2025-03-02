ac587c99fda219c2d8deb01eea6c1a757181c29f
Chocolatey Community <chocolatey@realdimensions.net>
2025-03-02 00:18:11 +0000

    AU: 3 updated - mattermost-desktop vim youtube-dl [skip ci]

--- automatic/mattermost-desktop/tools/chocolateyInstall.ps1
+++ automatic/mattermost-desktop/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName            = 'mattermost-desktop'
   fileType               = 'msi'
-  file                   = "$toolsDir\mattermost-desktop-5.11.0-win-arm64.msi"
-  file64                 = "$toolsDir\mattermost-desktop-5.11.0-win-x64.msi"
-  checksum               = '13EBE757AD453EF3CE9C45D42D04FF12F6D646CB6AB284F0325D6C50A9F33C92'
-  checksum64             = 'E805AFBE1355C95A1A70D771CF92DB6DE25CB0E9FBD79B3FFE81A83088168ADD'
+  file                   = "$toolsDir\mattermost-desktop-5.11.1-win-arm64.msi"
+  file64                 = "$toolsDir\mattermost-desktop-5.11.1-win-x64.msi"
+  checksum               = '5203BB4D31AEE380AC76A741605A5664B09E5230E99927616AB01400F319D452'
+  checksum64             = 'F85853E977F8AC3170AD9A47ACC6F1C50AAF01B8CCAD177E382ECD254F50F18A'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1"
