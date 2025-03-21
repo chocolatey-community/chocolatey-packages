243fe266ccae731cb616e4ca6700f6269d6febae
Chocolatey Community <chocolatey@realdimensions.net>
2025-03-21 00:18:50 +0000

    AU: 2 updated - chromium gimp [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '136.0.7080.0-snapshots'
+$version = '136.0.7081.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1435217/mini_installer.exe'
-  checksum      = '42B3F1C85D5FD8E1ED60FE2090D0EEEF74DAB2CB80146A2A2F341B9388643947'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1435677/mini_installer.exe'
+  checksum      = '87A1AEF1FBD8CF7C5C4511DA3E0A043BE8D6654CAF683FC079778688CB6FE977'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$fallbackUrl32 = 'https://download.gimp.org/gimp/v3.0/windows/gimp-3.0.0-setup.exe'
+$fallbackUrl32 = 'https://download.gimp.org/gimp/v3.0/windows/gimp-3.0.0-setup-1.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://download.gimp.org/gimp/v3.0/windows/gimp-3.0.0-setup.exe'
+  url            = 'https://download.gimp.org/gimp/v3.0/windows/gimp-3.0.0-setup-1.exe'
   softwareName   = 'GIMP'
-  checksum       = 'ab6f9aa481120097f032c39f07cb70990929878fa65bf4ec6d1669d7a616770a'
+  checksum       = 'c1101f6fe5103c2986bd39a8c4162b52ea915c1d8066c79a86497329afd01fe4'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /RESTARTEXITCODE=3010 /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
