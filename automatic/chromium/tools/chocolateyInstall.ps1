08744f4c0f59103d6d5e3826c000ab55f3b0617f
Chocolatey Community <chocolatey@realdimensions.net>
2025-03-31 00:32:05 +0000

    AU: 3 updated - chromium gnucash vim [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '136.0.7100.0-snapshots'
+$version = '136.0.7101.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1439958/mini_installer.exe'
-  checksum      = 'E7842271CA1954D8502C8918F34C211B5F3A0EA3780996D2CD51A370C17C0CA7'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1440039/mini_installer.exe'
+  checksum      = 'D613039710DD4D0BD8D3F4CD1FD37AD15C3490997F17468870F3058CAAB8D755'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/gnucash/tools/chocolateyInstall.ps1
+++ automatic/gnucash/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\gnucash-5.10.setup.exe"
+  file           = "$toolsPath\gnucash-5.11.setup.exe"
 
   softwareName   = 'GnuCash*'
 
