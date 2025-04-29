ea61c8ca53a409f0685319c931244ec5bc3b57ac
Chocolatey Community <chocolatey@realdimensions.net>
2025-04-29 00:21:11 +0000

    AU: 1 updated - azcopy10 [skip ci]

--- automatic/azcopy10/tools/chocolateyInstall.ps1
+++ automatic/azcopy10/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     packageName    = $packageName
-    url64          = 'https://azcopyvnext-awgzd8g7aagqhzhe.b02.azurefd.net/releases/release-10.28.1-20250326/azcopy_windows_amd64_10.28.1.zip'
-    checksum64     = 'afbb11b1c86f3b32b3c62a44267e45dce8420d2c8204e175b64041516d2c072f'
+    url64          = 'https://azcopyvnext-awgzd8g7aagqhzhe.b02.azurefd.net/releases/release-10.29.0-20250428/azcopy_windows_amd64_10.29.0.zip'
+    checksum64     = 'eb6e9cfe79aa95d8cbdcef64501d36dec951848cf7591eb404fd8c154aa6e7c0'
     checksumType64 = 'sha256'
-    url            = 'https://azcopyvnext-awgzd8g7aagqhzhe.b02.azurefd.net/releases/release-10.28.1-20250326/azcopy_windows_386_10.28.1.zip'
-    checksum       = '20227e1f80611eb3891e4ef4b34e6a113f51bb0469d71fdcf6904f387a0fb9c2'
+    url            = 'https://azcopyvnext-awgzd8g7aagqhzhe.b02.azurefd.net/releases/release-10.29.0-20250428/azcopy_windows_386_10.29.0.zip'
+    checksum       = 'a28ccabab154b227eed4af953b3a93168d7458beaa34ee30ea0a8ab4f6e17483'
     checksumType   = 'sha256'
     destination    = $toolsDir
 }
