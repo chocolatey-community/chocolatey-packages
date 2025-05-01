2c3e80288d69f18aa841c1931c92817148612cdf
Chocolatey Community <chocolatey@realdimensions.net>
2025-05-01 18:18:34 +0000

    AU: 2 updated - brave Firefox [skip ci]

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.79.97/BraveBrowserStandaloneSilentBetaSetup32.exe'
-  checksum    = '93E71B4E43EA8BDCF81BAA134134EF544F2A33D6F7845EBC2F3AA806FCA54FD5'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.78.94/BraveBrowserStandaloneSilentSetup32.exe'
+  checksum    = 'AB597B1C5F874D7FC1727FA7CAB08B408717A1BC1081A66D1E8F7AE41BF3CF81'
   checksumType= 'sha256'
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.79.97'
+[version]$softwareVersion = '1.78.94'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
