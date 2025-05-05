b0ebca39c83de02b50fdac7c74b08cbe3e6a10cb
Chocolatey Community <chocolatey@realdimensions.net>
2025-05-05 12:19:40 +0000

    AU: 3 updated - chromium Opera skype [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '138.0.7162.0-snapshots'
+$version = '138.0.7163.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1455461/mini_installer.exe'
-  checksum      = 'DCEAED1504EC0D051AA336A9478C5116AFA906777F84E6DF0F734188C566BE13'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1455580/mini_installer.exe'
+  checksum      = '7771C078B9153AB97C0DAD398DF07207A26527F1A516E03D2D7CA76D84987B71'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -11,17 +11,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/118.0.5461.60/win/Opera_118.0.5461.60_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/118.0.5461.60/win/Opera_118.0.5461.60_Setup_x64.exe'
-  checksum       = '39c601172c65a4479fb0eea8df6531b786456c7c3d28e68822a9c8b7227529be'
-  checksum64     = 'b5dbcdd33fc0cf1298623d0e436d3de18f44cc9ebab1cd9c19092be66345dc6a'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/118.0.5461.83/win/Opera_118.0.5461.83_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/118.0.5461.83/win/Opera_118.0.5461.83_Setup_x64.exe'
+  checksum       = 'c665e848d0c3c10349088e0c1a65a6a4a3331e063cfeac3f56b373bf4b4a7282'
+  checksum64     = '1712b8b02af0d7c60a3e2c670cd2913e68ca9101a71714a134f5eafa4772d2ff'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '118.0.5461.60'
+$version = '118.0.5461.83'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
