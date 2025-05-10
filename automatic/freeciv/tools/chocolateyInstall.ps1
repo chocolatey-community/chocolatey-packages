e6a5485d0db0cff279e7d1620ebcf6d3ef59c76c
Chocolatey Community <chocolatey@realdimensions.net>
2025-05-10 00:18:40 +0000

    AU: 1 updated - freeciv [skip ci]

--- automatic/freeciv/tools/chocolateyInstall.ps1
+++ automatic/freeciv/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'Freeciv*'
 
-  checksum       = '2b0232257a507c48e658cd9d1b118587b1d68f0a052d3fd9c65104352b55c485'
+  checksum       = '2d2632480e8d1248f64a3f6f2ee65085382733236d7d526593fc7e43dffeeea7'
   checksumType   = 'sha256'
-  url            = 'https://files.freeciv.org/packages/windows/Freeciv-3.1.4-msys2-win64-10-gtk3.22-setup.exe'
+  url            = 'https://files.freeciv.org/packages/windows/Freeciv-3.1.5-msys2-win64-10-gtk3.22-setup.exe'
 
   silentArgs     = '/S'
   validExitCodes = @(0)
