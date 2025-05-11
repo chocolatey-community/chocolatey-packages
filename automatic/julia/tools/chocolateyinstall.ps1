4cff9d47878a912e8142d3558e01ee0007bc36ce
Chocolatey Community <chocolatey@realdimensions.net>
2025-05-11 22:18:23 +0000

    AU: 1 updated - julia [skip ci] https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f/9894c9ef30672654c73274df6d43d338942428d7

--- automatic/julia/tools/chocolateyinstall.ps1
+++ automatic/julia/tools/chocolateyinstall.ps1
@@ -5,17 +5,17 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url           = ''
-  checksum      = ''
+  url           = 'https://julialang-s3.julialang.org/bin/winnt/x86/1.11/julia-1.11.5-win32.exe'
+  checksum      = '8372D64E1A8DE4D7611C0ABB9006BB0A2C9C2FFAB0F0660BD260E538BA91698C'
   checksumType  = 'sha256'
-  file64        = "$toolsDir\julia-1.10.5-win64.exe"
+  file64        = "$toolsDir\julia-1.11.5-win64.exe"
 
   softwareName  = 'Julia*'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
-$packageVersion = "1.10.5"
+$packageVersion = "1.11.5"
 
 if ((Get-OSArchitectureWidth -compare 32) -or ($env:chocolateyForceX86 -eq $true)) {
     Install-ChocolateyPackage @packageArgs
