3d94ac76a25382e5cafc80550323d54355c9728c
AdmiringWorm <kim@chocolatey.io>
2025-05-12 11:44:39 +0200

    (maint) Update build scripts to template equivalent
    This updates the files ".appveyor.yml", "test_all.ps1" and "update_all.ps1" to the equivalent that is suggested in our packages template repository, with specific modifications related to this repository.

--- test_all.ps1
+++ test_all.ps1
@@ -6,72 +6,71 @@ if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 $global:au_root = Resolve-Path $Root
 
 if (($Name.Length -gt 0) -and ($Name[0] -match '^random (.+)')) {
-    [array] $lsau = lsau
+  [array] $lsau = lsau
 
-    $group = [int]$Matches[1]
-    $n = (Get-Random -Maximum $group)
-    Write-Host "TESTING GROUP $($n+1) of $group"
+  $group = [int]$Matches[1]
+  $n = (Get-Random -Maximum $group)
+  Write-Host "TESTING GROUP $($n+1) of $group"
 
-    $group_size = [int]($lsau.Count / $group) + 1
-    $Name = $lsau | select -First $group_size -Skip ($group_size*$n) | % { $_.Name }
+  $group_size = [int]($lsau.Count / $group) + 1
+  $Name = $lsau | select -First $group_size -Skip ($group_size * $n) | % { $_.Name }
 
-    Write-Host ($Name -join ' ')
-    Write-Host ('-'*80)
+  Write-Host ($Name -join ' ')
+  Write-Host ('-' * 80)
 }
 
 $options = [ordered]@{
-    Force   = $true
-    Push    = $false
-    Threads = 10
+  Force       = $true
+  Push        = $false
+  Threads     = 10
 
-    IgnoreOn = @(                                      #Error message parts to set the package ignore status
-        'Could not create SSL/TLS secure channel'
-        'Could not establish trust relationship'
-        'The operation has timed out'
-        'Internal Server Error'
-        'Service Temporarily Unavailable'
-        'Choco pack failed with exit code 1'
-    )
+  IgnoreOn    = @(                                      #Error message parts to set the package ignore status
+    'Could not create SSL/TLS secure channel'
+    'Could not establish trust relationship'
+    'The operation has timed out'
+    'Internal Server Error'
+    'Service Temporarily Unavailable'
+    'Choco pack failed with exit code 1'
+  )
 
-    RepeatOn = @(                                      #Error message parts on which to repeat package updater
-        'Could not create SSL/TLS secure channel'             # https://github.com/chocolatey/chocolatey-packages/issues/718
-        'Could not establish trust relationship'              # -||-
-        'Unable to connect'
-        'The remote name could not be resolved'
-        'Choco pack failed with exit code 1'                  # https://github.com/chocolatey/chocolatey-packages/issues/721
-        'The operation has timed out'
-        'Internal Server Error'
-        'An exception occurred during a WebClient request'
-        'Job returned no object, Vector smash ?'
-        'remote session failed with an unexpected state'
-    )
-    RepeatSleep   = 60                                      #How much to sleep between repeats in seconds, by default 0
-    RepeatCount   = 2                                       #How many times to repeat on errors, by default 1
+  RepeatOn    = @(                                      #Error message parts on which to repeat package updater
+    'Could not create SSL/TLS secure channel'             # https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
+    'Could not establish trust relationship'
+    'Unable to connect'
+    'The remote name could not be resolved'
+    'Choco pack failed with exit code 1'                  # https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
+    'The operation has timed out'
+    'Internal Server Error'
+    'An exception occurred during a WebClient request'
+    'Job returned no object, Vector smash ?'
+  )
+  RepeatSleep = 60                                      #How much to sleep between repeats in seconds, by default 0
+  RepeatCount = 2                                       #How many times to repeat on errors, by default 1
 
-    Report = @{
-        Type = 'markdown'                                   #Report type: markdown or text
-        Path = "$PSScriptRoot\Update-Force-Test-${n}.md"      #Path where to save the report
-        Params= @{                                          #Report parameters:
-            Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
-            NoAppVeyor  = $true                             #  Markdown: do not show AppVeyor build shield
-            Title       = "Update Force Test - Group ${n}"
-            UserMessage = "[Ignored](#ignored) | [Update report](https://gist.github.com/$Env:gist_id) | [Build](https://ci.appveyor.com/project/chocolatey-community/chocolatey-coreteampackages-xnxcr) | **USING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
-        }
+  Report      = @{
+    Type   = 'markdown'                                   #Report type: markdown or text
+    Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"    #Path where to save the report
+    Params = @{                                          #Report parameters:
+      Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
+      NoAppVeyor      = $true                             #  Markdown: do not show AppVeyor build shield
+      Title           = "Update Force Test - Group ${n}"
+      UserMessage     = "[Ignored](#ignored) | [Update report](https://gist.github.com/$Env:gist_id) | [Build](https://ci.appveyor.com/project/chocolatey-community/chocolatey-coreteampackages-xnxcr)"       #  Markdown, Text: Custom user message to show
     }
+  }
 
-    Gist = @{
-        Id     = $Env:gist_id_test                          #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"       #List of files to add to the gist
-        Description = "Update Force Test Report #powershell #chocolatey"
-    }
+  Gist        = @{
+    Id          = $Env:gist_id_test                          #Your gist id; leave empty for new private or anonymous gist
+    ApiKey      = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+    Path        = "$PSScriptRoot\Update-Force-Test-${n}.md"  #List of files to add to the gist
+    Description = "Update Force Test Report #powershell #chocolatey"
+  }
 
-    ModulePaths = @("$PSScriptRoot\scripts\au_extensions.psm1"; "Wormies-AU-Helpers")
+  ModulePaths = @("$PSScriptRoot\scripts\au_extensions.psm1"; "Wormies-AU-Helpers")
 
-    BeforeEach  = {
-      param($PackageName, $Options )
-      $Options.ModulePaths | % { Import-Module $_ }
-      $global:au_Force = $true # Some of the helper scripts rely on this one
+  BeforeEach  = {
+    param($PackageName, $Options )
+    $Options.ModulePaths | % { Import-Module $_ }
+    $global:au_Force = $true # Some of the helper scripts rely on this one
   }
 }
 
@@ -79,8 +78,9 @@ $options = [ordered]@{
 
 if ($PSVersionTable.PSVersion.major -ge 6) {
   $AvailableTls = [enum]::GetValues('Net.SecurityProtocolType') | Where-Object { $_ -ge 'Tls' } # PowerShell 6+ does not support SSL3, so use TLS minimum
-  $AvailableTls.ForEach({[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_})
-} else {
+  $AvailableTls.ForEach({ [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_ })
+}
+else {
   [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor [System.Net.SecurityProtocolType]::Tls -bor [System.Net.SecurityProtocolType]::Ssl3
 }
 
@@ -89,5 +89,5 @@ $global:info = updateall -Name $Name -Options $Options
 $au_errors = $global:info | ? { $_.Error } | select -ExpandProperty Error
 
 if ($ThrowOnErrors -and $au_errors.Count -gt 0) {
-    throw 'Errors during update'
+  throw 'Errors during update'
 }
--- update_all.ps1
+++ update_all.ps1
@@ -1,130 +1,126 @@
-# AU Packages Template: https://github.com/majkinetor/au-packages-template
-
 param([string[]] $Name, [string] $ForcedPackages, [string] $Root = "$PSScriptRoot\automatic")
 
-if (Test-Path $PSScriptRoot/update_vars.ps1) {
-    . $PSScriptRoot/update_vars.ps1
-}
+if (Test-Path $PSScriptRoot/update_vars.ps1) { . $PSScriptRoot/update_vars.ps1 }
 
 $Options = [ordered]@{
-    WhatIf                    = $au_WhatIf                              #Whatif all packages
-    Force                     = $false                                  #Force all packages
-    Timeout                   = 100                                     #Connection timeout in seconds
-    UpdateTimeout             = 1200                                    #Update timeout in seconds
-    Threads                   = 10                                      #Number of background jobs to use
-    Push                      = $Env:au_Push -eq 'true'                 #Push to chocolatey
-    PushAll                   = $true                                   #Allow to push multiple packages at once
-    PluginPath                = ''                                      #Path to user plugins
-    IgnoreOn                  = @(                                      #Error message parts to set the package ignore status
-        'Could not create SSL/TLS secure channel'
-        'Could not establish trust relationship'
-        'The operation has timed out'
-        'Internal Server Error'
-        'Service Temporarily Unavailable'
-        'The connection was closed unexpectedly.'
-        'package version already exists'
-        'already exists on a Simple OData Server'             # https://github.com/chocolatey/chocolatey.org/issues/613
-        'Conflict'
-        'A system shutdown has already been scheduled'        # https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
-    )
-    RepeatOn                  = @(                                      #Error message parts on which to repeat package updater
-        'Could not create SSL/TLS secure channel'             # https://github.com/chocolatey/chocolatey-packages/issues/718
-        'Could not establish trust relationship'              # -||-
-        'Unable to connect'
-        'The remote name could not be resolved'
-        'Choco pack failed with exit code 1'                  # https://github.com/chocolatey/chocolatey-packages/issues/721
-        'The operation has timed out'
-        'Internal Server Error'
-        'An exception occurred during a WebClient request'
-        'remote session failed with an unexpected state'
-        'The connection was closed unexpectedly.'
-    )
-    #RepeatSleep   = 250                                    #How much to sleep between repeats in seconds, by default 0
-    #RepeatCount   = 2                                      #How many times to repeat on errors, by default 1
+  WhatIf                    = $au_WhatIf                              #Whatif all packages
+  Force                     = $false                                  #Force all packages
+  Timeout                   = 100                                     #Connection timeout in seconds
+  UpdateTimeout             = 1200                                    #Update timeout in seconds
+  Threads                   = 10                                      #Number of background jobs to use
+  Push                      = $Env:au_Push -eq 'true'                 #Push to chocolatey
+  PushAll                   = $true                                   #Allow to push multiple packages at once
+  PluginPath                = ''                                      #Path to user plugins
+  IgnoreOn                  = @(                                      #Error message parts to set the package ignore status
+    'Could not create SSL/TLS secure channel'
+    'Could not establish trust relationship'
+    'The operation has timed out'
+    'Internal Server Error'
+    'Service Temporarily Unavailable'
+    'The connection was closed unexpectedly.'
+    'package version already exists'
+    'already exists on a Simple OData Server'             # https://github.com/chocolatey/chocolatey.org/issues/613
+    'Conflict'
+    'A system shutdown has already been scheduled'        # https://gist.github.com/choco-bot/a14b1e5bfaf70839b338eb1ab7f8226f#wps-office-free
+  )
+  RepeatOn                  = @(                                      #Error message parts on which to repeat package updater
+    'Could not create SSL/TLS secure channel'             # https://github.com/chocolatey/chocolatey-coreteampackages/issues/718
+    'Could not establish trust relationship'
+    'Unable to connect'
+    'The remote name could not be resolved'
+    'Choco pack failed with exit code 1'                  # https://github.com/chocolatey/chocolatey-coreteampackages/issues/721
+    'The operation has timed out'
+    'Internal Server Error'
+    'An exception occurred during a WebClient request'
+    'remote session failed with an unexpected state'
+    'The connection was closed unexpectedly.'
+  )
+  #RepeatSleep   = 250                                    #How much to sleep between repeats in seconds, by default 0
+  #RepeatCount   = 2                                      #How many times to repeat on errors, by default 1
 
-    Report                    = @{
-        Type   = 'markdown'                                   #Report type: markdown or text
-        Path   = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
-        Params = @{                                          #Report parameters:
-            Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
-            NoAppVeyor      = $false                            #  Markdown: do not show AppVeyor build shield
-            UserMessage     = "[Ignored](#ignored) | [History](#update-history) | [Force Test](https://gist.github.com/$Env:gist_id_test) | [Releases](https://github.com/$Env:github_user_repo/tags) | **TESTING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
-            NoIcons         = $false                            #  Markdown: don't show icon[Releases](https://github.com/$Env:github_user_repo/tags)
-            IconSize        = 32                                #  Markdown: icon size
-            Title           = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
-        }
+  #NoCheckChocoVersion = $true                            #Turn on this switch for all packages
+
+
+  Report                    = @{
+    Type   = 'markdown'                                   #Report type: markdown or text
+    Path   = "$PSScriptRoot\Update-AUPackages.md"         #Path where to save the report
+    Params = @{                                          #Report parameters:
+      Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
+      NoAppVeyor      = $false                            #  Markdown: do not show AppVeyor build shield
+      UserMessage     = "[Ignored](#ignored) | [History](#update-history) | [Force Test](https://gist.github.com/$Env:gist_id_test) | [Releases](https://github.com/$Env:github_user_repo/tags)"       #  Markdown, Text: Custom user message to show
+      NoIcons         = $false                            #  Markdown: don't show icon
+      IconSize        = 32                                #  Markdown: icon size
+      Title           = ''                                #  Markdown, Text: TItle of the report, by default 'Update-AUPackages'
     }
+  }
 
-    History                   = @{
-        Lines           = 90                                          #Number of lines to show
-        Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
-        Path            = "$PSScriptRoot\Update-History.md"            #Path where to save history
+  History                   = @{
+    Lines           = 120                                         #Number of lines to show
+    Github_UserRepo = $Env:github_user_repo             #User repo to be link to commits
+    Path            = "$PSScriptRoot\Update-History.md"            #Path where to save history
+  }
+
+  Gist                      = @{
+    Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
+    ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+    Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
+  }
+
+  Git                       = @{
+    User     = ''                                       #Git username, leave empty if github api key is used
+    Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+  }
+
+  GitReleases               = @{
+    ApiToken    = $Env:github_api_key                   #Your github api key
+    ReleaseType = 'package'                             #Either 1 release per date, or 1 release per package
+  }
+
+  RunInfo                   = @{
+    Exclude = 'password', 'apikey', 'apitoken'          #Option keys which contain those words will be removed
+    Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
+  }
+
+  Mail                      = if ($Env:mail_user) {
+    @{
+      To          = $Env:mail_user
+      From        = $Env:mail_from
+      Server      = $Env:mail_server
+      UserName    = $Env:mail_user
+      Password    = $Env:mail_pass
+      Port        = $Env:mail_port
+      EnableSsl   = $Env:mail_enablessl -eq 'true'
+      Attachment  = "$PSScriptRoot\update_info.xml"
+      UserMessage = "Update status: Update status: https://gist.github.com/choco-bot/$Env:gist_id"
+      SendAlways  = $false                        #Send notifications every time
     }
+  }
+  else {}
 
-    Gist                      = @{
-        Id     = $Env:gist_id                               #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-AUPackages.md", "$PSScriptRoot\Update-History.md"       #List of files to add to the gist
-    }
+  ForcedPackages            = $ForcedPackages -split ' '
+  UpdateIconScript          = "$PSScriptRoot\scripts\Update-IconUrl.ps1"
+  UpdatePackageSourceScript = "$PSScriptRoot\scripts\Update-PackageSourceUrl.ps1"
+  ModulePaths               = @("$PSScriptRoot\scripts\au_extensions.psm1"; "Wormies-AU-Helpers")
+  BeforeEach                = {
+    param($PackageName, $Options )
+    $Options.ModulePaths | ForEach-Object { Import-Module $_ }
+    . $Options.UpdateIconScript $PackageName.ToLowerInvariant() -Quiet -ThrowErrorOnIconNotFound
+    . $Options.UpdatePackageSourceScript $PackageName.ToLowerInvariant() -Quiet
+    Expand-Aliases -Directory "$PWD"
 
-    Git                       = @{
-        User     = ''                                       #Git username, leave empty if github api key is used
-        Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
-    }
+    $pattern = "^${PackageName}(?:\\(?<stream>[^:]+))?(?:\:(?<version>.+))?$"
+    $p = $Options.ForcedPackages | Where-Object { $_ -match $pattern }
+    if (!$p) { return }
 
-    GitReleases               = @{
-        ApiToken    = $Env:github_api_key
-        ReleaseType = 'package'
-    }
-
-    RunInfo                   = @{
-        Exclude = 'password', 'apikey', 'apitoken'          #Option keys which contain those words will be removed
-        Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
-    }
-
-    Mail                      = if ($Env:mail_user) {
-        @{
-            To          = $Env:mail_user
-            Server      = $Env:mail_server
-            UserName    = $Env:mail_user
-            Password    = $Env:mail_pass
-            Port        = $Env:mail_port
-            EnableSsl   = $Env:mail_enablessl -eq 'true'
-            UserMessage = "Update status: Update status: https://gist.github.com/choco-bot/$Env:gist_id"
-            SendAlways  = $false                        #Send notifications every time
-        }
-    }
-    else {
-    }
-
-    ForcedPackages            = $ForcedPackages -split ' '
-    UpdateIconScript          = "$PSScriptRoot\scripts\Update-IconUrl.ps1"
-    UpdatePackageSourceScript = "$PSScriptRoot\scripts\Update-PackageSourceUrl.ps1"
-    ModulePaths               = @("$PSScriptRoot\scripts\au_extensions.psm1"; "Wormies-AU-Helpers")
-    BeforeEach                = {
-        param($PackageName, $Options )
-        $Options.ModulePaths | ForEach-Object { Import-Module $_ }
-        . $Options.UpdateIconScript $PackageName.ToLowerInvariant() -Quiet -ThrowErrorOnIconNotFound
-        . $Options.UpdatePackageSourceScript $PackageName.ToLowerInvariant() -Quiet
-        Expand-Aliases -Directory "$PWD"
-
-        $pattern = "^${PackageName}(?:\\(?<stream>[^:]+))?(?:\:(?<version>.+))?$"
-        $p = $Options.ForcedPackages | Where-Object { $_ -match $pattern }
-        if (!$p) {
-            return
-        }
-
-        $global:au_Force = $true
-        $global:au_IncludeStream = $Matches['stream']
-        $global:au_Version = $Matches['version']
-    }
+    $global:au_Force = $true
+    $global:au_IncludeStream = $Matches['stream']
+    $global:au_Version = $Matches['version']
+  }
 }
 
-if ($ForcedPackages) {
-    Write-Host "FORCED PACKAGES: $ForcedPackages"
-}
-
-$global:au_Root = $Root                                    #Path to the AU packages
+if ($ForcedPackages) { Write-Host "FORCED PACKAGES: $ForcedPackages" }
+$global:au_Root = $Root          #Path to the AU packages
+$global:au_GalleryUrl = ''             #URL to package gallery, leave empty for Chocolatey Gallery
 $global:info = updateall -Name $Name -Options $Options
 
 #Uncomment to fail the build on AppVeyor on any package error
