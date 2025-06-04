8278e59dba61ea0f4bc122c6e0028b80a0d7affd
Chocolatey-AU <chocolatey@realdimensions.net>
2025-06-04 18:23:06 +0000

    AU: 1 updated - webstorm [skip ci]

--- automatic/webstorm/tools/ChocolateyInstall.ps1
+++ automatic/webstorm/tools/ChocolateyInstall.ps1
@@ -19,8 +19,8 @@ New-Item -ItemType Directory -Force -Path $installDir
 $arguments              = @{
     packageName         = $env:ChocolateyPackageName
     softwareName        = 'WebStorm *'
-    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2025.1.1.exe'
-    checksum            = 'e148c643139b47338b3442892e15a2290b8d33e0abd2515a754f4b9e7adf800b'
+    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2025.1.2.exe'
+    checksum            = '4981e5b54ac810a54036cad37762108cb46a08d94662967f17dfdc858d9a4dde'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = $silentArgs
