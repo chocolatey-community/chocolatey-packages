93d6d5881fa005ee12b18b44967e0a2b6a36c2b2
Chocolatey-AU <chocolatey@realdimensions.net>
2025-06-25 00:23:20 +0000

    AU: 4 updated - dropbox nodejs nodejs.install vim [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/d5e2ebc8cdd74ff61fd2caa22d4d3203dea3ccec

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '227.3.4733'
+$version = '227.4.4774'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,10 +13,10 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20227.3.4733%20Offline%20Installer_superfluous.x86.exe'
-    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20227.3.4733%20Offline%20Installer_superfluous.x64.exe'
-    checksum       = '0292833f240fa9b5359a02d09bcdd7bdcede3bb56f36ee0d492ec510f1b098b9'
-    checksum64     = 'fb501e9322b9cb2bd44657bacf74abbb827b0b3aff4e10505264d441cb0e23a3'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20227.4.4774%20Offline%20Installer_superfluous.x86.exe'
+    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20227.4.4774%20Offline%20Installer_superfluous.x64.exe'
+    checksum       = '0098d79148e4b5eb49d6f1780ec336e83ff122f28b35a6df0daf45fc42009f43'
+    checksum64     = 'df9bfecf1cce59a8e6ca2bd213eeba677599b938b3dd1c11b964b6e8e9271fa5'
     fileType       = 'exe'
     checksumType   = 'sha256'
     checksumType64 = 'sha256'
--- automatic/nodejs.install/tools/chocolateyInstall.ps1
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   PackageName    = 'nodejs.install'
   FileType       = 'msi'
   SoftwareName   = 'Node.js'
-  File           = "$toolsPath\node-v20.19.3-x86.msi"
-  File64         = "$toolsPath\node-v20.19.3-x64.msi"
+  File           = ''
+  File64         = "$toolsPath\node-v24.3.0-x64.msi"
   SilentArgs     = '/quiet ADDLOCAL=ALL'
   ValidExitCodes = @(0)
 }
