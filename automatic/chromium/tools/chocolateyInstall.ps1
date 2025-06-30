2723ff2a4a2aa41a5b831989782a622a08c7291f
Chocolatey-AU <chocolatey@realdimensions.net>
2025-06-30 18:23:37 +0000

    AU: 4 updated - chromium notepadplusplus notepadplusplus.commandline notepadplusplus.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/598cb4c2922f59c7983535d8180e8e3a292607dd

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '140.0.7270.0-snapshots'
+$version = '140.0.7271.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1480324/mini_installer.exe'
-  checksum      = 'E01165E42E209D6162B86630C286C1E40FAA2C9C947939EAB835B78C4D057EDE'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1480584/mini_installer.exe'
+  checksum      = '717FFA4801697118DBCF3A04EAE621F56703C32ABDC43D5361752A9F48DC4E5A'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
