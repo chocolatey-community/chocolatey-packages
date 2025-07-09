1a48bdda1b433b65430f89adff754ae3fe9e1ac5
Chocolatey-AU <chocolatey@realdimensions.net>
2025-07-09 18:22:45 +0000

    AU: 3 updated - brave nodejs nodejs.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/bdb4a59f937635243f2d17d67f5bcf791a83dac5

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.81.110/BraveBrowserStandaloneSilentBetaSetup32.exe'
-  checksum    = 'E1CEC2093975E7CF9A4497B33B2D90415FD6A6721A9B342357B466B1D5E5DFD8'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.80.120/BraveBrowserStandaloneSilentSetup32.exe'
+  checksum    = 'CE771DD2A68E3F267535AD659B06647705BE60FD01767C9BB3D359917566EEC7'
   checksumType= 'sha256'
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
 }
 
-[version]$softwareVersion = '1.81.110'
+[version]$softwareVersion = '1.80.120'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
--- automatic/nodejs.install/tools/chocolateyInstall.ps1
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   FileType       = 'msi'
   SoftwareName   = 'Node.js'
   File           = ''
-  File64         = "$toolsPath\node-v24.3.0-x64.msi"
+  File64         = "$toolsPath\node-v24.4.0-x64.msi"
   SilentArgs     = '/quiet ADDLOCAL=ALL'
   ValidExitCodes = @(0)
 }
