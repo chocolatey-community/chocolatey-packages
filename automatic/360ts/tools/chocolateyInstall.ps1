df9ecc490b97ca14558ff77a064762f46b268c10
Chocolatey-AU <chocolatey@realdimensions.net>
2025-07-10 12:23:48 +0000

    AU: 3 updated - 360ts brave etcd [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/f1d2ac025aa8eca84dd0491409f2b03acba9df03

--- automatic/360ts/tools/chocolateyInstall.ps1
+++ automatic/360ts/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = '360ts'
   fileType               = 'exe'
-  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_11.0.0.1208.exe'
-  checksum               = '98c6b9d7d874fddca093d0f2a4f032d865477218067b32d966cea1d5dc7e4e26'
+  url                    = 'https://free.360totalsecurity.com/totalsecurity/360TS_Setup_11.0.0.1212.exe'
+  checksum               = '027d8db8ed3fb3a61090d3b314a49409eb8435590dff1c3ab8a705cc21c775c8'
   checksumType           = 'sha256'
   silentArgs             = '/S'
   validExitCodes         = @(0)
--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.80.120/BraveBrowserStandaloneSilentSetup32.exe'
-  checksum    = 'CE771DD2A68E3F267535AD659B06647705BE60FD01767C9BB3D359917566EEC7'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.81.112/BraveBrowserStandaloneSilentBetaSetup32.exe'
+  checksum    = '539563E0C15831AC029ECCACEE754C28DEEB0A37F2509BC4BC637ACAA63F6808'
   checksumType= 'sha256'
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.80.120'
+[version]$softwareVersion = '1.81.112'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
