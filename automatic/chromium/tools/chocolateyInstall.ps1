afd5573e70fa85d3c27dc07a32da2756e130d020
Chocolatey-AU <chocolatey@realdimensions.net>
2025-07-19 00:21:35 +0000

    AU: 4 updated - chromium freecad mp3tag vim [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/d135b311fb2cec2e3310918e79421d14bcdd6d4b

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '140.0.7304.0-snapshots'
+$version = '140.0.7305.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1488645/mini_installer.exe'
-  checksum      = '9B297135EC80033EC886B0F5173E058B2A50FBDD2F11EF5148A9437FCF21B5DF'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1489039/mini_installer.exe'
+  checksum      = '535CA6776540BC421870FA791C43E23CC862CD1321E4198E0F7E977D56BBC220'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,8 +24,8 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv330csetup.exe"
-  file64         = "$toolsPath\mp3tagv330c-x64-setup.exe"
+  file           = "$toolsPath\mp3tagv330dsetup.exe"
+  file64         = "$toolsPath\mp3tagv330d-x64-setup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
