22f20ea369e30bf9c937bc4e18a6a5d798a5598d
Chocolatey-AU <chocolatey@realdimensions.net>
2025-08-01 00:21:47 +0000

    AU: 4 updated - nodejs nodejs.install typescript Waterfox [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/8e3eca97803585733cd0c9f0729bbcaba6f0e30b

--- automatic/nodejs.install/tools/chocolateyInstall.ps1
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   FileType       = 'msi'
   SoftwareName   = 'Node.js'
   File           = ''
-  File64         = "$toolsPath\node-v24.4.1-x64.msi"
+  File64         = "$toolsPath\node-v24.5.0-x64.msi"
   SilentArgs     = '/quiet ADDLOCAL=ALL'
   ValidExitCodes = @(0)
 }
