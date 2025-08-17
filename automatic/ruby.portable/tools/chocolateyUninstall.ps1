e04ccba896c4e792e74c6b9861d033d60824cd8b
Paul Broadwith <paul@pauby.com>
2025-08-17 23:55:16 +0100

    (#2256) Shim bat and cmd

---
+++ automatic/ruby.portable/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsPath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+# Remove shims added for .bat and .cmd files, in the Ruby bin folder
+$rubyDir = Join-Path -Path $toolsPath -ChildPath 'ruby\ruby*\bin\'
+'*.bat', '*.cmd' | ForEach-Object {
+  Get-ChildItem -Path (Join-Path -Path $rubyDir -ChildPath $_) | ForEach-Object {
+    Remove-BinFile -Name $_.BaseName -Path $_.FullName
+  }
+}
