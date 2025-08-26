d6a3b55a7635bf7a1ac930168303cf0ce603c560
Chocolatey-AU <chocolatey@realdimensions.net>
2025-08-26 00:25:35 +0000

    AU: 3 updated - freecad vim wesnoth [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/59bc5dbfcc130c919fd8190e2539267663ea897f

--- automatic/wesnoth/tools/chocolateyInstall.ps1
+++ automatic/wesnoth/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = 'wesnoth'
   fileType       = 'exe'
-  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.19.14/wesnoth-1.19.14-win64.exe/download'
+  url            = 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.19.15/wesnoth-1.19.15-win64.exe/download'
   softwareName   = 'Battle for Wesnoth*'
-  checksum       = 'f4505fd37abc85f2f965169d871625cfc05a419ccaaf5e5290226134a4aab57a'
+  checksum       = 'a17bd1fcdc7bebbf4b83cca838e1882bece27d7f092646b88661a0d47b5dcd5e'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
