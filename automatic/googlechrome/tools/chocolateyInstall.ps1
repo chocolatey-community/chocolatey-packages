1e07a8a7c8bc61225dcf8dd3c8d9ab94bac1f827
Chocolatey-AU <chocolatey@realdimensions.net>
2025-08-28 00:22:28 +0000

    AU: 3 updated - GoogleChrome nodejs nodejs.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/de3c0e709a4253a35adfa736fceaf03f0c7b7d19

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '139.0.7258.155'
+$version = '140.0.7339.41'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
