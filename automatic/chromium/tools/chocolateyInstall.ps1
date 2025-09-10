ec395b7b82df30663d94c5581494dd4031edf198
Chocolatey-AU <chocolatey@realdimensions.net>
2025-09-10 18:26:40 +0000

    AU: 2 updated - chromium mattermost-desktop [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/63d2187040a80c9dd598a1c4d767990457524c7a

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '140.0.7339.128'
+$version = '142.0.7407.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://github.com/Hibbiki/chromium-win32/releases/download/v109.0.5414.120-r1070088/mini_installer.sync.exe'
-  checksum      = 'C1AABD6EEF38D759BBD92CA9516353D88C463A1E8389A875B4D12D53DB467F98'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1513802/mini_installer.exe'
+  checksum      = 'C79C74C0921D4A3795960EF43A4B4F1CDBB8BE6AE29A230AC0D2A0201891214E'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/mattermost-desktop/tools/chocolateyInstall.ps1
+++ automatic/mattermost-desktop/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName            = 'mattermost-desktop'
   fileType               = 'msi'
-  file                   = "$toolsDir\mattermost-desktop-5.13.0-win-arm64.msi"
-  file64                 = "$toolsDir\mattermost-desktop-5.13.0-win-x64.msi"
-  checksum               = '3141710FAF79749515FB38CB9C39D7327D81A5492D373BCCEF6304E439FED9BE'
-  checksum64             = 'D32C39D5F80D5FB22B55C5E4D6C673A51978EB3920CC8D239116574F6D79807A'
+  file                   = "$toolsDir\mattermost-desktop-5.13.1-win-arm64.msi"
+  file64                 = "$toolsDir\mattermost-desktop-5.13.1-win-x64.msi"
+  checksum               = '8A345FF386CCB166815EE40CEA3823B4839E688EE672963398CC90F3A63F8DA3'
+  checksum64             = 'F31303B3AEDDC229D315E47356B5B5171B2203FC14CF9FC672383E356996A210'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1"
