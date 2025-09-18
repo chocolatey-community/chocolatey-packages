c842e723bbcb3d3ff53005a37d3f89308e6899c2
Gary Ewan Park <gep13@gep13.co.uk>
2025-09-18 14:34:40 +0100

    (poweriso) Update regex for version and urls
    The layout of the PowerIso download page has changed, so the previous
    regex for finding the information no longer works.  This commit updates
    the main regex for finding the latest version number to be
    "V (?<version>[\d\.]+)"
    and then it extracts the version number.  Following that, rather than
    use some partially hard-coded URL's for the downloads, actually do
    other regex's to locate the URL's, and then pass them through to AU.

--- automatic/poweriso/update.ps1
+++ automatic/poweriso/update.ps1
@@ -18,15 +18,20 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $re = 'PowerISO v([\d.]+)'
-  $version = ($download_page.Content -split "`n") -match $re | Select-Object -first 1
-  if ($version -match $re) { $version = $Matches[1] } else { throw "Can't find version" }
-  $majorVersion = $version -replace "^(\d+).*$", '$1'
+  $versionRegularExpression = 'V (?<version>[\d\.]+),'
+  $version = ($download_page.Content -split "`n") -match $versionRegularExpression | Select-Object -first 1
+  if ($version -match $versionRegularExpression) { $version = $Matches.version } else { throw "Can't find version" }
+
+  $32bitDownloadUrlRegularExpression = 'PowerISO[^-]*\.exe'
+  $64bitDownloadUrlRegularExpression = 'PowerISO.*-x64\.exe'
+
+  $url32     = $download_page.links | Where-Object href -match $32bitDownloadUrlRegularExpression | Select-Object -First 1 -expand href
+  $url64     = $download_page.links | Where-Object href -match $64bitDownloadUrlRegularExpression | Select-Object -First 1 -expand href
 
   @{
-    Version = $version
-    Url32   = "https://www.poweriso.com/PowerISO$majorVersion.exe"
-    Url64   = "https://www.poweriso.com/PowerISO$majorVersion-x64.exe"
+    Version = Get-ChocolateyNormalizedVersion $version
+    Url32   = $url32
+    Url64   = $url64
   }
 }
 
