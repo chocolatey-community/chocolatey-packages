7b76c34db8e1c3fcee62c138ae79c91648cb5005
Chocolatey-AU <chocolatey@realdimensions.net>
2025-09-18 10:17:53 +0000

    AU: 3 updated - 1password 1password8 thunderbird [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/e212620d40660e6e0edd697120e9754dbcae1d29

--- automatic/1password8/tools/chocolateyInstall.ps1
+++ automatic/1password8/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.11.2.msi'
+  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.11.8.msi'
   softwareName   = '1Password*'
-  checksum       = '815467bd2cbdf704a0a694195eb17472f9b1544d2dec77e5fa75d2d1c13b4947'
+  checksum       = '3bf4ef47b760d65d602d8484bc015e4a4ebcab5541ce4474fa3c2391083a6b42'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 1641, 3010)
