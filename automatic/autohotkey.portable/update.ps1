83b0b4c588845dd189707917e1bb61bd551677c9
James Ruskin <james@chocolatey.io>
2025-09-18 12:30:59 +0100

    (autohotkey.portable) Simplifies Update Script
    The build was seeing "(403) forbidden" when trying to find the latest
    version of AutoHotkey. As we were already using GitHub Releases as our
    download location, it made sense to convert to using the GH releases API
    to source the version information too.
    
    On top of this, this commit simplifies the portable update script by
    removing the use of streams.

--- automatic/autohotkey.portable/update.ps1
+++ automatic/autohotkey.portable/update.ps1
@@ -1,7 +1,5 @@
 ﻿Import-Module Chocolatey-AU
 
-$releases = 'https://autohotkey.com/download'
-
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
@@ -10,48 +8,25 @@ function global:au_SearchReplace {
     }
 
     ".\tools\verification.txt"      = @{
-      "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL)"
-      "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL)"
-      "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum)"
-      "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum)"
-      "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL)"
+      "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+      "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+      "(?i)(Get-RemoteChecksum).*" = "`${1} $($Latest.URL32)"
     }
   }
 }
+
 function global:au_BeforeUpdate {
-  Remove-Item "$PSScriptRoot\tools\*.zip"
-  $filePath = "$PSScriptRoot\tools\$($Latest.FileName)"
-  Invoke-WebRequest $Latest.URL -OutFile $filePath -UseBasicParsing
-  $Latest.ChecksumType = 'sha256'
-  $Latest.Checksum = Get-FileHash -Algorithm $Latest.ChecksumType -Path $filePath | ForEach-Object Hash
+  Get-RemoteFiles -Purge -NoSuffix
 }
 
 function global:au_GetLatest {
-  $version_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $LatestRelease = Get-GitHubRelease AutoHotkey AutoHotkey
 
-  $urls = $version_page.Links | Where-Object href -match "^[\d\.]+\/$" | Where-Object href -NotMatch "1\.0" | ForEach-Object href
-
-  $streams = @{}
-  $urls | ForEach-Object {
-    $releasesUrl = "$releases/$_"
-    $versionWithHash = Invoke-WebRequest -Uri "$releasesUrl/version.txt" -UseBasicParsing | ForEach-Object Content
-    $version = $versionWithHash -replace '(\d+.\d+-\w+)-\w+', '$1'
-    $version = $version -replace '(-\w+)\.', '$1'
-    if (!$version) { $version = $versionWithHash }
-
-    $url = "$releasesUrl/AutoHotkey_${versionWithHash}.zip"
-
-    $key = $releasesUrl -split '\/' | Select-Object -last 1 -Skip 1
-    if (!$streams.ContainsKey($key)) {
-      $streams.Add($key, @{
-          Version  = $version
-          URL      = $url
-          FileName = $url -split '/' | Select-Object -Last 1
-        })
-    }
+  @{
+    Version  = Get-Version $LatestRelease.tag_name
+    URL32    = $LatestRelease.assets.Where{$_.name.EndsWith('.zip')}.browser_download_url
+    FileName = $LatestRelease.assets.Where{$_.name.EndsWith('.zip')}.name
   }
-
-  return @{ Streams = $streams }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -NoCheckUrl -ChecksumFor none
