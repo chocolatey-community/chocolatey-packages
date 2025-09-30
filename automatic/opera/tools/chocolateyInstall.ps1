0d5dc0bd6eb60f78cdc67e8bb05a1b4a754acded
Chocolatey-AU <chocolatey@realdimensions.net>
2025-09-30 09:40:32 +0000

    AU: 2 updated - librecad Opera [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/f2a4ebd898e48e58f4acfbb1e8acddce2acdf195

--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -11,17 +11,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/122.0.5643.71/win/Opera_122.0.5643.71_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/122.0.5643.71/win/Opera_122.0.5643.71_Setup_x64.exe'
-  checksum       = '3e9714c7add62947989dcce6d18573904784cdd0f40379f4b595fa61d030f3e0'
-  checksum64     = 'ad157c01c822b54b33cbbf10030a50db94315d85a45ddc6173e6f14aabc7c90a'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/122.0.5643.92/win/Opera_122.0.5643.92_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/122.0.5643.92/win/Opera_122.0.5643.92_Setup_x64.exe'
+  checksum       = 'ed4841ea549f43775fcb3e985600bf965eeb3361ad868cbfb770d5b5b687acba'
+  checksum64     = '231d6acf7d0dd212ff1c6269bb18fc4c61b9ec31c2fab2048e8febed9972c321'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '122.0.5643.71'
+$version = '122.0.5643.92'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
