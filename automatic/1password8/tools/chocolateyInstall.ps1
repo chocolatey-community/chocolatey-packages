fd5c537a6ba3b9612435268c2aa474b8f438f66f
Chocolatey-AU <chocolatey@realdimensions.net>
2025-09-30 18:22:34 +0000

    AU: 4 updated - 1password 1password8 Firefox musescore [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/69bf407c2f5f8443d415c80375f3eca78db71758

--- automatic/1password8/tools/chocolateyInstall.ps1
+++ automatic/1password8/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.11.10.msi'
+  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.11.12.msi'
   softwareName   = '1Password*'
-  checksum       = '02c03350692bb6b262e3cfa8dfcdbfe2dc0016112ed041596f87d396834749a0'
+  checksum       = '05a51b47b2a8c884ce98934050b5cff93a91864eb2dc58f88d8db881e919d456'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 1641, 3010)
