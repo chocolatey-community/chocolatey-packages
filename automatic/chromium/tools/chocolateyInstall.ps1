86309b8357024dac8ce4c77d0fa2fb83edf590e2
Chocolatey-AU <chocolatey@realdimensions.net>
2025-10-16 18:23:39 +0000

    AU: 5 updated - chromium thunderbird winscp winscp.install winscp.portable [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/21a91306d8c438f054680ccf47ab921b93affeb4

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '143.0.7476.0-snapshots'
+$version = '143.0.7477.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1530637/mini_installer.exe'
-  checksum      = '1E7F58A49D996BFC37940E8557993821DC9BDD9F53A167129271B6CE33AEA5E3'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1530911/mini_installer.exe'
+  checksum      = '0E94425FBCEFB686D0B594C809D57FD12080F09B0A23E05EC70F40A2952F2837'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
