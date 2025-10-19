f359aff94be1c26bb92ff700a91bef596c39cee4
Chocolatey-AU <chocolatey@realdimensions.net>
2025-10-19 18:20:10 +0000

    AU: 3 updated - chromium gitextensions k9s [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/ed124267819020d69942a2c1cb2eaf78740935cf

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '143.0.7482.0-snapshots'
+$version = '143.0.7483.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1531934/mini_installer.exe'
-  checksum      = '861954268AE42119DFE7BA51BE986A3EE96BB1583F279B9FD4805494B2D338CB'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1531970/mini_installer.exe'
+  checksum      = '30F68007A064881D1CC72A7766BD372624F42AAC3FD5C66D86C38B9DA6AFF1E1'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/gitextensions/tools/chocolateyInstall.ps1
+++ automatic/gitextensions/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = 'Git Extensions*'
     fileType       = 'msi'
-    file           = "$toolsDir\GitExtensions-x64-5.2.1.18061-0d74cfdc3.msi"
+    file           = "$toolsDir\GitExtensions-x64-6.0.2.18262-f02a06bf5.msi"
     silentArgs     = '/quiet /norestart ADDDEFAULT=ALL REMOVE=AddToPath'
     validExitCodes = @(0, 3010, 1641)
 }
