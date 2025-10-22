6db92c45865755c2f9320934b9d65e0c9dfde677
Chocolatey-AU <chocolatey@realdimensions.net>
2025-10-22 18:20:48 +0000

    AU: 2 updated - mp3tag musescore [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/e2a6ddbc8ae0bda20024c00254e4a8e6afe54dfa

--- automatic/mp3tag/tools/ChocolateyInstall.ps1
+++ automatic/mp3tag/tools/ChocolateyInstall.ps1
@@ -24,8 +24,8 @@ New-Item $iniFile -type file -force -value $iniContent
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\mp3tagv331dsetup.exe"
-  file64         = "$toolsPath\mp3tagv331d-x64-setup.exe"
+  file           = "$toolsPath\mp3tagv331esetup.exe"
+  file64         = "$toolsPath\mp3tagv331e-x64-setup.exe"
   silentArgs     = "/S"
   validExitCodes = @(0)
 }
