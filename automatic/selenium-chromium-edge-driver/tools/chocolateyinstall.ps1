72821637bdc7fa579624e4754c06258f3bf118aa
Chocolatey-AU <chocolatey@realdimensions.net>
2025-11-09 00:20:56 +0000

    AU: 3 updated - papercut selenium-chromium-edge-driver vim [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/b18959ca6f348c50260f862c2aa5b455c21ca4b2

--- automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
+++ automatic/selenium-chromium-edge-driver/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $parameters = Get-PackageParameters
 
 $packageArgs = @{
   packageName    = 'selenium-chromium-edge-driver'
-  url            = 'https://msedgedriver.microsoft.com/142.0.3595.65/edgedriver_win32.zip'
-  url64          = 'https://msedgedriver.microsoft.com/142.0.3595.65/edgedriver_win64.zip'
-  checksum       = '81e7127f635580cd89393c662f3a4fb750af6b9e51a4ef53c4188436c7e37c22'
-  checksum64     = '1f21a60b93a5bc7fdd6cfb54bd2cb1bb92704a85513781290c3340cb84ae1d9c'
+  url            = 'https://msedgedriver.microsoft.com/142.0.3595.69/edgedriver_win32.zip'
+  url64          = 'https://msedgedriver.microsoft.com/142.0.3595.69/edgedriver_win64.zip'
+  checksum       = 'fd2d6a5272e47e24b403446fac7febce2b97f1dadd55a420ecdbf124e0bd11b2'
+  checksum64     = '3095f3d06e77660711dab5eb1d705346d74608cc03ed315f3585615890d7c32d'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   unzipLocation  = $seleniumDir
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.1.1898_x86.zip"
-  file64        = "$toolsDir\gvim_9.1.1898_x64.zip"
+  file          = "$toolsDir\gvim_9.1.1900_x86.zip"
+  file64        = "$toolsDir\gvim_9.1.1900_x64.zip"
 }
 
 $installArgs = @{
