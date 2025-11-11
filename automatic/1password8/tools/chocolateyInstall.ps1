795d6a7c9525c92e262f0140f23abe5c1e613baa
Chocolatey-AU <chocolatey@realdimensions.net>
2025-11-11 18:21:55 +0000

    AU: 6 updated - 1password 1password8 clipgrab papercut vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/134d3e3e65532942459086eb90d7ff89bd68d637

--- automatic/1password8/tools/chocolateyInstall.ps1
+++ automatic/1password8/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.11.16.msi'
+  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.11.18.msi'
   softwareName   = '1Password*'
-  checksum       = '33dcfe064bac5334c843adc3bf784f9bcdbe9053259b75cce07fb24b67fe62a7'
+  checksum       = 'e68ebbfca75a999819127c28b8c231af9281f5c946fcf7abfbdcdd35b5b12c22'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 1641, 3010)
