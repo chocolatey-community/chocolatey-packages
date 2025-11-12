304fc2f86cfb37525dab3ea62eb6db66d2ad1624
Chocolatey-AU <chocolatey@realdimensions.net>
2025-11-12 06:22:48 +0000

    AU: 6 updated - chromium cmake cmake.install cmake.portable etcd Firefox [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/ebb82b773c376de429146aea760a9077fde58e4d

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '144.0.7524.0-snapshots'
+$version = '144.0.7525.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1543263/mini_installer.exe'
-  checksum      = '18F9AB1F2AFE6F1C845F9713D2F6DF0F644242501E298B48234B57AA642EBF0F'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1543504/mini_installer.exe'
+  checksum      = '194B0D810A1BBE4DE3A03C542DE64CCC1393DB13CC57D4EB001068E045FC3A9B'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
