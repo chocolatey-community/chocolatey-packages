8f3921b06a2686b79c2ddd55671013bc1e5f6ddb
Chocolatey-AU <chocolatey@realdimensions.net>
2025-11-14 06:22:01 +0000

    AU: 1 updated - mattermost-desktop [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/cc88b0219b2b3ee2405de7089b76ed5bb7ce3383

--- automatic/mattermost-desktop/tools/chocolateyInstall.ps1
+++ automatic/mattermost-desktop/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName            = 'mattermost-desktop'
   fileType               = 'msi'
-  file                   = "$toolsDir\mattermost-desktop-5.13.2-win-arm64.msi"
-  file64                 = "$toolsDir\mattermost-desktop-5.13.2-win-x64.msi"
-  checksum               = '4FCE4BF8AEAAC928BC8EA3C6D610E80A3FE58F9A197DAAC988F9318C9E269255'
-  checksum64             = 'D3C7F8A92BC5D7651FE2352A1644CDF4937E521170089FDD39FB8BD513BC56BC'
+  file                   = "$toolsDir\mattermost-desktop-6.0.0-win-arm64.msi"
+  file64                 = "$toolsDir\mattermost-desktop-6.0.0-win-x64.msi"
+  checksum               = '8E6795DF6D4460416F782AB1A70C4B03736CA9590CF20444E0B70163D22D5CC7'
+  checksum64             = 'B994D941A92CF441717239D7E63B7F26F3823CC52845FF5094F1804C98B16D26'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1"
