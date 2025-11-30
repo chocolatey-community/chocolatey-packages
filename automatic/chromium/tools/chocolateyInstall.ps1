109f0c64b1e65c86463d8b016feacb013f27ac33
Chocolatey-AU <chocolatey@realdimensions.net>
2025-11-30 12:20:34 +0000

    AU: 2 updated - chromium graphviz [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/fcd9369313fb930c9542228ab28ce3c2289c31b1

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '144.0.7555.0-snapshots'
+$version = '144.0.7556.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1551766/mini_installer.exe'
-  checksum      = 'EF1560ADB5A6C8A856B7EFA9925705BD369359FD703C2F5A779AFB9C7931572E'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1551816/mini_installer.exe'
+  checksum      = '8420728CFF25EB849166A2418D11A735C89FE4321EF98419FB58B3E3C7136B3C'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
