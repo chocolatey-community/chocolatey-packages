fb2c8619e164ddd1e17ba90878f1501699f498ab
Peter J. Mello <RogueScholar@users.noreply.github.com>
2026-01-02 18:25:00 -0800

    Add stream for Ruby v4.0.x to installation JSON
    With Ruby having released a new major version, RubyInstaller subsequently
    released their first installer package for Windows in the 4.0.x series. This
    commit informs the AU PowerShell module that URIs with version strings beginning
    with 4.0 are valid and to process them as a new stream.
    
    It also removes the logic used to isolate and record packages for x86 Windows
    (32-bit) concommitant with their decision to cease publishing them for all new
    release versions.
    
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- automatic/ruby.install/tools/chocolateyInstall.ps1
+++ automatic/ruby.install/tools/chocolateyInstall.ps1
@@ -9,16 +9,20 @@ $installDir = if ($pp['InstallDir']) { $pp['InstallDir'] } else { Join-Path (Get
 $tasks = 'assocfiles', 'noridkinstall'
 if ( !$pp['NoPath'] ) { $tasks += 'modpath' }
 
-Write-Host "Ruby is going to be installed in '$installDir'"
+Write-Information "Ruby is going to be installed in '$installDir'"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  file           = "$toolsPath\rubyinstaller-3.4.8-1-x86.exe"
-  file64         = "$toolsPath\rubyinstaller-3.4.8-1-x64.exe"
+  file64         = "$toolsPath\rubyinstaller-4.0.0-1-x64.exe"
   silentArgs     = '/verysilent /allusers /dir="{0}" /tasks="{1}"' -f $installDir, ($tasks -join ',')
   validExitCodes = @(0)
   softwareName   = 'ruby *'
 }
 Install-ChocolateyInstallPackage @packageArgs
-Get-ChildItem $toolsPath\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" '' } }
+Get-ChildItem $toolsPath\*.exe | ForEach-Object {
+  Remove-Item $_ -ea 0
+  if (Test-Path $_) {
+    Set-Content "$_.ignore" ''
+  }
+}
--- automatic/ruby.install/update.ps1
+++ automatic/ruby.install/update.ps1
@@ -6,14 +6,11 @@ function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
       "(?i)(^\s*fileType\s*=\s*)('.*')"           = "`$1'$($Latest.FileType)'"
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*"   = "`${1}$($Latest.FileName32)`""
       "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
     }
 
     ".\legal\VERIFICATION.txt"      = @{
-      "(?i)(\s+x32:).*"              = "`${1} $($Latest.URL32)"
       "(?i)(\s+x64:).*"              = "`${1} $($Latest.URL64)"
-      "(?i)(checksum32:).*"          = "`${1} $($Latest.Checksum32)"
       "(?i)(checksum64:).*"          = "`${1} $($Latest.Checksum64)"
       "(?i)(Get-RemoteChecksum).*"   = "`${1} $($Latest.URL64)"
       "(?i)(is obtained from:\s*).*" = "`${1} $($Latest.LicenseUrl)"
@@ -52,22 +49,28 @@ function GetStreams() {
 
   $x64releaseUrls | ForEach-Object {
     $version = $_ -replace '\-([\d]+)', '.$1' -replace 'rubyinstaller.' -replace 'ruby.' -split '/' | Select-Object -Last 1 -Skip 1
-    if ($version -match '[a-z]') { Write-Host "Skipping prerelease: '$version'"; return }
+    if ($version -match '[a-z]') {
+      Write-Information "Skipping prerelease: '$version'"
+      return
+    }
     $versionTwoPart = $version -replace '([\d]+\.[\d]+).*', "`$1"
 
     if ($streams.$versionTwoPart) { return }
 
     $url64 = $_ | Select-Object -ExpandProperty href
-    $url32 = $releaseUrls | Where-Object href -notmatch "$re64|arm" | Where-Object href -match $version | Select-Object -ExpandProperty href
 
-    if (!$url32 -or !$url64) {
-      Write-Host "Skipping due to missing installer: '$version'"; return
+    if (!$url64) {
+      Write-Information "Skipping due to missing installer: '$version'"
+      return
     }
 
-    $streams.$versionTwoPart = @{ URL32 = $url32 ; URL64 = $url64 ; Version = Get-FixVersion -Version $version -OnlyFixBelowVersion "2.5.4" }
+    $streams.$versionTwoPart = @{
+      URL64 = $url64
+      Version = Get-FixVersion -Version $version -OnlyFixBelowVersion "2.5.4"
+    }
   }
 
-  Write-Host $streams.Count 'streams collected'
+  Write-Information $streams.Count 'streams collected'
   $streams
 }
 
