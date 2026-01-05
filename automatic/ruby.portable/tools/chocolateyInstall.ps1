007494dfb49a77246f80a842628d58f78ae87b5a
Peter J. Mello <admin@petermello.net>
2026-01-05 19:40:32 +0000

    Update ruby{,.install,.portable} packages to v4.x
    Add streams for Ruby v4.0.x to JSON objects.
    
    With Ruby having released a new major version
    (v4), RubyInstaller subsequently released their
    first installer packages for Windows in the v4.0.x
    series. This commit informs the AU PowerShell
    module that URIs with version strings beginning
    with 4.0 are valid to process as a new stream.
    
    It also removes the logic used to isolate and
    record packages for x86 Windows (32-bit),
    concomitant with their decision to cease
    publishing them for all future release versions.
    
    Signed-off-by: Peter J. Mello <admin@petermello.net>

--- automatic/ruby.portable/tools/chocolateyInstall.ps1
+++ automatic/ruby.portable/tools/chocolateyInstall.ps1
@@ -1,21 +1,32 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$toolsPath = "$(Split-Path -Path ${MyInvocation}.MyCommand.Definition -Parent)"
+$rubyDir = "$(Join-Path -Path ${toolsPath} -ChildPath (
+  Join-Path -Path 'ruby' -ChildPath 'ruby*' -ChildPath 'bin'
+) -Resolve)"
+
+Write-Information "Ruby portable interpreter directory: '${rubyDir}'."
+
+$exeName = 'rubyinstaller-4.0.2-1-x64.7z'
+$exePath = "$(Join-Path -Path ${toolsPath} -ChildPath ${exeName})"
 
 $packageArgs = @{
-  File        = "$toolsPath\rubyinstaller-3.4.9-1-x86.7z"
-  File64      = "$toolsPath\rubyinstaller-3.4.9-1-x64.7z"
-  Destination = "$toolsPath\ruby"
-  PackageName = $env:ChocolateyPackageName
+  File64      = ${exePath}
+  Destination = (Join-Path -Path ${toolsPath} -ChildPath 'ruby')
+  PackageName = ${Env:ChocolateyPackageName}
 }
 
 Get-ChocolateyUnzip @packageArgs
 
-# Add shims for .bat and .cmd files, in the bin folder, that Chocolatey CLI will not shim
-$rubyDir = Join-Path -Path $toolsPath -ChildPath 'ruby\ruby*\bin\'
-'*.bat', '*.cmd' | ForEach-Object {
-  Get-ChildItem -Path (Join-Path -Path $rubyDir -ChildPath $_) | ForEach-Object {
-    Install-BinFile -Name $_.BaseName -Path $_.FullName
-  }
+# Add shims for .bat and .cmd files, in the bin folder, that Chocolatey CLI will
+# not shim.
+Get-ChildItem -Path "${rubyDir}\*" -Include (
+  '*.bat',
+  '*.cmd'
+) -File | ForEach-Object {
+  Install-BinFile -Name $_.BaseName -Path $_.FullName
 }
 
-Get-ChildItem $toolsPath\*.7z | ForEach-Object { Remove-Item $_ -ea 0 }
+Get-ChildItem -Path ${toolsPath} -Filter '*.7z' -File | ForEach-Object {
+  Remove-Item -LiteralPath $_.FullName -Force -ErrorAction SilentlyContinue
+}
