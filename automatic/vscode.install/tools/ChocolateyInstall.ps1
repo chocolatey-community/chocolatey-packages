621f63669f149f127970cc61d9d8730521c5cfdf
Alessandro Riolo <alessandro.riolo@gmail.com>
2026-01-16 16:21:55 +0000

    upgrade vscode to 1.108.0 #1
    upgrade vscode to 1.108.0 #1

--- automatic/vscode.install/tools/ChocolateyInstall.ps1
+++ automatic/vscode.install/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
 $softwareName = 'Microsoft Visual Studio Code'
-$version = '1.107.1'
+$version = '1.108.0'
 if ($version -eq (Get-UninstallRegistryKey "$softwareName").DisplayVersion) {
   Write-Host "VS Code $version is already installed."
   return
@@ -16,11 +16,11 @@ Close-VSCode
 $packageArgs = @{
   packageName    = 'vscode.install'
   fileType       = 'exe'
-  url64bit       = 'https://update.code.visualstudio.com/1.107.1/win32-x64/stable'
+  url64bit       = 'https://update.code.visualstudio.com/1.108.0/win32-x64/stable'
 
   softwareName   = "$softwareName"
 
-  checksum64     = '91fe774ecfa2e3fbe8c2e9c7cb680ecc5869ca0d9aac6ea08764d286284c2f44'
+  checksum64     = 'A5FC0EE29DF37DA61D5A5244581A91E318EDD5ADC64D487289CE34459C390D4D'
   checksumType64 = 'sha256'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
