4b75c7ebba1728a3cb59a8215f64e41c660a5a5e
Chocolatey-AU <chocolatey@realdimensions.net>
2026-02-10 18:39:25 +0000

    AU: 2 updated - nodejs nodejs.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/4e48dd56a8650f8f841cbd28f5e6b27599fe4909

--- automatic/nodejs.install/tools/chocolateyInstall.ps1
+++ automatic/nodejs.install/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   FileType       = 'msi'
   SoftwareName   = 'Node.js'
   File           = ''
-  File64         = "$toolsPath\node-v25.6.0-x64.msi"
+  File64         = "$toolsPath\node-v25.6.1-x64.msi"
   SilentArgs     = '/quiet ADDLOCAL=ALL'
   ValidExitCodes = @(0)
 }
