116dc68141d98c37e7c224cb05e6ccf3042137f1
Chocolatey-AU <chocolatey@realdimensions.net>
2026-02-13 00:38:35 +0000

    AU: 3 updated - chromium GoogleChrome vim [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/ab0559c233562f34b1d434e92265806860892b74

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '147.0.7684.0-snapshots'
+$version = '147.0.7685.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1583676/mini_installer.exe'
-  checksum      = 'C76CBC48258770B4C29050CF9DF8CC495667087FB24F368003C91A725C397706'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1584192/mini_installer.exe'
+  checksum      = '0B8598BE50352FAE3FA21990E1B4C8738F3A4A4EDBD464CBF7AFE5FAB328DBA7'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '145.0.7632.46'
+$version = '145.0.7632.68'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = 'ec9a77e2d61440f62be2106735d9e3e295e2388e5dc3ef8620f01b216dd8c630'
-  checksum64             = '45e7e3c6f8e3cf2bfc4553e7332cccbb79430985c997d2bd1c0c84ec77482076'
+  checksum               = 'c785ff0a64d2bafd50827f108c20a3327d94ebd1d065733a79d818b214a9c686'
+  checksum64             = '5be504c983761650f30bb3b348145a5acc36be8bbdb520865267b0902cc5fdbf'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
