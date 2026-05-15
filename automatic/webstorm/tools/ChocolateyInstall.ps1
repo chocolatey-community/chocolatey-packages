b343a877fd5f7338f9000b32d2655a80b0be45b5
Chocolatey-AU <chocolatey@realdimensions.net>
2026-05-15 12:37:05 +0000

    AU: 2 updated - poweriso webstorm [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/b693162c24dfe1694277086da871892b26711ca7

--- automatic/webstorm/tools/ChocolateyInstall.ps1
+++ automatic/webstorm/tools/ChocolateyInstall.ps1
@@ -19,8 +19,8 @@ New-Item -ItemType Directory -Force -Path $installDir
 $arguments              = @{
     packageName         = $env:ChocolateyPackageName
     softwareName        = 'WebStorm *'
-    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2026.1.1.exe'
-    checksum            = 'b4069019e3c0ee962f1d5fa93ecb66d5f911e9de63f87a410d86c23bb3945048'
+    url                 = 'https://download.jetbrains.com/webstorm/WebStorm-2026.1.2.exe'
+    checksum            = 'ff2a17927caa426e0cc5743319c371ec791639c3a67c18fd8470920d62982b1f'
     fileType            = 'exe'
     checksumType        = 'sha256'
     silentArgs          = $silentArgs
