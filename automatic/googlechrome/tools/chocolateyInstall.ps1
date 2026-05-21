ef0ed1bf393f035aebea74fde0c696a948eb1ec9
Chocolatey-AU <chocolatey@realdimensions.net>
2026-05-21 00:37:48 +0000

    AU: 4 updated - GoogleChrome notepadplusplus notepadplusplus.commandline notepadplusplus.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/68933350dfd31ca232e54f45e169bba9972c633a

--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '148.0.7778.179'
+$version = '149.0.7827.22'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
