e4b03885304fe91a2147eda85759047c994344ad
Chocolatey-AU <chocolatey@realdimensions.net>
2026-06-10 18:37:45 +0000

    AU: 8 updated - chromium jenkins python python3 python314 vscode vscode.install zotero [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '151.0.7886.0-snapshots'
+$version = '151.0.7887.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1644540/mini_installer.exe'
-  checksum      = '473B6DD23F0CDFBDC60EFF83B27DF0DB112AB5CBEBCE28073BD068299C7A7537'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1644672/mini_installer.exe'
+  checksum      = 'F4883795FBC12BB7297761C9C1D8F237249063D69BBE4BA34AF83B9BA33E9D13'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
