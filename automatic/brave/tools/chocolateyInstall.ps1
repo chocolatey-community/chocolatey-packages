1762b7a7d7099fc5be20420b84a46935c9a4a8d0
Chocolatey-AU <chocolatey@realdimensions.net>
2026-06-11 12:38:06 +0000

    AU: 4 updated - brave msys2 musescore spotify [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/85cb253c5cc87ed611df6f857d53287c8265b559

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.91.171/BraveBrowserStandaloneSilentSetup32.exe'
-  checksum    = '401AFF780C13FDBFEE46DBA6058E5CCB18F50B66A8EC7A021914368206B15BAF'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.92.120/BraveBrowserStandaloneSilentBetaSetup32.exe'
+  checksum    = '40FCF3AD25D822356FA06186697A95350D5A517FEFAEF24A01A75EB09E866B41'
   checksumType= 'sha256'
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.91.171'
+[version]$softwareVersion = '1.92.120'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
