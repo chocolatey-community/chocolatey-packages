e0ee30e8c9bbceacbfffaffc1b2fe60e1f52b0c3
AdmiringWorm <kim@chocolatey.io>
2026-06-16 11:04:14 +0200

    (lockhunter) Update checksums [PUSH lockhunter]

--- manual/lockhunter/tools/chocolateyInstall.ps1
+++ manual/lockhunter/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:chocolateyPackageName
   fileType       = 'EXE'
   url            = 'https://lockhunter.com/assets/exe/lockhuntersetup_3-4-3.exe'
-  checksum       = '02F738111A7EF929B8017277109FF3CB188ED0896FA385B79205DA888AFA266D'
+  checksum       = '2BA380C9A643CAF04A17F41D22722595BC3B62BF1855D5DE04816F1D1C2DBEA9'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
