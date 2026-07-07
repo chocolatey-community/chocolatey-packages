f08655303d589113b91c2e79928c282ef0aa2c71
Chocolatey-AU <chocolatey@realdimensions.net>
2026-07-07 06:36:08 +0000

    AU: 2 updated - vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/f8020b2e0570cfd23a808ccb1459689d432ee6e0

--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,11 +22,11 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url64bit       = 'https://vscode.download.prss.microsoft.com/dbazure/download/insider/bbefcbd1189ee487d66e8e03b4180553a70e3820/VSCodeSetup-x64-1.128.0-insider.exe'
+  url64bit       = 'https://vscode.download.prss.microsoft.com/dbazure/download/insider/bff8736385420572b86b407d6ba73bb97961166f/VSCodeSetup-x64-1.128.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum64     = '80d148804c2cea5dbe2acb893e18fcf50e73f189c4e1d28d71b2e55b847936ec0861bcbeb5786c6928349a605eb5d0c46b61dce33552c80ae1d3963d07406aa7'
+  checksum64     = '0d783682cb41829585acab0bea5a1958fe91eba9a592992b770c2d5b10ceb0ca31122bf9dc9ab807891aae4c21451c129bad2ea8379f943bebbe736257d51a64'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
