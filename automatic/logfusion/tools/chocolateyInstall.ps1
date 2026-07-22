fa748dffa4bc27c3c62a358a7ef93cf44b7aca34
Chocolatey-AU <chocolatey@realdimensions.net>
2026-07-22 00:36:30 +0000

    AU: 4 updated - logfusion openshift-cli vscode-insiders vscode-insiders.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/a326b02c1d927753dd9785e4f6d833e8bc0e3c03

--- automatic/logfusion/tools/chocolateyInstall.ps1
+++ automatic/logfusion/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $arguments          = @{
     packageName     = $env:ChocolateyPackageName
     softwareName    = 'LogFusion'
-    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/117/LogFusionSetup-7.1.exe'
-    checksum        = '8047cc698fba9d937f373380567b2934d2a506f7b22e38c5cf043dcc9c9829ce'
+    url             = 'https://binaryfortressdownloads.com/Download/BFSFiles/117/LogFusionSetup-7.2.exe'
+    checksum        = '603229b23af26e899f4186123013cd4d321d80003ccdd922544bee236d8658a5'
     fileType        = 'exe'
     checksumType    = 'sha256'
     silentArgs      = '/VERYSILENT /LAUNCHAFTER=0'
--- automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
+++ automatic/vscode-insiders.install/tools/chocolateyInstall.ps1
@@ -22,11 +22,11 @@ function Get-MergeTasks {
 $packageArgs = @{
   packageName    = "$env:ChocolateyPackageName"
   fileType       = 'exe'
-  url64bit       = 'https://vscode.download.prss.microsoft.com/dbazure/download/insider/0e04915922a9556b77ceae50a86a9d5ee9a61371/VSCodeSetup-x64-1.130.0-insider.exe'
+  url64bit       = 'https://vscode.download.prss.microsoft.com/dbazure/download/insider/d7465d6f56c9afb8ed3696a9eaeb4f3f6d62156d/VSCodeSetup-x64-1.131.0-insider.exe'
 
   softwareName   = 'Microsoft Visual Studio Code Insiders'
 
-  checksum64     = '0de0e013d3caf0beac69c7096b0217d52e7afbb013320ffd62a81708c5f3c9be750ea208ba6976add4e39076b463c6eda72cc0dfa334bddf767782bfaaaee01d'
+  checksum64     = '555b28d77162db30c9724c9865f0895c37e94157808fd4d7beb29dcfab5cac1ac2bed76ef567981754e212c6520ef7267af3ca250c42bd7cb43fde99326c08af'
   checksumType64 = 'sha512'
 
   silentArgs     = '/verysilent /suppressmsgboxes /mergetasks="{0}" /log="{1}\install.log"' -f (Get-MergeTasks), (Get-PackageCacheLocation)
