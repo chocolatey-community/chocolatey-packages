464470fb234075e8d638d83c46f645fec6c35513
Chocolatey-AU <chocolatey@realdimensions.net>
2026-07-29 00:37:33 +0000

    AU: 5 updated - 1password 1password8 openshift-cli php vim [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/39f3ac4612849e16ff2b26ef33227be33c53e1c8

--- automatic/1password8/tools/chocolateyInstall.ps1
+++ automatic/1password8/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.12.28.msi'
+  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.12.30.msi'
   softwareName   = '1Password*'
-  checksum       = 'c7fd4808fba19af902584551b0aac92b9cf16542ede546fbe42d65e286abde90'
+  checksum       = '65bbda1a2efea756625be2b1698b6f3b73e14830f20ec142dc98d33c73ffcbfa'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 1641, 3010)
--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.2.0865_x86.zip"
-  file64        = "$toolsDir\gvim_9.2.0865_x64.zip"
+  file          = "$toolsDir\gvim_9.2.0870_x86.zip"
+  file64        = "$toolsDir\gvim_9.2.0870_x64.zip"
 }
 
 $installArgs = @{
