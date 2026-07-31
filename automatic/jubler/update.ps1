5d0e0d942643bf1862fdfb72f03b75bf1ceecf2e
Panayotis Katsaloulis <panayotis@panayotis.com>
2026-07-31 22:15:38 +0300

    jubler: update to 10.0.0 and track GitHub releases

--- automatic/jubler/update.ps1
+++ automatic/jubler/update.ps1
@@ -1,10 +1,7 @@
-﻿Import-Module Chocolatey-AU
+Import-Module Chocolatey-AU
 Import-Module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
 
-$releases = 'http://www.jubler.org/download.html'
-$softwareName = 'Jubler subtitle editor'
-
-function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix }
+$releases = 'https://github.com/teras/Jubler/releases/latest'
 
 function global:au_AfterUpdate {
   Update-ChangelogVersion -version $Latest.Version
@@ -13,20 +10,14 @@ function global:au_AfterUpdate {
 function global:au_SearchReplace {
   @{
     ".\legal\VERIFICATION.txt" = @{
-      "(?i)(^\s*location on\:?\s*)\<.*\>" = "`${1}<$releases>"
-      "(?i)(\s*32\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL32)>"
-      "(?i)(\s*64\-Bit Software.*)\<.*\>" = "`${1}<$($Latest.URL64)>"
-      "(?i)(^\s*checksum\s*type\:).*" = "`${1} $($Latest.ChecksumType32)"
-      "(?i)(^\s*checksum(32)?\:).*" = "`${1} $($Latest.Checksum32)"
-      "(?i)(^\s*checksum64\:).*" = "`${1} $($Latest.Checksum64)"
+      "(?i)(64\-Bit software\:\s*)\<.*\>" = "`${1}<$($Latest.URL64)>"
+      "(?i)(^\s*checksum\s*type\:).*"     = "`${1} $($Latest.ChecksumType64)"
+      "(?i)(^\s*checksum\:).*"            = "`${1} $($Latest.Checksum64)"
     }
-    ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
-      "(?i)(^\s*file\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName32)`""
-      "(?i)(^\s*file64\s*=\s*`"[$]toolsPath\\).*" = "`${1}$($Latest.FileName64)`""
-    }
-    ".\tools\chocolateyUninstall.ps1" = @{
-      "(?i)^(\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+    ".\tools\chocolateyinstall.ps1" = @{
+      "(?i)^(\s*url64bit\s*=\s*)'.*'"       = "`${1}'$($Latest.URL64)'"
+      "(?i)^(\s*checksum64\s*=\s*)'.*'"     = "`${1}'$($Latest.Checksum64)'"
+      "(?i)^(\s*checksumType64\s*=\s*)'.*'" = "`${1}'$($Latest.ChecksumType64)'"
     }
   }
 }
@@ -34,24 +25,17 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
-  $re    = 'jubler.*\.exe'
-  $urls   = $download_page.links | Where-Object href -match $re | Select-Object -First 2 -expand href
+  $re  = 'Jubler-.*-x64\.exe$'
+  $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
+  if ($url -notmatch '^https?://') { $url = 'https://github.com' + $url }
 
-  $version  = $urls[0] -split '\/v?' | Select-Object -Last 1 -Skip 1
+  if ($url -match 'Jubler-([\d.]+)-x64\.exe') { $version = $Matches[1] }
 
   @{
-    URL32 = $urls -notmatch "64\.exe" | Select-Object -first 1
-    URL64 = $urls -match "64\.exe" | Select-Object -first 1
-    Version = $version
+    URL64    = $url
+    Version  = $version
     FileType = 'exe'
   }
 }
 
-
-try {
-    update -ChecksumFor none
-} catch {
-    $ignore = "Unable to connect to the remote server"
-    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' } else { throw $_ }
-}
-
+update -ChecksumFor 64
