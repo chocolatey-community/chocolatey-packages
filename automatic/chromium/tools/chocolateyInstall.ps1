cbfed18d1829a4b5b6d2d999328ec36e146382e8
Chocolatey-AU <chocolatey@realdimensions.net>
2026-08-04 18:37:06 +0000

    AU: 7 updated - chromium Firefox openshift-cli python python3 python315 spotify [skip ci]

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 . (Join-Path $toolsDir 'helper.ps1')
 
-$version = '153.0.7991.0-snapshots'
+$version = '153.0.7992.0-snapshots'
 $hive = "hkcu"
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $Chromium = $hive + ":" + $chromium_string
@@ -15,8 +15,8 @@ if (Test-Path $Chromium) {
 
 $packageArgs = @{
   packageName   = 'chromium'
-  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1673193/mini_installer.exe'
-  checksum      = 'D59D69E68487708E21BA04B4AA0047D13C06DDD40564525FB36023B4DBF3CBA5'
+  url           = 'https://storage.googleapis.com/chromium-browser-snapshots/Win/1673287/mini_installer.exe'
+  checksum      = '42C1B0F129F0BE91F34704E68757B8B6577A9ADF281710E49405DED92108EA17'
   checksumType  = 'sha256'
   file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
