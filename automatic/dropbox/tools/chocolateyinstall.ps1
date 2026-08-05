d3fbd286b5cc6e19665a16b5a2b98587e2577488
Chocolatey-AU <chocolatey@realdimensions.net>
2026-08-05 00:40:50 +0000

    AU: 4 updated - dropbox GoogleChrome openshift-cli prey [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/60a1be878d9c77e2bd991c4074fef4573eee9d9a

--- automatic/dropbox/tools/chocolateyinstall.ps1
+++ automatic/dropbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ if (!$PSScriptRoot) {
 }
 . "$PSScriptRoot\helper.ps1"
 
-$version = '264.3.3345'
+$version = '264.4.3385'
 
 if (!(IsVersionAlreadyInstalled $version)) {
   $stop_dropbox = if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) { $false } else { $true }
@@ -13,10 +13,10 @@ if (!(IsVersionAlreadyInstalled $version)) {
   $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = "Dropbox"
-    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20264.3.3345%20Offline%20Installer.x86.exe'
-    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20264.3.3345%20Offline%20Installer.x64.exe'
-    checksum       = '12ce8012b7a311ce8b9152deacf8e1023ea6ea81af8a54d80bb411277f8a1fdb'
-    checksum64     = '59cfbd547a9ecac67d4854b90c4431dfe35d48edf47a702f7eaa9d87c0d67c52'
+    url            = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20264.4.3385%20Offline%20Installer.x86.exe'
+    url64          = 'https://edge.dropboxstatic.com/dbx-releng/client/Dropbox%20264.4.3385%20Offline%20Installer.x64.exe'
+    checksum       = '6f804f366ea049c3d7d7c85876c9258d1299d36b17b1c1b49351d0f6d0ed2da4'
+    checksum64     = '2dab725ae84f2f40902ad58b46393a4a1903f14c151080e61cec917916d9a2e5'
     fileType       = 'exe'
     checksumType   = 'sha256'
     checksumType64 = 'sha256'
--- automatic/googlechrome/tools/chocolateyInstall.ps1
+++ automatic/googlechrome/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '151.0.7922.72'
+$version = '151.0.7922.76'
 if ($version -eq (Get-ChromeVersion)) {
   Write-Host "Google Chrome $version is already installed."
   return
@@ -12,8 +12,8 @@ $packageArgs = @{
   fileType               = 'MSI'
   url                    = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise.msi'
   url64bit               = 'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-  checksum               = 'f3455e4715884dbe7df29343a2ad860a689c1a78e7ca349e694ca45b2e8740ed'
-  checksum64             = 'efedcf81888851eb40aaebf1d09ba0ce73ce2a1ba8b5ffe3ce7dc095b597d0da'
+  checksum               = 'a0d85781106a816d562f01b98ed27affede7104c674dc7f67d4611c4f2aac615'
+  checksum64             = '3c6f4d683f377b16b4480eb4ceaac98430231cfbaf781b2e3c24ebc938a86bd0'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
