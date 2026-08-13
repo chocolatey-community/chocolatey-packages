525b6f52addb467aadb2e2aaabcdca2aae49192b
Chocolatey-AU <chocolatey@realdimensions.net>
2026-08-13 12:36:19 +0000

    AU: 5 updated - brave chromium peazip peazip.install vscode-insiders [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/93a33eb5309e8d7164712698c62120e988000c4f

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.93.136/BraveBrowserStandaloneSilentSetup32.exe'
-  checksum    = 'E60E95974C93507009477F2F945AACB4F5FBAD209BB5179C445EDB4DDFD73931'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.94.109/BraveBrowserStandaloneSilentBetaSetup32.exe'
+  checksum    = '99F3C19129168F7D84AAD673ABBFF211C5BD1CA4154F7303E682F04C6BDDFF96'
   checksumType= 'sha256'
-  file64      = "$toolsPath\BraveBrowserStandaloneSilentSetup.exe"
+  file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.93.136'
+[version]$softwareVersion = '1.94.109'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
