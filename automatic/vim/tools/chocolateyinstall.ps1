08c288f30f70a47e1e4d9621f402119d04b6d46c
Chocolatey-AU <chocolatey@realdimensions.net>
2026-09-03 00:37:09 +0000

    AU: 2 updated - Minikube vim [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/93db480b730cf1ddba20077a16ea935fe1d8fe38

--- automatic/vim/tools/chocolateyinstall.ps1
+++ automatic/vim/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $installDir = Get-InstallDir
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $installDir
-  file          = "$toolsDir\gvim_9.2.1031_x86.zip"
-  file64        = "$toolsDir\gvim_9.2.1031_x64.zip"
+  file          = "$toolsDir\gvim_9.2.1036_x86.zip"
+  file64        = "$toolsDir\gvim_9.2.1036_x64.zip"
 }
 
 $installArgs = @{
