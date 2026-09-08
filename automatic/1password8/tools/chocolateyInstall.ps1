0e016e3c7a59020550da064c7f1c112d998fa06f
Chocolatey-AU <chocolatey@realdimensions.net>
2026-09-08 18:40:14 +0000

    AU: 8 updated - 1password 1password8 Ghostscript Ghostscript.app musescore openshift-cli vscode vscode.install [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/d3aa691df26faa14c012b6242b9bc5671460232e

--- automatic/1password8/tools/chocolateyInstall.ps1
+++ automatic/1password8/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.12.34.msi'
+  url            = 'https://downloads.1password.com/win/1PasswordSetup-8.12.36.msi'
   softwareName   = '1Password*'
-  checksum       = '038702f3035ca13c81970318219bc8a29e001198bdd4a2cae639cc059a30d7e4'
+  checksum       = 'ee86b2d92a6d0b49861da14fe7ee89dc27390b6b6d29a1c74b013a3507dee54d'
   checksumType   = 'sha256'
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 1641, 3010)
