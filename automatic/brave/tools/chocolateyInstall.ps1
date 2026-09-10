76d26acf8a98d5707f0e707578f4e9de28b24b7c
Chocolatey-AU <chocolatey@realdimensions.net>
2026-09-10 12:40:56 +0000

    AU: 3 updated - brave Cygwin Opera [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/84896b809c168b170fcd174bbcc7187b348a6344

--- automatic/brave/tools/chocolateyInstall.ps1
+++ automatic/brave/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  url         = 'https://github.com/brave/brave-browser/releases/download/v1.96.48/BraveBrowserStandaloneSilentBetaSetup32.exe'
-  checksum    = '34D8C741FF2F9D324070ED7950A3D551BD431D0169A53ADAA4A272C18953693D'
+  url         = 'https://github.com/brave/brave-browser/releases/download/v1.96.50/BraveBrowserStandaloneSilentBetaSetup32.exe'
+  checksum    = 'E5784C7ECCD07713EA7142B7441335E8CC726721F53AAA21ECFBFE480150702D'
   checksumType= 'sha256'
   file64      = "$toolsPath\BraveBrowserStandaloneSilentBetaSetup.exe"
 }
 
-[version]$softwareVersion = '1.96.48'
+[version]$softwareVersion = '1.96.50'
 
 Write-Host "Checking already installed version..."
 $installedVersion = Get-InstalledVersion
--- automatic/opera/tools/chocolateyInstall.ps1
+++ automatic/opera/tools/chocolateyInstall.ps1
@@ -11,17 +11,17 @@ $parameters += if ($pp.NoTaskbarShortcut) { " /pintotaskbar=0"; Write-Host "Oper
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://get.geo.opera.com/pub/opera/desktop/135.0.5973.92/win/Opera_135.0.5973.92_Setup.exe'
-  url64          = 'https://get.geo.opera.com/pub/opera/desktop/135.0.5973.92/win/Opera_135.0.5973.92_Setup_x64.exe'
-  checksum       = '7e124e894f7d806fe37b2a758a43087ee2aa2b1870bfabfec236f30599d12ab5'
-  checksum64     = '6478007ace6e1b99e57d5fdc1d03d50f7b2385b96d4de32b39d9d0a737e47132'
+  url            = 'https://get.geo.opera.com/pub/opera/desktop/135.0.5973.123/win/Opera_135.0.5973.123_Setup.exe'
+  url64          = 'https://get.geo.opera.com/pub/opera/desktop/135.0.5973.123/win/Opera_135.0.5973.123_Setup_x64.exe'
+  checksum       = 'b7904c96adaee49d45c1071ced6f129d83e8d191b7c3460a6e67de20a8fe6f57'
+  checksum64     = '0d607b3311e4a2a55f2cbba9f61ef9400ed2050174134d9303112c1748eff6bc'
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/install /silent /launchopera=0 /setdefaultbrowser=0 /allusers=1' + $parameters
   validExitCodes = @(0)
 }
 
-$version = '135.0.5973.92'
+$version = '135.0.5973.123'
 if (!$Env:ChocolateyForce -and (IsVersionAlreadyInstalled $version)) {
   Write-Output "Opera $version is already installed. Skipping download and installation."
 }
