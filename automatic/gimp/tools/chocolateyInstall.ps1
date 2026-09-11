1575b0115ee859064328898fe633638b4ade4eb1
Chocolatey-AU <chocolatey@realdimensions.net>
2026-09-11 06:36:57 +0000

    AU: 1 updated - gimp [skip ci] https://gist.github.com/choco-bot/4bdeeb99f084db66ab47d2afebe95c8e/65c938ffc818cb16019b45aba93661b0665492ab

--- automatic/gimp/tools/chocolateyInstall.ps1
+++ automatic/gimp/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
-$fallbackUrl32 = 'https://download.gimp.org/gimp/v3.2/windows/gimp-3.2.4-setup.exe'
+$fallbackUrl32 = 'https://download.gimp.org/gimp/v3.2/windows/gimp-3.2.6-setup.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = 'https://download.gimp.org/gimp/v3.2/windows/gimp-3.2.4-setup.exe'
+  url            = 'https://download.gimp.org/gimp/v3.2/windows/gimp-3.2.6-setup.exe'
   softwareName   = 'GIMP*'
-  checksum       = 'ec31d757dd82831d201ffcf47ffeac4175df739e0c02d5122e89aeeadfb988cc'
+  checksum       = '9337cccbc01d4098ee7a3dab215b3afbe6ece99c5287c92441d6f12cf541ebca'
   checksumType   = 'sha256'
   silentArgs     = "/VERYSILENT /NORESTART /RESTARTEXITCODE=3010 /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes = @(0)
